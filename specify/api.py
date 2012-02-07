5e411ae72f8039e3c42cf256cf28f45135549834
Ben Anhalt <anhalt@ku.edu>
2012-02-06 18:06:13 -0600

    tastypie api includes foreignkeys now

--- specify/api.py
+++ specify/api.py
@@ -1,4 +1,5 @@
 from tastypie.resources import ModelResource
+from tastypie.fields import ForeignKey
 from django.db.models import get_app, get_models
 
 resources = []
@@ -8,6 +9,14 @@ for model in get_models(get_app('specify')):
         queryset = model.objects.all()
 
     clsname = model.__name__ + 'Resource'
-    cls = type(clsname, (ModelResource,), {'Meta': Meta,})
+
+    attrs = {'Meta': Meta}
+    for f in model._meta.fields:
+        if not f.rel: continue
+        relname = __name__ + '.' + \
+            f.related.parent_model.__name__ + 'Resource'
+        attrs[f.name] = ForeignKey(relname, f.name, null=f.null)
+
+    cls = type(clsname, (ModelResource,), attrs)
     globals()[clsname] = cls
     resources.append(cls)
--- specify/urls.py
+++ specify/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
 from tastypie.api import Api
-from tastypie.resources import ModelDeclarativeMetaclass
 from specify.api import resources
 
 api = Api(api_name='specify')
