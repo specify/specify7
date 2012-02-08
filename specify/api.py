e0166be0feaf0765180b3b9aeff39a2b445c4450
Ben Anhalt <anhalt@ku.edu>
2012-02-08 14:03:20 -0600

    now possible to specify a list of one to many relationships for the api

--- specify/api.py
+++ specify/api.py
@@ -1,9 +1,26 @@
 from tastypie.resources import ModelResource
 from tastypie.fields import ForeignKey, ToManyField
-from django.db.models import get_app, get_models
-from specify.models import Collectionobject, Determination
+from django.db.models import get_app, get_model, get_models
 
-resources = []
+app = get_app('specify')
+
+to_many_relationships = {
+    'Collectionobject': ['Determination', 'Preparation'],
+}
+
+def make_to_many_field(model, related):
+    related_model = getattr(app, related)
+    def get_related_objs(bundle):
+        filter_args = {model.__name__.lower(): bundle.obj}
+        return related_model.objects.filter(**filter_args)
+    related_resource = "%s.%s%s" % (__name__, related, "Resource")
+    return ToManyField(related_resource, get_related_objs)
+
+def make_resource_with_foreignkeys(model, attrs={}):
+    clsname = model.__name__ + 'Resource'
+    attrs.update(get_fk_fields(model))
+    attrs['Meta'] = make_standard_meta(model)
+    return type(clsname, (ModelResource,), attrs)
 
 def get_fk_fields(model):
     attrs = {}
@@ -20,26 +37,10 @@ def make_standard_meta(model):
 
     return Meta
 
-def make_collectionobjectresource():
-    model = Collectionobject
-    attrs = get_fk_fields(model)
-    attrs['Meta'] = make_standard_meta(model)
-    def get_determinations(bundle):
-        return Determination.objects.filter(collectionobject=bundle.obj)
-    attrs['determinations'] = ToManyField('specify.api.DeterminationResource',
-                                          get_determinations)
-    return type('CollectionobjectResource', (ModelResource,), attrs)
-
-CollectionobjectResource = make_collectionobjectresource()
-resources.append(CollectionobjectResource)
-
-for model in get_models(get_app('specify')):
-    clsname = model.__name__ + 'Resource'
-    if clsname in globals(): continue
-
-    attrs = get_fk_fields(model)
-    attrs['Meta'] = make_standard_meta(model)
-
-    cls = type(clsname, (ModelResource,), attrs)
-    globals()[clsname] = cls
-    resources.append(cls)
+def build_resource(model):
+    extra_fields = dict((related.lower() + "s", make_to_many_field(model, related))
+                        for related in to_many_relationships.get(model.__name__, []))
+    return make_resource_with_foreignkeys(model, extra_fields)
+    
+resources = [build_resource(model) for model in get_models(app)]
+globals().update(dict((resource.__name__, resource) for resource in resources))
