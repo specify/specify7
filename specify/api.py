d8fdd22c090001858a28c6b651b04575f71abd8f
Ben Anhalt <anhalt@ku.edu>
2012-02-08 15:29:11 -0600

    give names to ToMany fields and clean up code a bit

--- specify/api.py
+++ specify/api.py
@@ -1,46 +1,44 @@
 from tastypie.resources import ModelResource
 from tastypie.fields import ForeignKey, ToManyField
-from django.db.models import get_app, get_model, get_models
+from django.db.models import get_models
 
-app = get_app('specify')
+from specify import models
 
 to_many_relationships = {
-    'Collectionobject': ['Determination', 'Preparation'],
-}
+    'Collectionobject': {
+        'Determination': 'determinations',
+        'Preparation': 'preparations',
+        }
+    }
 
 def make_to_many_field(model, related):
-    related_model = getattr(app, related)
+    related_model = getattr(models, related)
     def get_related_objs(bundle):
         filter_args = {model.__name__.lower(): bundle.obj}
         return related_model.objects.filter(**filter_args)
     related_resource = "%s.%s%s" % (__name__, related, "Resource")
     return ToManyField(related_resource, get_related_objs)
 
-def make_resource_with_foreignkeys(model, attrs={}):
-    clsname = model.__name__ + 'Resource'
-    attrs.update(get_fk_fields(model))
-    attrs['Meta'] = make_standard_meta(model)
-    return type(clsname, (ModelResource,), attrs)
-
-def get_fk_fields(model):
-    attrs = {}
-    for f in model._meta.fields:
-        if not f.rel: continue
-        relname = __name__ + '.' + \
-            f.related.parent_model.__name__ + 'Resource'
-        attrs[f.name] = ForeignKey(relname, f.name, null=f.null)
-    return attrs
-
-def make_standard_meta(model):
-    class Meta:
-        queryset = model.objects.all()
-
-    return Meta
+def make_fk_field(field):
+    relname = "%s.%sResource" % (__name__, field.related.parent_model.__name__)
+    return ForeignKey(relname, field.name, null=field.null)
 
 def build_resource(model):
-    extra_fields = dict((related.lower() + "s", make_to_many_field(model, related))
-                        for related in to_many_relationships.get(model.__name__, []))
-    return make_resource_with_foreignkeys(model, extra_fields)
+    fk_fields = dict((field.name, make_fk_field(field))
+                     for field in model._meta.fields if field.rel)
+
+    to_many_fields = dict((fieldname, make_to_many_field(model, related))
+                          for related, fieldname in
+                          to_many_relationships.get(model.__name__, {}).items())
+
+    class Meta:
+        queryset = model.objects.all()
     
-resources = [build_resource(model) for model in get_models(app)]
+    attrs = {'Meta': Meta}
+    attrs.update(fk_fields)
+    attrs.update(to_many_fields)
+    clsname = model.__name__ + 'Resource'
+    return type(clsname, (ModelResource,), attrs)
+
+resources = [build_resource(model) for model in get_models(models)]
 globals().update(dict((resource.__name__, resource) for resource in resources))
