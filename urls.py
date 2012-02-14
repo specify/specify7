941062e8d7fa2c5db4ce468fa7770fa0f15d508d
Ben Anhalt <anhalt@ku.edu>
2012-02-14 15:00:27 -0600

    add index page

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic import TemplateView
 from tastypie.api import Api
 from specify.api import resources
 
@@ -12,7 +13,7 @@ for r in resources: api.register(r())
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'djangospecify.views.home', name='home'),
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
     url(r'^specify/', include('djangospecify.specify.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
