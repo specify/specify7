cebf4803821e683d0638de72c14bec9407d970d6
Ben Anhalt <anhalt@ku.edu>
2012-03-01 14:05:11 -0600

    pretty

--- specify/encryption.py
+++ specify/encryption.py
@@ -6,10 +6,11 @@ ITERATION_COUNT = 1000
 def decrypt(text, password):
     key = password.encode() # since it could be a unicode object
     fromhex = text.decode('hex')
-    salt = fromhex[:8]
-    ciphertext = fromhex[8:]
+    salt, ciphertext = fromhex[:8], fromhex[8:]
+
     derivedkey = generate_derivedkey(key, salt)
     deskey, iv = derivedkey[:8], derivedkey[8:]
+
     des = DES.new(deskey, DES.MODE_CBC, iv)
     padded = des.decrypt(ciphertext)
     paddinglen = ord(padded[-1])
