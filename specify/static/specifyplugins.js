e3de4c7277682d5c61c2433ccfe93b092d313f2c
Ben Anhalt <anhalt@ku.edu>
2012-03-16 13:19:25 -0500

    google maps plugin

--- specify/static/specifyplugins.js
+++ specify/static/specifyplugins.js
@@ -1,37 +1,38 @@
-define(['jquery', 'schemalocalization', 'specifyapi'], function($, schemalocalization, api) {
-    "use strict";
-    function inFormTable(control) {
-        return control.parents().is('.specify-formtable-row');
-    }
+define(
+    ['jquery', 'underscore', 'schemalocalization', 'specifyapi', 'text!gmapplugin.html'],
+    function($, _, schemalocalization, api, gmaptemplate_html) {
+        "use strict";
+        var gmaptemplate = _.template(gmaptemplate_html);
 
-    return {
-        PartialDateUI: function(control, init, data) {
-            control[0].type = 'text'; // this probably breaks IE (f*** you IE)
-            control.datepicker({dateFormat: $.datepicker.ISO_8601});
-            if (!inFormTable(control)) {
+        return {
+            PartialDateUI: function(control, init, data) {
+                control[0].type = 'text'; // this probably breaks IE (f*** you IE)
+                control.datepicker({dateFormat: $.datepicker.ISO_8601});
                 var label = control.parents().last().find('label[for="' + control.prop('id') + '"]');
-                var model = control.closest('[data-specify-model]').attr('data-specify-model');
-                label.text(schemalocalization.getLocalizedLabelForField(init.df, model));
+                if (!label.text()) {
+                    var model = control.closest('[data-specify-model]').attr('data-specify-model');
+                    label.text(schemalocalization.getLocalizedLabelForField(init.df, model));
+                }
+                if (data) {
+                    api.getDataFromResource(data, init.df).done(_.bind(control.val, control));
+                } else { control.val(''); }
+            },
+            WebLinkButton: function(control, init) {
+                var form = control.closest('.specify-view-content');
+                var watched = form.find(
+                    '#' + 'specify-field-' + form.prop('id').split('-').pop() + '-' + init.watch);
+                switch(init.weblink) {
+                case 'MailTo':
+                    control.click(function() {
+                        var addr = watched.val();
+                        addr && window.open('mailto:' + addr);
+                    });
+                    control.attr('value', 'EMail');
+                    break;
+                }
+            },
+            LocalityGoogleEarth: function(control, init, data) {
+                control.replaceWith(gmaptemplate(data));
             }
-            if (data) {
-                api.getDataFromResource(data, init.df).done(_.bind(control.val, control));
-            } else { control.val(''); }
-        },
-        WebLinkButton: function(control, init) {
-            var form = inFormTable(control) ?
-                control.closest('.specify-formtable-row') :
-                control.closest('form');
-            var watched = form.find(
-                '#' + 'specify-field-' + form.prop('id').split('-').pop() + '-' + init.watch);
-            switch(init.weblink) {
-            case 'MailTo':
-                control.click(function() {
-                    var addr = watched.val();
-                    addr && window.open('mailto:' + addr);
-                });
-                control.attr('value', 'EMail');
-                break;
-            }
-        },
-    };
-});
+        };
+    });
