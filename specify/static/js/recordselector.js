fb4f34baee903a406633181b135fac55d884ee49
Ben Anhalt <anhalt@ku.edu>
2012-04-02 16:36:10 -0500

    fix record selector

--- specify/static/js/recordselector.js
+++ specify/static/js/recordselector.js
@@ -8,7 +8,7 @@ define(['jquery', 'underscore', 'jquery-ui'], function($, _) {
         var request, showingSpinner, intervalId;
         var redraw = function(offset) {
             debug && console.log('want to redraw at ' + offset);
-            if (offset < bottom || offset > top-1) return false;
+            if (offset < bottom || offset > top-1) return;
             buildContent(collection.at(offset - bottom)).done(function(content) {
                 var curOffset = slider.slider('value');
                 if (curOffset === offset) {
@@ -20,7 +20,6 @@ define(['jquery', 'underscore', 'jquery-ui'], function($, _) {
                                          curOffset + ' but data is for ' + offset);
                 }
             });
-            return true;
         };
         slider.slider({
             max: (collection.totalCount || collection.length) - 1,
@@ -29,7 +28,7 @@ define(['jquery', 'underscore', 'jquery-ui'], function($, _) {
                 request && request.abort();
                 collection.offset = Math.max(0, ui.value - Math.floor(collection.length/2));
                 request = collection.fetch().done(function() {
-                    debug && console.log('got collection at offset ' + offset);
+                    debug && console.log('got collection at offset ' + collection.offset);
                     request = null;
                     bottom = collection.offset;
                     top = bottom + collection.length;
@@ -38,7 +37,8 @@ define(['jquery', 'underscore', 'jquery-ui'], function($, _) {
             }, 750),
             slide: function(event, ui) {
                 $('.ui-slider-handle', this).text(ui.value + 1);
-                if (!redraw(ui.value)) {
+                if (ui.value < bottom || ui.value > top-1) {
+                    if (showingSpinner) return;
                     var content = $(contentSelector, node);
                     var spinner = $('<img src="/static/img/icons/specify128spinner.gif">');
                     var div = $('<div>').css({height: content.height(),
@@ -46,7 +46,7 @@ define(['jquery', 'underscore', 'jquery-ui'], function($, _) {
                     spinner.height(Math.min(128, 0.90*content.height()));
                     content.empty().append(div);
                     showingSpinner = true;
-                }
+                } else _.defer(redraw, ui.value);
             }
         }).find('.ui-slider-handle').
             css({'min-width': '1.2em', width: 'auto', 'text-align': 'center', padding: '0 3px 0 3px'}).
