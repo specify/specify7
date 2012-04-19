7a032987aa9f19cd5192eea87e6f2dffa27394a8
Ben Anhalt <anhalt@ku.edu>
2012-04-19 18:30:39 -0500

    improve dataobjformat

--- specify/static/js/dataobjformatters.js
+++ specify/static/js/dataobjformatters.js
@@ -30,9 +30,13 @@ define([
             return whenAll(deferreds).pipe(function (fieldVals) {
                 var result = [];
                 fields.each(function (index) {
+                    if (!fieldVals[index]) return;
                     var fieldNode = $(this);
-                    var field = resource.specifyModel.getField(fieldNode.text());
                     fieldNode.attr('sep') && result.push(fieldNode.attr('sep'));
+
+                    var format = fieldNode.attr('format');
+                    if (!_(format).isUndefined() && format.trim() === '') return;
+                    var field = resource.specifyModel.getField(fieldNode.text());
                     result.push(uiformat(field, fieldVals[index]));
                 });
                 return result.join('');
