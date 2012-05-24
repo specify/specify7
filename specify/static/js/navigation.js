29753a911b86070452cc48758267371b286e3c27
Ben Anhalt <anhalt@ku.edu>
2012-05-24 11:36:53 -0500

    fix navigation for datamodel views

--- specify/static/js/navigation.js
+++ specify/static/js/navigation.js
@@ -1,7 +1,8 @@
 define(['jquery', 'underscore', 'backbone'], function($, _, Backbone) {
     return {
         navigate: function(url, options) {
-            Backbone.history.navigate(url.replace(/^\/specify/, ''), options);
+            url = url.replace(RegExp('^' + window.location.origin + '/specify'), '');
+            Backbone.history.navigate(url, options);
         },
         go: function(url) {
             this.navigate(url, true);
