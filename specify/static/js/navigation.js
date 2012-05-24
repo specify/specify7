adf2e18f538c0d3f34c0e4d446d5b30add46cbd8
Ben Anhalt <anhalt@ku.edu>
2012-05-24 15:45:20 -0500

    fix bug introduced into navigation

--- specify/static/js/navigation.js
+++ specify/static/js/navigation.js
@@ -1,8 +1,8 @@
 define(['jquery', 'underscore', 'backbone'], function($, _, Backbone) {
     return {
         navigate: function(url, options) {
-            url = url.replace(RegExp('^' + window.location.origin + '/specify'), '');
-            Backbone.history.navigate(url, options);
+            url = url.replace(RegExp('^' + window.location.origin), '');
+            Backbone.history.navigate(url.replace(/^\/specify/, ''), options);
         },
         go: function(url) {
             this.navigate(url, true);
