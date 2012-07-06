27a6095a280ac9973eb03aa095201a9ea232d84d
Ben Anhalt <anhalt@ku.edu>
2012-07-06 11:14:59 -0500

    add in some stuff for requirejs optimization

---
+++ specify/static/js/app.build.js
@@ -0,0 +1,16 @@
+({
+    baseUrl: '.',
+    name: 'main',
+    out: 'main-built.js',
+    paths: {
+        'jquery': "https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery",
+        'jquery-ui': "https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui",
+        'jquery-bbq': "vendor/jquery.ba-bbq",
+        'underscore': "vendor/underscore",
+        'backbone': "vendor/backbone",
+        'resources': '../resources',
+        'tmpls': '../html/templates',
+        'beautify-html': "vendor/beautify-html",
+        'text': "vendor/text",
+    }
+})
