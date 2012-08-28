bc0c94995164921242cde02bddbf8dfc96745804
Ben Anhalt <anhalt@ku.edu>
2012-08-28 12:20:24 -0500

    update require optimization build file

--- specify/static/js/app.build.js
+++ specify/static/js/app.build.js
@@ -2,15 +2,29 @@
     baseUrl: '.',
     name: 'main',
     out: 'main-built.js',
+    inlineText: false,
     paths: {
-        'jquery': "https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery",
-        'jquery-ui': "https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui",
-        'jquery-bbq': "vendor/jquery.ba-bbq",
-        'underscore': "vendor/underscore",
-        'backbone': "vendor/backbone",
-        'resources': '../resources',
-        'tmpls': '../html/templates',
-        'beautify-html': "vendor/beautify-html",
-        'text': "vendor/text",
+        'jquery'         : "vendor/jquery-1.7.2",
+        'jquery-ui'      : "vendor/jquery-ui",
+        'jquery-bbq'     : "vendor/jquery.ba-bbq",
+        'jquery-mockjax' : "vendor/jquery.mockjax",
+        'underscore'     : "vendor/underscore",
+        'backbone'       : "vendor/backbone",
+        'qunit'          : "vendor/qunit",
+        'beautify-html'  : "vendor/beautify-html",
+        'CoffeeScript'   : "vendor/CoffeeScript",
+        'cs'             : "vendor/cs",
+        'text'           : "vendor/text",
+        'resources'      : '/static/config',
+        'tmpls'          : '/static/html/templates',
+        'context'        : '/context',
+        'properties'     : '/properties'
+    },
+    shim: {
+        'jquery-ui'      : ['jquery'],
+        'jquery-bbq'     : ['jquery'],
+        'jquery-mockjax' : ['jquery'],
+        'backbone'       : { deps: ['jquery', 'underscore'], exports: 'Backbone' },
+        'qunit'          : { deps: ['jquery'], exports: 'QUnit' }
     }
 })
