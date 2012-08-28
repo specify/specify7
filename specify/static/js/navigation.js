c8413145d1c35909342fd6bb04de7e06703d8734
Ben Anhalt <anhalt@ku.edu>
2012-08-28 17:00:05 -0500

    handle missing window.location.origin in non-webkit based browsers

--- specify/static/js/navigation.js
+++ specify/static/js/navigation.js
@@ -1,7 +1,10 @@
 define(['jquery', 'underscore', 'backbone'], function($, _, Backbone) {
     return {
         navigate: function(url, options) {
-            url = url.replace(RegExp('^' + window.location.origin), '');
+            var origin = window.location.origin || (
+                window.location.protocol + '//' + window.location.host);
+
+            url = url.replace(RegExp('^' + origin), '');
             Backbone.history.navigate(url.replace(/^\/specify/, ''), options);
         },
         go: function(url) {
