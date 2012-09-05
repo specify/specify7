fd3e86c52309c4658677e661907df6e19416bc83
Ben Anhalt <anhalt@ku.edu>
2012-09-05 15:18:55 -0500

    schema localization string unescape doesn't break for null values

--- specify/static/js/schemabase.js
+++ specify/static/js/schemabase.js
@@ -10,7 +10,7 @@ define(['underscore'], function(_) {
             return _(this.models).find(function(model) { return model.tableId === tableId; });
         },
         unescape: function(str) {
-            return str.replace(/([^\\])\\n/g, '$1\n');
+            return str && str.replace(/([^\\])\\n/g, '$1\n');
         },
         orgHierarchy: ['collectionobject', 'collection', 'discipline', 'division', 'institution']
     };
