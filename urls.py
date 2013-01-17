fa4b809826dd60c9de610fb633570f093e639852
Ben Anhalt <anhalt@ku.edu>
2013-01-17 16:31:48 -0600

    a de-cruftified version of the app container page in preparation of add a extjs container around everything

--- urls.py
+++ urls.py
@@ -3,18 +3,23 @@ from django.views.generic.simple import redirect_to
 
 urlpatterns = patterns(
     '',
+    # log in and log out pages
     (r'^accounts/login/$', 'context.views.login'),
     (r'^accounts/logout/$', 'context.views.logout'),
 
+    # just redirect root url to the main specify view
     url(r'^$', redirect_to, {'url': '/specify/'}),
 
+    # this is the main specify view
     url(r'^specify/', include('frontend.urls')),
-    url(r'^images/(?P<path>.+)$', 'specify.views.images'),
-    url(r'^properties/(?P<name>.+).properties$', 'specify.views.properties'),
 
+    # the main business data API
     url(r'^api/specify/(?P<model>\w+)/(?P<id>\d+)/$', 'specify.views.resource'),
     url(r'^api/specify/(?P<model>\w+)/$', 'specify.views.collection'),
 
+    # access to various UI and app resources starts here
+    url(r'^images/(?P<path>.+)$', 'specify.views.images'),
+    url(r'^properties/(?P<name>.+).properties$', 'specify.views.properties'),
 
     url(r'^express_search/', include('express_search.urls')),
     url(r'^context/', include('context.urls')),
