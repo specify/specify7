a909df9462b0eb1b6fb94f59979ebe9b485e1da3
timo <timo@ku.edu>
2013-02-18 17:30:10 -0600

    oh no

--- specify_settings.py
+++ specify_settings.py
@@ -1,14 +1,14 @@
 
 # The webapp server piggy backs on the thick client.
 # Set the path to a thick client installation.
-THICK_CLIENT_LOCATION = '/home/ben/Specify6.4.11'
+THICK_CLIENT_LOCATION = '/home/timo/thinclient/Specify'
 
 # Set the database name to the mysql database you
 # want to access.
-DATABASE_NAME = 'KU_Fish_Tissue'
+DATABASE_NAME = 'unm6'
 
 
 # The master user login. Use the same values as
 # you did setting up the thick client.
-MASTER_NAME = 'Master'
-MASTER_PASSWORD = 'Master'
+MASTER_NAME = 'root'
+MASTER_PASSWORD = 'root'
