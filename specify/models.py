008375140ec26a74825d3c63d63954c98c4de279
Ben Anhalt <anhalt@ku.edu>
2013-03-01 17:30:12 -0600

    improve setting up of the specify user model

--- specify/models.py
+++ specify/models.py
@@ -1,5 +1,6 @@
 from build_models import build_models
 from check_versions import check_versions
+from setup_user_model import setup_user_model
 
 models_by_tableid = build_models(__name__)
 
@@ -8,22 +9,5 @@ globals().update((model.__name__, model)
                  for model in models_by_tableid.values())
 
 check_versions(Spversion)
-
-def setup_specify_user():
-    def get_full_name(self): return self.name
-    def get_short_name(self): return self.name
-    def is_authenticated(self): return True
-
-    Specifyuser.USERNAME_FIELD = 'name'
-    Specifyuser.REQUIRED_FIELDS = []
-    Specifyuser.is_active = True
-    Specifyuser.get_full_name = get_full_name
-    Specifyuser.get_short_name = get_short_name
-    Specifyuser.is_authenticated = is_authenticated
-
-    from django.contrib.auth.signals import user_logged_in
-    from django.contrib.auth.models import update_last_login
-    user_logged_in.disconnect(update_last_login)
-
-setup_specify_user()
+setup_user_model(Specifyuser)
 
