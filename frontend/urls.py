860b320f70ceeeb44a92210d3c52d58f84432478
Ben Anhalt <anhalt@ku.edu>
2013-04-18 17:45:50 -0500

    fixed revision reporting in footer

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,18 +1,12 @@
 from django.conf.urls import patterns
 from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
-
-from sh import git, ErrorReturnCode
-
-try:
-    revision = git('rev-parse', 'HEAD')
-except ErrorReturnCode:
-    revision = 'unknown'
+from django.conf import settings
 
 class View(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(View, self).get_context_data(**kwargs)
-        context['revision'] = revision
+        context['revision'] = settings.REVISION
         return context
 
 # just send the form.html webapp container for _all_ URLs.
