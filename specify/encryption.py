855d51ae75a34e0cf4f424c4a040acb4316e648c
Ben Anhalt <anhalt@ku.edu>
2013-08-30 14:11:44 -0500

    add method for encrypting specify passwords

--- specify/encryption.py
+++ specify/encryption.py
@@ -1,5 +1,9 @@
+# See edu.ku.brc.helpers.Encryption and rfc2898
+
+from itertools import islice
 from hashlib import md5
 from Crypto.Cipher import DES
+from Crypto.Random.random import randint
 
 ITERATION_COUNT = 1000
 
@@ -15,6 +19,26 @@ def decrypt(text, password):
     padded = des.decrypt(ciphertext)
     paddinglen = ord(padded[-1])
     return padded[:-paddinglen]
+
+def encrypt(text, password):
+    paddinglen = 8 - len(text) % 8
+    padded = text + chr(paddinglen) * paddinglen
+
+    key = password.encode()
+    salt = make_salt()
+
+    derivedkey = generate_derivedkey(key, salt)
+    deskey, iv = derivedkey[:8], derivedkey[8:]
+
+    des = DES.new(deskey, DES.MODE_CBC, iv)
+    ciphertext = des.encrypt(padded)
+    return str(salt + ciphertext).encode('hex')
+
+def rand_byte():
+    return randint(0, 0xff)
+
+def make_salt():
+    return str(bytearray(islice(iter(rand_byte, None), 8)))
 
 def generate_derivedkey(key, salt, iterations=ITERATION_COUNT):
     out = key + salt
