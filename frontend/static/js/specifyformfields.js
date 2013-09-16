1ac2222c349ff0b7f85a5260d6246d71d066a787
Ben Anhalt <anhalt@ku.edu>
2013-09-16 15:24:54 -0500

    better error handling for bad/unknow field uitype

--- frontend/static/js/specifyformfields.js
+++ frontend/static/js/specifyformfields.js
@@ -1,7 +1,13 @@
 define(['jquery', 'underscore'], function($, _) {
     "use strict";
     return function(doingFormTable, mode, cell, id) {
-        return {
+        var uitype = cell.attr('uitype');
+        if (!uitype) {
+            console.error('field is missing uitype', cell);
+            uitype = 'text';
+        }
+
+        var makeField = {
             checkbox: function() {
                 var control = $('<input type=checkbox class="specify-field">');
                 control.attr('disabled', doingFormTable || mode === 'view');
@@ -56,6 +62,14 @@ define(['jquery', 'underscore'], function($, _) {
             browse: function() {
                 return $('<input type=file class="specify-field">');
             }
-        }[cell.attr('uitype') || 'text']();
+        };
+
+        var maker = makeField[uitype];
+        if (!maker) {
+            console.error('unknown field uitype:', uitype);
+            maker = makeField['text'];
+        }
+
+        return maker();
     };
 });
