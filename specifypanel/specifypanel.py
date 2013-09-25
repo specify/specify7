fbe85e4ee9b1fc711d37b7a8c71b107a2246dc3a
Ben Anhalt <anhalt@ku.edu>
2013-09-25 12:26:56 -0500

    add github post commit hook

--- specifypanel/specifypanel.py
+++ specifypanel/specifypanel.py
@@ -9,7 +9,11 @@ SPECIFYWEB_DIR = path.abspath(path.join(path.dirname(__file__), '..'))
 DB_MAP_FILE = path.join(SPECIFYWEB_DIR, 'db_map.json')
 APACHE_CONF_FILE = path.join(SPECIFYWEB_DIR, 'specify_panel.conf')
 VIRTHOST_WSGI = path.join(SPECIFYWEB_DIR, 'specify_virtual_host.wsgi')
+PANEL_WSGI = path.join(SPECIFYWEB_DIR, 'specify_panel.wsgi')
 MANAGE_PY = path.join(SPECIFYWEB_DIR, 'specifyweb', "manage.py")
+
+MYSQL_USER = "-uMasterUser"
+MYSQL_PASS = "-pMasterPassword"
 
 with open(APACHE_CONF_FILE) as f:
     conf = f.read()
@@ -26,7 +30,7 @@ def main():
         else:
             raise
 
-    show_databases = check_output(["/usr/bin/mysql", "-uMasterUser", "-pMasterPassword", "-e", "show databases"])
+    show_databases = check_output(["/usr/bin/mysql", MYSQL_USER, MYSQL_PASS, "-e", "show databases"])
     available_dbs = set(show_databases.split()[1:]) - {'information_schema', 'performance_schema', 'mysql'}
     return template('main.tpl', servers=SERVERS, db_map=db_map, available_dbs=available_dbs, host=request.get_header('Host'))
 
@@ -54,11 +58,11 @@ def upload_db():
     response.set_header('Content-Type', 'text/plain')
 
     yield "dropping db.\n"
-    call(["/usr/bin/mysqladmin", "-f", "-uMasterUser", "-pMasterPassword", "drop", db_name])
+    call(["/usr/bin/mysqladmin", "-f", MYSQL_USER, MYSQL_PASS, "drop", db_name])
     yield "creating db.\n"
-    check_call(["/usr/bin/mysqladmin", "-uMasterUser", "-pMasterPassword", "create", db_name])
+    check_call(["/usr/bin/mysqladmin", MYSQL_USER, MYSQL_PASS, "create", db_name])
     yield "loading db.\n"
-    mysql = Popen(["/usr/bin/mysql", "-uMasterUser", "-pMasterPassword", db_name], stdin=PIPE)
+    mysql = Popen(["/usr/bin/mysql", MYSQL_USER, MYSQL_PASS, db_name], stdin=PIPE)
 
     loaded = 0
     for chunk in iter(lambda: upload_file.read(CHUNK_SIZE), ""):
@@ -78,7 +82,7 @@ def upload_db():
 @route('/export/')
 def export():
     db_name = request.query['dbname']
-    mysqldump = Popen(["/usr/bin/mysqldump", "-uMasterUser", "-pMasterPassword", db_name], stdout=PIPE)
+    mysqldump = Popen(["/usr/bin/mysqldump", MYSQL_USER, MYSQL_PASS, db_name], stdout=PIPE)
 
     response.set_header('Content-Type', 'text/plain')
     response.set_header('Content-Disposition', 'attachment; filename=%s.sql' % db_name)
@@ -98,6 +102,20 @@ def drop_form():
 @route('/drop/', method='POST')
 def drop():
     db_name = request.forms['dbname']
-    call(["/usr/bin/mysqladmin", "-f", "-uMasterUser", "-pMasterPassword", "drop", db_name])
+    call(["/usr/bin/mysqladmin", "-f", MYSQL_USER, MYSQL_PASS, "drop", db_name])
     redirect('/')
 
+@route('/github_hook/', method='POST')
+def github_hook():
+    check_call(["/usr/bin/git",
+                "--work-tree=" + SPECIFYWEB_DIR,
+                "--git-dir=" + path.join(SPECIFYWEB_DIR, '.git'),
+                "pull"])
+    check_call(['/usr/bin/touch', VIRTHOST_WSGI])
+    check_call(['/usr/bin/touch', PANEL_WSGI])
+
+
+if __name__ == '__main__':
+    from bottle import run, debug
+    debug(True)
+    run(host='0.0.0.0', port='8080', reloader=True)
