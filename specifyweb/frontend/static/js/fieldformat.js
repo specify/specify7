06e7b083f848e457f744b6ad6fec5513c60ead8a
Ben Anhalt <anhalt@ku.edu>
2014-04-08 16:09:13 -0500

    clean up fieldformat.js

--- specifyweb/frontend/static/js/fieldformat.js
+++ specifyweb/frontend/static/js/fieldformat.js
@@ -1,24 +1,33 @@
 define(['jquery', 'underscore', 'agenttypepicklist'], function($, _, agenttypes) {
     "use strict";
 
+    var byType = {
+        'java.lang.Boolean': function(value) {
+            return _isNull(value) ? '' : (
+                value ? 'True' : 'False');
+        }
+    };
+
     return function(field, value) {
         var asInt = parseInt(value, 10);
         if (field.getFormat() === 'CatalogNumberNumeric') {
-            if (_.isNaN(asInt)) {
-                return value;
-            } else {
-                return asInt;
-            }
-        } else if (field.name === 'timestampModified' || field.name === 'timestampCreated') {
+            return _.isNaN(asInt) ? value : asInt;
+        }
+
+        if (field.name === 'timestampModified' || field.name === 'timestampCreated') {
             return value && value.split('T')[0] || value;
-        } else if (field.name === 'agentType' && field.model.name === 'Agent') {
+        }
+
+        if (field.name === 'agentType' && field.model.name === 'Agent') {
             var agentType = _.find(agenttypes, function(type) { return type.value === asInt; });
             return agentType ? agentType.title : value || '';
-        } else if (field.type === 'java.lang.Boolean') {
-            if (_.isNull(value)) return '';
-            return value ? 'True' : 'False';
-        } else {
-            return value || '';
         }
+
+        var bt = byType[field.type];
+        if (bt) {
+            return bt(value);
+        }
+
+        return value || '';
     };
 });
