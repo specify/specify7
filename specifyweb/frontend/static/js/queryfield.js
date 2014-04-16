54a2b10777b9637b34a0ea8a9ad5f8726c18d9c6
Ben Anhalt <anhalt@ku.edu>
2014-04-16 12:09:34 -0500

    breakout queryfieldinput.

--- specifyweb/frontend/static/js/queryfield.js
+++ specifyweb/frontend/static/js/queryfield.js
@@ -1,45 +1,9 @@
 define([
-    'jquery', 'underscore', 'backbone', 'schema', 'domain', 'fieldformat', 'queryfieldspec'
-], function($, _, Backbone, schema, domain, fieldformat, queryfieldspec) {
+    'jquery', 'underscore', 'backbone', 'schema', 'domain', 'queryfieldspec', 'queryfieldinput'
+], function($, _, Backbone, schema, domain, queryfieldspec, QueryFieldInputUI) {
     "use strict";
 
     var SORT_ICONS = ["ui-icon-bullet", "ui-icon-carat-1-n", "ui-icon-carat-1-s"];
-
-    var FieldInputUI = Backbone.View.extend({
-        __name__: "FieldInputUI",
-        events: {
-            'change input': 'changed'
-        },
-        opName: 'NA',
-        format: false,
-        input: '<input type="text">',
-        initialize: function(options) {
-            this.inputFormatter = options.field.getUIFormatter();
-            this.outputFormatter = function(value) { return fieldformat(options.field, value); };
-        },
-        getValue: function() {
-            return this.$('input').val();
-        },
-        setValue: function(value) {
-            this.format && (value = this.outputFormatter(value));
-            this.$('input').val(value);
-        },
-        render: function() {
-            this.$el.empty();
-            $('<a class="field-operation">').text(this.opName).appendTo(this.el);
-            this.input && $(this.input).appendTo(this.el);
-            return this;
-        },
-        changed: function() {
-            var value = this.getValue();
-            if (this.format && this.inputFormatter) {
-                var formatterValues = this.inputFormatter.parse(value); // TODO: don't accept autonumber patterns maybe...
-                formatterValues && (value = this.inputFormatter.canonicalize(formatterValues));
-                // TODO: make a warning for badly formatted input values?
-            }
-            this.trigger('changed', this, value);
-        }
-    });
 
     var types = {
         strings: ['text', 'java.lang.String'],
@@ -48,40 +12,6 @@ define([
         dates: ['java.util.Calendar', 'java.util.Date', 'java.sql.Timestamp'],
         bools: ['java.lang.Boolean']
     };
-
-    var opInfo = [
-        {opName: 'Like', negation: 'Not Like', types: ['strings']},
-        {opName: '=', negation: '≠', types: ['strings', 'numbers', 'dates'], format: true},
-        {opName: '>', negation: '≯', types: ['numbers', 'dates'], format: true},
-        {opName: '<', negation: '≮', types: ['numbers', 'dates'], format: true},
-        {opName: '≥', negation: '≱', types: ['numbers'], format: true},
-        {opName: '≤', negation: '≰', types: ['numbers'], format: true},
-        {opName: 'True', negation: 'Not True', types: ['bools'], input: null},
-        {opName: 'False', negation: 'Not False', types: ['bools'], input: null},
-        {opName: 'Does not matter', types: ['bools'], input: null},
-
-        {opName: 'Between', negation: 'Not Between', types: ['strings', 'dates', 'numbers'],
-         input: '<input type="text"> and <input type="text">',
-         getValue: function() {
-             return _.map(this.$('input'), function(input) { return $(input).val(); }).join(',');
-         },
-         setValue: function(value) {
-             var values = value.split(',');
-             _.each(this.$('input'), function(input, i) { $(input).val(values[i]); });
-         }
-        },
-
-        {opName: 'In', negation: 'Not In', types: ['strings', 'numbers']},
-        {opName: 'Contains', negation: "Doesn't Contain", types: ['strings']},
-        {opName: 'Empty', negation: 'Not Empty', types: ['strings', 'bools', 'dates', 'numbers'], input: null},
-        {opName: 'True or Null', negation: 'False', types: ['bools'], input: null},
-        {opName: 'False or Null', negation: 'True', types: ['bools'], input: null}
-    ];
-
-    var FieldInputUIByOp = _.map(opInfo, function(extras) {
-        var options = _.extend({ __name__: "OpFieldInputUI" }, extras);
-        return FieldInputUI.extend(options);
-    });
 
     return Backbone.View.extend({
         __name__: "QueryField",
@@ -265,9 +195,9 @@ define([
                     .append('<option>Select Op...</option>')
                     .append('<option value="anything">(any)</option>');
             var type = this.getTypeForOp();
-            _.each(opInfo, function(info, i) {
-                if (_(info.types).contains(type)) {
-                    $('<option>', {value: i}).text(info.opName).appendTo(opSelect);
+            _.each(QueryFieldInputUI, function(ui, i) {
+                if (_(ui.prototype.types).contains(type)) {
+                    $('<option>', {value: i}).text(ui.prototype.opName).appendTo(opSelect);
                 }
             }, this);
         },
@@ -374,7 +304,7 @@ define([
             this.$('.field-select-grp, .datepart-select, .op-select').hide();
             this.$('.field-controls').removeClass('hidden');
             if (!this.formattedRecord && this.operation != 'anything') {
-                this.inputUI = new (FieldInputUIByOp[this.operation])({
+                this.inputUI = new (QueryFieldInputUI[this.operation])({
                     field: _.last(this.joinPath),
                     el: this.$('.field-input')
                 });
---
+++ specifyweb/frontend/static/js/queryfieldinput.js
@@ -0,0 +1,75 @@
+define([
+    'jquery', 'underscore', 'backbone', 'fieldformat'
+], function($, _, Backbone, fieldformat) {
+    "use strict";
+
+    var FieldInputUI = Backbone.View.extend({
+        __name__: "FieldInputUI",
+        events: {
+            'change input': 'changed'
+        },
+        opName: 'NA',
+        format: false,
+        input: '<input type="text">',
+        initialize: function(options) {
+            this.inputFormatter = options.field.getUIFormatter();
+            this.outputFormatter = function(value) { return fieldformat(options.field, value); };
+        },
+        getValue: function() {
+            return this.$('input').val();
+        },
+        setValue: function(value) {
+            this.format && (value = this.outputFormatter(value));
+            this.$('input').val(value);
+        },
+        render: function() {
+            this.$el.empty();
+            $('<a class="field-operation">').text(this.opName).appendTo(this.el);
+            this.input && $(this.input).appendTo(this.el);
+            return this;
+        },
+        changed: function() {
+            var value = this.getValue();
+            if (this.format && this.inputFormatter) {
+                var formatterValues = this.inputFormatter.parse(value); // TODO: don't accept autonumber patterns maybe...
+                formatterValues && (value = this.inputFormatter.canonicalize(formatterValues));
+                // TODO: make a warning for badly formatted input values?
+            }
+            this.trigger('changed', this, value);
+        }
+    });
+
+    var opInfo = [
+        {opName: 'Like', negation: 'Not Like', types: ['strings']},
+        {opName: '=', negation: '≠', types: ['strings', 'numbers', 'dates'], format: true},
+        {opName: '>', negation: '≯', types: ['numbers', 'dates'], format: true},
+        {opName: '<', negation: '≮', types: ['numbers', 'dates'], format: true},
+        {opName: '≥', negation: '≱', types: ['numbers'], format: true},
+        {opName: '≤', negation: '≰', types: ['numbers'], format: true},
+        {opName: 'True', negation: 'Not True', types: ['bools'], input: null},
+        {opName: 'False', negation: 'Not False', types: ['bools'], input: null},
+        {opName: 'Does not matter', types: ['bools'], input: null},
+
+        {opName: 'Between', negation: 'Not Between', types: ['strings', 'dates', 'numbers'],
+         input: '<input type="text"> and <input type="text">',
+         getValue: function() {
+             return _.map(this.$('input'), function(input) { return $(input).val(); }).join(',');
+         },
+         setValue: function(value) {
+             var values = value.split(',');
+             _.each(this.$('input'), function(input, i) { $(input).val(values[i]); });
+         }
+        },
+
+        {opName: 'In', negation: 'Not In', types: ['strings', 'numbers']},
+        {opName: 'Contains', negation: "Doesn't Contain", types: ['strings']},
+        {opName: 'Empty', negation: 'Not Empty', types: ['strings', 'bools', 'dates', 'numbers'], input: null},
+        {opName: 'True or Null', negation: 'False', types: ['bools'], input: null},
+        {opName: 'False or Null', negation: 'True', types: ['bools'], input: null}
+    ];
+
+    return _.map(opInfo, function(extras) {
+        var options = _.extend({ __name__: "OpFieldInputUI" }, extras);
+        return FieldInputUI.extend(options);
+    });
+});
