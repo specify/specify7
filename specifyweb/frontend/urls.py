514c7d084f9fc4ecc3c78b459636e24ffe3462d3
Ben Anhalt <anhalt@ku.edu>
2014-04-21 12:30:18 -0500

    automatically use optimized JS and CSS when available.

--- specifyweb/frontend/urls.py
+++ specifyweb/frontend/urls.py
@@ -1,11 +1,5 @@
 from django.conf.urls import patterns
-from django.views.generic import TemplateView
-from django.contrib.auth.decorators import login_required
 
-
-# just send the form.html webapp container for _all_ URLs.
-# the webapp (in specifyapp.js) will interpret them
-urlpatterns = patterns(
-    '',
-    (r'', login_required(TemplateView.as_view(template_name="specify.html"))),
+urlpatterns = patterns('specifyweb.frontend.views',
+    (r'', 'specify')
 )
--- specifyweb/frontend/views.py
+++ specifyweb/frontend/views.py
@@ -1,1 +1,24 @@
-# Create your views here.
+import os
+import logging
+import subprocess
+
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.template import loader, Context
+
+DIR = os.path.dirname(__file__)
+
+logger = logging.getLogger(__name__)
+
+MT_CONTEXT = Context()
+
+@login_required
+def specify(request):
+    # This seems to cost about 16-30 ms.
+    up_to_date = subprocess.call(['make', '-q', '-C', DIR]) == 0
+    logger.debug('js and css optimization is up-to-date: %s', up_to_date)
+
+    template = 'specify-built.html' if up_to_date else 'specify.html'
+    resp = loader.get_template(template).render(MT_CONTEXT)
+    return HttpResponse(resp)
+
