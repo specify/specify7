bf1ad63d9b7270c52226433b53fac8bafef415c9
Ben Anhalt <anhalt@ku.edu>
2014-06-19 15:38:48 -0500

    add settings for report runner service

--- specifyweb/report_runner/views.py
+++ specifyweb/report_runner/views.py
@@ -1,7 +1,4 @@
-import json
-import os
 import requests
-from uuid import uuid4
 
 from django.views.decorators.http import require_GET
 from django.http import HttpResponse
@@ -11,32 +8,6 @@ from specifyweb.specify.views import login_required
 from specifyweb.specify.api import objs_to_data, toJson
 from specifyweb.specify.models import Spappresource
 
-TEST_JSON = '''
-{
-    "databaseSettings": {
-        "databaseName": "KU_Fish_Tissue_20131007",
-        "masterUser": "Master",
-        "masterPassword": "Master",
-        "hostname": "localhost"
-    },
-    "specifySettings": {
-        "collectionName": "KUFishvoucher",
-        "userName": "testuser"
-    },
-    "reportId": 26,
-    "recordSetId": null,
-    "reportParameters": {},
-    "queryParameters": [
-        {
-            "queryFieldId": 703,
-            "text1": "Andy",
-            "text2": "",
-            "operator": 1,
-            "isNegated": false
-        }
-    ]
-}
-'''
 
 class ReportException(Exception):
     pass
@@ -44,13 +15,24 @@ class ReportException(Exception):
 @require_GET
 @login_required
 def run(request):
+    if settings.REPORT_RUNNER_HOST == '':
+        raise ReportException("Report service is not configured.")
+
+    if settings.REPORT_RUNNER_PORT == '':
+        port = 80
+    else:
+        port = settings.REPORT_RUNNER_PORT
+
     options = dict(request.GET)
     options.update({
         "collectionName": request.specify_collection.collectionname,
         "userName": request.specify_user.name
     })
 
-    r = requests.get("http://localhost:8080/report", params=options)
+    r = requests.get("http://%s:%s/report" %
+                     (settings.REPORT_RUNNER_HOST, port),
+                     params=options)
+
     if r.status_code == 200:
         return HttpResponse(r.content, content_type="application/pdf")
     else:
