a1b7191e43e7e368981389db6efa1bfcf6e9517f
Ben Anhalt <anhalt@ku.edu>
2014-08-20 17:09:04 -0500

    fix collection relationship plugins.

--- specifyweb/frontend/static/js/collectionrelonetomanyplugin.js
+++ specifyweb/frontend/static/js/collectionrelonetomanyplugin.js
@@ -1,7 +1,7 @@
 define([
     'jquery', 'underscore', 'specifyapi', 'dataobjformatters',
-    'navigation', 'uiplugin', 'whenall'
-], function($, _, api, dataobjformatters, navigation, UIPlugin, whenAll) {
+    'navigation', 'uiplugin', 'whenall', 'domain'
+], function($, _, api, dataobjformatters, navigation, UIPlugin, whenAll, domain) {
     "use strict";
     var format = dataobjformatters.format;
 
@@ -11,44 +11,57 @@ define([
             'click a': 'go'
         },
         render: function() {
-            var _this = this;
             var table = $('<table>').addClass('collectionrelonetomanyplugin');
             this.$el.replaceWith(table);
             this.setElement(table);
             table.append('<tr><th>Collection Object</th><th>Collection</th></tr>');
-            this.model.isNew() || $.when(
-                this.model.rget('rightsiderels'),
-                api.getCollectionObjectRelTypeByName(this.init.relname)
-            ).pipe(function(related, reltype) {
-                related.filters.collectionreltype = reltype.id;
-
-                var getCollection = reltype.rget('leftsidecollection', true).pipe(function(lsCol) {
-                    _this.otherCollection = lsCol;
-                    return format(lsCol);
-                });
-
-                var getCollectionObjects = related.fetch().pipe(function() {
-                    return whenAll(related.map(function(rel) {
-                        return rel.rget('leftside', true).pipe(function(co) {
-                            return whenAll([co.viewUrl(), format(co)]);
-                        });
-                    }));
-                });
-
-                return $.when(getCollection, getCollectionObjects);
-            }).done(function(collection, coInfo) {
-                _.each(coInfo, function(info) {
-                    var url = info[0], label = info[1];
-                    var tr = $('<tr>').appendTo(table);
-                    $('<a>', { href: url })
-                        .text(label)
-                        .appendTo($('<td>').appendTo(tr));
-                    $('<a>', { href: url })
-                        .text(collection)
-                        .appendTo($('<td>').appendTo(tr));
-                });
-            });
+            this.model.isNew() || this.fillIn();
             return this;
+        },
+        fillIn: function() {
+            api.getCollectionObjectRelTypeByName(this.init.relname).pipe(function(relType) {
+                return $.when(relType, relType.rget('leftsidecollection'), relType.rget('rightsidecollection'));
+            }).done(this.gotRelType.bind(this));
+        },
+        gotRelType: function(relType, leftSideCollection, rightSideCollection) {
+            this.relType = relType;
+            switch (domain.levels.collection.id) {
+            case leftSideCollection.id:
+                this.side = 'left';
+                this.otherSide = 'right';
+                this.otherCollection = rightSideCollection;
+                break;
+            case rightSideCollection.id:
+                this.side = 'right';
+                this.otherSide = 'left';
+                this.otherCollection = leftSideCollection;
+                break;
+            default:
+                throw new Error("related collection plugin used with relation that doesn't match current collection");
+            }
+            this.model.rget(this.side + 'siderels').done(this.gotRels.bind(this));
+        },
+        gotRels: function(related) {
+            related.filters.collectionreltype = this.relType.id;
+
+            var otherSide = this.otherSide + 'side';
+            related.fetch().pipe(function() {
+                return whenAll(related.map(function(rel) {
+                    return rel.rget(otherSide, true);
+                }));
+            }).done(this.gotRelatedObjects.bind(this));
+        },
+        gotRelatedObjects: function(collectionObjects) {
+            var table = this.el;
+            var otherCollectionFormatted = format(this.otherCollection);
+
+           _.each(collectionObjects, function(co) {
+               var tr = $('<tr>').appendTo(table);
+               var label = $('<a>', { href: co.viewUrl() }).appendTo($('<td>').appendTo(tr));
+               format(co).done(function(text) { label.text(text); });
+               var collection = $('<a>', { href: co.viewUrl() }).appendTo($('<td>').appendTo(tr));
+               otherCollectionFormatted.done(function(text) { collection.text(text); });
+           });
         },
         go: function(evt) {
             evt.preventDefault();
--- specifyweb/frontend/static/js/collectionrelonetooneplugin.js
+++ specifyweb/frontend/static/js/collectionrelonetooneplugin.js
@@ -1,58 +1,28 @@
 define([
-    'jquery', 'underscore', 'specifyapi', 'dataobjformatters',
-    'navigation', 'uiplugin', 'whenall'
-], function($, _, api, dataobjformatters, navigation, UIPlugin, whenAll) {
+    'jquery', 'underscore', 'dataobjformatters', 'collectionrelonetomanyplugin'
+], function($, _, dataobjformatters, OneToMany) {
     "use strict";
     var format = dataobjformatters.format;
 
-    return UIPlugin.extend({
+    return OneToMany.extend({
         __name__: "CollectionRelOneToOnePlugin",
-        events: {
-            'click a': 'go',
-            'click button': 'set'
-        },
         render: function() {
-            var _this = this;
             var control = $('<div><a /><button>Set</button></div>');
             this.$el.replaceWith(control);
             this.setElement(control);
             this.$('button').hide(); // disable this for now.
-
-            $.when(
-                this.model.rget('leftsiderels'),
-                api.getCollectionObjectRelTypeByName(this.init.relname)
-            ).pipe(function(related, reltype) {
-                related.filters.collectionreltype = reltype.id;
-
-                var getCollection = reltype.rget('rightsidecollection', true).pipe(function(rsCol) {
-                    _this.otherCollection = rsCol;
-                    return format(rsCol);
-                });
-
-                var getCollectionObject = related.fetch({ limit: 1 }).pipe(function() {
-                    return (related.length < 1) ? null :
-                        related.first().rget('rightside', true).pipe(function(co) {
-                            _this.otherCO = co;
-                            return format(co);
-                        });
-                });
-
-                return $.when(getCollection, getCollectionObject);
-            }).done(function(collection, coLabel) {
-                if (coLabel) {
-                    _this.$('a').text(coLabel).attr('href', _this.otherCO.viewUrl());
-                } else {
-                    _this.$('a').hide();
-                }
-            });
+            this.fillIn();
             return this;
         },
-        go: function(evt) {
-            evt.preventDefault();
-            navigation.switchCollection(this.otherCollection, $(evt.currentTarget).prop('href'));
-        },
-        set: function(evt) {
-            evt.preventDefault();
+        gotRelatedObjects: function(collectionObjects) {
+            var a = this.$('a');
+            if (collectionObjects.length > 0) {
+                var co = collectionObjects[0];
+                a.attr('href', co.viewUrl());
+                format(co).done(function(text) { a.text(text); });
+            } else {
+                a.hide();
+            }
         }
     });
 });
