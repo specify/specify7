e5b14242a5d54ba85b0cf16debf3fcb757e9521f
Ben Anhalt <anhalt@ku.edu>
2014-10-09 14:00:59 -0500

    add order by to aggregation.

--- specifyweb/stored_queries/format.py
+++ specifyweb/stored_queries/format.py
@@ -84,13 +84,17 @@ class ObjectFormatter(object):
         aggregatorNode = self.getAggregatorDef(specify_model, aggregator_name)
         logger.debug("using aggregator: %s", ElementTree.tostring(aggregatorNode))
         formatter_name = aggregatorNode.attrib.get('format', None)
-        separator = aggregatorNode.attrib.get('separator', '')
+        separator = aggregatorNode.attrib.get('separator', None)
+        order_by = aggregatorNode.attrib.get('orderfieldname', None)
 
         orm_table = getattr(models, field.relatedModelName)
+        if order_by is not None:
+            order_by = getattr(orm_table, order_by)
+
         join_column = list(inspect(getattr(orm_table, field.otherSideName)).property.local_columns)[0]
         subquery = orm.Query([]).select_from(orm_table) \
                              .filter(join_column == getattr(rel_table, rel_table._id)) \
                              .correlate(rel_table)
         subquery, formatted = self.objformat(subquery, orm_table, formatter_name, {})
-        aggregated = coalesce(group_concat(formatted, separator), '')
+        aggregated = coalesce(group_concat(formatted, separator, order_by), '')
         return subquery.add_column(aggregated).as_scalar()
--- specifyweb/stored_queries/group_concat.py
+++ specifyweb/stored_queries/group_concat.py
@@ -11,9 +11,17 @@ class group_concat(expression.FunctionElement):
 @compiler.compiles(group_concat)
 def _group_concat_mysql(element, compiler, **kwargs):
     expr = compiler.process(element.clauses.clauses[0])
-    if len(element.clauses) == 2:
-        separator = compiler.process(element.clauses.clauses[1])
-        return 'GROUP_CONCAT(%s SEPARATOR %s)' % (expr, separator)
-    else:
-        return 'GROUP_CONCAT(%s)' % expr
+    def process_clause(idx):
+        return compiler.process(element.clauses.clauses[idx])
+
+    separator = process_clause(1) if len(element.clauses) > 1 else None
+    order_by = process_clause(2) if len(element.clauses) > 2 else None
+
+    inner = expr
+    if order_by is not None:
+        inner += " ORDER BY %s" % order_by
+    if separator is not None:
+        inner += " SEPARATOR %s" % separator
+
+    return 'GROUP_CONCAT(%s)' % inner
 
