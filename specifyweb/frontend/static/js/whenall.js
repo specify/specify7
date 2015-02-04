24e03f107c8988f65203e329c45ab2c085e937b4
Ben Anhalt <anhalt@ku.edu>
2015-02-04 17:16:00 -0600

    querycbx can search multiple fields. results are simply concatted.

--- specifyweb/frontend/static/js/whenall.js
+++ specifyweb/frontend/static/js/whenall.js
@@ -1,5 +1,8 @@
 define(['jquery', 'underscore'], function($, _) {
     return function(deferreds) {
+        if (deferreds.length == 1) {
+            return $.when(deferreds[0]).pipe(function() { return [arguments]; });
+        }
         return $.when.apply($, _(deferreds).toArray()).pipe(function() { return _(arguments).toArray(); });
     };
 });
