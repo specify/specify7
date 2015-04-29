specify/specify7

commit b41fc2705066b3c4330c793e7d5b852abbaec1d2

add password change and user agent tools. improve user tools.

specifyweb/frontend/static/js/headerui.js
@@ -1,5 +1,5 @@
 define([
-    'require', 'jquery', 'underscore', 'backbone', 'navigation', 'domain', 'templates', 'schema',
+    'require', 'jquery', 'underscore', 'backbone', 'navigation', 'domain', 'schema', 'usertools',
     'jquery-bbq', 'jquery-ui',
 // Tasks included in header:
     'toolbarwelcome',
@@ -8,14 +8,15 @@ define([
     'toolbarrecordsets',
     'toolbarquery',
     'toolbarreport',
-    'toolbarattachments'
+    'toolbarattachments',
+    'toolbarmasterkey',
+    'toolbarusers'
 ], function headerUI(
-    require, $, _, Backbone, navigation, domain, templates, schema,
+    require, $, _, Backbone, navigation, domain, schema, UserTools,
     jquery_bbq, jquery_ui
 ) {
     "use strict";
-
-    var toolModules = _.chain(arguments).tail(headerUI.length).filter(function(mod){ return !mod.disabled; }).value();
+    var toolModules = _(arguments).tail(headerUI.length);
 
     var ExpressSearchInput = Backbone.View.extend({
         __name__: "ExpressSearchInput",
@@ -51,12 +52,16 @@ define([
             var app = require('specifyapp');
             this.user = app.user;
 
-            _.each(toolModules, function(module) {
+            this.toolModules = toolModules.filter(function(mod){ return !(mod.disabled && mod.disabled(app.user)); });
+
+            this.visibleTools = this.toolModules.filter(function(t) { return t.icon != null; });
+            this.hiddenTools = this.toolModules.filter(function(t) { return t.icon == null; });
+
+            _.each(this.toolModules, function(module) {
                 app.router.route('task/' + module.task + '/', 'startTask', module.execute.bind(module));
             });
         },
         render: function() {
-            var _this = this;
             (new ExpressSearchInput()).render().$el.appendTo(this.el);
             this.user.isauthenticated && this.$('#user-tools a.username').text(this.user.name);
             this.$('#user-tools a.login-logout')
@@ -78,30 +83,25 @@ define([
             });
             this.$('#header-loading').remove();
             this.$el.append('<nav id="site-nav">');
-            var ul = $('<ul>');
-            _(toolModules).each(function(toolDef) {
-                $('<a>', { href: '/specify/task/' + toolDef.task + '/' })
-                    .text(toolDef.title)
-                    .prepend($('<img>', {src: toolDef.icon}))
-                    .appendTo($('<li>').appendTo(ul));
-            });
-            this.$('#site-nav').append(ul);
+            var lis = this.visibleTools.map(this.makeButton);
+            $('<ul>').append(lis).appendTo(this.$('#site-nav'));
             return this;
+        },
+        makeButton: function(toolDef) {
+            var li = $('<li>');
+            $('<a>', { href: '/specify/task/' + toolDef.task + '/' })
+                .text(toolDef.title)
+                .prepend($('<img>', {src: toolDef.icon}))
+                .appendTo(li);
+            return li[0];
         },
         siteNavClick: function(evt) {
             evt.preventDefault();
             var index = this.$('#site-nav > ul > li > a').index(evt.currentTarget);
-            toolModules[index].execute();
+            this.visibleTools[index].execute();
         },
         openUserTools: function(evt) {
-            $(templates.usertools({user: this.user}))
-                .appendTo(this.el)
-                .dialog({
-                    modal: true,
-                    buttons: [
-                        {text: 'Cancel', click: function() { $(this).dialog('close'); }}
-                    ]
-                });
+            new UserTools({user: this.user, tools: this.hiddenTools}).render();
         },
         changeCollection: function(evt) {
             navigation.switchCollection(parseInt(this.$('#user-tools select').val()), '/');

specifyweb/frontend/static/html/templates/passwordchange.html
@@ -1,0 +1,7 @@
+<form>
+  <table>
+    <tr><td><label>Password</label></td><td><input type="password" name="pass1" placeholder="Password"></td></tr>
+    <tr><td><label>Confirm</label></td><td><input type="password" name="pass2" placeholder="Confirm"></td></tr>
+  </table>
+  <input type="submit" style="display: none;">
+</form>

specifyweb/frontend/static/html/templates/usertools.html
@@ -1,10 +1,0 @@
-<div title="User">
-  <ul>
-    <li><a href="/accounts/logout/">Log out</a></li>
-    <li><a href="/accounts/password_change/">Change password</a></li>
-    <li><a href="/specify/master_key/" class="intercept-navigation">Generate <em>Specify 6</em> master key</a></li>
-    <!-- <% if (user.isadmin) { %> -->
-    <!-- <li><a href="/specify/manage_users/" class="intercept-navigation">Manage users</a></li> -->
-    <!-- <% } %> -->
-  </ul>
-</div>

specifyweb/frontend/static/js/hosttaxonplugin.js
@@ -23,7 +23,7 @@ define([
             new QueryCbx({
                 el: this.el,
                 model: this.model,
-                relatedModel: schema.Taxon,
+                relatedModel: schema.Taxon, // Todo: this shouldn't work.
                 forceCollection: rightsideCollection,
                 hideButtons: true,
                 init: {},

specifyweb/frontend/static/js/passwordplugin.js
@@ -1,0 +1,57 @@
+define([
+    'jquery', 'underscore', 'backbone', 'uiplugin', 'templates'
+], function($, _, Backbone, UIPlugin, templates) {
+    "use strict";
+
+    var Dialog = Backbone.View.extend({
+        __name__: "PasswordResetDialog",
+        events: {
+            'submit form' : 'submit'
+        },
+        render: function() {
+            this.$el.attr('title', "Reset Password");
+            this.$el.append(templates.passwordchange());
+            this.$el.dialog({
+                modal: true,
+                width: 'auto',
+                close: function() { $(this).remove(); },
+                buttons: [
+                    {text: 'Set', click: this.submit.bind(this)},
+                    {text: 'Cancel', click: function() { $(this).dialog('close'); }}
+                ]
+            });
+            return this;
+        },
+        submit: function(event) {
+            event && event.preventDefault();
+            var pass1 = this.$('input[name="pass1"]').val();
+            var pass2 = this.$('input[name="pass2"]').val();
+
+            if (pass1.length < 6) {
+                alert("Password must have at least six characters.");
+                return;
+            }
+            if (pass1 !== pass2) {
+                alert("Passwords do not match.");
+                return;
+            }
+            $.post('/api/set_password/' + this.model.id + '/', {password: pass1});
+            this.$el.dialog('close');
+        }
+    });
+
+    return UIPlugin.extend({
+        __name__: "PasswordUIPlugin",
+        events: {
+            'click': 'click'
+        },
+        render: function() {
+            this.$el.attr('value', 'Reset Password');
+            this.model.isNew() && this.$el.attr('title', 'Save user before setting password.').prop('disabled', true);
+            return this;
+        },
+        click: function(event) {
+            new Dialog({model: this.model}).render();
+        }
+    });
+});

specifyweb/frontend/static/js/querycbx.js
@@ -99,9 +99,7 @@ define([
             var init = this.init || specifyform.parseSpecifyProperties(control.data('specify-initialize'));
             if (!init.clonebtn || init.clonebtn.toLowerCase() !== "true") this.$('.querycbx-clone').hide();
 
-            var field = this.model.specifyModel.getField(this.fieldName);
-
-            this.relatedModel || (this.relatedModel = field.getRelatedModel());
+            this.relatedModel || (this.relatedModel = this.model.specifyModel.getField(this.fieldName).getRelatedModel());
             this.typesearch || (this.typesearch = lookupTypesearch(init.name));
 
             var selectStmt = this.typesearch.text();

specifyweb/frontend/static/js/specifyapp.js
@@ -8,8 +8,7 @@ define([
     'treetask',
     'expresssearchtask',
     'datamodeltask',
-    'attachmentstask',
-    'masterkeytask'
+    'attachmentstask'
 ], function module(
     $, _, Backbone, schema, businessRules, errorview,
     WelcomeView, HeaderUI, NotFoundView, navigation,

specifyweb/frontend/static/js/specifyplugins.js
@@ -2,13 +2,15 @@ define([
     'jquery', 'underscore', 'specifyapi', 'latlongui', 'partialdateui',
     'collectionrelonetomanyplugin', 'collectionrelonetooneplugin',
     'uiplugin', 'geolocateplugin', 'weblinkbutton', 'attachmentplugin',
-    'hosttaxonplugin', 'templates'
+    'hosttaxonplugin', 'passwordplugin', 'useragentsplugin', 'templates'
 ], function($, _, api, LatLonUI, PartialDateUI, collectionrelonetomanyplugin,
             collectionrelonetooneplugin, UIPlugin, GeoLocatePlugin, WebLinkButton,
-            AttachmentPlugin, HostTaxonPlugin, templates) {
+            AttachmentPlugin, HostTaxonPlugin, PasswordPlugin, UserAgentsPlugin, templates) {
     "use strict";
 
     return {
+        PasswordUI: PasswordPlugin,
+        UserAgentsUI: UserAgentsPlugin,
         HostTaxonPlugin: HostTaxonPlugin,
         ColRelTypePlugin: collectionrelonetooneplugin,
         CollectionRelOneToManyPlugin: collectionrelonetomanyplugin,

commit 4c4a388ce1a0739cdf40b1cb9c8d3d8b97348c45

fix bug introduced in b41fc27. fixes #72

specifyweb/frontend/static/js/headerui.js
@@ -52,7 +52,9 @@ define([
             var app = require('specifyapp');
             this.user = app.user;
 
-            this.toolModules = toolModules.filter(function(mod){ return !(mod.disabled && mod.disabled(app.user)); });
+            this.toolModules = toolModules.filter(function(mod){
+                return !(_.isFunction(mod.disabled) ? mod.disabled(app.user) : mod.disabled);
+            });
 
             this.visibleTools = this.toolModules.filter(function(t) { return t.icon != null; });
             this.hiddenTools = this.toolModules.filter(function(t) { return t.icon == null; });