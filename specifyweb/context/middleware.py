specify/specify7

commit b1784a2cec4baad673c7e106ab9e17accc96b102

set cache control headers to allow more resources to be cached by browsers. fixes #73

specifyweb/context/middleware.py
@@ -1,43 +1,54 @@
 from django.http import HttpResponseBadRequest
+from django.utils.functional import SimpleLazyObject
 from django.conf import settings
 
 from specifyweb.specify.models import Collection, Specifyuser, Agent
 from specifyweb.specify.filter_by_col import filter_by_collection
 
+
+def get_cached(attr, func, request):
+    if not hasattr(request, attr):
+        setattr(request, attr, func(request))
+    return getattr(request, attr)
+
+def get_user(request):
+    if request.user.is_authenticated():
+        return request.user
+    elif settings.ANONYMOUS_USER:
+        return Specifyuser.objects.get(name=settings.ANONYMOUS_USER)
+    else:
+        return None
+
+def get_collection(request):
+    qs = Collection.objects.select_related('discipline',
+                                           'discipline__division',
+                                           'discipline__division__institution')
+
+    try:
+        collection_id = int(request.COOKIES.get('collection', ''))
+    except ValueError:
+        return qs.all()[0]
+    else:
+        return qs.get(id=collection_id)
+
+def get_agent(request):
+    try:
+        return filter_by_collection(Agent.objects, request.collection) \
+            .select_related('specifyuser') \
+            .get(specifyuser=request.specify_user)
+    except Agent.DoesNotExist:
+        return None
+
+
 class ContextMiddleware(object):
     """Adds information about the logged in user and collection to requests."""
     def process_request(self, request):
-        if request.user.is_authenticated():
-            specify_user = request.user
-        elif settings.ANONYMOUS_USER:
-            specify_user = Specifyuser.objects.get(name=settings.ANONYMOUS_USER)
-        else:
-            return
-
-        qs = Collection.objects.select_related('discipline',
-                                               'discipline__division',
-                                               'discipline__division__institution')
-
-        try:
-            collection_id = int(request.session.get('collection', ''))
-        except ValueError:
-            collection = qs.all()[0]
-        else:
-            collection = qs.get(id=collection_id)
-
-        try:
-            agent = filter_by_collection(Agent.objects, collection) \
-                .select_related('specifyuser') \
-                .get(specifyuser=specify_user)
-        except Agent.DoesNotExist:
-            agent = None
-
-        request.specify_collection = collection
-        request.specify_user_agent = agent
-        request.specify_user = specify_user
+        request.specify_collection = SimpleLazyObject(lambda: get_cached('_cached_collection', get_collection, request))
+        request.specify_user_agent = SimpleLazyObject(lambda: get_cached('_cached_agent', get_agent, request))
+        request.specify_user       = SimpleLazyObject(lambda: get_cached('_cached_specify_user', get_user, request))
 
     def process_template_response(self, request, response):
         collection = getattr(request, 'specify_collection', None)
-        if collection:
+        if collection is not None:
             response.context_data['collection'] = collection
         return response

specifyweb/attachment_gw/views.py
@@ -5,6 +5,7 @@ import requests, time, hmac, json
 
 from django.http import HttpResponse
 from django.views.decorators.http import require_GET
+from django.views.decorators.cache import cache_control
 from django.conf import settings
 
 from specifyweb.specify.views import login_maybe_required
@@ -25,6 +26,7 @@ def get_collection():
 
 @login_maybe_required
 @require_GET
+@cache_control(max_age=86400, private=True)
 def get_settings(request):
     if server_urls is None:
         return HttpResponse("{}", content_type='application/json')

specifyweb/context/urls.py
@@ -1,18 +1,22 @@
-from django.conf.urls import patterns
+from django.conf.urls import url
 
-urlpatterns = patterns(
-    'specifyweb.context.views',
-    (r'^login/$', 'api_login'),
-    (r'^collection/$', 'collection'),
-    (r'^user.json$', 'user'),
-    (r'^system_info.json$', 'system_info'),
-    (r'^domain.json$', 'domain'),
-    (r'^view.json$', 'view'),
-    (r'^datamodel.json$', 'datamodel'),
-    (r'^schema_localization.json$', 'schema_localization'),
-    (r'^app.resource$', 'app_resource'),
-    (r'^available_related_searches.json$', 'available_related_searches'),
-    (r'^attachment_settings.json$', 'attachment_settings'),
-    (r'^report_runner_status.json$', 'report_runner_status'),
-    (r'^remoteprefs.properties$', 'remote_prefs')
-)
+from specifyweb.attachment_gw.views import get_settings as attachment_settings
+from specifyweb.report_runner.views import get_status as report_runner_status
+from . import views
+
+urlpatterns = [
+    url(r'^login/$', views.api_login),
+    url(r'^collection/$', views.collection),
+    url(r'^user.json$', views.user),
+    url(r'^system_info.json$', views.system_info),
+    url(r'^domain.json$', views.domain),
+    url(r'^view.json$', views.view),
+    url(r'^datamodel.json$', views.datamodel),
+    url(r'^schema_localization.json$', views.schema_localization),
+    url(r'^app.resource$', views.app_resource),
+    url(r'^available_related_searches.json$', views.available_related_searches),
+    url(r'^remoteprefs.properties$', views.remote_prefs),
+
+    url(r'^attachment_settings.json$', attachment_settings),
+    url(r'^report_runner_status.json$', report_runner_status),
+]

specifyweb/context/views.py
@@ -3,6 +3,7 @@ import re
 from django.http import HttpResponse, HttpResponseBadRequest, Http404, HttpResponseForbidden
 from django.views.decorators.http import require_http_methods, require_GET
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.cache import cache_control
 from django.contrib.auth import authenticate, views as auth_views, logout as auth_logout, login as auth_login
 from django.utils import simplejson
 from django.conf import settings
@@ -11,27 +12,31 @@ from specifyweb.specify.models import Collection, Spappresourcedata, Spversion
 from specifyweb.specify.serialize_datamodel import datamodel_to_json
 from specifyweb.specify.views import login_maybe_required
 from specifyweb.specify.specify_jar import specify_jar
-from specifyweb.attachment_gw.views import get_settings as attachment_settings
-from specifyweb.report_runner.views import get_status as report_runner_status
 
 from .app_resource import get_app_resource
 from .viewsets import get_view
 from .schema_localization import get_schema_localization
 
+def set_collection_cookie(response, collection_id):
+    response.set_cookie('collection', str(collection_id), max_age=365*24*60*60)
+
 def login(request):
     """A Django view to log users into the system.
     Supplements the stock Django login with a collection selection.
     """
+    collection = ''
     if request.method == 'POST':
         try:
-            request.session['collection'] = request.POST['collection_id']
+            collection = request.POST['collection_id']
         except:
             return HttpResponseBadRequest('collection id value missing')
 
     kwargs = {
         'template_name': 'login.html',
         'extra_context': { 'collections': Collection.objects.all() } }
-    return auth_views.login(request, **kwargs)
+    response = auth_views.login(request, **kwargs)
+    set_collection_cookie(response, collection)
+    return response
 
 def logout(request):
     """A Django view to log users out."""
@@ -56,9 +61,9 @@ def api_login(request):
             collection = Collection.objects.get(id=data['collection'])
         except Collection.DoesNotExist:
             return HttpResponseBadRequest('collection %s does not exist' % data['collection'])
-        request.session['collection'] = collection.id
-
-        return HttpResponse('', status=204)
+        response = HttpResponse('', status=204)
+        set_collection_cookie(response, collection.id)
+        return response
 
     return HttpResponse(simplejson.dumps(dict(
         collections={c.collectionname: c.id for c in Collection.objects.all()},
@@ -78,14 +83,16 @@ def collection(request):
             return HttpResponseBadRequest('bad collection id', content_type="text/plain")
         except Collection.DoesNotExist:
             return HttpResponseBadRequest('collection does not exist', content_type="text/plain")
-        request.session['collection'] = str(collection.id)
-        return HttpResponse('ok')
+        response = HttpResponse('ok')
+        set_collection_cookie(response, collection.id)
+        return response
     else:
-        collection = request.session.get('collection', '')
+        collection = request.COOKIES.get('collection', '')
         return HttpResponse(collection, content_type="text/plain")
 
 @login_maybe_required
 @require_GET
+@cache_control(max_age=86400, private=True)
 def user(request):
     """Return json representation of the currently logged in SpecifyUser."""
     from specifyweb.specify.api import obj_to_data, toJson
@@ -97,6 +104,7 @@ def user(request):
 
 @login_maybe_required
 @require_GET
+@cache_control(max_age=86400, private=True)
 def domain(request):
     """Return the context hierarchy of the logged in collection."""
     collection = request.specify_collection
@@ -112,6 +120,7 @@ def domain(request):
 
 @login_maybe_required
 @require_GET
+@cache_control(max_age=86400, private=True)
 def app_resource(request):
     """Return a Specify app resource by name taking into account the logged in user and collection."""
     try:
@@ -128,6 +137,7 @@ def app_resource(request):
 
 @login_maybe_required
 @require_GET
+@cache_control(max_age=86400, private=True)
 def available_related_searches(request):
     """Return a list of the available 'related' express searches."""
     from specifyweb.express_search import related_searches
@@ -146,6 +156,7 @@ datamodel_json = None
 
 @require_GET
 @login_maybe_required
+@cache_control(max_age=86400, public=True)
 def datamodel(request):
     from specifyweb.specify.models import datamodel
     global datamodel_json
@@ -156,6 +167,7 @@ def datamodel(request):
 
 @require_GET
 @login_maybe_required
+@cache_control(max_age=86400, private=True)
 def schema_localization(request):
     """Return the schema localization information for the logged in collection."""
     sl = get_schema_localization(request.specify_collection)
@@ -163,6 +175,7 @@ def schema_localization(request):
 
 @require_GET
 @login_maybe_required
+@cache_control(max_age=86400, private=True)
 def view(request):
     """Return a Specify view definition by name taking into account the logged in user and collection."""
     if 'collectionid' in request.GET:
@@ -181,6 +194,7 @@ def view(request):
 
 @require_GET
 @login_maybe_required
+@cache_control(max_age=86400, private=True)
 def remote_prefs(request):
     res = Spappresourcedata.objects.filter(
         spappresource__name='preferences',
@@ -190,6 +204,7 @@ def remote_prefs(request):
     return HttpResponse(data, content_type='text/x-java-properties')
 
 @require_GET
+@cache_control(max_age=86400, public=True)
 def system_info(request):
     spversion = Spversion.objects.get()
 

specifyweb/report_runner/views.py
@@ -3,6 +3,7 @@ import json
 
 from django.views.decorators.http import require_GET, require_POST
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.cache import cache_control
 from django.db.models import Q
 from django.http import HttpResponse
 from django.conf import settings
@@ -16,6 +17,7 @@ class ReportException(Exception):
     pass
 
 @require_GET
+@cache_control(max_age=86400, private=True)
 def get_status(request):
     resp = {'available': settings.REPORT_RUNNER_HOST != ''}
     return HttpResponse(toJson(resp), content_type="application/json")

specifyweb/specify/views.py
@@ -29,7 +29,7 @@ def api_view(dispatch_func):
     in the api logic."""
     @login_maybe_required
     @csrf_exempt
-    @cache_control(private=True, max_age=0)
+    @cache_control(private=True, max_age=2)
     def view(request, *args, **kwargs):
         if request.method != "GET" and (
             settings.RO_MODE or
@@ -59,6 +59,7 @@ def rows(request, model):
     return api.rows(request, model)
 
 @require_GET
+@cache_control(max_age=365*24*60*60, public=True)
 def images(request, path):
     """A Django view that serves images and icons from the Specify thickclient jar file."""
     mimetype = mimetypes.guess_type(path)[0]
@@ -71,6 +72,7 @@ def images(request, path):
 
 @login_maybe_required
 @require_GET
+@cache_control(max_age=24*60*60, public=True)
 def properties(request, name):
     """A Django view that serves .properities files from the thickclient jar file."""
     path = name + '.properties'

commit 152367976eb6f6d7e02c0351ced87f86c463a81a

fix bug introduced in b1784a2

specifyweb/context/middleware.py
@@ -33,7 +33,7 @@ def get_collection(request):
 
 def get_agent(request):
     try:
-        return filter_by_collection(Agent.objects, request.collection) \
+        return filter_by_collection(Agent.objects, request.specify_collection) \
             .select_related('specifyuser') \
             .get(specifyuser=request.specify_user)
     except Agent.DoesNotExist: