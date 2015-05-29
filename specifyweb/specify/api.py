d2b12dbdf03e4e70d1434f2ef80db25052199507
Ben Anhalt <anhalt@ku.edu>
2015-05-29 11:58:45 -0500

    correctly delete dependent fk objects.
    Dependent -to-one related objects are deleted when the referencing
    object is deleted or the referencing field is set to null. fixes #86

--- specifyweb/specify/api.py
+++ specifyweb/specify/api.py
@@ -327,6 +327,18 @@ def set_fields_from_data(obj, data):
         if direct and not isinstance(field, ForeignKey):
             setattr(obj, field_name, prepare_value(field, val))
 
+def is_dependent_field(obj, field_name):
+    return obj.specify_model.get_field(field_name).dependent or (
+            obj.__class__ is models.Collectionobject and
+            field_name == 'collectingevent' and
+            obj.collection.isembeddedcollectingevent)
+
+def get_related_or_none(obj, field_name):
+    try:
+        return getattr(obj, field_name)
+    except ObjectDoesNotExist:
+        return None
+
 def handle_fk_fields(collection, agent, obj, data):
     """Where 'obj' is a Django model instance and 'data' is a dict,
     set foreign key fields in the object from the provided data.
@@ -345,25 +357,18 @@ def handle_fk_fields(collection, agent, obj, data):
     else:
         items = data.items()
 
+    dependents_to_delete = []
     for field_name, val in items:
         field, model, direct, m2m = obj._meta.get_field_by_name(field_name)
         if not isinstance(field, ForeignKey): continue
 
-        try:
-            old_related = getattr(obj, field_name)
-        except ObjectDoesNotExist:
-            old_related = None
-
-        dependent = obj.specify_model.get_field(field_name).dependent or (
-            obj.__class__ is models.Collectionobject and
-            field_name == 'collectingevent' and
-            obj.collection.isembeddedcollectingevent)
+        old_related = get_related_or_none(obj, field_name)
+        dependent = is_dependent_field(obj, field_name)
 
         if val is None:
             setattr(obj, field_name, None)
             if dependent and old_related:
-                auditlog.remove(old_related, agent, obj)
-                old_related.delete()
+                dependents_to_delete.append(old_related)
 
         elif isinstance(val, field.related.parent_model):
             # The related value was patched into the data by a parent object.
@@ -396,11 +401,11 @@ def handle_fk_fields(collection, agent, obj, data):
 
             setattr(obj, field_name, rel_obj)
             if dependent and old_related and old_related.id != rel_obj.id:
-                auditlog.remove(old_related, agent, obj)
-                old_related.delete()
+                dependents_to_delete.append(old_related)
             data[field_name] = obj_to_data(rel_obj)
         else:
             raise Exception('bad foreign key field in data')
+    return dependents_to_delete
 
 def handle_to_many(collection, agent, obj, data):
     """For every key in the dict 'data' which is a *-to-many field in the
@@ -450,16 +455,26 @@ def handle_to_many(collection, agent, obj, data):
 
 @transaction.commit_on_success
 def delete_resource(agent, name, id, version):
-    return delete_obj(agent, name, id, version)
-
-def delete_obj(agent, name, id, version, parent_obj=None):
     """Delete the resource with 'id' and model named 'name' with optimistic
     locking 'version'.
     """
     obj = get_object_or_404(name, id=int(id))
+    return delete_obj(agent, obj, version)
+
+def delete_obj(agent, obj, version=None, parent_obj=None):
+    dependents_to_delete = filter(None, (
+        get_related_or_none(obj, field.name)
+        for field in obj._meta.fields
+        if isinstance(field, ForeignKey) and is_dependent_field(obj, field.name)
+    ))
+
     auditlog.remove(obj, agent, parent_obj)
-    bump_version(obj, version)
+    if version is not None:
+        bump_version(obj, version)
     obj.delete()
+
+    for dep in dependents_to_delete:
+      delete_obj(agent, dep, parent_obj=obj)
 
 @transaction.commit_on_success
 def put_resource(collection, agent, name, id, version, data):
@@ -471,7 +486,7 @@ def update_obj(collection, agent, name, id, version, data, parent_obj=None):
     """
     obj = get_object_or_404(name, id=int(id))
     data = cleanData(obj.__class__, data, agent)
-    handle_fk_fields(collection, agent, obj, data)
+    dependents_to_delete = handle_fk_fields(collection, agent, obj, data)
     set_fields_from_data(obj, data)
 
     try:
@@ -484,6 +499,8 @@ def update_obj(collection, agent, name, id, version, data, parent_obj=None):
     bump_version(obj, version)
     obj.save(force_update=True)
     auditlog.update(obj, agent, parent_obj)
+    for dep in dependents_to_delete:
+        delete_obj(agent, dep, parent_obj=obj)
     handle_to_many(collection, agent, obj, data)
     return obj
 
@@ -568,11 +585,7 @@ def field_to_val(obj, field):
     be either a regular field or a *-to-one field.
     """
     if isinstance(field, ForeignKey):
-        dependent = obj.specify_model.get_field(field.name).dependent or (
-            obj.__class__ is models.Collectionobject and
-            field.name == 'collectingevent' and
-            obj.collection.isembeddedcollectingevent)
-        if dependent:
+        if is_dependent_field(obj, field.name):
             related_obj = getattr(obj, field.name)
             if related_obj is None: return None
             return obj_to_data(related_obj)
