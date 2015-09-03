16eda40ea545320c7b2291ed70397b6699cbf59f
Ben Anhalt <anhalt@ku.edu>
2015-09-03 13:19:50 -0500

    clean up tree buildFullName.

--- specifyweb/frontend/static/js/treebusinessrules.js
+++ specifyweb/frontend/static/js/treebusinessrules.js
@@ -1,9 +1,37 @@
 define(['q', 'underscore'], function(Q, _) {
     "use strict";
 
-    function buildFullName(resource, acc, start, parent, defitem) {
+    function buildFullName(resource, options) {
+        return Q.all([resource, [], true,
+                      resource.getRelated('parent', {prePop: true, noBusinessRules: true}),
+                      resource.getRelated('definitionitem', {prePop: true, noBusinessRules: true})
+                     ])
+            .spread(_buildFullName)
+            .then(
+                function(acc) {
+                    return {
+                        key: 'tree-structure',
+                        valid: true,
+                        action: function() {
+                            return resource.set('fullname', acc.reverse().join(' '));
+                        }
+                    };
+                },
+                function(error) {
+                    if (error === 'bad-tree-structure' && options.reportBadStructure)
+                        return {
+                            key: 'tree-structure',
+                            valid: false,
+                            reason: 'Bad tree structure.'
+                        };
+                    else throw error;
+                }
+            );
+    }
+
+    function _buildFullName(resource, acc, start, parent, defitem) {
         if (parent && (parent.get('rankid') >= resource.get('rankid'))) {
-            return Q.reject('Bad tree structure.');
+            throw 'bad-tree-structure';
         }
         if (start || defitem.get('isinfullname')) acc.push(resource.get('name'));
         if (parent == null) {
@@ -12,7 +40,7 @@ define(['q', 'underscore'], function(Q, _) {
             return Q.all([parent, acc, false,
                           parent.getRelated('parent', {prePop: true, noBusinessRules: true}),
                           parent.getRelated('definitionitem', {prePop: true, noBusinessRules: true})
-                         ]).spread(buildFullName);
+                         ]).spread(_buildFullName);
         }
     };
 
@@ -25,34 +53,23 @@ define(['q', 'underscore'], function(Q, _) {
             });
         },
         run: function(resource, fieldName) {
-            var promise = treeBusinessRules.buildFullName(resource).then(
-                function(acc) {
-                    return {
-                        key: 'tree-structure',
-                        valid: true,
-                        action: function() {
-                            return resource.set('fullname', acc.reverse().join(' '));
-                        }
-                    };
-                },
-                function(reason) {
-                    return (fieldName == 'parent') && Q.when({
-                        key: 'tree-structure',
-                        valid: false,
-                        reason: reason
-                    });
-                }
-            );
+            var promise;
+            switch (fieldName) {
+            case 'parent':
+                // only report bad tree structure as a problem with the parent field.
+                promise = buildFullName(resource, {reportBadStructure: true});
+                break;
+            case 'name':
+                promise = buildFullName(resource, {reportBadStructure: false});
+                break;
+            default:
+                promise = Q(null);
+            }
+
             promise.then(function(result) { console.debug('Tree BR finished',
                                                           {node: resource, field: fieldName},
                                                           result); });
             return promise;
-        },
-        buildFullName: function(resource) {
-            return Q.all([resource, [], true,
-                          resource.getRelated('parent', {prePop: true, noBusinessRules: true}),
-                          resource.getRelated('definitionitem', {prePop: true, noBusinessRules: true})
-                         ]).spread(buildFullName);
         },
         init: function(resource) {
             resource.isNew() && resource.set('isaccepted', true);
