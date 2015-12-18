a598dc2187042621648f16dbf8c89e970e8b34d2
Ben Anhalt <anhalt@ku.edu>
2015-12-18 12:49:06 -0600

    remove some requirejs stuff.

--- specifyweb/frontend/js_src/lib/main.js
+++ specifyweb/frontend/js_src/lib/main.js
@@ -1,38 +1,5 @@
-// require({
-//     baseUrl: "/static/js",
-//     waitSeconds: 0,
-//     priority: ['jquery'],
-//     paths: {
-//         'jquery'         : "vendor/jquery-1.7.2",
-//         'jquery-ui'      : "vendor/jquery-ui-1.10.2.custom",
-//         'jquery-bbq'     : "vendor/jquery.ba-bbq",
-//         'jquery-ctxmenu' : "vendor/jquery.contextMenu.v1.6.6",
-//         'handsontable'   : "vendor/handsontable.full",
-//         'bacon'          : "//cdnjs.cloudflare.com/ajax/libs/bacon.js/0.7.82/Bacon",
-//         'immutable'      : "//cdnjs.cloudflare.com/ajax/libs/immutable/3.7.5/immutable",
-//         'underscore'     : "vendor/underscore",
-//         'backbone-orig'  : "vendor/backbone",
-//         'qunit'          : "vendor/qunit",
-//         'text'           : "vendor/text",
-//         'moment'         : "vendor/moment",
-//         'd3'             : "vendor/d3",
-//         'q'              : "vendor/q-1.4.1",
-//         'resources'      : '/static/config',
-//         'tmpls'          : '/static/html/templates',
-//         'context'        : '/context',
-//         'properties'     : '/properties'
-//     },
-//     shim: {
-//         'jquery-ui'      : ['jquery'],
-//         'jquery-bbq'     : ['jquery'],
-//         'jquery-ctxmenu' : ['jquery', 'jquery-ui'],
-//         'underscore'     : { exports: '_' },
-//         'moment'         : { exports: 'moment' },
-//         'handsontable'   : { exports: 'Handsontable' },
-//         'backbone-orig'  : { deps: ['jquery', 'underscore'], exports: 'Backbone' }
-//     }
-// });
 "use strict";
+
 var initialContext = require('./initialcontext.js');
 var startApp = require('./startapp.js');
 
