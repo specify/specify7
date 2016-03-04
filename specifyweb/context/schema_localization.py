3c67d1ab4b538acde263e25c5e15a471888ba87f
Ben Anhalt <anhalt@ku.edu>
2016-03-04 16:57:14 -0600

    use raw SQL to load schema localization.

--- specifyweb/context/schema_localization.py
+++ specifyweb/context/schema_localization.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 
 from django.utils import simplejson
 from django.conf import settings
+from django.db import connection
 
 from specifyweb.specify.models import (
     Splocalecontainer as Container,
@@ -15,30 +16,39 @@ def get_schema_localization(collection, schematype):
     if (disc, schematype) in schema_localization_cache:
         return schema_localization_cache[(disc, schematype)]
 
-    strings = dict(
-        ((i.containername_id, i.containerdesc_id, i.itemname_id, i.itemdesc_id), i.text) \
-            for i in SpString.objects.filter(language=settings.SCHEMA_LANGUAGE)
-        )
+    lang = settings.SCHEMA_LANGUAGE
 
-    ifields = ('format', 'ishidden', 'isuiformatter', 'picklistname',
-               'type', 'isrequired', 'weblinkname',)
+    cursor = connection.cursor()
+    cursor.execute("""
+    select name, format, ishidden!=0, isuiformatter, picklistname, type, aggregator, defaultui, n.text, d.text
+    from splocalecontainer
+    left outer join splocaleitemstr n on n.splocalecontainernameid = splocalecontainerid and n.language = %s
+    left outer join splocaleitemstr d on d.splocalecontainerdescid = splocalecontainerid and d.language = %s
+    where schematype = %s and disciplineid = %s;
+    """, [lang, lang, schematype, disc.id])
 
-    items = defaultdict(dict)
-    for i in Item.objects.all():
-        items[i.container_id][i.name.lower()] = item = dict((field, getattr(i, field)) for field in ifields)
-        item.update({
-                'name': strings.get((None, None, i.id, None), None),
-                'desc': strings.get((None, None, None, i.id), None)})
+    cfields = ('format', 'ishidden', 'isuiformatter', 'picklistname', 'type', 'aggregator', 'defaultui', 'name', 'desc')
 
-    cfields = ('format', 'ishidden', 'isuiformatter', 'picklistname', 'type', 'aggregator', 'defaultui')
+    containers = {
+        row[0]: dict(items={}, **{field: row[i+1] for i, field in enumerate(cfields)})
+        for row in cursor.fetchall()
+    }
 
-    containers = {}
-    for c in Container.objects.filter(discipline=disc, schematype=schematype):
-        containers[c.name] = container = dict((field, getattr(c, field)) for field in cfields)
-        container.update({
-                'name': strings.get((c.id, None, None, None), None),
-                'desc': strings.get((None, c.id, None, None), None),
-                'items': items[c.id] })
+    cursor.execute("""
+    select container.name, item.name,
+           item.format, item.ishidden!=0, item.isuiformatter, item.picklistname,
+           item.type, item.isrequired, item.weblinkname, n.text, d.text
+    from splocalecontainer container
+    inner join splocalecontaineritem item on item.splocalecontainerid = container.splocalecontainerid
+    left outer join splocaleitemstr n on n.splocalecontaineritemnameid = item.splocalecontaineritemid and n.language = %s
+    left outer join splocaleitemstr d on d.splocalecontaineritemdescid = item.splocalecontaineritemid and d.language = %s
+    where schematype = %s and disciplineid = %s;
+    """, [lang, lang, schematype, disc.id])
+
+    ifields = ('format', 'ishidden', 'isuiformatter', 'picklistname', 'type', 'isrequired', 'weblinkname', 'name', 'desc')
+
+    for row in cursor.fetchall():
+        containers[row[0]]['items'][row[1].lower()] = {field: row[i+2] for i, field in enumerate(ifields)}
 
     sl = schema_localization_cache[(disc, schematype)] =  simplejson.dumps(containers)
     return sl
