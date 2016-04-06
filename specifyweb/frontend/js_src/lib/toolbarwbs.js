56392f73ad54480ed386e4e04f2d3a7846067ec6
Ben Anhalt <anhalt@ku.edu>
2016-04-06 16:09:26 -0500

    update seach and workbench icons.

--- specifyweb/frontend/js_src/lib/toolbarwbs.js
+++ specifyweb/frontend/js_src/lib/toolbarwbs.js
@@ -7,7 +7,7 @@ var userInfo    = require('./userinfo.js');
 module.exports =  {
         task: 'workbenches',
         title: 'Workbench',
-        icon: '/images/Workbench32x32.png',
+        icon: '/static/img/workbench.png',
         execute: function() {
             var wbs = new schema.models.Workbench.LazyCollection({
                 filters: { specifyuser: userInfo.id, orderby: 'name' }
