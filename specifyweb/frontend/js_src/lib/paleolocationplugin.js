f9bcc58b70812f8fbe45864a0b3f6446cc6eb176
Tomislav Urban <turban@tacc.utexas.edu>
2016-12-01 12:43:13 -0600

    Fleshed out paleo location plugin to retrieve input parameters from the appropriate place based on the form that it is being called from. Modularized the JS for better readability.

--- specifyweb/frontend/js_src/lib/paleolocationplugin.js
+++ specifyweb/frontend/js_src/lib/paleolocationplugin.js
@@ -1,40 +1,106 @@
 "use strict";
 
-var $ = require('jquery');
+const $ = require('jquery');
 
-var UIPlugin = require('./uiplugin.js');
-var template = require('./templates/pmapplugin.html');
+const UIPlugin = require('./uiplugin.js');
+const template = require('./templates/pmapplugin.html');
 const Q = require('q');
-const schema = require('./schema.js');
 
-module.exports =  UIPlugin.extend({
-        __name__: "PaleolocationMapPlugin",
-        events: {
-            'click': 'click'
-        },
-        render: function() {
-            this.$el.attr('value', 'Paleo Map').prop('disabled', false);
-            return this;
-        },
-        click: function(evt) {
-            evt.preventDefault();
-            var lat = this.model.get('latitude1');
-            var lng = this.model.get('longitude1');
-            var start_ma = Q(this.model.rget('paleocontext', true))
-                  .then(pc => pc == null ? '' : pc.get('chronosstratid'))
-                  .Q(this.model.rget('geologictimeperiod', true))
-                  .then(gtp => gtp == null ? '' : gtp.get('startperiod'));
-            if (lat != null && lng != null && start_ma != null) {
-                $('<div>').append(template({lat: lat, lng: lng, ma: start_ma})).dialog({
-                    width: 800,
-                    height: 600,
-                    title: this.model.specifyModel.getLocalizedName(),
-                    close: function() { $(this).remove(); }
-                }).css({ overflow: 'hidden' });
-            } else {
-                $('<div title="No coordinates"><p>Locality must have coordinates and paleo context to be mapped.</p></div>')
-                    .dialog({close: function() { $(this).remove(); }});
+module.exports = UIPlugin.extend({
+    __name__: "PaleolocationMapPlugin",
+    events: {
+        'click': 'click'
+    },
+    render: function () {
+        this.$el.attr('value', 'Paleo Map').prop('disabled', false);
+        return this;
+    },
+    click: function (evt) {
+        evt.preventDefault();
+        this.paleoRefData().done(function (data) {
+            data ? this.openPaleoMap(data) : this.paleoRequired();
+        }.bind(this));
+    },
+    paleoRequired: function () {
+        $('<div title="Geography Required">' +
+            '<p><span class="ui-icon ui-icon-alert" style="display: inline-block;"></span>' +
+            'The Paleo Map plugin requires that the locality have geographic coordinates and that the paleo ' +
+            'context have a geographic age with at least a start time or and end time populated.' +
+            '</p></div>'
+        ).dialog({
+            close: function () {
+                $(this).remove();
             }
+        });
+    },
+    openPaleoMap: function (data) {
+        if (data == null) {
+            const form = this.model.specifyModel.getLocalizedName();  // TODO: is this the model/form/table name?
+            $('<div title="No coordinates"><p>' + form + ' must have coordinates and paleo context to be mapped.</p></div>')
+                .dialog({
+                    close: function () {
+                        $(this).remove();
+                    }
+                });
+        } else {
+            $('<div>').append(template({lat: data.lat, lng: data.lng, ma: data.ma})).dialog({ // TODO
+                width: 800,
+                height: 600,
+                title: form,
+                close: function () {
+                    $(this).remove();
+                }
+            }).css({overflow: 'hidden'});
         }
-    }, { pluginsProvided: ['LocalityPaleoMap'] });
+    },
+    paleoRefData: function () {
+
+        let lat, lng, start_ma, end_ma;
+
+        if (this.model.name == 'locality') {
+            // retrieve the geographic coordinates relative to the locality table
+            lat = this.model.get('latitude1');
+            lng = this.model.get('longitude1');
+        } else if (this.model.name == 'collectingevent') {
+            // ...relative to the collectingevent table
+            lat = Q(this.model.rget('locality.latitude1', true));
+            lng = Q(this.model.rget('locality.longitude1', true));
+        } else if (this.model.name == 'collectionobject') {
+            // ...relative to the collectionobject table
+            lat = Q(this.model.rget('collectingevent.locality.latitude1', true));
+            lng = Q(this.model.rget('collectingevent.locality.longitude1', true));
+        } else {
+            $('<div title="Incorrect Form"><p>This plugin cannot be used on this form. Try moving it to the locality, ' +
+                'collecting event or collection object forms.</p></div>')
+                .dialog({
+                    close: function () {
+                        $(this).remove();
+                    }
+                });
+        }
+
+        // Because the paleo context is related directly to each of the possible forms in the same way
+        // we can treat the retrieval of the age in the same all for all forms.
+        start_ma = Q(this.model.rget('paleocontext.geologictimeperiod.startperiod', true));
+        end_ma = Q(this.model.rget('paleocontext.geologictimeperiod.endperiod', true));
+        let ma;
+
+        return Q.all([lat, lng, start_ma, end_ma]).spread((lat, lng, start_ma, end_ma) => { // TODO
+            if (lat == null) return null;
+            if (lng == null) return null;
+            // calculate the mid-point of the age if possible
+            if (start_ma == null && end_ma == null) {
+                return null;
+            } else if (start_ma == null) {
+                ma = end_ma;
+            } else if (end_ma == null) {
+                ma = start_ma;
+            } else {
+                ma = ((start_ma + end_ma) / 2);
+            }
+
+            return Object.assign(lat, lng, ma); // TODO
+        });
+    }
+}, {pluginsProvided: ['PaleoMap']});
 
