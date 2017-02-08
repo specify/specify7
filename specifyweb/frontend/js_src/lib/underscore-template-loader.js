93218ad40f3d29d5bb4b8a9f5f27b5e41cf2fb6d
Ben Anhalt <anhalt@ku.edu>
2017-02-08 17:14:09 -0600

    get webpack to watch template files.

--- specifyweb/frontend/js_src/lib/underscore-template-loader.js
+++ specifyweb/frontend/js_src/lib/underscore-template-loader.js
@@ -3,6 +3,7 @@ var utils = require('loader-utils');
 
 module.exports = function(source) {
     this.cacheable && this.cacheable();
+    this.addDependency(this.resourcePath);
     var options = utils.parseQuery(this.query);
     var t = _.template(source, options);
     return "var _ = require('underscore'); module.exports = " + t.source + ';';
