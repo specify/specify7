f998bf414d9fc066806f805e96fe1896e3545811
timo11 <timo@ku.edu>
2020-01-24 14:45:16 -0600

    nonnull vals logged for deletes. #594

--- specifyweb/specify/auditlog.py
+++ specifyweb/specify/auditlog.py
@@ -48,15 +48,35 @@ class AuditLog(object):
         log_obj = self._log(action, obj, agent, parent_record)
         if log_obj is not None:
             for vals in dirty_flds:
-                self._log_fld_update(vals, obj, log_obj, agent)
+                self._log_fld_update(vals, log_obj, agent)
         return log_obj
         
     def insert(self, obj, agent, parent_record=None):
         return self._log(auditcodes.INSERT, obj, agent, parent_record)
 
     def remove(self, obj, agent, parent_record=None):
-        return self._log(auditcodes.REMOVE, obj, agent, parent_record)
-
+        log_obj = self._log(auditcodes.REMOVE, obj, agent, parent_record)
+        if log_obj is not None:
+            for spfld in obj.specify_model.fields:
+                fldattr = spfld.name.lower()
+                if fldattr != 'version' and hasattr(obj, fldattr):
+                    val = getattr(obj, fldattr)
+                    if val is not None:
+                        self._log_fld_update({'field_name': fldattr, 'old_value': val, 'new_value': None}, log_obj, agent)
+            for spfld in obj.specify_model.relationships:
+                if spfld.type.lower().endswith("many-to-one"):
+                    fldattr = spfld.name.lower()
+                    if hasattr(obj, fldattr):
+                        val = getattr(obj, fldattr)
+                        field = obj._meta.get_field(fldattr);
+                        if isinstance(val, field.related_model):
+                            self._log_fld_update({'field_name': fldattr, 'old_value': val.id, 'new_value': None}, log_obj, agent)
+                        elif isinstance(val, basestring) and not val.endswith('.None'):
+                            fk_model, fk_id = parse_uri(val)
+                            if fk_model == field.related_model.__name__.lower() and  fk_id is not None:
+                                self._log_fld_update({'field_name': fldattr, 'old_value': fk_id, 'new_value': None}, log_obj, agent)
+        return log_obj
+        
     def _log(self, action, obj, agent, parent_record):
         if self.isAuditing():
             logger.info("inserting into auditlog: %s", [action, obj, agent, parent_record])
@@ -80,7 +100,7 @@ class AuditLog(object):
                 createdbyagent=agent,
                 modifiedbyagent=agent)
     
-    def _log_fld_update(self, vals, obj, log, agent):
+    def _log_fld_update(self, vals, log, agent):
         return Spauditlogfield.objects.create(
             fieldname=vals['field_name'],
             newvalue=vals['new_value'],
