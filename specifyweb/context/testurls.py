5628752579661500f27cabb009e35eafe9f26b28
Ben Anhalt <anhalt@ku.edu>
2020-02-27 13:41:54 -0600

    fix tests.

--- specifyweb/context/testurls.py
+++ specifyweb/context/testurls.py
@@ -1,11 +1,12 @@
-from django.conf.urls import patterns
+from django.conf.urls import url
 
-urlpatterns = patterns(
-    'specifyweb.context.testsviews',
-    (r'^collection/$', 'collection'),
-    (r'^domain.json$', 'domain'),
-    (r'^viewsets/(?P<level>\d+).xml$', 'viewsets'),
-    (r'^schema_localization.json$', 'schema_localization'),
-    (r'^app.resource$', 'app_resource'),
-    (r'^available_related_searches.json$', 'available_related_searches'),
-)
+from . import testsviews as views
+
+urlpatterns = [
+    url(r'^collection/$', views.collection),
+    url(r'^domain.json$', views.domain),
+    url(r'^viewsets/(?P<level>\d+).xml$', views.viewsets),
+    url(r'^schema_localization.json$', views.schema_localization),
+    url(r'^app.resource$', views.app_resource),
+    url(r'^available_related_searches.json$', views.available_related_searches),
+]
