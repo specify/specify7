1ccafe14b7f7cf25a9b0220ae21a7801eaff6117
Ben Anhalt <ben.anhalt@gmail.com>
2020-02-26 20:49:59 -0600

    figuring out test errors.

--- specifyweb/specify/build_models.py
+++ specifyweb/specify/build_models.py
@@ -99,7 +99,7 @@ def make_relationship(modelname, rel, datamodel):
     # I think maybe it is a superclass thing and not really a table?
     # Ignore it for now.
     if relatedmodel == 'Usergroupscope':
-        return None
+        return models.IntegerField(db_column=rel.column, null=True)
 
     if rel.type == 'one-to-many':
         return None # only define the "to" side of the relationship
