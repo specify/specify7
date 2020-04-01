6a171356e715adc8b7a0a030a0cd1a968b4d1223
Ben Anhalt <ben.anhalt@gmail.com>
2020-04-01 15:11:50 -0500

    typing.

--- specifyweb/specify/uiformatters.py
+++ specifyweb/specify/uiformatters.py
@@ -1,5 +1,5 @@
 import re, logging
-from collections import namedtuple
+from typing import NamedTuple, List, Optional, Sequence
 from xml.etree import ElementTree
 from xml.sax.saxutils import quoteattr
 from django.db import connection
@@ -11,31 +11,16 @@ from specifyweb.context.app_resource import get_app_resource
 
 from .filter_by_col import filter_by_collection
 
-def get_uiformatter(collection, user, formatter_name):
-    xml, __ = get_app_resource(collection, user, "UIFormatters")
-    node = ElementTree.XML(xml).find('.//format[@name=%s]' % quoteattr(formatter_name))
-    if node is None: return None
-    external = node.find('external')
-    if external is not None:
-        name = external.text.split('.')[-1]
-        if name == 'CatalogNumberUIFieldFormatter':
-            return UIFormatter('CollectionObject', 'CatalogNumber', [CNNField()], formatter_name)
-        else:
-            return None
-    else:
-        return UIFormatter(
-            model_name = node.attrib['class'].split('.')[-1],
-            field_name = node.attrib['fieldname'],
-            fields = list(map(new_field, node.findall('field'))),
-            format_name = formatter_name,
-        )
-
 class AutonumberOverflowException(Exception):
     pass
 
-ScopeInfo = namedtuple('ScopeInfo', "db_id_field id scope django_id_field")
+class ScopeInfo(NamedTuple):
+    db_id_field: str
+    id: int
+    scope: str
+    django_id_field: str
 
-def get_autonumber_group_filter(model, collection, format_name):
+def get_autonumber_group_filter(model, collection, format_name: str):
     default = lambda objs: filter_by_collection(objs, collection)
 
     scope_info = (
@@ -74,19 +59,23 @@ def get_autonumber_group_filter(model, collection, format_name):
         return default
 
 
-class UIFormatter(namedtuple('UIFormatter', "model_name field_name fields format_name")):
+class UIFormatter(NamedTuple):
+    model_name: str
+    field_name: str
+    fields: List
+    format_name: str
 
-    def parse_regexp(self):
+    def parse_regexp(self) -> str:
         regexp = ''.join('(%s)' % f.wild_or_value_regexp() for f in self.fields)
         return '^%s$' % regexp
 
-    def parse(self, value):
+    def parse(self, value: str) -> Sequence[str]:
         match = re.match(self.parse_regexp(), value)
         if match is None:
             raise ValueError("value doesn't match formatter")
         return match.groups()
 
-    def needs_autonumber(self, vals):
+    def needs_autonumber(self, vals: Sequence[str]) -> bool:
         for f, v in zip(self.fields, vals):
             if f.is_wild(v):
                 logger.debug("found wild field: %s with value: %s", f, v)
@@ -94,13 +83,13 @@ class UIFormatter(namedtuple('UIFormatter', "model_name field_name fields format
         logger.debug("no wild fields found")
         return False
 
-    def autonumber_regexp(self, vals):
+    def autonumber_regexp(self, vals: Sequence[str]) -> str:
         return '^{}$'.format(''.join(
             '({})'.format(field.value_regexp() if field.is_wild(val) else re.escape(val))
             for field, val in zip(self.fields, vals)
         ))
 
-    def fillin_year(self, vals, year=None):
+    def fillin_year(self, vals: Sequence[str], year: Optional[int]=None) -> List[str]:
         if year is None:
             year = date.today().year
 
@@ -109,7 +98,7 @@ class UIFormatter(namedtuple('UIFormatter', "model_name field_name fields format
             for field, val in zip(self.fields, vals)
         ]
 
-    def prepare_autonumber_thunk(self, collection, model, vals, year=None):
+    def prepare_autonumber_thunk(self, collection, model, vals: Sequence[str], year: Optional[int]=None):
         with_year = self.fillin_year(vals, year)
         fieldname = self.field_name.lower()
 
@@ -136,9 +125,9 @@ class UIFormatter(namedtuple('UIFormatter', "model_name field_name fields format
 
         return apply_autonumbering_to
 
-    def fill_vals_after(self, prior):
+    def fill_vals_after(self, prior: str) -> List[str]:
 
-        def inc_val(size, val):
+        def inc_val(size: int, val: str) -> str:
             format_code = "%0{}d".format(size)
             new_val = format_code % (1 + int(val))
             if len(new_val) > size:
@@ -152,16 +141,112 @@ class UIFormatter(namedtuple('UIFormatter', "model_name field_name fields format
             for field, val in zip(self.fields, self.parse(prior))
         ]
 
-    def fill_vals_no_prior(self, vals):
+    def fill_vals_no_prior(self, vals: Sequence[str]) -> List[str]:
         return [
             "1".zfill(field.size) if field.is_wild(val) else val
             for field, val in zip(self.fields, vals)
         ]
 
-    def canonicalize(self, values):
+    def canonicalize(self, values: Sequence[str]) -> str:
         return ''.join([field.canonicalize(value) for field, value in zip(self.fields, values)])
 
-def new_field(node):
+class Field(NamedTuple):
+    size: int
+    value: str
+    inc: bool
+    by_year: bool
+
+    def can_autonumber(self) -> bool:
+        return self.inc or self.by_year
+
+    def wild_regexp(self) -> str:
+        return re.escape(self.value)
+
+    def value_regexp(self) -> str:
+        return self.wild_regexp()
+
+    def is_wild(self, value: str) -> bool:
+        logger.debug("%s checking if value %s is wild", self, value)
+        return bool(re.match("^%s$" % self.wild_regexp(), value) and not
+                    re.match("^%s$" % self.value_regexp(), value))
+
+    def wild_or_value_regexp(self) -> str:
+        if self.can_autonumber():
+            return '%s|%s' % (self.wild_regexp(), self.value_regexp())
+        else:
+            return self.value_regexp()
+
+    def canonicalize(self, value: str) -> str:
+        return value
+
+class NumericField(Field):
+    def __new__(cls, size, value=None, inc=False, by_year=False):
+        value = size * '#'
+        return Field.__new__(cls, size, value, inc, by_year)
+
+    def value_regexp(self) -> str:
+        return r'[0-9]{%d}' % self.size
+
+class YearField(Field):
+    def value_regexp(self) -> str:
+        return r'[0-9]{%d}' % self.size
+
+class AlphaNumField(Field):
+    def value_regexp(self) -> str:
+        return r'[a-zA-Z0-9]{%d}' % self.size
+
+class AnyCharField(Field):
+    def value_regexp(self) -> str:
+        return r'.{%d}' % self.size
+
+class AlphaField(Field):
+    def value_regexp(self) -> str:
+        return r'[a-zA-Z]{%d}' % self.size
+
+class ConstantField(Field):
+    def is_wild(self, value: str) -> bool:
+        return False
+
+    def value_regexp(self) -> str:
+        return self.wild_regexp()
+
+class SeparatorField(Field):
+    def is_wild(self, value: str) -> bool:
+        return False
+
+    def value_regexp(self) -> str:
+        return self.wild_regexp()
+
+class CNNField(NumericField):
+    def __new__(cls):
+        return NumericField.__new__(cls, size=9, inc=9)
+
+    def value_regexp(self) -> str:
+        return r'[0-9]{0,%d}' % self.size
+
+    def canonicalize(self, value: str) -> str:
+        return value.zfill(self.size)
+
+def get_uiformatter(collection, user, formatter_name: str) -> Optional[UIFormatter]:
+    xml, __ = get_app_resource(collection, user, "UIFormatters")
+    node = ElementTree.XML(xml).find('.//format[@name=%s]' % quoteattr(formatter_name))
+    if node is None: return None
+    external = node.find('external')
+    if external is not None and external.text is not None:
+        name = external.text.split('.')[-1]
+        if name == 'CatalogNumberUIFieldFormatter':
+            return UIFormatter('CollectionObject', 'CatalogNumber', [CNNField()], formatter_name)
+        else:
+            return None
+    else:
+        return UIFormatter(
+            model_name = node.attrib['class'].split('.')[-1],
+            field_name = node.attrib['fieldname'],
+            fields = list(map(new_field, node.findall('field'))),
+            format_name = formatter_name,
+        )
+
+def new_field(node) -> Field:
     Field = {
         'constant': ConstantField,
         'alpha': AlphaField,
@@ -176,73 +261,3 @@ def new_field(node):
         value = node.attrib.get('value'),
         inc = node.attrib.get('inc') == 'true',
         by_year = node.attrib.get('byyear') == 'true')
-
-
-class Field(namedtuple("Field", "size value inc by_year")):
-    def can_autonumber(self):
-        return self.inc or self.by_year
-
-    def wild_regexp(self):
-        return re.escape(self.value)
-
-    def is_wild(self, value):
-        logger.debug("%s checking if value %s is wild", self, value)
-        return (re.match("^%s$" % self.wild_regexp(), value) and not
-                re.match("^%s$" % self.value_regexp(), value))
-
-    def wild_or_value_regexp(self):
-        if self.can_autonumber():
-            return '%s|%s' % (self.wild_regexp(), self.value_regexp())
-        else:
-            return self.value_regexp()
-
-    def canonicalize(self, value):
-        return value
-
-class NumericField(Field):
-    def __new__(cls, size, value=None, inc=False, by_year=False):
-        value = size * '#'
-        return Field.__new__(cls, size, value, inc, by_year)
-
-    def value_regexp(self):
-        return r'[0-9]{%d}' % self.size
-
-class YearField(Field):
-    def value_regexp(self):
-        return r'[0-9]{%d}' % self.size
-
-class AlphaNumField(Field):
-    def value_regexp(self):
-        return r'[a-zA-Z0-9]{%d}' % self.size
-
-class AnyCharField(Field):
-    def value_regexp(self):
-        return r'.{%d}' % self.size
-
-class AlphaField(Field):
-    def value_regexp(self):
-        return r'[a-zA-Z]{%d}' % self.size
-
-class ConstantField(Field):
-    def is_wild(self, value):
-        return False
-
-    def value_regexp(self):
-        return self.wild_regexp()
-
-class SeparatorField(Field):
-    def is_wild(self, value):
-        return False
-
-    def value_regexp(self):
-        return self.wild_regexp()
-
-class CNNField(NumericField):
-    def __new__(cls):
-        return NumericField.__new__(cls, size=9, inc=9)
-
-    def value_regexp(self):
-        return r'[0-9]{0,%d}' % self.size
-
-    def canonicalize(self, value):
-        return value.zfill(self.size)
