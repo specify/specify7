5ea58d990699373bcf02c183429e6c99c56938d1
Ben Anhalt <ben.anhalt@gmail.com>
2020-05-13 16:49:38 -0500

    settings.

--- default.nix
+++ default.nix
@@ -3,10 +3,10 @@ stdenv.mkDerivation rec {
   name = "env";
   env = buildEnv { name = name; paths = buildInputs; };
   buildInputs = [
-    python
-    python27Packages.virtualenv
-    python27Packages.pip
-    mariadb
+    python36
+    python36Packages.virtualenv
+    python36Packages.pip
+    libmysqlclient
     libzip
     openssl
     openldap
--- specifyweb/settings/__init__.py
+++ specifyweb/settings/__init__.py
@@ -56,21 +56,21 @@ DATABASES = {
 
 TESTING_DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-#        'ENGINE': 'hibernateboolsbackend.backends.mysql',
-        'NAME': "specifytest",
-        # 'USER': "Master",
-        # 'PASSWORD': "Master",
+        # 'ENGINE': 'django.db.backends.sqlite3',
+       'ENGINE': 'specifyweb.hibernateboolsbackend.backends.mysql',
+        'NAME': "specifytestnew",
+        'USER': "master",
+        'PASSWORD': "master",
         # 'HOST': '127.0.0.1',
         # 'PORT': '3307',
     }
 }
 
 if 'test' in sys.argv:
-    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
+    DATABASES = TESTING_DATABASES
+    # DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
     # DATABASES['default']['TEST']['NAME'] = 'specifytest.sqlite3'
     # TESTING = True
-    # DATABASES = TESTING_DATABASES
     SA_DATABASE_URL = 'sqlite:///:memory:'
 else:
     # TESTING = False
