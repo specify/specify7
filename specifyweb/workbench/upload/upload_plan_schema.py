96d10ccecaa7e5dc5da190393b9990de86f19ad0
Ben Anhalt <ben.anhalt@gmail.com>
2020-07-17 11:32:42 -0500

    add documentation to the upload plan schema.

--- specifyweb/workbench/upload/upload_plan_schema.py
+++ specifyweb/workbench/upload/upload_plan_schema.py
@@ -6,6 +6,8 @@ from .treerecord import TreeRecord
 from .data import Uploadable
 
 schema = {
+    'title': 'Specify 7 Workbench Upload Plan',
+    'description': 'The workbench upload plan defines how to load columnar data into the Specify datamodel.',
     "$schema": "http://json-schema.org/schema#",
 
     '$ref': '#/definitions/uploadable',
@@ -13,13 +15,37 @@ schema = {
     'definitions': {
         'uploadTable': {
             'type': 'object',
+            'description': 'The uploadTable structure defines how to upload data for a given table.',
             'properties': {
-                'name' : { 'type': 'string' },
-                'wbcols': { 'type': 'object', 'additionalProperties': { 'type': 'string' } },
-                'static': { 'type': 'object' },
-                'toOne': { 'type': 'object', 'additionalProperties': { '$ref': '#/definitions/uploadable' } },
+                'name' : {
+                    'type': 'string',
+                    'description': 'The name of the Specify table the data is to be loaded into.',
+                    'examples': ['Collectionobject', 'Collectingevent', 'Agent']
+                },
+                'wbcols': {
+                    'type': 'object',
+                    'description': 'Maps the columns of the destination table to the headers of the source columns of input data.',
+                    'additionalProperties': { 'type': 'string' },
+                    'examples': [
+                        {'catalognumber': 'Specimen #', 'catalogeddate': 'Recored Date', 'objectcondition': 'Condition'},
+                        {'lastname': 'Collector 1 Last Name', 'firstname': 'Collector 1 First Name'},
+                    ]
+                },
+                'static': {
+                    'type': 'object',
+                    'description': 'A set of static values that will be added to every record loaded.',
+                    'examples': [
+                        {'ispublic': True, 'license': 'CC BY-NC-ND 2.0'}
+                    ]
+                },
+                'toOne': {
+                    'type': 'object',
+                    'description': 'Maps the names of -to-one relationships of the table to upload definitions for each.',
+                    'additionalProperties': { '$ref': '#/definitions/uploadable' }
+                },
                 'toMany': {
                     'type': 'object',
+                    'desciption': 'Maps the names of -to-many relationships of the table to an array of upload definitions for each.',
                     'additionalProperties': { 'type': 'array', 'items': { '$ref': '#/definitions/toManyRecord' } }
                 }
             },
@@ -29,11 +55,36 @@ schema = {
 
         'toManyRecord': {
             'type': 'object',
+            'description': 'The toManyRecord structure defines how to upload data for one record into a given table that stands '
+            'in a many-to-one relationship to another.',
             'properties': {
-                'name' : { 'type': 'string' },
-                'wbcols': { 'type': 'object', 'additionalProperties': { 'type': 'string' } },
-                'static': { 'type': 'object' },
-                'toOne': { 'type': 'object', 'additionalProperties': { '$ref': '#/definitions/uploadable' } },
+                'name' : {
+                    'type': 'string',
+                    'description': 'The name of the Specify table the data is to be loaded into. '
+                    'This value must be the same for every element of a toMany array.',
+                    'examples': ['Collectionobject', 'Collectingevent', 'Agent']
+                },
+                'wbcols': {
+                    'type': 'object',
+                    'description': 'Maps the columns of the destination table to the headers of the source columns of input data.',
+                    'additionalProperties': { 'type': 'string' },
+                    'examples': [
+                        {'catalognumber': 'Specimen #', 'catalogeddate': 'Recored Date', 'objectcondition': 'Condition'},
+                        {'lastname': 'Collector 1 Last Name', 'firstname': 'Collector 1 First Name'},
+                    ]
+                },
+                'static': {
+                    'type': 'object',
+                    'description': 'A set of static values that will be added to every record loaded.',
+                    'examples': [
+                        {'ispublic': True, 'license': 'CC BY-NC-ND 2.0'}
+                    ]
+                },
+                'toOne': {
+                    'type': 'object',
+                    'description': 'Maps the names of -to-one relationships of the table to upload definitions for each.',
+                    'additionalProperties': { '$ref': '#/definitions/uploadable' }
+                },
             },
             'required': [ 'name', 'wbcols', 'static', 'toOne' ],
             'additionalProperties': False
@@ -41,11 +92,31 @@ schema = {
 
         'treeRecord': {
             'type': 'object',
+            'description': 'The treeRecord structure defines how to upload data into Specify tree type table.',
             'properties': {
-                'name' : { 'type': 'string' },
-                'treedefname': { 'type': 'string' },
-                'treedefid': { 'type': 'integer' },
-                'ranks': { 'type': 'object', 'additionalProperties': { 'type': 'string' } },
+                'name' : {
+                    'type': 'string',
+                    'description': 'The name of the Specify tree table the data is to be loaded into.',
+                    'examples': ['Taxon', 'Geography', 'Storage']
+                },
+                'treedefname': {
+                    'type': 'string',
+                    'description': 'The name of the Specify tree definition table for this tree.',
+                    'examples': ['Taxontreedef', 'Geographytreedef', 'Storagetreedef']
+                },
+                'treedefid': {
+                    'type': 'integer',
+                    'description': 'The row id of the row in the Specify tree definition table for the definition of this tree.'
+                },
+                'ranks': {
+                    'type': 'object',
+                    'description': 'Maps the ranks of the tree to the headers of the source columns of input data.',
+                    'additionalProperties': { 'type': 'string' },
+                    'examples': [
+                        {"Continent/Ocean": "Continent", "Country": "Country", "State/Prov/Pref": "State", "Region": "County"},
+                        {"Class": "Class", "Superfamily": "Superfamily", "Family": "Family", "Genus": "Genus", "Subgenus": "Subgenus", "Species": "Species", "Subspecies": "Subspecies"}
+                    ]
+                },
             },
             'required': [ 'name', 'treedefname', 'treedefid', 'ranks' ],
             'additionalProperties': False
@@ -53,6 +124,9 @@ schema = {
 
         'uploadable': {
             'type': 'object',
+            'description': 'The uploadable structure differentiates among types of uploadable data structures which can be either '
+            'the base structure for uploading data or stand in a -to-one relationship to another uploadable structure. Currently only '
+            'uploadTable or treeRecord.',
             'patternProperties': {
                 '^uploadTable$': { '$ref': '#/definitions/uploadTable' },
                 '^treeRecord$': { '$ref': '#/definitions/treeRecord' },
