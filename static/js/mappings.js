65f280578d218ded845c1175f313094e2c893190
Maksym Patiiuk <40512816+maxxxxxdlp@users.noreply.github.com>
2020-07-24 17:45:40 +0300

    Adds basic data model view

--- static/js/mappings.js
+++ static/js/mappings.js
@@ -3,7 +3,10 @@ const mappings = {
 
 		const global = this;
 
-		global.column__data_model = document.getElementById('column__data_model');
+		global.list__data_model = document.getElementById('list__data_model');
+
+		global.button__map = document.getElementById('button__map');
+		global.button__delete = document.getElementById('button__delete');
 
 		global.list__headers = document.getElementById('list__headers');
 		global.button__new_field = document.getElementById('button__new_field');
@@ -20,62 +23,139 @@ const mappings = {
 
 			data_model.forEach(function (table_data) {
 
-				let table_name = table_data['classname'].split('.').pop()
-				const friendly_table_name = global.get_friendly_table_name(table_name)
+				let table_name = table_data['classname'].split('.').pop();
+				const friendly_table_name = global.get_friendly_name(table_name);
 				table_name = table_name.toLowerCase();
 
 				let fields = {};
 
 				if (table_data['system']) {
-					system_tables.push(table_name)
+					system_tables.push(table_name);
 					return true;
 				}
 
-				let table = {
+				table_data['fields'].forEach(function (field) {
+
+					let field_name = field['column'];
+					const friendly_field_name = global.get_friendly_name(field_name)
+					field_name = field_name.toLowerCase()
+
+					fields[field_name] = friendly_field_name;
+
+				});
+
+				tables[table_name] = {
 					friendly_table_name: friendly_table_name,
 					fields: fields,
 				};
 
-				tables[table_name] = table;
-
-				data_model_html += table_name;
+				data_model_html += '<label>' +
+					'	<input type="radio" name="table" class="radio__table" data-level="0" data-table="' + table_name + '">' +
+					'	<div tabindex="0" class="line">' +
+					'		<div class="mapping">' + friendly_table_name + '</div>' +
+					'	</div>' +
+					'</label>';
 
 			});
 
-			global.data_model = data_model_html;
+			global.list__data_model.innerHTML = data_model_html;
+
+			global.radios__table = document.getElementsByClassName('radio__table');
+
+			Object.values(global.radios__table).forEach(function (line) {
+				line.addEventListener('change', global.table_line_change);
+			});
+
+
 			global.tables = tables;
 			global.data_model = data_model;//TODO: remove this
-		}
-		xhr.send();
 
+			global.update_headers();
+
+		};
+		xhr.send();
 
 	},
 
+	header_line_change: function (event) {
+
+		const global = mappings;
+
+		const radio = event.target;
+		const label = radio.nextElementSibling;
+
+		global.button__delete.disabled = label.tagName!=='label' || label.getElementsByClassName('undefined').length===0;
+
+		mappings.selected_header = radio;//.getAttribute('data-header');
+
+	},
+
+	table_line_change: function (event) {
+
+		const global = mappings;
+		const radio = event.target;
+		const label = radio.parentElement;
+		const table_name = radio.getAttribute('data-table');
+		const level = parseInt(radio.getAttribute('data-level'))+1;
+		const fields = global.list__data_model.getElementsByClassName('field');
+
+		if(fields.length>0)
+			Object.values(fields).forEach(function(field){//remove open fields
+				global.list__data_model.removeChild(field);
+			});
+
+		let fields_html = '';
+
+		for (const [field_name, friendly_field_name] of Object.entries(global.tables[table_name]['fields']))
+			fields_html += '<label class="field">' +
+				'	<input type="radio" name="field" class="radio__field" data-level="'+level+'" data-field="' + field_name + '">' +
+				'	<div tabindex="0" class="line">' +
+				'		<div class="mapping" style="padding-left: '+(level*10)+'px">' + friendly_field_name + '</div>' +
+				'	</div>' +
+				'</label>';
+
+		label.outerHTML += fields_html;
+
+		global.button__map.disabled = level===0;
+
+		mappings.selected_table = radio;
+
+	},
 
 	update_headers: function (headers = []) {
 
 		const global = this;
 
+		if (typeof global.line__headers !== "undefined")//fix memory leaks in old browsers
+			Object.values(global.line__headers).forEach(function (line) {
+				line.removeEventListener('change', global.header_line_change);
+			});
+
+
 		let headers_html = '';
 
 		headers.forEach(function (header) {
 			headers_html += '<label>' +
-				'<input type="radio" name="header">' +
-				'<div tabindex="0" class="line">' +
-					'<div class="undefined mapping"></div>' +
-					'<div class="header">' + header + '</div>' +
-				'</div>' +
-			'</label>';
+				'	<input type="radio" name="header" class="radio__header" data-header="' + header + '">' +
+				'	<div tabindex="0" class="line">' +
+				'		<div class="undefined mapping"></div>' +
+				'		<div class="header">' + header + '</div>' +
+				'	</div>' +
+				'</label>';
 		});
 
 		global.list__headers.innerHTML = headers_html;
 
-		global.line__headers = document.getElementsByClassName('line');
+		global.line__headers = document.getElementsByClassName('radio__header');
+
+		Object.values(global.line__headers).forEach(function (line) {
+			line.addEventListener('change', global.header_line_change);
+		});
 
 	},
 
 
-	get_friendly_table_name: table_name => table_name.replace(/[A-Z]/g, letter => ` ${letter}`).trim(),
+	get_friendly_name: table_name => table_name.replace(/[A-Z]/g, letter => ` ${letter}`).replace('D N A', 'DNA').trim(),
 
 
 	render_table: function (table_name, table_data) {
