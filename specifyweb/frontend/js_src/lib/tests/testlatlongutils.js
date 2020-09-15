5b2d03f59f56f418247c461374589308ff0c584a
Ben Anhalt <ben.anhalt@gmail.com>
2020-09-15 16:53:03 -0500

    handle lat long sign separately. fixes #730

--- specifyweb/frontend/js_src/lib/tests/testlatlongutils.js
+++ specifyweb/frontend/js_src/lib/tests/testlatlongutils.js
@@ -10,22 +10,26 @@ module.exports =  function() {
     var parse = latlongutils.parse;
 
     _.each({
-        '34.123 N': [34.123, latlongutils.Lat],
-        '36:07 N': [36, 7, latlongutils.Lat],
-        '39:51:41 N': [39, 51, 41, latlongutils.Lat],
-        '00.07152778 N': [0.07152778, latlongutils.Lat],
-        '17:22.88 N': [17, 22.88, latlongutils.Lat],
-        '39:51:41.02 N': [39, 51, 41.02, latlongutils.Lat],
-        '-39:51:41': [-39, 51, 41, latlongutils.Coord],
-        '39:51:41 s': [-39, 51, 41, latlongutils.Lat],
-        '39:51.41 w': [-39, 51.41, latlongutils.Long],
-        '.34': [0.34, latlongutils.Coord],
-        '-.34': [-0.34, latlongutils.Coord],
-        '17:22.88 E': [17, 22.88, latlongutils.Long],
-        '28° N': [28, latlongutils.Lat],
-        '28° 19\' N': [28, 19, latlongutils.Lat],
-        '28° 19\' 0.121" N': [28, 19, 0.121, latlongutils.Lat],
-        '115° 34\' 59.872" W': [-115, 34, 59.872, latlongutils.Long],
+        '34.123 N': [1, 34.123, latlongutils.Lat],
+        '36:07 N': [1, 36, 7, latlongutils.Lat],
+        '39:51:41 N': [1, 39, 51, 41, latlongutils.Lat],
+        '00.07152778 N': [1, 0.07152778, latlongutils.Lat],
+        '17:22.88 N': [1, 17, 22.88, latlongutils.Lat],
+        '39:51:41.02 N': [1, 39, 51, 41.02, latlongutils.Lat],
+        '-39:51:41': [-1, 39, 51, 41, latlongutils.Coord],
+        '39:51:41 s': [-1, 39, 51, 41, latlongutils.Lat],
+        '39:51.41 w': [-1, 39, 51.41, latlongutils.Long],
+        '.34': [1, 0.34, latlongutils.Coord],
+        '-.34': [-1, 0.34, latlongutils.Coord],
+        '17:22.88 E': [1, 17, 22.88, latlongutils.Long],
+        '28° N': [1, 28, latlongutils.Lat],
+        '28° 19\' N': [1, 28, 19, latlongutils.Lat],
+        '28° 19\' 0.121" N': [1, 28, 19, 0.121, latlongutils.Lat],
+        '115° 34\' 59.872" W': [-1, 115, 34, 59.872, latlongutils.Long],
+        '1 01 S': [-1, 1, 1, latlongutils.Lat],
+        '1 01 W': [-1, 1, 1, latlongutils.Long],
+        '0 01 S': [-1, 0, 1, latlongutils.Lat],
+        '0 01 W': [-1, 0, 1, latlongutils.Long],
         '': null,
         ' ': null,
         'foobar': null,
@@ -39,37 +43,40 @@ module.exports =  function() {
                 assert.equal(result, null);
                 return;
             }
+            assert.notEqual(result, null);
             assert.ok(result instanceof type);
-            assert.deepEqual(result._components, value);
+            assert.deepEqual([result._sign].concat(result._components), value);
         });
     });
 
     QUnit.module('latlongutils.toDegs');
 
     _.each({
-        '28° 19\' 0.121" N': [28.3167002778],
-        '115° 34\' 59.872" W': [-115.5832977778],
+        '28° 19\' 0.121" N': [1, 28.3167002778],
+        '115° 34\' 59.872" W': [-1, 115.5832977778],
     }, function(value, key) {
         QUnit.test(key, function() {
-            var result = latlongutils.parse(key).toDegs()._components;
-            assert.equal(result.length, value.length);
-            assert.equal(Math.round(result.pop() * 1e9), Math.round(value.pop() * 1e9));
+            var result = latlongutils.parse(key).toDegs();
+            assert.equal(result._components.length, value.length - 1);
+            assert.equal(result._sign, value[0]);
+            assert.equal(Math.round(result._components.pop() * 1e9), Math.round(value.pop() * 1e9));
         });
     });
 
     QUnit.module('latlongutils.toDegsMinsSecs');
 
     _.each({
-        '28.3167002778': [28, 19, 0.121],
-        '-115.5832977778': [-115, 34, 59.872],
-        '28': [28, 0, 0],
-        '-115.5': [-115, 30, 0],
-        '-115.51': [-115, 30, 36],
+        '28.3167002778': [1, 28, 19, 0.121],
+        '-115.5832977778': [-1, 115, 34, 59.872],
+        '28': [1, 28, 0, 0],
+        '-115.5': [-1, 115, 30, 0],
+        '-115.51': [-1, 115, 30, 36],
     }, function(value, key) {
         QUnit.test(key, function() {
             var coord = new latlongutils.Coord(parseFloat(key));
-            var result = coord.toDegsMinsSecs()._components;
-            assert.equal(result.length, value.length);
+            var result = coord.toDegsMinsSecs();
+            assert.equal(result._components.length, value.length - 1);
+            assert.equal(result._sign, value[0]);
             while(result.length) {
                 assert.equal(Math.round(result.pop() * 1e3), Math.round(value.pop() * 1e3));
             }
@@ -79,13 +86,14 @@ module.exports =  function() {
     QUnit.module('latlongutils.toDegsMins');
 
     _.each({
-        '28.5': [28, 30],
-        '-115.25': [-115, 15],
+        '28.5': [1, 28, 30],
+        '-115.25': [-1, 115, 15],
     }, function(value, key) {
         QUnit.test(key, function() {
             var coord = new latlongutils.Coord(parseFloat(key));
-            var result = coord.toDegsMins()._components;
-            assert.equal(result.length, value.length);
+            var result = coord.toDegsMins();
+            assert.equal(result._components.length, value.length - 1);
+            assert.equal(result._sign, value[0]);
             while(result.length) {
                 assert.equal(Math.round(result.pop() * 1e3), Math.round(value.pop() * 1e3));
             }
@@ -95,13 +103,16 @@ module.exports =  function() {
     QUnit.module('latlongutils.format');
 
     _.each({
-        '28° 19\' 0.121"': [28, 19, 0.121],
-        '-115° 34\' 59.872"': [-115, 34, 59.872],
-        '28° 19\'': [28, 19],
-        '-115° 34.44\'': [-115, 34.44],
+        '28° 19\' 0.121"': [1, 28, 19, 0.121],
+        '-115° 34\' 59.872"': [-1, 115, 34, 59.872],
+        '28° 19\'': [1, 28, 19],
+        '-115° 34.44\'': [-1, 115, 34.44],
+        '-1° 1\'': [-1, 1, 1],
+        '-0° 1\'': [-1, 0, 1],
     }, function(value, key) {
         QUnit.test(key, function() {
             var coord = new latlongutils.Coord();
+            coord._sign = value.shift();
             coord._components = value;
             assert.equal(coord.format(), key);
         });
@@ -110,10 +121,13 @@ module.exports =  function() {
     _.each({
         '28° 30\' 36" N': new latlongutils.Lat(28.51),
         '115° 30\' 36" W': new latlongutils.Long(-115.51),
+        '0° 30\' 36" W': new latlongutils.Long(-0.51),
         '28° 30\' 36" S': new latlongutils.Lat(-28.51),
+        '0° 30\' 36" S': new latlongutils.Lat(-0.51),
         '115° 30\' 36" E': new latlongutils.Long(115.51),
         '28° 30\' 36"': new latlongutils.Coord(28.51),
         '-115° 30\' 36"': new latlongutils.Coord(-115.51),
+        '-0° 30\' 36"': new latlongutils.Coord(-0.51),
     }, function(value, key) {
         QUnit.test(key, function() {
             assert.equal(value.toDegsMinsSecs().format(), key);
@@ -122,22 +136,22 @@ module.exports =  function() {
 
     QUnit.module('latlongutil.*.parse');
     _.each({
-        '34.123 N': [34.123, 'Lat'],
-        '36:07 N': [36, 7, 'Lat'],
-        '39:51:41 N': [39, 51, 41, 'Lat'],
-        '00.07152778 N': [0.07152778, 'Lat'],
-        '17:22.88 N': [17, 22.88, 'Lat'],
-        '39:51:41.02 N': [39, 51, 41.02, 'Lat'],
-        '-39:51:41': [-39, 51, 41, 'Coord'],
-        '39:51:41 s': [-39, 51, 41, 'Lat'],
-        '39:51.41 w': [-39, 51.41, 'Long'],
-        '.34': [0.34, 'Coord'],
-        '-.34': [-0.34, 'Coord'],
-        '17:22.88 E': [17, 22.88, 'Long'],
-        '28° N': [28, 'Lat'],
-        '28° 19\' N': [28, 19, 'Lat'],
-        '28° 19\' 0.121" N': [28, 19, 0.121, 'Lat'],
-        '115° 34\' 59.872" W': [-115, 34, 59.872, 'Long'],
+        '34.123 N': [1, 34.123, 'Lat'],
+        '36:07 N': [1, 36, 7, 'Lat'],
+        '39:51:41 N': [1, 39, 51, 41, 'Lat'],
+        '00.07152778 N': [1, 0.07152778, 'Lat'],
+        '17:22.88 N': [1, 17, 22.88, 'Lat'],
+        '39:51:41.02 N': [1, 39, 51, 41.02, 'Lat'],
+        '-39:51:41': [-1, 39, 51, 41, 'Coord'],
+        '39:51:41 s': [-1, 39, 51, 41, 'Lat'],
+        '39:51.41 w': [-1, 39, 51.41, 'Long'],
+        '.34': [1, 0.34, 'Coord'],
+        '-.34': [-1, 0.34, 'Coord'],
+        '17:22.88 E': [1, 17, 22.88, 'Long'],
+        '28° N': [1, 28, 'Lat'],
+        '28° 19\' N': [1, 28, 19, 'Lat'],
+        '28° 19\' 0.121" N': [1, 28, 19, 0.121, 'Lat'],
+        '115° 34\' 59.872" W': [-1, 115, 34, 59.872, 'Long'],
         '': null,
         ' ': null,
         'foobar': null,
@@ -151,7 +165,7 @@ module.exports =  function() {
                 } else if (type === 'Coord' ||
                            giventype === 'Coord' ||
                            giventype === type) {
-                    assert.deepEqual(result._components, value);
+                    assert.deepEqual([result._sign].concat(result._components), value);
                 } else {
                     assert.equal(result, null);
                 }
