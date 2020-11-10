a90d851a934b3598438281336ce808bbf00b6433
Ben Anhalt <ben.anhalt@gmail.com>
2020-11-10 15:45:48 -0600

    clean up validation results UI code.

--- specifyweb/frontend/js_src/lib/wbview.js
+++ specifyweb/frontend/js_src/lib/wbview.js
@@ -29,7 +29,6 @@ const WBView = Backbone.View.extend({
         'click .wb-delete': 'delete',
         'click .wb-save': 'saveClicked',
         'click .wb-export': 'export',
-        'click .wb-next-error, .wb-prev-error': 'gotoError',
         'click .wb-toggle-highlights': 'toggleHighlights',
         'click .wb-cell_navigation': 'navigateCells',
         'click .wb-search-button': 'searchCells',
@@ -41,9 +40,9 @@ const WBView = Backbone.View.extend({
         this.data = data;
         this.uploadStatus = uploadStatus;
         this.highlightsOn = false;
-        this.column_indexes = [];
-        this.cells_to_highlight = {};
-        this.validation_results_object = [];
+        this.cellInfo = [];
+        this.rowValidationRequests = {};
+        this.search_query = null;
     },
     render() {
         const mappingsPromise = Q(this.wb.rget('workbenchtemplate.workbenchtemplatemappingitems'))
@@ -64,24 +63,13 @@ const WBView = Backbone.View.extend({
         if (this.data.length < 1)
             this.data.push(Array(columns.length + 1).fill(null));
 
-        columns = columns.map(columns=>{
-            columns.validator = this.validateCell;
-            return columns;
-        });
-
-        let column_indexes = colHeaders;
-
-        this.column_indexes = column_indexes;
-
         //initialize Handsontable
         const onChanged = this.spreadSheetChanged.bind(this);
-        const renderer = this.renderCell.bind(this);
 
         this.hot = new Handsontable(this.$('.wb-spreadsheet')[0], {
             height: this.calcHeight(),
             data: this.data,
-            cells: () => ({renderer: renderer}),
-            cell: this.validation_results_array,
+            cells: this.defineCell.bind(this, columns.length),
             colHeaders: colHeaders,
             columns: columns,
             minSpareRows: 0,
@@ -113,114 +101,87 @@ const WBView = Backbone.View.extend({
                   .then(() => rows.map(row => [row.id, row.get('biogeomancerresults')])))
             .done(results => this.parseUploadResults(results));
     },
+    initCellInfo(row, col) {
+        const cols = this.hot.countCols();
+        if(typeof this.cellInfo[row*cols + col] === "undefined") {
+            this.cellInfo[row*cols + col] = {isNew: false, issues: [], matchesSearch: false};
+        }
+    },
     parseUploadResults(uploadResults) {
-        //parse validation results
+        const cols = this.hot.countCols();
+        const headerToCol = {};
+        for (let i = 0; i < cols; i++) {
+            headerToCol[this.hot.getColHeader(i)] = i;
+        }
 
-        const inverted_column_indexes = Object.entries(this.column_indexes).reduce((inverted_column_indexes,[column_index, column_name])=>{
-            inverted_column_indexes[column_name] = parseInt(column_index);
-            return inverted_column_indexes;
-        },{});
+        this.cellInfo = [];
+        uploadResults.forEach(([rowId, resultJSON], row) => {
+            let result;
 
-        this.validation_results_object = uploadResults.reduce((validation_results, row_data, row_key)=>{
-
-            let validation_results_raw = null;
             try {
-                validation_results_raw = JSON.parse(row_data[1]);
-            } catch (exception) {
-                if(!(exception instanceof SyntaxError))//only catch JSON parse errors
-                    throw exception;
-                console.error('Failed to parse validation message (' + (typeof row_data[1]) + '): '+ row_data[1]);
+                result = JSON.parse(resultJSON);
+            } catch (e) {
+                return;
             }
 
-            if(validation_results_raw===null)
-                return validation_results;
-
-            if(typeof validation_results[row_key] === "undefined")
-                validation_results[row_key] = {};
-
-            function add_error_message(column_name, issue){
-
-                const column_index = inverted_column_indexes[column_name];
-
-                if(typeof validation_results[row_key] === "undefined")
-                    validation_results[row_key] = [];
-
-                if(typeof validation_results[row_key][column_index] === "undefined")
-                    validation_results[row_key][column_index] = [];
+            const add_error_message = (column_name, issue) => {
+                const col = headerToCol[column_name];
+                this.initCellInfo(row, col);
+                const cellInfo = this.cellInfo[row*cols + col];
 
                 const ucfirst_issue = issue[0].toUpperCase() + issue.slice(1);
-                validation_results[row_key][column_index].push(ucfirst_issue);
-            }
+                cellInfo.issues.push(ucfirst_issue);
+            };
 
-            for(const table_issue of validation_results_raw['tableIssues'])
-                for(const column_name of table_issue['columns'])
-                    add_error_message(column_name,table_issue['issue']);
+            result.tableIssues.forEach(table_issue => table_issue.columns.forEach(column_name => {
+                add_error_message(column_name, table_issue.issue);
+            }));
 
-            for(const cell_issue of validation_results_raw['cellIssues'])
-                add_error_message(cell_issue['column'],cell_issue['issue']);
+            result.cellIssues.forEach(cell_issue => {
+                add_error_message(cell_issue.column, cell_issue.issue);
+            });
 
-            for(const table_issue of validation_results_raw['newRows'])
-                for(const column_name of table_issue['columns'])
-                    if(typeof validation_results[row_key][column_name] === "undefined")
-                        validation_results[row_key][column_name] = true;
+            result.newRows.forEach(table => table.columns.forEach(column_name => {
+                const col = headerToCol[column_name];
+                this.initCellInfo(row, col);
+                const cellInfo = this.cellInfo[row*cols + col];
+                cellInfo.isNew = true;
+            }));
+        });
 
-            return validation_results;
-
-        },[]);
-
-        //attach error messages to cells
-        this.validation_results_array = [];
-        let navigation_cells_count = {
-            'new_cells': 0,
-            'invalid_cells': 0,
+        const cellCounts = {
+            new_cells: this.cellInfo.reduce((count, info) => count + (info.isNew ? 1 : 0), 0),
+            invalid_cells: this.cellInfo.reduce((count, info) => count + (info.issues.length ? 1 : 0), 0),
+            search_results: this.cellInfo.reduce((count, info) => count + (info.matchesSearch ? 1 : 0), 0),
         };
-        for(const [row_key,row_data] of Object.entries(this.validation_results_object))
-            for(const [cell_key,cell_data] of Object.entries(row_data)) {
-
-                const data = {
-                    'row': row_key,
-                    'col': cell_key,
-                    'cell_type': (typeof cell_data === "object") ? 'invalid_cells' : 'new_cells',
-                };
-
-                if (data['cell_type']==='invalid_cells')
-                    data['comment'] = {value: cell_data.join('<br>')};
-
-                navigation_cells_count[data['cell_type']]++;
-
-                this.validation_results_array.push(data);
-
-            }
-        this.validation_results_sorted = {
-            'invalid_cells': this.validation_results_array.filter(validation_result=>validation_result['cell_type']==='invalid_cells'),
-            'new_cells': this.validation_results_array.filter(validation_result=>validation_result['cell_type']==='new_cells')
-        };
-
 
         //update navigation information
-        for(const navigation_total_element of Object.values(document.getElementsByClassName('wb-navigation_total'))){
+        Object.values(document.getElementsByClassName('wb-navigation_total')).forEach(navigation_total_element => {
             const navigation_type = navigation_total_element.parentElement.getAttribute('data-navigation_type');
-            if(typeof navigation_cells_count[navigation_type] !== "undefined")
-                navigation_total_element.innerText = navigation_cells_count[navigation_type];
-        }
+            navigation_total_element.innerText = cellCounts[navigation_type];
+        });
 
         this.hot.render();
     },
-    renderCell(instance, td, row, col, prop, value, cellProperties) {
+    defineCell(cols, row, col, prop) {
+        let cell_data;
+        try {
+            cell_data = this.cellInfo[row*cols + col];
+        } catch (e) {
+        };
 
-        if(typeof this.validation_results_object[row] !== "undefined" && typeof this.validation_results_object[row][col] !== "undefined"){
+        return {
+            comment: cell_data && {value: cell_data.issues.join('<br>')},
+            renderer: function(instance, td, row, col, prop, value, cellProperties) {
+                if(cell_data && cell_data.isNew)
+                    td.classList.add('wb-no-match-cell');
 
-            const column_validation_result = this.validation_results_object[row][col];
+                if(cell_data && cell_data.issues.length)
+                    td.classList.add('wb-invalid-cell');
 
-            if(typeof column_validation_result === "boolean")
-                td.classList.add('wb-no-match-cell');
-
-            else if(typeof column_validation_result === "object")
-                td.classList.add('wb-invalid-cell');
-
-        }
-
-        Handsontable.renderers.TextRenderer.apply(null, arguments);
+                Handsontable.renderers.TextRenderer.apply(null, arguments);
+            }
+        };
     },
     openPlan() {
         navigation.go(`/workbench-plan/${this.wb.id}/`);
@@ -269,7 +230,7 @@ const WBView = Backbone.View.extend({
     },
     save: function() {
         // clear validation
-        this.validation_results_object = [];
+        this.cellInfo = [];
         this.hot.render();
 
         //show saving progress bar
@@ -416,19 +377,6 @@ const WBView = Backbone.View.extend({
         this.highlightsOn = true;
         this.hot.render();
     },
-    gotoError: function(evt) {
-        const dir = $(evt.currentTarget).hasClass('wb-prev-error') ? -1 : 1;
-        const nCols = this.hot.countCols();
-        const currentPos = this.currentPos ? this.currentPos[0] * nCols + this.currentPos[1] : -1;
-        const maxPos = nCols * this.hot.countRows();
-        for(let i = currentPos + dir; i >= 0 && i < maxPos; i += dir) {
-            if (this.infoFromLog.byPos[i] != null) {
-                this.currentPos = [Math.floor(i / nCols), i % nCols];
-                break;
-            }
-        }
-        this.currentPos && this.hot.selectCell(this.currentPos[0], this.currentPos[1]);
-    },
     removeHighlights: function() {
         this.highlightsOn = false;
         this.hot.render();
@@ -486,78 +434,60 @@ const WBView = Backbone.View.extend({
         a.click();
     },
     navigateCells: function(e,match_current_cell=false){
-
         const button = e.target;
         const direction = button.getAttribute('data-navigation_direction');
         const button_parent = button.parentElement;
         const type = button_parent.getAttribute('data-navigation_type');
 
-        const selected_cell = this.hot.getSelectedLast();
-        let current_row = 0;
-        let current_col = 0;
-
-        if(typeof selected_cell !== "undefined")
-            [current_row, current_col] = selected_cell;
-
         const number_of_columns = this.hot.countCols();
-        const get_cell_position = (row,col) => parseInt(row)*number_of_columns + parseInt(col);
 
-        const cells_to_search = {};
-        for(const cell of this.validation_results_sorted[type])
-            cells_to_search[get_cell_position(cell['row'],cell['col'])] = cell;
+        const selected_cell = this.hot.getSelectedLast();
 
-        let cell_keys = Object.keys(cells_to_search).sort((a,b) => a-b);
-        let cell_keys_length = cell_keys.length;
-
-        if(cell_keys_length===0)
-            return;
-
-        let current_position = get_cell_position(current_row,current_col);
-
-        let target_cell=-1;
-        let cell_relative_position=0;
-
-        let comparison_function;
-
-        if(direction==='next')
-            comparison_function = (position, current_position) => position > current_position;
-        else {
-            cell_keys = cell_keys.reverse();
-            comparison_function = (position, current_position) => position < current_position;
+        let current_position = 0;
+        if(typeof selected_cell !== "undefined") {
+            const [row, col] = selected_cell;
+            current_position = row * number_of_columns + col;
         }
 
-        for(const position of cell_keys.map(key=>parseInt(key))) {
-            if (
-                (match_current_cell && position===current_position) ||
-                comparison_function(position,current_position)
-            ) {
-                target_cell = position;
-                break;
+        const cellIsType = (info) => {
+            switch(type) {
+            case 'invalid_cells':
+                return info.issues.length > 0;
+            case 'new_cells':
+                return info.isNew;
+            case 'search_results':
+                return info.matchesSearch;
+            default:
+                return false;
             }
-            cell_relative_position++;
+        };
+
+        let new_position = current_position;
+        let found = false;
+        for (;
+             new_position >= 0 && new_position < this.cellInfo.length;
+             new_position += direction === 'next' ? 1 : -1)
+        {
+            if (new_position === current_position && !match_current_cell) continue;
+
+            const info = this.cellInfo[new_position];
+            if (typeof info === "undefined") continue;
+            found = cellIsType(info);
+            if (found) break;
         }
 
-        // following cell was found. Horay! 🎉🎉🎉
-        if(target_cell !== -1){
-            const coordinates = ['row','col','row','col'].map(key=>parseInt(cells_to_search[target_cell][key]));
-            this.hot.selectCell(...coordinates);
+        if (found) {
+            const row = Math.floor(new_position / number_of_columns);
+            const col = new_position - row * number_of_columns;
+            this.hot.selectCell(row, col, row, col);
 
-            if(direction!=='next')
-                cell_relative_position = cell_keys_length - cell_relative_position;
-            else
-                cell_relative_position++;
-
+            const cell_relative_position = this.cellInfo.reduce((count, info, i) => count + (cellIsType(info) && i <= new_position ? 1 : 0), 0);
             const current_position_element = button_parent.getElementsByClassName('wb-navigation_position')[0];
             current_position_element.innerText = cell_relative_position;
-
-            return true;
         }
-        else  // better luck next time 😓
-            return false;
-
     },
     searchCells: function(e){
-
+        const cols = this.hot.countCols();
         const button = e.target;
         const container = button.parentElement;
         const navigation_position_element = container.getElementsByClassName('wb-navigation_position')[0];
@@ -567,13 +497,17 @@ const WBView = Backbone.View.extend({
         const search_query = search_query_element.value;
 
         const searchPlugin = this.hot.getPlugin('search');
-        this.validation_results_sorted['search_results'] = {
-            search_query: search_query,
-            results: searchPlugin.query(search_query)
-        };
+        const results = searchPlugin.query(search_query);
+        this.search_query = search_query;
+
+        this.cellInfo.forEach(cellInfo => {cellInfo.matchesSearch = false;});
+        results.forEach(({row, col}) => {
+            this.initCellInfo(row, col);
+            this.cellInfo[row*cols + col].matchesSearch = true;
+        });
         this.hot.render();
 
-        navigation_total_element.innerText = this.validation_results_sorted['search_results']['results'].length;
+        navigation_total_element.innerText = results.length;
         navigation_position_element.innerText = 0;
 
         if(!this.navigateCells({target:navigation_button[0]},true))
@@ -581,23 +515,23 @@ const WBView = Backbone.View.extend({
 
     },
     replaceCells: function(e){
-
+        const cols = this.hot.countCols();
         const button = e.target;
         const container = button.parentElement;
         const replacement_value_element = container.getElementsByClassName('wb-replace_value')[0];
         const replacement_value = replacement_value_element.value;
 
-        this.hot.setDataAtCell(this.validation_results_sorted['search_results']['results'].map(cell=>
-            [
-                cell['row'],
-                cell['col'],
-                cell['data'].replaceAll(
-                    this.validation_results_sorted['search_results']['search_query'],
-                    replacement_value
-                )
-            ]
-        ));
+        const cellUpdates = [];
+        this.cellInfo.forEach((info, i) => {
+            if (info.matchesSearch) {
+                const row = Math.floor(i / cols);
+                const col = i - row * cols;
+                const cellValue = this.hot.getDataAtCell(row, col);
+                cellUpdates.push([row, col, cellValue.split(this.search_query).join(replacement_value)]);
+            }
+        });
 
+        this.hot.setDataAtCell(cellUpdates);
     },
     toggleToolbelt: function(e){
         const button = e.target;
