e5e4d79dacd3114c2f8d95026bfef0105aac5579
Ben Anhalt <ben.anhalt@gmail.com>
2020-12-17 15:56:46 -0600

    convert wb import page to react.

---
+++ specifyweb/frontend/js_src/lib/components/wbimport.tsx
@@ -0,0 +1,370 @@
+import React, {Component} from 'react';
+
+import Papa from 'papaparse';
+import * as XLSX from 'xlsx';
+
+const encodings = require('../encodings.js');
+
+const PREVIEW_SIZE = 10;
+
+type FileType = 'csv' | 'xls';
+
+type FileTypeState = {type: 'FileTypeState',}
+
+type ChooseCSVFileState = {type: 'ChooseCSVFileState', encoding: string,}
+
+type ChooseXLSFileState = {type: 'ChooseXLSFileState',}
+
+type PreviewCSVState = {type:'PreviewCSVState', preview: string[][], file: File, encoding: string, datasetName: string, hasHeader: boolean}
+
+type PreviewXLSState = {type:'PreviewXLSState', preview: string[][], file: File, datasetName: string, hasHeader: boolean}
+
+type LoadingFileState = {type: 'LoadingFileState'}
+
+type WbImportState = FileTypeState | ChooseCSVFileState | ChooseXLSFileState | PreviewCSVState | PreviewXLSState | LoadingFileState;
+
+
+
+type FileTypeAction = {type: 'FileTypeAction', fileType: FileType}
+
+type EncodingAction = {type: 'EncodingAction', encoding: string}
+
+type FileSelectedAction = {type: 'FileSelectedAction', file: File}
+
+type GotPreviewAction = {type: 'GotPreviewAction', preview: string[][], file: File}
+
+type ToggleHeaderAction = {type: 'ToggleHeaderAction'}
+
+type SetDataSetNameAction  = {type: 'SetDataSetNameAction', value: string}
+
+type DoImportAction = {type: 'DoImportAction'}
+
+type Action = FileTypeAction | EncodingAction | FileSelectedAction | GotPreviewAction | ToggleHeaderAction | SetDataSetNameAction | DoImportAction;
+
+
+type HandleAction = (action: Action) => void;
+
+
+export default class WbImport extends Component<{doImport: (name: string, header: string[], data: string[][]) => void}, WbImportState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {type: 'FileTypeState'};
+    }
+
+    generateCSVPreview(file: File, encoding: string) {
+        Papa.parse(file, {
+	    encoding: encoding,
+	    preview: PREVIEW_SIZE,
+	    complete: ({data}) => this.update({type: 'GotPreviewAction', preview: data, file: file})
+        });
+    }
+
+
+    generateXLSPreview(file: File) {
+        loadXLS(file, true, (data) => this.update({type: 'GotPreviewAction', preview: data, file: file}));
+    }
+
+    doImportCSV(file: File, name: string, hasHeader: boolean, encoding: string) {
+        const doIt = () =>
+            Papa.parse(file, {
+	        encoding: encoding,
+	        complete: ({data}) => {
+                    const {rows, header} = extractHeader(data, hasHeader);
+                    this.props.doImport(name, header, rows);
+                }
+            });
+
+        setTimeout(doIt, 0);
+    }
+
+    doImportXLS(file: File, name: string, hasHeader: boolean) {
+        const doIt = () =>
+            loadXLS(file, false, (data: string[][]) => {
+                const {rows, header} = extractHeader(data, hasHeader);
+                this.props.doImport(name, header, rows);
+            });
+
+        setTimeout(doIt, 0);
+    }
+
+
+    update(action: Action) {
+        const setState = (s: WbImportState) => { this.setState(s); };
+
+        switch (action.type) {
+            case 'FileTypeAction':
+                switch (action.fileType) {
+                    case 'xls':
+                        setState({type: 'ChooseXLSFileState'});
+                        break;
+                    case 'csv':
+                        setState({type: 'ChooseCSVFileState', encoding: "utf-8"});
+                        break;
+                    default:
+                        assertExhaustive(action.fileType);
+                }
+                break;
+
+            case 'EncodingAction':
+                switch (this.state.type) {
+                    case 'ChooseCSVFileState':
+                        setState({type: 'ChooseCSVFileState', encoding: action.encoding});
+                        break;
+
+                    case 'PreviewCSVState':
+                        setState({type: 'ChooseCSVFileState', encoding: action.encoding});
+                        this.generateCSVPreview(this.state.file, this.state.encoding);
+                        break;
+
+                }
+                break;
+
+            case 'FileSelectedAction':
+                switch (this.state.type) {
+                    case 'ChooseCSVFileState':
+                    case 'PreviewCSVState':
+                        this.generateCSVPreview(action.file, this.state.encoding);
+                        break;
+
+                    case 'ChooseXLSFileState':
+                    case 'PreviewXLSState':
+                        this.generateXLSPreview(action.file);
+                        break;
+                }
+                break;
+
+            case 'GotPreviewAction':
+                switch (this.state.type) {
+                    case 'ChooseCSVFileState':
+                    case 'PreviewCSVState':
+                        setState({
+                            type: 'PreviewCSVState',
+                            preview: action.preview,
+                            file: action.file,
+                            datasetName: action.file.name.replace(/\.[^\.]*$/, ''),  // remove extentsion
+                            encoding: this.state.encoding,
+                            hasHeader: true,
+                        });
+                        break;
+
+                    case 'ChooseXLSFileState':
+                    case 'PreviewXLSState':
+                        setState({
+                            type: 'PreviewXLSState',
+                            preview: action.preview,
+                            file: action.file,
+                            datasetName: action.file.name.replace(/\.[^\.]*$/, ''),  // remove extentsion
+                            hasHeader: true,
+                        });
+                        break;
+                }
+                break;
+
+            case 'ToggleHeaderAction':
+                switch (this.state.type) {
+                    case 'PreviewCSVState':
+                    case 'PreviewXLSState':
+                        setState({...this.state, hasHeader: !this.state.hasHeader});
+                        break;
+                }
+                break;
+
+            case 'SetDataSetNameAction':
+                switch (this.state.type) {
+                    case 'PreviewCSVState':
+                    case 'PreviewXLSState':
+                        setState({...this.state, datasetName: action.value});
+                        break;
+                }
+                break;
+
+            case 'DoImportAction':
+                switch (this.state.type) {
+                    case 'PreviewCSVState':
+                        setState({type: 'LoadingFileState'});
+                        this.doImportCSV(this.state.file, this.state.datasetName, this.state.hasHeader, this.state.encoding);
+                        break;
+
+                    case 'PreviewXLSState':
+                        setState({type: 'LoadingFileState'});
+                        this.doImportXLS(this.state.file, this.state.datasetName, this.state.hasHeader);
+                        break;
+                }
+                break;
+
+
+            default:
+                assertExhaustive(action);
+        }
+    }
+
+    render() {
+        const update = (action: Action) => this.update(action);
+
+        let ui;
+        switch (this.state.type) {
+            case 'FileTypeState':
+                ui = <SelectFileType fileType={null} update={update} />;
+                break;
+
+            case 'ChooseXLSFileState':
+                ui = (<>
+                    <SelectFileType fileType="xls" update={update} />
+                    <ChooseFile fileType="xls" update={update} />
+                    </>);
+                break;
+
+            case 'ChooseCSVFileState':
+                ui = (<>
+                    <SelectFileType fileType="csv" update={update} />
+                    <ChooseEncoding encoding={this.state.encoding} update={update} />
+                    <ChooseFile fileType="csv" update={update} />
+                    </>);
+                break;
+
+            case 'PreviewCSVState':
+                ui = (<>
+                    <SelectFileType fileType="csv" update={update} />
+                    <ChooseEncoding encoding={this.state.encoding} update={update} />
+                    <ChooseFile fileType="csv" update={update} />
+                    <ToggleHeader hasHeader={this.state.hasHeader} update={update} />
+                    <ChooseName name={this.state.datasetName} update={update} />
+                    <DoImportButton update={update} />
+                    <h2>Preview Dataset</h2>
+                    <Preview data={this.state.preview} hasHeader={this.state.hasHeader} />
+                    </>);
+                break;
+
+            case 'PreviewXLSState':
+                ui = (<>
+                    <SelectFileType fileType="xls" update={update} />
+                    <ChooseFile fileType="xls" update={update} />
+                    <ToggleHeader hasHeader={this.state.hasHeader} update={update} />
+                    <ChooseName name={this.state.datasetName} update={update} />
+                    <DoImportButton update={update} />
+                    <h2>Preview Dataset</h2>
+                    <Preview data={this.state.preview} hasHeader={this.state.hasHeader} />
+                    </>);
+                break;
+
+            case 'LoadingFileState':
+                ui = <p>Loading...</p>;
+                break;
+
+            default:
+                assertExhaustive(this.state);
+        }
+
+        return (<div>
+                <h2>Import Dataset</h2>
+                {ui}
+                </div>);
+    }
+}
+
+
+function SelectFileType(props: {fileType: FileType | null, update: HandleAction}) {
+    function selected(value: string) {
+        if (value == "csv" || value == "xls") {
+            props.update({type: 'FileTypeAction', fileType: value});
+        }
+    }
+
+    return (<p>
+            <select onChange={event => selected(event.target.value)} value={props.fileType || ""}>
+            <option>Choose File Type</option>
+            <option value="xls">Excel</option>
+            <option value="csv">CSV</option>
+            </select>
+            </p>);
+}
+
+
+function ChooseEncoding(props: {encoding: string | null, update: HandleAction}) {
+    function selected(value: string) {
+        props.update({type: 'EncodingAction', encoding: value});
+    }
+
+    const options = encodings.allLabels.map((encoding: string) => <option key={encoding}>{encoding}</option>);
+
+    return (<p>
+            Character encoding:{" "}
+            <select onChange={event => selected(event.target.value)} value={props.encoding || ""}>
+            {options}
+            </select>
+            </p>);
+}
+
+
+function ChooseFile(props: {fileType: FileType, update: HandleAction}) {
+    function changed(target: HTMLInputElement) {
+        if (target.files && target.files[0]) {
+            props.update({type: 'FileSelectedAction', file: target.files[0]});
+        }
+    }
+
+    const exts =
+        props.fileType === 'csv' ? ".csv,.tsv,.txt" :
+        props.fileType === 'xls' ? ".xls,.xlsx" :
+        assertExhaustive(props.fileType);
+
+    return (<p>Choose {props.fileType} file: <input type="file" accept={exts} onChange={event => changed(event.target)}/></p>);
+}
+
+
+function Preview(props: {data: string[][], hasHeader: boolean}) {
+    const hasHeader = props.hasHeader;
+    const data = props.data;
+    const {rows, header} = extractHeader(data, hasHeader);
+
+    const headerCells = header.map((cell, i) => <th key={i}>{cell}</th>);
+    const dataRows = rows.map((row, i) => <tr key={i}>{ row.map((cell, i) => <td key={i}>{cell}</td>) }</tr>);
+
+    return <table><thead><tr className="header">{headerCells}</tr></thead><tbody>{dataRows}</tbody></table>;
+}
+
+function ChooseName(props: {name: string, update: HandleAction}) {
+    return (<p>
+        Name for new data set:{" "}
+        <input type="text" value={props.name} onChange={event => props.update({type: 'SetDataSetNameAction', value: event.target.value})}/>
+        </p>);
+}
+
+function ToggleHeader(props: {hasHeader: boolean, update: HandleAction}) {
+    return (<label>First row is header:{" "}
+        <input type="checkbox" onChange={()=>props.update({type: "ToggleHeaderAction"})} checked={props.hasHeader}/>
+        </label>);
+}
+
+function DoImportButton(props: {update: HandleAction}) {
+    return <button onClick={() => props.update({type: 'DoImportAction'})}>Import</button>;
+}
+
+function extractHeader(data: string[][], headerInData: boolean): {rows: string[][], header: string[]} {
+    const header = headerInData ? data[0] : data[0].map((_1, i) => "Column " + (i+1));
+    const rows = headerInData ? data.slice(1) : data;
+    return {rows: rows, header: header};
+}
+
+function loadXLS(file: File, forPreview: boolean, callback: (data: string[][]) => void) {
+    const reader = new FileReader();
+    reader.readAsBinaryString(file);
+    reader.onload = (e: ProgressEvent<FileReader>) => {
+
+        const options: XLSX.ParsingOptions = forPreview ? {type: "binary", sheetRows: PREVIEW_SIZE} : {type: "binary"};
+	const workbook = XLSX.read(e.target?.result, options);
+
+	const first_sheet_name = workbook.SheetNames[0];
+	const first_workbook = workbook.Sheets[first_sheet_name];
+	const data: string[][] = XLSX.utils.sheet_to_json(first_workbook, {header: 1})
+            .map(row => Array.isArray(row) ? Array.from(row, v => v || "") : []);
+
+        callback(data);
+    }
+}
+
+
+function assertExhaustive(x: never): never {
+    throw new Error("Non-exhaustive switch. Unhandled case:" + x);
+}
+
--- specifyweb/frontend/js_src/lib/wbimport.js
+++ specifyweb/frontend/js_src/lib/wbimport.js
@@ -1,53 +1,32 @@
-"use strict";
+
 require('../css/wbimport.css');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import WbImport from './components/wbimport';
 
 const $ = require('jquery');
 const _ = require('underscore');
 const Backbone = require('./backbone.js');
-const Bacon = require('baconjs');
-const Papa = require('papaparse');
 const Q = require('q');
-const xlsx = require('xlsx');
 
 const schema = require('./schema.js');
-const wbimport = require('./templates/wbimport.html');
 const navigation = require('./navigation.js');
 const app = require('./specifyapp.js');
 const uniquifyWorkbenchName = require('./wbuniquifyname.js');
 const userInfo = require('./userinfo.js');
-const encodings = require('./encodings.js');
 
-function Preview($table, previews, hasHeader) {
-    Bacon.combineWith(
-        previews, hasHeader,
-        (preview, header) =>
-            preview.map(
-                (row, i) => $('<tr>')
-                    .addClass(header && i===0 ? 'header' : '')
-                    .append(row.map(cell => $('<td>').text(cell)[0]))[0]
-            ))
-        .onValue(trs => $table.empty().append(trs));
-}
 
-function ValueProperty($el) {
-    return $el.asEventStream('change')
-        .map(event => $el.val())
-        .toProperty($el.val());
-}
+const WBImportView = Backbone.View.extend({
+    __name__: "WBImportView",
+    className: 'workbench-import-view',
+    render: function() {
+        ReactDOM.render(<WbImport doImport={doImport} />, this.el);
+        return this;
+    }
+});
 
-function CheckedProperty($el) {
-    return $el.asEventStream('change')
-        .map(event => $el.prop('checked'))
-        .toProperty($el.prop('checked'));
-}
-
-function QfromCallback(func) {
-    const defer = Q.defer();
-    func(result => defer.resolve(result));
-    return defer.promise;
-}
-
-function doImport(template, file, workbenchName, header, encoding) {
+function doImport(name, header, data) {
     const dialog = $('<div><div class="progress-bar"></div></div>').dialog({
         title: 'Importing',
         modal: true,
@@ -56,192 +35,41 @@ function doImport(template, file, workbenchName, header, encoding) {
     });
     $('.progress-bar', dialog).progressbar({value: false});
 
-    template.set('name', workbenchName);
+    const template = new schema.models.WorkbenchTemplate.Resource({
+        specifyuser: userInfo.resource_uri,
+        workbenchtemplatemappingitems: header.map(
+            (column, i) => new schema.models.WorkbenchTemplateMappingItem.Resource({
+                caption: column,
+                fieldname: column,
+                vieworder: i,
+                origimportcolumnindex: i
+            })
+        )
+    });
+    template.set('name', name);
+
     const workbench = new schema.models.Workbench.Resource({
-        name: workbenchName,
+        name: name,
         workbenchtemplate: template,
         specifyuser: userInfo.resource_uri,
-        srcfilepath: file.name
+        srcfilepath: ""
     });
 
-
-    const file_extension = file.name.split('.').pop();
-    let parseQ;
-	if(file_extension === 'xls' || file_extension === 'xlsx')
-		parseQ = QfromCallback(
-			callback => callback(readXLSX(file))
-		);
-	else
-		parseQ = QfromCallback(
-			callback => Papa.parse(file, {
-				encoding: encoding,
-				complete: parse => callback(parse.data)})
-		);
-
-    return parseQ.then(
-        rows => header ? rows.slice(1) : rows
-    ).then(
-        rows => rows.map(row => [null].concat(row))
-    ).then(
-        rows => Q(workbench.save()).then(() => rows)
-    ).then(
-        data => Q($.ajax('/api/workbench/rows/' + workbench.id + '/', {
-            data: JSON.stringify(data),
-            type: "PUT"
-        }))
-    ).tap(
-        () => dialog.dialog('close')
-    ).then(
-        () => workbench
-    );
+    Q(workbench.save())
+        .then(
+            () => Q($.ajax('/api/workbench/rows/' + workbench.id + '/', {
+                data: JSON.stringify(data.map(row => [null].concat(row))),
+                type: "PUT"
+            }))
+        ).done(() => {
+            dialog.dialog('close');
+            navigation.go('/workbench/' + workbench.id + '/');
+        });
 }
 
 
-function makeWorkbenchName(input, fileSelected) {
-    const selectedFileName = fileSelected.map(
-        file => file.name.replace(/\.[^\.]*$/, ''));  // remove extentsion
-    return Bacon.combineWith(
-        input, selectedFileName,
-        (entered, selected) => entered === '' ? selected : entered)
-        .flatMap(name => Bacon.fromPromise(uniquifyWorkbenchName(name)));
-}
-
-
-function createTemplate(columns) {
-    return new schema.models.WorkbenchTemplate.Resource({
-        specifyuser: userInfo.resource_uri,
-        workbenchtemplatemappingitems: makeMappingItems(columns)
-    });
-}
-
-function makeMappingItems(columns) {
-    return columns.map(
-        (column, i) => new schema.models.WorkbenchTemplateMappingItem.Resource({
-            caption: column,
-            fieldname: column,
-            vieworder: i,
-            origimportcolumnindex: i
-        })
-    );
-}
-
-function readXLSX(file, preview_size=0){
-
-	return new Promise((resolve, reject)=>{
-
-		const reader = new FileReader();
-		reader.readAsBinaryString(file);
-		reader.onload = e=>{
-
-			const raw_file_data = e.target.result;  // read file contents
-
-			const read_config = {
-				type: 'binary',
-			};
-			if(preview_size !== 0)
-				read_config.sheetRows = preview_size;
-
-			const workbook = xlsx.read(raw_file_data, read_config);  // parse file contents
-
-			const first_sheet_name = workbook.SheetNames[0];
-			const first_workbook = workbook.Sheets[first_sheet_name];  // get the first sheet data
-			let data = xlsx.utils.sheet_to_json(first_workbook, {header: 1});
-
-			if(data.length>0){  // make each row have the same number of cells
-				const header_row = data[0];
-				const number_of_headers = header_row.length;
-				const empty_row = header_row.map(()=>"");
-				data = data.map(line=>{
-					const cells_to_add = number_of_headers-line.length;
-					if(cells_to_add>0)
-						return [...line, ...empty_row.slice(0,cells_to_add)];
-					return line;
-				});
-			}
-			resolve(data);
-		};
-
-	});
-
-}
-
-const WBImportView = Backbone.View.extend({
-    __name__: "WBImportView",
-    className: 'workbench-import-view',
-    initialize: function(options) {
-        this.templates = options.templates;
-    },
-    render: function() {
-        this.$el.append(wbimport({
-            encodings: encodings.allLabels,
-            nameChars: schema.models.Workbench.getField('name').length
-        }));
-
-        const fileSelected = this.$(':file').asEventStream('change')
-              .map(event => event.currentTarget.files[0])
-              .filter(file => file != null);
-
-        fileSelected.onValue(() => this.$(':hidden').show());
-
-        const workbenchName = makeWorkbenchName(ValueProperty(this.$(':text')), fileSelected);
-
-        workbenchName.onValue(wbName => this.$(':text').val(wbName));
-
-        const encodingSelected = ValueProperty(this.$('select.encoding'));
-
-        const hasHeader = CheckedProperty(this.$(':checkbox'));
-
-		const previews = Bacon.combineAsArray(fileSelected, encodingSelected)
-			.flatMap(
-				([file, encoding]) => Bacon.fromCallback(
-					callback => {
-
-						const file_extension = file.name.split('.').pop();
-						const preview_size = 10;
-
-						if(file_extension === 'xls' || file_extension === 'xlsx')
-							readXLSX(file, preview_size).then(data=>callback({data: data}));
-
-						else
-							Papa.parse(file, {
-								encoding: encoding,
-								preview: preview_size,
-								complete: callback
-							});
-					}
-				))
-			.map(parse => parse.data);
-
-        Preview(this.$('table'), previews, hasHeader);
-
-        const columns = Bacon.combineWith(
-            previews, hasHeader,
-            (preview, header) =>
-                header ? preview[0] : preview[0].map((__, i) => "Column " + (i + 1)));
-
-
-        const buttonClicks = this.$('button').button().asEventStream('click');
-
-        const createdTemplate = columns.sampledBy(buttonClicks).map(createTemplate);
-
-        Bacon.combineWith(createdTemplate, fileSelected, workbenchName, hasHeader, encodingSelected,
-                          doImport
-        )
-            .flatMap(q => Bacon.fromPromise(q))
-            .onValue(wb => navigation.go('/workbench/' + wb.id + '/'));
-
-        return this;
-    }
-});
-
-
-module.exports = function() {
+export default function() {
     app.setTitle("Import Dataset");
-    const templates = new schema.models.WorkbenchTemplate.LazyCollection({
-        filters: { specifyuser: userInfo.id }
-    });
-    templates.fetch({ limit: 500 }).done(function() {
-        app.setCurrentView(new WBImportView({ templates: templates }));
-    });
+    app.setCurrentView(new WBImportView());
 };
 
