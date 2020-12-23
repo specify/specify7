b7ee0fd2199bfa3d60769790b7ae8369dd4e5bde
Ben Anhalt <ben.anhalt@gmail.com>
2020-12-23 15:04:50 -0600

    remove old wb template editor and deps.

--- specifyweb/frontend/js_src/lib/wbsdialog.js
+++ specifyweb/frontend/js_src/lib/wbsdialog.js
@@ -6,73 +6,72 @@ var Backbone = require('./backbone.js');
 
 var schema              = require('./schema.js');
 var navigation          = require('./navigation.js');
-var WBTemplateEditor    = require('./wbtemplateeditor.js');
 var userInfo    = require('./userinfo.js');
 var EditResourceDialog  = require('./editresourcedialog.js');
 const uniquifyName = require('./wbuniquifyname.js');
 
-    var NewWorkbenchDialog = Backbone.View.extend({
-        __name__: "NewWorkbenchDialog",
-        className: "table-list-dialog",
-        events: {
-            'click a': 'select'
-        },
-        render: function() {
-            this.templates = new schema.models.WorkbenchTemplate.LazyCollection({
-                filters: { specifyuser: userInfo.id, orderby: 'name' }
-            });
-            this.templates.fetch({ limit: 500 }).done(this.gotTemplates.bind(this));
-            return this;
-        },
-        gotTemplates: function() {
-            if (this.templates.length < 1) {
-                this.newTemplate();
-                return;
-            }
-            var entries = this.templates.map(this.dialogEntry, this);
-            $('<table>').append(entries).appendTo(this.el);
-            this.$el.dialog({
-                title: "Choose Dataset Template",
-                maxHeight: 400,
-                modal: true,
-                close: function() { $(this).remove(); },
-                buttons: [
-                    { text: 'New', click: this.newTemplate.bind(this) },
-                    { text: 'Cancel', click: function() { $(this).dialog('close'); } }
-                ]
-            });
-        },
-        dialogEntry: function(template) {
-            var link = $('<a href="#">').text(template.get('name'));
-            return $('<tr>').append(
-                $('<td>').append(link)
-            )[0];
-        },
-        newTemplate: function() {
-            new WBTemplateEditor({ columns: [] })
-                .render()
-                .on('created', this.makeWorkbench, this);
-        },
-        select: function(event) {
-            event.preventDefault();
-            var i = this.$('a').index(event.currentTarget);
-            var template = this.templates.at(i).clone();
-            this.makeWorkbench(template);
-        },
-        makeWorkbench: function(template) {
-            var workbench = new schema.models.Workbench.Resource();
-            workbench.set({
-                workbenchtemplate: template,
-                specifyuser: template.get('specifyuser')
-            });
+    // var NewWorkbenchDialog = Backbone.View.extend({
+    //     __name__: "NewWorkbenchDialog",
+    //     className: "table-list-dialog",
+    //     events: {
+    //         'click a': 'select'
+    //     },
+    //     render: function() {
+    //         this.templates = new schema.models.WorkbenchTemplate.LazyCollection({
+    //             filters: { specifyuser: userInfo.id, orderby: 'name' }
+    //         });
+    //         this.templates.fetch({ limit: 500 }).done(this.gotTemplates.bind(this));
+    //         return this;
+    //     },
+    //     gotTemplates: function() {
+    //         if (this.templates.length < 1) {
+    //             this.newTemplate();
+    //             return;
+    //         }
+    //         var entries = this.templates.map(this.dialogEntry, this);
+    //         $('<table>').append(entries).appendTo(this.el);
+    //         this.$el.dialog({
+    //             title: "Choose Dataset Template",
+    //             maxHeight: 400,
+    //             modal: true,
+    //             close: function() { $(this).remove(); },
+    //             buttons: [
+    //                 { text: 'New', click: this.newTemplate.bind(this) },
+    //                 { text: 'Cancel', click: function() { $(this).dialog('close'); } }
+    //             ]
+    //         });
+    //     },
+    //     dialogEntry: function(template) {
+    //         var link = $('<a href="#">').text(template.get('name'));
+    //         return $('<tr>').append(
+    //             $('<td>').append(link)
+    //         )[0];
+    //     },
+    //     newTemplate: function() {
+    //         new WBTemplateEditor({ columns: [] })
+    //             .render()
+    //             .on('created', this.makeWorkbench, this);
+    //     },
+    //     select: function(event) {
+    //         event.preventDefault();
+    //         var i = this.$('a').index(event.currentTarget);
+    //         var template = this.templates.at(i).clone();
+    //         this.makeWorkbench(template);
+    //     },
+    //     makeWorkbench: function(template) {
+    //         var workbench = new schema.models.Workbench.Resource();
+    //         workbench.set({
+    //             workbenchtemplate: template,
+    //             specifyuser: template.get('specifyuser')
+    //         });
 
-            new EditResourceDialog({ resource: workbench })
-                .render()
-                .on('savecomplete',
-                    () => uniquifyName(workbench.get('name'), workbench.id).done(
-                        name => workbench.set('name', name).save().done(() => navigation.go('/workbench/' + workbench.id + '/'))));
-        }
-    });
+    //         new EditResourceDialog({ resource: workbench })
+    //             .render()
+    //             .on('savecomplete',
+    //                 () => uniquifyName(workbench.get('name'), workbench.id).done(
+    //                     name => workbench.set('name', name).save().done(() => navigation.go('/workbench/' + workbench.id + '/'))));
+    //     }
+    // });
 
 
 module.exports =  Backbone.View.extend({
@@ -90,7 +89,7 @@ module.exports =  Backbone.View.extend({
                 modal: true,
                 close: function() { $(this).remove(); },
                 buttons: [
-                    { text: 'New', click: this.newWB.bind(this) },
+                    // { text: 'New', click: this.newWB.bind(this) },
                     { text: 'Import', click: function() { navigation.go('/workbench-import/'); } },
                     { text: 'Cancel', click: function() { $(this).dialog('close'); } }
                 ]
@@ -113,8 +112,8 @@ module.exports =  Backbone.View.extend({
             }, 100);
             return entry[0];
         },
-        newWB: function() {
-            new NewWorkbenchDialog().render();
-        }
+        // newWB: function() {
+        //     new NewWorkbenchDialog().render();
+        // }
     });
 
