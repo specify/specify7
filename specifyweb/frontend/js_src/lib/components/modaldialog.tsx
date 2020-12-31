39f9c0176cb5dad26ccc71a7eba4102b781f1009
Maksym Patiiuk <40512816+maxxxxxdlp@users.noreply.github.com>
2020-12-31 21:38:18 +0200

    Creates React Model Dialog

---
+++ specifyweb/frontend/js_src/lib/components/modaldialog.tsx
@@ -0,0 +1,48 @@
+"use strict";
+
+import React from "react";
+import $ from 'jquery';
+
+export const ModalDialog = (props :ModalDialogProps) => {
+	const dialog_ref = React.useRef(null);
+
+	React.useEffect(() => {
+		const dialog = $(dialog_ref).dialog({
+			modal: true,
+			close: () => dialog.remove(),
+			buttons: [
+				{
+					text: 'Cancel', click: function () {
+						$(this).remove();
+					}
+				}
+			],
+			...props.properties
+		});
+		if (typeof props.onLoadCallback !== "undefined")
+			props.onLoadCallback(dialog);
+
+		return () => {
+			dialog.remove();
+		};
+	});
+
+	return (<div ref={dialog_ref}>
+		{props.children}
+	</div>);
+};
+
+//Loading Screen
+const LoadingScreen_onLoadCallback = (dialog :JQuery<HTMLElement>) =>
+	$('.progress-bar', dialog).progressbar({value: false});
+
+export const LoadingScreen = () =>
+	<ModalDialog
+		onLoadCallback={LoadingScreen_onLoadCallback}
+		properties={{
+			title: 'Loading',
+			buttons: []
+		}}
+	>
+		<div className="progress-bar"/>
+	</ModalDialog>;
