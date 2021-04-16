9b454d31faa0668cc908b16c4c745e642f123d75
Maksym Patiiuk <40512816+maxxxxxdlp@users.noreply.github.com>
2021-04-16 20:39:23 +0300

    Add configurations for ESLint, StyleLint and Preetier

---
+++ specifyweb/frontend/js_src/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.eslint.json',
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ['@maxxxxxdlp/eslint-config'],
+};
---
+++ specifyweb/frontend/js_src/.prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // single quote is more popular among JS libraries
+  singleQuote: true,
+  // GitHub renders Markdown properly, so we can wrap it safely
+  proseWrap: 'always',
+};
