aef9723d4006f8622e4aea6397fe54762bbbfcc5
Maksym Patiiuk <40512816+maxxxxxdlp@users.noreply.github.com>
2021-04-27 23:44:26 +0300

    Change wbdsdialog's layout to a table

--- specifyweb/frontend/js_src/lib/wbsdialog.js
+++ specifyweb/frontend/js_src/lib/wbsdialog.js
@@ -1,61 +1,84 @@
-"use strict";
+'use strict';
 
-const $        = require('jquery');
-const _        = require('underscore');
+const $ = require('jquery');
 const Backbone = require('./backbone.js');
+const navigation = require('./navigation.js');
 
-const schema              = require('./schema.js');
-const navigation          = require('./navigation.js');
-const userInfo    = require('./userinfo.js');
-const EditResourceDialog  = require('./editresourcedialog.js');
-const uniquifyName = require('./wbuniquifyname.js');
+require('../css/wbdsdialog.css');
 
-module.exports =  Backbone.View.extend({
-    __name__: "WbsDialog",
-    className: "wbs-dialog table-list-dialog",
-    initialize({datasets}) {
-        this.datasets = datasets;
-    },
-    render() {
-        const entries = _.map(this.datasets, this.dialogEntry, this);
-        $('<table>').append(entries).appendTo(this.el);
-        this.$el.dialog({
-            title: "Datasets",
-            maxHeight: 400,
-            modal: true,
-            close() { $(this).remove(); },
-            buttons: [
-                { text: 'Import', click() { navigation.go('/workbench-import/'); } },
-                { text: 'Cancel', click() { $(this).dialog('close'); } }
-            ]
-        });
-        return this;
-    },
-    dialogEntry(ds) {
-        const img = $('<img>', { src: '/images/Workbench32x32.png' });
-        const href = `/workbench/${ds.id}/`;
-        const state = ds.uploadresult === null ?
-            'un-uploaded' :
-            ds.uploadresult.success ?
-                'uploaded' : 'has-upload-error';
-        const styles = {
-            'un-uploaded': 'color: #00f',
-            'uploaded': 'font-weight: 100',
-            'has-upload-error': 'color: #f00'
-        };
-        const label = {
-            'un-uploaded': '',
-            'uploaded': ' [uploaded]',
-            'has-upload-error': ' [upload failed]',
-        };
-        const link = $('<a>', {
-            href: href,
-            class: "intercept-navigation",
-            style: styles[state],
-        }).text(ds.name + label[state]);
-        return $('<tr>').append(
-            $('<td>').append(img),
-            $('<td>').append(link),
-            $('<td class="item-count" style="display:none">'));
-    },
+module.exports = Backbone.View.extend({
+  __name__: 'WbsDialog',
+  className: 'wbs-dialog table-list-dialog',
+  initialize({ datasets }) {
+    this.datasets = datasets;
+  },
+  render() {
+    $(
+      this.datasets.length === 0
+        ? `<p>No data sets present. Use the "Import" button to import data</p>`
+        : `<table class="wb-ds-dialog-table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Upload</th>
+            <th>Date Created</th>
+            <th>Owner</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${this.datasets.map(this.datasetToHTML).join('')}
+        </tbody>
+      </table>`
+    ).appendTo(this.el);
+    this.$el.dialog({
+      title: 'Data Sets',
+      maxHeight: 400,
+      width: 600,
+      modal: true,
+      close() {
+        $(this).remove();
+      },
+      buttons: [
+        {
+          text: 'Import',
+          click() {
+            navigation.go('/workbench-import/');
+          },
+        },
+        {
+          text: 'Cancel',
+          click() {
+            $(this).dialog('close');
+          },
+        },
+      ],
+    });
+    return this;
+  },
+  datasetToHTML(dataset) {
+    const state =
+      dataset.uploadresult === null
+        ? 'un-uploaded'
+        : dataset.uploadresult.success
+        ? 'uploaded'
+        : 'has-upload-error';
+
+    const uploadedStates = {
+      'un-uploaded': '',
+      uploaded: 'Success',
+      'has-upload-error': '',
+    };
+
+    return `<tr class="wb-ds-dialog-status-${state}">
+      <td>
+        <a href="/workbench/${dataset.id}/">
+          <img src="/images/Workbench32x32.png" alt="">
+          ${dataset.name}
+        </a>
+      </td>
+      <td>${uploadedStates[state]}</td>
+      <td>${new Date(dataset.timestampcreated).toLocaleString()}</td>
+      <td>[TODO]</td>
+    </tr>`;
+  },
 });
