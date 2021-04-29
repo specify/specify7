777c01c93c4e6cc82a0edb3717d73b1534d61800
Maksym Patiiuk <40512816+maxxxxxdlp@users.noreply.github.com>
2021-04-30 01:31:35 +0300

    Add UI for changing data set owner

--- specifyweb/frontend/js_src/lib/datasetmeta.js
+++ specifyweb/frontend/js_src/lib/datasetmeta.js
@@ -5,6 +5,8 @@ import app from './specifyapp';
 import { format } from './dataobjformatters';
 import schema from './schema';
 import resourceApi from './resourceapi';
+import navigation from './navigation';
+import userInfo from './userinfo.js';
 
 export default Backbone.View.extend({
   __name__: 'DataSetNameView',
@@ -17,6 +19,8 @@ export default Backbone.View.extend({
     this.model = schema.getModel('agent');
     this.createdByAgent = null;
     this.modifiedByAgent = null;
+    this.changeOwnerDialog = null;
+    this.listOfUsers = null;
   },
   render() {
     if (this.dialog !== null) {
@@ -103,7 +107,7 @@ export default Backbone.View.extend({
       Modified By: <i class="modified-by-field"></i><br>
       Imported file name: <i>${this.dataset.importedfilename}</i><br>
     </div>`).dialog({
-      title: 'Edit dataset metadata',
+      title: 'Edit Data Set metadata',
       modal: true,
       open(evt, ui) {
         $('.ui-dialog-titlebar-close', ui.dialog).hide();
@@ -111,7 +115,11 @@ export default Backbone.View.extend({
       close: () => this.render(),
       buttons: {
         Cancel: () => this.render(),
-        Save: this.setName.bind(this),
+        Save: this.setName.bind(this, this.render),
+        'Change owner': this.setName.bind(
+          this,
+          this.changeOwnerWindow.bind(this)
+        ),
       },
     });
 
@@ -120,7 +128,7 @@ export default Backbone.View.extend({
       this.dialog.find('.modified-by-field')
     );
   },
-  setName() {
+  setName(callback) {
     if (this.dialog === null) return;
 
     const [newName, newRemarks] = ['.dataset-name', '.dataset-remarks']
@@ -131,7 +139,7 @@ export default Backbone.View.extend({
       newName === this.dialog.name &&
       newRemarks === this.dialog.remarks.toString()
     )
-      this.render();
+      callback();
     else {
       uniquifyName(newName, this.dataset.id).done((uniqueName) => {
         $.ajax(`/api/workbench/dataset/${this.dataset.id}/`, {
@@ -142,9 +150,67 @@ export default Backbone.View.extend({
         }).done(() => {
           this.dataset.name = uniqueName;
           this.dataset.remarks = newRemarks;
-          this.render();
+          callback();
         });
       });
     }
+  },
+  fetchListOfUsers() {
+    return new Promise((resolve) =>
+      this.listOfUsers === null
+        ? fetch('/api/specify/specifyuser/?limit=500')
+            .then((response) => response.json())
+            .then(({ objects: users }) =>
+              resolve(
+                (this.listOfUsers = users
+                  .map((user) => ({
+                    id: user.id,
+                    name: user.name,
+                  }))
+                  .filter(({ id }) => id !== userInfo.id))
+              )
+            )
+        : resolve(this.listOfUsers)
+    );
+  },
+  changeOwnerWindow() {
+    this.fetchListOfUsers().then((users) => {
+      this.changeOwnerDialog = $(`<div>
+        Select new owner:<br>
+        <select class="select-user-picklist" size="10" style="width:100%">
+          ${users
+            .map(
+              (user) => `<option value="${user.id}">
+            ${user.name}
+          </option>`
+            )
+            .join('<br>')}
+        </select>
+      </div>`).dialog({
+        title: 'Change Data Set Ownership',
+        modal: true,
+        open(evt, ui) {
+          $('.ui-dialog-titlebar-close', ui.dialog).hide();
+        },
+        close: () => this.changeOwnerDialog.dialog('destroy'),
+        buttons: {
+          Cancel: () => this.changeOwnerDialog.dialog('destroy'),
+          'Change owner': this.changeOwner.bind(this),
+        },
+      });
+    });
+  },
+  changeOwner() {
+    const selectedOwner = this.changeOwnerDialog
+      .find('.select-user-picklist')
+      .val();
+    if (!selectedOwner) return;
+    $.post(`/api/workbench/transfer/${this.dataset.id}/`, {
+      specifyuserid: selectedOwner,
+    })
+      .done(() => navigation.go('/specify/'))
+      .fail((error) => {
+        throw error;
+      });
   },
 });
