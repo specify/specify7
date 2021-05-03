specify/specify7

commit 91b389d5b24d4e8177c4ee46626ee8bc1affc030

Fix Handsontable being too tall

specifyweb/frontend/js_src/lib/wbview.js
@@ -455,7 +455,13 @@ const WBView = Backbone.View.extend({
         return true;
     },
     calcHeight: function() {
-        return $(window).height() - this.$el.offset().top - 50;
+
+        const offsetTop = this.$el.offset().top;
+
+        if(offsetTop === 0)
+            setTimeout(this.resize.bind(this), 20);
+
+        return $(window).height() - offsetTop - 55;
     },
     saveClicked: function() {
         this.save().done();

commit 79e237398e38d85a42a0fc82a696cfdff38410eb

Fix infinite HOT resizing bug introduced in 91b389d5
Not sure what I meant to fix by this, but it is no longer necessary
as of 54453ae8

specifyweb/frontend/js_src/lib/wbview.js
@@ -79,7 +79,7 @@ const WBView = Backbone.View.extend({
     this.$el.append(
       template({
         is_uploaded: this.uploaded,
-        is_manager: userInfo.usertype === "Manager"
+        is_manager: userInfo.usertype === 'Manager',
       })
     );
 
@@ -718,7 +718,6 @@ const WBView = Backbone.View.extend({
     return true;
   },
   calcHeight: function () {
-    if (this.$el.offset().top === 0) setTimeout(this.resize.bind(this), 20);
     return (
       $(window).height() - 15 - this.$el.find('.wb-spreadsheet').offset().top
     );