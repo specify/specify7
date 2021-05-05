db5b3eabbab2cf1aa7de6d8a8b7550ff4b973829
Ben Anhalt <ben.anhalt@gmail.com>
2021-05-05 17:24:19 -0500

    add testcase to validate tables openapi spec.
    and fix the issues that were uncovered.

--- specifyweb/specify/tests.py
+++ specifyweb/specify/tests.py
@@ -1,3 +1,6 @@
+from jsonschema import validate  # type: ignore
+from jsonschema.exceptions import ValidationError  # type: ignore
+
 from django.test import TestCase
 from django.conf import settings
 
@@ -7,3 +10,10 @@ from .test_load_datamodel import *
 if settings.TEST_RUNNER == 'selenium_testsuite_runner.SeleniumTestSuiteRunner':
     from .selenium_tests import *
 
+class OpenApiTests(TestCase):
+    def test_tables_spec(self) -> None:
+        from .schema import generate_openapi_for_tables
+        from specifyweb.context.openapi_schema import schema
+
+        spec = generate_openapi_for_tables()
+        validate(instance=spec, schema=schema)
