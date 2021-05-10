ca5da30e1f549e04c3e75abf8dd709c68d33f33a
Maksym Patiiuk <40512816+maxxxxxdlp@users.noreply.github.com>
2021-05-10 23:55:23 +0300

    Fix `docker-entrypoint.sh` not working in development containers

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,12 @@
- #!/bin/bash
+#!/bin/bash
 set -e
 
-if [ "$1" = 've/bin/gunicorn' ]; then
-    echo "Updating static files in /volumes/static-files/."
-    rsync -a --delete specifyweb/frontend/static/ /volumes/static-files/frontend-static
-    cd /opt/specify7
-    echo "Applying Django migrations."
-    ve/bin/python manage.py migrate notifications
-    ve/bin/python manage.py migrate workbench
+if [ "$1" = 've/bin/gunicorn' ] || [ "$1" = 've/bin/python' ]; then
+  echo "Updating static files in /volumes/static-files/."
+  rsync -a --delete specifyweb/frontend/static/ /volumes/static-files/frontend-static
+  cd /opt/specify7
+  echo "Applying Django migrations."
+  ve/bin/python manage.py migrate notifications
+  ve/bin/python manage.py migrate workbench
 fi
 exec "$@"
