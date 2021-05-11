2aa810407d60607ffd73dfe16012fddaf86fb3cb
Maksym Patiiuk <40512816+maxxxxxdlp@users.noreply.github.com>
2021-05-12 02:23:54 +0300

    Safeguard docker container against errors during the migration of notifications
    See #789

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,10 @@ if [ "$1" = 've/bin/gunicorn' ] || [ "$1" = 've/bin/python' ]; then
   rsync -a --delete specifyweb/frontend/static/ /volumes/static-files/frontend-static
   cd /opt/specify7
   echo "Applying Django migrations."
+  set +e
+  # The following command is prone to failing
   ve/bin/python manage.py migrate notifications
+  set -e
   ve/bin/python manage.py migrate workbench
 fi
 exec "$@"
