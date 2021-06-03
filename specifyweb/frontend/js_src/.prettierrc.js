e6fe65a942e30be076a3ee094de8f3ab4a44247c
Maksym Patiiuk <40512816+maxxxxxdlp@users.noreply.github.com>
2021-06-03 17:57:46 +0300

    Update pre-commit hooks
    Add new prettier plugins
    Remove reduntant hooks
    Improve codespell's config

--- specifyweb/frontend/js_src/.prettierrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  // single quote is more popular among JS libraries
-  singleQuote: true,
-  // GitHub renders Markdown properly, so we can wrap it safely
-  proseWrap: 'always',
-};
