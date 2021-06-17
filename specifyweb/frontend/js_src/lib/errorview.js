fd8c79c54197b40334f0d79ed580b9a1e4793b0b
Ben Anhalt <ben.anhalt@gmail.com>
2021-06-17 11:04:01 -0500

    simplify server error dialog.

--- specifyweb/frontend/js_src/lib/errorview.js
+++ specifyweb/frontend/js_src/lib/errorview.js
@@ -20,8 +20,8 @@ module.exports = {
         UnhandledErrorView: Backbone.View.extend({
             __name__: "UnhandledErrorView",
             render: function() {
-                this.$el.attr('title', 'Unexpected Error')
-                    .append('<p>An unexpected error has occurred during communication with the server.</p>');
+                this.$el.attr('title', 'Server Error')
+                    .append('<p>An error has occurred during communication with the server.</p>');
                 var response = this.options.jqxhr.responseText;
                 if (/^Traceback:/m.test(response)) {
                     this.$el.append($('<textarea readonly>').val(response).css({'min-width': 800, 'min-height': 600}));
@@ -31,8 +31,7 @@ module.exports = {
                     width: 'auto',
                     open: function(evt, ui) { $('.ui-dialog-titlebar-close', ui.dialog).hide(); },
                     buttons: [
-                        {text: 'Previous Page', click: function() { window.history.back(); }},
-                        {text: 'Reload Page', click: function() { window.location.reload(); }},
+                        {text: 'Okay', click: function() { window.location = "/"; }},
                     ]
                 });
                 navigation.clearUnloadProtect();
