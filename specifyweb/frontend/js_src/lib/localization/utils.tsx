736bbc24468404bbf1df2734b8288f3bf65ca48e
Max Patiiuk <40512816+maxxxxxdlp@users.noreply.github.com>
2021-07-01 18:33:07 +0300

    Add guidelines for front-end localization

--- specifyweb/frontend/js_src/lib/localization/utils.tsx
+++ specifyweb/frontend/js_src/lib/localization/utils.tsx
@@ -16,3 +16,68 @@ export const createDictionary =
   ): typeof dictionary[typeof key] =>
     key in dictionary ? dictionary[key] : assertExhaustive(key);
 
+/*
+ *
+ * Guidelines for Programmers:
+ *
+ * - All keys must use strict camel case, unless absolutely necessary to do
+ *   otherwise (e.x, in case of "S2N" or other proper nouns that contain
+ *   numbers or capitalized letters)
+ *
+ * - Prefer full terms rather than acronyms or shortened versions.
+ *   Some people may be unfamiliar with the acronyms used. Also, a single term
+ *   may have multiple shortened variants, leading to inconsistencies and bugs.
+ *   Notable exception is the "wb" (Workbench), as it is used extensively and
+ *   does not have conflicting meanings. "ds" (Data Set) would not be a great
+ *   choice as it is not used as widely, and can be confused for other terms
+ *   (Disk Space, Demo Software, Describe Specify, Do Something, etc)
+ *
+ * - Similarly, try to be as consistent as possible in key naming.
+ *   For example, in case of the dialog that appears when upload plan is not
+ *   defined, use noUploadPlanDialogTitle, noUploadPlanDialogHeading,
+ *   noUploadPlanMessageDialogMessage
+ *   for specifying the title, heading and the message of the dialog
+ *   respectively.
+ *
+ * - Each dictionary must be named in camel case and end with "Text" for
+ *   consistency and easy grepping
+ *
+ * - Do not use dynamic references, unless absolutely unavoidable.
+ *   Incorrect example:
+ *   wbText(hasError ?  'errorOccurred' : 'successMessage')
+ *   Correct example:
+ *   hasError ? wbText('errorOccurred') : wbText('successMessage')
+ *   Similarly, don't construct key names dynamically.
+ *   This is needed to simplify finding references of a particular value
+ *   in the code. Also, it would allow to easily find unused values and remove
+ *   them from the dictionary.
+ *
+ * - Each entry may be a string, a JSX Element, or a function that takes
+ *   arbitrary arguments and returns a string or a JSX Element. It is important
+ *   to not that in the case of functions, it must be pure (e.x produce the same
+ *   output given the same output). It should also not relay on any external
+ *   variables as those should be specified as an argument instead.
+ *   Incorrect example:
+ *   newDataSetName: ()=>`New Data Set ${new Date().toDateString()}`,
+ *   Correct example:
+ *   newDataSetName: (date: string)=>`New Data Set ${date}`,
+ *
+ * - When writing multi line strings, keep in mind that some values are
+ *   going to be used in whitespace sensitive contexts. Most common example
+ *   is the "title" attribute of a button. Another example is the cell comment
+ *   text in the workbench. In such cases, use array join instead of the grave
+ *   accent mark.
+ *   Incorrect example:
+ *   someValue: `
+ *     Lorem Ipsum is simply dummy text of the printing and typesetting
+ *     industry. Lorem Ipsum has been the industry's standard dummy text
+ *     ever since the 1500s
+ *   `,
+ *   Correct example:
+ *   someValue: [
+ *     'Lorem Ipsum is simply dummy text of the printing and typesetting',
+ *     'industry. Lorem Ipsum has been the industry's standard dummy text',
+ *     'ever since the 1500s'
+ *   ].join(''),
+ *
+ */
