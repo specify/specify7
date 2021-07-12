9247fcafff2d6d6a6786983992ac7f77a795f198
Ben Anhalt <ben.anhalt@gmail.com>
2021-07-12 12:49:52 -0500

    remove purescript from default.nix.

--- default.nix
+++ default.nix
@@ -1,12 +1,5 @@
 with import <nixpkgs> {};
-let
-  easyPS = import (pkgs.fetchFromGitHub {
-    owner = "justinwoo";
-    repo = "easy-purescript-nix";
-    rev = "aa3e608608232f4a009b5c132ae763fdabfb4aba";
-    sha256 = "0y6jikncxs9l2zgngbd1775f1zy5s1hdc5rhkyzsyaalcl5cajk8";
-  }) {};
-in stdenv.mkDerivation rec {
+stdenv.mkDerivation rec {
   name = "env";
   env = buildEnv { name = name; paths = buildInputs; };
   buildInputs = [
@@ -19,7 +12,5 @@ in stdenv.mkDerivation rec {
     openldap
     cyrus_sasl
     nodejs
-    easyPS.spago
-    easyPS.purs
   ];
 }
