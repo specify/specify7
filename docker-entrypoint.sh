21127d1a9d6487c9a6ed4b545165dc907aae0d59
Ben Anhalt <ben.anhalt@gmail.com>
2021-07-12 17:22:05 -0500

    run manage.py compilemessages during build-backend docker stage.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,7 +8,8 @@ if [ "$1" = 've/bin/gunicorn' ] || [ "$1" = 've/bin/python' ]; then
   echo "Applying Django migrations."
   set +e
   # The following commands are prone to failing
-  # See https://github.com/specify/specify7/issues/789 and https://github.com/specify/docker-compositions/issues/7
+  # See https://github.com/specify/specify7/issues/789
+  # and https://github.com/specify/docker-compositions/issues/7
   ve/bin/python manage.py migrate notifications
   ve/bin/python manage.py migrate workbench
   set -e
