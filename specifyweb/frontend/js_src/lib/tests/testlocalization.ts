76e29ef9b0cfe0b406c5e76c35fe728a05548efc
Max Patiiuk <max@patii.uk>
2021-08-05 16:45:20 +0300

    Make localization tests use modules instead of regex

--- specifyweb/frontend/js_src/lib/tests/testlocalization.ts
+++ specifyweb/frontend/js_src/lib/tests/testlocalization.ts
@@ -9,6 +9,8 @@
 
 import fs from 'fs';
 import path from 'path';
+import type { IR, R } from '../components/wbplanview';
+import type { CompoundValue } from '../localization/utils';
 
 if (typeof process.argv[1] === 'undefined')
   throw new Error('Unable to find the path of the current directory');
@@ -25,6 +27,7 @@ const libraryDirectory = path.join(path.dirname(jsSourceDirectory), 'lib');
 
 // Directory that contains the localization script
 const localizationDirectory = path.join(libraryDirectory, 'localization');
+const compiledLocalizationDirectory = '../localization';
 
 // Directories that contain front-end source code (non-recursively)
 const directoriesToScan = ['./', './components', './templates'];
@@ -44,192 +47,239 @@ function error(value: string): void {
   process.exitCode = 1;
   console.error(`\u001B[31m${value}\u001B[0m\n\n`);
 }
+const reDictionaryName = /export default (?<dictionaryName>\w+);/;
 
 log(`Looking for localization dictionaries in ${localizationDirectory}`);
 
-const reFindDictionary =
-  /const (?<dictionaryName>[a-z][A-Za-z]+Text) = createDictionary/;
-const reFindKeys =
-  /^ {2}(?<keyName>\w+):\s*(?<keyType>\(\s*<|function[ (|]|.)/gm;
 const lookAroundLength = 40;
 
 type Key = {
-  readonly expectsArguments: boolean;
+  readonly value: CompoundValue;
   useCount: number;
 };
 
-type Dictionary = Readonly<Record<string, Key>>;
+type Dictionary = IR<Key>;
 
-const localizationFiles = fs.readdirSync(localizationDirectory);
-const dictionaries = Object.fromEntries(
-  localizationFiles
-    .map<undefined | [string, Dictionary]>((fileName) => {
-      log(`Looking for a dictionary in ${fileName}`);
-      const fileContent = fs
-        .readFileSync(path.join(localizationDirectory, fileName))
-        .toString();
-      const regexMatch = reFindDictionary.exec(fileContent);
-      reFindDictionary.lastIndex = 0;
-      if (regexMatch === null || typeof regexMatch.groups === 'undefined') {
-        warn(`Unable to find a dictionary in ${fileName}`);
-        return undefined;
-      }
-      const dictionaryName = regexMatch.groups.dictionaryName;
-      const trimmedDictionary = fileContent.slice(regexMatch.index);
-      const keys = Object.fromEntries(
-        Array.from(trimmedDictionary.matchAll(reFindKeys), ({ groups }) =>
-          typeof groups === 'undefined'
-            ? undefined
-            : [
-                groups.keyName,
+// This allows to call await at the top level
+(async (): Promise<void> => {
+  const localizationFiles = fs.readdirSync(localizationDirectory);
+  const dictionaries = Object.fromEntries(
+    Array.from(
+      await Promise.all(
+        localizationFiles.map<Promise<undefined | [string, Dictionary]>>(
+          async (fileName) => {
+            const compiledFilePath = path.join(
+              compiledLocalizationDirectory,
+              fileName
+            );
+            const filePathWithoutExtension = compiledFilePath
+              .split('.')
+              .slice(0, -1)
+              .join('.');
+
+            const dictionaryFile = await import(filePathWithoutExtension);
+            const dictionary = dictionaryFile?.default?.dictionary;
+            if (typeof dictionary !== 'object') {
+              warn(`Unable to find a dictionary in ${fileName}`);
+              return undefined;
+            }
+            if (verbose) log(`Found a dictionary in ${fileName}`);
+
+            const filePath = path.join(localizationDirectory, fileName);
+            const fileContent = fs.readFileSync(filePath).toString();
+            const regexMatch = reDictionaryName.exec(fileContent);
+            const dictionaryName = regexMatch?.groups?.dictionaryName;
+
+            if (typeof dictionaryName !== 'string') {
+              error(`Unable to find a dictionary in ${fileName}`);
+              return undefined;
+            }
+
+            if (Object.keys(dictionary).length === 0) {
+              error(
+                `Unable to find any keys in the ${dictionaryName} dictionary`
+              );
+              return undefined;
+            }
+
+            const entries = Object.fromEntries(
+              Object.entries(dictionary).map(([key, value]) => [
+                key,
                 {
-                  expectsArguments:
-                    groups.keyType === 'function(' ||
-                    groups.keyType === 'function ' ||
-                    (groups.keyType === '(' && groups.keyType.length === 1),
+                  value: value as CompoundValue,
                   useCount: 0,
                 },
-              ]
-        ).filter(
-          (content): content is [string, Key] => typeof content !== 'undefined'
+              ])
+            );
+
+            return [dictionaryName, entries];
+          }
         )
-      );
-
-      if (Object.keys(keys).length === 0) {
-        error(`Unable to find any keys in the ${dictionaryName} dictionary`);
-        return undefined;
-      }
-
-      return [dictionaryName, keys];
-    })
-    .filter(
+      )
+    ).filter(
       (content): content is [string, Dictionary] =>
         typeof content !== 'undefined'
     )
-);
-
-if (Object.keys(dictionaries).length === 0)
-  error('Unable to find any localization dictionaries');
-
-const sourceFiles = directoriesToScan
-  .flatMap((directoryName) =>
-    fs
-      .readdirSync(path.join(libraryDirectory, directoryName))
-      .map((fileName) => path.join(directoryName, fileName))
-  )
-  .filter((fileName) =>
-    ['js', 'jsx', 'ts', 'tsx', 'html'].includes(
-      fileName.split('.').slice(-1)[0]
-    )
   );
 
-sourceFiles.forEach((fileName) => {
-  if (verbose) log(`Looking for language string usages in ${fileName}`);
+  if (Object.keys(dictionaries).length === 0)
+    error('Unable to find any localization dictionaries');
 
-  const fileContent = fs
-    .readFileSync(path.join(libraryDirectory, fileName))
-    .toString();
-  let foundUsages = false;
-
-  Object.entries(dictionaries).forEach(([dictionaryName, keys]) => {
-    const regex = new RegExp(
-      `${dictionaryName}\\s*\\(\\s*(?<keyName>[^)]+)\\s*\\)\\s*(?<hasArguments>\\()?`,
-      'g'
+  const sourceFiles = directoriesToScan
+    .flatMap((directoryName) =>
+      fs
+        .readdirSync(path.join(libraryDirectory, directoryName))
+        .map((fileName) => path.join(directoryName, fileName))
+    )
+    .filter((fileName) =>
+      ['js', 'jsx', 'ts', 'tsx', 'html'].includes(
+        fileName.split('.').slice(-1)[0]
+      )
     );
 
-    Array.from(fileContent.matchAll(regex)).forEach(({ groups, index }) => {
-      if (typeof groups === 'undefined' || typeof index === 'undefined') return;
+  sourceFiles.forEach((fileName) => {
+    if (verbose) log(`Looking for language string usages in ${fileName}`);
 
-      const hasArguments = typeof groups.hasArguments !== 'undefined';
+    const fileContent = fs
+      .readFileSync(path.join(libraryDirectory, fileName))
+      .toString();
+    let foundUsages = false;
 
-      const paddedKeyName = groups.keyName.trim();
-      const keyName = paddedKeyName.slice(1, -1).trim();
-
-      const position = fileContent.slice(
-        index - lookAroundLength,
-        index + lookAroundLength + dictionaryName.length + keyName.length
+    Object.entries(dictionaries).forEach(([dictionaryName, entries]) => {
+      const regex = new RegExp(
+        `${dictionaryName}\\s*\\(\\s*(?<keyName>[^)]+)\\s*\\)\\s*(?<hasArguments>\\()?`,
+        'g'
       );
-      const lineNumber = (fileContent.slice(0, index).match(/\n/g) ?? [])
-        .length;
 
-      if (
-        !`'"\``.includes(paddedKeyName[0]) ||
-        !paddedKeyName.startsWith(paddedKeyName.slice(-1)[0])
-      ) {
-        error(
-          [
-            `Found invalid key ${dictionaryName}[${paddedKeyName}] in ${fileName}\n`,
-            `Key must be a string literal, not a variable or function call.\n`,
-            `\n`,
-            `On line ${lineNumber}:\n`,
-            `${position}`,
-          ].join('')
+      Array.from(fileContent.matchAll(regex)).forEach(({ groups, index }) => {
+        if (typeof groups === 'undefined' || typeof index === 'undefined')
+          return;
+
+        const hasArguments = typeof groups.hasArguments !== 'undefined';
+
+        const paddedKeyName = groups.keyName.trim();
+        const keyName = paddedKeyName.slice(1, -1).trim();
+
+        const position = fileContent.slice(
+          index - lookAroundLength,
+          index + lookAroundLength + dictionaryName.length + keyName.length
         );
-        return;
-      }
+        const lineNumber = (fileContent.slice(0, index).match(/\n/g) ?? [])
+          .length;
 
-      if (!(keyName in keys)) {
-        error(
-          [
-            `Found unknown key ${dictionaryName}.${keyName} in ${fileName}\n`,
-            `\n`,
-            `On line ${lineNumber}:\n`,
-            `${position}`,
-          ].join('')
-        );
-        return;
-      }
-
-      if (keys[keyName].expectsArguments !== hasArguments) {
-        if (hasArguments)
+        if (
+          !`'"\``.includes(paddedKeyName[0]) ||
+          !paddedKeyName.startsWith(paddedKeyName.slice(-1)[0])
+        ) {
           error(
             [
-              `${fileName} provides arguments to ${dictionaryName}.${keyName} `,
-              `but it is not supposed to.\n`,
+              `Found invalid key ${dictionaryName}[${paddedKeyName}] in ${fileName}\n`,
+              `Key must be a string literal, not a variable or function call.\n`,
               `\n`,
               `On line ${lineNumber}:\n`,
               `${position}`,
             ].join('')
           );
-        else
+          return;
+        }
+
+        if (!(keyName in entries)) {
           error(
             [
-              `${fileName} does not provide arguments to `,
-              `${dictionaryName}.${keyName} but it is supposed to.\n`,
+              `Found unknown key ${dictionaryName}.${keyName} in ${fileName}\n`,
               `\n`,
               `On line ${lineNumber}:\n`,
               `${position}`,
             ].join('')
           );
-      }
+          return;
+        }
 
-      keys[keyName].useCount += 1;
-      foundUsages = true;
+        const expectsArguments = typeof entries[keyName].value === 'function';
+        if (expectsArguments !== hasArguments) {
+          if (hasArguments)
+            error(
+              [
+                `${fileName} provides arguments to ${dictionaryName}.${keyName} `,
+                `but it is not supposed to.\n`,
+                `\n`,
+                `On line ${lineNumber}:\n`,
+                `${position}`,
+              ].join('')
+            );
+          else
+            error(
+              [
+                `${fileName} does not provide arguments to `,
+                `${dictionaryName}.${keyName} but it is supposed to.\n`,
+                `\n`,
+                `On line ${lineNumber}:\n`,
+                `${position}`,
+              ].join('')
+            );
+        }
+
+        entries[keyName].useCount += 1;
+        foundUsages = true;
+      });
     });
+
+    if (!foundUsages && verbose) log(`Didn't find any usages in ${fileName}`);
   });
 
-  if (!foundUsages && verbose) log(`Didn't find any usages in ${fileName}`);
-});
+  // Find duplicate values
+  const compoundDictionaries = Object.entries(dictionaries).reduce<
+    R<(readonly [fileName: string, key: string])[]>
+  >((compoundDictionaries, [fileName, entries]) => {
+    Object.entries(entries).forEach(([key, { value }]) => {
+      const valueString =
+        typeof value === 'string'
+          ? value
+          : typeof value === 'object'
+          ? JSON.stringify(value)
+          : value.toString();
+      compoundDictionaries[valueString] ??= [];
+      compoundDictionaries[valueString].push([fileName, key]);
+    });
+    return compoundDictionaries;
+  }, {});
+  Object.entries(compoundDictionaries)
+    .filter(([_valueString, instances]) => instances.length > 1)
+    .forEach(([valueString, instances]) => {
+      warn(
+        [
+          'Multiple instances of the same value were found in:\n',
+          ...instances.map(
+            ([fileName, key]) => `\t"${fileName}" under key "${key}"\n`
+          ),
+          'Value:\n',
+          valueString,
+        ].join('')
+      );
+    });
 
-Object.entries(dictionaries).forEach(([dictionaryName, keys]) =>
-  Object.entries(keys)
-    .filter(([_keyName, { useCount }]) => useCount === 0)
-    .forEach(([keyName]) =>
-      error(`No usages of ${dictionaryName}.${keyName} found`)
-    )
-);
-
-if (verbose) {
-  log(dictionaries);
+  // Unused key errors
   Object.entries(dictionaries).forEach(([dictionaryName, keys]) =>
-    log(
-      `${dictionaryName} has ${
-        Object.keys(keys).length
-      } keys with a total use count of ${Object.values(keys)
-        .map(({ useCount }) => useCount)
-        .reduce((total, useCount) => total + useCount, 0)}`
-    )
+    Object.entries(keys)
+      .filter(([_keyName, { useCount }]) => useCount === 0)
+      .forEach(([keyName]) =>
+        error(`No usages of ${dictionaryName}.${keyName} found`)
+      )
   );
-}
-warn(`Warnings: ${warningsCount}`);
-warn(`Errors: ${errorsCount}`);
+
+  // Output stats
+  if (verbose) {
+    log(dictionaries);
+    Object.entries(dictionaries).forEach(([dictionaryName, keys]) =>
+      log(
+        `${dictionaryName} has ${
+          Object.keys(keys).length
+        } keys with a total use count of ${Object.values(keys)
+          .map(({ useCount }) => useCount)
+          .reduce((total, useCount) => total + useCount, 0)}`
+      )
+    );
+  }
+  warn(`Warnings: ${warningsCount}`);
+  warn(`Errors: ${errorsCount}`);
+})();
