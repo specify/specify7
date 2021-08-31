226c3e4dfb66d5bbfbd85b0bc1990f75c53ea46b
Max Patiiuk <max@patii.uk>
2021-08-31 09:52:43 -0500

    Show error message if user has no agent
    Fixes #349

--- specifyweb/frontend/js_src/lib/userinfo.js
+++ specifyweb/frontend/js_src/lib/userinfo.js
@@ -2,12 +2,36 @@
 
 var _              = require('underscore');
 var initialContext = require('./initialcontext.js');
+const commonText = require('./localization/common').default;
+const $ = require('jquery');
 
     var userInfo = {};
     initialContext.load('user.json', function(data) {
         _.extend(userInfo, data, {
-            isReadOnly:  !_(['Manager', 'FullAccess']).contains(data.usertype)
+            isReadOnly: !_(['Manager', 'FullAccess']).contains(data.usertype)
         });
+    }, (error)=>{
+        if(error.status === 400){
+            const dialog = $(`<div>
+                ${commonText('noAgentDialogHeader')}
+                <p>${commonText('noAgentDialogMessage')}</p>
+            </div>`).dialog({
+                modal: true,
+                dialogClass: 'ui-dialog-persistent',
+                title: commonText('noAgentDialogTitle'),
+                close: ()=>{
+                    window.location.href = '/accounts/logout/';
+                },
+                buttons: [
+                    {
+                        text: commonText('logOut'),
+                        click: ()=>dialog.dialog('close'),
+                    }
+                ]
+            });
+            dialog[0].classList.add('ui-dialog-persistent');
+        }
+        else throw new Error(error);
     });
 
 module.exports = userInfo;
