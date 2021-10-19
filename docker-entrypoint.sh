c6628756a7375b74487dadc0c721db5666b0d1d9
Max Patiiuk <max@patii.uk>
2021-10-19 17:11:50 +0300

    Prevent django migrations from crashing containers
    Fixes specify/docker-compositions#7

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,9 +7,10 @@ if [ "$1" = 've/bin/gunicorn' ] || [ "$1" = 've/bin/python' ]; then
   cd /opt/specify7
   echo "Applying Django migrations."
   set +e
-  # The following command is prone to failing
+  # The following commands are prone to failing
+  # See https://github.com/specify/specify7/issues/789 and https://github.com/specify/docker-compositions/issues/7
   ve/bin/python manage.py migrate notifications
-  set -e
   ve/bin/python manage.py migrate workbench
+  set -e
 fi
 exec "$@"
