6d6b491ccc19e1a8203d03e5e62af7648c9106c0
Max Patiiuk <max@patii.uk>
2021-10-26 09:56:37 -0500

    Increase max data set size to 400mb

--- specifyweb/settings/__init__.py
+++ specifyweb/settings/__init__.py
@@ -224,6 +224,9 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 JAVA_PATH = '/usr/bin/java'
 
+DATA_UPLOAD_MAX_MEMORY_SIZE = 419430400  # 300mb
+FILE_UPLOAD_MAX_MEMORY_SIZE = 104857600  # 100mb
+
 try:
     from .local_logging_settings import LOGGING
 except ImportError:
