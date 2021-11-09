f64828ac3d00df7e0de1ab3e8993d20c76aa353c
Max Patiiuk <max@patii.uk>
2021-11-09 10:44:10 -0600

    Turn buttons in the forms dialog into links
    See #975

--- specifyweb/frontend/js_src/lib/formsdialog.js
+++ specifyweb/frontend/js_src/lib/formsdialog.js
@@ -32,7 +32,7 @@ module.exports = Backbone.View.extend({
         __name__: "FormsDialog",
         tagName: 'nav',
         className: "forms-dialog",
-        events: {'click button': 'selected'},
+        events: {'click a': 'handleClick'},
         render: function() {
             var render = this._render.bind(this);
             if(getFormsPromise.isFulfilled()) {
@@ -53,9 +53,12 @@ module.exports = Backbone.View.extend({
             }
             return this;
         },
-        _render: function() {
-            var entries = _.map(views, this.dialogEntry, this);
-            $('<ul>').css('padding',0).append(entries).appendTo(this.el);
+        _render: function(forms) {
+            this.forms = forms;
+            $('<ul>')
+                .css('padding',0)
+                .append(views.map((view,index)=>this.dialogEntry(view, index)))
+            .appendTo(this.el);
             this.$el.dialog({
                 title: formsText('formsDialogTitle'),
                 maxHeight: 400,
@@ -68,11 +71,16 @@ module.exports = Backbone.View.extend({
             });
             return this;
         },
-        dialogEntry: function(view) {
+        dialogEntry: function(view,index) {
+            const form = this.forms[index];
+            const modelName = form['class'].split('.').pop();
+            const model = schema.getModel(modelName);
             return $('<li>').append(
-                $('<button>')
-                    .addClass("fake-link")
+                $('<a>')
+                    .addClass("fake-link intercept-navigation")
+                    .attr('href', new model.Resource().viewUrl())
                     .css({fontSize: '0.8rem'})
+                    .attr('data-model-name', modelName)
                     .append(
                         $(
                             '<img>',
@@ -87,14 +95,13 @@ module.exports = Backbone.View.extend({
                     )
             )[0];
         },
-        selected: function(evt) {
-            var index = this.$('button').index(evt.currentTarget);
-            this.$el.dialog('close');
-            getFormsPromise.done(forms => {
-                var form = forms[index];
-                var model = schema.getModel(form['class'].split('.').pop());
-                this.trigger('selected', model);
-            });
-        }
+        handleClick(event){
+            if(typeof this.options.onSelected === 'undefined')
+                return;
+            event.preventDefault();
+            const modelName = event.target.getAttribute('data-model-name');
+            const model = schema.getModel(modelName);
+            this.options.onSelected(model);
+        },
     });
 
