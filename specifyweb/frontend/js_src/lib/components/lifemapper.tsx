f25c0e173a03292e3e510e31a8e0a3c2602153a4
Max Patiiuk <max@patii.uk>
2021-11-24 14:42:55 -0600

    Use the long version of the Specify Network logo

--- specifyweb/frontend/js_src/lib/components/lifemapper.tsx
+++ specifyweb/frontend/js_src/lib/components/lifemapper.tsx
@@ -193,10 +193,7 @@ export function SpecifyNetworkBadge({
           window.addEventListener('message', messageHandler);
         }}
       >
-        <img
-          src="/static/img/specify_network_box_only.svg"
-          alt="Specify Network"
-        />
+        <img src="/static/img/specify_network_logo_long.svg" alt="" />
       </a>
     </>
   );
