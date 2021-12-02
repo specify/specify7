specify/specify7

commit 6b19378e63d5089a23c117bb6bbcf4d71d433d49

Make Forms and Interactions dialogs open faster
Fixes #593

specifyweb/frontend/js_src/lib/formsdialog.js
@@ -15,15 +15,18 @@ const formsText = require('./localization/forms').default;
 
 
     // I don't think the non-sidebar items are ever used in Sp6.
-    var views;
+    let views;
+    let forms;
+    let getFormsPromise;
     initialContext.load(
         'app.resource?name=DataEntryTaskInit',
-        data => views = _.map($('view', data), $).filter(view => view.attr('sidebar') === 'true'));
-
-    function getFormsPromise() {
-        return Q.all(views.map(
-            view => specifyform.getView(view.attr('view')).pipe(form => form)));
-    }
+        data => {
+            views = _.map($('view', data), $).filter(view => view.attr('sidebar') === 'true');
+            getFormsPromise = Q.all(views.map(
+                view => specifyform.getView(view.attr('view')).pipe(form => form))
+            );
+        }
+    );
 
 module.exports = Backbone.View.extend({
         __name__: "FormsDialog",
@@ -31,7 +34,24 @@ module.exports = Backbone.View.extend({
         events: {'click a': 'selected'},
         render: function() {
             var render = this._render.bind(this);
-            getFormsPromise().done(render);
+            if(typeof getFormsPromise === 'undefined')
+              render();
+            else {
+              const loadingDialog = $(
+                  '<div><div class="progress-bar"></div></div>'
+              ).dialog({
+                  title: commonText('loading'),
+                  modal: true,
+                  dialogClass: 'ui-dialog-no-close',
+              });
+              $('.progress-bar', loadingDialog).progressbar({ value: false });
+              getFormsPromise.done(fetchedForms=>{
+                forms=fetchedForms;
+                getFormsPromise = undefined;
+                loadingDialog.dialog('destroy');
+                render();
+              });
+            }
             return this;
         },
         _render: function(forms) {

specifyweb/frontend/js_src/lib/interactionsdialog.js
@@ -17,7 +17,7 @@ var reports           = require('./reports.js');
 const formsText = require('./localization/forms').default;
 const commonText = require('./localization/common').default;
 
-    var interaction_entries, views, actions;
+    var interaction_entries, views, actions, forms, getFormsPromise;
 
     initialContext.load('app.resource?name=InteractionsTaskInit', function (data) {
         interaction_entries = _.map($('entry', data), $)
@@ -28,6 +28,10 @@ const commonText = require('./localization/common').default;
         actions = interaction_entries.filter(isActionEntry);
 
         actions.forEach(actionEntry => actionEntry.table = getTableForObjToCreate(actionEntry));
+
+        getFormsPromise = Q.all(views.map(
+          view => Q(specifyform.getView(view.attr('view'))).then(form => form)
+        ));
     });
 
     function getTableForObjToCreate(action) {
@@ -46,9 +50,6 @@ const commonText = require('./localization/common').default;
         return actionAttr && actionAttr != 'OpenNewView' && actionAttr != 'NEW_ACC';
     }
 
-    function getFormsPromise() {
-        return Q.all(views.map(view => Q(specifyform.getView(view.attr('view'))).then(form => form)));
-    }
 
 module.exports = Backbone.View.extend({
         __name__: "InteractionsDialog",
@@ -59,7 +60,24 @@ module.exports = Backbone.View.extend({
         },
         render: function() {
             var render = this._render.bind(this);
-            getFormsPromise().done(render);
+            if(typeof getFormsPromise === 'undefined')
+              render();
+            else {
+              const loadingDialog = $(
+                  '<div><div class="progress-bar"></div></div>'
+              ).dialog({
+                  title: commonText('loading'),
+                  modal: true,
+                  dialogClass: 'ui-dialog-no-close',
+              });
+              $('.progress-bar', loadingDialog).progressbar({ value: false });
+              getFormsPromise.done(fetchedForms=>{
+                forms=fetchedForms;
+                getFormsPromise = undefined;
+                loadingDialog.dialog('destroy');
+                render();
+              });
+            }
             return this;
         },
         _render: function(forms) {

commit 27739575be5254593cd8b57b54a64185e237cdef

fix regression introduced by pr #1088.

specifyweb/frontend/js_src/lib/formsdialog.js
@@ -34,9 +34,9 @@ module.exports = Backbone.View.extend({
         events: {'click a': 'selected'},
         render: function() {
             var render = this._render.bind(this);
-            if(typeof getFormsPromise === 'undefined')
-              render();
-            else {
+            if(getFormsPromise.isFulfilled()) {
+                this._render();
+            } else {
               const loadingDialog = $(
                   '<div><div class="progress-bar"></div></div>'
               ).dialog({
@@ -46,16 +46,13 @@ module.exports = Backbone.View.extend({
               });
               $('.progress-bar', loadingDialog).progressbar({ value: false });
               getFormsPromise.done(fetchedForms=>{
-                forms=fetchedForms;
-                getFormsPromise = undefined;
                 loadingDialog.dialog('destroy');
-                render();
+                this._render();
               });
             }
             return this;
         },
-        _render: function(forms) {
-            this.forms = forms;
+        _render: function() {
             var entries = _.map(views, this.dialogEntry, this);
             $('<table>').append(entries).appendTo(this.el);
             this.$el.dialog({
@@ -78,9 +75,11 @@ module.exports = Backbone.View.extend({
         selected: function(evt) {
             var index = this.$('a').index(evt.currentTarget);
             this.$el.dialog('close');
-            var form = this.forms[index];
-            var model = schema.getModel(form['class'].split('.').pop());
-            this.trigger('selected', model);
+            getFormsPromise.done(forms => {
+                var form = forms[index];
+                var model = schema.getModel(form['class'].split('.').pop());
+                this.trigger('selected', model);
+            });
         }
     });
 

specifyweb/frontend/js_src/lib/interactionsdialog.js
@@ -59,10 +59,9 @@ module.exports = Backbone.View.extend({
             'click a.interaction-action': 'interactionActionClick'
         },
         render: function() {
-            var render = this._render.bind(this);
-            if(typeof getFormsPromise === 'undefined')
-              render();
-            else {
+            if(getFormsPromise.isFulfilled()) {
+                this._render();
+            } else {
               const loadingDialog = $(
                   '<div><div class="progress-bar"></div></div>'
               ).dialog({
@@ -72,10 +71,8 @@ module.exports = Backbone.View.extend({
               });
               $('.progress-bar', loadingDialog).progressbar({ value: false });
               getFormsPromise.done(fetchedForms=>{
-                this.forms = fetchedForms;
-                getFormsPromise = undefined;
                 loadingDialog.dialog('destroy');
-                render();
+                this._render();
               });
             }
             return this;
@@ -125,10 +122,12 @@ module.exports = Backbone.View.extend({
         },
         selected: function(evt) {
             var index = this.$('a').filter(".intercept-navigation").index(evt.currentTarget);
-            this.$el.dialog('close');
-            var form = this.forms[index];
-            var model = schema.getModel(form['class'].split('.').pop());
-            this.trigger('selected', model);
+            getFormsPromise.done(forms => {
+                this.$el.dialog('close');
+                var form = forms[index];
+                var model = schema.getModel(form['class'].split('.').pop());
+                this.trigger('selected', model);
+            });
         },
         isRsAction: function(actionName) {
             return actionName == 'NEW_GIFT' || actionName == 'NEW_LOAN';