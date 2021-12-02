specify/specify7

commit 6b19378e63d5089a23c117bb6bbcf4d71d433d49

Make Forms and Interactions dialogs open faster
Fixes #593

specifyweb/frontend/js_src/lib/interactionsdialog.js
@@ -17,7 +17,7 @@ var reports           = require('./reports.js');
 const formsText = require('./localization/forms').default;
 const commonText = require('./localization/common').default;
 
-    var interaction_entries, views, actions;
+    var interaction_entries, views, actions, forms, getFormsPromise;
 
     initialContext.load('app.resource?name=InteractionsTaskInit', function (data) {
         interaction_entries = _.map($('entry', data), $)
@@ -28,6 +28,10 @@ const commonText = require('./localization/common').default;
         actions = interaction_entries.filter(isActionEntry);
 
         actions.forEach(actionEntry => actionEntry.table = getTableForObjToCreate(actionEntry));
+
+        getFormsPromise = Q.all(views.map(
+          view => Q(specifyform.getView(view.attr('view'))).then(form => form)
+        ));
     });
 
     function getTableForObjToCreate(action) {
@@ -46,9 +50,6 @@ const commonText = require('./localization/common').default;
         return actionAttr && actionAttr != 'OpenNewView' && actionAttr != 'NEW_ACC';
     }
 
-    function getFormsPromise() {
-        return Q.all(views.map(view => Q(specifyform.getView(view.attr('view'))).then(form => form)));
-    }
 
 module.exports = Backbone.View.extend({
         __name__: "InteractionsDialog",
@@ -59,7 +60,24 @@ module.exports = Backbone.View.extend({
         },
         render: function() {
             var render = this._render.bind(this);
-            getFormsPromise().done(render);
+            if(typeof getFormsPromise === 'undefined')
+              render();
+            else {
+              const loadingDialog = $(
+                  '<div><div class="progress-bar"></div></div>'
+              ).dialog({
+                  title: commonText('loading'),
+                  modal: true,
+                  dialogClass: 'ui-dialog-no-close',
+              });
+              $('.progress-bar', loadingDialog).progressbar({ value: false });
+              getFormsPromise.done(fetchedForms=>{
+                forms=fetchedForms;
+                getFormsPromise = undefined;
+                loadingDialog.dialog('destroy');
+                render();
+              });
+            }
             return this;
         },
         _render: function(forms) {

specifyweb/frontend/js_src/lib/formsdialog.js
@@ -15,15 +15,18 @@ const formsText = require('./localization/forms').default;
 
 
     // I don't think the non-sidebar items are ever used in Sp6.
-    var views;
+    let views;
+    let forms;
+    let getFormsPromise;
     initialContext.load(
         'app.resource?name=DataEntryTaskInit',
-        data => views = _.map($('view', data), $).filter(view => view.attr('sidebar') === 'true'));
-
-    function getFormsPromise() {
-        return Q.all(views.map(
-            view => specifyform.getView(view.attr('view')).pipe(form => form)));
-    }
+        data => {
+            views = _.map($('view', data), $).filter(view => view.attr('sidebar') === 'true');
+            getFormsPromise = Q.all(views.map(
+                view => specifyform.getView(view.attr('view')).pipe(form => form))
+            );
+        }
+    );
 
 module.exports = Backbone.View.extend({
         __name__: "FormsDialog",
@@ -31,7 +34,24 @@ module.exports = Backbone.View.extend({
         events: {'click a': 'selected'},
         render: function() {
             var render = this._render.bind(this);
-            getFormsPromise().done(render);
+            if(typeof getFormsPromise === 'undefined')
+              render();
+            else {
+              const loadingDialog = $(
+                  '<div><div class="progress-bar"></div></div>'
+              ).dialog({
+                  title: commonText('loading'),
+                  modal: true,
+                  dialogClass: 'ui-dialog-no-close',
+              });
+              $('.progress-bar', loadingDialog).progressbar({ value: false });
+              getFormsPromise.done(fetchedForms=>{
+                forms=fetchedForms;
+                getFormsPromise = undefined;
+                loadingDialog.dialog('destroy');
+                render();
+              });
+            }
             return this;
         },
         _render: function(forms) {

commit 3d12d24ac3f36972c621c45c73bac6d9ae980408

Fix Interactions dialog not storing fetched forms.
Likely a regression introduced by PR #1088

specifyweb/frontend/js_src/lib/interactionsdialog.js
@@ -72,7 +72,7 @@ module.exports = Backbone.View.extend({
               });
               $('.progress-bar', loadingDialog).progressbar({ value: false });
               getFormsPromise.done(fetchedForms=>{
-                forms=fetchedForms;
+                this.forms = fetchedForms;
                 getFormsPromise = undefined;
                 loadingDialog.dialog('destroy');
                 render();
@@ -80,8 +80,7 @@ module.exports = Backbone.View.extend({
             }
             return this;
         },
-        _render: function(forms) {
-            this.forms = forms;
+        _render: function() {
             var entries = _.map(interaction_entries, this.dialogEntry, this);
             $('<table>').append(entries).appendTo(this.el);
             this.$el.dialog({