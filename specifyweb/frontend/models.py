eea4efac8648f3c409e43b5afe94f7f6470407d9
Ben Anhalt <ben.anhalt@gmail.com>
2021-12-03 16:25:38 -0600

    oauth prototype.

--- specifyweb/frontend/models.py
+++ specifyweb/frontend/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from specifyweb.specify import models as spmodels
+Specifyuser = getattr(spmodels, 'Specifyuser')
+
+class Spuserexternalid(models.Model):
+    provider = models.CharField(max_length=256)
+    providerid = models.CharField(max_length=256)
+    specifyuser = models.ForeignKey(Specifyuser, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "spuserexternalid"
+        constraints = [
+            models.UniqueConstraint(fields=["provider", "providerid"], name="unique_spuser_external_id")
+        ]
