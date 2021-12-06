1c10c3096ab9798da27a8d95f6db3affb2f5a66e
Max Patiiuk <max@patii.uk>
2021-12-06 10:44:11 -0600

    Add Schema Config State reducer

--- specifyweb/frontend/js_src/lib/schemaconfigreducer.ts
+++ specifyweb/frontend/js_src/lib/schemaconfigreducer.ts
@@ -1,7 +1,10 @@
 import type { Action } from 'typesafe-reducer';
 import { generateReducer } from 'typesafe-reducer';
+import { SpLocaleItem } from './components/schemaconfig';
+import { mainState } from './components/schemaconfigstate';
 
 import type { States } from './components/schemaconfigstate';
+import { RA } from './components/wbplanview';
 
 type ChooseLanguageAction = Action<
   'ChooseLanguageAction',
@@ -10,11 +13,57 @@ type ChooseLanguageAction = Action<
   }
 >;
 
-export type Actions = ChooseLanguageAction;
+type ChangeLanguageAction = Action<'ChangeLanguageAction'>;
+
+type ChangeTableAction = Action<
+  'ChangeTableAction',
+  {
+    tableId: number;
+  }
+>;
+
+type SetTableItemsAction = Action<
+  'SetTableItemsAction',
+  {
+    items: RA<SpLocaleItem>;
+  }
+>;
+
+type ChangeItemAction = Action<
+  'ChangeItemAction',
+  {
+    itemId: number;
+  }
+>;
+
+export type Actions =
+  | ChooseLanguageAction
+  | ChangeLanguageAction
+  | ChangeTableAction
+  | SetTableItemsAction
+  | ChangeItemAction;
 
 export const reducer = generateReducer<States, Actions>({
   ChooseLanguageAction: ({ action: { language } }) => ({
     type: 'MainState',
     language,
+  }),
+  ChangeLanguageAction: () => ({
+    type: 'DialogState',
+  }),
+  ChangeTableAction: ({ action: { tableId }, state }) => ({
+    ...mainState(state),
+    tableId,
+    items: undefined,
+    item: undefined,
+  }),
+  SetTableItemsAction: ({ action: { items }, state }) => ({
+    ...mainState(state),
+    items,
+    item: items[0]?.id,
+  }),
+  ChangeItemAction: ({ action: { itemId }, state }) => ({
+    ...mainState(state),
+    itemId,
   }),
 });
