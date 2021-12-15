b2c982506314cf3d75ff1f6881e0f42413d9ae88
Ben Anhalt <ben.anhalt@gmail.com>
2021-12-15 09:59:47 -0600

    don't cache schema config on the server.
    This allows schema changes to be visible with just a browser cache refresh.

--- specifyweb/context/schema_localization.py
+++ specifyweb/context/schema_localization.py
@@ -4,27 +4,61 @@ import json
 from django.conf import settings
 from django.db import connection
 
-from specifyweb.specify.models import (
-    Splocalecontainer as Container,
-    Splocalecontaineritem as Item,
-    Splocaleitemstr as SpString)
 
-schema_localization_cache = {}
-
-def get_schema_localization(collection, schematype):
+def get_schema_localization(collection, schematype, lang=settings.SCHEMA_LANGUAGE):
     disc = collection.discipline
-    if (disc, schematype) in schema_localization_cache:
-        return schema_localization_cache[(disc, schematype)]
-
-    lang = settings.SCHEMA_LANGUAGE
 
     cursor = connection.cursor()
+
+    # # It's possible to generate the json in the DB as follows:
+
+    # cursor.execute("""
+    # select json_objectagg(
+    #   name, json_object(
+    #     'format', format,
+    #     'ishidden', ishidden != 0,
+    #     'isuiformatter', isuiformatter != 0,
+    #     'picklistname', picklistname,
+    #     'type', type,
+    #     'aggregator', aggregator,
+    #     'defaultui', defaultui,
+    #     'name', n.text,
+    #     'desc', d.text,
+    #     'items', json_merge('{}', items.items)
+    # ))
+    # from splocalecontainer
+    # left outer join (
+    #        select splocalecontainerid, json_objectagg(
+    #        lower(name), json_object(
+    #          'format', format,
+    #          'ishidden', ishidden != 0,
+    #          'isuiformatter', isuiformatter != 0,
+    #          'picklistname', picklistname,
+    #          'type', type,
+    #          'isrequired', isrequired != 0,
+    #          'weblinkname', weblinkname,
+    #          'name', n.text,
+    #          'desc', d.text
+    #       )) as items
+    #       from splocalecontaineritem item
+    #       left outer join splocaleitemstr n on n.splocalecontaineritemnameid = item.splocalecontaineritemid and n.language = %s
+    #       left outer join splocaleitemstr d on d.splocalecontaineritemdescid = item.splocalecontaineritemid and d.language = %s
+    #       group by splocalecontainerid
+    # ) items using (splocalecontainerid)
+    # left outer join splocaleitemstr n on n.splocalecontainernameid = splocalecontainerid and n.language = %s
+    # left outer join splocaleitemstr d on d.splocalecontainerdescid = splocalecontainerid and d.language = %s
+    # where schematype = %s and disciplineid = %s
+    # """, [lang, lang, lang, lang, schematype, disc.id])
+
+    # return cursor.fetchone()[0]
+
     cursor.execute("""
-    select name, format, ishidden!=0, isuiformatter, picklistname, type, aggregator, defaultui, n.text, d.text
+    select name, format, ishidden, isuiformatter, picklistname, type, aggregator, defaultui, n.text, d.text
     from splocalecontainer
     left outer join splocaleitemstr n on n.splocalecontainernameid = splocalecontainerid and n.language = %s
     left outer join splocaleitemstr d on d.splocalecontainerdescid = splocalecontainerid and d.language = %s
-    where schematype = %s and disciplineid = %s;
+    where schematype = %s and disciplineid = %s
+    order by name
     """, [lang, lang, schematype, disc.id])
 
     cfields = ('format', 'ishidden', 'isuiformatter', 'picklistname', 'type', 'aggregator', 'defaultui', 'name', 'desc')
@@ -36,13 +70,14 @@ def get_schema_localization(collection, schematype):
 
     cursor.execute("""
     select container.name, item.name,
-           item.format, item.ishidden!=0, item.isuiformatter, item.picklistname,
+           item.format, item.ishidden, item.isuiformatter, item.picklistname,
            item.type, item.isrequired, item.weblinkname, n.text, d.text
     from splocalecontainer container
     inner join splocalecontaineritem item on item.splocalecontainerid = container.splocalecontainerid
     left outer join splocaleitemstr n on n.splocalecontaineritemnameid = item.splocalecontaineritemid and n.language = %s
     left outer join splocaleitemstr d on d.splocalecontaineritemdescid = item.splocalecontaineritemid and d.language = %s
-    where schematype = %s and disciplineid = %s;
+    where schematype = %s and disciplineid = %s
+    order by item.name
     """, [lang, lang, schematype, disc.id])
 
     ifields = ('format', 'ishidden', 'isuiformatter', 'picklistname', 'type', 'isrequired', 'weblinkname', 'name', 'desc')
@@ -50,6 +85,5 @@ def get_schema_localization(collection, schematype):
     for row in cursor.fetchall():
         containers[row[0]]['items'][row[1].lower()] = {field: row[i+2] for i, field in enumerate(ifields)}
 
-    sl = schema_localization_cache[(disc, schematype)] = json.dumps(containers)
-    return sl
+    return json.dumps(containers)
 
