a525a4eed2ca8443abd68786f92e48b621e4e1d3
Ben Anhalt <ben.anhalt@gmail.com>
2021-12-22 16:14:18 -0600

    some documentation.

--- specifyweb/accounts/urls.py
+++ specifyweb/accounts/urls.py
@@ -6,14 +6,23 @@ from . import views
 
 urlpatterns = [
     path('login/', views.oic_login),
+
+    # Login with Specify username and password:
     path('legacy_login/', auth_views.LoginView.as_view(template_name='login.html')),
+
+    # OpenId Connect callback endpoint:
     path('oic_callback/', views.oic_callback),
+
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html', next_page='/accounts/login/')),
     path('password_change/', auth_views.PasswordChangeView.as_view(
         template_name='password_change.html', success_url='/')),
 
     path('support_login/', views.support_login),
-    path('invite_link/<int:userid>/', views.invite_link),
-    path('use_invite_link/', views.use_invite_link),
     path('choose_collection/', views.choose_collection),
+
+    # API endpoint to generate an invite link:
+    path('invite_link/<int:userid>/', views.invite_link),
+
+    # Invite links point here:
+    path('use_invite_link/', views.use_invite_link),
 ]
