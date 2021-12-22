84c4b75f85523072b0ac5da872d6860dba471082
Ben Anhalt <ben.anhalt@gmail.com>
2021-12-22 16:14:18 -0600

    some documentation.

--- specifyweb/accounts/models.py
+++ specifyweb/accounts/models.py
@@ -4,8 +4,9 @@ from specifyweb.specify import models as spmodels
 Specifyuser = getattr(spmodels, 'Specifyuser')
 
 class Spuserexternalid(models.Model):
-    provider = models.CharField(max_length=256)
-    providerid = models.CharField(max_length=256)
+    """Maps external user identities to Specify user accounts."""
+    provider = models.CharField(max_length=256)  # From a key in settings.OAUTH_LOGIN_PROVIDERS
+    providerid = models.CharField(max_length=256) # The user's id in the external system.
     specifyuser = models.ForeignKey(Specifyuser, on_delete=models.CASCADE)
 
     class Meta:
