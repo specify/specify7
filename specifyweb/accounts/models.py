adfeb651a2d41dd4401ea70f32f90ab9e665cd6b
Ben Anhalt <ben.anhalt@gmail.com>
2021-12-23 14:43:04 -0600

    store a copy of user id token when they auth using external id.

--- specifyweb/accounts/models.py
+++ specifyweb/accounts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django_jsonfield_backport.models import JSONField # type: ignore
 
 from specifyweb.specify import models as spmodels
 Specifyuser = getattr(spmodels, 'Specifyuser')
@@ -6,7 +7,9 @@ Specifyuser = getattr(spmodels, 'Specifyuser')
 class Spuserexternalid(models.Model):
     """Maps external user identities to Specify user accounts."""
     provider = models.CharField(max_length=256)  # From a key in settings.OAUTH_LOGIN_PROVIDERS
-    providerid = models.CharField(max_length=256) # The user's id in the external system.
+    providerid = models.CharField(max_length=4095) # The user's id in the external system.
+    idtoken = JSONField(null=True) # Place to store the JWT of the user
+    enabled = models.BooleanField(default=True) # Can this id be used to login.
     specifyuser = models.ForeignKey(Specifyuser, on_delete=models.CASCADE)
 
     class Meta:
