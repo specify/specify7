1e40de5240796651ad99532896a270fa2567790e
Max Patiiuk <max@patii.uk>
2021-12-26 10:36:53 -0600

    Rewite ToolBarDataEntry to TypeScript

---
+++ specifyweb/frontend/js_src/lib/toolbardataentry.ts
@@ -0,0 +1,13 @@
+import type { MenuItem } from './components/main';
+import FormsDialog from './formsdialog';
+import commonText from './localization/common';
+
+const menuItem: MenuItem = {
+  task: 'data',
+  title: commonText('dataEntry'),
+  icon: '/static/img/data entry.png',
+  path: '/specify/view',
+  view: ({ onClose }) => new FormsDialog({ onClose }),
+};
+
+export default menuItem;
