0c49c3ac8aedb695c840282ec3046f1fe0891085
Max Patiiuk <max@patii.uk>
2021-12-26 10:37:12 -0600

    Rewrite ToolBarRecordSets to TypeScript

---
+++ specifyweb/frontend/js_src/lib/toolbarrecordsets.ts
@@ -0,0 +1,34 @@
+import type { MenuItem } from './components/main';
+import type { Schema } from './legacytypes';
+import commonText from './localization/common';
+import RecordSetsDialog from './recordsetsdialog';
+import schema from './schema';
+import userInfo from './userinfo';
+
+const menuItem: MenuItem = {
+  task: 'recordsets',
+  title: commonText('recordSets'),
+  icon: '/static/img/record sets.png',
+  view({ onClose }) {
+    const recordSets = new (
+      schema as unknown as Schema
+    ).models.RecordSet.LazyCollection({
+      filters: {
+        specifyuser: userInfo.id,
+        type: 0,
+        domainfilter: true,
+        orderby: '-timestampcreated',
+      },
+    });
+    const promise = new Promise((resolve) =>
+      recordSets.fetch({ limit: 5000 }).done(() => resolve(recordSets))
+    );
+    return new RecordSetsDialog({
+      recordSets: promise,
+      readOnly: userInfo.isReadOnly,
+      onClose,
+    }).render();
+  },
+};
+
+export default menuItem;
