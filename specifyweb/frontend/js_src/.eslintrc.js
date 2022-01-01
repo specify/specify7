44a1d6142976542852716870f8228aee4dcf0305
Max Patiiuk <max@patii.uk>
2022-01-01 17:08:44 -0600

    Update ESLint config

--- specifyweb/frontend/js_src/.eslintrc.js
+++ specifyweb/frontend/js_src/.eslintrc.js
@@ -1,5 +1,10 @@
 require('@rushstack/eslint-patch/modern-module-resolution');
 
+
+const OFF = 'off';
+const WARN = 'warn';
+const ERROR = 'error';
+
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
@@ -12,7 +17,22 @@ module.exports = {
   },
   extends: ['@maxxxxxdlp/eslint-config'],
   rules: {
-    '@typescript-eslint/no-empty-interface': 'off',
-    '@next/next/no-img-element': 'off',
+    '@typescript-eslint/no-empty-interface': OFF,
+    '@next/next/google-font-display': OFF,
+    '@next/next/google-font-preconnect': OFF,
+    '@next/next/link-passhref': OFF,
+    '@next/next/no-css-tags': OFF,
+    '@next/next/no-document-import-in-page': OFF,
+    '@next/next/no-head-import-in-document': OFF,
+    '@next/next/no-html-link-for-pages': OFF,
+    '@next/next/no-img-element': OFF,
+    '@next/next/no-head-element': OFF,
+    '@next/next/no-page-custom-font': OFF,
+    '@next/next/no-sync-scripts': OFF,
+    '@next/next/no-title-in-document-head': OFF,
+    '@next/next/no-unwanted-polyfillio': OFF,
+    '@next/next/inline-script-id': OFF,
+    '@next/next/no-typos': OFF,
+    '@next/next/next-script-for-ga': OFF,
   },
 };
