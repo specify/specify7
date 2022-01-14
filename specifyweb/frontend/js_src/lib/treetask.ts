d41e49290894a0e506eee1e4f85a9e9bda998387
Max Patiiuk <max@patii.uk>
2022-01-14 15:12:36 -0600

    Delete previous TreeView implementation

--- specifyweb/frontend/js_src/lib/treetask.ts
+++ specifyweb/frontend/js_src/lib/treetask.ts
@@ -5,11 +5,8 @@ import router from './router';
 import { setCurrentView } from './specifyapp';
 import type { Collection } from './specifymodel';
 
-export default function () {
+export default function Routes(): void {
   router.route('tree/:table/', 'tree', async (table: string) =>
-    import('./treeview').then(({ default: treeView }) => treeView(table))
-  );
-  router.route('newTree/:table/', 'tree', async (table: string) =>
     import('./components/treeview').then(({ default: TreeView }) => {
       const treePromise = (getTreeDef as GetTreeDefinition)(table);
       if (treePromise === null) {
