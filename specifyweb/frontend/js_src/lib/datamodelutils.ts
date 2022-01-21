731517931354d15f234b23e22e116f6ac5285449
Max Patiiuk <max@patii.uk>
2022-01-21 16:46:35 -0600

    Improve SpecifyResource and Collection typing

--- specifyweb/frontend/js_src/lib/datamodelutils.ts
+++ specifyweb/frontend/js_src/lib/datamodelutils.ts
@@ -1,3 +1,19 @@
+import type { SpecifyResource } from './legacytypes';
+import { IR, RA } from './types';
+import {
+  Geography,
+  GeographyTreeDef,
+  GeologicTimePeriod,
+  GeologicTimePeriodTreeDef,
+  LithoStrat,
+  LithoStratTreeDef,
+  Storage,
+  StorageTreeDef,
+  Tables,
+  Taxon,
+  TaxonTreeDef,
+} from './datamodel';
+
 /* The dataModel types types were generated using this code snippet: */
 /* eslint-disable multiline-comment-style*/
 /* ;
@@ -80,3 +96,89 @@ function regenerate() {
 
 */
 /* eslint-enable multiline-comment-style*/
+
+export type SerializedModel<SCHEMA extends AnySchema> = UnFetchedRelationships<
+  SCHEMA['toOneIndependent']
+> & {
+  [KEY in keyof SCHEMA['toManyDependent']]: RA<
+    SerializedModel<SCHEMA['toManyDependent'][KEY][number]>
+  >;
+} & SCHEMA['toOneDependent'] &
+  SCHEMA['fields'] & {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    readonly resource_uri: string;
+    readonly id: number;
+  };
+
+/** Like resource.toJSON(), but keys are converted to camel case */
+export const serializeModel = <SCHEMA extends AnySchema>(
+  resource: SpecifyResource<SCHEMA>
+): SerializedModel<SCHEMA> =>
+  // @ts-expect-error
+  Object.fromEntries(
+    Object.entries(resource?.toJSON() ?? resource).map(
+      ([lowercaseFieldName, value]) => [
+        resource.specifyModel.fields.find(
+          ({ name }) => name.toLowerCase() === lowercaseFieldName
+        )?.name ?? lowercaseFieldName,
+        typeof value === 'object' && value !== null
+          ? serializeModel(value)
+          : value,
+      ]
+    )
+  );
+
+/** Resolve table name from table schema */
+export type TableName<SCHEMA extends AnySchema> = keyof {
+  [TABLE_NAME in keyof Tables as Tables[TABLE_NAME] extends SCHEMA
+    ? TABLE_NAME
+    : never]: Tables[TABLE_NAME];
+};
+
+/** Replace relationship objects with api URLs */
+export type UnFetchedRelationships<RELATIONSHIPS extends IR<AnySchema | null>> =
+  {
+    [KEY in keyof RELATIONSHIPS]:
+      | string
+      | Exclude<RELATIONSHIPS[KEY], AnySchema>;
+  };
+
+export type AnySchema = {
+  readonly fields: IR<string | number | boolean | null>;
+  readonly toOneDependent: IR<AnySchema | null>;
+  readonly toOneIndependent: IR<AnySchema | null>;
+  readonly toManyDependent: IR<RA<AnySchema>>;
+  readonly toManyIndependent: IR<RA<AnySchema>>;
+};
+
+export type AnyTree = AnySchema &
+  (Geography | Storage | Taxon | GeologicTimePeriod | LithoStrat);
+
+export type AnyTreeDef = AnySchema &
+  (
+    | GeographyTreeDef
+    | GeologicTimePeriodTreeDef
+    | LithoStratTreeDef
+    | StorageTreeDef
+    | TaxonTreeDef
+  );
+
+export type KeysToLowerCase<T extends IR<unknown>> = {
+  [KEY in keyof T as Lowercase<string & KEY>]: T[KEY];
+};
+
+/**
+ * Back-end seems to accept keys in lower-case format only, so have to convert
+ * them back
+ */
+export const keysToLowerCase = <T extends IR<unknown>>(object: T) =>
+  Object.fromEntries(
+    Object.entries(object).map(([key, value]) => [
+      key.toLowerCase(),
+      typeof value === 'object' && value !== null
+        ? Array.isArray(value)
+          ? value.map((item) => keysToLowerCase(item))
+          : keysToLowerCase(value as IR<unknown>)
+        : value,
+    ])
+  );
--- specifyweb/frontend/js_src/lib/legacytypes.ts
+++ specifyweb/frontend/js_src/lib/legacytypes.ts
@@ -1,29 +1,137 @@
+import type {
+  AnySchema,
+  AnyTree,
+  AnyTreeDef,
+  KeysToLowerCase,
+  TableName,
+  UnFetchedRelationships,
+} from './datamodelutils';
 import type SaveBlockers from './saveblockers';
-import type SpecifyModel from './specifymodel';
-import type { RA } from './types';
+import schema from './schema';
+import type { Collection, default as SpecifyModel } from './specifymodel';
+import type { IR, RA, RR } from './types';
 
-export type SpecifyResource = {
+export type SpecifyResource<SCHEMA extends AnySchema> = {
+  readonly attributes: IR<never> &
+    KeysToLowerCase<
+      UnFetchedRelationships<SCHEMA['toOneIndependent']> &
+        UnFetchedRelationships<SCHEMA['toOneDependent']> &
+        SCHEMA['fields']
+    > & {
+      readonly resource_uri: string;
+      readonly id: number;
+    };
+  readonly toJSON: () => IR<never> &
+    KeysToLowerCase<
+      UnFetchedRelationships<SCHEMA['toOneIndependent']> &
+        SCHEMA['toManyDependent'] &
+        SCHEMA['toOneDependent'] &
+        SCHEMA['fields']
+    > & {
+      readonly resource_uri: string;
+      readonly id: number;
+    };
   readonly id: number;
+  readonly needsSaved: boolean;
   readonly cid: string;
-  readonly get: <TYPE = unknown>(fieldName: string) => TYPE;
-  readonly rget: <TYPE = unknown>(
-    fieldName: string,
-    prePopulate?: boolean
-  ) => Promise<TYPE>;
-  readonly set: (fieldName: string, value: unknown) => void;
+  get: <
+    FIELD_NAME extends
+      | keyof SCHEMA['fields']
+      | keyof SCHEMA['toOneIndependent']
+      | keyof SCHEMA['toOneDependent']
+      | keyof SCHEMA['toManyIndependent']
+      | keyof SCHEMA['toManyDependent']
+      | 'resource_uri'
+      | 'id',
+    VALUE extends (IR<never> &
+      SCHEMA['toOneIndependent'] &
+      SCHEMA['toOneDependent'] &
+      SCHEMA['toManyIndependent'] &
+      SCHEMA['toManyDependent'] &
+      SCHEMA['fields'] & {
+        readonly resource_uri: string;
+        readonly id: number;
+      })[FIELD_NAME]
+  >(
+    fieldName: FIELD_NAME
+  ) => [VALUE] extends [never]
+    ? never
+    : VALUE extends AnySchema
+    ? VALUE extends null
+      ? string | null
+      : string
+    : VALUE extends RA<AnySchema>
+    ? string
+    : VALUE;
+  // Case-insensitive fetch of a -to-one resource
+  rget: <
+    FIELD_NAME extends
+      | keyof SCHEMA['toOneIndependent']
+      | keyof SCHEMA['toOneDependent'],
+    VALUE extends (IR<never> &
+      SCHEMA['toOneIndependent'] &
+      SCHEMA['toOneDependent'])[FIELD_NAME]
+  >(
+    fieldName: FIELD_NAME
+  ) => [VALUE] extends [never]
+    ? never
+    : Promise<
+        SpecifyResource<Exclude<VALUE, null>> | Exclude<VALUE, AnySchema>
+      >;
+  // Case-insensitive fetch of a -to-many resource
+  rgetCollection: <
+    FIELD_NAME extends
+      | keyof SCHEMA['toManyIndependent']
+      | keyof SCHEMA['toManyDependent'],
+    VALUE extends (IR<never> &
+      SCHEMA['toManyIndependent'] &
+      SCHEMA['toManyDependent'])[FIELD_NAME]
+  >(
+    fieldName: FIELD_NAME
+  ) => [VALUE] extends [never]
+    ? never
+    : Promise<Collection<SpecifyResource<Exclude<VALUE, null>[number]>>>;
+  readonly set: <
+    FIELD_NAME extends
+      | keyof SCHEMA['fields']
+      | keyof SCHEMA['toOneIndependent']
+      | keyof SCHEMA['toOneDependent']
+      | keyof SCHEMA['toManyIndependent']
+      | keyof SCHEMA['toManyDependent']
+      | 'resource_uri'
+      | 'id',
+    VALUE extends (IR<never> &
+      SCHEMA['toOneIndependent'] &
+      SCHEMA['toOneDependent'] & {
+        [KEY in keyof SCHEMA['toManyDependent']]: RA<
+          SpecifyResource<SCHEMA['toManyDependent'][KEY][number]>
+        >;
+      } & RR<keyof SCHEMA['toManyDependent'], string> &
+      SCHEMA['fields'] & {
+        readonly resource_uri: string;
+        readonly id: number;
+      })[FIELD_NAME]
+  >(
+    fieldName: FIELD_NAME,
+    value: VALUE
+  ) => SpecifyResource<SCHEMA>;
   readonly save: () => Promise<void>;
+  readonly fetch: () => Promise<SpecifyResource<SCHEMA>>;
+  readonly fetchIfNotPopulated: () => Promise<SpecifyResource<SCHEMA>>;
+  readonly populated: boolean;
   readonly destroy: () => Promise<void>;
   readonly viewUrl: () => string;
-  readonly Resource: new () => SpecifyResource;
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  readonly Resource: new () => SpecifyResource<SCHEMA>;
   readonly isNew: () => boolean;
-  readonly clone: () => SpecifyResource;
+  readonly clone: () => SpecifyResource<SCHEMA>;
   readonly specifyModel: Readonly<SpecifyModel>;
-  readonly saveBlockers: Readonly<SaveBlockers>;
-  readonly parent?: SpecifyResource;
+  readonly saveBlockers: Readonly<SaveBlockers<SCHEMA>>;
+  readonly parent?: SpecifyResource<SCHEMA>;
   readonly format: () => Promise<string>;
   readonly url: () => string;
   readonly collection: {
-    readonly related: SpecifyResource;
+    readonly related: SpecifyResource<SCHEMA>;
   };
   readonly on: (
     eventName: string,
@@ -43,4 +151,11 @@ export type SpecifyResource = {
   };
 };
 
-export type GetTreeDefinition = (tableName: string) => Promise<SpecifyResource>;
+const a = new schema.models.SpQuery.LazyCollection({ filters: { id: 1 } });
+a.fetch().then(({ models: [model] }) => {
+  model.set('fields', []);
+});
+
+export type GetTreeDefinition<SCHEMA extends AnyTreeDef> = (
+  tableName: TableName<AnyTree>
+) => Promise<SpecifyResource<SCHEMA>>;
