65df5a53bed8e0cfb7ee2916a8dca692b2036374
Ben Anhalt <ben.anhalt@gmail.com>
2022-03-10 14:57:57 -0600

    introduce permissions registry and apply to permissions API.

--- specifyweb/permissions/permissions.py
+++ specifyweb/permissions/permissions.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable, Tuple, List
+from typing import Any, Callable, Tuple, List, Dict
 
 import logging
 logger = logging.getLogger(__name__)
@@ -7,6 +7,49 @@ from django.db import connection
 from django.core.exceptions import ObjectDoesNotExist
 
 from . import models
+
+registry: Dict[str, List[str]] = dict()
+
+class PermissionTargetAction:
+    _resource = "undefined"
+    _action = "undefined"
+
+    def resource(self) -> str:
+        return self._resource
+
+    def action(self) -> str:
+        return self._action
+
+
+class PermissionTargetMeta(type):
+    def __new__(cls, name, bases, attrs):
+        if bases: # skip PermissionsTarget base class
+            resource: str = attrs['resource']
+            assert resource not in registry
+
+            actions = registry[resource] = []
+
+            for k, v in attrs.items():
+                if isinstance(v, PermissionTargetAction):
+                    v._resource = attrs['resource']
+                    v._action = k
+                    actions.append(k)
+
+        return super(PermissionTargetMeta, cls).__new__(cls, name, bases, attrs)
+
+class PermissionTarget(metaclass=PermissionTargetMeta):
+    pass
+
+def check_permission_targets(collectionid: int, userid: int, targets: List[PermissionTargetAction]) -> None:
+    if not targets: return
+
+    perm_requests = [(collectionid, userid, t.resource(), t.action()) for t in targets]
+    results = [(r, *enforce_single(*r)) for r in perm_requests]
+    logger.debug("permissions check: %s", results)
+
+    denials = [(r, reason) for r, allowed, reason in results if not allowed]
+    if denials:
+        raise AccessDeniedException(denials)
 
 class AccessDeniedException(Exception):
     pass
