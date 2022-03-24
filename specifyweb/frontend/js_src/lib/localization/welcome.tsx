e8323ad71329e6477a76e19f608051a27f8e109c
Max Patiiuk <max@patii.uk>
2022-03-24 16:19:47 -0500

    Update LICENCE statement

--- specifyweb/frontend/js_src/lib/localization/welcome.tsx
+++ specifyweb/frontend/js_src/lib/localization/welcome.tsx
@@ -101,59 +101,61 @@ const welcomeText = createDictionary({
   },
   disclosure: {
     'en-us': `
-      Specify Software is a product of the Specify Collections Consortium
-      that is funded by its member institutions. Consortium Founding Members
-      include: University of Michigan, University of Florida, Denmark
-      Consortium of Museums, and the University of Kansas. The Consortium
+      Specify software is a product of the Specify Collections Consortium that
+      is governed and funded by its member institutions. Consortium Founding
+      Members include: Commonwealth Scientific and Industrial Research
+      Organisation (CSIRO), Denmark Consortium of Museums, University of
+      Florida, University of Kansas, and University of Michigan. The Consortium
       operates under the non-profit, 501(c)3, U.S. tax status of the University
-      of Kansas Center for Research. Specify was supported previously by
-      multiple awards from the U.S. National Science Foundation.`,
+      of Kansas Center for Research. Specify was supported from 1996 to 2018 by
+      grants from the U.S. National Science Foundation.`,
     'ru-ru': `
-      Specify Software является продуктом консорциума Specify Collections.
-      который финансируется организациями-членами. Члены-учредители консорциума
-      включают: University of Michigan, University of Florida, Denmark
-      Consortium of Museums и University of Kansas. Консорциум
-      действует под некоммерческой организацией, 501(c)3, налоговым статусом США
-      Университета University of Kansas. Specify ранее поддерживался
-      многочисленные награды фонда U.S. National Science Foundation.`,
+      Specify software является продуктом консорциума Specify Collections.
+      который управляется и финансируется организациями-членами.
+      Члены-учредители консорциума включают: Commonwealth Scientific and
+      Industrial Research Organisation (CSIRO), University of Michigan,
+      University of Florida, Denmark Consortium of Museums и University of
+      Kansas. Консорциум действует под некоммерческой организацией, 501(c)3,
+      налоговым статусом США университета University of Kansas. Specify
+      поддерживался с 1996 по 2018 год грантами фонда U.S. National Science 
+      Foundation.`,
     ca: `
-      Specify Software is a product of the Specify Collections Consortium
-      that is funded by its member institutions. Consortium Founding Members
-      include: University of Michigan, University of Florida, Denmark
-      Consortium of Museums, and the University of Kansas. The Consortium
+      Specify software is a product of the Specify Collections Consortium that
+      is governed and funded by its member institutions. Consortium Founding
+      Members include: Commonwealth Scientific and Industrial Research
+      Organisation (CSIRO), Denmark Consortium of Museums, University of
+      Florida, University of Kansas, and University of Michigan. The Consortium
       operates under the non-profit, 501(c)3, U.S. tax status of the University
-      of Kansas Center for Research. Specify was supported previously by
-      multiple awards from the U.S. National Science Foundation.`,
+      of Kansas Center for Research. Specify was supported from 1996 to 2018 by
+      grants from the U.S. National Science Foundation.`,
     'es-es': `
-      Specify Software is a product of the Specify Collections Consortium
-      that is funded by its member institutions. Consortium Founding Members
-      include: University of Michigan, University of Florida, Denmark
-      Consortium of Museums, and the University of Kansas. The Consortium
+      Specify software is a product of the Specify Collections Consortium that
+      is governed and funded by its member institutions. Consortium Founding
+      Members include: Commonwealth Scientific and Industrial Research
+      Organisation (CSIRO), Denmark Consortium of Museums, University of
+      Florida, University of Kansas, and University of Michigan. The Consortium
       operates under the non-profit, 501(c)3, U.S. tax status of the University
-      of Kansas Center for Research. Specify was supported previously by
-      multiple awards from the U.S. National Science Foundation.`,
+      of Kansas Center for Research. Specify was supported from 1996 to 2018 by
+      grants from the U.S. National Science Foundation.`,
   },
   licence: {
     'en-us': `
-      Specify 7 Copyright © 2022 University of Kansas Center for
-      Research. Specify comes with ABSOLUTELY NO WARRANTY. This is
-      free software licensed under GNU General Public License 2
-      (GPL2).`,
+      Specify 7, Copyright 2022, University of Kansas Center for Research.
+      Specify comes with ABSOLUTELY NO WARRANTY. This is free, open-source
+      software licensed under GNU General Public License v2.`,
     'ru-ru': `
-      Specify 7 Авторские права © 2022 University of Kansas для исследования.
+      Specify 7, Авторские права 2022, University of Kansas для исследования.
       Specify поставляется с СОВЕРШЕННО ОТСУТСТВИЕМ ГАРАНТИИ. Это
-      бесплатное программное обеспечение под лицензией GNU General Public
-      License 2 (GPL2).`,
+      бесплатное программное обеспечение с открытым исходным кодом под лицензией
+      GNU General Public License v2.`,
     ca: `
-      Specify 7 Copyright © 2022 University of Kansas Center for
-      Research. Specify comes with ABSOLUTELY NO WARRANTY. This is
-      free software licensed under GNU General Public License 2
-      (GPL2).`,
+      Specify 7, Copyright 2022, University of Kansas Center for Research.
+      Specify comes with ABSOLUTELY NO WARRANTY. This is free, open-source
+      software licensed under GNU General Public License v2.`,
     'es-es': `
-      Specify 7 Copyright © 2022 University of Kansas Center for
-      Research. Specify comes with ABSOLUTELY NO WARRANTY. This is
-      free software licensed under GNU General Public License 2
-      (GPL2).`,
+      Specify 7, Copyright 2022, University of Kansas Center for Research.
+      Specify comes with ABSOLUTELY NO WARRANTY. This is free, open-source
+      software licensed under GNU General Public License v2.`,
   },
   systemInformation: {
     'en-us': 'System Information',
