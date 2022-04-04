ddd9525e175666e78c297fa7157224988abe538c
Ben Anhalt <ben.anhalt@gmail.com>
2022-04-04 11:40:41 -0500

    skip_collection_access_check on account urls got lost during merge.

--- specifyweb/accounts/urls.py
+++ specifyweb/accounts/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from ..permissions.permissions import skip_collection_access_check
 from django.conf import settings
+from django.contrib.auth import views as auth_views
+from django.urls import path
 
+from specifyweb.permissions.permissions import skip_collection_access_check
 from . import views
-
 
 urlpatterns = [
     path('login/',
@@ -18,14 +17,25 @@ urlpatterns = [
 
     # OpenId Connect callback endpoint:
     path('oic_callback/', views.oic_callback),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/accounts/login/')),
-    path('password_change/', skip_collection_access_check(auth_views.PasswordChangeView.as_view(
-        template_name='password_change.html', success_url='/')),
 
-    path('support_login/',
-        skip_collection_access_check(views.support_login)),
-    path('choose_collection/',
-        skip_collection_access_check(views.choose_collection)),
+    path(
+        'logout/',
+        skip_collection_access_check(auth_views.LogoutView.as_view(next_page='/accounts/login/'))
+    ),
+
+    path(
+        'password_change/',
+        skip_collection_access_check(
+            auth_views.PasswordChangeView.as_view(
+                template_name='password_change.html', success_url='/'))
+    ),
+
+    path(
+        'choose_collection/',
+        skip_collection_access_check(views.choose_collection)
+    ),
+
+    path('support_login/', views.support_login),
 
     # API endpoint to generate an invite link:
     path('invite_link/<int:userid>/', views.invite_link),
