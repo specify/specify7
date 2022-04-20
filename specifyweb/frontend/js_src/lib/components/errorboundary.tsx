4c13c9681d3b04d939f08754ddaa7b9931f69de6
Max Patiiuk <max@patii.uk>
2022-04-20 11:03:05 -0500

    Add cache buster link to error messages

--- specifyweb/frontend/js_src/lib/components/errorboundary.tsx
+++ specifyweb/frontend/js_src/lib/components/errorboundary.tsx
@@ -18,7 +18,7 @@ import { setCurrentView } from '../specifyapp';
 import { getSystemInfo } from '../systeminfo';
 import type { RA } from '../types';
 import { userInformation } from '../userinfo';
-import { Button, Link } from './basic';
+import { Button, className, Link } from './basic';
 import { displayError } from './contexts';
 import { copyTextToClipboard, downloadFile } from './filepicker';
 import { useBooleanState } from './hooks';
@@ -76,6 +76,12 @@ function ErrorDialog({
             {commonText('downloadErrorMessage')}
           </Button.Blue>
           <CopyErrorMessage message={copiableMessage} />
+          <Link.LikeFancyButton
+            className={className.blueButton}
+            href="/specify/task/cache-buster/"
+          >
+            {commonText('clearCache')}
+          </Link.LikeFancyButton>
           <span className="flex-1 -ml-2" />
           <Button.Red onClick={(): void => window.location.assign('/')}>
             {commonText('close')}
