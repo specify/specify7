3829b19ab6ccf36f019639bfdd7c2e3c1e3915ac
Max Patiiuk <max@patii.uk>
2022-06-23 21:21:56 -0500

    Document newly added tree remote prefs
    See #1735

--- specifyweb/frontend/js_src/lib/remoteprefs.ts
+++ specifyweb/frontend/js_src/lib/remoteprefs.ts
@@ -207,6 +207,37 @@ export const remotePrefsDefinitions = f.store(
         formatter: [formatter().trim],
         isLegacy: false,
       },
+      'sp7.allow_adding_child_to_synonymized_parent.GeologicTimePeriod': {
+        description:
+          'Allowed to add children to synopsized Geologic Time Period records',
+        defaultValue: false,
+        parser: 'java.lang.Boolean',
+        isLegacy: false,
+      },
+      'sp7.allow_adding_child_to_synonymized_parent.Taxon': {
+        description: 'Allowed to add children to synopsized Taxon records',
+        defaultValue: false,
+        parser: 'java.lang.Boolean',
+        isLegacy: false,
+      },
+      'sp7.allow_adding_child_to_synonymized_parent.Geography': {
+        description: 'Allowed to add children to synopsized Geography records',
+        defaultValue: false,
+        parser: 'java.lang.Boolean',
+        isLegacy: false,
+      },
+      'sp7.allow_adding_child_to_synonymized_parent.LithoStrat': {
+        description: 'Allowed to add children to synopsized LithoStrat records',
+        defaultValue: false,
+        parser: 'java.lang.Boolean',
+        isLegacy: false,
+      },
+      'sp7.allow_adding_child_to_synonymized_parent.Storage': {
+        description: 'Allowed to add children to synopsized Storage records',
+        defaultValue: false,
+        parser: 'java.lang.Boolean',
+        isLegacy: false,
+      },
       // This is actually stored in Global Prefs:
       /*
        * 'AUDIT_LIFESPAN_MONTHS': {
