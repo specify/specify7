f55013ced4aac94288978a4fa746b8b1ecf81050
Max Patiiuk <max@patii.uk>
2022-08-31 16:52:16 -0500

    Add utility for mocking time in tests

--- specifyweb/frontend/js_src/lib/tests/helpers.ts
+++ specifyweb/frontend/js_src/lib/tests/helpers.ts
@@ -24,3 +24,11 @@ export const requireContext = (): void =>
     await initialContext;
     await treeRanksPromise;
   });
+
+export const testTime = new Date('2022-08-31T03:37:10.4');
+
+export const mockTime = (date = testTime): void =>
+  beforeAll(() => {
+    jest.useFakeTimers();
+    jest.setSystemTime(date);
+  });
