8dc05a3c20a106d069632f632a9cd8e12ed22b50
realVinayak <realvinayak123@gmail.com>
2022-10-11 16:38:47 -0500

    Add localization to stats page

---
+++ specifyweb/frontend/js_src/lib/localization/stats.tsx
@@ -0,0 +1,126 @@
+/**
+ * Localization strings used in the Statistics Page
+ *
+ * @module
+ */
+
+import { createDictionary } from './utils';
+
+// Refer to "Guidelines for Programmers" in ./README.md before editing this file
+
+/* eslint-disable @typescript-eslint/naming-convention */
+export const statsText = createDictionary({
+  collectionStatistics: {
+    'en-us': 'Collection Statistics',
+    'ru-ru': 'Статистика коллекции',
+  },
+  holdings: {
+    'en-us': 'Holdings',
+    'ru-ru': 'Холдинги',
+  },
+  specimens: {
+    'en-us': 'Specimens',
+    'ru-ru': 'Образцы',
+  },
+  preparations: {
+    'en-us': 'Preparations',
+    'ru-ru': 'Подготовка',
+  },
+  synonymize: {
+    'en-us': 'Synonymize',
+    'ru-ru': 'Сделать синонимом',
+  },
+  typeSpecimens: {
+    'en-us': 'Type Specimens',
+    'ru-ru': 'Типовые образцы',
+  },
+  familiesRepresented: {
+    'en-us': 'Families Represented',
+    'ru-ru': 'Представленные семьи',
+  },
+  generaRepresented: {
+    'en-us': 'Genera Represented',
+    'ru-ru': 'Представленные роды',
+  },
+  speciesRepresented: {
+    'en-us': 'Species Represented',
+    'ru-ru': 'Представленные виды',
+  },
+  loans: {
+    'en-us': 'Loans',
+    'ru-ru': 'Кредиты',
+  },
+  itemsOnLoans: {
+    'en-us': 'Items on Loans',
+    'ru-ru': 'Предметы в кредит',
+  },
+  openLoansCount: {
+    'en-us': 'Open Loans Count',
+    'ru-ru': 'Количество открытых кредитов',
+  },
+  overdueLoansCount: {
+    'en-us': 'Overdue Loans Count',
+    'ru-ru': 'Количество просроченных кредитов',
+  },
+  taxonomicTree: {
+    'en-us': 'Taxonomic Tree',
+    'ru-ru': 'Таксономическое дерево',
+  },
+  classes: {
+    'en-us': 'Classes',
+    'ru-ru': 'Классы',
+  },
+  orders: {
+    'en-us': 'Orders',
+    'ru-ru': 'Заказы',
+  },
+  families: {
+    'en-us': 'Families',
+    'ru-ru': 'Семьи',
+  },
+  genera: {
+    'en-us': 'Genera',
+    'ru-ru': 'Роды',
+  },
+  species: {
+    'en-us': 'Species',
+    'ru-ru': 'Разновидность',
+  },
+  localityGeography: {
+    'en-us': 'Locality / Geography',
+    'ru-ru': 'Местность / География',
+  },
+  localityCount: {
+    'en-us': 'Locality Count',
+    'ru-ru': 'Количество локаций',
+  },
+  geographyEntryCount: {
+    'en-us': 'Geography Entry Count',
+    'ru-ru': 'Количество входов по географии',
+  },
+  countriesCount: {
+    'en-us': 'Countries',
+    'ru-ru': 'Страны',
+  },
+  georeferencedLocalityCount: {
+    'en-us': 'Georeferenced Localities Count',
+    'ru-ru': 'Количество мест с географической привязкой',
+  },
+  catalogStats: {
+    'en-us': 'Collection Catalogs',
+    'ru-ru': 'Каталоги коллекций',
+  },
+  catalogedLast7Days: {
+    'en-us': 'Cataloged Last 7 Days',
+    'ru-ru': 'В каталоге за последние 7 дней',
+  },
+  catalogedLast30Days: {
+    'en-us': 'Cataloged Last 30 Days',
+    'ru-ru': 'В каталоге за последние 30 дней',
+  },
+  catalogedLastYear: {
+    'en-us': 'Cataloged Last Year',
+    'ru-ru': 'Каталогизирован в прошлом году',
+  },
+});
+/* eslint-enable @typescript-eslint/naming-convention */
