specify/specify7

commit fcf850766ef20a9e8693dfc06151b4e00709ee7c

Fix PartialDateUi modifying readOnly date
Fixes #2415

specifyweb/frontend/js_src/lib/components/FormPlugins/PartialDateUi.tsx
@@ -249,6 +249,7 @@ export function PartialDateUi<SCHEMA extends AnySchema>({
   ]);
 
   React.useEffect(() => {
+    if (isReadOnly) return;
     /*
      * If resource changes, a new moment is set, but its value won't get
      * propagated on the first call to this useEffect.

specifyweb/frontend/js_src/lib/components/QueryBuilder/ResultsTable.tsx
@@ -86,6 +86,7 @@ function Row({
   readonly isLast: boolean;
   readonly onSelected?: (isSelected: boolean, isShiftClick: boolean) => void;
 }): JSX.Element {
+  // REFACTOR: replace this with getResourceViewUrl()
   const [resource] = useLiveState<
     SpecifyResource<AnySchema> | false | undefined
   >(

commit 13579dddf366458b08c2086f3dade335b176a494

Fix PartialDateUi not working in read only mode
Fixes #2438

Caused by fcf850766e

specifyweb/frontend/js_src/lib/components/FormPlugins/PartialDateUi.tsx
@@ -203,7 +203,6 @@ export function PartialDateUi<SCHEMA extends AnySchema>({
   // Unparsed raw input
   const [inputValue, setInputValue] = React.useState('');
 
-  const isSettingInitialMoment = React.useRef<boolean>(true);
   const isInitialized = React.useRef<boolean>(false);
 
   React.useEffect(() => {
@@ -212,7 +211,6 @@ export function PartialDateUi<SCHEMA extends AnySchema>({
         silent: true,
       });
 
-    isSettingInitialMoment.current = true;
     isInitialized.current = false;
 
     const destructor = resourceOn(
@@ -249,7 +247,6 @@ export function PartialDateUi<SCHEMA extends AnySchema>({
   ]);
 
   React.useEffect(() => {
-    if (isReadOnly) return;
     /*
      * If resource changes, a new moment is set, but its value won't get
      * propagated on the first call to this useEffect.
@@ -257,8 +254,6 @@ export function PartialDateUi<SCHEMA extends AnySchema>({
      */
     if (!isInitialized.current) {
       isInitialized.current = true;
-      isSettingInitialMoment.current =
-        typeof resource.get(dateField) === 'string';
       return;
     }
     if (moment === undefined) {
@@ -279,16 +274,7 @@ export function PartialDateUi<SCHEMA extends AnySchema>({
       )
         resource.set(precisionField, precisions[precision] as never);
 
-      if (isSettingInitialMoment.current)
-        /*
-         * Don't set the value on the first run
-         * If this isn't done, unload protect would be needlessly triggered if
-         * current value in the date field does not exactly match the formatted
-         * value (i.e, happens for timestampModified fields since those include
-         * time, whereas formatted date doesn't)
-         */
-        isSettingInitialMoment.current = false;
-      else resource.set(dateField, value as never);
+      if (!isReadOnly) resource.set(dateField, value as never);
       resource.saveBlockers?.remove(`invaliddate:${dateField}`);
 
       if (precision === 'full') setInputValue(moment.format(inputFullFormat));
@@ -387,7 +373,6 @@ export function PartialDateUi<SCHEMA extends AnySchema>({
                     type: monthType,
                     placeholder: monthFormat(),
                     title: moment?.format(monthFormat()),
-                    value: moment?.format(inputMonthFormat),
                     ...(monthSupported
                       ? {}
                       : {
@@ -399,7 +384,6 @@ export function PartialDateUi<SCHEMA extends AnySchema>({
                     type: dateType,
                     placeholder: fullDateFormat(),
                     title: moment?.format(fullDateFormat()),
-                    value: moment?.format(inputFullFormat),
                     ...(dateSupported
                       ? {}
                       : {