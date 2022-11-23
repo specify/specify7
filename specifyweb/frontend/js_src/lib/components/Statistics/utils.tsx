fed51fe542224c9c5e5f7a00d75645ee515be7da
realVinayak <realvinayak123@gmail.com>
2022-11-23 12:33:13 -0600

    modify frontend stats result to include backend results

--- specifyweb/frontend/js_src/lib/components/Statistics/utils.tsx
+++ specifyweb/frontend/js_src/lib/components/Statistics/utils.tsx
@@ -17,6 +17,7 @@ import { FrontEndStatsResultDialog } from './ResultsDialog';
 import { Button } from '../Atoms/Button';
 import { SpecifyResource } from '../DataModel/legacyTypes';
 import { deserializeResource } from '../../hooks/resource';
+import { commonText } from '../../localization/common';
 
 /** Fetch statistics from the QueryBuilderAPI. */
 export function useFrontEndStatsQuery(
@@ -101,20 +102,47 @@ export type BackendStat = State<
 
 export type StatItemSpec = BackendStat | QueryBuildStat;
 
-export function FrontEndStatsResult({
+export function StatsResult({
   statValue,
   query,
   statLabel,
+  onClick: handleClick,
+  onRemove: handleRemove,
 }: {
-  readonly statValue: string | undefined;
-  readonly query: SpecifyResource<SpQuery>;
+  readonly statValue: string | number | undefined;
+  readonly query: SpecifyResource<SpQuery> | undefined;
   readonly statLabel: string;
+  readonly onClick: (() => void) | undefined;
+  readonly onRemove: (() => void) | undefined;
 }): JSX.Element {
   const [isOpen, handleOpen, handleClose] = useBooleanState();
   return (
     <>
-      <Button.LikeLink onClick={handleOpen}>{statValue}</Button.LikeLink>
-      {isOpen && (
+      {statLabel === undefined ? (
+        <div>{commonText('loading')}</div>
+      ) : (
+        <p className="flex gap-2">
+          {typeof handleRemove === 'function' && (
+            <Button.Icon
+              icon="trash"
+              title={commonText('remove')}
+              onClick={handleRemove}
+            />
+          )}
+          <Button.LikeLink
+            className="flex-1"
+            onClick={
+              handleClick ?? (query === undefined ? undefined : handleOpen)
+            }
+          >
+            <span>{statLabel}</span>
+            <span className="-ml-2 flex-1" />
+            <span>{statValue ?? commonText('loading')}</span>
+          </Button.LikeLink>
+        </p>
+      )}
+
+      {isOpen && query !== undefined && (
         <FrontEndStatsResultDialog
           query={query}
           onClose={handleClose}
