b99fe2247dbf846e212db5e94bb024013b4f18c5
realVinayak <realvinayak123@gmail.com>
2022-12-30 10:18:58 -0600

    Fix incorrect filters for spappresourcedir

--- specifyweb/context/collection_resources.py
+++ specifyweb/context/collection_resources.py
@@ -77,8 +77,11 @@ collection_resources = openapi(schema={
     }
 })(Resources.as_view(_spappresourcedirfilter= lambda request: {
                 'ispersonal': False,
+                'specifyuser__isnull': True,
+                'usertype__isnull': True,
 }, _spappresourcefilter= lambda request: {
-                'spappresourcedir__ispersonal':False
+                'spappresourcedir__ispersonal':False,
+                'spappresourcedir__specifyuser__isnull': True,
 }, _spappresourcefilterpost=lambda request: {
     'specifyuser': request.specify_user
 }))
