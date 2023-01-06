4f03cab1a425f1a579efa97d70b0e0d192d45ffa
Max Patiiuk <max@patii.uk>
2023-01-06 03:15:42 +0000

    Sync localization strings with Weblate
    Triggered by 643b877a1741997587d9e48676e2e02c168da88a on branch refs/heads/weblate-localization

--- specifyweb/frontend/js_src/lib/localization/attachments.ts
+++ specifyweb/frontend/js_src/lib/localization/attachments.ts
@@ -12,25 +12,43 @@ export const attachmentsText = createDictionary({
   attachments: {
     'en-us': 'Attachments',
     'ru-ru': 'Вложения',
+    'es-es': 'Archivos adjuntos',
+    'fr-fr': 'Pièces jointes',
+    'uk-ua': 'Додатки',
   },
   scale: {
     'en-us': 'Scale',
     'ru-ru': 'Масштаб',
+    'es-es': 'Escala',
+    'fr-fr': 'Échelle',
+    'uk-ua': 'масштаб',
   },
   attachmentServerUnavailable: {
     'en-us': 'Attachment server unavailable.',
     'ru-ru': 'Сервер прикрепленных файлов недоступен.',
+    'es-es': 'Servidor de archivos adjuntos no disponible.',
+    'fr-fr': 'Serveur de pièces jointes indisponible.',
+    'uk-ua': 'Сервер вкладень недоступний.',
   },
   orderBy: {
     'en-us': 'Order By',
     'ru-ru': 'Сортировать по',
+    'es-es': 'ordenar por',
+    'fr-fr': 'Commandé par',
+    'uk-ua': 'Сортувати по',
   },
   uploadingInline: {
     'en-us': 'Uploading…',
     'ru-ru': 'Закачивание…',
+    'es-es': 'Subiendo…',
+    'fr-fr': 'Téléchargement…',
+    'uk-ua': 'Завантаження…',
   },
   noAttachments: {
     'en-us': 'There are no attachments',
     'ru-ru': 'В вашей коллекции нет вложений',
+    'es-es': 'No hay archivos adjuntos',
+    'fr-fr': "Il n'y a pas de pièces jointes",
+    'uk-ua': 'Додатків немає',
   },
 } as const);
