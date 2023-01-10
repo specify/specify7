5eec5477ab069d434e046161e03db457d0965c27
realVinayak <realvinayak123@gmail.com>
2023-01-10 16:06:24 -0600

    Return fulfilled and pending promises

--- specifyweb/frontend/js_src/lib/utils/ajax/throttledAjax.ts
+++ specifyweb/frontend/js_src/lib/utils/ajax/throttledAjax.ts
@@ -1,42 +1,68 @@
 import { RR, WritableArray } from '../types';
+import { SpecifyResource } from '../../components/DataModel/legacyTypes';
+import { SpQuery } from '../../components/DataModel/types';
 
-const currentRequestsGenerator = <T>(): WritableArray<Promise<T>> => [];
+export type PromiseWithSpec<T, S> = Promise<T> & { spec: S };
+const currentRequestsGenerator = <T, S>(): WritableArray<
+  PromiseWithSpec<T, S>
+> => [];
+
+const fulfilledNetworkRequests: {
+  networkRequests: WritableArray<PromiseWithSpec<any, any>>;
+} = { networkRequests: [] };
+
+export const cleanFulfilledRequests = (): void => {
+  fulfilledNetworkRequests.networkRequests = [];
+};
 
 export const networkRequestsSpec: RR<
   'backendStats' | 'queryStats',
   {
-    readonly currentRequests: WritableArray<Promise<any>>;
+    readonly currentRequests: WritableArray<PromiseWithSpec<any, any>>;
     readonly maxFetchCount: number;
   }
 > = {
   queryStats: {
     maxFetchCount: 10,
-    currentRequests: currentRequestsGenerator<number | string | undefined>(),
+    currentRequests: currentRequestsGenerator<
+      number | string | undefined,
+      SpecifyResource<SpQuery>
+    >(),
   },
   backendStats: {
     maxFetchCount: 1,
-    currentRequests: currentRequestsGenerator<number>(),
+    currentRequests: currentRequestsGenerator<number, string>(),
   },
 };
 
-export async function throttledAjax<T>(
+export async function throttledAjax<T, S>(
   key: keyof typeof networkRequestsSpec,
-  promiseGenerator: () => Promise<T>
+  promiseGenerator: () => Promise<T>,
+  promiseSpec: S
 ) {
   const { maxFetchCount, currentRequests } = networkRequestsSpec[key];
+  const indexInFulfilled = fulfilledNetworkRequests.networkRequests.findIndex(
+    (currentPromise) => {
+      return currentPromise.spec === promiseSpec;
+    }
+  );
+  if (indexInFulfilled !== -1) {
+    return fulfilledNetworkRequests.networkRequests[indexInFulfilled];
+  }
+  const promiseIndex = currentRequests.findIndex((currentPromise) => {
+    return currentPromise.spec === promiseSpec;
+  });
+  if (promiseIndex !== -1) {
+    return currentRequests[promiseIndex];
+  }
   while (currentRequests.length > maxFetchCount) {
-    await Promise.any(currentRequests).then(
-      () =>
-        new Promise<void>((resolve) => {
-          setTimeout(() => {
-            resolve();
-          }, 1000);
-        })
-    );
+    await Promise.any(currentRequests);
   }
   const newPromise = promiseGenerator().finally(() => {
     currentRequests.splice(currentRequests.indexOf(newPromise), 1);
-  });
+    fulfilledNetworkRequests.networkRequests.push(newPromise);
+  }) as PromiseWithSpec<T, S>;
+  newPromise.spec = promiseSpec;
   currentRequests.push(newPromise);
   return newPromise;
 }
