05e4d7859338314aaadfa66252df179583e4600b
Max Patiiuk <max@patii.uk>
2023-01-10 21:04:26 -0600

    Fix excessive whitespace in WB validation messages
    Fixes #2760

--- specifyweb/frontend/js_src/lib/components/WorkBench/wbView.js
+++ specifyweb/frontend/js_src/lib/components/WorkBench/wbView.js
@@ -275,7 +275,10 @@ export const WBView = Backbone.View.extend({
               });
               this.$('.wb-validate, .wb-data-check')
                 .prop('disabled', true)
-                .prop('title', whitespaceSensitive(wbText.wbValidateUnavailable()));
+                .prop(
+                  'title',
+                  whitespaceSensitive(wbText.wbValidateUnavailable())
+                );
             } else {
               this.$('.wb-validate, .wb-data-check').prop('disabled', false);
               this.$('.wb-show-upload-view')
@@ -841,9 +844,20 @@ export const WBView = Backbone.View.extend({
      * This is the only type of validation that is done on the front-end
      */
     const newIssues = f.unique([
-      ...(isValid ? [] : [whitespaceSensitive(backEndText.failedParsingPickList({value:`"${value}"`}))]),
+      ...(isValid
+        ? []
+        : [
+            whitespaceSensitive(
+              backEndText.failedParsingPickList({ value: `"${value}"` })
+            ),
+          ]),
       ...issues.filter(
-        (issue) => !issue.endsWith(whitespaceSensitive(backEndText.failedParsingPickList({value:''})))
+        (issue) =>
+          !issue.endsWith(
+            whitespaceSensitive(
+              backEndText.failedParsingPickList({ value: '' })
+            )
+          )
       ),
     ]);
     if (JSON.stringify(issues) !== JSON.stringify(newIssues))
@@ -1336,7 +1350,9 @@ export const WBView = Backbone.View.extend({
       physicalRow,
       physicalCol,
       'issues'
-    ).some((issue) => issue.endsWith(backEndText.failedParsingPickList({value:''})));
+    ).some((issue) =>
+      issue.endsWith(backEndText.failedParsingPickList({ value: '' }))
+    );
     if (hasFrontEndValidationErrors)
       /*
        * Since isModified state has higher priority then issues, we need to
@@ -2284,15 +2300,17 @@ export const WBView = Backbone.View.extend({
     if (['NullRecord', 'PropagatedFailure', 'Matched'].includes(uploadStatus)) {
     } else if (uploadStatus === 'ParseFailures')
       statusData.failures.forEach((line) => {
-        const [issueMessage, payload, column] = line.length === 2 ? [line[0], {}, line[1]] : line;
+        const [issueMessage, payload, column] =
+          line.length === 2 ? [line[0], {}, line[1]] : line;
         setMetaCallback(
           'issues',
-          resolveValidationMessage(issueMessage,payload ?? {}),
+          whitespaceSensitive(
+            resolveValidationMessage(issueMessage, payload ?? {})
+          ),
           [column],
           resolveColumns
-        )
-        }
-      );
+        );
+      });
     else if (uploadStatus === 'NoMatch')
       setMetaCallback(
         'issues',
@@ -2303,7 +2321,9 @@ export const WBView = Backbone.View.extend({
     else if (uploadStatus === 'FailedBusinessRule')
       setMetaCallback(
         'issues',
-        resolveValidationMessage(statusData.message,statusData.payload ?? {}),
+        whitespaceSensitive(
+          resolveValidationMessage(statusData.message, statusData.payload ?? {})
+        ),
         statusData.info.columns,
         resolveColumns
       );
@@ -2510,10 +2530,7 @@ export const WBView = Backbone.View.extend({
     ) {
       const hasErrors = cellCounts.invalidCells > 0;
       uploadButton.prop('disabled', hasErrors);
-      uploadButton.attr(
-        'title',
-        hasErrors ? title : undefined
-      );
+      uploadButton.attr('title', hasErrors ? title : undefined);
     }
 
     if (this.refreshInitiatedBy) this.operationCompletedMessage(cellCounts);
@@ -2526,21 +2543,25 @@ export const WBView = Backbone.View.extend({
         cellCounts.invalidCells === 0
           ? {
               header: wbText.validationNoErrors(),
-              message: <>
-                {wbText.validationNoErrorsDescription()}
-                <br />
-                <br />
-                {wbText.validationReEditWarning()}
-              </>,
+              message: (
+                <>
+                  {wbText.validationNoErrorsDescription()}
+                  <br />
+                  <br />
+                  {wbText.validationReEditWarning()}
+                </>
+              ),
             }
           : {
               header: wbText.validationErrors(),
-              message: <>
-                {wbText.validationErrorsDescription()}
-                <br />
-                <br />
-                {wbText.validationReEditWarning()}
-              </>,
+              message: (
+                <>
+                  {wbText.validationErrorsDescription()}
+                  <br />
+                  <br />
+                  {wbText.validationReEditWarning()}
+                </>
+              ),
             },
       upload:
         cellCounts.invalidCells === 0
@@ -2550,12 +2571,14 @@ export const WBView = Backbone.View.extend({
             }
           : {
               header: wbText.uploadErrors(),
-              message: <>
-                {wbText.uploadErrorsDescription()}
-                <br />
-                <br />
-                {wbText.uploadErrorsSecondDescription()}
-              </>,
+              message: (
+                <>
+                  {wbText.uploadErrorsDescription()}
+                  <br />
+                  <br />
+                  {wbText.uploadErrorsSecondDescription()}
+                </>
+              ),
             },
       unupload: {
         header: wbText.dataSetRollback(),
