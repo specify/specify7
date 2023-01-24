specify/specify7

commit 9e7e3cec2c8c5797915a0112b105e9bb303484f9

Refactor the value comparison logic
Simplify it to decrease bug surface

specifyweb/frontend/js_src/lib/components/DataModel/resourceApi.js
@@ -218,23 +218,18 @@ function eventHandlerForToOne(related, field) {
               typeof (oldValue??'') !== 'object' &&
               typeof (newValue??'') !== 'object'
             ) {
-                if(oldValue === newValue) return this;
-                else if(
+                if(
                   /*
-                   * Don't trigger unload protect if value changed from
-                   * string to number (back-end sends certain numeric fields
-                   * as strings. Front-end converts those to numbers)
+                   * Don't trigger unload protect if:
+                   *  - value didn't change
+                   *  - value changed from string to number (back-end sends
+                   *    decimal numeric fields as string. Front-end converts
+                   *    those to numbers)
+                   *  - value was trimmed
                    * REFACTOR: this logic should be moved to this.parse()
                    * TEST: add test for "5A" case
                    */
-                  oldValue.toString() === newValue.toString()
-                )
-                    options ??= {silent: true};
-                else if(
-                  // Trimming the value shouldn't trigger the unload protect
-                  typeof oldValue === 'string' &&
-                  typeof newValue === 'string' &&
-                  oldValue === newValue.trim()
+                  oldValue?.toString() === newValue?.toString().trim()
                 )
                     options ??= {silent: true};
             }

commit 9268cac1e8dd8919de8212edabd1b57e16ac39e1

Do not trigger unload protect if value did not change
Fixes #2868
Caused by 9e7e3ce

specifyweb/frontend/js_src/lib/components/DataModel/resourceApi.js
@@ -218,7 +218,8 @@ function eventHandlerForToOne(related, field) {
               typeof (oldValue??'') !== 'object' &&
               typeof (newValue??'') !== 'object'
             ) {
-                if(
+                if (oldValue === newValue) return this;
+                else if(
                   /*
                    * Don't trigger unload protect if:
                    *  - value didn't change