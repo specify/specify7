607bae7b5a11b684aec357f6f3b5282fa3b2797c
melton-jason <meltonjason12@yahoo.com>
2023-01-31 09:15:23 -0600

    Add code comments

--- specifyweb/middleware/general.py
+++ specifyweb/middleware/general.py
@@ -1,9 +1,9 @@
 import traceback
 import json
-from typing import Optional, Dict, List
+from typing import Any, Optional, Dict, List
 from django import http
 from django.core import serializers
-from django.db.models import QuerySet
+from django.db import models
 from django.conf import settings
 from django.forms.models import model_to_dict
 
@@ -22,7 +22,7 @@ class SpecifyExceptionWrapper():
             'traceback' : traceback.format_exc()
             }
 
-        if isinstance(self.data, QuerySet):
+        if isinstance(self.data, models.QuerySet):
             result['data'] = serialize_django_obj(self.data)
 
         from ..specify import api
@@ -61,20 +61,37 @@ class GeneralMiddleware:
                 return http.HttpResponse(exception.to_json(), status=exception.status_code)
 
 
-def serialize_django_obj(django_obj) -> Dict[str, str or int or List[str]]:
-    from django.db.models.base import ModelState
+def serialize_django_obj(django_obj: models.QuerySet or models.Model) -> List[Dict[str, Any]] or Dict[str, Any]:
+    """Attempt to serialize two common objects in Django, a Queryset or a Model. 
+    If the object is a Queryset, return a list of dictonaries containing the important (non-null) fields
+    Similarly, if the object is a single Model, return a dictonary containing every field
+    Otherwise, return a string version of <django_obj>
+    """
 
-    if isinstance(django_obj, QuerySet):
-        parsed_data = json.loads(serializers.serialize('json', django_obj, use_natural_foreign_keys=True))
+    if isinstance(django_obj, models.QuerySet):
+        # We use a django serializer to convert the queryset to a string and then convert 
+        # the string to a json object.
+        # The loaded json object is a list of dictonaries which all have the keys 'model', 'pk', and 'fields'
+        # See the Django documentation for more information
+        #   https://docs.djangoproject.com/en/2.2/topics/serialization/#json-1
+
+        raw_queryset = json.loads(serializers.serialize('json', django_obj))
         parsed_data = [
             {
                 "Model" : resource['model'].split(".")[1].capitalize(),
                 "Id" : resource['pk'],
                 "Non-Null Fields" : [f"{field} : {value}" for field, value in resource['fields'].items() if value is not None]
             }
-            for resource in parsed_data
+            for resource in raw_queryset
         ]
-    elif hasattr(django_obj, "_state") and isinstance(django_obj.__dict__['_state'], ModelState):
+    
+    # We can not directly check if the object is a model with isinstance(). However, every model object
+    # contains a '_state' attribute which is of type ModelState.
+    
+    # Both cases for Querysets and Models do not include to-many relationship data for fields
+    elif hasattr(django_obj, "_state") and isinstance(django_obj.__dict__['_state'], models.base.ModelState):
         parsed_data = model_to_dict(django_obj)
-    else: return django_obj
+    else: 
+        # If the passed-in object is unknown, return the object as a string
+        return str(django_obj)
     return parsed_data
