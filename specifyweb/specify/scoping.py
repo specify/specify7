c9eb1378f652f66973c48c3df42d697204f6fe23
melton-jason <meltonjason12@yahoo.com>
2023-04-07 10:49:11 -0500

    Properly infer scope on backend and remove redunant methods

--- specifyweb/specify/scoping.py
+++ specifyweb/specify/scoping.py
@@ -12,12 +12,11 @@ class Scoping(namedtuple('Scoping', 'obj')):
 
     def __call__(self):
         table = self.obj.__class__.__name__.lower()
-        scope =  getattr(self, table, lambda: None)()
-        if scope is None:
-            inferred_scope = self._infer_scope()
-            if inferred_scope is None: return self._default_institution_scope()
+        try:
+            scope =  getattr(self, table)()
+        except AttributeError:
+            return self._infer_scope
         return scope
-
 
 ################################################################################
 
@@ -30,46 +29,11 @@ class Scoping(namedtuple('Scoping', 'obj')):
 
     def agent(self): return self._simple_division_scope()
 
-    def borrow(self): return self._simple_collection_scope()
-
-    def collectingevent(self): return self._simple_discipline_scope()
-
-    def collectionobject(self): return self._simple_collection_scope()
-
-    def conservdescription(self): return self._simple_division_scope()
-
     def conservevent(self): return Scoping(self.obj.conservdescription)()
-
-    def dnasequence(self): return self._simple_collection_scope()
-
-    def dnasequencing(self): return self._simple_collection_scope()
-
-    def exchangein(self): return self._simple_division_scope()
-
-    def exchangeout(self): return self._simple_division_scope()
-
-    def fieldnotebook(self): return self._simple_discipline_scope()
 
     def fieldnotebookpage(self): return Scoping(self.obj.pageset)()
 
     def fieldnotebookpageset(self): return Scoping(self.obj.fieldnotebook)()
-
-    def gift(self): return self._simple_discipline_scope()
-
-    def loan(self): return self._simple_discipline_scope()
-
-    def locality(self): return self._simple_discipline_scope()
-
-    def permit(self):
-        return INSTITUTION_SCOPE, self.obj.institution_id
-
-    def preparation(self): return self._simple_collection_scope()
-
-    def referencework(self):
-        institution = models.Institution.objects.get()
-        return INSTITUTION_SCOPE, institution.id
-
-    def repositoryagreement(self): return self._simple_division_scope()
 
     def taxon(self):
         return DISCIPLINE_SCOPE, self.obj.definition.discipline.id
@@ -89,6 +53,8 @@ class Scoping(namedtuple('Scoping', 'obj')):
         if hasattr(self.obj, "division_id"): return self._simple_division_scope()
         if hasattr(self.obj, "discipline_id") : return self._simple_discipline_scope()
         if hasattr(self.obj, "collectionmemberid"): return self._simple_collection_scope()
+        if hasattr(self.obj, "collection_id"): return self._simple_collection_scope()
+        return self._default_institution_scope()
 
     # If the table has no scope, and scope can not be inferred then scope to institution
     def _default_institution_scope(self):
