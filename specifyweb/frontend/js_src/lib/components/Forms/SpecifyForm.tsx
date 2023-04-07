57893f293822fa6acd4d72ec00b7d27617a7c259
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2023-04-07 19:24:16 +0000

    Lint code with ESLint and Prettier
    Triggered by 67d42c5c15be11ee7c888686c5a0adf3c3c0dc34 on branch refs/heads/issue-1543

--- specifyweb/frontend/js_src/lib/components/Forms/SpecifyForm.tsx
+++ specifyweb/frontend/js_src/lib/components/Forms/SpecifyForm.tsx
@@ -91,12 +91,12 @@ export function SpecifyForm<SCHEMA extends AnySchema>({
   const formRef = React.useRef<HTMLDivElement | null>(null);
 
   React.useEffect(() => {
-    if (focusFirstFieldPref === true) {
+    if (focusFirstFieldPref) {
       const timeoutId = setTimeout(() => {
         if (formRef.current === null) return;
         const firstFocusableElement = formRef.current.querySelector(
           'button, a, input:not([type="hidden"]), select, textarea, [tabindex]:not([tabindex="-1"])'
-        ) as HTMLElement;
+        )!;
 
         firstFocusableElement?.focus();
       }, 100);
