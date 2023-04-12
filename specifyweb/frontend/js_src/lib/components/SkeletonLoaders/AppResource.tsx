261d3fc5770828adb61d7b056a353ceb4b960f63
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2023-04-12 14:02:06 +0000

    Lint code with ESLint and Prettier
    Triggered by 0e09e7aa599dbdef3553d8817f6b7c32a874f909 on branch refs/heads/issue-2998

--- specifyweb/frontend/js_src/lib/components/SkeletonLoaders/AppResource.tsx
+++ specifyweb/frontend/js_src/lib/components/SkeletonLoaders/AppResource.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
+
 import { Skeleton } from './Skeleton';
 
-export const AppResourceSkeleton = () => (
-  <Skeleton>
-    <rect height="8" rx="3" ry="3" width="141" x="28" y="1" />
-    <rect height="10" rx="2" ry="2" width="10" x="10" y="0" />
-    <rect height="10" rx="2" ry="2" width="10" x="190" y="0" />
-    <rect height="10" rx="2" ry="2" width="15" x="203" y="0" />
-    <rect height="10" rx="2" ry="2" width="15" x="220.5" y="0" />
-    <rect height="8" rx="2" ry="2" width="195" x="28" y="28" />
-    <rect height="8" rx="2" ry="2" width="195" x="28" y="48" />
-    <rect height="8" rx="2" ry="2" width="195" x="28" y="68" />
-    <rect height="8" rx="2" ry="2" width="195" x="28" y="88" />
-    <rect height="8" rx="2" ry="2" width="195" x="28" y="108" />
-    <rect height="8" rx="2" ry="2" width="195" x="28" y="128" />
-    <rect height="8" rx="2" ry="2" width="195" x="28" y="148" />
-    <rect height="8" rx="2" ry="2" width="195" x="28" y="168" />
-    <rect height="8" rx="2" ry="2" width="195" x="28" y="188" />
-    <rect height="8" rx="2" ry="2" width="195" x="28" y="208" />
-    <rect height="8" rx="2" ry="2" width="195" x="28" y="228" />
-    <rect height="8" rx="2" ry="2" width="195" x="28" y="248" />
-  </Skeleton>
-);
+export function AppResourceSkeleton() {
+  return (
+    <Skeleton>
+      <rect height="8" rx="3" ry="3" width="141" x="28" y="1" />
+      <rect height="10" rx="2" ry="2" width="10" x="10" y="0" />
+      <rect height="10" rx="2" ry="2" width="10" x="190" y="0" />
+      <rect height="10" rx="2" ry="2" width="15" x="203" y="0" />
+      <rect height="10" rx="2" ry="2" width="15" x="220.5" y="0" />
+      <rect height="8" rx="2" ry="2" width="195" x="28" y="28" />
+      <rect height="8" rx="2" ry="2" width="195" x="28" y="48" />
+      <rect height="8" rx="2" ry="2" width="195" x="28" y="68" />
+      <rect height="8" rx="2" ry="2" width="195" x="28" y="88" />
+      <rect height="8" rx="2" ry="2" width="195" x="28" y="108" />
+      <rect height="8" rx="2" ry="2" width="195" x="28" y="128" />
+      <rect height="8" rx="2" ry="2" width="195" x="28" y="148" />
+      <rect height="8" rx="2" ry="2" width="195" x="28" y="168" />
+      <rect height="8" rx="2" ry="2" width="195" x="28" y="188" />
+      <rect height="8" rx="2" ry="2" width="195" x="28" y="208" />
+      <rect height="8" rx="2" ry="2" width="195" x="28" y="228" />
+      <rect height="8" rx="2" ry="2" width="195" x="28" y="248" />
+    </Skeleton>
+  );
+}
--- specifyweb/frontend/js_src/lib/components/SkeletonLoaders/AttachmentPlugin.tsx
+++ specifyweb/frontend/js_src/lib/components/SkeletonLoaders/AttachmentPlugin.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
+
 import { Skeleton } from './Skeleton';
 
-export const AttachmentPluginSkeleton = () => (
-  <Skeleton viewBox="0 0 220 400">
-    <rect height="140" rx="2" ry="2" width="120" x="7" y="0" />
-    <rect height="7" rx="2" ry="2" width="36" x="135" y="0" />
-    <rect height="12" rx="2" ry="2" width="12" x="191" y="0" />
-    <rect height="5" rx="2" ry="2" width="68" x="135" y="18" />
-    <rect height="5" rx="2" ry="2" width="68" x="135" y="28" />
-    <rect height="5" rx="2" ry="2" width="68" x="135" y="38" />
-    <rect height="5" rx="2" ry="2" width="68" x="135" y="48" />
-    <rect height="5" rx="2" ry="2" width="68" x="135" y="58" />
-    <rect height="5" rx="2" ry="2" width="33" x="135" y="68" />
-    <rect height="5" rx="2" ry="2" width="68" x="135" y="78" />
-    <rect height="5" rx="2" ry="2" width="68" x="135" y="88" />
-    <rect height="5" rx="2" ry="2" width="68" x="135" y="98" />
-    <rect height="15" rx="2" ry="2" width="30" x="135" y="125" />
-    <rect height="15" rx="2" ry="2" width="30" x="173" y="125" />
-  </Skeleton>
-);
+export function AttachmentPluginSkeleton() {
+  return (
+    <Skeleton viewBox="0 0 220 400">
+      <rect height="140" rx="2" ry="2" width="120" x="7" y="0" />
+      <rect height="7" rx="2" ry="2" width="36" x="135" y="0" />
+      <rect height="12" rx="2" ry="2" width="12" x="191" y="0" />
+      <rect height="5" rx="2" ry="2" width="68" x="135" y="18" />
+      <rect height="5" rx="2" ry="2" width="68" x="135" y="28" />
+      <rect height="5" rx="2" ry="2" width="68" x="135" y="38" />
+      <rect height="5" rx="2" ry="2" width="68" x="135" y="48" />
+      <rect height="5" rx="2" ry="2" width="68" x="135" y="58" />
+      <rect height="5" rx="2" ry="2" width="33" x="135" y="68" />
+      <rect height="5" rx="2" ry="2" width="68" x="135" y="78" />
+      <rect height="5" rx="2" ry="2" width="68" x="135" y="88" />
+      <rect height="5" rx="2" ry="2" width="68" x="135" y="98" />
+      <rect height="15" rx="2" ry="2" width="30" x="135" y="125" />
+      <rect height="15" rx="2" ry="2" width="30" x="173" y="125" />
+    </Skeleton>
+  );
+}
