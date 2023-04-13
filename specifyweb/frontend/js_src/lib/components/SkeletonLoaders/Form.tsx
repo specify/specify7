4fa777c65d79bb653318ca06723a55d2b207a8b9
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2023-04-13 09:51:59 -0700

    Fix unnecessary scroll in form skeleton

--- specifyweb/frontend/js_src/lib/components/SkeletonLoaders/Form.tsx
+++ specifyweb/frontend/js_src/lib/components/SkeletonLoaders/Form.tsx
@@ -3,22 +3,22 @@ import { Skeleton } from './Skeleton';
 
 export function FormSkeleton() {
   return (
-    <div className="w-[120vh]">
+    <div className="w-full">
       {Array.from({ length: 3 }, () => (
         <div className="justify-beween m-1 mb-8 flex flex-col flex-wrap gap-4">
-          <div className="flex flex-wrap items-center justify-between gap-2">
-            <div className="flex items-center gap-2">
+          <div className="flex flex-wrap items-center justify-between gap-8">
+            <div className="m-4 flex items-center gap-2">
               <Skeleton.Line />
               <Skeleton.SmallRectangle />
             </div>
-            <div className="flex gap-2">
+            <div className="m-4 flex gap-2">
               <Skeleton.Line />
               <Skeleton.SmallRectangle />
               <Skeleton.SmallCircle />
               <Skeleton.SmallCircle />
               <Skeleton.SmallCircle />
             </div>
-            <div className="flex items-center gap-2">
+            <div className="m-4 flex items-center gap-2">
               <Skeleton.Line />
               <Skeleton.SmallRectangle />
             </div>
