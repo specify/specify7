1228f98232835b3c8ef93f4ab611c1417d5e2182
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2023-05-12 11:48:52 -0400

    Replace describe tests with theorie tests

--- specifyweb/frontend/js_src/lib/utils/__tests__/hexToHsl.test.ts
+++ specifyweb/frontend/js_src/lib/utils/__tests__/hexToHsl.test.ts
@@ -1,39 +1,11 @@
+import { theories } from '../../tests/utils';
 import { hexToHsl } from '../utils';
 
-describe('hexToHsl', () => {
-  test('should return HSL for red color', () => {
-    const hexColor = '#ff0000';
-    const expectedHsl = { hue: 0, saturation: 100, lightness: 50 };
-    expect(hexToHsl(hexColor)).toEqual(expectedHsl);
-  });
-
-  test('should return HSL for hex color #000000', () => {
-    const hexColor = '#000000';
-    const expectedHsl = { hue: 0, saturation: 0, lightness: 0 };
-    expect(hexToHsl(hexColor)).toEqual(expectedHsl);
-  });
-
-  test('should return HSL for hex color #ffffff', () => {
-    const hexColor = '#ffffff';
-    const expectedHsl = { hue: 0, saturation: 0, lightness: 100 };
-    expect(hexToHsl(hexColor)).toEqual(expectedHsl);
-  });
-
-  test('should return HSL for hex color #8368e3', () => {
-    const hexColor = '#8368e3';
-    const expectedHsl = { hue: 253, saturation: 69, lightness: 65 };
-    expect(hexToHsl(hexColor)).toEqual(expectedHsl);
-  });
-
-  test('should return HSL for hex color #b1ff14', () => {
-    const hexColor = '#b1ff14';
-    const expectedHsl = { hue: 80, saturation: 100, lightness: 54 };
-    expect(hexToHsl(hexColor)).toEqual(expectedHsl);
-  });
-
-  test('should return HSL for hex color #f78605', () => {
-    const hexColor = '#f78605';
-    const expectedHsl = { hue: 32, saturation: 96, lightness: 49 };
-    expect(hexToHsl(hexColor)).toEqual(expectedHsl);
-  });
-});
+theories(hexToHsl, [
+  { in: ['#f78605'], out: { hue: 32, saturation: 96, lightness: 49 } },
+  { in: ['#b1ff14'], out: { hue: 80, saturation: 100, lightness: 54 } },
+  { in: ['#8368e3'], out: { hue: 253, saturation: 69, lightness: 65 } },
+  { in: ['#ffffff'], out: { hue: 0, saturation: 0, lightness: 100 } },
+  { in: ['#000000'], out: { hue: 0, saturation: 0, lightness: 0 } },
+  { in: ['#ff0000'], out: { hue: 0, saturation: 100, lightness: 50 } },
+]);
