specify/specify7

commit d9a13379bcb1d743d1759e4fdca5b965e4777102

Integrate business rules with agent merging

specifyweb/frontend/js_src/lib/components/FormFields/index.tsx
@@ -112,7 +112,7 @@ const fieldRenderers: {
           rows={formType === 'formTable' ? 1 : rows}
           value={value?.toString() ?? ''}
           onBlur={(): void => updateValue(value?.toString() ?? '')}
-          onValueChange={(value): void => updateValue(value, false)}
+          onValueChange={updateValue}
         />
       </ErrorBoundary>
     );

specifyweb/frontend/js_src/lib/components/Merging/index.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 import { useSearchParameter } from '../../hooks/navigation';
-import { useSaveBlockers } from '../../hooks/resource';
 import { useAsyncState } from '../../hooks/useAsyncState';
 import { useCachedState } from '../../hooks/useCachedState';
-import { useId } from '../../hooks/useId';
 import { commonText } from '../../localization/common';
 import { mergingText } from '../../localization/merging';
 import { treeText } from '../../localization/tree';
@@ -20,7 +18,6 @@ import { ErrorMessage } from '../Atoms';
 import { Button } from '../Atoms/Button';
 import { Input, Label } from '../Atoms/Form';
 import { Link } from '../Atoms/Link';
-import { Submit } from '../Atoms/Submit';
 import { LoadingContext } from '../Core/Contexts';
 import type { AnySchema, SerializedResource } from '../DataModel/helperTypes';
 import type { SpecifyResource } from '../DataModel/legacyTypes';
@@ -29,13 +26,14 @@ import { deserializeResource } from '../DataModel/serializers';
 import type { SpecifyTable } from '../DataModel/specifyTable';
 import { getTable } from '../DataModel/tables';
 import type { Tables } from '../DataModel/types';
-import { SaveBlockedDialog } from '../Forms/Save';
+import { SaveButton } from '../Forms/Save';
 import { Dialog } from '../Molecules/Dialog';
 import { userPreferences } from '../Preferences/userPreferences';
 import { formatUrl } from '../Router/queryString';
 import { OverlayContext, OverlayLocation } from '../Router/Router';
 import { autoMerge } from './autoMerge';
 import { CompareRecords } from './Compare';
+import { useId } from '../../hooks/useId';
 
 const recordMergingTables = new Set<keyof Tables>(['Agent']);
 
@@ -113,7 +111,7 @@ export function MergingDialog(): JSX.Element | null {
     [ids, setIds]
   );
 
-  const handleDismiss = (dismissedId: number) =>
+  const handleDismiss = (dismissedId: number): void =>
     setIds(ids.filter((id) => id !== dismissedId).join(','));
 
   return table === undefined ? null : (
@@ -142,7 +140,9 @@ function Merging({
     [ids, handleClose]
   );
 
-  const id = useId('merging-dialog');
+  const [form, setForm] = React.useState<HTMLFormElement | null>(null);
+  const formId = useId('merging')('form');
+
   const loading = React.useContext(LoadingContext);
   const [error, setError] = React.useState<string | undefined>(undefined);
   const [merged, setMerged] = useAsyncState(
@@ -154,9 +154,7 @@ function Merging({
               table,
               initialRecords.current,
               userPreferences.get('recordMerging', 'behavior', 'autoPopulate')
-            ).then((merged) =>
-              deserializeResource(merged as SerializedResource<AnySchema>)
-            ),
+            ).then((merged) => deserializeResource(merged)),
       [table, records]
     ),
     true
@@ -170,9 +168,7 @@ function Merging({
             onClick={(): void =>
               loading(
                 autoMerge(table, records, false)
-                  .then((merged) =>
-                    deserializeResource(merged as SerializedResource<AnySchema>)
-                  )
+                  .then((merged) => deserializeResource(merged))
                   .then(setMerged)
               )
             }
@@ -184,14 +180,15 @@ function Merging({
           <Button.BorderedGray onClick={handleClose}>
             {commonText.cancel()}
           </Button.BorderedGray>
-          <MergeButton id={id} mergeResource={merged} />
+          <MergeButton form={form} mergeResource={merged} />
         </>
       }
       onClose={handleClose}
     >
       {typeof error === 'string' && <ErrorMessage>{error}</ErrorMessage>}
       <CompareRecords
-        formId={id('form')}
+        formRef={setForm}
+        id={formId}
         merged={merged}
         records={records}
         table={table}
@@ -252,38 +249,23 @@ function Merging({
 }
 
 function MergeButton<SCHEMA extends AnySchema>({
-  id,
+  form,
   mergeResource,
 }: {
-  readonly id: (suffix: string) => string;
+  readonly form: HTMLFormElement | null;
   readonly mergeResource: SpecifyResource<SCHEMA>;
-}): JSX.Element {
-  const [formId] = React.useState(id('form'));
-  const [saveBlocked, setSaveBlocked] = React.useState(false);
-  const [showSaveBlockedDialog, setShowBlockedDialog] = React.useState(false);
-
-  const blockers = useSaveBlockers({ resource: mergeResource });
-
-  React.useEffect(() => {
-    setSaveBlocked(!blockers.every((blocker) => blocker.deferred));
-  });
-
-  return (
-    <>
-      {saveBlocked ? (
-        <Submit.Red className="cursor-not-allowed">
-          {treeText.merge()}
-        </Submit.Red>
-      ) : (
-        <Submit.Blue form={formId}>{treeText.merge()}</Submit.Blue>
-      )}
-      {showSaveBlockedDialog && (
-        <SaveBlockedDialog
-          resource={mergeResource}
-          onClose={() => setShowBlockedDialog(false)}
-        />
-      )}
-    </>
+}): JSX.Element | null {
+  return form === null ? null : (
+    <SaveButton
+      form={form}
+      label={treeText.merge()}
+      resource={mergeResource}
+      // Prevent regular form submit in favor of our custom
+      onSaving={(): false => {
+        form.requestSubmit();
+        return false;
+      }}
+    />
   );
 }
 

specifyweb/frontend/js_src/lib/components/DataModel/saveBlockers.tsx
@@ -26,7 +26,7 @@ type ResourceBlockers = {
   readonly blockers: RA<Blocker>;
   readonly listeners: WeakMap<
     LiteralField | Relationship,
-    RA<(blocker: Blocker) => boolean>
+    RA<(blocker: BlockerWithResource) => boolean>
   >;
 };
 
@@ -34,12 +34,14 @@ const blockerEvents = eventListener<{
   readonly change: SpecifyResource<AnySchema>;
 }>();
 
-export type Blocker = {
-  readonly field: RA<LiteralField | Relationship>;
+type Blocker = {
+  readonly field: LiteralField | Relationship;
   readonly message: string;
 };
-type BlockerWithResource = Blocker & {
+export type BlockerWithResource = {
+  readonly field: RA<LiteralField | Relationship>;
   readonly resources: RA<SpecifyResource<AnySchema>>;
+  readonly message: string;
 };
 
 export function useSaveBlockers(
@@ -86,14 +88,13 @@ export function useSaveBlockers(
             ? errors(getFieldBlockers(resource, field))
             : errors;
         const blockers = f.unique(resolvedErrors).map((error) => ({
-          field: [field],
+          field,
           message: error,
         }));
         const resourceBlockers = getResourceBlockers(resource)!;
         const newBlockers = [
           ...resourceBlockers.blockers.filter(
-            (blocker) =>
-              blocker.field.length === 1 && blocker.field[0] !== field
+            (blocker) => blocker.field !== field
           ),
           ...blockers,
         ];
@@ -114,9 +115,7 @@ function getFieldBlockers(
 ): RA<string> {
   const blockers = saveBlockers.get(resource)?.blockers ?? [];
   return blockers
-    .filter(
-      (blocker) => blocker.field.length === 1 && blocker.field[0] === field
-    )
+    .filter((blocker) => blocker.field === field)
     .map(({ message }) => message);
 }
 
@@ -129,7 +128,8 @@ function getResourceBlockers(
 }
 
 export function useAllSaveBlockers(
-  resource: SpecifyResource<AnySchema> | undefined
+  resource: SpecifyResource<AnySchema> | undefined,
+  filterBlockers?: LiteralField | Relationship
 ): RA<BlockerWithResource> {
   const [blockers, setBlockers] = React.useState<RA<BlockerWithResource>>([]);
   React.useEffect(
@@ -140,7 +140,7 @@ export function useAllSaveBlockers(
         ? setBlockers([])
         : blockerEvents.on(
             'change',
-            () => setBlockers(getAllBlockers(resource)),
+            () => setBlockers(getAllBlockers(resource, filterBlockers)),
             true
           ),
     [resource]
@@ -152,25 +152,37 @@ export function useAllSaveBlockers(
  * Recursively get all blockers for current resource and all children resources
  */
 const getAllBlockers = (
-  resource: SpecifyResource<AnySchema>
+  resource: SpecifyResource<AnySchema>,
+  filterBlockers?: LiteralField | Relationship
 ): RA<BlockerWithResource> => [
-  ...(saveBlockers.get(resource)?.blockers.map((blocker) => ({
-    ...blocker,
-    resources: [resource],
-  })) ?? []),
-  ...Object.entries(resource.dependentResources).flatMap(
-    ([fieldName, collectionOrResource]) =>
+  ...(saveBlockers
+    .get(resource)
+    ?.blockers.filter(
+      ({ field }) => filterBlockers === undefined || field === filterBlockers
+    )
+    .map(({ field, message }) => ({
+      field: [field],
+      message,
+      resources: [resource],
+    })) ?? []),
+  ...Object.entries(resource.dependentResources)
+    .filter(
+      ([field]) =>
+        filterBlockers === undefined ||
+        field.toLowerCase() === filterBlockers?.name.toLowerCase()
+    )
+    .flatMap(([fieldName, collectionOrResource]) =>
       (collectionOrResource instanceof ResourceBase
         ? getAllBlockers(collectionOrResource as SpecifyResource<AnySchema>)
         : (collectionOrResource as Collection<AnySchema>).models.flatMap(
-            getAllBlockers
+            f.unary(getAllBlockers)
           )
       ).map(({ field, resources, message }) => ({
         field: [resource.specifyTable.strictGetField(fieldName), ...field],
         resources: [...resources, resource],
         message,
       }))
-  ),
+    ),
 ];
 
 /**
@@ -181,7 +193,7 @@ const getAllBlockers = (
 export function useBlockerHandler(
   resource: SpecifyResource<AnySchema> | undefined,
   field: LiteralField | Relationship | undefined,
-  callback: (blocker: Blocker) => boolean
+  callback: (blocker: BlockerWithResource) => boolean
 ): void {
   React.useEffect(() => {
     if (resource === undefined || field === undefined) return undefined;
@@ -201,20 +213,20 @@ export function useBlockerHandler(
  * For each blocker, fire the blocker handler if set. Return the first blocker
  * that does not have any handler function setup.
  */
-export function findUnclaimedBlocker(
+export const findUnclaimedBlocker = (
   blockers: RA<BlockerWithResource>
-): Blocker | undefined {
-  return blockers.find((blocker) => {
+): BlockerWithResource | undefined =>
+  blockers.find((blocker) => {
     const {
       field,
       resources: [resource, ...children],
       message,
     } = blocker;
-    if (field.length === 1) {
-      const listeners = getResourceBlockers(resource)?.listeners;
-      const currentListeners = listeners.get(field[0]) ?? [];
-      return currentListeners.some((listener) => listener(blocker));
-    } else
+
+    const listeners = getResourceBlockers(resource)?.listeners;
+    const currentListeners = listeners.get(field[0]) ?? [];
+    if (currentListeners.some((listener) => listener(blocker))) return true;
+    else if (field.length > 1)
       return (
         findUnclaimedBlocker([
           {
@@ -224,5 +236,5 @@ export function findUnclaimedBlocker(
           },
         ]) !== undefined
       );
+    else return false;
   });
-}

specifyweb/frontend/js_src/lib/components/Errors/assert.ts
@@ -32,6 +32,7 @@ export function error(message: Error | string, ...rest: RA<unknown>): never {
 /**
  * Throw an error, but only if in development mode.
  * Otherwise, log the error
+ *
  * REFACTOR: use this instead of error whenever possible
  */
 export const softError =

specifyweb/frontend/js_src/lib/components/Forms/BaseResourceView.tsx
@@ -179,6 +179,7 @@ export const FormContext = React.createContext<
   ]
 >([
   {
+    // FIXME: remove if not used
     triedToSubmit: false,
   },
   undefined,

specifyweb/frontend/js_src/lib/components/Forms/Save.tsx
@@ -15,11 +15,12 @@ import { Submit } from '../Atoms/Submit';
 import { LoadingContext } from '../Core/Contexts';
 import type { AnySchema } from '../DataModel/helperTypes';
 import type { SpecifyResource } from '../DataModel/legacyTypes';
-import type { Blocker } from '../DataModel/saveBlockers';
+import type { BlockerWithResource } from '../DataModel/saveBlockers';
 import {
   findUnclaimedBlocker,
   useAllSaveBlockers,
 } from '../DataModel/saveBlockers';
+import type { LiteralField, Relationship } from '../DataModel/specifyField';
 import type { Tables } from '../DataModel/types';
 import { error } from '../Errors/assert';
 import { errorHandledBy } from '../Errors/FormatError';
@@ -47,14 +48,17 @@ export const saveFormUnloadProtect = formsText.unsavedFormUnloadProtect();
 export function SaveButton<SCHEMA extends AnySchema = AnySchema>({
   resource,
   form,
+  label: saveLabel = commonText.save(),
   disabled = false,
   saveRequired: externalSaveRequired = false,
+  filterBlockers,
   onSaving: handleSaving,
   onSaved: handleSaved,
   onAdd: handleAdd,
 }: {
   readonly resource: SpecifyResource<SCHEMA>;
   readonly form: HTMLFormElement;
+  readonly label?: LocalizedString;
   readonly disabled?: boolean;
   /*
    * Can enable Save button even if no save is required (i.e., when there were
@@ -67,6 +71,8 @@ export function SaveButton<SCHEMA extends AnySchema = AnySchema>({
   ) => false | undefined | void;
   readonly onSaved?: () => void;
   readonly onAdd?: (newResource: SpecifyResource<SCHEMA>) => void;
+  // Only display save blockers for a given field
+  readonly filterBlockers?: LiteralField | Relationship;
 }): JSX.Element {
   const id = useId('save-button');
   const saveRequired = useIsModified(resource);
@@ -75,13 +81,13 @@ export function SaveButton<SCHEMA extends AnySchema = AnySchema>({
     saveFormUnloadProtect
   );
 
-  const blockers = useAllSaveBlockers(resource);
+  const blockers = useAllSaveBlockers(resource, filterBlockers);
   const saveBlocked = blockers.length > 0;
 
   const [isSaving, setIsSaving] = React.useState(false);
-  const [shownBlocker, setShownBlocker] = React.useState<Blocker | undefined>(
-    undefined
-  );
+  const [shownBlocker, setShownBlocker] = React.useState<
+    BlockerWithResource | undefined
+  >(undefined);
   const [isSaveConflict, hasSaveConflict] = useBooleanState();
 
   const [formId, setFormId] = React.useState(id('form'));
@@ -238,7 +244,7 @@ export function SaveButton<SCHEMA extends AnySchema = AnySchema>({
             form.classList.remove(className.notSubmittedForm)
           }
         >
-          {commonText.save()}
+          {saveLabel}
         </SubmitComponent>
       )}
       {isSaveConflict ? (
@@ -263,11 +269,11 @@ export function SaveButton<SCHEMA extends AnySchema = AnySchema>({
   );
 }
 
-export function SaveBlockedDialog({
+function SaveBlockedDialog({
   blocker: { field, message },
   onClose: handleClose,
 }: {
-  readonly blocker: Blocker;
+  readonly blocker: BlockerWithResource;
   readonly onClose: () => void;
 }): JSX.Element {
   const pathPreview = React.useMemo(

specifyweb/frontend/js_src/lib/components/Merging/Compare.tsx
@@ -15,14 +15,16 @@ import { CompareField } from './CompareField';
 import { MergingHeader } from './Header';
 
 export function CompareRecords({
-  formId,
+  id,
+  formRef,
   table,
   records,
   merged,
   onMerge: handleMerge,
   onDismiss: handleDismiss,
 }: {
-  readonly formId: string;
+  readonly id: string;
+  readonly formRef: (form: HTMLFormElement | null) => void;
   readonly table: SpecifyTable;
   readonly records: RA<SerializedResource<AnySchema>>;
   readonly merged: SpecifyResource<AnySchema>;
@@ -39,7 +41,8 @@ export function CompareRecords({
   const conformation = useMergeConformation(table, resources);
   return (
     <MergeContainer
-      id={formId}
+      id={id}
+      formRef={formRef}
       recordCount={records.length}
       onSubmit={(): void => handleMerge(merged, resources)}
     >
@@ -63,18 +66,25 @@ export function CompareRecords({
 }
 
 export function MergeContainer({
+  formRef,
   id,
   recordCount,
   children,
   onSubmit: handleSubmit,
 }: {
+  readonly formRef: (form: HTMLFormElement | null) => void;
   readonly id: string;
   readonly recordCount: number;
   readonly children: React.ReactNode;
   readonly onSubmit: () => void;
 }): JSX.Element {
   return (
-    <Form className="overflow-hidden" id={id} onSubmit={handleSubmit}>
+    <Form
+      className="overflow-hidden"
+      id={id}
+      forwardRef={formRef}
+      onSubmit={handleSubmit}
+    >
       <table
         className={`
           grid-table

specifyweb/frontend/js_src/lib/components/Merging/CompareSubView.tsx
@@ -17,14 +17,18 @@ import {
 import { Button } from '../Atoms/Button';
 import { className } from '../Atoms/className';
 import { icons } from '../Atoms/Icons';
-import { Submit } from '../Atoms/Submit';
 import type { AnySchema } from '../DataModel/helperTypes';
 import type { SpecifyResource } from '../DataModel/legacyTypes';
 import { resourceOn } from '../DataModel/resource';
+import {
+  useAllSaveBlockers,
+  useBlockerHandler,
+} from '../DataModel/saveBlockers';
 import { serializeResource } from '../DataModel/serializers';
 import type { Relationship } from '../DataModel/specifyField';
 import type { Collection } from '../DataModel/specifyTable';
 import type { Accession } from '../DataModel/types';
+import { SaveButton } from '../Forms/Save';
 import { FormattedResource } from '../Molecules/FormattedResource';
 import { relationshipIsToMany } from '../WbPlanView/mappingHelpers';
 import { resourceToGeneric } from './autoMerge';
@@ -68,25 +72,23 @@ export function MergeSubviewButton({
     [resource, relationship, getCount]
   );
 
-  const [isValid, setValid] = React.useState(true);
-  React.useEffect(() => {
-    if (merged !== undefined) return;
-    const relationshipName = relationship.relatedTable.name;
-    return resourceOn(
-      resource,
-      'blockersChanged',
-      () => {
-        const hasSaveBlocker = Array.from(
-          resource.saveBlockers?.blockingResources ?? [],
-          (resource) => resource.specifyTable.name
-        ).includes(relationshipName);
-        setValid(!hasSaveBlocker);
-      },
-      true
-    );
-  }, [resource]);
+  // If trying to save with invalid sub view, open the sub view
+  useBlockerHandler(
+    merged,
+    relationship,
+    React.useCallback(() => {
+      if (!isOpen) return false;
+      handleOpen();
+      return true;
+    }, [isOpen, handleOpen])
+  );
 
-  const SubviewButton = isValid ? Button.Gray : Button.Red;
+  const blockers = useAllSaveBlockers(
+    // Only get the blockers for the current resource
+    merged === undefined ? resource : undefined,
+    relationship
+  );
+  const SubviewButton = blockers.length === 0 ? Button.Gray : Button.Red;
 
   return (
     <>
@@ -109,6 +111,16 @@ export function MergeSubviewButton({
   );
 }
 
+function getChildren(
+  resource: SpecifyResource<AnySchema>,
+  relationship: Relationship
+): RA<SpecifyResource<AnySchema>> {
+  const children = resource.getDependentResource(relationship.name);
+  return relationshipIsToMany(relationship)
+    ? (children as Collection<AnySchema> | undefined)?.models ?? []
+    : filterArray([children]);
+}
+
 function MergeDialog({
   relationship,
   merged,
@@ -120,25 +132,12 @@ function MergeDialog({
   readonly resources: RA<SpecifyResource<AnySchema> | undefined>;
   readonly onClose: () => void;
 }): JSX.Element {
-  const id = useId('merge-dialog');
-
-  const getChildren = React.useCallback(
-    (resource: SpecifyResource<AnySchema>) => {
-      const children = resource.getDependentResource(relationship.name);
-      return relationshipIsToMany(relationship)
-        ? (children as Collection<AnySchema> | undefined)?.models ?? []
-        : filterArray([children]);
-    },
-    [relationship]
-  );
-
   const [mergedRecords, setMergedRecords] = React.useState(() =>
-    getChildren(merged)
+    getChildren(merged, relationship)
   );
 
   const [children, setChildren] = useChildren(
     mergedRecords,
-    getChildren,
     resources,
     relationship
   );
@@ -168,22 +167,38 @@ function MergeDialog({
       </Button.Green>
     ) : undefined;
 
+  const [form, setForm] = React.useState<HTMLFormElement | null>(null);
+  const formId = useId('merge-dialog')('form');
+
   return (
     <MergeDialogContainer
       buttons={
         <>
           <ToggleMergeView />
           <span className="-ml-2 flex-1" />
-          <Submit.Gray form={id('form')}>{commonText.close()}</Submit.Gray>
+          {form === null ? undefined : (
+            <SaveButton
+              filterBlockers={relationship}
+              form={form}
+              label={commonText.close()}
+              resource={merged}
+              onSaving={(): false => {
+                handleClose();
+                return false;
+              }}
+            />
+          )}
         </>
       }
       header={mergingText.mergeFields({ field: relationship.label })}
       onClose={handleClose}
     >
       <MergeContainer
-        id={id('form')}
+        formRef={setForm}
+        id={formId}
         recordCount={resources.length}
-        onSubmit={handleClose}
+        // The save button will set a separate submit listener
+        onSubmit={f.void}
       >
         <thead>
           <tr>
@@ -270,15 +285,14 @@ function MergeDialog({
  */
 function useChildren(
   mergedRecords: RA<SpecifyResource<AnySchema>>,
-  getChildren: (
-    resource: SpecifyResource<AnySchema>
-  ) => RA<SpecifyResource<AnySchema>>,
   resources: RA<SpecifyResource<AnySchema> | undefined>,
   relationship: Relationship
 ): GetOrSet<RA<RA<SpecifyResource<AnySchema> | undefined>>> {
   return useTriggerState(
     React.useMemo<RA<RA<SpecifyResource<AnySchema> | undefined>>>(() => {
-      const children = resources.map((record) => f.maybe(record, getChildren));
+      const children = resources.map((record) =>
+        record === undefined ? undefined : getChildren(record, relationship)
+      );
       const maxCount = Math.max(
         ...[mergedRecords, ...children].map((children) => children?.length ?? 0)
       );

commit 8d46af27604e977f120931ed30c8bc6bc89588a8

Fix unable to type new lines
Caused by refactoring in d9a13379b and 6bdeba010

specifyweb/frontend/js_src/lib/components/FormFields/Field.tsx
@@ -145,16 +145,21 @@ function Field({
       tabIndex={isReadOnly ? -1 : undefined}
       value={value?.toString() ?? ''}
       onBlur={
-        // FIXME: is this needed besides onChange?
         isReadOnly ? undefined : ({ target }): void => updateValue(target.value)
       }
+      onValueChange={(value): void => updateValue(value, false)}
       /*
        * Update data model value before onBlur, as onBlur fires after onSubmit
        * if form is submitted using the ENTER key
        */
       onChange={(event): void => {
         const input = event.target as HTMLInputElement;
-        updateValue(input.value);
+        /*
+         * Don't show validation errors on value change for input fields until
+         * field is blurred, unless user tried to paste a date (see definition
+         * of Input.Generic)
+         */
+        updateValue(input.value, event.type === 'paste');
       }}
     />
   );

specifyweb/frontend/js_src/lib/components/FormFields/index.tsx
@@ -112,7 +112,7 @@ const fieldRenderers: {
           rows={formType === 'formTable' ? 1 : rows}
           value={value?.toString() ?? ''}
           onBlur={(): void => updateValue(value?.toString() ?? '')}
-          onValueChange={updateValue}
+          onValueChange={(value): void => updateValue(value, false)}
         />
       </ErrorBoundary>
     );

specifyweb/frontend/js_src/lib/components/QueryComboBox/useTypeSearch.tsx
@@ -24,7 +24,7 @@ export function useTypeSearch(
 ): TypeSearch | false | undefined {
   const relatedTable =
     initialRelatedTable ??
-    (field?.isRelationship ? field.relatedTable : undefined) ??
+    (field.isRelationship ? field.relatedTable : undefined) ??
     (typeof initialTypeSearch === 'object'
       ? initialTypeSearch.table
       : undefined);

specifyweb/frontend/js_src/lib/hooks/useResourceValue.tsx
@@ -52,7 +52,7 @@ export function useResourceValue<
   trim?: boolean
 ): ReturnType<typeof useValidation> & {
   readonly value: T | undefined;
-  readonly updateValue: (newValue: T) => void;
+  readonly updateValue: (newValue: T, reportErrors?: boolean) => void;
   // See useValidation for documentation of these props:
   readonly validationRef: React.RefCallback<INPUT>;
   readonly inputRef: React.MutableRefObject<INPUT | null>;
@@ -88,7 +88,7 @@ export function useResourceValue<
      *   type explicitly as @typescript-eslint/strict-boolean-expressions can't
      *   infer implicit types
      */
-    (newValue: T) => {
+    (newValue: T, reportErrors = true) => {
       if (ignoreChangeRef.current || resource === undefined) return;
 
       /*
@@ -121,9 +121,13 @@ export function useResourceValue<
 
       const parsedValue = parseResults.isValid ? parseResults.parsed : newValue;
       const formattedValue =
-        field?.isRelationship === true && newValue === '' ? null : parsedValue;
+        field?.isRelationship === true && newValue === ''
+          ? null
+          : ['checkbox', 'date'].includes(parser.type ?? '') || reportErrors
+          ? parsedValue
+          : newValue;
       setValue(
-        (parser.type === 'number'
+        (parser.type === 'number' && reportErrors
           ? f.parseFloat(parser?.printFormatter?.(parsedValue, parser) ?? '') ??
             parsedValue
           : formattedValue) as T