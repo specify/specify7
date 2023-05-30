55b2ab781b5395b369dde5d2ba717bce6cfa2d74
Anonymous <noreply@weblate.org>
2023-05-30 18:50:59 +0000

    Sync localization strings with Weblate
    Triggered by 2afc7de65be2cf57da57ea2fbe2531b136b4218d on branch refs/heads/weblate-localization

--- specifyweb/frontend/js_src/lib/localization/attachments.ts
+++ specifyweb/frontend/js_src/lib/localization/attachments.ts
@@ -99,5 +99,10 @@ export const attachmentsText = createDictionary({
   },
   hideForm: {
     'en-us': 'Hide Form',
+    'de-ch': 'Formular ausblenden',
+    'es-es': 'Ocultar formulario',
+    'fr-fr': 'Masquer le formulaire',
+    'ru-ru': 'Скрыть форму',
+    'uk-ua': 'Сховати форму',
   },
 } as const);
