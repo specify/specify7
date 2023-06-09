6dc0b593897badcf02423e2d68fca090d92e07ee
Anonymous <noreply@weblate.org>
2023-06-09 17:50:38 +0000

    Sync localization strings with Weblate
    Triggered by 9ed3841d383d1b3cc62907c678fe2b4b8340a7e0 on branch refs/heads/weblate-localization

--- specifyweb/frontend/js_src/lib/localization/specifyNetwork.ts
+++ specifyweb/frontend/js_src/lib/localization/specifyNetwork.ts
@@ -82,23 +82,119 @@ export const specifyNetworkText = createDictionary({
     'uk-ua': 'Подробиці',
   },
   mapDescription: {
-    'en-us':
-      'This map shows occurrences of this taxon from the iDigBio and GBIF aggregators.',
+    'en-us': `
+      This map shows occurrences of this taxon from the iDigBio and GBIF
+      aggregators.
+    `,
+    'de-ch': `
+      Diese Karte zeigt Vorkommen dieses Taxons aus den Aggregatoren iDigBio und
+      GBIF.
+    `,
+    'es-es': `
+      Este mapa muestra las ocurrencias de este taxón de los agregadores iDigBio
+      y GBIF.
+    `,
+    'fr-fr': `
+      Cette carte montre les occurrences de ce taxon des agrégateurs iDigBio et
+      GBIF.
+    `,
+    'ru-ru': `
+      На этой карте показаны вхождения этого таксона из агрегаторов iDigBio и
+      GBIF.
+    `,
+    'uk-ua': `
+      Ця карта показує випадки появи цього таксону з агрегаторів iDigBio та
+      GBIF.
+    `,
   },
   iDigBioDescription: {
     'en-us': `
-      Occurrences recorded in iDigBio are shown as round green points, except for those from
-      the current collection recorded in iDigBio that are round red points. Zooming in on the
-      red points brings up a blue teardrop pin, clicking on the pin executes a locality query
-      in Specify that shows all of the species in the current Specify database collected from
-      that location.
+      Occurrences recorded in iDigBio are shown as round green points, except
+      for those from the current collection recorded in iDigBio that are round
+      red points. Zooming in on the red points brings up a blue teardrop pin,
+      clicking on the pin executes a locality query in Specify that shows all of
+      the species in the current Specify database collected from that location.
+    `,
+    'de-ch': `
+      In iDigBio erfasste Vorkommen werden als runde grüne Punkte angezeigt, mit
+      Ausnahme der in iDigBio erfassten Vorkommen aus der aktuellen Sammlung,
+      bei denen es sich um runde rote Punkte handelt. Wenn Sie die roten Punkte
+      vergrößern, wird eine blaue tropfenförmige Stecknadel angezeigt. Wenn Sie
+      auf die Stecknadel klicken, wird eine Standortabfrage in Specify
+      ausgeführt, die alle Arten in der aktuellen Specify-Datenbank anzeigt, die
+      an diesem Standort gesammelt wurden.
+    `,
+    'es-es': `
+      Las ocurrencias registradas en iDigBio se muestran como puntos verdes
+      redondos, excepto aquellas de la colección actual registrada en iDigBio
+      que son puntos rojos redondos. Al hacer zoom en los puntos rojos, aparece
+      un alfiler azul en forma de lágrima, al hacer clic en el alfiler se
+      ejecuta una consulta de localidad en Especificar que muestra todas las
+      especies en la base de datos Especificar actual recopiladas de esa
+      ubicación.
+    `,
+    'fr-fr': `
+      Les occurrences enregistrées dans iDigBio sont représentées par des points
+      verts ronds, à l'exception de celles de la collection actuelle
+      enregistrées dans iDigBio qui sont des points rouges ronds. Zoomer sur les
+      points rouges fait apparaître une épingle bleue en forme de larme,
+      cliquer sur l'épingle exécute une requête de localité dans Spécifier qui
+      montre toutes les espèces dans la base de données Spécifier actuelle
+      collectées à partir de cet emplacement.
+    `,
+    'ru-ru': `
+      Вхождения, записанные в iDigBio, отображаются в виде круглых зеленых
+      точек, за исключением событий из текущей коллекции, записанных в iDigBio,
+      которые отображаются в виде круглых красных точек. При увеличении красных
+      точек отображается синяя булавка-капля, щелчок по булавке выполняет запрос
+      местоположения в Specify, который показывает все виды в текущей базе
+      данных Specify, собранные из этого местоположения.
+    `,
+    'uk-ua': `
+      Випадки, записані в iDigBio, відображаються круглими зеленими точками, за
+      винятком випадків із поточної колекції, записаної в iDigBio, які мають
+      круглі червоні точки. Якщо збільшити масштаб червоних точок, з’явиться
+      блакитна краплевидна шпилька, натиснувши на шпильку, буде виконано запит
+      місцевості в Specify, який покаже всі види в поточній базі даних Specify,
+      зібрані з цього місця.
     `,
   },
   gbifDescription: {
     'en-us': `
-      For GBIF data, individual points and clusters of points are shown as hexagons of different
-      colors ranging from yellow to orange to red. Dark red hexagons corresponding to densest
-      distributions of collected specimens of that species.
+      For GBIF data, individual points and clusters of points are shown as
+      hexagons of different colors ranging from yellow to orange to red. Dark
+      red hexagons corresponding to densest distributions of collected specimens
+      of that species.
+    `,
+    'de-ch': `
+      Bei GBIF-Daten werden einzelne Punkte und Punktcluster als Sechsecke in
+      verschiedenen Farben von Gelb über Orange bis Rot dargestellt. Dunkelrote
+      Sechsecke, die der dichtesten Verteilung gesammelter Exemplare dieser Art
+      entsprechen.
+    `,
+    'es-es': `
+      Para los datos de GBIF, los puntos individuales y los grupos de puntos se
+      muestran como hexágonos de diferentes colores que van del amarillo al
+      naranja y al rojo. Hexágonos de color rojo oscuro que corresponden a las
+      distribuciones más densas de especímenes recolectados de esa especie.
+    `,
+    'fr-fr': `
+      Pour les données GBIF, les points individuels et les groupes de points
+      sont représentés par des hexagones de différentes couleurs allant du jaune
+      à l'orange au rouge. Hexagones rouge foncé correspondant aux
+      distributions les plus denses de spécimens collectés de cette espèce.
+    `,
+    'ru-ru': `
+      Для данных GBIF отдельные точки и группы точек отображаются в виде
+      шестиугольников разных цветов от желтого до оранжевого и красного.
+      Темно-красные шестиугольники, соответствующие наиболее плотному
+      распределению собранных экземпляров этого вида.
+    `,
+    'uk-ua': `
+      Для даних GBIF окремі точки та кластери точок відображаються у вигляді
+      шестикутників різних кольорів від жовтого до оранжевого та червоного.
+      Темно-червоні шестикутники відповідають найщільнішому розподілу зібраних
+      зразків цього виду.
     `,
   },
   connectToGbif: {
