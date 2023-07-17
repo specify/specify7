32566a54dc23ad6f40f83dde2f1979a5bc7c5735
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2023-07-17 06:57:36 -0700

    Add break-words style with css

--- specifyweb/frontend/js_src/lib/components/Statistics/StatsResult.tsx
+++ specifyweb/frontend/js_src/lib/components/Statistics/StatsResult.tsx
@@ -70,7 +70,7 @@ export function StatsResult({
       ) : (
         <li className="flex gap-2">
           <Button.LikeLink className="flex-1" onClick={handleClickResolved}>
-            <span className="self-start break-all">{label}</span>
+          <span className="self-start" style={{ wordBreak: "break-word" }}>{label}</span>
             <span className="-ml-2 flex-1" />
             <span className="self-start">{value ?? commonText.loading()}</span>
           </Button.LikeLink>
