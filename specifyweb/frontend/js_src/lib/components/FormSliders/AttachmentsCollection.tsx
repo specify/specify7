782a426ae4e22d6ee7ff8d64d94a8b1f8112fdcd
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2023-07-20 07:03:36 -0700

    Create a gallery icon

--- specifyweb/frontend/js_src/lib/components/FormSliders/AttachmentsCollection.tsx
+++ specifyweb/frontend/js_src/lib/components/FormSliders/AttachmentsCollection.tsx
@@ -42,7 +42,7 @@ export function AttachmentsCollection({
         title={attachmentsText.attachments()}
         onClick={handleOpenAttachments}
         >
-          {icons.photos}
+          {icons.gallery}
         </Button.Small>
       {showAllAttachments && (
         <Dialog
