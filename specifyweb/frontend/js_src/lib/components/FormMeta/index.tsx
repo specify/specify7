f8af133cca65e6fe411c202b306afd20baca6576
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2023-07-24 08:09:29 -0700

    Export recordMergingTables variable

--- specifyweb/frontend/js_src/lib/components/FormMeta/index.tsx
+++ specifyweb/frontend/js_src/lib/components/FormMeta/index.tsx
@@ -36,6 +36,7 @@ import { QueryTreeUsages } from './QueryTreeUsages';
 import { ReadOnlyMode } from './ReadOnlyMode';
 import { ShareRecord } from './ShareRecord';
 import { SubViewMeta } from './SubViewMeta';
+import { recordMergingTables } from '../Merging';
 
 /**
  * Form preferences host context aware user preferences and other meta-actions.
@@ -179,7 +180,7 @@ function MetaDialog({
                   action="update"
                   tableName={resource.specifyModel.name}
                 >
-                  {resource.specifyModel.name === 'Agent' && (
+                  {recordMergingTables.has(resource.specifyModel.name) && (
                     <MergeRecord resource={resource} />
                   )}
                 </ProtectedTable>
