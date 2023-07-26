47c0f8a80b3c924201086ffe90390b5f1fc29698
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2023-07-26 11:59:14 -0700

    Disable link in stat item if value === error
    Fixes #3833

--- specifyweb/frontend/js_src/lib/components/Statistics/StatsResult.tsx
+++ specifyweb/frontend/js_src/lib/components/Statistics/StatsResult.tsx
@@ -69,8 +69,14 @@ export function StatsResult({
         </>
       ) : (
         <li className="flex gap-2">
-          <Button.LikeLink className="flex-1" onClick={handleClickResolved}>
-            <span className="self-start" style={{ wordBreak: "break-word" }}>{label}</span>
+          <Button.LikeLink
+            className="flex-1"
+            onClick={handleClickResolved}
+            disabled={value === 'Error'}
+          >
+            <span className="self-start" style={{ wordBreak: 'break-word' }}>
+              {label}
+            </span>
             <span className="-ml-2 flex-1" />
             <span className="self-start">{value ?? commonText.loading()}</span>
           </Button.LikeLink>
