fcce821bc9dc92ed473d10ff5e354cd980fb1157
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2023-07-27 08:29:34 -0700

    Hide export to CSV button if no displayed field
    Fixes #3834

--- specifyweb/frontend/js_src/lib/components/QueryBuilder/Results.tsx
+++ specifyweb/frontend/js_src/lib/components/QueryBuilder/Results.tsx
@@ -9,7 +9,14 @@ import { commonText } from '../../localization/common';
 import { interactionsText } from '../../localization/interactions';
 import { queryText } from '../../localization/query';
 import { f } from '../../utils/functools';
-import { filterArray, type GetOrSet, type GetSet, type IR, type R, type RA } from '../../utils/types';
+import {
+  filterArray,
+  type GetOrSet,
+  type GetSet,
+  type IR,
+  type R,
+  type RA,
+} from '../../utils/types';
 import { removeKey } from '../../utils/utils';
 import { Container, H3 } from '../Atoms';
 import { Button } from '../Atoms/Button';
@@ -197,12 +204,12 @@ export function QueryResults(props: Props): JSX.Element {
           </Button.Small>
         )}
         <div className="-ml-2 flex-1" />
-        {extraButtons}
+        {props.displayedFields.length > 0 && extraButtons}
         {hasIdField &&
         Array.isArray(results) &&
         Array.isArray(loadedResults) &&
         results.length > 0 &&
-        typeof fetchResults === 'function'? (
+        typeof fetchResults === 'function' ? (
           <>
             {hasPermission('/record/replace', 'update') &&
               hasTablePermission(model.name, 'update') && (
@@ -393,7 +400,8 @@ export function useFetchQueryResults({
   const resultsRef = React.useRef(results);
   const handleSetResults = React.useCallback(
     (results: RA<QueryResultRow | undefined> | undefined) => {
-      const filteredResults = results !== undefined ? filterArray(results) : undefined
+      const filteredResults =
+        results !== undefined ? filterArray(results) : undefined;
       setResults(filteredResults);
       resultsRef.current = results;
     },
