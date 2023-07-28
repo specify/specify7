1d4017d6b10cecb6da16a33494fa12fd32c7edfd
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2023-07-28 10:30:33 -0700

    Use setTimeout + use time variables

--- specifyweb/frontend/js_src/lib/components/Molecules/DateElement.tsx
+++ specifyweb/frontend/js_src/lib/components/Molecules/DateElement.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { LANGUAGE } from '../../localization/utils/config';
 import { getRelativeDate } from '../Atoms/Internationalization';
-import { HOUR, MILLISECONDS, MINUTE } from '../Atoms/timeUnits';
+import { HOUR, MILLISECONDS, MINUTE, SECOND } from '../Atoms/timeUnits';
 
 const longDate = new Intl.DateTimeFormat(LANGUAGE, {
   dateStyle: 'full',
@@ -25,56 +25,33 @@ export function DateElement({
   );
 
   React.useEffect(() => {
-    let intervalValue = 0;
-    let interval: NodeJS.Timeout | undefined;
+    let timeout: NodeJS.Timeout | undefined;
 
-    if (date) {
-      interval = setInterval(() => {
+    function updateRelativeDate() {
+      if (date) {
         const dateObject = new Date(date);
         const now = new Date();
-        const timeDifference = now.getTime() - dateObject.getTime();
+        const timeDifference =
+          (now.getTime() - dateObject.getTime()) / MILLISECONDS;
 
-        if (timeDifference < 60 * MILLISECONDS)
-          intervalValue = 5 * MILLISECONDS;
-        else if (timeDifference < 6 * MINUTE) intervalValue = MINUTE;
-        else intervalValue = HOUR;
+        let timeoutValue = 0;
+
+        if (timeDifference < MINUTE) timeoutValue = SECOND;
+        else if (timeDifference < HOUR) timeoutValue = MINUTE;
+        else timeoutValue = HOUR;
 
         setRelativeDate(getRelativeDate(new Date(date)));
-      }, intervalValue);
+
+        timeout = setTimeout(updateRelativeDate, timeoutValue);
+      }
     }
 
+    updateRelativeDate();
+
     return () => {
-      clearInterval(interval);
+      clearTimeout(timeout);
     };
   }, [date]);
-
-  // React.useEffect(() => {
-  //   let timeout: NodeJS.Timeout | undefined;
-
-  //   function updateRelativeDate() {
-  //     if (date) {
-  //       const dateObject = new Date(date);
-  //       const now = new Date();
-  //       const timeDifference = now.getTime() - dateObject.getTime();
-
-  //       let timeoutValue = 0;
-
-  //       if (timeDifference < 60 * MILLISECONDS) timeoutValue = 5 * MILLISECONDS;
-  //       else if (timeDifference < 6 * MINUTE) timeoutValue = MINUTE;
-  //       else timeoutValue = HOUR;
-
-  //       setRelativeDate(getRelativeDate(new Date(date)));
-
-  //       timeout = setTimeout(updateRelativeDate, timeoutValue);
-  //     }
-  //   }
-
-  //   updateRelativeDate();
-
-  //   return () => {
-  //     clearTimeout(timeout);
-  //   };
-  // }, [date]);
 
   if (typeof date !== 'string' || Number.isNaN(Date.parse(date)))
     return <>{fallback}</>;
