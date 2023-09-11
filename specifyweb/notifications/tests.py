85096e9c3336b33985afddfdfd6bedf6d54864e3
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2023-09-11 11:46:47 -0700

    Back end tests for notiifcations

--- specifyweb/notifications/tests.py
+++ specifyweb/notifications/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+import json
+from urllib import request
+from specifyweb.notifications.models import Message
+from django.test import Client
 
-# Create your tests here.
+from specifyweb.specify.api_tests import ApiTests
+
+class NotificationsTests(ApiTests): 
+ def test_get_notification_with_param_since(self): 
+  testMessage = Message.objects.create(
+   user=self.specifyuser,
+   timestamp='2023-08-25T21:20:14.177591', 
+   content=json.dumps({
+        'type': 'quer-export-to-csv-complete',
+        'file' : 'query_results_2023-08-25T21:20.14.156542.csv',
+        'message_id': 506,
+        'timestamp': "2023-08-25T21:20:14.177591",
+    })
+ )
+
+  c = Client()
+  c.force_login(self.specifyuser)
+
+  response = c.get('/api/notifications/messages/?since=2023-07-25T21:20:14.177591')
+
+  mockResponse = [json.loads(testMessage.content)]
+
+  responseReturned = json.loads(response.content)
+
+  self.assertEqual(mockResponse, responseReturned)
+
