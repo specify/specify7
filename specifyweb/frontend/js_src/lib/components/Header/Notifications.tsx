b49971220bce7f4224128c3ae7c854e2b67ac444
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2023-09-12 11:21:44 -0700

    Test: refactor doFetch to a hook

--- specifyweb/frontend/js_src/lib/components/Header/Notifications.tsx
+++ specifyweb/frontend/js_src/lib/components/Header/Notifications.tsx
@@ -1,28 +1,24 @@
 import React from 'react';
-import type { LocalizedString } from 'typesafe-i18n';
 
 import { useBooleanState } from '../../hooks/useBooleanState';
 import { commonText } from '../../localization/common';
 import { notificationsText } from '../../localization/notifications';
-import { ajax } from '../../utils/ajax';
 import { formData } from '../../utils/ajax/helpers';
 import { ping } from '../../utils/ajax/ping';
 import { f } from '../../utils/functools';
-import { formatDateForBackEnd } from '../../utils/parser/dateFormat';
-import type { IR, RA } from '../../utils/types';
-import { sortFunction } from '../../utils/utils';
+import type { RA } from '../../utils/types';
 import { Button } from '../Atoms/Button';
 import { icons } from '../Atoms/Icons';
 import { ErrorBoundary } from '../Errors/ErrorBoundary';
 import { DateElement } from '../Molecules/DateElement';
 import { Dialog, dialogClassNames, LoadingScreen } from '../Molecules/Dialog';
-import { formatUrl } from '../Router/queryString';
+import { useNotificationsFetch } from './hooks';
 import { MenuButton } from './index';
 import type { GenericNotification } from './NotificationRenderers';
 import { notificationRenderers } from './NotificationRenderers';
 
 const INITIAL_INTERVAL = 5000;
-const INTERVAL_MULTIPLIER = 1.1;
+export const INTERVAL_MULTIPLIER = 1.1;
 
 export function Notifications({
   isCollapsed,
@@ -42,10 +38,21 @@ export function Notifications({
     if (notificationCount === 0) handleClose();
   }, [notificationCount, handleClose]);
 
-  React.useEffect(() => {
-    let pullInterval = INITIAL_INTERVAL;
-    let lastFetchedTimestamp: Date | undefined;
+  let pullInterval = INITIAL_INTERVAL;
+  let lastFetchedTimestamp: Date | undefined;
+  let destructorCalled = false;
+  let timeout: NodeJS.Timeout | undefined = undefined;
 
+  const { doFetch } = useNotificationsFetch({
+    pullInterval,
+    freezeFetchPromise,
+    destructorCalled,
+    notifications: [notifications, setNotifications],
+    lastFetchedTimestamp,
+    timeout,
+  });
+
+  React.useEffect(() => {
     const handler = (): void => {
       if (timeout !== undefined) globalThis.clearTimeout(timeout);
 
@@ -57,97 +64,8 @@ export function Notifications({
 
     document.addEventListener('visibilitychange', handler);
 
-    let timeout: NodeJS.Timeout | undefined = undefined;
-
-    function doFetch(since = new Date()): void {
-      const startFetchTimestamp = new Date();
-
-      const url = formatUrl(`/notifications/messages/`, {
-        since: formatDateForBackEnd(since),
-      });
-
-      /*
-       * Poll interval is scaled exponentially to reduce requests if the tab is
-       * left open.
-       */
-      pullInterval *= INTERVAL_MULTIPLIER;
-
-      // Don't fetch while a message is being deleted or marked as read
-      (freezeFetchPromise.current ?? Promise.resolve())
-        .then(async () =>
-          ajax<
-            RA<
-              Omit<GenericNotification, 'messageId' | 'payload'> & {
-                // eslint-disable-next-line @typescript-eslint/naming-convention
-                readonly message_id: string;
-              }
-            >
-          >(
-            url,
-
-            {
-              headers: { Accept: 'application/json' },
-              /*
-               * Don't show modal error dialog on errors. Several reasons why:
-               *  - Request may fail if Django migrations weren't run
-               *  - Request is initialized automatically, not by user, thus having
-               *    an error dialog appear out of blue could be confusing/unexpected
-               *  - Notifications is not a critical component, so if it fails, it
-               *    shouldn't bring down entire application
-               */
-              errorMode: 'silent',
-            }
-          )
-        )
-        .then(({ data: newNotifications }) => {
-          if (destructorCalled) return undefined;
-
-          setNotifications((existingNotifications) => {
-            const mappedNewNotifications = newNotifications.map(
-              ({ message_id, read, timestamp, type, ...rest }) => ({
-                messageId: message_id,
-                read,
-                timestamp,
-                type,
-                payload: rest as IR<LocalizedString>,
-              })
-            );
-
-            const filteredNewNotifications = mappedNewNotifications.filter(
-              (newNotification) =>
-                !existingNotifications?.some(
-                  (existingNotification) =>
-                    existingNotification.messageId === newNotification.messageId
-                )
-            );
-
-            return [
-              ...(existingNotifications ?? []),
-              ...filteredNewNotifications,
-            ].sort(
-              sortFunction(
-                ({ timestamp }) => new Date(timestamp).getTime(),
-                true
-              )
-            );
-          });
-          lastFetchedTimestamp = startFetchTimestamp;
-          // Stop updating if tab is hidden
-          timeout =
-            document.visibilityState === 'hidden'
-              ? undefined
-              : globalThis.setTimeout(
-                  () => doFetch(lastFetchedTimestamp),
-                  pullInterval
-                );
-          return timeout;
-        })
-        .catch(console.error);
-    }
-
     doFetch();
 
-    let destructorCalled = false;
     return (): void => {
       document.removeEventListener('visibilitychange', handler);
       destructorCalled = true;
---
+++ specifyweb/frontend/js_src/lib/components/Header/hooks.tsx
@@ -0,0 +1,111 @@
+import { LocalizedString } from 'typesafe-i18n';
+import { ajax } from '../../utils/ajax';
+import { formatDateForBackEnd } from '../../utils/parser/dateFormat';
+import { GetOrSet, IR, RA } from '../../utils/types';
+import { sortFunction } from '../../utils/utils';
+import { formatUrl } from '../Router/queryString';
+import { GenericNotification } from './NotificationRenderers';
+import { INTERVAL_MULTIPLIER } from './Notifications';
+
+export function useNotificationsFetch({
+  pullInterval,
+  freezeFetchPromise,
+  destructorCalled,
+  notifications: [_, setNotifications],
+  lastFetchedTimestamp,
+  timeout,
+}: {
+  readonly pullInterval: number;
+  readonly freezeFetchPromise: React.MutableRefObject<
+    Promise<void> | undefined
+  >;
+  readonly destructorCalled: boolean;
+  readonly notifications: GetOrSet<RA<GenericNotification> | undefined>;
+  readonly lastFetchedTimestamp: Date | undefined;
+  readonly timeout: NodeJS.Timeout | undefined;
+}): { doFetch: (since?: Date) => void } {
+  const doFetch = (since = new Date()): void => {
+    const startFetchTimestamp = new Date();
+
+    const url = formatUrl(`/notifications/messages/`, {
+      since: formatDateForBackEnd(since),
+    });
+
+    /*
+     * Poll interval is scaled exponentially to reduce requests if the tab is
+     * left open.
+     */
+    pullInterval *= INTERVAL_MULTIPLIER;
+
+    // Don't fetch while a message is being deleted or marked as read
+    (freezeFetchPromise.current ?? Promise.resolve())
+      .then(async () =>
+        ajax<
+          RA<
+            Omit<GenericNotification, 'messageId' | 'payload'> & {
+              // eslint-disable-next-line @typescript-eslint/naming-convention
+              readonly message_id: string;
+            }
+          >
+        >(
+          url,
+
+          {
+            headers: { Accept: 'application/json' },
+            /*
+             * Don't show modal error dialog on errors. Several reasons why:
+             *  - Request may fail if Django migrations weren't run
+             *  - Request is initialized automatically, not by user, thus having
+             *    an error dialog appear out of blue could be confusing/unexpected
+             *  - Notifications is not a critical component, so if it fails, it
+             *    shouldn't bring down entire application
+             */
+            errorMode: 'silent',
+          }
+        )
+      )
+      .then(({ data: newNotifications }) => {
+        if (destructorCalled) return undefined;
+
+        setNotifications((existingNotifications) => {
+          const mappedNewNotifications = newNotifications.map(
+            ({ message_id, read, timestamp, type, ...rest }) => ({
+              messageId: message_id,
+              read,
+              timestamp,
+              type,
+              payload: rest as IR<LocalizedString>,
+            })
+          );
+
+          const filteredNewNotifications = mappedNewNotifications.filter(
+            (newNotification) =>
+              !existingNotifications?.some(
+                (existingNotification) =>
+                  existingNotification.messageId === newNotification.messageId
+              )
+          );
+
+          return [
+            ...(existingNotifications ?? []),
+            ...filteredNewNotifications,
+          ].sort(
+            sortFunction(({ timestamp }) => new Date(timestamp).getTime(), true)
+          );
+        });
+        lastFetchedTimestamp = startFetchTimestamp;
+        // Stop updating if tab is hidden
+        timeout =
+          document.visibilityState === 'hidden'
+            ? undefined
+            : globalThis.setTimeout(
+                () => doFetch(lastFetchedTimestamp),
+                pullInterval
+              );
+        return timeout;
+      })
+      .catch(console.error);
+  };
+
+  return { doFetch };
+}
