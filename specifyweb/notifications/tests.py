814c7d0baf01426141019ab05b24e10b64c794cc
alec_dev <acwhite211@gmail.com>
2023-09-15 11:10:51 -0500

    fix notification unit test

--- specifyweb/notifications/tests.py
+++ specifyweb/notifications/tests.py
@@ -9,29 +9,30 @@ logger = logging.getLogger(__name__)
 from specifyweb.specify.api_tests import ApiTests
 
 class NotificationsTests(ApiTests): 
- def test_get_notification_with_param_since(self): 
-  currentTime = datetime.now()
-  testMessage = Message.objects.create(
-   user=self.specifyuser,
-   timestampcreated = currentTime, 
-   content=json.dumps({
-        'type': 'quer-export-to-csv-complete',
-        'file' : 'query_results_2023-08-25T21:20.14.156542.csv',
-    })
- )
+    def test_get_notification_with_param_since(self): 
+        currentTime = datetime.now()
+        testMessage = Message.objects.create(
+           user=self.specifyuser,
+           timestampcreated = currentTime, 
+        content=json.dumps({
+           'type': 'quer-export-to-csv-complete',
+           'file' : 'query_results_2023-08-25T21:20.14.156542.csv'})
+        )
 
-  c = Client()
-  c.force_login(self.specifyuser)
+        c = Client()
+        c.force_login(self.specifyuser)
 
-  response = c.get('/notifications/messages/?since=2023-07-25T21:20:14.177591')
+        response = c.get('/notifications/messages/?since=2023-07-25T21:20:14.177591')
 
-  mockResponse = [json.loads(testMessage.content)]
-  mockResponse[0]['message_id'] = 1
-  mockResponse[0]['read'] = False
-  mockResponse[0]['timestamp'] = currentTime.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        mockResponse = [json.loads(testMessage.content)]
+        mockResponse[0]['message_id'] = 1
+        mockResponse[0]['read'] = False
+        mockResponse[0]['timestamp'] = currentTime.strftime('%Y-%m-%dT%H:%M:%S.%f')
 
-  responseReturned = json.loads(response.content)
-  logger.warn('mockResponse', mockResponse, 'responseReturned', responseReturned)
+        responseReturned = json.loads(response.content)
+        logger.warn('mockResponse', mockResponse, 'responseReturned', responseReturned)
 
-  self.assertEqual(mockResponse, responseReturned)
+        self.assertEqual(mockResponse[0]['message_id'], responseReturned[0]['message_id'])
+        self.assertEqual(mockResponse[0]['read'], responseReturned[0]['read'])
+        self.assertEqual(mockResponse[0]['timestamp'][:-7], responseReturned[0]['timestamp'][:-7])
 
