702a64f4110e494bdc1573455a95af3993abf6a9
realVinayak <realvinayak123@gmail.com>
2023-10-08 01:42:43 -0500

    Skip create permission check if validating

--- specifyweb/workbench/upload/auditor.py
+++ specifyweb/workbench/upload/auditor.py
@@ -12,16 +12,19 @@ logger = logging.getLogger(__name__)
 class Auditor(NamedTuple):
     collection: Any
     audit_log: Optional[AuditLog]
-
+    skip_create_permission_check: bool = False
     def insert(self, inserted_obj: Any, agent: Union[int, Any], parent_record: Optional[Any]) -> None:
-        if agent is None:
-            logger.warn('WB inserting %s with no createdbyagent. Skipping permissions check.', inserted_obj)
-        else:
-            if isinstance(agent, int):
-                # TODO: Optimize this potential w/ just memoization
-                agent_obj = Agent.objects.get(id=agent)
 
-            check_table_permissions(self.collection, agent_obj, inserted_obj, "create")
+        if not self.skip_create_permission_check:
+
+            if agent is None:
+                logger.warn('WB inserting %s with no createdbyagent. Skipping permissions check.', inserted_obj)
+            else:
+                if isinstance(agent, int):
+                    # TODO: Optimize this potential w/ just memoization
+                    agent_obj = Agent.objects.get(id=agent)
+
+                check_table_permissions(self.collection, agent_obj, inserted_obj, "create")
 
         if self.audit_log is not None:
             self.audit_log.insert(inserted_obj, agent, parent_record)
