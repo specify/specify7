22b1f4c4cdd02303bd1833247655a62cdcd3d3d5
realVinayak <realvinayak123@gmail.com>
2023-10-08 02:30:05 -0500

    Simplify conditions by reordering

--- specifyweb/workbench/upload/auditor.py
+++ specifyweb/workbench/upload/auditor.py
@@ -15,16 +15,14 @@ class Auditor(NamedTuple):
     skip_create_permission_check: bool = False
     def insert(self, inserted_obj: Any, agent: Union[int, Any], parent_record: Optional[Any]) -> None:
 
-        if not self.skip_create_permission_check:
+        if agent is None:
+            logger.warn('WB inserting %s with no createdbyagent. Skipping permissions check.', inserted_obj)
+        elif not self.skip_create_permission_check:
+            if isinstance(agent, int):
+                # TODO: Optimize this potential w/ just memoization
+                agent_obj = Agent.objects.get(id=agent)
 
-            if agent is None:
-                logger.warn('WB inserting %s with no createdbyagent. Skipping permissions check.', inserted_obj)
-            else:
-                if isinstance(agent, int):
-                    # TODO: Optimize this potential w/ just memoization
-                    agent_obj = Agent.objects.get(id=agent)
-
-                check_table_permissions(self.collection, agent_obj, inserted_obj, "create")
+            check_table_permissions(self.collection, agent_obj, inserted_obj, "create")
 
         if self.audit_log is not None:
             self.audit_log.insert(inserted_obj, agent, parent_record)
