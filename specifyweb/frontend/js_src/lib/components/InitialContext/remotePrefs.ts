1359ea8c2e37cce7dae388b4b82282adbb398422
melton-jason <meltonjason12@yahoo.com>
2023-10-25 22:49:31 +0000

    Lint code with ESLint and Prettier
    Triggered by 1334838f21fec0d9e51f10085635c669a2ca22ff on branch refs/heads/issue-3278

--- specifyweb/frontend/js_src/lib/components/InitialContext/remotePrefs.ts
+++ specifyweb/frontend/js_src/lib/components/InitialContext/remotePrefs.ts
@@ -192,10 +192,11 @@ export const remotePrefsDefinitions = f.store(
         isLegacy: true,
       },
 
-      /* This pref was implemented in Specify 7 in https://github.com/specify/specify7/pull/2818
-         and went through many iterations and changes.
-         See the Pull Request for the full context and implementation/design decision. 
-      */
+      /*
+       * This pref was implemented in Specify 7 in https://github.com/specify/specify7/pull/2818
+       * and went through many iterations and changes.
+       * See the Pull Request for the full context and implementation/design decision.
+       */
       'TaxonTreeEditor.DisplayAuthor': {
         description:
           'Display Authors of Taxons next to nodes in the Tree Viewer',
