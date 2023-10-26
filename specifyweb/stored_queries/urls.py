f61890ee014462f111c2131f72727638071212bd
realVinayak <realvinayak123@gmail.com>
2023-10-25 22:46:27 -0500

    Add unit test and expected errors

--- specifyweb/stored_queries/urls.py
+++ specifyweb/stored_queries/urls.py
@@ -5,7 +5,6 @@ from . import build_models_tests
 
 urlpatterns = [
     url(r'^query/(?P<id>\d+)/$', views.query),
-    url(r'^test_datamodel/', build_models_tests.test_sqlalchemy_model),
     url(r'^ephemeral/$', views.ephemeral),
     url(r'^exportcsv/$', views.export_csv),
     url(r'^exportkml/$', views.export_kml),
