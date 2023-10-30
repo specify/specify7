d0dcc1bac05cee5589d7666f44fe5497c8dcd550
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2023-10-30 10:45:04 -0700

    Warn many-to-many relationship not being supported instead of error
    Fixes #4167

--- specifyweb/frontend/js_src/lib/components/FormEditor/viewSpec.ts
+++ specifyweb/frontend/js_src/lib/components/FormEditor/viewSpec.ts
@@ -361,7 +361,7 @@ const subViewSpec = (
           }
           if (field?.type === 'many-to-many') {
             // ResourceApi does not support .rget() on a many-to-many
-            console.error('Many-to-many relationships are not supported');
+            console.warn('Many-to-many relationships are not supported');
             return undefined;
           }
           return fields;
