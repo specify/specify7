770d0205f571144fa10093477300aa9102cd3d2a
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2023-12-04 09:00:22 -0800

    Remove undefined value from records to prevent attachment loading
    Fixes #4247

--- specifyweb/frontend/js_src/lib/components/Attachments/RecordSetAttachment.tsx
+++ specifyweb/frontend/js_src/lib/components/Attachments/RecordSetAttachment.tsx
@@ -46,9 +46,9 @@ export function RecordSetAttachments<SCHEMA extends AnySchema>({
         )
       );
 
-      const fetchCount = records.findIndex(
-        (record) => record?.populated !== true
-      );
+      const fetchCount = records
+        .filter((record) => record !== undefined)
+        .findIndex((record) => record?.populated !== true);
 
       fetchedCount.current = fetchCount === -1 ? records.length : fetchCount;
 
