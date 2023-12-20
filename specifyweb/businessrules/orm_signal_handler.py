43cd3768896d858e0f7cfc84e07c5d1574f990b6
melton-jason <meltonjason12@yahoo.com>
2023-12-20 13:50:23 -0600

    [backend] improve signal handling

--- specifyweb/businessrules/orm_signal_handler.py
+++ specifyweb/businessrules/orm_signal_handler.py
@@ -1,10 +1,16 @@
-from django.db.models import signals
+from typing import Union, Literal, Optional, Hashable
+
+from django.db.models import signals, Model
 from django.dispatch import receiver
 
 from specifyweb.specify import models
 
+# See https://docs.djangoproject.com/en/3.2/ref/signals/#module-django.db.models.signals
+MODEL_SIGNAL = Union[Literal["pre_init"], Literal["post_init"], Literal["pre_save"],
+                     Literal["post_save"], Literal["pre_delete"], Literal["post_delete"], Literal["m2m_changed"]]
 
-def orm_signal_handler(signal, model=None, dispatch_uid=None):
+
+def orm_signal_handler(signal: MODEL_SIGNAL, model: Optional[str] = None, dispatch_uid: Optional[Hashable] = None):
     def _dec(rule):
         receiver_kwargs = {'dispatch_uid': dispatch_uid}
         if model is not None:
@@ -24,3 +30,8 @@ def orm_signal_handler(signal, model=None, dispatch_uid=None):
 
         return receiver(getattr(signals, signal), **receiver_kwargs)(handler)
     return _dec
+
+
+def disconnect_signal(signal: MODEL_SIGNAL, model: Optional[Model] = None, dispatch_uid: Optional[Hashable] = None) -> bool:
+    return getattr(signals, signal).disconnect(
+        sender=model, dispatch_uid=dispatch_uid)
