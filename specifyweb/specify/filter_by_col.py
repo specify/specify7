a14445e10c535b00831e0ebc29e71998eea15aa5
realVinayak <realvinayak123@gmail.com>
2023-12-26 18:36:17 -0600

    Allow handling multiples in scoping

--- specifyweb/specify/filter_by_col.py
+++ specifyweb/specify/filter_by_col.py
@@ -2,14 +2,47 @@
 Modules for filtering resources by the collection logged in
 """
 
-from django.core.exceptions import FieldError
+from django.core.exceptions import FieldError, ObjectDoesNotExist
 from django.db.models import Q
 
 from . import scoping
 from .models import Geography, Geologictimeperiod, Lithostrat, Taxon, Storage, \
     Attachment
 
-HIERARCHY = ['collectionobject', 'collection', 'discipline', 'division', 'institution']
+# Defines hierarchies, and paths to take up hierarchy on.
+# For example, collectionobject also serves as collectionobject
+# TODO: Add more paths.
+
+HIERARCHIES = ['collection', 'discipline', 'division', 'institution']
+
+
+HIERARCHY_ADDONS = {
+    'collection': ['collectionobject']
+}
+
+def pluralize(field):
+    return f"{field}s"
+
+
+def get_lookup(model, field):
+    for single in [True, False]:
+        lookup = field if single else pluralize(field)
+        if getattr(model, lookup, None):
+            return lookup
+    return None
+
+def get_hierarchy_lookup(model):
+    for hierarchy in HIERARCHIES:
+        direct_lookup = get_lookup(model, hierarchy)
+        if direct_lookup:
+            return hierarchy, direct_lookup
+        child_lookups = [get_lookup(model, field)
+                         for field in HIERARCHY_ADDONS.get(hierarchy, [])]
+        for child_lookup in child_lookups:
+            if child_lookup:
+                return hierarchy, f'{child_lookup}__{hierarchy}'
+    return None
+
 
 class HierarchyException(Exception):
     pass
@@ -38,25 +71,20 @@ def filter_by_collection(queryset, collection, strict=True):
     except FieldError:
         pass
 
-    for fieldname in HIERARCHY:
-        if getattr(queryset.model, fieldname, None):
-            break
-    else:
+
+    hierarchy_lookup = get_hierarchy_lookup(queryset.model)
+    if hierarchy_lookup is None:
         if strict:
             raise HierarchyException('queryset model ' + queryset.model.__name__ + ' has no hierarchy field')
         else:
             return queryset
 
-    if fieldname == 'collectionobject':
-        lookup = 'collectionobject__collection'
-        join = 'collection'
-    else:
-        lookup = join = fieldname
+    current_hierarchy, lookup = hierarchy_lookup
 
     value = collection
     field = 'collection'
-    while field != join:
-        field = HIERARCHY[ 1+HIERARCHY.index(field) ]
+    while field != current_hierarchy:
+        field = HIERARCHIES[ 1+HIERARCHIES.index(field) ]
         value = getattr(value, field)
 
     return queryset.filter(**{ lookup: value })
