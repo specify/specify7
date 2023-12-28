0fe9e2c498ffea63aa1ae61bcd100bc50b9cdcf3
melton-jason <meltonjason12@yahoo.com>
2023-12-27 20:45:31 -0600

    Generalize CSV file picker

--- specifyweb/frontend/js_src/lib/components/Molecules/FilePicker.tsx
+++ specifyweb/frontend/js_src/lib/components/Molecules/FilePicker.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import type { LocalizedString } from 'typesafe-i18n';
 
+import { useAsyncState } from '../../hooks/useAsyncState';
 import { useBooleanState } from '../../hooks/useBooleanState';
+import { useStateForContext } from '../../hooks/useStateForContext';
+import { useTriggerState } from '../../hooks/useTriggerState';
 import { attachmentsText } from '../../localization/attachments';
 import { commonText } from '../../localization/common';
-import type { RA } from '../../utils/types';
+import { wbText } from '../../localization/workbench';
+import type { GetOrSet, GetSet, RA } from '../../utils/types';
+import { localized } from '../../utils/types';
+import { Container, H2, H3 } from '../Atoms';
+import { Button } from '../Atoms/Button';
 import { className } from '../Atoms/className';
+import { Input, Select } from '../Atoms/Form';
 import type { TagProps } from '../Atoms/wrapper';
+import {
+  extractHeader,
+  parseCsv,
+  wbImportPreviewSize,
+} from '../WbImport/helpers';
+import { encodings } from '../WorkBench/encodings';
+import { loadingGif } from '.';
+import type { AutoCompleteItem } from './AutoComplete';
+import { AutoComplete } from './AutoComplete';
 import { useDragDropFiles } from './useDragDropFiles';
 
 export function FilePicker({
@@ -172,3 +190,301 @@ export const fileToText = async (
     fileReader.addEventListener('error', () => reject(fileReader.error));
     fileReader.readAsText(file, encoding);
   });
+
+export function CsvFilePicker({
+  header,
+  getSetHasHeader,
+  onFileImport: handleFileImport,
+  onFileSelected: handleFileSelected,
+}: {
+  readonly header: LocalizedString;
+  readonly getSetHasHeader: GetSet<boolean>;
+  readonly onFileImport: (hasHeader: boolean) => void;
+  readonly onFileSelected?: (file: File) => void;
+}): JSX.Element {
+  const [file, setFile] = React.useState<File | undefined>();
+
+  return (
+    <Container.Full>
+      <H2>{header}</H2>
+      <div className="w-96">
+        <FilePicker
+          acceptedFormats={['.csv', '.tsv', '.psv', '.txt', '.xls', '.xlsx']}
+          onFileSelected={(file): void => {
+            if (typeof handleFileSelected === 'function')
+              handleFileSelected(file);
+            setFile(file);
+          }}
+        />
+      </div>
+      {typeof file === 'object' && (
+        <CsvFilePreview
+          file={file}
+          getSetHasHeader={getSetHasHeader}
+          onFileImport={handleFileImport}
+        />
+      )}
+    </Container.Full>
+  );
+}
+
+export function CsvFilePreview({
+  file,
+  getSetHasHeader,
+  children,
+  onFileImport: handleFileImport,
+}: {
+  readonly file: File;
+  readonly getSetHasHeader: GetOrSet<boolean | undefined>;
+  readonly children?: JSX.Element | undefined;
+  readonly onFileImport: (
+    hasHeader: boolean,
+    encoding: string,
+    getSetDelimiter: GetOrSet<string | undefined>
+  ) => void;
+}): JSX.Element {
+  const [encoding, setEncoding] = React.useState<string>('utf-8');
+  const getSetDelimiter = useStateForContext<string | undefined>(undefined);
+  const preview = useCsvPreview(file, encoding, getSetDelimiter);
+
+  return (
+    <Layout
+      getSetHasHeader={getSetHasHeader}
+      preview={preview}
+      onFileImport={(hasHeader): void =>
+        handleFileImport(hasHeader, encoding, getSetDelimiter)
+      }
+    >
+      {children === undefined ? <></> : children}
+      <ChooseEncoding
+        encoding={encoding}
+        isDisabled={!Array.isArray(preview)}
+        onChange={setEncoding}
+      />
+      <ChooseDelimiter
+        getSetDelimiter={getSetDelimiter}
+        isDisabled={!Array.isArray(preview)}
+      />
+    </Layout>
+  );
+}
+
+export function useCsvPreview(
+  file: File,
+  encoding: string,
+  getSetDelimiter: GetSet<string | undefined>
+): LocalizedString | RA<RA<string>> | undefined {
+  const [delimiter, setDelimiter] = getSetDelimiter;
+  const [preview] = useAsyncState<LocalizedString | RA<RA<string>>>(
+    React.useCallback(
+      async () =>
+        parseCsv(
+          file,
+          encoding,
+          [delimiter, setDelimiter],
+          wbImportPreviewSize
+        ).catch((error) => localized(error.message)),
+      [file, encoding, delimiter, setDelimiter]
+    ),
+    false
+  );
+  return preview;
+}
+
+function ChooseEncoding({
+  encoding = '',
+  isDisabled,
+  onChange: handleChange,
+}: {
+  readonly encoding: string;
+  readonly isDisabled: boolean;
+  readonly onChange: (encoding: string) => void;
+}): JSX.Element {
+  return (
+    <label className="contents">
+      {wbText.characterEncoding()}
+      <Select
+        disabled={isDisabled}
+        value={encoding}
+        onValueChange={handleChange}
+      >
+        {encodings.map((encoding: string) => (
+          <option key={encoding}>{encoding}</option>
+        ))}
+      </Select>
+    </label>
+  );
+}
+
+export const delimiters: RA<AutoCompleteItem<string>> = [
+  { label: wbText.comma(), searchValue: ',', data: ',' },
+  { label: wbText.tab(), searchValue: '\t', data: '\t' },
+  { label: wbText.semicolon(), searchValue: ';', data: ';' },
+  { label: wbText.space(), searchValue: ' ', data: ' ' },
+  { label: wbText.pipe(), searchValue: '|', data: '|' },
+];
+
+function ChooseDelimiter({
+  isDisabled,
+  getSetDelimiter: [delimiter, handleChange],
+}: {
+  readonly isDisabled: boolean;
+  readonly getSetDelimiter: GetSet<string | undefined>;
+}): JSX.Element {
+  const [state, setState] = useTriggerState<string | undefined>(delimiter);
+
+  /**
+   * Don't disable the component if it is currently focused, as disabling it
+   * would lead to focus loss, which is bad UX and an accessibility issue.
+   */
+  const inputRef = React.useRef<HTMLInputElement | null>(null);
+  const isFocused = inputRef.current === document.activeElement;
+  const disabled = isDisabled && !isFocused;
+
+  return (
+    <label className="contents">
+      {wbText.delimiter()}
+      <AutoComplete<string>
+        aria-label={undefined}
+        delay={0}
+        disabled={disabled}
+        filterItems
+        forwardRef={inputRef}
+        inputProps={{
+          onBlur: (): void =>
+            state === undefined ? handleChange(undefined) : undefined,
+        }}
+        minLength={0}
+        source={delimiters}
+        value={
+          state ?? (state === delimiter ? wbText.determineAutomatically() : '')
+        }
+        onChange={({ data }): void => handleChange(data)}
+        onCleared={(): void => {
+          setState(undefined);
+        }}
+        onNewValue={handleChange}
+      />
+    </label>
+  );
+}
+
+export function Layout({
+  preview,
+  getSetHasHeader: [hasHeader = true, setHasHeader],
+  children,
+  onFileImport: handleFileImport,
+}: {
+  readonly preview: LocalizedString | RA<RA<string>> | undefined;
+  readonly getSetHasHeader: GetOrSet<boolean | undefined>;
+  readonly children?: JSX.Element | RA<JSX.Element>;
+  readonly onFileImport: (hasHeader: boolean) => void;
+}): JSX.Element {
+  return (
+    <>
+      <div className="grid w-96 grid-cols-2 items-center gap-2">
+        {children}
+        <ToggleHeader
+          hasHeader={hasHeader}
+          isDisabled={preview === undefined}
+          onChange={setHasHeader}
+        />
+        <Button.Secondary
+          className="col-span-full justify-center text-center"
+          disabled={preview === undefined}
+          onClick={(): void => handleFileImport(hasHeader)}
+        >
+          {wbText.importFile()}
+        </Button.Secondary>
+      </div>
+      {typeof preview === 'string' ? (
+        <BadImport error={preview} />
+      ) : Array.isArray(preview) ? (
+        <Preview hasHeader={hasHeader} preview={preview} />
+      ) : (
+        loadingGif
+      )}
+    </>
+  );
+}
+
+function ToggleHeader({
+  hasHeader,
+  isDisabled,
+  onChange: handleChange,
+}: {
+  readonly hasHeader: boolean;
+  readonly isDisabled: boolean;
+  readonly onChange: (hasHeader: boolean) => void;
+}): JSX.Element {
+  return (
+    <label className="contents">
+      {wbText.firstRowIsHeader()}
+      <span>
+        <Input.Checkbox
+          checked={hasHeader}
+          disabled={isDisabled}
+          onChange={(): void => handleChange(!hasHeader)}
+        />
+      </span>
+    </label>
+  );
+}
+
+function Preview({
+  preview,
+  hasHeader,
+}: {
+  readonly preview: RA<RA<string>>;
+  readonly hasHeader: boolean;
+}): JSX.Element {
+  const { rows, header } = extractHeader(preview, hasHeader);
+
+  return (
+    <div>
+      <H3>{wbText.previewDataSet()}</H3>
+      <div className="overflow-auto">
+        <table>
+          <thead>
+            <tr className="bg-gray-200 text-center dark:bg-neutral-700">
+              {header.map((cell, index) => (
+                <th
+                  className="border border-gray-700 p-1 dark:border-gray-500"
+                  key={index}
+                  scope="col"
+                >
+                  {cell}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {rows.map((row, index) => (
+              <tr key={index}>
+                {row.map((cell, index) => (
+                  <td className="border border-gray-500" key={index}>
+                    {cell}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+function BadImport({
+  error,
+}: {
+  readonly error: LocalizedString;
+}): JSX.Element {
+  return (
+    <p role="alert">
+      {wbText.errorImporting()}
+      <br />
+      {error}
+    </p>
+  );
+}
