52e768bf90dbbd023a461dba1a4c67f55dde7662
realVinayak <realvinayak123@gmail.com>
2024-01-03 07:50:00 -0600

    Add comment

--- specifyweb/frontend/js_src/lib/components/Router/ReactLazy.tsx
+++ specifyweb/frontend/js_src/lib/components/Router/ReactLazy.tsx
@@ -3,6 +3,14 @@ import React from 'react';
 import type { IR } from '../../utils/types';
 import { LoadingScreen } from '../Molecules/Dialog';
 
+/**
+ * Using this allows Webpack to split code bundles.
+ * React Suspense takes care of rendering a loading screen if component is
+ * being fetched.
+ * Having a separate Suspense for each async component rather than a one main
+ * suspense on the top level prevents all components from being un-rendered
+ * when any component is being loaded.
+ */
 export function ReactLazy<PROPS extends IR<unknown>>(
   componentPromise: () => Promise<React.FunctionComponent<PROPS>>
 ) {
