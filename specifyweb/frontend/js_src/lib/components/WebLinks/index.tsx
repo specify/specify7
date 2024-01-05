00e51a579e167c3ea47b6bc74927a9c388f4381c
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2024-01-05 06:27:45 -0800

    Remove gap

--- specifyweb/frontend/js_src/lib/components/WebLinks/index.tsx
+++ specifyweb/frontend/js_src/lib/components/WebLinks/index.tsx
@@ -159,7 +159,7 @@ export function WebLinkField({
           >
             {image}
           </Component>
-          {isInEditor && <div className="flex items-center gap-2">{url}</div>}
+          {isInEditor && <div className="flex items-center">{url}</div>}
           {showPrompt && (
             <PromptDialog
               label={definition.name}
