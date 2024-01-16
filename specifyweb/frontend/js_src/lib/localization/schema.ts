5ea7cb9de5720c4ae53c7559ac056d0121905727
Google Translate <noreply-mt-google-translate@weblate.org>
2024-01-16 08:07:03 +0000

    Sync localization strings with Weblate
    Triggered by 5cc006f54a6792165ca53dd603a67d4770b86ea8 on branch refs/heads/weblate-localization

--- specifyweb/frontend/js_src/lib/localization/schema.ts
+++ specifyweb/frontend/js_src/lib/localization/schema.ts
@@ -364,7 +364,7 @@ export const schemaText = createDictionary({
   schemaViewTitle: {
     'en-us': 'Schema Config: {tableName:string}',
     'es-es': 'Configuración de esquema: {tableName:string}',
-    'fr-fr': 'Configuration du schéma : {tableName :string}',
+    'fr-fr': 'Configuration du schéma : {tableName:string}',
     'ru-ru': 'Конфигурация схемы: {tableName:string}',
     'uk-ua': 'Конфігурація схеми: {tableName:string}',
     'de-ch': 'Schema-Konfiguration: {tableName:string}',
