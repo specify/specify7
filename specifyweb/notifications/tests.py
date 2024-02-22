83c7951320410f547befbb6de91076748c061a76
Pashia Vang <pashiavang2022@gmail.com>
2024-02-22 10:05:33 -0600

    added test for delete_all api

--- specifyweb/notifications/tests.py
+++ specifyweb/notifications/tests.py
@@ -34,4 +34,30 @@ class NotificationsTests(ApiTests):
         self.assertEqual(mockResponse[0]['message_id'], responseReturned[0]['message_id'])
         self.assertEqual(mockResponse[0]['read'], responseReturned[0]['read'])
         self.assertEqual(mockResponse[0]['timestamp'][:-7], responseReturned[0]['timestamp'][:-7])
+        
+    def test_delete_all(self):
+        # Create some test messages
+        testMessage1 = Message.objects.create(
+            user=self.specifyuser,
+            timestampcreated=datetime.now(),
+            content=json.dumps({'type': 'test-type-1', 'file': 'test_file_1.csv'})
+        )
+        testMessage2 = Message.objects.create(
+            user=self.specifyuser,
+            timestampcreated=datetime.now(),
+            content=json.dumps({'type': 'test-type-2', 'file': 'test_file_2.csv'})
+        )
 
+        c = Client()
+        c.force_login(self.specifyuser)
+
+        # Send a POST request to the delete_all endpoint with the IDs of the test messages
+        response = c.post('/notifications/delete_all/', {'message_ids': json.dumps([testMessage1.id, testMessage2.id])})
+
+        # Check if the response is OK
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content.decode(), 'OK')
+
+        # Check if the messages have been deleted
+        self.assertFalse(Message.objects.filter(id=testMessage1.id).exists())
+        self.assertFalse(Message.objects.filter(id=testMessage2.id).exists())
