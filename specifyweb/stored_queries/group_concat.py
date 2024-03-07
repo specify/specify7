44a1f5794e81b29080f826b4558f48c564730732
alec_dev <acwhite211@gmail.com>
2024-03-07 09:41:19 -0600

    fix bug by changing group_concat from Functional to Column Element

--- specifyweb/stored_queries/group_concat.py
+++ specifyweb/stored_queries/group_concat.py
@@ -5,11 +5,32 @@ import sqlalchemy
 from sqlalchemy.sql import expression
 from sqlalchemy.ext import compiler
 
-class group_concat(expression.FunctionElement):
+# class changed from FunctionElement to ColumnElement
+class group_concat(expression.ColumnElement):
     name = "group_concat"
+    def __init__(self, expr, separator=None, order_by=None):
+        self.expr = expr
+        self.separator = separator
+        self.order_by = order_by
 
 @compiler.compiles(group_concat)
 def _group_concat_mysql(element, compiler, **kwargs):
+    # Old way of extracting clauses, when group_concat was a FunctionElement
+    # expr, separator, order_by = extract_clauses(element, compiler)
+
+    inner_expr= compiler.process(element.expr, **kwargs)
+
+    if element.order_by is not None:
+        order_by = compiler.process(element.order_by, **kwargs)
+        inner_expr+= " ORDER BY %s" % order_by
+    if element.separator is not None:
+        # Resorting to text() + bindparams to avoid SQL injection and fix parameter ordering bug
+        separator = compiler.process(sqlalchemy.text(" SEPARATOR :sep").bindparams(sep=element.separator), **kwargs)
+        inner_expr+= " %s" % separator
+
+    return 'GROUP_CONCAT(%s)' % inner_expr
+
+def extract_clauses(element, compiler):
     expr = compiler.process(element.clauses.clauses[0])
     def process_clause(idx):
         return compiler.process(element.clauses.clauses[idx])
@@ -17,11 +38,5 @@ def _group_concat_mysql(element, compiler, **kwargs):
     separator = process_clause(1) if len(element.clauses) > 1 else None
     order_by = process_clause(2) if len(element.clauses) > 2 else None
 
-    inner = expr
-    if order_by is not None:
-        inner += " ORDER BY %s" % order_by
-    if separator is not None:
-        inner += " SEPARATOR %s" % separator
-
-    return 'GROUP_CONCAT(%s)' % inner
+    return expr, separator, order_by
 
