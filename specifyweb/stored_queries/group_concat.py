3f9d2d98b7cefdcc7d061643943f9d4e658bd6d3
realVinayak <realvinayak123@gmail.com>
2024-03-07 08:31:44 -0600

    Convert group concat into a column element

--- specifyweb/stored_queries/group_concat.py
+++ specifyweb/stored_queries/group_concat.py
@@ -5,23 +5,24 @@ import sqlalchemy
 from sqlalchemy.sql import expression
 from sqlalchemy.ext import compiler
 
-class group_concat(expression.FunctionElement):
+class group_concat(expression.ColumnElement):
     name = "group_concat"
+    def __init__(self, expr, separator=None, order_by=None):
+        self.expr = expr
+        self.separator = separator
+        self.order_by = order_by
 
 @compiler.compiles(group_concat)
 def _group_concat_mysql(element, compiler, **kwargs):
-    expr = compiler.process(element.clauses.clauses[0])
-    def process_clause(idx):
-        return compiler.process(element.clauses.clauses[idx])
-
-    separator = process_clause(1) if len(element.clauses) > 1 else None
-    order_by = process_clause(2) if len(element.clauses) > 2 else None
-
+    expr = compiler.process(element.expr, **kwargs)
+    order_by = compiler.process(element.order_by, **kwargs) if element.order_by is not None else None
+    # text() + bindparams is done purely for safety. Can't directly trust user provided separator
+    separator = compiler.process(sqlalchemy.text(" SEPARATOR :sep").bindparams(sep=element.separator), **kwargs) if element.separator is not None else None
     inner = expr
     if order_by is not None:
         inner += " ORDER BY %s" % order_by
     if separator is not None:
-        inner += " SEPARATOR %s" % separator
+        inner += ' %s' % separator
 
     return 'GROUP_CONCAT(%s)' % inner
 
