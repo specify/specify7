aea565c96662731e0d2a569a9aaa1fa02c181b11
alec_dev <acwhite211@gmail.com>
2024-03-08 14:48:07 -0600

    create SpTimestampedModel

--- specifyweb/specify/models.py
+++ specifyweb/specify/models.py
@@ -5,6 +5,30 @@ Sets up Django ORM with the Specify datamodel
 from .build_models import build_models
 from .check_versions import check_versions
 from .datamodel import datamodel
+from django.utils import timezone
+
+class SpTimestampedModel(models.Model):
+    """
+    SpTimestampedModel(id, timestampcreated, timestampmodified)
+    """
+
+    # Fields
+    timestampcreated = models.DateTimeField(db_column='TimestampCreated')
+    timestampmodified = models.DateTimeField(db_column='TimestampModified')
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        timestamp_override = kwargs.pop('timestamp_override', False)
+
+        if not timestamp_override:
+            if not self.id:
+                self.timestampcreated = timezone.now()
+            
+            self.timestampmodified = timezone.now()
+        
+        super().save(*args, **kwargs)
 
 models_by_tableid = build_models(__name__, datamodel)
 
