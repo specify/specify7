ddebb7a3daf98c30713e70be469019e5dd46d9f1
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2024-03-12 07:45:00 -0700

    Add aggregators to QB

--- specifyweb/frontend/js_src/lib/components/QueryBuilder/Line.tsx
+++ specifyweb/frontend/js_src/lib/components/QueryBuilder/Line.tsx
@@ -167,7 +167,7 @@ export function QueryLine({
         canOpenMap = fieldName === 'latitude1' || fieldName === 'longitude1';
       } else if (isMapped)
         fieldType =
-          dataModelField?.isRelationship && relationshipIsToMany(dataModelField)
+          isFormatted && mappingPath.at(-1) === '#1'
             ? 'aggregator'
             : 'formatter';
 
