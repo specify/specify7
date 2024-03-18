26b65d424d691643aad8de2d62e585ad74b7ef17
Sharad <16229739+sharadsw@users.noreply.github.com>
2024-03-18 13:00:10 -0400

    Add a React component that mirrors WbView

--- specifyweb/frontend/js_src/lib/components/WorkBench/WbView.tsx
+++ specifyweb/frontend/js_src/lib/components/WorkBench/WbView.tsx
@@ -53,8 +53,307 @@ import { wbViewTemplate } from './Template';
 import { WbActions } from './WbActions';
 import { WbUtils } from './WbUtils';
 import { WbValidation } from './WbValidation';
+import type { LocalizedString } from 'typesafe-i18n';
+import { className } from '../Atoms/className';
+import { Input } from '../Atoms/Form';
+import { DataSetName } from './DataSetMeta';
+import { WbSpreadsheet } from './WbSpreadsheet';
+import { useBooleanState } from '../../hooks/useBooleanState';
+import { WbToolkit } from './WbToolkit';
 
 export type WbStatus = 'unupload' | 'upload' | 'validate';
+
+function Navigation({
+  name,
+  label,
+}: {
+  readonly name: string;
+  readonly label: LocalizedString;
+}): JSX.Element {
+  return (
+    <span
+      aria-atomic
+      className="wb-navigation-section flex rounded"
+      data-navigation-type={name}
+    >
+      <Button.Small
+        className="wb-cell-navigation brightness-80 hover:brightness-70 p-2 ring-0"
+        data-navigation-direction="previous"
+        variant="bg-inherit text-gray-800 dark:text-gray-100"
+        onClick={f.never}
+      >
+        {'<'}
+      </Button.Small>
+      <Button.Small
+        className={`
+          wb-navigation-text hover:brightness-70 grid grid-cols-[auto_1fr_auto_1fr_auto]
+          items-center ring-0
+          ${className.ariaHandled}
+        `}
+        title={wbText.clickToToggle()}
+        variant="bg-inherit text-gray-800 dark:text-gray-100"
+        onClick={f.never}
+      >
+        {label} (<span className="wb-navigation-position text-center">0</span>/
+        <span className="wb-navigation-total">0</span>)
+      </Button.Small>
+      <Button.Small
+        className="wb-cell-navigation brightness-80 hover:brightness-70 p-2 ring-0"
+        data-navigation-direction="next"
+        type="button"
+        variant="bg-inherit text-gray-800 dark:text-gray-100"
+        onClick={f.never}
+      >
+        {'>'}
+      </Button.Small>
+    </span>
+  );
+}
+
+export type HandlersObject = {
+  [key: string]: {
+    show: boolean;
+    open: () => void;
+    close: () => void;
+    toggle: () => void;
+  };
+};
+
+// Returns an object consisting of handler objects for dialog actions in the Workbench
+function useWbViewHandlers() {
+  const handlers = {
+    toolkit: useBooleanState(),
+    devPlan: useBooleanState(),
+    changeOwner: useBooleanState(),
+    geoLocate: useBooleanState(),
+    leafletMap: useBooleanState(),
+    coordinatesConversion: useBooleanState(),
+  };
+
+  // convert useBooleanState()'s resulting array to an object
+  const handlersObject: HandlersObject = {};
+  for (const [name, state] of Object.entries(handlers)) {
+    const [show, open, close, toggle] = state;
+    handlersObject[name] = { show, open, close, toggle };
+  }
+
+  return handlersObject;
+}
+
+export function WbViewReact({
+  dataset,
+  hotRef,
+  handleDatasetDelete,
+}: {
+  readonly dataset: Dataset;
+  readonly hotRef: any;
+  handleDatasetDelete: () => void;
+}): JSX.Element {
+  const data = React.useMemo<RA<RA<string | null>>>(
+    () =>
+      dataset.rows.length === 0
+        ? [Array.from(dataset.columns).fill(null)]
+        : dataset.rows,
+    [dataset.rows]
+  );
+  const isMapped = Boolean(dataset.uploadplan);
+  const isUploaded =
+    dataset.uploadresult !== null && dataset.uploadresult.success;
+  const canUpdate = hasPermission('/workbench/dataset', 'update');
+  const [canLiveValidate] = userPreferences.use(
+    'workBench',
+    'general',
+    'liveValidation'
+  );
+  const { toolkit, devPlan, ...toolkitOptions } = useWbViewHandlers();
+  const mappings = React.useMemo(
+    (): WbMapping | undefined => parseWbMappings(dataset),
+    [dataset]
+  );
+  const hot = hotRef.current ? hotRef.current.hotInstance : undefined;
+  return (
+    <>
+      <div
+        className="flex items-center justify-between gap-x-1 gap-y-2 whitespace-nowrap"
+        role="toolbar"
+      >
+        <div className="wb-name-container contents">
+          <DataSetName
+            dataset={dataset}
+            getRowCount={() =>
+              hot
+                ? dataset.rows.length
+                : hot.countRows() - hot.countEmptyRows(true)
+            }
+          />
+        </div>
+        <Button.Small
+          aria-haspopup="grid"
+          aria-pressed={toolkit.show}
+          className="wb-show-toolkit"
+          onClick={toolkit.toggle}
+        >
+          {commonText.tools()}
+        </Button.Small>
+        <span className="-ml-1 flex-1" />
+        {/* This button is here for debugging only */}
+        <Button.Small
+          className={`
+            wb-show-plan
+            ${process.env.NODE_ENV === 'development' ? '' : 'hidden'}
+          `}
+          onClick={devPlan.open}
+        >
+          [DEV] Show Plan
+        </Button.Small>
+        {canUpdate || isMapped ? (
+          <Link.Small href={`/specify/workbench/plan/${dataset.id}/`}>
+            {wbPlanText.dataMapper()}
+          </Link.Small>
+        ) : undefined}
+        {!isUploaded && hasPermission('/workbench/dataset', 'validate') && (
+          <>
+            <Button.Small
+              className={`wb-data-check ${canLiveValidate ? '' : 'hidden'}`}
+              onClick={undefined}
+            >
+              {wbText.dataCheck()}
+            </Button.Small>
+            <Button.Small
+              aria-haspopup="dialog"
+              className="wb-validate"
+              onClick={undefined}
+            >
+              {wbText.validate()}
+            </Button.Small>
+          </>
+        )}
+        <Button.Small
+          aria-haspopup="tree"
+          className="wb-show-upload-view"
+          title={wbText.wbUploadedUnavailable()}
+          onClick={undefined}
+        >
+          {commonText.results()}
+        </Button.Small>
+        {isUploaded ? (
+          hasPermission('/workbench/dataset', 'unupload') && (
+            <Button.Small
+              aria-haspopup="dialog"
+              className="wb-unupload"
+              onClick={f.never}
+            >
+              {wbText.rollback()}
+            </Button.Small>
+          )
+        ) : (
+          <>
+            {hasPermission('/workbench/dataset', 'upload') && (
+              <Button.Small
+                aria-haspopup="dialog"
+                className="wb-upload"
+                onClick={f.never}
+              >
+                {wbText.upload()}
+              </Button.Small>
+            )}
+            {hasPermission('/workbench/dataset', 'update') && (
+              <>
+                <Button.Small
+                  aria-haspopup="dialog"
+                  className="wb-revert"
+                  onClick={undefined}
+                >
+                  {wbText.revert()}
+                </Button.Small>
+                <Button.Small
+                  aria-haspopup="dialog"
+                  className="wb-save"
+                  variant={className.saveButton}
+                  onClick={undefined}
+                >
+                  {commonText.save()}
+                </Button.Small>
+              </>
+            )}
+          </>
+        )}
+      </div>
+      {toolkit.show && (
+        <WbToolkit
+          dataset={dataset}
+          hotRef={hotRef}
+          toolkitOptions={toolkitOptions}
+          mappings={mappings as WbMapping}
+          data={data}
+        />
+      )}
+      {devPlan.show && (
+        <DevShowPlan
+          dataSetId={dataset.id}
+          uploadPlan={dataset.uploadplan ?? ({} as UploadPlan)}
+          onChanged={(plan) => {
+            overwriteReadOnly(dataset, 'uploadplan', plan);
+            // TODO: figure out this trigger
+            // trigger('refresh');
+          }}
+          onClose={devPlan.close}
+          onDeleted={handleDatasetDelete}
+        />
+      )}
+      <div className="flex flex-1 gap-4 overflow-hidden">
+        <section className="wb-spreadsheet flex-1 overflow-hidden overscroll-none">
+          <WbSpreadsheet
+            dataset={dataset}
+            hotRef={hotRef}
+            isUploaded={isUploaded}
+            data={data}
+          />
+        </section>
+        <aside aria-live="polite" className="wb-uploaded-view-wrapper hidden" />
+      </div>
+      <div
+        aria-label={wbText.navigation()}
+        className="flex flex-wrap justify-end gap-x-1 gap-y-2"
+        role="toolbar"
+      >
+        <span className="contents" role="search">
+          <div className="flex">
+            <Input.Generic
+              aria-label={commonText.searchQuery()}
+              autoComplete="on"
+              className="wb-search-query"
+              placeholder={commonText.search()}
+              spellCheck
+              title={commonText.searchQuery()}
+              type="search"
+            />
+          </div>
+          {!isUploaded && hasPermission('/workbench/dataset', 'update') ? (
+            <div className="flex">
+              <Input.Text
+                aria-label={wbText.replacementValue()}
+                autoComplete="on"
+                className="wb-replace-value"
+                placeholder={wbText.replace()}
+                title={wbText.replacementValue()}
+              />
+            </div>
+          ) : undefined}
+          <span className="wb-advanced-search-wrapper" />
+        </span>
+        <Navigation label={wbText.searchResults()} name="searchResults" />
+        {!isUploaded && hasPermission('/workbench/dataset', 'update') ? (
+          <Navigation label={wbText.modifiedCells()} name="modifiedCells" />
+        ) : undefined}
+        <Navigation label={wbText.newCells()} name="newCells" />
+        {!isUploaded && (
+          <Navigation label={wbText.errorCells()} name="invalidCells" />
+        )}
+      </div>
+    </>
+  );
+}
 
 // REFACTOR: when rewriting to React, add ErrorBoundaries
 
@@ -119,7 +418,7 @@ export class WbView extends Backbone.View {
     public refreshInitiatedBy: WbStatus | undefined,
     // eslint-disable-next-line functional/prefer-readonly-type
     public refreshInitiatorAborted: boolean,
-    public readonly element: HTMLElement,
+    public readonly element: HTMLElement, // shouldn't be needed in React
     public readonly options: {
       readonly onSetUnloadProtect: (unloadProtect: boolean) => void;
       readonly onDeleted: () => void;
