6f2797921a2c5c2734c3010ab4e4fa8361bc1e49
Sharad <16229739+sharadsw@users.noreply.github.com>
2024-03-29 11:34:35 -0400

    Refactor index.ts

--- specifyweb/frontend/js_src/lib/components/WorkBench/index.tsx
+++ specifyweb/frontend/js_src/lib/components/WorkBench/index.tsx
@@ -1,58 +1,44 @@
 import React from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import type { LocalizedString } from 'typesafe-i18n';
 
-import { useUnloadProtect } from '../../hooks/navigation';
 import { useAsyncState } from '../../hooks/useAsyncState';
 import { useBooleanState } from '../../hooks/useBooleanState';
 import { useErrorContext } from '../../hooks/useErrorContext';
 import { commonText } from '../../localization/common';
-import { localityText } from '../../localization/locality';
-import { wbPlanText } from '../../localization/wbPlan';
 import { wbText } from '../../localization/workbench';
 import { ajax } from '../../utils/ajax';
 import { f } from '../../utils/functools';
-import type { GetSet, RA } from '../../utils/types';
-import { replaceItem } from '../../utils/utils';
-import { Button } from '../Atoms/Button';
+import type { GetSet } from '../../utils/types';
 import { className } from '../Atoms/className';
-import { Input } from '../Atoms/Form';
-import { Link } from '../Atoms/Link';
 import { LoadingContext } from '../Core/Contexts';
 import { useMenuItem } from '../Header/MenuContext';
 import { treeRanksPromise } from '../InitialContext/treeRanks';
 import { Dialog } from '../Molecules/Dialog';
-import { Portal } from '../Molecules/Portal';
-import { hasPermission, hasTablePermission } from '../Permissions/helpers';
-import { userPreferences } from '../Preferences/userPreferences';
 import { NotFoundView } from '../Router/NotFoundView';
 import type { Dataset } from '../WbPlanView/Wrapped';
-import type { WbStatus } from './WbView';
-import { WbView as WbViewClass, WbViewReact } from './WbView';
-import { DataSetName } from './DataSetMeta';
-import { WbSpreadsheet } from './WbSpreadsheet';
-import Handsontable from 'handsontable';
+import { WbViewReact } from './WbView';
+
 
 const fetchTreeRanks = async (): Promise<true> => treeRanksPromise.then(f.true);
 
 const fetchDataSet = async (
-    dataSetId: number | undefined
-  ): Promise<Dataset | undefined> =>
-    typeof dataSetId === 'number'
-      ? ajax<Dataset>(`/api/workbench/dataset/${dataSetId}/`, {
-          headers: { Accept: 'application/json' },
-        }).then(({ data }) => data)
-      : undefined;
+  dataSetId: number | undefined
+): Promise<Dataset | undefined> =>
+  typeof dataSetId === 'number'
+    ? ajax<Dataset>(`/api/workbench/dataset/${dataSetId}/`, {
+        headers: { Accept: 'application/json' },
+      }).then(({ data }) => data)
+    : undefined;
 
 // BUG: intercept 403 (if dataset has been transferred to another user)
 function useDataSet(
-    dataSetId: number | undefined
-  ): GetSet<Dataset | undefined> {
-    return useAsyncState(
-      React.useCallback(async () => fetchDataSet(dataSetId), [dataSetId]),
-      true
-    );
-  }
+  dataSetId: number | undefined
+): GetSet<Dataset | undefined> {
+  return useAsyncState(
+    React.useCallback(async () => fetchDataSet(dataSetId), [dataSetId]),
+    true
+  );
+}
 
 export function WorkBenchReact(): JSX.Element | null {
   useMenuItem('workBench');
@@ -69,13 +55,14 @@ export function WorkBenchReact(): JSX.Element | null {
 
   const navigate = useNavigate();
   const hotRef = React.useRef(null);
+  const spreadsheetContainer = React.useRef<HTMLElement | null>(null);
 
   // temporary null check, replace with loading screen?
   if (!dataSet || !treeRanksLoaded) return null;
 
   const triggerRefresh = () => {
     loading(fetchDataSet(dataSet!.id).then(setDataSet));
-  }
+  };
 
   return dataSetId === undefined ? (
     <NotFoundView />
@@ -92,12 +79,13 @@ export function WorkBenchReact(): JSX.Element | null {
   ) : (
     <>
       <div className="contents">
-        <section className={`wbs-form ${className.containerFull}`}>
+        <section className={`wbs-form ${className.containerFull}`} ref={spreadsheetContainer}>
           <WbViewReact
             dataset={dataSet}
             hotRef={hotRef}
             handleDatasetDelete={handleDeleted}
             triggerRefresh={triggerRefresh}
+            spreadsheetContainer={spreadsheetContainer}
           />
         </section>
       </div>
