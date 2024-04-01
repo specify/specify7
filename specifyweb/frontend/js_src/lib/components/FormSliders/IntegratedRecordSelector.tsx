f0260cc37e9c4a80786651e48005e9202bb5b40f
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2024-04-01 11:22:07 -0700

    Remove import

--- specifyweb/frontend/js_src/lib/components/FormSliders/IntegratedRecordSelector.tsx
+++ specifyweb/frontend/js_src/lib/components/FormSliders/IntegratedRecordSelector.tsx
@@ -13,7 +13,6 @@ import type { AnySchema } from '../DataModel/helperTypes';
 import type { SpecifyResource } from '../DataModel/legacyTypes';
 import { useAllSaveBlockers } from '../DataModel/saveBlockers';
 import type { Collection } from '../DataModel/specifyTable';
-import { tables } from '../DataModel/tables';
 import type {
   DisposalPreparation,
   GiftPreparation,
