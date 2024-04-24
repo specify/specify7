bcadebd3066b8faf1e17f6c3c2df1f02bdfe65e2
alec_dev <acwhite211@gmail.com>
2024-04-24 16:18:05 -0500

    remove remaining changes in perparation_rules

--- specifyweb/businessrules/rules/preparation_rules.py
+++ specifyweb/businessrules/rules/preparation_rules.py
@@ -1,6 +1,4 @@
 from specifyweb.businessrules.orm_signal_handler import orm_signal_handler
-from django.db.models import Sum
-from specifyweb.specify import models
 
 @orm_signal_handler('pre_save', 'Preparation')
 def preparation_pre_save(preparation):
