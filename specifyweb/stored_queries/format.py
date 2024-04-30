98b84b5841a3e9d3ebb45de13bf15618602239e4
alec_dev <acwhite211@gmail.com>
2024-04-30 15:41:32 -0500

    fix index error for the formatter

--- specifyweb/stored_queries/format.py
+++ specifyweb/stored_queries/format.py
@@ -168,7 +168,11 @@ class ObjectFormatter(object):
             for node in caseNode.findall('field'):
                 query, expr, _ = self.make_expr(query, node.text, node.attrib, orm_table, specify_model, cycle_with_self)
                 field_exprs.append(expr)
-            expr = concat(*field_exprs) if len(field_exprs) > 1 else field_exprs[0]
+            
+            expr = ""
+            if field_exprs:
+                expr = concat(*field_exprs) if len(field_exprs) > 1 else field_exprs[0]
+
             return query, caseNode.attrib.get('value', None), expr
 
         switchNode = formatterNode.find('switch')
