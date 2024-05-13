6b084013c233bd4a67442c43a32ec89aaddd0822
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2024-05-13 09:13:20 -0400

    Display view btn in case edit btn is set to false

--- specifyweb/frontend/js_src/lib/components/FormParse/fields.ts
+++ specifyweb/frontend/js_src/lib/components/FormParse/fields.ts
@@ -212,13 +212,11 @@ const processFieldType: {
       maxLength: f.parseInt(getProperty('maxLength')),
     };
   },
-  QueryComboBox({ getProperty, fields, table, cell }) {
+  QueryComboBox({ getProperty, fields }) {
     if (fields === undefined) {
       console.error('Trying to render a query combobox without a field name');
       return { type: 'Blank' };
     } else if (fields.at(-1)?.isRelationship === true) {
-      const isParentTaxon =
-        table.name === 'Taxon' && cell.attributes.name === 'parent';
       return {
         type: 'QueryComboBox',
         hasCloneButton: getProperty('cloneBtn')?.toLowerCase() === 'true',
@@ -226,7 +224,8 @@ const processFieldType: {
         hasSearchButton: getProperty('searchBtn')?.toLowerCase() !== 'false',
         hasEditButton: getProperty('editBtn')?.toLowerCase() !== 'false',
         hasViewButton:
-          getProperty('viewBtn') === undefined && isParentTaxon
+          getProperty('viewBtn') === undefined &&
+          getProperty('editBtn')?.toLowerCase() === 'false'
             ? true
             : getProperty('viewBtn')?.toLowerCase() === 'true',
         typeSearch: getProperty('name'),
