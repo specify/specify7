186a0f67841e0facbd1785a298796cb468aa6d90
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2024-05-24 10:33:34 -0700

    Fix boolean

--- specifyweb/frontend/js_src/lib/components/QueryBuilder/Wrapped.tsx
+++ specifyweb/frontend/js_src/lib/components/QueryBuilder/Wrapped.tsx
@@ -301,7 +301,7 @@ function Wrapped({
 
   const showSeries =
     table.name === 'CollectionObject' &&
-    state.fields.find((field) => field.mappingPath[0] === 'catalogNumber');
+    state.fields.some((field) => field.mappingPath[0] === 'catalogNumber');
 
   return treeRanksLoaded ? (
     <ReadOnlyContext.Provider value={isReadOnly}>
