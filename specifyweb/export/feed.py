ec573e44f8ca8ca7a406be9da5c4c6af59fb6809
melton-jason <meltonjason12@yahoo.com>
2024-05-27 12:23:11 -0500

    Don't send additional notificaitons if logged in as noifyUser user

--- specifyweb/export/feed.py
+++ specifyweb/export/feed.py
@@ -66,7 +66,8 @@ def update_feed(force=False, notify_user: Optional[Specifyuser] = None):
             if notify_user_id:
                 user = Specifyuser.objects.get(id=notify_user_id)
                 create_notification(user, filename)
-                create_notification(notify_user, None)
+                if notify_user_id != user.id:
+                    create_notification(notify_user, None)
             elif notify_user:
                 create_notification(notify_user)
 
