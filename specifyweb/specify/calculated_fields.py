83f91bcb753f2130f6dcbf2c2902bd7653306932
alec_dev <acwhite211@gmail.com>
2024-06-03 12:16:26 -0500

    ensure positive unresolved quantities count

--- specifyweb/specify/calculated_fields.py
+++ specifyweb/specify/calculated_fields.py
@@ -105,7 +105,7 @@ def calculate_extra_fields(obj, data: Dict[str, Any]) -> Dict[str, Any]:
         quantities = sum(prep.get('quantity', 0) for prep in preps)
         unresolved_prep_count = sum(not prep["isresolved"] for prep in preps)
         unresolved_quantities = sum(
-            ((prep.get("quantity") or 0) - (prep.get("quantityresolved") or 0))
+            max((prep.get("quantity") or 0) - (prep.get("quantityresolved") or 0), 0)
             * (not prep["isresolved"])
             for prep in preps
         )
