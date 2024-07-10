b40008b7c26b51369f16eabc91cf1e9db102ff6a
alec_dev <acwhite211@gmail.com>
2024-07-10 15:31:01 -0500

    create geo trees django command

---
+++ specifyweb/specify/management/commands/create_geo_trees.py
@@ -0,0 +1,26 @@
+from typing import Any
+from django.core.management.base import BaseCommand
+from specifyweb.specify.tree_views import TAXON_TREES, TAXON_RANKS
+from specifyweb.specify.models import Taxontreedef, Taxontreedefitem
+
+class Command(BaseCommand):
+    def handle(self, *args: Any, **options: Any) -> str | None:
+        help = "Add initial geology taxon tree definitions to the database."
+
+        for tree in TAXON_TREES:
+            if Taxontreedef.objects.filter(name=tree).exists():
+                continue
+            tree_name = f"{tree} Taxon"
+            ttd = Taxontreedef.objects.create(name=tree_name)
+            rank_id = -10
+            ttdi = None
+            for rank in TAXON_RANKS:
+                name = f"{tree} {rank}"
+                rank_id += 10
+                ttdi = Taxontreedefitem.objects.create(
+                    name=name,
+                    title=name,
+                    rankid=rank_id,
+                    parent=ttdi,
+                    treedef=ttd,
+                )
