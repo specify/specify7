62a96c52a25c462cdcd33e9c5cdc5ebbc5022f83
combs-a <combs.anya@gmail.com>
2024-07-10 19:03:14 +0000

    Lint code with ESLint and Prettier
    Triggered by 425af04f78b8e7555cd9a1779ad7fe63562c6e8a on branch refs/heads/dev-notifs

--- specifyweb/frontend/js_src/lib/components/Notifications/hooks.tsx
+++ specifyweb/frontend/js_src/lib/components/Notifications/hooks.tsx
@@ -5,11 +5,12 @@ import { ajax } from '../../utils/ajax';
 import { formatDateForBackEnd } from '../../utils/parser/dateFormat';
 import type { IR, RA } from '../../utils/types';
 import { sortFunction } from '../../utils/utils';
+import { MINUTE, SECOND } from '../Atoms/timeUnits';
 import { formatUrl } from '../Router/queryString';
-import { SECOND, MINUTE } from '../Atoms/timeUnits'
 import type { GenericNotification } from './NotificationRenderers';
 
-const INITIAL_INTERVAL = process.env.NODE_ENV === 'development' ? 1 * MINUTE : 5 * SECOND;
+const INITIAL_INTERVAL =
+  process.env.NODE_ENV === 'development' ? Number(MINUTE) : 5 * SECOND;
 const INTERVAL_MULTIPLIER = 1.1;
 
 export function useNotificationsFetch({
