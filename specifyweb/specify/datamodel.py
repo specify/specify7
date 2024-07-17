47c34f2ec3ac8af7edf33fbf3ad91d24481b4689
alec_dev <acwhite211@gmail.com>
2024-07-17 11:16:22 -0500

    cogtype is a required field in the datamodel

--- specifyweb/specify/datamodel.py
+++ specifyweb/specify/datamodel.py
@@ -8272,7 +8272,7 @@ datamodel = Datamodel(tables=[
         ],
         relationships=[
             Relationship(name='collection', type='many-to-one', required=False, relatedModelName='Collection', column='CollectionID'),
-            Relationship(name='cogtype', type='many-to-one', required=False, relatedModelName='CollectionObjectGroupType', column='COGTypeID'),
+            Relationship(name='cogtype', type='many-to-one', required=True, relatedModelName='CollectionObjectGroupType', column='COGTypeID'),
             Relationship(name='parentcojos', type='one-to-many', required=False, relatedModelName='CollectionObjectGroupJoin', otherSideName='parentcog', dependent=True),
             Relationship(name='cojo', type='one-to-one', required=False, relatedModelName='CollectionObjectGroupJoin', otherSideName='childcog', dependent=True),
             Relationship(name='createdByAgent', type='many-to-one', required=False, relatedModelName='Agent', column='CreatedByAgentID'),
