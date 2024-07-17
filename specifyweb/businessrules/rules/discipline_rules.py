2a274fde30ee98ee9c6cb1d967c22254fe1731e1
alec_dev <acwhite211@gmail.com>
2024-07-17 16:48:18 -0500

    fix scoping unit tests

--- specifyweb/businessrules/rules/discipline_rules.py
+++ specifyweb/businessrules/rules/discipline_rules.py
@@ -1,6 +1,6 @@
 from specifyweb.businessrules.orm_signal_handler import orm_signal_handler
 from specifyweb.specify.models import Taxontreedef
-
+import re
 
 @orm_signal_handler('pre_save', 'Discipline')
 def create_taxontreedef_if_null(discipline):
@@ -8,5 +8,15 @@ def create_taxontreedef_if_null(discipline):
         # only do this for new disciplines
         return
     if discipline.taxontreedef is None:
+        # Get all Taxontreedef objects with names starting with 'Sample' followed by a number
+        sample_taxontreedefs = Taxontreedef.objects.filter(name__regex=r'^Sample\d+$')
+        max_number = 0
+        for taxontreedef in sample_taxontreedefs:
+            # Extract the number from the name
+            number = int(re.search(r'\d+$', taxontreedef.name).group())
+            max_number = max(max_number, number)
+        
+        # Create a new Taxontreedef with the incremented name
+        new_number = max_number + 1
         discipline.taxontreedef = Taxontreedef.objects.create(
-            name='Sample')
+            name=f'Sample{new_number}')
