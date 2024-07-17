a39c9da3e9194cbfd1761dc40484d85695808b5f
alec_dev <acwhite211@gmail.com>
2024-07-17 10:09:33 -0500

    leave out cogtype_pre_save for now

--- specifyweb/businessrules/rules/cogtype_rules.py
+++ specifyweb/businessrules/rules/cogtype_rules.py
@@ -15,9 +15,9 @@ COG_TYPE_TYPES = {
 }
 
 # TODO: Maybe remove this rule?
-@orm_signal_handler('pre_save', 'CollectionObjectGroupType')
-def cogtype_pre_save(cog_type):
-    # if cog_type.name not in COG_TYPE_NAMES:
-    #     raise BusinessRuleException(f'Invalid cog type name: {cog_type.name}')
-    if cog_type.type not in COG_TYPE_TYPES:
-        raise BusinessRuleException(f'Invalid cog type: {cog_type.type}')
+# @orm_signal_handler('pre_save', 'CollectionObjectGroupType')
+# def cogtype_pre_save(cog_type):
+#     # if cog_type.name not in COG_TYPE_NAMES:
+#     #     raise BusinessRuleException(f'Invalid cog type name: {cog_type.name}')
+#     if cog_type.type not in COG_TYPE_TYPES:
+#         raise BusinessRuleException(f'Invalid cog type: {cog_type.type}')
