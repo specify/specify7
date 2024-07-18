ca133053c6b1dabfc3eca8ae64a7327d7d891a99
melton-jason <meltonjason12@yahoo.com>
2024-07-18 18:32:43 -0500

    Add preliminary businessrule to set default COT on CO

--- specifyweb/businessrules/tests/test_collectionobject.py
+++ specifyweb/businessrules/tests/test_collectionobject.py
@@ -1,14 +1,31 @@
-from specifyweb.specify import models
+from specifyweb.specify.models import Collection, Collectionobject, CollectionObjectType
 from specifyweb.specify.tests.test_api import ApiTests
 from ..exceptions import BusinessRuleException
+
 
 class CollectionObjectTests(ApiTests):
     def test_catalog_number_unique_in_collection(self):
         with self.assertRaises(BusinessRuleException):
-            models.Collectionobject.objects.create(
+            Collectionobject.objects.create(
                 collection=self.collection,
                 catalognumber=self.collectionobjects[0].catalognumber)
 
-        models.Collectionobject.objects.create(
+        Collectionobject.objects.create(
             collection=self.collection,
             catalognumber=self.collectionobjects[0].catalognumber + 'foo')
+
+    def test_default_collectionobjecttype(self):
+        default_type = CollectionObjectType.objects.create(
+            name="default type",
+            collection=self.collection,
+            taxontreedef=self.discipline.taxontreedef
+        )
+        self.collection.collectionobjecttype = default_type
+        self.collection.save()
+
+        test_co = Collectionobject.objects.create(
+            collection=self.collection
+        )
+
+        self.assertIsNotNone(test_co.collectionobjecttype)
+        self.assertEqual(test_co.collectionobjecttype, default_type)
