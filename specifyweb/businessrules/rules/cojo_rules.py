cb705308c338e1207072d0a9d659090557672e18
alec_dev <acwhite211@gmail.com>
2024-07-18 10:49:56 -0500

    add to cojo_pre_save business rule

--- specifyweb/businessrules/rules/cojo_rules.py
+++ specifyweb/businessrules/rules/cojo_rules.py
@@ -1,8 +1,13 @@
+from specifyweb.businessrules.exceptions import BusinessRuleException
 from specifyweb.businessrules.orm_signal_handler import orm_signal_handler
 from specifyweb.specify.models import CollectionObjectGroupJoin
 
 @orm_signal_handler('pre_save', 'CollectionObjectGroupJoin')
 def cojo_pre_save(cojo):
+    # Ensure the both the childcog and childco fields are not null.
+    if cojo.childcog == None and cojo.childco == None:
+        raise BusinessRuleException('Both childcog and childco cannot be null.')
+
     # For records with the same parentcog field, there can be only one isPrimare field set to True.
     # So when a record is saved with isPrimary set to True, we need to set all other records with the same parentcog
     # to isPrimary = False.
