c23ec25b3293c276305c67925dc07739538b2498
melton-jason <meltonjason12@yahoo.com>
2024-07-31 14:26:05 -0500

    Unify tree scoping code on backend

--- specifyweb/stored_queries/execution.py
+++ specifyweb/stored_queries/execution.py
@@ -14,7 +14,7 @@ from sqlalchemy import sql, orm, func, select
 from sqlalchemy.sql.expression import asc, desc, insert, literal
 
 from specifyweb.stored_queries.group_concat import group_by_displayed_fields
-from specifyweb.specify.tree_utils import scoped_treedef_ids
+from specifyweb.specify.tree_utils import get_treedefs
 
 from . import models
 from .format import ObjectFormatter
@@ -53,11 +53,13 @@ def filter_by_collection(model, query, collection):
 
     if model is models.Taxon:
         logger.info("filtering taxon to discipline: %s", collection.discipline.name)
-        return query.filter(model.TaxonTreeDefID.in_(scoped_treedef_ids(collection, "Taxon")))
+        tree_def_ids = [treedef[0] for treedef in get_treedefs(collection, 'taxon')]
+        return query.filter(model.TaxonTreeDefID.in_(tree_def_ids))
 
     if model is models.TaxonTreeDefItem:
         logger.info("filtering taxon rank to discipline: %s", collection.discipline.name)
-        return query.filter(model.TaxonTreeDefID.in_(scoped_treedef_ids(collection, 'Taxon')))
+        tree_def_ids = [treedef[0] for treedef in get_treedefs(collection, 'taxon')]
+        return query.filter(model.TaxonTreeDefID.in_(tree_def_ids))
 
     if model is models.Geography:
         logger.info("filtering geography to discipline: %s", collection.discipline.name)
