7c052d2bb3f2e133ee85dc462b734f84546caa6b
alec_dev <acwhite211@gmail.com>
2024-08-20 15:45:21 -0500

    fix mypy error

--- specifyweb/workbench/upload/upload_plan_schema.py
+++ specifyweb/workbench/upload/upload_plan_schema.py
@@ -332,31 +332,6 @@ def parse_tree_record(collection, table: Table, to_parse: Dict, base_treedefid: 
         treedef_id=base_treedefid
     )
 
-def get_treedef_id(rank_name: str, is_adjusting: bool, base_treedef_id: Optional[int] = None) -> Optional[int]:
-    def find_treedef(treedef_id: Optional[int] = None, is_adjusting: bool = False) -> Optional['Taxontreedef']:
-        filter_kwargs = {'name': rank_name}
-        if treedef_id is not None:
-            filter_kwargs['treedef_id'] = str(treedef_id)
-        ranks = Taxontreedefitem.objects.filter(**filter_kwargs)
-        if ranks.count() == 0:
-            return None
-        elif ranks.count() > 1:
-            if is_adjusting:
-                raise ValueError(f"Multiple treedefitems with name {rank_name}") 
-        return ranks.first().treedef
-
-    treedef = find_treedef(base_treedef_id)
-    if treedef:
-        return treedef.id
-
-    treedef = find_treedef()
-    if treedef:
-        return treedef.id
-
-    if is_adjusting:
-        raise ValueError(f"Could not find treedefitem with name {rank_name}")
-    return None
-
 
 def find_treedef(
     rank_name: str, treedef_id: Optional[int] = None, is_adjusting: bool = False
@@ -369,7 +344,12 @@ def find_treedef(
         return None
     elif ranks.count() > 1 and is_adjusting and treedef_id is not None:
         raise ValueError(f"Multiple treedefitems with name {rank_name} and treedef_id {treedef_id}")
-    return ranks.first().treedef
+    
+    first_rank = ranks.first()
+    if first_rank is None:
+        return None
+    
+    return first_rank.treedef
 
 def get_treedef_id(rank_name: str, is_adjusting: bool, base_treedef_id: Optional[int] = None) -> Optional[int]:
     for treedef_id in [base_treedef_id, None]:
