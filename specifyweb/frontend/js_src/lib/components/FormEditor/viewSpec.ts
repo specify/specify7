2e46526f4a8dbac57d9f8ca2682bfe34174d6be4
melton-jason <meltonjason12@yahoo.com>
2024-09-02 09:36:40 -0500

    Show helpful error when rendering to-many as querycbx

--- specifyweb/frontend/js_src/lib/components/FormEditor/viewSpec.ts
+++ specifyweb/frontend/js_src/lib/components/FormEditor/viewSpec.ts
@@ -17,6 +17,7 @@ import { syncers } from '../Syncer/syncers';
 import type { SimpleXmlNode } from '../Syncer/xmlToJson';
 import { createSimpleXmlNode } from '../Syncer/xmlToJson';
 import { createXmlSpec } from '../Syncer/xmlUtils';
+import { relationshipIsToMany } from '../WbPlanView/mappingHelpers';
 
 /* eslint-disable @typescript-eslint/no-magic-numbers */
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
@@ -799,8 +800,32 @@ const textAreaSpec = (
     ),
   });
 
-const queryComboBoxSpec = f.store(() =>
+const queryComboBoxSpec = (
+  _spec: SpecToJson<ReturnType<typeof rawFieldSpec>>,
+  {
+    table,
+  }: {
+    readonly table: SpecifyTable | undefined;
+  }
+) =>
   createXmlSpec({
+    field: pipe(
+      rawFieldSpec(table).field,
+      syncer(
+        ({ parsed, ...rest }) => {
+          if (
+            parsed?.some(
+              (field) => field.isRelationship && relationshipIsToMany(field)
+            )
+          )
+            console.error(
+              'Unable to render a to-many relationship as a querycbx. Use a Subview instead'
+            );
+          return { parsed, ...rest };
+        },
+        (value) => value
+      )
+    ),
     // Customize view name
     dialogViewName: syncers.xmlAttribute('initialize displayDlg', 'skip'),
     searchDialogViewName: syncers.xmlAttribute('initialize searchDlg', 'skip'),
@@ -836,8 +861,7 @@ const queryComboBoxSpec = f.store(() =>
       syncers.maybe(syncers.toBoolean),
       syncers.default<boolean>(true)
     ),
-  })
-);
+  });
 
 const checkBoxSpec = f.store(() =>
   createXmlSpec({
