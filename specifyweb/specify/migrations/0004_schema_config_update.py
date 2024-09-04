c20eeb2f1d3d9447e8d02bccce5eaa9741e59a4d
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2024-09-04 11:43:44 -0700

    Add missing file

---
+++ specifyweb/specify/migrations/0004_schema_config_update.py
@@ -0,0 +1,177 @@
+"""
+This migration updates the Schema Config entries for pre-geo tables. 
+Geo migration added new fields to already existing tables. This migration updates the Schema Config to reflect those changes.
+
+Fields added:
+Collection -> collectionObjectType
+GeographyTreeDef -> discipline
+GeologicTimePeriodTreeDef -> discipline
+LithostratTreeDef -> discipline
+StorageTreeDef -> institution
+TaxonTreeDef -> discipline
+"""
+from django.db import migrations
+
+FIELD_DATA = [
+    {
+        "table": "Collection",
+        "field": "collectionObjectType",
+        "isrequired": False,
+    },
+    {
+        "table": "GeographyTreeDef",
+        "field": "discipline",
+        "isrequired": True,
+    },
+    {
+        "table": "GeologicTimePeriodTreeDef",
+        "field": "discipline",
+        "isrequired": True,
+    },
+    {
+        "table": "LithostratTreeDef",
+        "field": "discipline",
+        "isrequired": True,
+    },
+    {
+        "table": "StorageTreeDef",
+        "field": "institution",
+        "isrequired": True,
+    },
+    {
+        "table": "TaxonTreeDef",
+        "field": "discipline",
+        "isrequired": True,
+    },
+]
+
+PICKLIST_NAME = 'CollectionObjectGroupType'
+COGTYPE_FIELD_NAME = 'cogType'
+PICKLIST_TEXT = 'Collection Object Group Type'
+
+def add_fields(apps):
+    Splocalecontainer = apps.get_model('specify', 'Splocalecontainer')
+    Splocalecontaineritem = apps.get_model('specify', 'Splocalecontaineritem')
+    Splocaleitemstr = apps.get_model('specify', 'Splocaleitemstr')
+
+    for data in FIELD_DATA:
+        for container in Splocalecontainer.objects.filter(name=data['table'], schematype=0):
+            container_item, _ = Splocalecontaineritem.objects.get_or_create(
+                name=data["field"],
+                type='ManyToOne',
+                container=container,
+                isrequired=data["isrequired"]
+            )
+            Splocaleitemstr.objects.get_or_create(
+                language='en',
+                text=data["field"],
+                itemname=container_item
+            )
+            Splocaleitemstr.objects.get_or_create(
+                language='en',
+                text=data["field"],
+                itemdesc=container_item
+            )
+
+def remove_fields(apps):
+    Splocalecontaineritem = apps.get_model('specify', 'Splocalecontaineritem')
+    Splocaleitemstr = apps.get_model('specify', 'Splocaleitemstr')
+
+    for data in FIELD_DATA:
+        Splocaleitemstr.objects.filter(
+            text=data["field"].title(),
+            itemname__name=data["field"],
+            itemname__container__name=data["table"], 
+            itemname__container__schematype=0
+        ).delete()
+        Splocaleitemstr.objects.filter(
+            text=data["field"],
+            itemdesc__name=data["field"],
+            itemdesc__container__name=data["table"], 
+            itemdesc__container__schematype=0
+        ).delete()
+        Splocalecontaineritem.objects.filter(
+            name=data["field"],
+            container__name=data["table"],
+            container__schematype=0,
+        ).delete()
+        
+def create_cogtype_picklist(apps):
+    Collection = apps.get_model('specify', 'Collection')
+    Picklist = apps.get_model('specify', 'Picklist')
+    
+    # Create a cogtype picklist for each collection
+    for collection in Collection.objects.all():
+        Picklist.objects.get_or_create(
+            name=PICKLIST_NAME,
+            issystem=True,
+            readonly=True,
+            sizelimit=-1,
+            sorttype=1,
+            type=1,
+            tablename='collectionobjectgrouptype',
+            collection=collection,
+            formatter=PICKLIST_NAME
+        )
+
+def revert_cogtype_picklist(apps):
+    Picklist = apps.get_model('specify', 'Picklist')
+
+    Picklist.objects.filter(name=PICKLIST_NAME).delete()
+
+# Updates COG -> cogtype to use the type 1 picklist created above
+def update_cogtype_splocalecontaineritem(apps):
+    Splocalecontaineritem = apps.get_model('specify', 'Splocalecontaineritem')
+
+    Splocalecontaineritem.objects.filter(container__name='collectionobjectgroup', container__schematype=0, name=COGTYPE_FIELD_NAME).update(
+        picklistname=PICKLIST_NAME,
+        type='ManyToOne',
+        isrequired=True
+    )
+
+def revert_cogtype_splocalecontaineritem(apps):
+    Splocalecontaineritem = apps.get_model('specify', 'Splocalecontaineritem')
+
+    Splocalecontaineritem.objects.filter(container__name='collectionobjectgroup', container__schematype=0, name=COGTYPE_FIELD_NAME).update(
+        picklistname=None,
+        type=None,
+        isrequired=None
+    )
+
+# Updates cogtype -> type to use the Default COGType picklist (Drill Core, Discrete, Consolidated)
+def update_cogtype_type_splocalecontaineritem(apps):
+    Splocalecontaineritem = apps.get_model('specify', 'Splocalecontaineritem')
+
+    Splocalecontaineritem.objects.filter(container__name='collectionobjectgrouptype', container__schematype=0, name='type').update(
+        picklistname='Default Collection Object Group Types',
+        isrequired=True
+    )
+
+def revert_cogtype_type_splocalecontaineritem(apps):
+    Splocalecontaineritem = apps.get_model('specify', 'Splocalecontaineritem')
+
+    Splocalecontaineritem.objects.filter(container__name='collectionobjectgrouptype', container__schematype=0, name='type').update(
+        picklistname=None,
+        isrequired=None
+    )
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('specify', '0003_cotype_picklist'),
+    ]
+
+    def apply_migration(apps, schema_editor):
+        add_fields(apps)
+        create_cogtype_picklist(apps)
+        update_cogtype_splocalecontaineritem(apps)
+        update_cogtype_type_splocalecontaineritem(apps)
+
+    def revert_migration(apps, schema_editor):
+        remove_fields(apps)
+        revert_cogtype_picklist(apps)
+        revert_cogtype_splocalecontaineritem(apps)
+        revert_cogtype_type_splocalecontaineritem(apps)
+
+    operations = [
+        migrations.RunPython(apply_migration, revert_migration, atomic=True)
+    ]
