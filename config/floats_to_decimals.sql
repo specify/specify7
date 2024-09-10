075f0dd88bd8b11bcf2392d6cf3e333b5c4a6dc8
alec_dev <acwhite211@gmail.com>
2024-09-10 13:58:53 -0500

    add Specify6.8.03 config file to Specify7

---
+++ config/floats_to_decimals.sql
@@ -0,0 +1,293 @@
+alter table accession
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10);
+alter table attachmentimageattribute
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10);
+alter table borrow
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10);
+alter table collectingeventattribute
+      modify Number9 decimal(20, 10),
+      modify Number12 decimal(20, 10),
+      modify Number13 decimal(20, 10),
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10),
+      modify Number3 decimal(20, 10),
+      modify Number4 decimal(20, 10),
+      modify Number5 decimal(20, 10),
+      modify Number6 decimal(20, 10),
+      modify Number7 decimal(20, 10),
+      modify Number8 decimal(20, 10),
+      modify Number11 decimal(20, 10),
+      modify Number10 decimal(20, 10);
+alter table collectingtripattribute
+      modify Number9 decimal(20, 10),
+      modify Number12 decimal(20, 10),
+      modify Number13 decimal(20, 10),
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10),
+      modify Number3 decimal(20, 10),
+      modify Number4 decimal(20, 10),
+      modify Number5 decimal(20, 10),
+      modify Number6 decimal(20, 10),
+      modify Number7 decimal(20, 10),
+      modify Number8 decimal(20, 10),
+      modify Number11 decimal(20, 10),
+      modify Number10 decimal(20, 10);
+alter table collectionobject
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10);
+alter table collectionobjectattribute
+      modify Number42 decimal(20, 10),
+      modify Number38 decimal(20, 10),
+      modify Number39 decimal(20, 10),
+      modify Number40 decimal(20, 10),
+      modify Number1 decimal(20, 10),
+      modify Number10 decimal(20, 10),
+      modify Number11 decimal(20, 10),
+      modify Number12 decimal(20, 10),
+      modify Number13 decimal(20, 10),
+      modify Number14 decimal(20, 10),
+      modify Number15 decimal(20, 10),
+      modify Number16 decimal(20, 10),
+      modify Number17 decimal(20, 10),
+      modify Number18 decimal(20, 10),
+      modify Number19 decimal(20, 10),
+      modify Number2 decimal(20, 10),
+      modify Number20 decimal(20, 10),
+      modify Number21 decimal(20, 10),
+      modify Number22 decimal(20, 10),
+      modify Number23 decimal(20, 10),
+      modify Number24 decimal(20, 10),
+      modify Number25 decimal(20, 10),
+      modify Number26 decimal(20, 10),
+      modify Number27 decimal(20, 10),
+      modify Number28 decimal(20, 10),
+      modify Number29 decimal(20, 10),
+      modify Number3 decimal(20, 10),
+      modify Number31 decimal(20, 10),
+      modify Number32 decimal(20, 10),
+      modify Number33 decimal(20, 10),
+      modify Number34 decimal(20, 10),
+      modify Number35 decimal(20, 10),
+      modify Number36 decimal(20, 10),
+      modify Number4 decimal(20, 10),
+      modify Number5 decimal(20, 10),
+      modify Number6 decimal(20, 10),
+      modify Number7 decimal(20, 10),
+      modify Number9 decimal(20, 10),
+      modify Number37 decimal(20, 10),
+      modify Number41 decimal(20, 10),
+      modify TopDistance decimal(20, 10),
+      modify BottomDistance decimal(20, 10);
+alter table collectionobjectproperty
+      modify Number21 decimal(20, 10),
+      modify Number22 decimal(20, 10),
+      modify Number23 decimal(20, 10),
+      modify Number24 decimal(20, 10),
+      modify Number25 decimal(20, 10),
+      modify Number26 decimal(20, 10),
+      modify Number27 decimal(20, 10),
+      modify Number28 decimal(20, 10),
+      modify Number29 decimal(20, 10),
+      modify Number30 decimal(20, 10),
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10),
+      modify Number3 decimal(20, 10),
+      modify Number4 decimal(20, 10),
+      modify Number5 decimal(20, 10),
+      modify Number6 decimal(20, 10),
+      modify Number7 decimal(20, 10),
+      modify Number8 decimal(20, 10),
+      modify Number9 decimal(20, 10),
+      modify Number10 decimal(20, 10),
+      modify Number11 decimal(20, 10),
+      modify Number12 decimal(20, 10),
+      modify Number13 decimal(20, 10),
+      modify Number14 decimal(20, 10),
+      modify Number15 decimal(20, 10),
+      modify Number16 decimal(20, 10),
+      modify Number17 decimal(20, 10),
+      modify Number18 decimal(20, 10),
+      modify Number19 decimal(20, 10),
+      modify Number20 decimal(20, 10);
+alter table commonnametxcitation
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10);
+alter table conservdescription
+      modify Width decimal(20, 10),
+      modify Height decimal(20, 10),
+      modify ObjLength decimal(20, 10);
+alter table dnaprimer
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10),
+      modify ReservedNumber3 decimal(20, 10),
+      modify ReservedNumber4 decimal(20, 10);
+alter table dnasequence
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10),
+      modify Number3 decimal(20, 10);
+alter table dnasequencingrun
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10),
+      modify Number3 decimal(20, 10);
+alter table dnasequencingruncitation
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10);
+alter table deaccession
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10),
+      modify Number3 decimal(20, 10),
+      modify Number4 decimal(20, 10),
+      modify Number5 decimal(20, 10);
+alter table determination
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10),
+      modify Number3 decimal(20, 10),
+      modify Number4 decimal(20, 10),
+      modify Number5 decimal(20, 10);
+alter table disposal
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10);
+alter table exchangein
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10);
+alter table exchangeout
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10);
+alter table geocoorddetail
+      modify GeoRefAccuracy decimal(20, 10);
+alter table geologictimeperiod
+      modify EndPeriod decimal(20, 10),
+      modify EndUncertainty decimal(20, 10),
+      modify StartPeriod decimal(20, 10),
+      modify StartUncertainty decimal(20, 10);
+alter table gift
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10);
+alter table lithostrat
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10);
+alter table loan
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10);
+alter table locality
+      modify MinElevation decimal(20, 10),
+      modify MaxElevation decimal(20, 10),
+      modify ElevationAccuracy decimal(20, 10),
+      modify LatLongAccuracy decimal(20, 10);
+alter table localitydetail
+      modify StartDepth decimal(20, 10),
+      modify EndDepth decimal(20, 10),
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10),
+      modify Number3 decimal(20, 10),
+      modify Number4 decimal(20, 10),
+      modify Number5 decimal(20, 10);
+alter table materialsample
+      modify GGBNConcentration decimal(20, 10),
+      modify GGBNAbsorbanceRatio260_230 decimal(20, 10),
+      modify GGBNAbsorbanceRatio260_280 decimal(20, 10),
+      modify GGBNVolume decimal(20, 10),
+      modify GGBNWeight decimal(20, 10),
+      modify GGBNSampleSize decimal(20, 10),
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10),
+      modify ReservedNumber3 decimal(20, 10),
+      modify ReservedNumber4 decimal(20, 10);
+alter table paleocontext
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10),
+      modify Number3 decimal(20, 10),
+      modify Number4 decimal(20, 10),
+      modify Number5 decimal(20, 10);
+alter table permit
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10);
+alter table preparation
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10);
+alter table preparationattribute
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10),
+      modify Number3 decimal(20, 10);
+alter table preparationproperty
+      modify Number21 decimal(20, 10),
+      modify Number22 decimal(20, 10),
+      modify Number23 decimal(20, 10),
+      modify Number24 decimal(20, 10),
+      modify Number25 decimal(20, 10),
+      modify Number26 decimal(20, 10),
+      modify Number27 decimal(20, 10),
+      modify Number28 decimal(20, 10),
+      modify Number29 decimal(20, 10),
+      modify Number30 decimal(20, 10),
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10),
+      modify Number3 decimal(20, 10),
+      modify Number4 decimal(20, 10),
+      modify Number5 decimal(20, 10),
+      modify Number6 decimal(20, 10),
+      modify Number7 decimal(20, 10),
+      modify Number8 decimal(20, 10),
+      modify Number9 decimal(20, 10),
+      modify Number10 decimal(20, 10),
+      modify Number11 decimal(20, 10),
+      modify Number12 decimal(20, 10),
+      modify Number13 decimal(20, 10),
+      modify Number14 decimal(20, 10),
+      modify Number15 decimal(20, 10),
+      modify Number16 decimal(20, 10),
+      modify Number17 decimal(20, 10),
+      modify Number18 decimal(20, 10),
+      modify Number19 decimal(20, 10),
+      modify Number20 decimal(20, 10);
+alter table project
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10);
+alter table referencework
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10);
+alter table repositoryagreement
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10);
+alter table shipment
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10);
+alter table taxon
+      modify Number3 decimal(20, 10),
+      modify Number4 decimal(20, 10),
+      modify Number5 decimal(20, 10);
+alter table taxonattribute
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10),
+      modify Number3 decimal(20, 10),
+      modify Number4 decimal(20, 10),
+      modify Number5 decimal(20, 10),
+      modify Number6 decimal(20, 10),
+      modify Number7 decimal(20, 10),
+      modify Number8 decimal(20, 10),
+      modify Number9 decimal(20, 10),
+      modify Number10 decimal(20, 10),
+      modify Number11 decimal(20, 10),
+      modify Number12 decimal(20, 10),
+      modify Number13 decimal(20, 10),
+      modify Number14 decimal(20, 10),
+      modify Number15 decimal(20, 10),
+      modify Number16 decimal(20, 10),
+      modify Number17 decimal(20, 10),
+      modify Number18 decimal(20, 10),
+      modify Number19 decimal(20, 10),
+      modify Number20 decimal(20, 10);
+alter table taxoncitation
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10);
+alter table treatmentevent
+      modify Number3 decimal(20, 10),
+      modify Number4 decimal(20, 10),
+      modify Number5 decimal(20, 10);
+alter table voucherrelationship
+      modify Number1 decimal(20, 10),
+      modify Number2 decimal(20, 10),
+      modify Number3 decimal(20, 10);
