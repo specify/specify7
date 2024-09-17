b101753ac2ebffeec3bb3c9066444f7450fda799
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2024-09-17 09:54:51 -0700

    Add useEffect to create newResource

--- specifyweb/frontend/js_src/lib/components/FormCells/COJODialog.tsx
+++ specifyweb/frontend/js_src/lib/components/FormCells/COJODialog.tsx
@@ -5,16 +5,49 @@ import { commonText } from '../../localization/common';
 import { formsText } from '../../localization/forms';
 import { localized } from '../../utils/types';
 import { DataEntry } from '../Atoms/DataEntry';
+import type { AnySchema } from '../DataModel/helperTypes';
+import type { SpecifyResource } from '../DataModel/legacyTypes';
+import type { SpecifyTable } from '../DataModel/specifyTable';
 import { tables } from '../DataModel/tables';
+import type {
+  CollectionObject,
+  CollectionObjectGroup,
+} from '../DataModel/types';
+import { ResourceView } from '../Forms/ResourceView';
 import { Dialog } from '../Molecules/Dialog';
 import { TableIcon } from '../Molecules/TableIcon';
 
-export function COJODialog(): JSX.Element | null {
+export function COJODialog({
+  parentResource,
+}: {
+  readonly parentResource: SpecifyResource<CollectionObjectGroup> | undefined;
+}): JSX.Element | null {
   const [isOpen, handleOpen, handleClose] = useBooleanState();
   const COJOChildrentables = [
     tables.CollectionObject,
     tables.CollectionObjectGroup,
   ];
+  const [state, setState] = React.useState<'Add' | 'Search' | undefined>(
+    undefined
+  );
+  const [resource, setResource] = React.useState<
+    | SpecifyTable<CollectionObject>
+    | SpecifyTable<CollectionObjectGroup>
+    | undefined
+  >(undefined);
+  const [newResource, setNewResource] = React.useState<
+    | SpecifyResource<CollectionObject>
+    | SpecifyResource<CollectionObjectGroup>
+    | undefined
+  >(undefined);
+
+  React.useEffect(() => {
+    if (resource !== undefined) {
+      const createdResource = new resource.Resource();
+      setNewResource(createdResource);
+      setState('Add');
+    }
+  }, [resource]);
   return (
     <>
       <DataEntry.Add onClick={handleOpen} />
@@ -30,13 +63,37 @@ export function COJODialog(): JSX.Element | null {
               <div className="flex items-center gap-2" key={table.name}>
                 <TableIcon label name={table.name} />
                 {localized(table.label)}
-                <DataEntry.Add onClick={undefined} />
+                <DataEntry.Add
+                  onClick={(): void => {
+                    setResource(table);
+                  }}
+                />
                 <DataEntry.Search aria-pressed="true" onClick={undefined} />
               </div>
             ))}
           </div>
         </Dialog>
       )}
+      {state === 'Add' && newResource !== undefined ? (
+        <ResourceView
+          dialog="nonModal"
+          isDependent={false}
+          isSubForm={false}
+          resource={newResource as SpecifyResource<CollectionObject>}
+          onAdd={undefined}
+          onClose={(): void => {
+            setState(undefined);
+            handleClose();
+          }}
+          onDeleted={undefined}
+          onSaved={(): void => {
+            parentResource?.set('cojo', newResource as never);
+            setState(undefined);
+            handleClose();
+          }}
+          onSaving={undefined}
+        />
+      ) : undefined}
     </>
   );
 }
