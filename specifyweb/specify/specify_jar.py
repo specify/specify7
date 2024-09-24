62d01d1f3748fc425e4707ad73b6eeeef187f345
alec_dev <acwhite211@gmail.com>
2024-09-24 16:59:51 -0500

    DEFAULT_SPECIFY_JAR_PATH

--- specifyweb/specify/specify_jar.py
+++ specifyweb/specify/specify_jar.py
@@ -3,4 +3,10 @@ from zipfile import ZipFile
 
 from django.conf import settings
 
-specify_jar = ZipFile(os.path.join(settings.SPECIFY_THICK_CLIENT, 'config/specify.jar'))
+DEFAULT_SPECIFY_JAR_PATH = '/opt/specify7/config/specify.jar'
+
+try:
+    specify_jar_path = os.path.join(settings.SPECIFY_THICK_CLIENT, 'config/specify.jar')
+    specify_jar = ZipFile(specify_jar_path)
+except (FileNotFoundError, KeyError):
+    specify_jar = ZipFile(DEFAULT_SPECIFY_JAR_PATH)
