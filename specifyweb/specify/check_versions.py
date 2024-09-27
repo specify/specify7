36b3ccaafb8586688620021f886851440a218d55
alec_dev <acwhite211@gmail.com>
2024-09-27 15:28:12 -0500

    Revert "remove specify jar dependency"
    This reverts commit c6fed09a7e0d0f7369831993238d01e4c78ea4f7.

--- specifyweb/specify/check_versions.py
+++ specifyweb/specify/check_versions.py
@@ -4,12 +4,12 @@ from xml.etree import ElementTree
 import os
 import re
 
-from .specify_jar import specify_jar_resources_en_properties
+from .specify_jar import specify_jar
 
 def check_versions(Spversion):
     """Check schema and application version compatibility."""
     SPECIFY_VERSION = re.findall(r'SPECIFY_VERSION=(.*)',
-                                 specify_jar_resources_en_properties)[0]
+                                 specify_jar.read('resources_en.properties'))[0]
 
     SCHEMA_VERSION = ElementTree.parse(os.path.join(settings.SPECIFY_CONFIG_DIR, 'schema_version.xml')).getroot().text
 
--- specifyweb/specify/specify_jar.py
+++ specifyweb/specify/specify_jar.py
@@ -1,18 +1,12 @@
 import os
+from zipfile import ZipFile
+
 from django.conf import settings
 
-DEFAULT_SPECIFY_JAR_PATH = '/opt/specify7/config/specify_jar'
+DEFAULT_SPECIFY_JAR_PATH = '/opt/specify7/config/specify.jar'
 
-# Determine the path to the specify_jar directory
-specify_jar_dir_path = os.path.join(settings.SPECIFY_THICK_CLIENT, 'config/specify_jar')
-
-# If specify_jar_dir_path does not exist, then use DEFAULT_SPECIFY_JAR_PATH
-if not os.path.exists(specify_jar_dir_path):
-    specify_jar_dir_path = DEFAULT_SPECIFY_JAR_PATH
-
-# Path to the resources_en.properties file
-resources_en_properties_path = os.path.join(specify_jar_dir_path, 'resources_en.properties')
-
-# Read the file into a string
-with open(resources_en_properties_path, 'r', encoding='utf-8') as file:
-    specify_jar_resources_en_properties = file.read()
+try:
+    specify_jar_path = os.path.join(settings.SPECIFY_THICK_CLIENT, 'config/specify.jar')
+    specify_jar = ZipFile(specify_jar_path)
+except (FileNotFoundError, KeyError):
+    specify_jar = ZipFile(DEFAULT_SPECIFY_JAR_PATH)
