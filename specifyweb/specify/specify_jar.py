f5c472fabcc1550941bdfc9275cedbb8ff4445bf
alec_dev <acwhite211@gmail.com>
2024-09-27 15:31:12 -0500

    let GitHub search for specify.jar 🤷

--- specifyweb/specify/specify_jar.py
+++ specifyweb/specify/specify_jar.py
@@ -5,8 +5,19 @@ from django.conf import settings
 
 DEFAULT_SPECIFY_JAR_PATH = '/opt/specify7/config/specify.jar'
 
+def find_jar(filename='specify.jar', search_dir='/'):
+    for root, dirs, files in os.walk(search_dir):
+        if filename in files:
+            return os.path.join(root, filename)
+    return None
+
 try:
     specify_jar_path = os.path.join(settings.SPECIFY_THICK_CLIENT, 'config/specify.jar')
     specify_jar = ZipFile(specify_jar_path)
 except (FileNotFoundError, KeyError):
-    specify_jar = ZipFile(DEFAULT_SPECIFY_JAR_PATH)
+    found_jar_path = find_jar()
+    if found_jar_path:
+        specify_jar = ZipFile(found_jar_path)
+    else:
+        # Fall back to default path if the file is not found
+        specify_jar = ZipFile(DEFAULT_SPECIFY_JAR_PATH)
