a650427b64dc58f990a8644b44b6317cb0f2d4a3
Sharad S <16229739+sharadsw@users.noreply.github.com>
2024-10-02 16:46:27 -0400

    Revert "Hide CollectionObjectType on the frontend"

--- specifyweb/frontend/js_src/lib/components/Toolbar/QueryTablesEdit.tsx
+++ specifyweb/frontend/js_src/lib/components/Toolbar/QueryTablesEdit.tsx
@@ -37,12 +37,8 @@ export function QueryTablesEdit({
     />
   );
 }
-/*
- * TODO: temp fix, remove this, use to hide geo tables for COG until 9.8 release
- * TODO: Revert #5236 to unhide COType
- */
+// TODO: temp fix, remove this, use to hide geo tables for COG until 9.8 release
 export const HIDDEN_GEO_TABLES = new Set([
-  'CollectionObjectType',
   'CollectionObjectGroup',
   'CollectionObjectGroupJoin',
   'CollectionObjectGroupType',
