eea7c34e1da57d2cdebcf4766305be0cb6414bd8
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2024-10-08 09:54:45 -0700

    Add tectonic default ranks
    Fixes #5294

---
+++ specifyweb/specify/migrations/0006_tectonic_ranks.py
@@ -0,0 +1,79 @@
+from django.apps import apps as specify_apps
+from django.db import migrations, models
+from specifyweb.specify.models import protect_with_blockers
+
+
+def create_default_tectonic_ranks(apps): 
+    TectonicUnit = apps.get_model('specify', 'TectonicUnitTreeDefItem')
+    TectonicTreeDef = apps.get_model('specify', 'TectonicUnitTreeDef')
+    Discipline = apps.get_model('specify', 'Discipline')
+    for discipline in Discipline.objects.all():
+        tectonic_tree_def = TectonicTreeDef.objects.create(name="Tectonic Unit", discipline=discipline)
+
+        root = TectonicUnit.objects.create(
+            name="Root",
+            title="Root",
+            rankid=0,
+            parentitem=None,
+            tectonicunittreedef=tectonic_tree_def,
+        )
+        superstructure = TectonicUnit.objects.create(
+            name="Superstructure",
+            title="Superstructure",
+            rankid=10,
+            parentitem=root,
+            tectonicunittreedef=tectonic_tree_def,
+        )
+        tectonic_domain = TectonicUnit.objects.create(
+            name="Tectonic Domain",
+            title="Tectonic Domain",
+            rankid=20,
+            parentitem=superstructure,
+            tectonicunittreedef=tectonic_tree_def,
+        )
+        tectonic_subdomain = TectonicUnit.objects.create(
+            name="Tectonic Subdomain",
+            title="Tectonic Subdomain",
+            rankid=30,
+            parentitem=tectonic_domain,
+            tectonicunittreedef=tectonic_tree_def,
+        )
+        tectonic_unit = TectonicUnit.objects.create(
+            name="Tectonic Unit",
+            title="Tectonic Unit",
+            rankid=40,
+            parentitem=tectonic_subdomain,
+            tectonicunittreedef=tectonic_tree_def,
+        )
+        tectonic_subunit = TectonicUnit.objects.create(
+            name="Tectonic Subunit",
+            title="Tectonic Subunit",
+            rankid=50,
+            parentitem=tectonic_unit,
+            tectonicunittreedef=tectonic_tree_def,
+        )
+
+def revert_default_tectonic_ranks(apps, schema_editor):
+    TectonicUnit = apps.get_model('specify', 'TectonicUnit')
+    TectonicTreeDef = apps.get_model('specify', 'TectonicUnitTreeDef')
+    Discipline = apps.get_model('specify', 'Discipline')
+    for discipline in Discipline.objects.all():
+        tectonic_tree_def = TectonicTreeDef.objects.filter(name="Tectonic Unit", discipline=discipline)
+        if tectonic_tree_def:
+            tectonic_tree_def.delete()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('specify', '0005_collectionobjectgroup_parentcojo'),
+    ]
+
+    def consolidated_python_django_migration_operations(apps, schema_editor):
+        create_default_tectonic_ranks(apps)
+
+    def revert_cosolidated_python_django_migration_operations(apps, schema_editor):
+        revert_default_tectonic_ranks(apps, schema_editor)
+    
+    operations = [
+                migrations.RunPython(consolidated_python_django_migration_operations, revert_cosolidated_python_django_migration_operations, atomic=True),
+    ]
