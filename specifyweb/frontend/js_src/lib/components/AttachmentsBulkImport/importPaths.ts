bf6fdcbb93b7df4db9d1e3dd95f54525c3e2431d
Grant Fitzsimmons <37256050+grantfitzsimmons@users.noreply.github.com>
2024-10-21 09:24:08 -0500

    Extend support for bulk attachment tables
    Adds support for uploading bulk attachments for the following tables
    * Storage (Full Name)
    * Preparation (Barcode, GUID)
    * Treatment Event (FieldNumber)
    * Exchange In (ExchangeInNumber)
    * Exchange Out (ExchangeOutNumber)
    * Reference Work (Title, GUID)
    * Locality (UniqueIdentifier, GUID)
    * Permit (PermitNumber)
    * Agent (GUID)
    * Collecting Trip (CollectingTripName)
    * Deaccession (DeaccessionNumber)

--- specifyweb/frontend/js_src/lib/components/AttachmentsBulkImport/importPaths.ts
+++ specifyweb/frontend/js_src/lib/components/AttachmentsBulkImport/importPaths.ts
@@ -19,6 +19,7 @@ export const staticAttachmentImportPaths: IR<ReturnType<typeof createItem>> = {
     'altCatalogNumber'
   ),
   taxonFullName: createItem('Taxon', 'fullName'),
+  storageFullName: createItem('Storage', 'fullName'),
   collectingEventGuid: createItem('CollectingEvent', 'guid'),
   collectingEventFieldnumber: createItem(
     'CollectingEvent',
@@ -28,5 +29,17 @@ export const staticAttachmentImportPaths: IR<ReturnType<typeof createItem>> = {
   accessionNumber: createItem('Accession', 'accessionNumber'),
   giftNumber: createItem('Gift', 'giftNumber'),
   preparationBarcode: createItem('Preparation', 'barCode'),
+  preparationGUID: createItem('Preparation', 'guid'),
   borrowInvoiceNumber: createItem('Borrow', 'invoiceNumber'),
+  treatmentEventFieldNumber: createItem('TreatmentEvent', 'fieldNumber'),
+  exchangeInAttachment: createItem('ExchangeIn', 'exchangeInNumber'),
+  exchangeOutAttachment: createItem('ExchangeOut', 'exchangeOutNumber'),
+  referenceWorkGUID: createItem('ReferenceWork', 'guid'),
+  referenceWorkTitle: createItem('ReferenceWork', 'title'),
+  localityUniqueIdentifier: createItem('Locality', 'uniqueIdentifier'),
+  localityGUID: createItem('Locality', 'guid'),
+  permitNumber: createItem('Permit', 'permitNumber'),
+  agentGUID: createItem('Agent', 'guid'),
+  collectingTripName: createItem('CollectingTrip', 'collectingTripName'),
+  deaccessionNumber: createItem('Deaccession', 'deaccessionNumber'),
 };
