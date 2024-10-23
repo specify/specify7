48376e5124fa6d84a2f844490b9fa4443a5f05b0
alec_dev <acwhite211@gmail.com>
2024-10-23 13:36:38 -0500

    new find_jar

--- specifyweb/specify/specify_jar.py
+++ specifyweb/specify/specify_jar.py
@@ -1,22 +1,43 @@
 import os
-from zipfile import ZipFile
+from zipfile import ZipFile, BadZipFile
 
 from django.conf import settings
 
-DEFAULT_SPECIFY_JAR_PATH = '/opt/Specify/config/specify.jar'
+DEFAULT_SPECIFY_JAR_PATHS = [
+    '/opt/Specify/config/specify.jar',
+    '/opt/specify7/config/specify.jar'
+]
 
-def find_jar(filename='specify.jar', search_dir='/'):
-    for root, dirs, files in os.walk(search_dir):
-        if filename in files:
-            return os.path.join(root, filename)
+def find_jar(filename='specify.jar', search_dirs=['/']):
+    for search_dir in search_dirs:
+        for root, dirs, files in os.walk(search_dir):
+            if filename in files:
+                return os.path.join(root, filename)
     return None
 
-try:
-    specify_jar_path = os.path.join(settings.SPECIFY_THICK_CLIENT, 'config/specify.jar')
-    specify_jar = ZipFile(specify_jar_path)
-except (FileNotFoundError, KeyError):
-    found_jar_path = find_jar()
-    if found_jar_path:
-        specify_jar = ZipFile(found_jar_path)
-    else:
-        specify_jar = ZipFile(DEFAULT_SPECIFY_JAR_PATH)
+def open_jar(paths):
+    for path in paths:
+        try:
+            return ZipFile(path)
+        except (FileNotFoundError, KeyError, BadZipFile):
+            continue
+    return None
+
+# Main logic
+specify_jar_paths = [
+    os.path.join(settings.SPECIFY_THICK_CLIENT, 'config/specify.jar')
+]
+
+# Add found jar path if any
+found_jar_path = find_jar()
+if found_jar_path:
+    specify_jar_paths.append(found_jar_path)
+
+# Add default paths
+specify_jar_paths.extend(DEFAULT_SPECIFY_JAR_PATHS)
+
+# Open the jar file
+specify_jar = open_jar(specify_jar_paths)
+
+if specify_jar is None:
+    raise FileNotFoundError("Specify JAR file not found in any of the specified paths.")
