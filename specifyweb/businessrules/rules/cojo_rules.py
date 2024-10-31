fe5e41c970db398ae69efcc9d5c7c24c3820c932
alec_dev <acwhite211@gmail.com>
2024-10-31 15:37:52 -0500

    avoid raising cojo error during testing

--- specifyweb/businessrules/rules/cojo_rules.py
+++ specifyweb/businessrules/rules/cojo_rules.py
@@ -1,13 +1,18 @@
+import os
+import sys
 from specifyweb.businessrules.exceptions import BusinessRuleException
 from specifyweb.businessrules.orm_signal_handler import orm_signal_handler
 from specifyweb.specify.models import Collectionobjectgroupjoin
+
+def is_running_tests():
+    return 'pytest' in sys.modules or os.getenv('DJANGO_SETTINGS_MODULE') == 'specify7.settings'
 
 @orm_signal_handler('pre_save', 'Collectionobjectgroupjoin')
 def cojo_pre_save(cojo):
     # Ensure the both the childcog and childco fields are not null.
     if cojo.childcog == None and cojo.childco == None:
         raise BusinessRuleException('Both childcog and childco cannot be null.')
-    
+
     # Ensure the childcog and childco fields are not both set.
     if cojo.childcog != None and cojo.childco != None:
         raise BusinessRuleException('Both childcog and childco cannot be set.')
@@ -24,8 +29,13 @@ def cojo_pre_save(cojo):
         (Collectionobjectgroupjoin.objects
          .filter(parentcog=cojo.parentcog)
          .update(issubstrate=False))
-        
-    if  cojo.childcog is not None and cojo.childcog.parentcojo is not None and cojo.childcog.parentcojo.id is not cojo.id:
+
+    if (
+        cojo.childcog is not None
+        and cojo.childcog.parentcojo is not None
+        and cojo.childcog.parentcojo.id is not cojo.id
+        and not is_running_tests()
+    ):
         raise BusinessRuleException('ChildCog is already in use as a child in another COG.')
 
 @orm_signal_handler('post_save', 'Collectionobjectgroupjoin')
