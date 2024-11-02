e105d1294bec733b9897e13ea099c35f0e47d033
Grant Fitzsimmons <37256050+grantfitzsimmons@users.noreply.github.com>
2024-11-01 19:39:57 -0500

    Update migration order

--- specifyweb/specify/migrations/0012_fix_accepted_taxon.py
+++ specifyweb/specify/migrations/0012_fix_accepted_taxon.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('specify', '0010_updateDelete_parentcojo'),
+        ('specify', '0011_cascading_tree_nodes'),
     ]
 
     operations = [
