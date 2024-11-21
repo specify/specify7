1db84c1c488f2e9bfba3be0ec0b1febb6d517270
melton-jason <meltonjason12@yahoo.com>
2024-11-21 15:04:07 -0600

    Add migration to handle COT catnum format name in schema config

--- specifyweb/specify/migrations/0015_collectionobjecttype_catalognumformatname.py
+++ specifyweb/specify/migrations/0015_collectionobjecttype_catalognumformatname.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('specify', '0013_collectionobjectgroup_parentcog'),
+        ('specify', '0014_spqueryfield_strict'),
     ]
 
     operations = [
