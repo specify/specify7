b9312cb57ecb65189f129726b0a9e7d6058699d6
melton-jason <meltonjason12@yahoo.com>
2024-11-27 09:21:49 -0600

    Remove comment

--- specifyweb/frontend/js_src/lib/tests/jsdom.js
+++ specifyweb/frontend/js_src/lib/tests/jsdom.js
@@ -1,12 +1,6 @@
 import { JSDOM } from 'jsdom';
 
 // Create a new JSDOM instance
-/**
- * FEATURE: Allow customizing the JSDOM userAgent, platform, and other global
- * properties
- * See https://github.com/jsdom/jsdom#advanced-configuration
- * See https://github.com/specify/specify7/pull/5389
- */
 const { window } = new JSDOM('<!DOCTYPE html><p>Hello world</p>');
 
 // Define window and other globals
