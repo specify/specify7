9d42144f88e798f358f930637106d96341ec8773
Grant Fitzsimmons <37256050+grantfitzsimmons@users.noreply.github.com>
2025-01-03 15:16:53 +0000

    Lint code with ESLint and Prettier
    Triggered by 33920fcac897e54bd4d0f680daa799434c09b6b0 on branch refs/heads/issue-6030

--- specifyweb/frontend/js_src/lib/components/FormCells/FormTable.tsx
+++ specifyweb/frontend/js_src/lib/components/FormCells/FormTable.tsx
@@ -237,7 +237,11 @@ export function FormTable<SCHEMA extends AnySchema>({
       <p>{formsText.noData()}</p>
     ) : (
       <div
-        className={isCollapsed ? 'hidden' : 'overflow-x-auto border border-gray-500 border-t-0 rounded-b p-1'}
+        className={
+          isCollapsed
+            ? 'hidden'
+            : 'overflow-x-auto border border-gray-500 border-t-0 rounded-b p-1'
+        }
         onScroll={handleScroll}
       >
         <DataEntry.Grid
