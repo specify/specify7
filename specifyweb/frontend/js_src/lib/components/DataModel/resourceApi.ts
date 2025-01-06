c7224963f9b3dd8dbaf4d1e61e55e2940baa258f
melton-jason <meltonjason12@yahoo.com>
2025-01-06 16:52:59 +0000

    Lint code with ESLint and Prettier
    Triggered by f357cd3f57985510f2a14ecceaad7edef6075476 on branch refs/heads/issue-5508

--- specifyweb/frontend/js_src/lib/components/DataModel/resourceApi.ts
+++ specifyweb/frontend/js_src/lib/components/DataModel/resourceApi.ts
@@ -896,7 +896,7 @@ export const ResourceBase = Backbone.Model.extend({
     });
 
     // Check added to avoid infinite loop in following forEach for collectionRelationship see https://github.com/specify/specify7/issues/6025
-    if (self.specifyTable.name === 'CollectionRelationship') return json
+    if (self.specifyTable.name === 'CollectionRelationship') return json;
 
     Object.entries(self.independentResources).forEach(
       ([fieldName, related]) => {
