3f845e5edc28a4c9ebf68e7557763ec4d2771ec9
Grant Fitzsimmons <37256050+grantfitzsimmons@users.noreply.github.com>
2025-01-08 07:48:45 -0600

    Update preferences.ts

--- specifyweb/frontend/js_src/lib/localization/preferences.ts
+++ specifyweb/frontend/js_src/lib/localization/preferences.ts
@@ -1413,7 +1413,7 @@ export const preferencesText = createDictionary({
     `,
   },
   sortByField: {
-    'en-us': 'Order by field',
+    'en-us': 'Order By Field',
   },
   lineWrap: {
     'en-us': 'Line wrap',
@@ -1495,7 +1495,7 @@ export const preferencesText = createDictionary({
     'ru-ru': 'Поиск с учетом регистра',
   },
   searchField: {
-    'en-us': 'Search field',
+    'en-us': 'Search Field',
     'ru-ru': 'Поле поиска',
     'es-es': 'Campo de búsqueda',
     'fr-fr': 'Champ de recherche',
