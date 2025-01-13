15054394a55978bc45d47fa7741890e92eee95b0
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2025-01-13 13:37:36 -0800

    Comment out previous migration

--- specifyweb/specify/migrations/0019_remove_parentCog.py
+++ specifyweb/specify/migrations/0019_remove_parentCog.py
@@ -10,8 +10,8 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='collectionobjectgroup',
-            name='parentcog',
-        ),
+        # migrations.RemoveField(
+        #     model_name='collectionobjectgroup',
+        #     name='parentcog',
+        # ),
     ]
