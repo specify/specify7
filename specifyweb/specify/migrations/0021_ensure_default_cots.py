7639c7f0eb4123a5b859e4636f7ab5798dbb943e
alec_dev <acwhite211@gmail.com>
2025-01-14 15:42:34 -0600

    fix schema_editor

--- specifyweb/specify/migrations/0021_ensure_default_cots.py
+++ specifyweb/specify/migrations/0021_ensure_default_cots.py
@@ -3,7 +3,7 @@ from specifyweb.specify.utils import create_default_collection_types
 
 class Migration(migrations.Migration):
 
-    def handle_default_collection_types(apps):
+    def handle_default_collection_types(apps, schema_editor):
         create_default_collection_types(apps)
 
     dependencies = [
