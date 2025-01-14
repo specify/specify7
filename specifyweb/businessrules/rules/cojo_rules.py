634b9986ae27048b1fdd2f3b7c1755793eb1f72e
melton-jason <meltonjason12@yahoo.com>
2025-01-14 02:40:17 -0600

    Add cojo businessrule to automatically set precedence

--- specifyweb/businessrules/rules/cojo_rules.py
+++ specifyweb/businessrules/rules/cojo_rules.py
@@ -1,18 +1,17 @@
-import os
-import sys
 from enum import Enum
+
+from django.db.models import Max
 
 from specifyweb.businessrules.exceptions import BusinessRuleException
 from specifyweb.businessrules.orm_signal_handler import orm_signal_handler
 from specifyweb.specify.models import Collectionobjectgroupjoin
+
 
 class COGType(Enum):
     DISCRETE = "Discrete"
     CONSOLIDATED = "Consolidated"
     DRILL_CORE = "Drill Core"
 
-def is_running_tests():
-    return any(module in sys.modules for module in ('pytest', 'unittest'))
 
 @orm_signal_handler('pre_save', 'Collectionobjectgroupjoin')
 def cojo_pre_save(cojo):
@@ -41,24 +40,33 @@ def cojo_pre_save(cojo):
         cojo.childcog is not None
         and cojo.childcog.cojo is not None
         and cojo.childcog.cojo.id is not cojo.id
-        and not is_running_tests()
     ):
-        raise BusinessRuleException('ChildCog is already in use as a child in another COG.')
+        raise BusinessRuleException(
+            'ChildCog is already in use as a child in another COG.')
 
     if (
         cojo.childco is not None
         and cojo.childco.cojo is not None
         and cojo.childco.cojo.id is not cojo.id
-        and not is_running_tests()
     ):
-        raise BusinessRuleException('ChildCo is already in use as a child in another COG.')
-    
+        raise BusinessRuleException(
+            'ChildCo is already in use as a child in another COG.')
+
+    if cojo.precedence is None:
+        others = Collectionobjectgroupjoin.objects.filter(
+            parentcog=cojo.parentcog
+        )
+        top = others.aggregate(Max('precedence'))['precedence__max']
+        cojo.precedence = 0 if top is None else top + 1
+
+
 @orm_signal_handler('post_save', 'Collectionobjectgroupjoin')
 def cojo_post_save(cojo):
     """
         For Consolidated COGs, mark the first CO child as primary if none have been set by the user
     """
-    co_children = Collectionobjectgroupjoin.objects.filter(parentcog=cojo.parentcog, childco__isnull=False)
+    co_children = Collectionobjectgroupjoin.objects.filter(
+        parentcog=cojo.parentcog, childco__isnull=False)
     if len(co_children) > 0 and not co_children.filter(isprimary=True).exists() and cojo.parentcog.cogtype.type == COGType.CONSOLIDATED.value:
         first_child = co_children.first()
         first_child.isprimary = True
