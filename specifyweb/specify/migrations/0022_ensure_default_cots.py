d01032c0eef2abb46604e52903ab54f5ceb266db
alec_dev <acwhite211@gmail.com>
2025-01-16 10:14:59 -0600

    set revert to delete COTs

--- specifyweb/specify/migrations/0022_ensure_default_cots.py
+++ specifyweb/specify/migrations/0022_ensure_default_cots.py
@@ -7,7 +7,17 @@ class Migration(migrations.Migration):
         create_default_collection_types(apps)
 
     def revert_default_collection_types(apps, schema_editor):
-        pass
+        # Set all collection records to have a null collectionobjecttype
+        Collectionobject = apps.get_model('specify', 'Collectionobject')
+        Collectionobject.objects.all().update(collectionobjecttype=None)
+
+        # Set all the collectionobject records of the collection
+        # NOTE: Do we want to do this?
+        Collectionobject.objects.update(collectionobjecttype=None)
+
+        # Delete all CollectionObjectType records
+        Collectionobjecttype = apps.get_model('specify', 'Collectionobjecttype')
+        Collectionobjecttype.objects.all().delete()
 
     dependencies = [
         ('specify', '0021_update_hidden_geo_tables'),
