edeed77d163745aa58e58f0cafaad51df8de81ab
alec_dev <acwhite211@gmail.com>
2025-02-13 10:53:57 -0600

    context django app upgrade

--- specifyweb/context/urls.py
+++ specifyweb/context/urls.py
@@ -2,7 +2,7 @@
 Defines the urls for the app context subsystem
 """
 
-from django.conf.urls import url
+from django.urls import path, re_path
 from django.urls import path
 
 from . import views, user_resources, collection_resources
@@ -10,28 +10,28 @@ from ..attachment_gw.views import get_settings as attachment_settings
 from ..report_runner.views import get_status as report_runner_status
 
 urlpatterns = [
-    url(r'^login/$', views.api_login),
-    url(r'^collection/$', views.collection),
-    url(r'^user_collection_access_for_sp6/(?P<userid>\d+)/$', views.user_collection_access_for_sp6),
-    url(r'^language/$', views.languages),
-    url(r'^schema/language/$', views.schema_language),
+    path('login/', views.api_login),
+    path('collection/', views.collection),
+    path('user_collection_access_for_sp6/<int:userid>/', views.user_collection_access_for_sp6),
+    path('language/', views.languages),
+    path('schema/language/', views.schema_language),
 
-    url(r'^api_endpoints.json$', views.api_endpoints),
-    url(r'^api_endpoints_all.json$', views.api_endpoints_all),
-    url(r'^user.json$', views.user),
-    url(r'^system_info.json$', views.system_info),
-    url(r'^domain.json$', views.domain),
-    url(r'^view.json$', views.view),
-    url(r'^views.json$', views.views),
-    url(r'^viewsets.json$', views.viewsets),
-    url(r'^datamodel.json$', views.datamodel),
-    url(r'^schema_localization.json$', views.schema_localization),
-    url(r'^app.resource$', views.app_resource),
-    url(r'^available_related_searches.json$', views.available_related_searches),
-    url(r'^remoteprefs.properties$', views.remote_prefs),
+    re_path(r'^api_endpoints.json$', views.api_endpoints),
+    re_path(r'^api_endpoints_all.json$', views.api_endpoints_all),
+    re_path(r'^user.json$', views.user),
+    re_path(r'^system_info.json$', views.system_info),
+    re_path(r'^domain.json$', views.domain),
+    re_path(r'^view.json$', views.view),
+    re_path(r'^views.json$', views.views),
+    re_path(r'^viewsets.json$', views.viewsets),
+    re_path(r'^datamodel.json$', views.datamodel),
+    re_path(r'^schema_localization.json$', views.schema_localization),
+    re_path(r'^app.resource$', views.app_resource),
+    re_path(r'^available_related_searches.json$', views.available_related_searches),
+    re_path(r'^remoteprefs.properties$', views.remote_prefs),
 
-    url(r'^attachment_settings.json$', attachment_settings),
-    url(r'^report_runner_status.json$', report_runner_status),
+    re_path(r'^attachment_settings.json$', attachment_settings),
+    re_path(r'^report_runner_status.json$', report_runner_status),
 
     path('user_resource/', user_resources.user_resources),
     path('user_resource/<int:resourceid>/', user_resources.user_resource),
