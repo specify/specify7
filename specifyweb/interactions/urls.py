8bc4c5f1db4c06ae71e297576e60b868682b271e
alec_dev <acwhite211@gmail.com>
2025-02-13 10:48:35 -0600

    interactions app django upgrade

--- specifyweb/interactions/urls.py
+++ specifyweb/interactions/urls.py
@@ -1,18 +1,18 @@
-from django.conf.urls import url
+from django.urls import path, re_path
 
 from .views import *
 
 urlpatterns = [
-    url(r'^preparations_available_rs/(?P<recordset_id>\d+)/', preps_available_rs),
-    url(r'^preparations_available_ids/', preps_available_ids),
-    url(r'^loan_return_all/', loan_return_all_items),
-    url(r'^prep_interactions/', prep_interactions),
-    url(r'^prep_availability/(?P<prep_id>\d+)/(?P<iprep_id>\d+)/(?P<iprep_name>\w+)/', prep_availability),
-    url(r'^prep_availability/(?P<prep_id>\d+)/', prep_availability),
+    re_path(r'^preparations_available_rs/(?P<recordset_id>\d+)/', preps_available_rs),
+    re_path(r'^preparations_available_ids/', preps_available_ids),
+    re_path(r'^loan_return_all/', loan_return_all_items),
+    re_path(r'^prep_interactions/', prep_interactions),
+    re_path(r'^prep_availability/(?P<prep_id>\d+)/(?P<iprep_id>\d+)/(?P<iprep_name>\w+)/', prep_availability),
+    re_path(r'^prep_availability/(?P<prep_id>\d+)/', prep_availability),
 
     # special COG APIs
     # url(r'^cog_consolidated_preps/(?P<model>\w+)/$', cog_consolidated_preps),
     # url(r'^remove_cog_consolidated_preps/(?P<model>\w+)/$', remove_cog_consolidated_preps),
     # url(r'^create_sibling_loan_preps/$', create_sibling_loan_preps),
-    url(r'^sibling_preps/$', get_sibling_preps),
+    path('sibling_preps/', get_sibling_preps),
 ]
