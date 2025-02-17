6836995588c3d46f33d3bec4ee3369f10d5cb934
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2025-02-17 13:22:47 -0800

    Add storage for guid field

--- specifyweb/businessrules/rules/guid_rules.py
+++ specifyweb/businessrules/rules/guid_rules.py
@@ -1,7 +1,7 @@
 from uuid import uuid4
 from specifyweb.businessrules.orm_signal_handler import orm_signal_handler
 
-from specifyweb.specify.models import Taxon, Geography
+from specifyweb.specify.models import Taxon, Geography, Storage
 
 
 @orm_signal_handler('pre_save')
@@ -10,7 +10,7 @@ def set_guids(sender, obj):
         return
 
     # See https://github.com/specify/specify7/issues/4243#issuecomment-1836990623
-    if sender in (Taxon, Geography):
+    if sender in (Taxon, Geography, Storage):
         return
     if obj.guid is None or obj.guid.strip() == "":
         obj.guid = str(uuid4())
