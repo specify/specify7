203eb528e9aa71755c971669939ae239be2a666d
alesan99 <alesanesp99@gmail.com>
2025-02-19 10:38:55 -0600

    Create new data set from attachments Remove dataset testing stuff

--- specifyweb/frontend/js_src/lib/components/WbImportAttachments/index.tsx
+++ specifyweb/frontend/js_src/lib/components/WbImportAttachments/index.tsx
@@ -5,32 +5,57 @@
  */
 
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { commonText } from '../../localization/common';
+import { attachmentsText } from '../../localization/attachments';
 import { Container, H2 } from '../Atoms';
 import { useMenuItem } from '../Header/MenuContext';
-import { FilePicker } from '../Molecules/FilePicker';
+import { FilePicker, Layout } from '../Molecules/FilePicker';
+import { createDataSet } from '../WbImport/helpers';
+import type { RA } from '../../utils/types';
 
 export function WbImportAttachmentsView(): JSX.Element {
   useMenuItem('workBench');
-  const [file] = React.useState<File | undefined>();
+  const navigate = useNavigate();
 
+  const handleFilesSelected = async (files: FileList): Promise<void> => {
+    // TODO: Remove
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      console.log(file);
+    }
+  
+    // TODO: Upload attachments to spdatasetAttachments
+    // Then this attachment column will be set to the ID of the attachment
+    // For multiple attachments either use new columns (a bit wasteful) or separate each ID with a comma (unconventional for the WB)
+    const data: RA<RA<string>> = [
+      ["Attachment"],
+      ...Array.from(files).map(file => [file.name] as RA<string>)
+    ];
+  
+    const { id } = await createDataSet({
+      dataSetName: attachmentsText.attachments(),
+      fileName: "attachments",
+      hasHeader: true,
+      data,
+    });
+    navigate(`/specify/workbench/${id}/`);
+  };
+
+  // TODO: Preview files and a separate "Import Attachments" button
   return (
-    <Container.Full>
-      <H2>{commonText.multipleFilePickerMessage()}</H2>
-      <div className="w-96">
-        <FilePicker
-          acceptedFormats={undefined}
-          containerClassName="min-w-fit"
-          showFileNames={false}
-          onFilesSelected={handleFilesSelected}
-        />
-      </div>
-      {typeof file === 'object' && <FilePicked file={file} />}
-    </Container.Full>
+    <>
+      <Container.Full>
+        <H2>{commonText.multipleFilePickerMessage()}</H2>
+        <div className="w-96">
+          <FilePicker
+            acceptedFormats={undefined}
+            showFileNames={true}
+            onFilesSelected={handleFilesSelected}
+          />
+        </div>
+      </Container.Full>
+    </>
   );
 }
-
-const handleFilesSelected = (files: FileList): void => {
-  console.log(files);
-};
