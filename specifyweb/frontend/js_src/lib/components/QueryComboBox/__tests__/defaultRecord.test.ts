610d6a0c06e34f147e2d793048c96a76a9dec9c0
alesan99 <alesanesp99@gmail.com>
2025-03-05 10:38:26 -0600

    Add tests for CURRENT_USER and BLANK

--- specifyweb/frontend/js_src/lib/components/QueryComboBox/__tests__/defaultRecord.test.ts
+++ specifyweb/frontend/js_src/lib/components/QueryComboBox/__tests__/defaultRecord.test.ts
@@ -18,7 +18,7 @@ test('Query Combo Box with default record selection', () => {
 	expect(resource.get('cataloger')).toBe(defaultRecord);
 });
 
-test('Query Combo Box with current user selected', () => {
+test('Query Combo Box with current agent selected', () => {
 	const resource = new tables.CollectionObject.Resource();
 	expect(resource.get('cataloger')).toBeUndefined();
 	const defaultRecord = 'CURRENT_AGENT';
@@ -28,4 +28,27 @@ test('Query Combo Box with current user selected', () => {
 	  defaultRecord,
 	});
 	expect(resource.get('cataloger')).toBe(userInformation.agent.resource_uri);
+});
+test('Query Combo Box with current user selected', () => {
+	const resource = new tables.RecordSet.Resource();
+	expect(resource.get('specifyUser')).toBeUndefined();
+	const defaultRecord = 'CURRENT_USER';
+	useQueryComboBoxDefaults({
+	  resource,
+	  field: tables.RecordSet.strictGetRelationship('specifyUser'),
+	  defaultRecord,
+	});
+	expect(resource.get('specifyUser')).toBe(userInformation.resource_uri);
+});
+
+test('Query Combo Box with default record override', () => {
+	const resource = new tables.CollectionObject.Resource();
+	expect(resource.get('cataloger')).toBeUndefined();
+	const defaultRecord = 'BLANK';
+	useQueryComboBoxDefaults({
+	  resource,
+	  field: tables.CollectionObject.strictGetRelationship('cataloger'),
+	  defaultRecord,
+	});
+	expect(resource.get('cataloger')).toBeUndefined();
 });
