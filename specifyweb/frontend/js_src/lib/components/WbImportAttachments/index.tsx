db2992549f006007a188e1ea3e118d0ec8c255a7
alesan99 <alesanesp99@gmail.com>
2025-03-11 11:56:28 -0500

    WIP Upload attachments and show IDs in dataset

--- specifyweb/frontend/js_src/lib/components/WbImportAttachments/index.tsx
+++ specifyweb/frontend/js_src/lib/components/WbImportAttachments/index.tsx
@@ -8,6 +8,11 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { attachmentsText } from '../../localization/attachments';
+import {
+  attachmentSettingsPromise,
+  fetchAssetToken,
+  uploadFile,
+} from '../Attachments/attachments';
 import { commonText } from '../../localization/common';
 import type { RA } from '../../utils/types';
 import { Container, H2 } from '../Atoms';
@@ -16,6 +21,13 @@ import { useMenuItem } from '../Header/MenuContext';
 import { FilePicker } from '../Molecules/FilePicker';
 import { createDataSet } from '../WbImport/helpers';
 import { ChooseName } from '../WbImport/index';
+import { LoadingContext } from '../Core/Contexts';
+import { useBooleanState } from '../../hooks/useBooleanState';
+import { raise } from '../Errors/Crash';
+import type { SpecifyResource } from '../DataModel/legacyTypes';
+import { tables } from '../DataModel/tables';
+import type { Attachment } from '../DataModel/types';
+// import { useErrorContext } from '../../hooks/useErrorContext';
 
 export function WbImportAttachmentsView(): JSX.Element {
   useMenuItem('workBench');
@@ -42,6 +54,11 @@ export function WbImportAttachmentsView(): JSX.Element {
 
 function FilesPicked({ files }: { readonly files: readonly File[] }): JSX.Element {
   const navigate = useNavigate();
+  const [uploadProgress, setUploadProgress] = React.useState<
+  number | true | undefined
+  >(undefined);
+  const [isFailed, handleFailed] = useBooleanState();
+  const loading = React.useContext(LoadingContext);
 
   const handleFilesSelected = async (
     files: readonly File[],
@@ -52,9 +69,44 @@ function FilesPicked({ files }: { readonly files: readonly File[] }): JSX.Elemen
      * Then this attachment column will be set to the ID of the attachment
      * For multiple attachments either use new columns (a bit wasteful) or separate each ID with a comma (unconventional for the WB)
      */
+
+    // go through all files in "files"
+
+    let attachments: SpecifyResource<Attachment>[] = [];
+    let attachmentIds: number[] = []; // TODO: These should be SpDataSetAttachments, not Attachments (For the DataSet -> SpDataSetAttachment -> Attachment relationship)
+
+    async function handleUploaded(attachment: SpecifyResource<Attachment>): Promise<void> {
+      await attachment.save();
+      console.log(attachment);
+      attachments.push(attachment);
+      attachmentIds.push(attachment.id);
+      console.log(attachment.id, "done");
+    }
+    console.log("uploading files");
+
+    // TODO Add loading bar
+    const uploads = files.map((file) =>
+      uploadFile(file, setUploadProgress)
+        .then((attachment) =>
+          attachment === undefined
+            ? handleFailed()
+            : handleUploaded(attachment)
+        )
+        .catch((error) => {
+          handleFailed();
+          raise(error);
+        })
+        .finally(() => setUploadProgress(undefined))
+    );
+
+    await Promise.all(uploads); // Wait for all uploads to be complete
+    console.log("probably done uploading files");
+
+    // TODO: Create SpDataSetAttachment Record, then link Attachments to SpDataSetAttachment
+
     const data: RA<RA<string>> = [
       ['Attachment'],
-      ...Array.from(files, (file) => [file.name] as RA<string>),
+      ...Array.from(attachmentIds, (id) => [id.toString()]),
     ];
 
     const { id } = await createDataSet({
@@ -85,3 +137,33 @@ function FilesPicked({ files }: { readonly files: readonly File[] }): JSX.Elemen
     </div>
   );
 }
+
+// const createWorkbenchDataSet = async () =>
+//   createEmptyDataSet<Dataset>(
+//     '/api/workbench/dataset/',
+//     wbText.newDataSetName({ date: new Date().toDateString() }),
+//     {
+//       importedfilename: '',
+//       columns: [],
+//     }
+//   );
+
+// export const createEmptyDataSet = async <
+//   DATASET extends AttachmentDataSet | Dataset,
+// >(
+//   datasetUrl: string,
+//   name: LocalizedString,
+//   props?: Partial<DATASET>
+// ): Promise<DATASET> =>
+//   ajax<DATASET>(datasetUrl, {
+//     method: 'POST',
+//     body: {
+//       name: await uniquifyDataSetName(name, undefined, datasetUrl),
+//       rows: [],
+//       ...props,
+//     },
+//     headers: {
+//       // eslint-disable-next-line @typescript-eslint/naming-convention
+//       Accept: 'application/json',
+//     },
+//   }).then(({ data }) => data);
