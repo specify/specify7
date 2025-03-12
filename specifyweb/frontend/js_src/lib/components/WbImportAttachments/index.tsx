fd2c42c6452fed3969b12d35339a6d92ba5c0006
alesan99 <alesanesp99@gmail.com>
2025-03-12 09:57:54 -0500

    Create SpDataSetAttachment records on wb attachment import

--- specifyweb/frontend/js_src/lib/components/WbImportAttachments/index.tsx
+++ specifyweb/frontend/js_src/lib/components/WbImportAttachments/index.tsx
@@ -8,26 +8,25 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { attachmentsText } from '../../localization/attachments';
-import {
-  attachmentSettingsPromise,
-  fetchAssetToken,
-  uploadFile,
-} from '../Attachments/attachments';
+import { uploadFile } from '../Attachments/attachments';
 import { commonText } from '../../localization/common';
 import type { RA } from '../../utils/types';
 import { Container, H2 } from '../Atoms';
 import { Button } from '../Atoms/Button';
 import { useMenuItem } from '../Header/MenuContext';
 import { FilePicker } from '../Molecules/FilePicker';
-import { createDataSet } from '../WbImport/helpers';
+import { uniquifyDataSetName } from '../WbImport/helpers';
 import { ChooseName } from '../WbImport/index';
 import { LoadingContext } from '../Core/Contexts';
 import { useBooleanState } from '../../hooks/useBooleanState';
 import { raise } from '../Errors/Crash';
 import type { SpecifyResource } from '../DataModel/legacyTypes';
 import { tables } from '../DataModel/tables';
-import type { Attachment } from '../DataModel/types';
-// import { useErrorContext } from '../../hooks/useErrorContext';
+import type { Attachment, SpDataSetAttachment } from '../DataModel/types';
+import { userInformation } from '../InitialContext/userInformation';
+import { Dialog } from '../Molecules/Dialog';
+import { Progress } from '../Atoms';
+import { loadingBar } from '../Molecules';
 
 export function WbImportAttachmentsView(): JSX.Element {
   useMenuItem('workBench');
@@ -54,9 +53,7 @@ export function WbImportAttachmentsView(): JSX.Element {
 
 function FilesPicked({ files }: { readonly files: readonly File[] }): JSX.Element {
   const navigate = useNavigate();
-  const [uploadProgress, setUploadProgress] = React.useState<
-  number | true | undefined
-  >(undefined);
+  const [uploadProgress, setUploadProgress] = React.useState<number | true | undefined>(undefined);
   const [isFailed, handleFailed] = useBooleanState();
   const loading = React.useContext(LoadingContext);
 
@@ -64,27 +61,30 @@ function FilesPicked({ files }: { readonly files: readonly File[] }): JSX.Elemen
     files: readonly File[],
     dataSetName: string
   ): Promise<void> => {
-    /*
-     * TODO: Upload attachments to spdatasetAttachments
-     * Then this attachment column will be set to the ID of the attachment
-     * For multiple attachments either use new columns (a bit wasteful) or separate each ID with a comma (unconventional for the WB)
-     */
+    dataSetName = await uniquifyDataSetName(dataSetName);
+    const dataSet = new tables.Spdataset.Resource({
+      name: dataSetName,
+      importedfilename: 'attachments',
+      columns: ['Attachment'] as never,
+      data: [[]] as never,
+      specifyuser: userInformation.resource_uri
+    });
+    await dataSet.save();
+    const dataSetUrl = dataSet.url();
 
-    // go through all files in "files"
-
-    let attachments: SpecifyResource<Attachment>[] = [];
-    let attachmentIds: number[] = []; // TODO: These should be SpDataSetAttachments, not Attachments (For the DataSet -> SpDataSetAttachment -> Attachment relationship)
-
+    let spDataSetAttachmentIds: number[] = [];
     async function handleUploaded(attachment: SpecifyResource<Attachment>): Promise<void> {
-      await attachment.save();
-      console.log(attachment);
-      attachments.push(attachment);
-      attachmentIds.push(attachment.id);
-      console.log(attachment.id, "done");
+      // Create SpDataSetAttachment Record for each uploaded attachment
+      const spDataSetAtt: SpecifyResource<SpDataSetAttachment> = new tables.SpDataSetAttachment.Resource({
+        attachment: attachment as never,
+        spdataset: dataSetUrl as never,
+        ordinal: 0
+      });
+      attachment.set('tableID', spDataSetAtt.specifyTable.tableId);
+      await spDataSetAtt.save();
+      spDataSetAttachmentIds.push(spDataSetAtt.id);
     }
-    console.log("uploading files");
 
-    // TODO Add loading bar
     const uploads = files.map((file) =>
       uploadFile(file, setUploadProgress)
         .then((attachment) =>
@@ -99,31 +99,41 @@ function FilesPicked({ files }: { readonly files: readonly File[] }): JSX.Elemen
         .finally(() => setUploadProgress(undefined))
     );
 
-    await Promise.all(uploads); // Wait for all uploads to be complete
-    console.log("probably done uploading files");
+    loading(Promise.all(uploads));
+    await Promise.all(uploads);
 
-    // TODO: Create SpDataSetAttachment Record, then link Attachments to SpDataSetAttachment
-
+    // Data set will contain the ids to the SpDataSetAttachment records
     const data: RA<RA<string>> = [
-      ['Attachment'],
-      ...Array.from(attachmentIds, (id) => [id.toString()]),
+      ...Array.from(spDataSetAttachmentIds, (id) => [id.toString()]),
     ];
-
-    const { id } = await createDataSet({
-      dataSetName,
-      fileName: 'attachments',
-      hasHeader: true,
-      data,
-    });
-    navigate(`/specify/workbench/${id}/`);
+    dataSet.set('data', data as never);
+    loading(dataSet.save().then(() => {
+      navigate(`/specify/workbench/plan/${dataSet.id}/`)
+    }));
   };
 
   const [dataSetName, setDataSetName] = React.useState<string>(
     attachmentsText.attachments()
   );
 
-  // TODO: Preview files
-  return (
+  // TODO: Preview files, progress bar
+  return isFailed ? (
+      <p>{attachmentsText.attachmentServerUnavailable()}</p>
+    ) : typeof uploadProgress === 'object' ? (
+      <Dialog
+        buttons={undefined}
+        header={attachmentsText.uploadingInline()}
+        onClose={undefined}
+      >
+        <div aria-live="polite">
+          {typeof uploadProgress === 'number' ? (
+            <Progress value={uploadProgress} />
+          ) : (
+            loadingBar
+          )}
+        </div>
+      </Dialog>
+    ) : (
     <div className="grid w-96 grid-cols-2 items-center gap-2 mt-2">
       <ChooseName name={dataSetName} onChange={setDataSetName} />
       <Button.Secondary
@@ -137,33 +147,3 @@ function FilesPicked({ files }: { readonly files: readonly File[] }): JSX.Elemen
     </div>
   );
 }
-
-// const createWorkbenchDataSet = async () =>
-//   createEmptyDataSet<Dataset>(
-//     '/api/workbench/dataset/',
-//     wbText.newDataSetName({ date: new Date().toDateString() }),
-//     {
-//       importedfilename: '',
-//       columns: [],
-//     }
-//   );
-
-// export const createEmptyDataSet = async <
-//   DATASET extends AttachmentDataSet | Dataset,
-// >(
-//   datasetUrl: string,
-//   name: LocalizedString,
-//   props?: Partial<DATASET>
-// ): Promise<DATASET> =>
-//   ajax<DATASET>(datasetUrl, {
-//     method: 'POST',
-//     body: {
-//       name: await uniquifyDataSetName(name, undefined, datasetUrl),
-//       rows: [],
-//       ...props,
-//     },
-//     headers: {
-//       // eslint-disable-next-line @typescript-eslint/naming-convention
-//       Accept: 'application/json',
-//     },
-//   }).then(({ data }) => data);
