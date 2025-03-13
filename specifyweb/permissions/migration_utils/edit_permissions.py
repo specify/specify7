44fd03e186ade8b759f2b61c761fa7b6b7da3c93
melton-jason <meltonjason12@yahoo.com>
2025-03-13 03:05:00 -0500

    Make permissions/migration_utils a Python package

--- specifyweb/permissions/migration_utils/edit_permissions.py
+++ specifyweb/permissions/migration_utils/edit_permissions.py
@@ -25,12 +25,14 @@ def add_permission(apps):
             action='create_recordset',
         )
 
+
 def add_stats_edit_permission(apps):
     Collection = apps.get_model('specify', 'Collection')
     Role = apps.get_model('permissions', 'Role')
     for collection_id in Collection.objects.values_list('id', flat=True):
         try:
-            all_full_access_roles = Role.objects.filter(collection_id=collection_id, name="Full Access - Legacy")
+            all_full_access_roles = Role.objects.filter(
+                collection_id=collection_id, name="Full Access - Legacy")
             for full_access_role in all_full_access_roles:
                 full_access_role.policies.create(resource="/preferences"
                                                           "/statistics",
