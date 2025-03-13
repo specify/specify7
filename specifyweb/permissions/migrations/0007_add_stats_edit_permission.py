0ff12efd58787b5847eb0df43840b29532e10447
melton-jason <meltonjason12@yahoo.com>
2025-03-13 02:01:27 -0500

    Remove duplicate permission migration functions

--- specifyweb/permissions/migrations/0007_add_stats_edit_permission.py
+++ specifyweb/permissions/migrations/0007_add_stats_edit_permission.py
@@ -1,19 +1,7 @@
 from django.db import migrations
 from specifyweb.specify.models import Collection  # type: ignore
+from specifyweb.permissions.migration_utils.edit_permissions import add_stats_edit_permission
 from ..models import Role
-
-
-def add_stats_edit_permission(apps, schema_editor):
-    for collection_id in Collection.objects.values_list('id', flat=True):
-        try:
-            all_full_access_roles = Role.objects.filter(collection_id=collection_id, name="Full Access - Legacy")
-            for full_access_role in all_full_access_roles:
-                full_access_role.policies.create(resource="/preferences"
-                                                          "/statistics",
-                                                 action="edit")
-        except:
-            print("Failed to assign stats edit permission in collection: ",
-                  collection_id)
 
 
 class Migration(migrations.Migration):
