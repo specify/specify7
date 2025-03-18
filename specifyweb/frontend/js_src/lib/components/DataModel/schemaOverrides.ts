38a76f0e0745a05a14c74d85b4c310b272718336
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2025-03-18 10:05:30 -0700

    Remove overide hidden for isPrimary

--- specifyweb/frontend/js_src/lib/components/DataModel/schemaOverrides.ts
+++ specifyweb/frontend/js_src/lib/components/DataModel/schemaOverrides.ts
@@ -226,7 +226,6 @@ const fieldOverwrites: typeof globalFieldOverrides = {
     definition: { visibility: 'hidden' },
     definitionItem: { visibility: 'hidden' },
     orderNumber: { visibility: 'hidden' },
-    isPrimary: { visibility: 'hidden' },
     isHybrid: { visibility: 'hidden' },
     isAccepted: { visibility: 'hidden' },
     fullName: { visibility: 'readOnly' },
