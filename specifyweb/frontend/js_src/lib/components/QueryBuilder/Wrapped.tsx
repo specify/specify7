226d2188bf09eb140fd828025a8037714026ff21
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2025-03-19 13:08:49 -0700

    Allow to save queries from tree
    Fixes #4925

--- specifyweb/frontend/js_src/lib/components/QueryBuilder/Wrapped.tsx
+++ specifyweb/frontend/js_src/lib/components/QueryBuilder/Wrapped.tsx
@@ -145,12 +145,12 @@ function Wrapped({
       throttle(
         () =>
           setSaveRequired(
-            state !== pendingState &&
-              initialFields.current !== JSON.stringify(state.fields)
+            (state !== pendingState &&
+              initialFields.current !== JSON.stringify(state.fields)) || autoRun
           ),
         200
       ),
-    [initialFields.current, state.fields]
+    [initialFields.current, state.fields, autoRun]
   );
 
   React.useEffect(checkForChanges, [state.fields]);
