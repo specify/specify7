a8a2ad6c322187e248019065b91a4ed1f9fd27d0
Sharad S <sharad.swaminathan@specifysoftware.org>
2025-03-27 15:31:48 -0400

    Fix visual order - For multiple trees, columns will be grouped by tree first

--- specifyweb/stored_queries/batch_edit.py
+++ specifyweb/stored_queries/batch_edit.py
@@ -1169,7 +1169,7 @@ def run_batch_edit_query(props: BatchEditProps):
 
     # We would have arbitarily sorted the columns, so our columns will not be correct.
     # Rather than sifting the data, we just add a default visual order.
-    visual_order = Func.first(sorted(headers_enumerated, key=lambda tup: tup[1][0]))
+    visual_order = Func.first(headers_enumerated)
 
     headers = Func.second(key_and_headers)
 
