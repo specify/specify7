5df922a3ced3f8794860517e575ca27e283bc84f
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2025-04-10 07:03:29 -0700

    Improve conditional in backendrules

--- specifyweb/businessrules/rules/component_rules.py
+++ specifyweb/businessrules/rules/component_rules.py
@@ -8,7 +8,9 @@ from specifyweb.specify.models import Component
 
 @orm_signal_handler('pre_save', 'Component')
 def component_pre_save(component):
- if component.parentcomponent is not None:
+ if component.parentcomponent is not None and not hasattr(component, "collectionobject"):
   component.collectionobject =  component.parentcomponent.collectionobject
+  component.save()
+  component.parentcomponent.collectionobject.components.add(component)
  
 
