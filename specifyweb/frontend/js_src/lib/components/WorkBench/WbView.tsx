17e24ff721064cb52a94a770624ab85211328f29
alesan99 <alesanesp99@gmail.com>
2025-04-23 10:29:43 -0500

    Hide attachments button on datasets without attachments

--- specifyweb/frontend/js_src/lib/components/WorkBench/WbView.tsx
+++ specifyweb/frontend/js_src/lib/components/WorkBench/WbView.tsx
@@ -49,6 +49,7 @@ import { useDisambiguationDialog } from './useDisambiguationDialog';
 import { WbAttachmentsPreview } from './WbAttachmentsPreview';
 import { WbSpreadsheet } from './WbSpreadsheet';
 import { WbValidation } from './WbValidation';
+import { ATTACHMENTS_COLUMN } from '../WbImportAttachments';
 
 export type WbStatus = 'unupload' | 'upload' | 'validate';
 
