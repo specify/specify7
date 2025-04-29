cf65548df7d535002f9b91903fccce3ce9c1cb00
alec_dev <acwhite211@gmail.com>
2025-04-29 10:38:33 -0500

    Update and rename 0027_selectseries.py to 0028_selectseries.py

--- specifyweb/specify/migrations/0028_selectseries.py
+++ specifyweb/specify/migrations/0028_selectseries.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('specify', '0026_taxontreedef_alter_delete'),
+        ('specify', '0027_CO_children'),
     ]
 
     operations = [
