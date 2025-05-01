1e288bc822fa9c3f0d02340588583a85877c62a1
melton-jason <meltonjason12@yahoo.com>
2025-05-01 14:27:04 -0500

    Cast catalogNumber to Numeric in query results if all formats are numeric
    Fixes #6464
    See related #5485 and #6245
    
    This handles the most common case.
    In the case where there exists both numeric and non-numeric formats for
    catalogNumber, the numeric formats will still be cast as a string.
    This also ONLY works with formats called CatalogNumberNumeric;
    i.e., any other purely numeric format will not be considered as numeric
    by the QueryBuilder

--- specifyweb/stored_queries/format.py
+++ specifyweb/stored_queries/format.py
@@ -350,6 +350,9 @@ class ObjectFormatter(object):
 
         if specify_field.type in ("java.lang.Integer", "java.lang.Short"):
             return field
+        
+        if specify_field is CollectionObject_model.get_field('catalogNumber') and all_numeric_catnum_formats(self.collection):
+            return cast(field, types.Numeric(65))
 
         if specify_field.type == 'json' and isinstance(field.comparator.type, types.JSON):
             return cast(field, types.Text)
@@ -364,6 +367,10 @@ def get_date_format() -> str:
     logger.debug("dateformat = %s = %s", date_format, mysql_date_format)
     return mysql_date_format
 
+def all_numeric_catnum_formats(collection) -> bool:
+    return (collection.catalognumformatname == 'CatalogNumberNumeric' and
+            all(cot.catalognumberformatname is None or cot.catalognumberformatname == 'CatalogNumberNumeric'
+                for cot in collection.cotypes.all()))
 
 MYSQL_TO_YEAR = {
     "%m %d %y": "%y",
