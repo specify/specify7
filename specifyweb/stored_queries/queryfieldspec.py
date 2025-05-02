e7daf1fa7fae1884c026d09de6b509b126381579
Sharad S <16229739+sharadsw@users.noreply.github.com>
2025-05-02 12:07:09 -0400

    Hotfixes: Fullname query hotfix + catalog number cast hotfix for `7.10.2` (#6465)
    * Handle case where rank name has spaces
    
    * Use name as default tree rank query
    
    * Handle edge case for fullname query
    
    * Use name for fullname
    
    * Skip test temporarily
    
    * Revert mapping for fullname
    
    * Fix case for fullName query with 1 space in rank
    - Also adds a unit test to ensure this doesn't break again
    
    * Cast catalogNumber to Numeric in query results if all formats are numeric
    
    Fixes #6464
    See related #5485 and #6245
    
    This handles the most common case.
    In the case where there exists both numeric and non-numeric formats for
    catalogNumber, the numeric formats will still be cast as a string.
    This also ONLY works with formats called CatalogNumberNumeric;
    i.e., any other purely numeric format will not be considered as numeric
    by the QueryBuilder
    
    * Add comment to catalogNumber cast for queries
    
    ---------
    
    Co-authored-by: melton-jason <meltonjason12@yahoo.com>

--- specifyweb/stored_queries/queryfieldspec.py
+++ specifyweb/stored_queries/queryfieldspec.py
@@ -69,21 +69,31 @@ def make_table_list(fs):
 
 
 def make_tree_fieldnames(table: Table, reverse=False):
-    mapping = {"ID": table.idFieldName.lower(), "": "fullname"}
+    mapping = {"ID": table.idFieldName.lower(), "": "name"}
     if reverse:
         return {value: key for (key, value) in mapping.items()}
     return mapping
 
 
-def find_tree_and_field(table, fieldname: str):
+def find_tree_and_field(table: Table, fieldname: str):
     fieldname = fieldname.strip()
     if fieldname == "":
         return None, None
-    # NOTE: Assumes rank names have no spaces
+    
     tree_rank_and_field = fieldname.split(" ")
     mapping = make_tree_fieldnames(table)
+
     if len(tree_rank_and_field) == 1:
         return tree_rank_and_field[0], mapping[""]
+    
+    # Handles case where rank name contains spaces
+    if len(tree_rank_and_field) >= 2:
+        if table.get_field(tree_rank_and_field[-1]) is not None:
+            tree_rank_and_field = [" ".join(tree_rank_and_field[:-1]), tree_rank_and_field[-1]]
+        else:
+            # Edge case: when rank name has spaces and there is no field in stringid (ie. fullname query)
+            tree_rank_and_field = [" ".join(tree_rank_and_field), ""]
+    
     tree_rank, tree_field = tree_rank_and_field
     return tree_rank, mapping.get(tree_field, tree_field)
 
