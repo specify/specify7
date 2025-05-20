87eaf81ebde985d2a33a3aa979ad3d7c584d3ee7
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2025-05-20 09:13:37 -0400

    Updates in localization

--- specifyweb/frontend/js_src/lib/localization/configurationText.ts
+++ specifyweb/frontend/js_src/lib/localization/configurationText.ts
@@ -1,5 +1,5 @@
 /**
- * Localization strings for the preferences menu
+ * Localization strings for the configuration tool
  *
  * @module
  */
@@ -9,10 +9,10 @@ import { createDictionary } from './utils';
 // Refer to "Guidelines for Programmers" in ./README.md before editing this file
 
 export const configurationText = createDictionary({
-configurationTool: {
-  'en-us': 'Configuration Tool'
-},
-specifySetUp: {
-  'en-us': 'Specify Setup'
-},
-} as const)
+  configurationTool: {
+    'en-us': 'Configuration Tool',
+  },
+  specifySetUp: {
+    'en-us': 'Specify Setup',
+  },
+} as const);
