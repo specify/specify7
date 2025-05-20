4e1d47f03f7216d0db2fa76d0615f3228d6569ef
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2025-05-20 14:38:31 -0400

    class name

--- specifyweb/frontend/js_src/lib/components/Toolbar/SystemConfigTool.tsx
+++ specifyweb/frontend/js_src/lib/components/Toolbar/SystemConfigTool.tsx
@@ -16,110 +16,127 @@ import { Dialog, LoadingScreen } from '../Molecules/Dialog';
 export function SystemConfigurationTool(): JSX.Element | null {
   const [allInfo, setAllInfo] = React.useState<InstitutionData | null>(null);
 
-  const [newResourceOpen, handleNewResource, closeNewResource] = useBooleanState()
+  const [newResourceOpen, handleNewResource, closeNewResource] =
+    useBooleanState();
 
-  const [parentId, setParentId] = React.useState<number | undefined>()
+  const [parentId, setParentId] = React.useState<number | undefined>();
 
-  const [newResource, setNewResource] = React.useState<SpecifyResource<Collection> | undefined>()
+  const [newResource, setNewResource] = React.useState<
+    SpecifyResource<Collection> | undefined
+  >();
 
   React.useEffect(() => {
-    fetchAllSystemData.then(setAllInfo).catch(() => console.warn('Error when fetching institution info'));
+    fetchAllSystemData
+      .then(setAllInfo)
+      .catch(() => console.warn('Error when fetching institution info'));
   }, []);
 
-  const renderHierarchy = (institution: InstitutionData | null): JSX.Element => {
+  const renderHierarchy = (
+    institution: InstitutionData | null
+  ): JSX.Element => {
     if (!institution) return <LoadingScreen />;
-  
+
     return (
       <Ul className="m-4">
         <li>
-          <div className='flex'>
-          <H2>{`Institution: ${institution.name}`}</H2>
-          <Button.Icon
-                  icon="plus"
-                  title="Add new division to institution"
-                  onClick={() => {
-                    console.log(`'Add new division to institution' ${institution.id}`)
-                    setParentId(institution.id)
-                    setNewResource(new tables.Division.Resource())
-                    handleNewResource()
-                  }}
-                />
-                </div>
-            <Ul className="m-6">
-              {institution.children.map((division) => (
-                <li key={division.id}>
-                  <div className='flex'>
+          <div className="flex">
+            <H2>{`Institution: ${institution.name}`}</H2>
+            <Button.Icon
+              icon="plus"
+              title="Add new division to institution"
+              onClick={() => {
+                console.log(
+                  `'Add new division to institution' ${institution.id}`
+                );
+                setParentId(institution.id);
+                setNewResource(new tables.Division.Resource());
+                handleNewResource();
+              }}
+            />
+          </div>
+          <Ul className="m-6">
+            {institution.children.map((division) => (
+              <li key={division.id}>
+                <div className="flex">
                   <H3>{`Division: ${division.name}`}</H3>
                   <Button.Icon
-                  icon="plus"
-                  title="Add new discipline to institution"
-                  onClick={() => {
-                    console.log(`'Add new discipline to division' ${division.id}`)
-                    setParentId(division.id)
-                    setNewResource(new tables.Discipline.Resource())
-                    handleNewResource()
-                  }}
-                />
+                    icon="plus"
+                    title="Add new discipline to institution"
+                    onClick={() => {
+                      console.log(
+                        `'Add new discipline to division' ${division.id}`
+                      );
+                      setParentId(division.id);
+                      setNewResource(new tables.Discipline.Resource());
+                      handleNewResource();
+                    }}
+                  />
                 </div>
-                  {division.children.length > 0 && (
-                    <Ul className="m-6">
-                      {division.children.map((discipline) => (
-                        <li key={discipline.id}>
-                          <div className='flex'>
+                {division.children.length > 0 && (
+                  <Ul className="m-6">
+                    {division.children.map((discipline) => (
+                      <li key={discipline.id}>
+                        <div className="flex">
                           <h4>{`Discipline: ${discipline.name}`}</h4>
                           <Button.Icon
                             icon="plus"
                             title="Add new collection to discipline"
-                            onClick={() =>{
-                              console.log(`'Add new collection to discipline' ${discipline.id}`)
-                              setParentId(discipline.id)
-                              setNewResource(new tables.Collection.Resource())
-                              handleNewResource()
-                              }}/>
-                          </div>
-                          {discipline.children.length > 0 && (
-                            <Ul className="m-6">
-                              {discipline.children.map((collection) => (
-                                <li className="m-4 list-disc" key={collection.id}>
-                                  <p>{collection.name}</p>
-                                </li>
-                              ))}
-                            </Ul>
-                          )}
-                        </li>
-                      ))}
-                    </Ul>
-                  )}
-                  
-                </li>
-              ))}
-            </Ul>
+                            onClick={() => {
+                              console.log(
+                                `'Add new collection to discipline' ${discipline.id}`
+                              );
+                              setParentId(discipline.id);
+                              setNewResource(new tables.Collection.Resource());
+                              handleNewResource();
+                            }}
+                          />
+                        </div>
+                        {discipline.children.length > 0 && (
+                          <Ul className="m-6">
+                            {discipline.children.map((collection) => (
+                              <li className="m-4 list-disc" key={collection.id}>
+                                <p>{collection.name}</p>
+                              </li>
+                            ))}
+                          </Ul>
+                        )}
+                      </li>
+                    ))}
+                  </Ul>
+                )}
+              </li>
+            ))}
+          </Ul>
         </li>
       </Ul>
     );
   };
 
   return (
-    <Container.FullGray>
+    <Container.FullGray className="sm:h-auto">
       <H2 className="text-2xl">{userText.systemConfigurationTool()}</H2>
       <div className="flex h-0 flex-1 flex-col gap-4 md:flex-row">
-      {allInfo === undefined || allInfo === null ? <LoadingScreen /> : renderHierarchy(allInfo)}
+        {allInfo === undefined || allInfo === null ? (
+          <LoadingScreen />
+        ) : (
+          renderHierarchy(allInfo)
+        )}
       </div>
-      {newResourceOpen ?     
+      {newResourceOpen ? (
         <Dialog header="Add new Resource" onClose={closeNewResource}>
           <ResourceView
-                dialog="modal"
-                isDependent={false}
-                isSubForm={false}
-                resource={newResource}
-                viewName={collection}
-                onAdd={undefined}
-                onClose={closeNewResource}
-                onDeleted={undefined}
-                onSaved={() => console.log('Click')}
-              />
-          </Dialog> 
-        : undefined}
+            dialog="modal"
+            isDependent={false}
+            isSubForm={false}
+            resource={newResource}
+            viewName={collection}
+            onAdd={undefined}
+            onClose={closeNewResource}
+            onDeleted={undefined}
+            onSaved={() => console.log('Click')}
+          />
+        </Dialog>
+      ) : undefined}
     </Container.FullGray>
   );
 }
@@ -145,7 +162,8 @@ type InstitutionData = {
         readonly name: string;
       }>;
     }>;
-  }>;}
+  }>;
+};
 // };
 
 let institutionData: InstitutionData;
@@ -158,4 +176,4 @@ export const fetchAllSystemData = load<InstitutionData>(
   return data;
 });
 
-export const getAllInfo = ():InstitutionData => institutionData
+export const getAllInfo = (): InstitutionData => institutionData;
