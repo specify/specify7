780195a0b75eb4270de12e76d75df82b91f6c346
alec_dev <acwhite211@gmail.com>
2025-06-04 14:59:18 -0500

    remove specify first migration dependency from workbench init migration

--- specifyweb/workbench/migrations/0001_initial.py
+++ specifyweb/workbench/migrations/0001_initial.py
@@ -10,8 +10,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('specify', '__first__'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL)
     ]
 
     operations = [
