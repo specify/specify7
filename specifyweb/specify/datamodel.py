72a1c8a8556bd4e904b793bd1bd6457774deffd2
alec_dev <acwhite211@gmail.com>
2025-06-06 01:56:50 -0500

    Revert "remove selectseries references"
    This reverts commit f34bb61ee68af844333b437aaa45265736ed0523.

--- specifyweb/specify/datamodel.py
+++ specifyweb/specify/datamodel.py
@@ -6595,6 +6595,7 @@ datamodel = Datamodel(tables=[
             Field(name='remarks', column='Remarks', indexed=False, unique=False, required=False, type='text', length=4096),
             Field(name='searchSynonymy', column='SearchSynonymy', indexed=False, unique=False, required=False, type='java.lang.Boolean'),
             Field(name='selectDistinct', column='SelectDistinct', indexed=False, unique=False, required=False, type='java.lang.Boolean'),
+            Field(name='selectSeries', column='SelectSeries', indexed=False, unique=False, required=False, type='java.lang.Boolean'),
             Field(name='smushed', column='Smushed', indexed=False, unique=False, required=False, type='java.lang.Boolean'),
             Field(name='sqlStr', column='SqlStr', indexed=False, unique=False, required=False, type='text', length=4096),
             Field(name='timestampCreated', column='TimestampCreated', indexed=False, unique=False, required=True, type='java.sql.Timestamp'),
