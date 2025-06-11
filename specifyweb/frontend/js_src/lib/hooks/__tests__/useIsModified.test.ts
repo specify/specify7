cc3ce13bc27de98d21739fbbb3d6ce1a512a07c3
realVinayak <realvinayak123@gmail.com>
2025-06-11 22:57:47 +0000

    Lint code with ESLint and Prettier
    Triggered by 09f81cdd47cdf403b77f87d070d555c1fbb62352 on branch refs/heads/issue-6630

--- specifyweb/frontend/js_src/lib/hooks/__tests__/useIsModified.test.ts
+++ specifyweb/frontend/js_src/lib/hooks/__tests__/useIsModified.test.ts
@@ -1,18 +1,19 @@
-import { renderHook, waitFor, act } from "@testing-library/react";
-import { useIsModified } from "../useIsModified";
+import { act,renderHook, waitFor } from "@testing-library/react";
+
 import { tables } from "../../components/DataModel/tables";
 import { requireContext } from "../../tests/helpers";
+import { useIsModified } from "../useIsModified";
 
 requireContext();
 
 describe("useIsModifiedTest", ()=>{
 
-    it("handles undefined resource", ()=>{
+    test("handles undefined resource", ()=>{
         const { result } = renderHook(()=>useIsModified(undefined));
         expect(result.current).toBe(false);
     })
 
-    it("returns true when resource needs saved (existing resource)", ()=>{
+    test("returns true when resource needs saved (existing resource)", ()=>{
         const collectionObject = new tables.CollectionObject.Resource({
             id: 1
         });
@@ -21,7 +22,7 @@ describe("useIsModifiedTest", ()=>{
         expect(result.current).toBe(true);
     });
 
-    it("returns true when new resource needs saved only when allowed (new call)", ()=>{
+    test("returns true when new resource needs saved only when allowed (new call)", ()=>{
         const collectionObject = new tables.CollectionObject.Resource();
 
         collectionObject.set("integer1", 10);
@@ -36,7 +37,7 @@ describe("useIsModifiedTest", ()=>{
     });
 
 
-    it("returns true when new resource needs saved only when allowed (render diff)", async ()=>{
+    test("returns true when new resource needs saved only when allowed (render diff)", async ()=>{
         const collectionObject = new tables.CollectionObject.Resource();
 
         collectionObject.set("integer1", 10);
@@ -54,14 +55,16 @@ describe("useIsModifiedTest", ()=>{
         expect(ignoreNewResult.current).toBe(true);
     });
 
-    it("handles saveRequired event", async ()=>{
+    test("handles saveRequired event", async ()=>{
         const collectionObject = new tables.CollectionObject.Resource({
             id: 1
         });
         const { result } = renderHook(()=>useIsModified(collectionObject));
         expect(result.current).toBe(false);
-        // Here, we aren't concerned with the source of the "saveRequired" (for all we know, something triggered it)
-        // So, manually trigger that event, instead of using .set()
+        /*
+         * Here, we aren't concerned with the source of the "saveRequired" (for all we know, something triggered it)
+         * So, manually trigger that event, instead of using .set()
+         */
         await act(()=>collectionObject.trigger("saverequired"));
         // The event-handler doesn't necessarily run immediately. So, need a waitFor.
         
@@ -70,7 +73,7 @@ describe("useIsModifiedTest", ()=>{
         });
     });
 
-    it("handles saved event", async ()=>{
+    test("handles saved event", async ()=>{
         const collectionObject = new tables.CollectionObject.Resource({
             id: 1
         });
