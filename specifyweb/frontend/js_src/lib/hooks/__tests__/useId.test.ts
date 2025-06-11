070a7d12cb6bf6e1ca2f4c35181f11f3d65ef0e2
realVinayak <realvinayak123@gmail.com>
2025-06-11 23:24:09 +0000

    Lint code with ESLint and Prettier
    Triggered by 03f65b6757a7a060e4c99152264cff64fb5aaf5b on branch refs/heads/issue-6632

--- specifyweb/frontend/js_src/lib/hooks/__tests__/useId.test.ts
+++ specifyweb/frontend/js_src/lib/hooks/__tests__/useId.test.ts
@@ -1,14 +1,17 @@
-import { renderHook, act } from "@testing-library/react"
+import { act,renderHook } from "@testing-library/react"
+
 import { useId } from "../useId"
 
 describe("useId", ()=>{
 
-    it("generates stable prefix", async ()=>{
+    test("generates stable prefix", async ()=>{
 
         const { result, rerender } = renderHook(()=>useId("test"));
 
-        // NOTES: asserting that the callback is stable is not good.
-        // Instead, just the stability of result is considered.
+        /*
+         * NOTES: asserting that the callback is stable is not good.
+         * Instead, just the stability of result is considered.
+         */
 
         // TODO: Technically, there should be a cleanup function where idStore gets cleaned up.
         expect(result.current("suffix")).toBe("test-0-suffix");
@@ -30,7 +33,7 @@ describe("useId", ()=>{
 
     });
 
-    it("handles empty suffix", async ()=>{
+    test("handles empty suffix", async ()=>{
         const { result } = renderHook(()=>useId("brand_new_prefix"));
         expect(result.current("")).toBe("brand_new_prefix-0");
     });
