a61c944fb33e61b6942d84b011ec1933e97ddaec
Google Translate <noreply-mt-google-translate@weblate.org>
2025-06-13 17:58:57 +0000

    Sync localization strings with Weblate
    Triggered by 667226ea406e7d84d04b2b63dddc6cadb160035e on branch refs/heads/weblate-localization

--- specifyweb/frontend/js_src/lib/localization/development.ts
+++ specifyweb/frontend/js_src/lib/localization/development.ts
@@ -5,36 +5,36 @@
  * @module
  */
 
-import { createDictionary } from './utils';
+import { createDictionary } from "./utils";
 
 // Refer to "Guidelines for Programmers" in ./README.md before editing this file
 
 export const developmentText = createDictionary({
   crashReportVisualizer: {
-    'en-us': 'Crash Report Visualizer',
-    'es-es': 'Visualizador los informes de errores',
-    'fr-fr': "Visualiseur de rapport d'incident",
-    'uk-ua': 'Візуалізатор звітів про збої',
-    'de-ch': 'Crash Report Visualizer',
-    'ru-ru': 'Визуализатор отчетов о сбоях',
-    'pt-br': 'Visualizador de Relatório de Falhas',
+    "en-us": "Crash Report Visualizer",
+    "es-es": "Visualizador los informes de errores",
+    "fr-fr": "Visualiseur de rapport d'incident",
+    "uk-ua": "Візуалізатор звітів про збої",
+    "de-ch": "Crash Report Visualizer",
+    "ru-ru": "Визуализатор отчетов о сбоях",
+    "pt-br": "Visualizador de Relatório de Falhas",
   },
   downloadAsHtml: {
-    'en-us': 'Download as HTML',
-    'es-es': 'Descargar en formato HTML',
-    'fr-fr': 'Télécharger au format HTML',
-    'ru-ru': 'Скачать как HTML',
-    'uk-ua': 'Завантажити як HTML',
-    'de-ch': 'Als HTML-Datei herunterladen',
-    'pt-br': 'Baixar como HTML',
+    "en-us": "Download as HTML",
+    "es-es": "Descargar en formato HTML",
+    "fr-fr": "Télécharger au format HTML",
+    "ru-ru": "Скачать как HTML",
+    "uk-ua": "Завантажити як HTML",
+    "de-ch": "Als HTML-Datei herunterladen",
+    "pt-br": "Baixar como HTML",
   },
   details: {
-    'en-us': 'Details',
-    'es-es': 'Más información',
-    'fr-fr': 'Informations',
-    'ru-ru': 'Подробности',
-    'uk-ua': 'Подробиці',
-    'de-ch': 'Details',
-    'pt-br': 'Detalhes',
+    "en-us": "Details",
+    "es-es": "Más información",
+    "fr-fr": "Informations",
+    "ru-ru": "Подробности",
+    "uk-ua": "Подробиці",
+    "de-ch": "Details",
+    "pt-br": "Detalhes",
   },
 } as const);
