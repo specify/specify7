f68a92d40d35a53f2025d32e524c81b50d930bef
realVinayak <realvinayak123@gmail.com>
2025-06-16 12:53:37 -0500

    (tests): Add tests for useParser

---
+++ specifyweb/frontend/js_src/lib/hooks/__tests__/useParser.test.tsx
@@ -0,0 +1,100 @@
+import { act, renderHook } from "@testing-library/react";
+import React from "react";
+
+import { SearchDialogContext } from "../../components/Core/Contexts";
+import { mount } from "../../tests/reactUtils";
+import { tables } from "../../components/DataModel/tables";
+import { useParser } from "../resource";
+import { requireContext } from "../../tests/helpers";
+import { Parser } from "../../utils/parser/definitions";
+
+
+requireContext();
+
+describe("useParser", () => {
+
+    function TestInSearchDialog({ field, resource, defaultParser, callback }: {
+        // Done this way so that, in future, tests don't need to be modified that much.
+        readonly field: Parameters<typeof useParser>[0],
+        readonly resource?: Parameters<typeof useParser>[1],
+        readonly defaultParser?: Parameters<typeof useParser>[2]
+        readonly callback: (parser: Parser) => void
+    }) {
+
+        const parser = useParser(field, resource, defaultParser);
+
+        React.useEffect(() => {
+            callback(parser);
+        }, [parser]);
+
+        return <></>
+    }
+
+    test("simple formatter gets set", async () => {
+        const collectionObject = new tables.CollectionObject.Resource({ id: 1 });
+        const numberField = tables.CollectionObject.strictGetField(
+            "number1"
+        );
+
+        let defaultParser: Parser | undefined = undefined;
+
+        const { result, rerender } = renderHook(() => useParser(numberField, collectionObject, defaultParser));
+
+        expect(result.current.type).toBe("number");
+
+        defaultParser = {
+            type: 'number',
+            step: 3,
+            required: false
+        };
+
+        await act(rerender);
+
+        expect(result.current.type).toBe("number");
+        expect(result.current.step).toBe(3);
+        expect(result.current.required).toBe(false);
+
+    });
+
+    test("formatter in simple search is always text", () => {
+        const numberField = tables.CollectionObject.strictGetField(
+            "number1"
+        );
+
+        const onParserSet = jest.fn();
+
+        mount(
+            <SearchDialogContext.Provider value>
+                <TestInSearchDialog field={numberField} callback={onParserSet} />
+            </SearchDialogContext.Provider>
+        );
+
+        expect(onParserSet).toBeCalled();
+        expect(onParserSet.mock.calls.at(-1).at(0)).toEqual({ type: "text" });
+
+    });
+
+    test("field is relationship", () => {
+        const collectionObject = new tables.CollectionObject.Resource({ id: 1 });
+        const collectingEvent = tables.CollectionObject.strictGetField(
+            "collectingevent"
+        );
+
+        const { result } = renderHook(() => useParser(collectingEvent, collectionObject));
+
+        expect(result.current).toEqual({ type: 'text', required: false });
+
+    });
+
+    test("field is undefined", () => {
+        const collectionObject = new tables.CollectionObject.Resource({ id: 1 });
+
+        const { result } = renderHook(() => useParser(undefined, collectionObject));
+
+        expect(result.current).toEqual({ type: 'text' });
+
+    });
+
+
+
+});
