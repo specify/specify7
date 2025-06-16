81329d617370aca184228818c6c77f6f840d2045
realVinayak <realvinayak123@gmail.com>
2025-06-16 10:01:19 -0500

    (tests): Add more usePromise test + skip broken state set

--- specifyweb/frontend/js_src/lib/hooks/__tests__/usePromise.test.ts
+++ specifyweb/frontend/js_src/lib/hooks/__tests__/usePromise.test.ts
@@ -1,10 +1,10 @@
-import { act,renderHook, waitFor } from "@testing-library/react";
+import { act, renderHook, waitFor } from "@testing-library/react";
 
 import { usePromise } from "../useAsyncState";
 
 describe("usePromise", ()=>{
 
-    test.skip("promise gets resolved and state set", async ()=>{
+    test("promise gets resolved and state set", async ()=>{
 
         const {result} = renderHook(()=>usePromise(Promise.resolve(10), false));
 
@@ -12,9 +12,30 @@ describe("usePromise", ()=>{
             expect(result.current[0]).toBe(10);
         });
 
-        await act(()=>result.current[1](11));
+        // TODO: Investigate why the below crashes the environment.
+        // I think the it is related to a previous bug in useMultipleAsyncState
+        // await act(()=>result.current[1](11));
 
-        expect(result.current[0]).toBe(11);
+        // expect(result.current[0]).toBe(11);
+
+    });
+
+    test("state changes when promise changes", async ()=>{
+
+        let promise = Promise.resolve(10);
+
+        const {result, rerender} = renderHook(()=>usePromise(promise, false));
+
+        waitFor(()=>{
+            expect(result.current[0]).toBe(10);
+        });
+
+        promise = Promise.resolve(11);
+        act(rerender);
+
+        waitFor(()=>{
+            expect(result.current[0]).toBe(11);
+        });
 
     });
 });
