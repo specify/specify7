307a1103c56e37723be61cf776eacada2367fef1
Caroline D. <108160931+CarolineDenis@users.noreply.github.com>
2025-06-16 15:36:26 +0000

    Lint code with ESLint and Prettier
    Triggered by 389028563cd9df2cbcd1484c4a1469a69f85a543 on branch refs/heads/main

--- specifyweb/frontend/js_src/lib/hooks/__tests__/usePromise.test.ts
+++ specifyweb/frontend/js_src/lib/hooks/__tests__/usePromise.test.ts
@@ -1,41 +1,36 @@
-import { act, renderHook, waitFor } from "@testing-library/react";
+import { act, renderHook, waitFor } from '@testing-library/react';
 
-import { usePromise } from "../useAsyncState";
+import { usePromise } from '../useAsyncState';
 
-describe("usePromise", ()=>{
+describe('usePromise', () => {
+  test('promise gets resolved and state set', async () => {
+    const { result } = renderHook(() => usePromise(Promise.resolve(10), false));
 
-    test("promise gets resolved and state set", async ()=>{
-
-        const {result} = renderHook(()=>usePromise(Promise.resolve(10), false));
-
-        waitFor(()=>{
-            expect(result.current[0]).toBe(10);
-        });
-
-        // TODO: Investigate why the below crashes the environment.
-        // I think the it is related to a previous bug in useMultipleAsyncState
-        // await act(()=>result.current[1](11));
-
-        // expect(result.current[0]).toBe(11);
-
+    waitFor(() => {
+      expect(result.current[0]).toBe(10);
     });
 
-    test("state changes when promise changes", async ()=>{
+    // TODO: Investigate why the below crashes the environment.
+    // I think the it is related to a previous bug in useMultipleAsyncState
+    // await act(()=>result.current[1](11));
 
-        let promise = Promise.resolve(10);
+    // expect(result.current[0]).toBe(11);
+  });
 
-        const {result, rerender} = renderHook(()=>usePromise(promise, false));
+  test('state changes when promise changes', async () => {
+    let promise = Promise.resolve(10);
 
-        waitFor(()=>{
-            expect(result.current[0]).toBe(10);
-        });
+    const { result, rerender } = renderHook(() => usePromise(promise, false));
 
-        promise = Promise.resolve(11);
-        act(rerender);
-
-        waitFor(()=>{
-            expect(result.current[0]).toBe(11);
-        });
-
+    waitFor(() => {
+      expect(result.current[0]).toBe(10);
     });
+
+    promise = Promise.resolve(11);
+    act(rerender);
+
+    waitFor(() => {
+      expect(result.current[0]).toBe(11);
+    });
+  });
 });
