12e0798a4cf1684690202922af01d51fe71b9932
realVinayak <realvinayak123@gmail.com>
2025-06-16 17:00:01 +0000

    Lint code with ESLint and Prettier
    Triggered by 660ef2dc85ea7247452fa254d62b5536d4c6533c on branch refs/heads/issue-6654

--- specifyweb/frontend/js_src/lib/hooks/__tests__/usePromise.test.ts
+++ specifyweb/frontend/js_src/lib/hooks/__tests__/usePromise.test.ts
@@ -2,22 +2,19 @@ import { act, renderHook, waitFor } from '@testing-library/react';
 
 import { usePromise } from '../useAsyncState';
 
-describe("usePromise", ()=>{
+describe('usePromise', () => {
+  test('promise gets resolved and state set', async () => {
+    const promise = Promise.resolve(10);
 
-    test("promise gets resolved and state set", async ()=>{
+    const { result } = renderHook(() => usePromise(promise, false));
 
-        const promise = Promise.resolve(10);
+    await waitFor(() => {
+      expect(result.current[0]).toBe(10);
+    });
 
-        const {result } = renderHook(()=>usePromise(promise, false));
+    await act(() => result.current[1](11));
 
-        await waitFor(()=>{
-            expect(result.current[0]).toBe(10);
-        });
-
-        await act(()=>result.current[1](11));
-
-        expect(result.current[0]).toBe(11);
-
+    expect(result.current[0]).toBe(11);
   });
 
   test('state changes when promise changes', async () => {
@@ -36,7 +33,5 @@ describe("usePromise", ()=>{
     await waitFor(() => {
       expect(result.current[0]).toBe(11);
     });
-
   });
-
 });
