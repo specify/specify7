e76e28cda7ec3a0383546adee5992d65acfa49a5
realVinayak <realvinayak123@gmail.com>
2025-06-17 18:37:44 -0500

    (tests): Simplify tests

--- specifyweb/interactions/tests/test_is_consolidated_cog.py
+++ specifyweb/interactions/tests/test_is_consolidated_cog.py
@@ -1,25 +1,12 @@
-from specifyweb.specify.tests.test_api import DefaultsSetup
 from specifyweb.interactions.cog_preps import is_consolidated_cog
-from specifyweb.specify.models import Collectionobjectgroup, Collectionobjectgrouptype
+from specifyweb.interactions.tests.test_cog import TestCogInteractions
 
 
-class TestIsConsolidatedCog(DefaultsSetup):
+class TestIsConsolidatedCog(TestCogInteractions):
 
     def test_not_consolidated_on_empty_cog(self):
         self.assertFalse(is_consolidated_cog(None))
 
     def test_consolidated_correctly_inferred(self):
-        cog_consolidated = Collectionobjectgroup.objects.create(
-            collection=self.collection,
-            cogtype=Collectionobjectgrouptype.objects.create(
-                name="microscope slide", type="Consolidated", collection=self.collection
-            ),
-        )
-        cog_discrete = Collectionobjectgroup.objects.create(
-            collection=self.collection,
-            cogtype=Collectionobjectgrouptype.objects.create(
-                name="microscope slide", type="Discrete", collection=self.collection
-            ),
-        )
-        self.assertTrue(is_consolidated_cog(cog_consolidated))
-        self.assertFalse(is_consolidated_cog(cog_discrete))
+        self.assertTrue(is_consolidated_cog(self.test_cog_consolidated))
+        self.assertFalse(is_consolidated_cog(self.test_cog_discrete))
