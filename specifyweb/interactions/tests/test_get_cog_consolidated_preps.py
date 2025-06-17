aad79b69118f619abc9a49ed87020a010b973d7e
realVinayak <realvinayak123@gmail.com>
2025-06-17 18:34:51 -0500

    (tests): Simplify test logic

--- specifyweb/interactions/tests/test_get_cog_consolidated_preps.py
+++ specifyweb/interactions/tests/test_get_cog_consolidated_preps.py
@@ -1,58 +1,14 @@
+from specifyweb.interactions.tests.test_cog import TestCogInteractions
 from specifyweb.specify.tests.test_api import DefaultsSetup
 from specifyweb.interactions.cog_preps import get_cog_consolidated_preps
 from specifyweb.specify.models import (
     Collectionobjectgroup,
-    Collectionobjectgrouptype,
     Collectionobjectgroupjoin,
-    Preptype,
     Preparation,
 )
 
 
-class TestGetCogConsolidatedPreps(DefaultsSetup):
-
-    def setUp(self):
-        super().setUp()
-        self.cogtype_consolidated = Collectionobjectgrouptype.objects.create(
-            name="TestConsolidated", type="Consolidated", collection=self.collection
-        )
-        self.prep_type = Preptype.objects.create(
-            name="testPrepType",
-            isloanable=False,
-            collection=self.collection,
-        )
-
-        self.test_cog_consolidated = Collectionobjectgroup.objects.create(
-            collection=self.collection,
-            cogtype=self.cogtype_consolidated,
-        )
-
-        self.test_cog_discrete = Collectionobjectgroup.objects.create(
-            collection=self.collection,
-            cogtype=self.cogtype,
-        )
-
-    @staticmethod
-    def _link_co_cog(co, cog):
-        Collectionobjectgroupjoin.objects.create(
-            parentcog=cog, childco=co, isprimary=True, issubstrate=True
-        )
-
-    @staticmethod
-    def _link_cog_cog(child_cog, cog):
-        Collectionobjectgroupjoin.objects.create(
-            parentcog=cog, childcog=child_cog, isprimary=True, issubstrate=True
-        )
-
-    def _create_prep(self, co, prep_list):
-        prep = Preparation.objects.create(collectionobject=co, preptype=self.prep_type)
-        if prep_list is not None:
-            prep_list.append(prep)
-
-    def _preps_match(self, base, computed):
-        self.assertCountEqual(
-            [prep.id for prep in base], [prep.id for prep in computed]
-        )
+class TestGetCogConsolidatedPreps(TestCogInteractions):
 
     def test_no_preparations_on_none_consolidated(self):
         self.assertEqual(get_cog_consolidated_preps(self.test_cog_discrete), [])
