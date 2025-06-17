5071b9bd91b596b682e9fb766eb657bb25ed7cc8
realVinayak <realvinayak123@gmail.com>
2025-06-17 15:01:34 -0500

    (tests): Add unit tests for unsetUnloadProtect

---
+++ specifyweb/frontend/js_src/lib/hooks/__tests__/unsetUnloadProtect.test.ts
@@ -0,0 +1,22 @@
+import { localized, RA } from "../../utils/types"
+import { unsetUnloadProtect } from "../navigation";
+
+describe("unsetUnloadProtect", ()=>{
+    test("removes error message", ()=>{
+        let errorMessages: RA<string> = ["custom error message",  "custom error message 2 (should not be removed)"];
+        const setUnloadProtects = (generator: ((old: RA<string>) => RA<string>) | RA<string>) => {
+            errorMessages = typeof generator === 'function' ? generator(errorMessages) : generator;
+        }
+        unsetUnloadProtect(setUnloadProtects, localized("custom error message"));
+        expect(errorMessages).toEqual(["custom error message 2 (should not be removed)"]);
+    });
+
+    test("returns same unload protects if message not found", ()=>{
+        let errorMessages: RA<string> = ["custom error message",  "custom error message 2 (should not be removed)"];
+        const setUnloadProtects = (generator: ((old: RA<string>) => RA<string>) | RA<string>) => {
+            errorMessages = typeof generator === 'function' ? generator(errorMessages) : generator;
+        }
+        unsetUnloadProtect(setUnloadProtects, localized("custom error message, does not exist"));
+        expect(errorMessages).toEqual(["custom error message",  "custom error message 2 (should not be removed)"]); 
+    });
+})
