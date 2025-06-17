ad7cd5cdaecd709cbfe5512179f749144306773f
realVinayak <realvinayak123@gmail.com>
2025-06-17 20:05:58 +0000

    Lint code with ESLint and Prettier
    Triggered by 5071b9bd91b596b682e9fb766eb657bb25ed7cc8 on branch refs/heads/issue-6675

--- specifyweb/frontend/js_src/lib/hooks/__tests__/unsetUnloadProtect.test.ts
+++ specifyweb/frontend/js_src/lib/hooks/__tests__/unsetUnloadProtect.test.ts
@@ -1,10 +1,11 @@
-import { localized, RA } from "../../utils/types"
+import type { RA } from "../../utils/types";
+import { localized } from "../../utils/types"
 import { unsetUnloadProtect } from "../navigation";
 
 describe("unsetUnloadProtect", ()=>{
     test("removes error message", ()=>{
         let errorMessages: RA<string> = ["custom error message",  "custom error message 2 (should not be removed)"];
-        const setUnloadProtects = (generator: ((old: RA<string>) => RA<string>) | RA<string>) => {
+        const setUnloadProtects = (generator: RA<string> | ((old: RA<string>) => RA<string>)) => {
             errorMessages = typeof generator === 'function' ? generator(errorMessages) : generator;
         }
         unsetUnloadProtect(setUnloadProtects, localized("custom error message"));
@@ -13,7 +14,7 @@ describe("unsetUnloadProtect", ()=>{
 
     test("returns same unload protects if message not found", ()=>{
         let errorMessages: RA<string> = ["custom error message",  "custom error message 2 (should not be removed)"];
-        const setUnloadProtects = (generator: ((old: RA<string>) => RA<string>) | RA<string>) => {
+        const setUnloadProtects = (generator: RA<string> | ((old: RA<string>) => RA<string>)) => {
             errorMessages = typeof generator === 'function' ? generator(errorMessages) : generator;
         }
         unsetUnloadProtect(setUnloadProtects, localized("custom error message, does not exist"));
