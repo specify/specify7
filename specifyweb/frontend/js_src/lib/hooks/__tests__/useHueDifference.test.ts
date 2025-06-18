6b3d7f10a32ddcf83faf75be6a2f8613010ac771
Grant Fitzsimmons <37256050+grantfitzsimmons@users.noreply.github.com>
2025-06-18 18:08:53 +0000

    Lint code with ESLint and Prettier
    Triggered by ccaaf8a3d7f7f6ff8534037b8cbd267d5920caaa on branch refs/heads/issue-6644

--- specifyweb/frontend/js_src/lib/hooks/__tests__/useHueDifference.test.ts
+++ specifyweb/frontend/js_src/lib/hooks/__tests__/useHueDifference.test.ts
@@ -1,20 +1,22 @@
-import { renderHook } from "@testing-library/react";
+import { renderHook } from '@testing-library/react';
 
-import { userPreferences } from "../../components/Preferences/userPreferences";
-import { useHueDifference } from "../useHueDifference";
+import { userPreferences } from '../../components/Preferences/userPreferences';
+import { useHueDifference } from '../useHueDifference';
 
-describe("useHueDifference", ()=>{
-
-    test("hue difference is correct", async ()=>{
-        userPreferences.setRaw({'general': {appearance: {accentColor3: "#1a9cff"}}})
-        const { result } = renderHook(()=>useHueDifference());
-        expect(result.current).toBe(179);
+describe('useHueDifference', () => {
+  test('hue difference is correct', async () => {
+    userPreferences.setRaw({
+      general: { appearance: { accentColor3: '#1a9cff' } },
     });
+    const { result } = renderHook(() => useHueDifference());
+    expect(result.current).toBe(179);
+  });
 
-    test("hue difference is adjusted when difference is negative", async ()=>{
-        userPreferences.setRaw({'general': {appearance: {accentColor3: '#ff1a1a'}}})
-        const { result } = renderHook(()=>useHueDifference());
-        expect(result.current).toBe(333);
+  test('hue difference is adjusted when difference is negative', async () => {
+    userPreferences.setRaw({
+      general: { appearance: { accentColor3: '#ff1a1a' } },
     });
-
-})
+    const { result } = renderHook(() => useHueDifference());
+    expect(result.current).toBe(333);
+  });
+});
