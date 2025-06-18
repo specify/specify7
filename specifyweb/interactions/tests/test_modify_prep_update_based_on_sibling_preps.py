44a3ba741caf5f2fc5525fe3bf3d2bdaa43ac7e7
realVinayak <realvinayak123@gmail.com>
2025-06-18 18:30:13 -0500

    (tests): Slight refactor to prevent dup in tests

---
+++ specifyweb/interactions/tests/test_modify_prep_update_based_on_sibling_preps.py
@@ -0,0 +1,8 @@
+from specifyweb.interactions.tests.test_cog_consolidated_prep_sibling_context import (
+    TestCogConsolidatedPrepSiblingContext,
+)
+
+
+class TestModifyPrepUpdateBasedOnSiblingPreps(
+    TestCogConsolidatedPrepSiblingContext
+): ...
