f8d5e9219f875a75c979d24f80515690e1c586ea
realVinayak <realvinayak123@gmail.com>
2025-06-17 19:44:32 -0500

    (tests): Add tests for get_all_sibling_preps_within_consolidated_cog

---
+++ specifyweb/interactions/tests/test_get_all_sibling_preps_within_consolidated_cog.py
@@ -0,0 +1,124 @@
+from specifyweb.interactions.cog_preps import (
+    get_all_sibling_preps_within_consolidated_cog,
+)
+from specifyweb.interactions.tests.test_cog import TestCogInteractions
+from specifyweb.specify.models import (
+    Collectionobjectgroupjoin,
+    Preparation,
+    Collectionobjectgroup,
+)
+from unittest import skip
+
+
+class TestGetSiblingPrepsWithinConsolidatedCog(TestCogInteractions):
+
+    def setUp(self):
+        super().setUp()
+        Collectionobjectgroupjoin.objects.all().delete()
+        Preparation.objects.all().delete()
+
+    def test_none_preparation(self):
+        self.assertEqual(get_all_sibling_preps_within_consolidated_cog(None), [])
+
+    def test_discrete_cog_parent(self):
+        prep_list = []
+        for num_preps, co in enumerate(self.collectionobjects):
+            self._create_prep(co, prep_list)
+            TestGetSiblingPrepsWithinConsolidatedCog._link_co_cog(
+                co, self.test_cog_discrete
+            )
+        for prep in prep_list:
+            self.assertEqual(get_all_sibling_preps_within_consolidated_cog(prep), [])
+
+    def test_consolidated_cog_parent_simple(self):
+        prep_list = []
+        for num_preps, co in enumerate(self.collectionobjects):
+            self._create_prep(co, prep_list)
+            TestGetSiblingPrepsWithinConsolidatedCog._link_co_cog(
+                co, self.test_cog_consolidated
+            )
+        for prep in prep_list:
+            # It should always return the same list.
+            self._preps_match(
+                prep_list, get_all_sibling_preps_within_consolidated_cog(prep)
+            )
+
+    @skip("currently skipped")
+    def test_consolidated_cog_parent_indirect(self):
+        # self.test_cog_consolidated is the "top"
+        # TODO: Write some kind of automated way to make the COG and CO structure.
+        # Perhaps be able to represent the tree in json, and these tests will automatically create
+        # records.
+
+        prep_list = []
+        for _ in range(5):
+            self._create_prep(self.collectionobjects[0], prep_list)
+
+        for _ in range(5):
+            self._create_prep(self.collectionobjects[1], None)
+
+        TestGetSiblingPrepsWithinConsolidatedCog._link_co_cog(
+            self.collectionobjects[0], self.test_cog_consolidated
+        )
+
+        TestGetSiblingPrepsWithinConsolidatedCog._link_cog_cog(
+            self.test_cog_discrete, self.test_cog_consolidated
+        )
+
+        TestGetSiblingPrepsWithinConsolidatedCog._link_co_cog(
+            self.collectionobjects[1], self.test_cog_discrete
+        )
+
+        cog_consolidated_grandchild = Collectionobjectgroup.objects.create(
+            collection=self.collection,
+            cogtype=self.cogtype_consolidated,
+        )
+
+        TestGetSiblingPrepsWithinConsolidatedCog._link_co_cog(
+            self.collectionobjects[2], cog_consolidated_grandchild
+        )
+
+        TestGetSiblingPrepsWithinConsolidatedCog._link_cog_cog(
+            cog_consolidated_grandchild, self.test_cog_consolidated
+        )
+
+        for _ in range(5):
+            self._create_prep(self.collectionobjects[2], prep_list)
+
+        cog_consolidated_greatgrandchild = Collectionobjectgroup.objects.create(
+            collection=self.collection,
+            cogtype=self.cogtype_consolidated,
+        )
+
+        TestGetSiblingPrepsWithinConsolidatedCog._link_co_cog(
+            self.collectionobjects[3], cog_consolidated_greatgrandchild
+        )
+
+        TestGetSiblingPrepsWithinConsolidatedCog._link_cog_cog(
+            cog_consolidated_greatgrandchild, cog_consolidated_grandchild
+        )
+
+        for _ in range(5):
+            self._create_prep(self.collectionobjects[3], prep_list)
+
+        cog_discrete_grandchild = Collectionobjectgroup.objects.create(
+            collection=self.collection,
+            cogtype=self.cogtype_consolidated,
+        )
+
+        TestGetSiblingPrepsWithinConsolidatedCog._link_co_cog(
+            self.collectionobjects[4], cog_discrete_grandchild
+        )
+
+        TestGetSiblingPrepsWithinConsolidatedCog._link_cog_cog(
+            cog_discrete_grandchild, cog_consolidated_greatgrandchild
+        )
+
+        for _ in range(5):
+            self._create_prep(self.collectionobjects[4], None)
+
+        for prep in prep_list:
+            # It should always return the same list.
+            self._preps_match(
+                prep_list, get_all_sibling_preps_within_consolidated_cog(prep)
+            )
