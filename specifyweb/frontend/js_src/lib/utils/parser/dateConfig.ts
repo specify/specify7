51fb7db38c1288067482eb214ed2aff9924f9c86
alesan99 <alesanesp99@gmail.com>
2025-06-18 13:44:02 -0500

    Make date format consistent

--- specifyweb/frontend/js_src/lib/utils/parser/dateConfig.ts
+++ specifyweb/frontend/js_src/lib/utils/parser/dateConfig.ts
@@ -1,5 +1,6 @@
 /**
  * See also FormPlugins/useDatePreferences.tsx
+ * 
+ * Once timezone support is added to the backend, this should be 'YYYY-MM-DDTHH:mm:ssZ'
  */
-
-export const databaseDateFormat = 'YYYY-MM-DD HH:mm:ss.SSS';
+export const databaseDateFormat = 'YYYY-MM-DDTHH:mm:ss';
