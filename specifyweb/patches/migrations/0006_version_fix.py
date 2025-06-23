7cafc1f9e4896c58a7e95d8408a2af3696fe7ad1
Grant Fitzsimmons <37256050+grantfitzsimmons@users.noreply.github.com>
2025-06-23 16:25:10 -0500

    Adds 0 as a default at DB level

--- specifyweb/patches/migrations/0006_version_fix.py
+++ specifyweb/patches/migrations/0006_version_fix.py
@@ -1,6 +1,6 @@
 from django.db import migrations, connection
 
-def set_null_versions_to_zero(apps, schema_editor):
+def set_null_versions_to_zero_and_default(apps, schema_editor):
     with connection.cursor() as cursor:
         cursor.execute("""
             SELECT table_name
@@ -10,7 +10,10 @@ def set_null_versions_to_zero(apps, schema_editor):
         tables = cursor.fetchall()
 
         for (table_name,) in tables:
+            # Set all NULL values to 0
             cursor.execute(f"UPDATE `{table_name}` SET `version` = 0 WHERE `version` IS NULL;")
+            # Set default to 0 at the DB level
+            cursor.execute(f"ALTER TABLE `{table_name}` MODIFY `version` INT DEFAULT 0;")
 
 class Migration(migrations.Migration):
 
@@ -19,5 +22,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(set_null_versions_to_zero),
+        migrations.RunPython(set_null_versions_to_zero_and_default),
     ]
