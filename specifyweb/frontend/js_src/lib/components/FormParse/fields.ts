87f7729c6808e7712e4aab54d0ef077c7fd73c3b
Grant Fitzsimmons <37256050+grantfitzsimmons@users.noreply.github.com>
2025-06-24 15:54:29 -0500

    Updates form comments

--- specifyweb/frontend/js_src/lib/components/FormParse/fields.ts
+++ specifyweb/frontend/js_src/lib/components/FormParse/fields.ts
@@ -1,7 +1,7 @@
 /**
  * Parse XML cell with a field definition into JSON
  *
- * Documentation - https://github.com/specify/specify7/wiki/Form-System#field
+ * Documentation - https://discourse.specifysoftware.org/t/editing-forms-in-specify/1557#p-2936-field-31
  * On any modifications, please check if documentation needs to be updated.
  */
 
