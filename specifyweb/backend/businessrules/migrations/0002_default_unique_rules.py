b4a9fc6da2233659c3e45404a584eccabb7d4452
pashiav <pashiavang2022@gmail.com>
2025-06-25 12:04:36 -0500

    update businessrules import paths for apps and migration 0002

--- specifyweb/backend/businessrules/migrations/0002_default_unique_rules.py
+++ specifyweb/backend/businessrules/migrations/0002_default_unique_rules.py
@@ -1,7 +1,7 @@
 from django.db import migrations
 
 from specifyweb.specify.datamodel import datamodel
-from specifyweb.businessrules.uniqueness_rules import apply_default_uniqueness_rules, rule_is_global, DEFAULT_UNIQUENESS_RULES
+from specifyweb.backend.businessrules.uniqueness_rules import apply_default_uniqueness_rules, rule_is_global, DEFAULT_UNIQUENESS_RULES
 
 
 def apply_default_rules(apps, schema_editor):
