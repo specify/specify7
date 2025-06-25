ed93892b8d2ada317f2a6a15c5c55e01d500fd15
Caroline D. <108160931+CarolineDenis@users.noreply.github.com>
2025-06-25 19:47:22 +0000

    Lint code with ESLint and Prettier
    Triggered by d0a651494f38ba2e6720ae17849eabe95926779c on branch refs/heads/main

--- specifyweb/frontend/js_src/lib/components/Attachments/__tests__/fetchOriginalUrl.test.ts
+++ specifyweb/frontend/js_src/lib/components/Attachments/__tests__/fetchOriginalUrl.test.ts
@@ -1,60 +1,61 @@
-import { overrideAjax } from "../../../tests/ajax";
-import attachmentSettings from "../../../tests/ajax/static/context/attachment_settings.json"
-import { requireContext } from "../../../tests/helpers";
-import { serializeResource } from "../../DataModel/serializers";
-import { tables } from "../../DataModel/tables";
-import { formatUrl } from "../../Router/queryString";
-import { fetchOriginalUrl, overrideAttachmentSettings } from "../attachments";
-
+import { overrideAjax } from '../../../tests/ajax';
+import attachmentSettings from '../../../tests/ajax/static/context/attachment_settings.json';
+import { requireContext } from '../../../tests/helpers';
+import { serializeResource } from '../../DataModel/serializers';
+import { tables } from '../../DataModel/tables';
+import { formatUrl } from '../../Router/queryString';
+import { fetchOriginalUrl, overrideAttachmentSettings } from '../attachments';
 
 requireContext();
 
-describe("fetchOriginalUrl", ()=>{
+describe('fetchOriginalUrl', () => {
+  const attachmentLocation = 'testLocation';
 
-    const attachmentLocation = "testLocation";
+  const mimeType = 'image/jpeg';
+  const fileName = 'testFile.jpg';
+  const testToken = 'testToken';
 
-    const mimeType = 'image/jpeg';
-    const fileName = 'testFile.jpg';
-    const testToken = "testToken";
+  test('url when token not required for get', async () => {
+    expect(attachmentSettings.token_required_for_get).toBe(false);
 
-    test("url when token not required for get", async ()=>{
-
-        expect(attachmentSettings.token_required_for_get).toBe(false);
-
-        const attachment = new tables.Attachment.Resource({
-            attachmentlocation: attachmentLocation,
-            mimetype: mimeType,
-            origfilename: "C:\\testSlash.jpg",
-            title: fileName,
-            isPublic: true
-        });
-
-        const url = await fetchOriginalUrl(serializeResource(attachment));
-
-        expect(url).toBe("http://host.docker.internal/fileget?coll=KU+Fish+Voucher+Collection&type=O&filename=testLocation&downloadname=testSlash.jpg");
-
+    const attachment = new tables.Attachment.Resource({
+      attachmentlocation: attachmentLocation,
+      mimetype: mimeType,
+      origfilename: 'C:\\testSlash.jpg',
+      title: fileName,
+      isPublic: true,
     });
 
-    overrideAjax(
-        formatUrl('/attachment_gw/get_token/', { filename: attachmentLocation }), 
-        testToken
+    const url = await fetchOriginalUrl(serializeResource(attachment));
+
+    expect(url).toBe(
+      'http://host.docker.internal/fileget?coll=KU+Fish+Voucher+Collection&type=O&filename=testLocation&downloadname=testSlash.jpg'
     );
+  });
 
-    test("url when token is required for get", async ()=>{
+  overrideAjax(
+    formatUrl('/attachment_gw/get_token/', { filename: attachmentLocation }),
+    testToken
+  );
 
-        overrideAttachmentSettings({...attachmentSettings, token_required_for_get: true});
-
-        const attachment = new tables.Attachment.Resource({
-            attachmentlocation: attachmentLocation,
-            mimetype: mimeType,
-            origfilename: "C:\\testSlash.jpg",
-            title: fileName,
-            isPublic: true
-        });
-
-        const url = await fetchOriginalUrl(serializeResource(attachment));
-
-        expect(url).toBe("http://host.docker.internal/fileget?coll=KU+Fish+Voucher+Collection&type=O&filename=testLocation&downloadname=testSlash.jpg&token=testToken");
+  test('url when token is required for get', async () => {
+    overrideAttachmentSettings({
+      ...attachmentSettings,
+      token_required_for_get: true,
     });
 
+    const attachment = new tables.Attachment.Resource({
+      attachmentlocation: attachmentLocation,
+      mimetype: mimeType,
+      origfilename: 'C:\\testSlash.jpg',
+      title: fileName,
+      isPublic: true,
+    });
+
+    const url = await fetchOriginalUrl(serializeResource(attachment));
+
+    expect(url).toBe(
+      'http://host.docker.internal/fileget?coll=KU+Fish+Voucher+Collection&type=O&filename=testLocation&downloadname=testSlash.jpg&token=testToken'
+    );
+  });
 });
