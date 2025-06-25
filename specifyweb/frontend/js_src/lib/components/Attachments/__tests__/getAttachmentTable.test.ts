2c84e4aec967b85d49fe1af37f1a4841bebd543b
realVinayak <realvinayak123@gmail.com>
2025-06-25 14:21:57 -0500

    (tests): Begin adding tests for getAttachmentTable

---
+++ specifyweb/frontend/js_src/lib/components/Attachments/__tests__/getAttachmentTable.test.ts
@@ -0,0 +1,17 @@
+import { requireContext } from "../../../tests/helpers";
+import { tables } from "../../DataModel/tables";
+import { getAttachmentTable } from "../Cell";
+
+requireContext();
+
+describe("getAttachmentTable", ()=>{
+
+    test.skip("attachment table", ()=>{
+        expect(getAttachmentTable(tables.Accession.tableId)).toBe(tables.Accession);
+    });
+
+    test.skip("non-attachment table", ()=>{
+        expect(getAttachmentTable(tables.Division.tableId)).toBe(undefined);
+    });
+
+});
