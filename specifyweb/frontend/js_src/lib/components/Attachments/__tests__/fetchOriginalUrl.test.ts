e8d0e7f1e45c837839df0fc4e50ee7c3c7706783
realVinayak <realvinayak123@gmail.com>
2025-06-25 13:43:29 -0500

    (tests): Add unit tests for fetchOriginalUrl

---
+++ specifyweb/frontend/js_src/lib/components/Attachments/__tests__/fetchOriginalUrl.test.ts
@@ -0,0 +1,60 @@
+import { requireContext } from "../../../tests/helpers";
+import { serializeResource } from "../../DataModel/serializers";
+import { tables } from "../../DataModel/tables";
+import { fetchOriginalUrl, overrideAttachmentSettings } from "../attachments";
+import attachmentSettings from "../../../tests/ajax/static/context/attachment_settings.json"
+import { overrideAjax } from "../../../tests/ajax";
+import { formatUrl } from "../../Router/queryString";
+
+
+requireContext();
+
+describe("fetchOriginalUrl", ()=>{
+
+    const attachmentLocation = "testLocation";
+
+    const mimeType = 'image/jpeg';
+    const fileName = 'testFile.jpg';
+    const testToken = "testToken";
+
+    test("url when token not required for get", async ()=>{
+
+        expect(attachmentSettings.token_required_for_get).toBe(false);
+
+        const attachment = new tables.Attachment.Resource({
+            attachmentlocation: attachmentLocation,
+            mimetype: mimeType,
+            origfilename: "C:\\testSlash.jpg",
+            title: fileName,
+            isPublic: true
+        });
+
+        const url = await fetchOriginalUrl(serializeResource(attachment));
+
+        expect(url).toBe("http://host.docker.internal/fileget?coll=KU+Fish+Voucher+Collection&type=O&filename=testLocation&downloadname=testSlash.jpg");
+
+    });
+
+    overrideAjax(
+        formatUrl('/attachment_gw/get_token/', { filename: attachmentLocation }), 
+        testToken
+    );
+
+    test("url when token is required for get", async ()=>{
+
+        overrideAttachmentSettings({...attachmentSettings, token_required_for_get: true});
+
+        const attachment = new tables.Attachment.Resource({
+            attachmentlocation: attachmentLocation,
+            mimetype: mimeType,
+            origfilename: "C:\\testSlash.jpg",
+            title: fileName,
+            isPublic: true
+        });
+
+        const url = await fetchOriginalUrl(serializeResource(attachment));
+
+        expect(url).toBe("http://host.docker.internal/fileget?coll=KU+Fish+Voucher+Collection&type=O&filename=testLocation&downloadname=testSlash.jpg&token=testToken");
+    });
+
+});
