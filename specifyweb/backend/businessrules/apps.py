d6ae17f7af6c468147e5a428d85507d706bc2847
pashiav <pashiavang2022@gmail.com>
2025-06-25 12:04:36 -0500

    update businessrules import paths for apps and migration 0002

--- specifyweb/backend/businessrules/apps.py
+++ specifyweb/backend/businessrules/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 
 class BussinessRuleConfig(AppConfig):
-    name = "specifyweb.businessrules"
+    name = "specifyweb.backend.businessrules"
 
     def ready(self) -> None:
         import specifyweb.backend.businessrules.rules
