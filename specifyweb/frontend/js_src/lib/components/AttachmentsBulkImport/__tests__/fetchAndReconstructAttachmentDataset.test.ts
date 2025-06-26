6d110105fb39ab7676ee2500557f698ff8e70c11
realVinayak <realvinayak123@gmail.com>
2025-06-26 14:55:09 +0000

    Lint code with ESLint and Prettier
    Triggered by 1c071d9f70202dc3c42a202d8537c78724634e27 on branch refs/heads/issue-6803

--- specifyweb/frontend/js_src/lib/components/AttachmentsBulkImport/__tests__/fetchAndReconstructAttachmentDataset.test.ts
+++ specifyweb/frontend/js_src/lib/components/AttachmentsBulkImport/__tests__/fetchAndReconstructAttachmentDataset.test.ts
@@ -1,103 +1,95 @@
-import { waitFor } from "@testing-library/react";
-import { overrideAjax } from "../../../tests/ajax";
-import { localized } from "../../../utils/types";
-import { getResourceApiUrl } from "../../DataModel/resource";
-import { FetchedDataSet } from "../types";
-import * as bulkAttachmentUtils from "../utils";
-import { fetchAndReconstructAttachmentDataset } from "../Import";
+import { waitFor } from '@testing-library/react';
+
+import { overrideAjax } from '../../../tests/ajax';
+import { localized } from '../../../utils/types';
+import { getResourceApiUrl } from '../../DataModel/resource';
+import { fetchAndReconstructAttachmentDataset } from '../Import';
+import type { FetchedDataSet } from '../types';
+import * as bulkAttachmentUtils from '../utils';
 
 const mockReconstructUploading = jest.fn();
 const mockReconstructDeleting = jest.fn();
 
-jest.spyOn(
-    bulkAttachmentUtils, 
-    "reconstructUploadingAttachmentSpec"
-).mockImplementation(async (...args)=>{
-        mockReconstructUploading(...args);
-        return []
-    }
-);
+jest
+  .spyOn(bulkAttachmentUtils, 'reconstructUploadingAttachmentSpec')
+  .mockImplementation(async (...args) => {
+    mockReconstructUploading(...args);
+    return [];
+  });
 
-jest.spyOn(
-    bulkAttachmentUtils, 
-    "reconstructDeletingAttachment"
-).mockImplementation(async (...args)=>{
-        mockReconstructDeleting(...args);
-        return []
-    }
-)
+jest
+  .spyOn(bulkAttachmentUtils, 'reconstructDeletingAttachment')
+  .mockImplementation(async (...args) => {
+    mockReconstructDeleting(...args);
+    return [];
+  });
 
+describe('fetchAndReconstructAttachmentDataset', () => {
+  beforeEach(() => {
+    mockReconstructUploading.mockClear();
+    mockReconstructDeleting.mockClear();
+  });
 
-describe("fetchAndReconstructAttachmentDataset", ()=>{
+  const makeDataSet = (
+    id: number,
+    state: 'deleting' | 'main' | 'uploading'
+  ): FetchedDataSet => ({
+    uploaderstatus: state,
+    id,
+    name: localized('test'),
+    timestampcreated: '',
+    timestampmodified: '',
+    createdbyagent: getResourceApiUrl('Agent', 1),
+    modifiedbyagent: null,
+    importedfilename: '',
+    remarks: '',
+    rows: [],
+    uploadplan: {
+      staticPathKey: 'collectionObjectCatalogNumber',
+      formatQueryResults: () => undefined,
+    },
+  });
 
-    beforeEach(()=>{
-        mockReconstructUploading.mockClear();
-        mockReconstructDeleting.mockClear();
+  const mainDatSetId = 3;
+  const mainDataSet = makeDataSet(mainDatSetId, 'main');
+
+  overrideAjax(`/attachment_gw/dataset/${mainDatSetId}/`, mainDataSet);
+
+  test('main state gets untouched', async () => {
+    await fetchAndReconstructAttachmentDataset(mainDatSetId);
+
+    expect(mockReconstructDeleting).toHaveBeenCalledTimes(0);
+    expect(mockReconstructUploading).toHaveBeenCalledTimes(0);
+  });
+
+  const uploadingDatSetId = 4;
+  const uploadingDataSet = makeDataSet(mainDatSetId, 'uploading');
+
+  overrideAjax(
+    `/attachment_gw/dataset/${uploadingDatSetId}/`,
+    uploadingDataSet
+  );
+
+  test('upload reconstruct called when uploading', async () => {
+    await fetchAndReconstructAttachmentDataset(uploadingDatSetId);
+
+    await waitFor(() => {
+      expect(mockReconstructDeleting).toHaveBeenCalledTimes(0);
+      expect(mockReconstructUploading).toHaveBeenCalledTimes(1);
     });
+  });
 
-    const makeDataSet = (id: number, state: 'main' | 'uploading' | 'deleting'): FetchedDataSet => ({
-        uploaderstatus: state,
-        id,
-        name: localized("test"),
-        timestampcreated: "",
-        timestampmodified: "",
-        createdbyagent: getResourceApiUrl("Agent", 1),
-        modifiedbyagent: null,
-        importedfilename: "",
-        remarks: "",
-        rows: [],
-        uploadplan: {
-            staticPathKey: "collectionObjectCatalogNumber",
-            formatQueryResults: ()=>undefined
-        }
-    })
+  const deletingDatSetId = 5;
+  const deletingDataSet = makeDataSet(mainDatSetId, 'deleting');
 
-    
-    const mainDatSetId = 3;
-    const mainDataSet = makeDataSet(mainDatSetId, "main");
+  overrideAjax(`/attachment_gw/dataset/${deletingDatSetId}/`, deletingDataSet);
 
-    overrideAjax(`/attachment_gw/dataset/${mainDatSetId}/`, mainDataSet);
+  test('upload reconstruct called when uploading', async () => {
+    await fetchAndReconstructAttachmentDataset(deletingDatSetId);
 
-    test("main state gets untouched", async ()=>{
-
-        await fetchAndReconstructAttachmentDataset(mainDatSetId);
-
-        expect(mockReconstructDeleting).toBeCalledTimes(0);
-        expect(mockReconstructUploading).toBeCalledTimes(0);
+    await waitFor(() => {
+      expect(mockReconstructDeleting).toHaveBeenCalledTimes(1);
+      expect(mockReconstructUploading).toHaveBeenCalledTimes(0);
     });
-
-    const uploadingDatSetId = 4;
-    const uploadingDataSet = makeDataSet(mainDatSetId, "uploading");
-
-    overrideAjax(`/attachment_gw/dataset/${uploadingDatSetId}/`, uploadingDataSet);
-
-    test("upload reconstruct called when uploading", async ()=>{
-
-        await fetchAndReconstructAttachmentDataset(uploadingDatSetId);
-
-        await waitFor(()=>{
-
-            expect(mockReconstructDeleting).toBeCalledTimes(0);
-            expect(mockReconstructUploading).toBeCalledTimes(1);
-        })
-
-    });
-
-    const deletingDatSetId = 5;
-    const deletingDataSet = makeDataSet(mainDatSetId, "deleting");
-
-    overrideAjax(`/attachment_gw/dataset/${deletingDatSetId}/`, deletingDataSet);
-
-    test("upload reconstruct called when uploading", async ()=>{
-
-        await fetchAndReconstructAttachmentDataset(deletingDatSetId);
-
-        await waitFor(()=>{
-
-            expect(mockReconstructDeleting).toBeCalledTimes(1);
-            expect(mockReconstructUploading).toBeCalledTimes(0);
-        })
-
-    });
-
+  });
 });
