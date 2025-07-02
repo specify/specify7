84e5e7e245165261e91f46d4d972921b71ab49ac
realVinayak <realvinayak123@gmail.com>
2025-07-02 07:29:59 -0500

    (tests): Add tests for fastParseInt

--- specifyweb/frontend/js_src/lib/utils/__tests__/functools.test.ts
+++ specifyweb/frontend/js_src/lib/utils/__tests__/functools.test.ts
@@ -130,6 +130,13 @@ describe('f.parseInt', () => {
   test('invalid case', () => expect(f.parseInt('a-1.4')).toBeUndefined());
 });
 
+describe('f.fastParseInt', () => {
+  test('simple case', () => expect(f.fastParseInt('1')).toBe(1));
+  test('float case', () => expect(f.fastParseInt('-1.4')).toBe(-1));
+  test('invalid case', () => expect(f.fastParseInt('a-1.4')).toBe(0));
+  test('empty string case', () => expect(f.fastParseInt("")).toBe(0));
+});
+
 describe('f.parseFloat', () => {
   test('simple case', () => expect(f.parseFloat('1')).toBe(1));
   test('float case', () => expect(f.parseFloat('-1.4')).toBe(-1.4));
