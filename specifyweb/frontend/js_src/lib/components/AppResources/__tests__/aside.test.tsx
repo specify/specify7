a80ba4af7f8dfc47579f6cd1aa95221c827056d9
realVinayak <realvinayak123@gmail.com>
2025-07-02 10:24:50 -0500

    (tests): Add tests for mutateConfirmation

---
+++ specifyweb/frontend/js_src/lib/components/AppResources/__tests__/aside.test.tsx
@@ -0,0 +1,87 @@
+import { RA } from "../../../utils/types";
+import { AppResourcesConformation, exportsForTests } from "../Aside";
+
+const { mutateConformation } = exportsForTests;
+
+describe("mutateConfirmation", () => {
+
+    test("child replaced", () => {
+        const conformations: RA<AppResourcesConformation> = [
+            {
+                children: [],
+                key: "TestKey1"
+            },
+            {
+                children: [
+                    {
+                        children: [],
+                        key: "ChildTestKey2_1"
+                    },
+                    {
+                        children: [],
+                        key: "ChildTestKey2_2"
+                    }
+                ],
+                key: "TestKey2"
+            },
+            {
+                children: [],
+                key: "TestKey3"
+            },
+            {
+                children: [],
+                key: "TestKey34"
+            },
+        ];
+
+        const childConformation = {
+            children: [
+                {
+                    children: [],
+                    key: "ChildTestKey3_1"
+                },
+                {
+                    children: [],
+                    key: "ChildTestKey3_2"
+                },
+            ],
+            key: "TestKey3"
+        };
+
+        const mutated = mutateConformation(
+            conformations,
+            "TestKey3",
+            [childConformation]
+        );
+
+        const expected = [
+            {
+                children: [],
+                key: "TestKey1"
+            },
+            {
+                children: [
+                    {
+                        children: [],
+                        key: "ChildTestKey2_1"
+                    },
+                    {
+                        children: [],
+                        key: "ChildTestKey2_2"
+                    }
+                ],
+                key: "TestKey2"
+            },
+            {
+                children: [childConformation],
+                key: "TestKey3"
+            },
+            {
+                children: [],
+                key: "TestKey34"
+            },
+        ];
+
+        expect(mutated).toEqual(expected);
+    });
+});
