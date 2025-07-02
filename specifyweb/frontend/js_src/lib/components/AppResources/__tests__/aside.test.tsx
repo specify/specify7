d729215c92b15657a665aebae9ddc0e604b104c3
realVinayak <realvinayak123@gmail.com>
2025-07-02 15:29:06 +0000

    Lint code with ESLint and Prettier
    Triggered by a80ba4af7f8dfc47579f6cd1aa95221c827056d9 on branch refs/heads/issue-6886

--- specifyweb/frontend/js_src/lib/components/AppResources/__tests__/aside.test.tsx
+++ specifyweb/frontend/js_src/lib/components/AppResources/__tests__/aside.test.tsx
@@ -1,87 +1,85 @@
-import { RA } from "../../../utils/types";
-import { AppResourcesConformation, exportsForTests } from "../Aside";
+import type { RA } from '../../../utils/types';
+import type { AppResourcesConformation } from '../Aside';
+import { exportsForTests } from '../Aside';
 
 const { mutateConformation } = exportsForTests;
 
-describe("mutateConfirmation", () => {
+describe('mutateConfirmation', () => {
+  test('child replaced', () => {
+    const conformations: RA<AppResourcesConformation> = [
+      {
+        children: [],
+        key: 'TestKey1',
+      },
+      {
+        children: [
+          {
+            children: [],
+            key: 'ChildTestKey2_1',
+          },
+          {
+            children: [],
+            key: 'ChildTestKey2_2',
+          },
+        ],
+        key: 'TestKey2',
+      },
+      {
+        children: [],
+        key: 'TestKey3',
+      },
+      {
+        children: [],
+        key: 'TestKey34',
+      },
+    ];
 
-    test("child replaced", () => {
-        const conformations: RA<AppResourcesConformation> = [
-            {
-                children: [],
-                key: "TestKey1"
-            },
-            {
-                children: [
-                    {
-                        children: [],
-                        key: "ChildTestKey2_1"
-                    },
-                    {
-                        children: [],
-                        key: "ChildTestKey2_2"
-                    }
-                ],
-                key: "TestKey2"
-            },
-            {
-                children: [],
-                key: "TestKey3"
-            },
-            {
-                children: [],
-                key: "TestKey34"
-            },
-        ];
+    const childConformation = {
+      children: [
+        {
+          children: [],
+          key: 'ChildTestKey3_1',
+        },
+        {
+          children: [],
+          key: 'ChildTestKey3_2',
+        },
+      ],
+      key: 'TestKey3',
+    };
 
-        const childConformation = {
-            children: [
-                {
-                    children: [],
-                    key: "ChildTestKey3_1"
-                },
-                {
-                    children: [],
-                    key: "ChildTestKey3_2"
-                },
-            ],
-            key: "TestKey3"
-        };
+    const mutated = mutateConformation(conformations, 'TestKey3', [
+      childConformation,
+    ]);
 
-        const mutated = mutateConformation(
-            conformations,
-            "TestKey3",
-            [childConformation]
-        );
+    const expected = [
+      {
+        children: [],
+        key: 'TestKey1',
+      },
+      {
+        children: [
+          {
+            children: [],
+            key: 'ChildTestKey2_1',
+          },
+          {
+            children: [],
+            key: 'ChildTestKey2_2',
+          },
+        ],
+        key: 'TestKey2',
+      },
+      {
+        children: [childConformation],
+        key: 'TestKey3',
+      },
+      {
+        children: [],
+        key: 'TestKey34',
+      },
+    ];
 
-        const expected = [
-            {
-                children: [],
-                key: "TestKey1"
-            },
-            {
-                children: [
-                    {
-                        children: [],
-                        key: "ChildTestKey2_1"
-                    },
-                    {
-                        children: [],
-                        key: "ChildTestKey2_2"
-                    }
-                ],
-                key: "TestKey2"
-            },
-            {
-                children: [childConformation],
-                key: "TestKey3"
-            },
-            {
-                children: [],
-                key: "TestKey34"
-            },
-        ];
-
-        expect(mutated).toEqual(expected);
-    });
+    expect(mutated).toEqual(expected);
+  });
 });
