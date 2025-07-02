1a925b5902ffa4abb3af9bbf21e9387807d8cdcc
realVinayak <realvinayak123@gmail.com>
2025-07-02 09:42:41 -0500

    (tests): Add tests for stripLastOccurrence and stripFileExtension

--- specifyweb/frontend/js_src/lib/utils/__tests__/utils.test.ts
+++ specifyweb/frontend/js_src/lib/utils/__tests__/utils.test.ts
@@ -23,6 +23,8 @@ import {
   sortFunction,
   spanNumber,
   split,
+  stripFileExtension,
+  stripLastOccurrence,
   takeBetween,
   toggleItem,
   toLowerCase,
@@ -309,4 +311,20 @@ theories(chunk, [
       [9, 10],
     ],
   },
+]);
+
+theories(stripLastOccurrence, [
+  { in: ["test", ":"], out: "test"},
+  { in: ["test:second", ":"], out: "test"},
+  { in: ["test:second:third", ":"], out: "test:second" },
+  { in: ["someText", ""], out: "someTex" },
+  { in: [" ", ":"], out: " " },
+  { in: [" ", ""], out: "" }
+]);
+
+theories(stripFileExtension, [
+  { in: ["test"], out: "test"},
+  { in: ["test.second"], out: "test"},
+  { in: ["test.second.jpg"], out: "test.second" },
+  { in: [" "], out: " " }
 ]);
