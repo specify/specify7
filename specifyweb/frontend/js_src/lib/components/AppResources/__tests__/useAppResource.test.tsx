7be5ea266538592db63fdb276d7a189191f96f60
realVinayak <realvinayak123@gmail.com>
2025-07-03 18:45:07 +0000

    Lint code with ESLint and Prettier
    Triggered by 31cdf2c137118d826c35cc7b271ab5143d4a10c1 on branch refs/heads/issue-6906

--- specifyweb/frontend/js_src/lib/components/AppResources/__tests__/useAppResource.test.tsx
+++ specifyweb/frontend/js_src/lib/components/AppResources/__tests__/useAppResource.test.tsx
@@ -1,154 +1,156 @@
-import React from "react";
-import { SerializedResource } from "../../DataModel/helperTypes";
-import { SpAppResource, SpViewSetObj } from "../../DataModel/types";
-import { exportsForTests } from "../EditorWrapper";
-import { AppResourceMode } from "../helpers";
-import { AppResources } from "../hooks";
-import { render, waitFor } from "@testing-library/react";
+import { render, waitFor } from '@testing-library/react';
+import React from 'react';
 import * as Router from 'react-router-dom';
-import { requireContext } from "../../../tests/helpers";
-import { tables } from "../../DataModel/tables";
-import { staticAppResources } from "./staticAppResources"
-import { serializeResource } from "../../DataModel/serializers";
-import { WritableArray } from "../../../utils/types";
-import { removeKey } from "../../../utils/utils";
 
+import { requireContext } from '../../../tests/helpers';
+import type { WritableArray } from '../../../utils/types';
+import { removeKey } from '../../../utils/utils';
+import type { SerializedResource } from '../../DataModel/helperTypes';
+import { serializeResource } from '../../DataModel/serializers';
+import { tables } from '../../DataModel/tables';
+import type { SpAppResource, SpViewSetObj } from '../../DataModel/types';
+import { exportsForTests } from '../EditorWrapper';
+import type { AppResourceMode } from '../helpers';
+import type { AppResources } from '../hooks';
+import { staticAppResources } from './staticAppResources';
 
 requireContext();
 
 const { useAppResource } = exportsForTests;
 
-
-function TestComponent({ newResource, resources, mode, onResultSet: handleResultSet }: {
-    readonly newResource: SerializedResource<SpAppResource | SpViewSetObj>,
-    readonly resources: AppResources,
-    readonly mode: AppResourceMode
-    readonly onResultSet: (value: SerializedResource<SpAppResource | SpViewSetObj>) => void;
+function TestComponent({
+  newResource,
+  resources,
+  mode,
+  onResultSet: handleResultSet,
+}: {
+  readonly newResource: SerializedResource<SpAppResource | SpViewSetObj>;
+  readonly resources: AppResources;
+  readonly mode: AppResourceMode;
+  readonly onResultSet: (
+    value: SerializedResource<SpAppResource | SpViewSetObj>
+  ) => void;
 }): JSX.Element {
-    const result = useAppResource(newResource, resources, mode);
-    React.useEffect(() => {
-        handleResultSet(result);
-    }, [result]);
-    return <></>
+  const result = useAppResource(newResource, resources, mode);
+  React.useEffect(() => {
+    handleResultSet(result);
+  }, [result]);
+  return <></>;
 }
 
 function TestComponentWrapper(
-    props: Parameters<typeof TestComponent>[0] & { readonly initialEntries: WritableArray<string> }
+  props: Parameters<typeof TestComponent>[0] & {
+    readonly initialEntries: WritableArray<string>;
+  }
 ): JSX.Element {
-    return <Router.MemoryRouter initialEntries={props.initialEntries}>
-        <Router.Routes>
-            <Router.Route element={
-                <TestComponent
-                    {...removeKey(props, "initialEntries")}
-                />
-            }
-                path=":id" />
-        </Router.Routes>
-    </Router.MemoryRouter >
+  return (
+    <Router.MemoryRouter initialEntries={props.initialEntries}>
+      <Router.Routes>
+        <Router.Route
+          element={<TestComponent {...removeKey(props, 'initialEntries')} />}
+          path=":id"
+        />
+      </Router.Routes>
+    </Router.MemoryRouter>
+  );
 }
 
-describe("useAppResource", () => {
-
-    test("appResource mode (existing resorurce)", async () => {
-
-        const spAppResource = new tables.SpAppResource.Resource({ name: "Brand New Resource" });
-        const serializedNewResource = serializeResource(spAppResource);
-
-        const onResultSet = jest.fn();
-        const appResourceToUse = staticAppResources.appResources[0];
-
-        render(
-            <TestComponentWrapper
-                initialEntries={[`/${appResourceToUse.id}`]}
-                newResource={serializedNewResource}
-                resources={
-                    staticAppResources as unknown as AppResources
-                }
-                mode="appResources"
-                onResultSet={onResultSet}
-            />
-        )
-
-        await waitFor(() => {
-            expect(onResultSet).toBeCalledTimes(1);
-            expect(onResultSet).toBeCalledWith(appResourceToUse);
-        });
+describe('useAppResource', () => {
+  test('appResource mode (existing resorurce)', async () => {
+    const spAppResource = new tables.SpAppResource.Resource({
+      name: 'Brand New Resource',
     });
+    const serializedNewResource = serializeResource(spAppResource);
 
-    test("appResource mode (new resorurce)", async () => {
+    const onResultSet = jest.fn();
+    const appResourceToUse = staticAppResources.appResources[0];
 
-        const spAppResource = new tables.SpAppResource.Resource({ name: "Brand New Resource" });
-        const serializedNewResource = serializeResource(spAppResource);
+    render(
+      <TestComponentWrapper
+        initialEntries={[`/${appResourceToUse.id}`]}
+        mode="appResources"
+        newResource={serializedNewResource}
+        resources={staticAppResources as unknown as AppResources}
+        onResultSet={onResultSet}
+      />
+    );
 
-        const onResultSet = jest.fn();
-
-        render(
-            <TestComponentWrapper
-                initialEntries={[`/2048`]}
-                newResource={serializedNewResource}
-                resources={
-                    staticAppResources as unknown as AppResources
-                }
-                mode="appResources"
-                onResultSet={onResultSet}
-            />
-        );
-
-        await waitFor(() => {
-            expect(onResultSet).toBeCalledTimes(1);
-            expect(onResultSet).toBeCalledWith(serializedNewResource);
-        });
+    await waitFor(() => {
+      expect(onResultSet).toHaveBeenCalledTimes(1);
+      expect(onResultSet).toHaveBeenCalledWith(appResourceToUse);
     });
+  });
 
-
-    test("viewSets mode (existing resorurce)", async () => {
-
-        const spViewSetResource = new tables.SpViewSetObj.Resource({ name: "Brand New ViewSet Resource" });
-        const serializedNewResource = serializeResource(spViewSetResource);
-
-        const onResultSet = jest.fn();
-        const viewSetToUse = staticAppResources.viewSets[0];
-
-        render(
-            <TestComponentWrapper
-                initialEntries={[`/${viewSetToUse.id}`]}
-                newResource={serializedNewResource}
-                resources={
-                    staticAppResources as unknown as AppResources
-                }
-                mode="viewSets"
-                onResultSet={onResultSet}
-            />
-        );
-
-        await waitFor(() => {
-            expect(onResultSet).toBeCalledTimes(1);
-            expect(onResultSet).toBeCalledWith(viewSetToUse);
-        });
+  test('appResource mode (new resorurce)', async () => {
+    const spAppResource = new tables.SpAppResource.Resource({
+      name: 'Brand New Resource',
     });
+    const serializedNewResource = serializeResource(spAppResource);
 
-    test("viewSets mode (new resorurce)", async () => {
+    const onResultSet = jest.fn();
 
-        const spViewSetResource = new tables.SpViewSetObj.Resource({ name: "Brand New ViewSet Resource" });
-        const serializedNewResource = serializeResource(spViewSetResource);
+    render(
+      <TestComponentWrapper
+        initialEntries={[`/2048`]}
+        mode="appResources"
+        newResource={serializedNewResource}
+        resources={staticAppResources as unknown as AppResources}
+        onResultSet={onResultSet}
+      />
+    );
 
-        const onResultSet = jest.fn();
-
-        render(
-            <TestComponentWrapper
-                initialEntries={[`/2048`]}
-                newResource={serializedNewResource}
-                resources={
-                    staticAppResources as unknown as AppResources
-                }
-                mode="viewSets"
-                onResultSet={onResultSet}
-            />
-        );
-
-        await waitFor(() => {
-            expect(onResultSet).toBeCalledTimes(1);
-            expect(onResultSet).toBeCalledWith(serializedNewResource);
-        });
+    await waitFor(() => {
+      expect(onResultSet).toHaveBeenCalledTimes(1);
+      expect(onResultSet).toHaveBeenCalledWith(serializedNewResource);
     });
+  });
+
+  test('viewSets mode (existing resorurce)', async () => {
+    const spViewSetResource = new tables.SpViewSetObj.Resource({
+      name: 'Brand New ViewSet Resource',
+    });
+    const serializedNewResource = serializeResource(spViewSetResource);
+
+    const onResultSet = jest.fn();
+    const viewSetToUse = staticAppResources.viewSets[0];
+
+    render(
+      <TestComponentWrapper
+        initialEntries={[`/${viewSetToUse.id}`]}
+        mode="viewSets"
+        newResource={serializedNewResource}
+        resources={staticAppResources as unknown as AppResources}
+        onResultSet={onResultSet}
+      />
+    );
+
+    await waitFor(() => {
+      expect(onResultSet).toHaveBeenCalledTimes(1);
+      expect(onResultSet).toHaveBeenCalledWith(viewSetToUse);
+    });
+  });
+
+  test('viewSets mode (new resorurce)', async () => {
+    const spViewSetResource = new tables.SpViewSetObj.Resource({
+      name: 'Brand New ViewSet Resource',
+    });
+    const serializedNewResource = serializeResource(spViewSetResource);
+
+    const onResultSet = jest.fn();
+
+    render(
+      <TestComponentWrapper
+        initialEntries={[`/2048`]}
+        mode="viewSets"
+        newResource={serializedNewResource}
+        resources={staticAppResources as unknown as AppResources}
+        onResultSet={onResultSet}
+      />
+    );
+
+    await waitFor(() => {
+      expect(onResultSet).toHaveBeenCalledTimes(1);
+      expect(onResultSet).toHaveBeenCalledWith(serializedNewResource);
+    });
+  });
 });
