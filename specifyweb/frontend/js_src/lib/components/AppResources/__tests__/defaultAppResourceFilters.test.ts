b4da1841bacc382ee2555b2d1f2c8c423a901b81
realVinayak <realvinayak123@gmail.com>
2025-07-04 10:32:00 -0500

    (tests): Add tests for defaultAppResourceFilters

---
+++ specifyweb/frontend/js_src/lib/components/AppResources/__tests__/defaultAppResourceFilters.test.ts
@@ -0,0 +1,29 @@
+import { defaultAppResourceFilters } from "../filtersHelpers";
+
+test("defaultAppResourceFilters", ()=>{
+    expect(defaultAppResourceFilters).toMatchInlineSnapshot(`
+{
+  "appResources": [
+    "collectionPreferences",
+    "dataEntryTables",
+    "dataObjectFormatters",
+    "defaultUserPreferences",
+    "expressSearchConfig",
+    "interactionsTables",
+    "label",
+    "leafletLayers",
+    "otherAppResources",
+    "otherJsonResource",
+    "otherPropertiesResource",
+    "otherXmlResource",
+    "report",
+    "rssExportFeed",
+    "typeSearches",
+    "uiFormatters",
+    "userPreferences",
+    "webLinks",
+  ],
+  "viewSets": true,
+}
+`)
+});
