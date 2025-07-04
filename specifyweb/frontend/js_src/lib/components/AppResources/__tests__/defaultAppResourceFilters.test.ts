0fb00ed2658c96f8be223cf1b17f69585c756405
realVinayak <realvinayak123@gmail.com>
2025-07-04 15:36:30 +0000

    Lint code with ESLint and Prettier
    Triggered by b4da1841bacc382ee2555b2d1f2c8c423a901b81 on branch refs/heads/issue-6916

--- specifyweb/frontend/js_src/lib/components/AppResources/__tests__/defaultAppResourceFilters.test.ts
+++ specifyweb/frontend/js_src/lib/components/AppResources/__tests__/defaultAppResourceFilters.test.ts
@@ -2,28 +2,28 @@ import { defaultAppResourceFilters } from "../filtersHelpers";
 
 test("defaultAppResourceFilters", ()=>{
     expect(defaultAppResourceFilters).toMatchInlineSnapshot(`
-{
-  "appResources": [
-    "collectionPreferences",
-    "dataEntryTables",
-    "dataObjectFormatters",
-    "defaultUserPreferences",
-    "expressSearchConfig",
-    "interactionsTables",
-    "label",
-    "leafletLayers",
-    "otherAppResources",
-    "otherJsonResource",
-    "otherPropertiesResource",
-    "otherXmlResource",
-    "report",
-    "rssExportFeed",
-    "typeSearches",
-    "uiFormatters",
-    "userPreferences",
-    "webLinks",
-  ],
-  "viewSets": true,
-}
-`)
+      {
+        "appResources": [
+          "collectionPreferences",
+          "dataEntryTables",
+          "dataObjectFormatters",
+          "defaultUserPreferences",
+          "expressSearchConfig",
+          "interactionsTables",
+          "label",
+          "leafletLayers",
+          "otherAppResources",
+          "otherJsonResource",
+          "otherPropertiesResource",
+          "otherXmlResource",
+          "report",
+          "rssExportFeed",
+          "typeSearches",
+          "uiFormatters",
+          "userPreferences",
+          "webLinks",
+        ],
+        "viewSets": true,
+      }
+    `)
 });
