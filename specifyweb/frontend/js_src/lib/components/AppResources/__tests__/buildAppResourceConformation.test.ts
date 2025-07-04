8ed90d475e47b565ec2052ac842fe1bbec3011e2
realVinayak <realvinayak123@gmail.com>
2025-07-04 19:50:56 +0000

    Lint code with ESLint and Prettier
    Triggered by ac24c72c5676dc0fa925b7164c8aab12f5d1ff08 on branch refs/heads/issue-6931

--- specifyweb/frontend/js_src/lib/components/AppResources/__tests__/buildAppResourceConformation.test.ts
+++ specifyweb/frontend/js_src/lib/components/AppResources/__tests__/buildAppResourceConformation.test.ts
@@ -1,144 +1,138 @@
-import { requireContext } from "../../../tests/helpers";
-import { buildAppResourceConformation } from "../helpers";
-import { utilsForTests } from "./utils";
+import { requireContext } from '../../../tests/helpers';
+import { buildAppResourceConformation } from '../helpers';
+import { utilsForTests } from './utils';
 
 requireContext();
 
-const {treeStructure, incrementor, makeTree } = utilsForTests;
+const { treeStructure, incrementor, makeTree } = utilsForTests;
 
-describe("buildAppResourceConformation", ()=>{
+describe('buildAppResourceConformation', () => {
+  test('simple 1-level case', () => {
+    const labelIncrementor = incrementor();
+    const keyIncrementor = incrementor();
+    const idIncrementor = incrementor();
 
-    test("simple 1-level case", ()=>{
+    /*
+     * Need to use typeof treeStructure because Node is not exported, and
+     * exporting it will pollute global namespace.
+     */
+    const simpleTree: typeof treeStructure = [
+      {
+        id: 0,
+        appResources: 2,
+        viewSets: 1,
+        children: [],
+      },
+      {
+        id: 0,
+        appResources: 0,
+        viewSets: 3,
+        children: [],
+      },
+      {
+        id: 0,
+        appResources: 1,
+        viewSets: 0,
+        children: [],
+      },
+      {
+        id: 0,
+        appResources: 0,
+        viewSets: 0,
+        children: [],
+      },
+    ];
+    const tree = makeTree(
+      simpleTree,
+      labelIncrementor,
+      keyIncrementor,
+      idIncrementor,
+      true
+    );
 
-        const labelIncrementor = incrementor();
-        const keyIncrementor = incrementor();
-        const idIncrementor = incrementor();
+    expect(buildAppResourceConformation(tree)).toMatchSnapshot();
+  });
 
-        // Need to use typeof treeStructure because Node is not exported, and 
-        // exporting it will pollute global namespace.
-        const simpleTree: typeof treeStructure  = [
-            {
-                id: 0, 
-                appResources: 2,
-                viewSets: 1,
-                children: []
-            },
-            {
-                id: 0, 
-                appResources: 0,
-                viewSets: 3,
-                children: []
-            },  
-            {
-                id: 0, 
-                appResources: 1,
-                viewSets: 0,
-                children: []
-            },  
-            {
-                id: 0, 
-                appResources: 0,
-                viewSets: 0,
-                children: []
-            },
-        ]
-        const tree = makeTree(
-            simpleTree,
-            labelIncrementor,
-            keyIncrementor,
-            idIncrementor,
-            true
-        );
+  test('multi-level tree', () => {
+    const labelIncrementor = incrementor();
+    const keyIncrementor = incrementor();
+    const idIncrementor = incrementor();
 
-        expect(buildAppResourceConformation(tree)).toMatchSnapshot();
-
-    }); 
-
-    test("multi-level tree", ()=>{
-
-        const labelIncrementor = incrementor();
-        const keyIncrementor = incrementor();
-        const idIncrementor = incrementor();
-
-        const treeWithFilledLeaf: typeof treeStructure = [
-            {
+    const treeWithFilledLeaf: typeof treeStructure = [
+      {
+        id: 0,
+        appResources: 0,
+        viewSets: 0,
+        children: [
+          {
+            id: 0,
+            appResources: 0,
+            viewSets: 0,
+            children: [
+              {
                 id: 0,
                 appResources: 0,
-                viewSets:0,
+                viewSets: 0,
                 children: [
-                    {
-                        id: 0,
-                        appResources: 0,
-                        viewSets: 0,
-                        children: [
-                            {
-                                id: 0,
-                                appResources: 0,
-                                viewSets: 0,
-                                children: [
-                                    {
-                                        id: 0,
-                                        appResources: 1,
-                                        viewSets: 2,
-                                        children: []
-                                    },
-                                    {
-                                        id: 0,
-                                        appResources: 0,
-                                        viewSets: 2,
-                                        children: []
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                ]
-            }
-        ];
+                  {
+                    id: 0,
+                    appResources: 1,
+                    viewSets: 2,
+                    children: [],
+                  },
+                  {
+                    id: 0,
+                    appResources: 0,
+                    viewSets: 2,
+                    children: [],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ];
 
-        const nestedEmptyTree = [
-            ...treeStructure,
-            ...treeWithFilledLeaf
-        ];
+    const nestedEmptyTree = [...treeStructure, ...treeWithFilledLeaf];
 
-        const tree = makeTree(
-            nestedEmptyTree,
-            labelIncrementor,
-            keyIncrementor,
-            idIncrementor,
-            true
-        );
+    const tree = makeTree(
+      nestedEmptyTree,
+      labelIncrementor,
+      keyIncrementor,
+      idIncrementor,
+      true
+    );
 
-        expect(buildAppResourceConformation(tree)).toMatchSnapshot();
-    });
+    expect(buildAppResourceConformation(tree)).toMatchSnapshot();
+  });
 
-    test("multi-level completely empty tree", ()=>{
-        const labelIncrementor = incrementor();
-        const keyIncrementor = incrementor();
-        const idIncrementor = incrementor();
+  test('multi-level completely empty tree', () => {
+    const labelIncrementor = incrementor();
+    const keyIncrementor = incrementor();
+    const idIncrementor = incrementor();
 
-        const makeEmptyTree = (breadth: number, levels: number): typeof treeStructure => (
-            levels === 0 
-            ? [] 
-            : Array.from({length: breadth}).map(
-                ()=>({
-                    id: 0, 
-                    viewSets: 0, 
-                    appResources: 0, 
-                    children: makeEmptyTree(breadth, levels - 1)
-                })
-            )
-        );
+    const makeEmptyTree = (
+      breadth: number,
+      levels: number
+    ): typeof treeStructure =>
+      levels === 0
+        ? []
+        : Array.from({ length: breadth }, () => ({
+            id: 0,
+            viewSets: 0,
+            appResources: 0,
+            children: makeEmptyTree(breadth, levels - 1),
+          }));
 
-        const emptyTree = makeTree(
-                makeEmptyTree(3, 3),
-                labelIncrementor,
-                keyIncrementor,
-                idIncrementor,
-                true
-            );
-        
-        expect(buildAppResourceConformation(emptyTree)).toMatchSnapshot();
-    });
-}
-)
+    const emptyTree = makeTree(
+      makeEmptyTree(3, 3),
+      labelIncrementor,
+      keyIncrementor,
+      idIncrementor,
+      true
+    );
+
+    expect(buildAppResourceConformation(emptyTree)).toMatchSnapshot();
+  });
+});
