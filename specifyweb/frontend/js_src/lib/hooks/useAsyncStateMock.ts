3243928dd4041c3eee4934a589745ef225107c4d
realVinayak <realvinayak123@gmail.com>
2025-07-07 18:10:50 +0000

    Lint code with ESLint and Prettier
    Triggered by 4264580a508a9c6c42a6e373f13806aeae9ec569 on branch refs/heads/issue-6940

--- specifyweb/frontend/js_src/lib/hooks/useAsyncStateMock.ts
+++ specifyweb/frontend/js_src/lib/hooks/useAsyncStateMock.ts
@@ -1,27 +1,26 @@
-// Like useAsyncState, but calls the callback function rather than updating the state.
-// Actually, useAsyncState can be refactored to use this
+/*
+ * Like useAsyncState, but calls the callback function rather than updating the state.
+ * Actually, useAsyncState can be refactored to use this
+ */
 
-import React from "react";
-import { f } from "../utils/functools";
+import React from 'react';
 
+import { f } from '../utils/functools';
 
 export function useAsyncStateMock<T>(
-    callback: () => Promise<T | undefined> | undefined,
-    onSetCallback: (value: T | undefined) => void,
-    
-){
+  callback: () => Promise<T | undefined> | undefined,
+  onSetCallback: (value: T | undefined) => void
+) {
+  React.useLayoutEffect(() => {
+    Promise.resolve(callback()).then((newState) => {
+      destructorCalled ? undefined : onSetCallback(newState);
+    });
 
-    React.useLayoutEffect(()=>{
-        Promise.resolve(callback()).then((newState)=>{
-            destructorCalled ? undefined : onSetCallback(newState)
-        });
+    let destructorCalled = false;
+    return () => {
+      destructorCalled = true;
+    };
+  }, [callback]);
 
-        let destructorCalled = false;
-        return ()=>{
-            destructorCalled = true;
-        }
-    }, [callback]);
-
-    return [undefined, f.void];
-
+  return [undefined, f.void];
 }
