6a700ff4f9ff13220820b946ce814d41226ab140
realVinayak <realvinayak123@gmail.com>
2025-07-07 16:01:14 +0000

    Lint code with ESLint and Prettier
    Triggered by 5bbca950bbd78f3672831ff8b8a30328198e79b1 on branch refs/heads/issue-6935

--- specifyweb/frontend/js_src/lib/components/AppResources/__tests__/useResourcesTree.test.ts
+++ specifyweb/frontend/js_src/lib/components/AppResources/__tests__/useResourcesTree.test.ts
@@ -1,96 +1,104 @@
-import { renderHook } from "@testing-library/react";
-import { AppResources, AppResourcesTree, useResourcesTree } from "../hooks";
-import { staticAppResources } from "./staticAppResources";
-import { SerializedResource } from "../../DataModel/helperTypes";
-import { Discipline } from "../../DataModel/types";
-import { requireContext } from "../../../tests/helpers";
-import { getAppResourceCount } from "../helpers";
-import { replaceItem } from "../../../utils/utils";
-import { getResourceApiUrl } from "../../DataModel/resource";
-import { RA } from "../../../utils/types";
+import { renderHook } from '@testing-library/react';
+
+import { requireContext } from '../../../tests/helpers';
+import type { RA } from '../../../utils/types';
+import { replaceItem } from '../../../utils/utils';
+import type { SerializedResource } from '../../DataModel/helperTypes';
+import { getResourceApiUrl } from '../../DataModel/resource';
+import type { Discipline } from '../../DataModel/types';
+import { getAppResourceCount } from '../helpers';
+import type { AppResources, AppResourcesTree } from '../hooks';
+import { useResourcesTree } from '../hooks';
+import { staticAppResources } from './staticAppResources';
 
 requireContext();
 
-describe("useResourcesTree", ()=>{
+describe('useResourcesTree', () => {
+  const getResourceCountTree = (result: AppResourcesTree) =>
+    result.reduce(
+      (count, resource) => count + getAppResourceCount(resource),
+      0
+    );
 
+  /*
+   * Const getResourceCountStatic = (resources: AppResources) => {
+   *     const allAppResources = [
+   *         ...resources.appResources.map(({resource_uri})=>resource_uri),
+   *         ...resources.viewSets.map(({resource_uri})=>resource_uri)
+   *     ]
+   *     return allAppResources.length;
+   * }
+   */
 
-    const getResourceCountTree = (result: AppResourcesTree) => result.reduce((count, resource)=>(count + getAppResourceCount(resource)), 0);
+  const resources = {
+    ...staticAppResources,
+    disciplines: [
+      {
+        id: 3,
+        isPaleoContextEmbedded: true,
+        name: 'Ichthyology',
+        paleoContextChildTable: 'collectionobject',
+        regNumber: '1344636812.54',
+        timestampCreated: '2012-08-09T12:23:29',
+        timestampModified: '2012-08-09T12:23:29',
+        type: 'fish',
+        version: 11,
+        createdByAgent: '/api/specify/agent/1/',
+        dataType: '/api/specify/datatype/1/',
+        division: '/api/specify/division/2/',
+        geographyTreeDef: '/api/specify/geographytreedef/1/',
+        taxonTreeDef: '/api/specify/taxontreedef/1/',
+        geologicTimePeriodTreeDef: '/api/specify/geologictimeperiodtreedef/1/',
+        lithoStratTreeDef: '/api/specify/lithostrattreedef/1/',
+        tectonicUnitTreeDef: '/api/specify/tectonicunittreedef/1/',
+        modifiedByAgent: '/api/specify/agent/2/',
+        attributeDefs: '/api/specify/attributedef/?discipline=3',
+        collections: '/api/specify/collection/?discipline=3',
+        spExportSchemas: '/api/specify/spexportschema/?discipline=3',
+        spLocaleContainers: '/api/specify/splocalecontainer/?discipline=3',
+        resource_uri: '/api/specify/discipline/3/',
+        userGroups: '/api/specify/SpPrincipal?scope=3',
+        _tableName: 'Discipline',
+      },
+    ] as unknown as RA<SerializedResource<Discipline>>,
+  };
 
-    // const getResourceCountStatic = (resources: AppResources) => {
-    //     const allAppResources = [
-    //         ...resources.appResources.map(({resource_uri})=>resource_uri),
-    //         ...resources.viewSets.map(({resource_uri})=>resource_uri)
-    //     ]
-    //     return allAppResources.length;
-    // }
+  test('missing appresource dir', () => {
+    const { result } = renderHook(() => useResourcesTree(resources));
 
-    const resources = {
-        ...staticAppResources,
-        disciplines: [{
-                "id": 3,
-                "isPaleoContextEmbedded": true,
-                "name": "Ichthyology",
-                "paleoContextChildTable": "collectionobject",
-                "regNumber": "1344636812.54",
-                "timestampCreated": "2012-08-09T12:23:29",
-                "timestampModified": "2012-08-09T12:23:29",
-                "type": "fish",
-                "version": 11,
-                "createdByAgent": "/api/specify/agent/1/",
-                "dataType": "/api/specify/datatype/1/",
-                "division": "/api/specify/division/2/",
-                "geographyTreeDef": "/api/specify/geographytreedef/1/",
-                "taxonTreeDef": "/api/specify/taxontreedef/1/",
-                "geologicTimePeriodTreeDef": "/api/specify/geologictimeperiodtreedef/1/",
-                "lithoStratTreeDef": "/api/specify/lithostrattreedef/1/",
-                "tectonicUnitTreeDef": "/api/specify/tectonicunittreedef/1/",
-                "modifiedByAgent": "/api/specify/agent/2/",
-                "attributeDefs": "/api/specify/attributedef/?discipline=3",
-                "collections": "/api/specify/collection/?discipline=3",
-                "spExportSchemas": "/api/specify/spexportschema/?discipline=3",
-                "spLocaleContainers": "/api/specify/splocalecontainer/?discipline=3",
-                "resource_uri": "/api/specify/discipline/3/",
-                "userGroups": "/api/specify/SpPrincipal?scope=3",
-                "_tableName": "Discipline"
-            }
-        ]  as unknown as RA<SerializedResource<Discipline>>
-    }
+    expect(result.current).toMatchSnapshot();
 
+    // There is only 1 resource with the matching spappresourcedir.
+    expect(getResourceCountTree(result.current)).toBe(1);
+  });
 
-    test('missing appresource dir', ()=>{
+  const setAppResourceDir = (
+    resources: AppResources,
+    key: 'appResources' | 'viewSets',
+    index: number,
+    newDir: number
+  ): AppResources => ({
+    ...resources,
+    [key]: replaceItem(resources[key as 'appResources'], index, {
+      ...resources[key as 'appResources'][index],
+      spAppResourceDir: getResourceApiUrl('SpAppResourceDir', newDir),
+    }),
+  });
 
-        const {result} = renderHook(()=>useResourcesTree(resources));
+  test('all appresource dir', () => {
+    const appResourceSet = resources.appResources.reduce(
+      (resources, _, index) =>
+        setAppResourceDir(resources, 'appResources', index, 4),
+      resources
+    );
 
-        expect(result.current).toMatchSnapshot();
+    const viewSet = setAppResourceDir(appResourceSet, 'viewSets', 0, 4);
 
-        // There is only 1 resource with the matching spappresourcedir.
-        expect(getResourceCountTree(result.current)).toBe(1);
+    const { result } = renderHook(() => useResourcesTree(viewSet));
 
-    });
+    expect(result.current).toMatchSnapshot();
 
-    const setAppResourceDir = (resources: AppResources, key: 'appResources' | 'viewSets', index: number, newDir: number): AppResources => ({
-        ...resources,
-        [key]: replaceItem(
-            resources[key as 'appResources'], 
-            index, 
-            ({...resources[key as 'appResources'][index], spAppResourceDir: getResourceApiUrl("SpAppResourceDir", newDir)})
-        )
-    });
-
-    test("all appresource dir", ()=>{
-
-        const appResourceSet = resources.appResources.reduce((resources, _, index)=>(
-            setAppResourceDir(resources, 'appResources', index, 4)
-        ), resources);
-
-        const viewSet = setAppResourceDir(appResourceSet, 'viewSets', 0, 4);
-
-        const {result} = renderHook(()=>useResourcesTree(viewSet));
-
-        expect(result.current).toMatchSnapshot();
-
-        // The user type resources are added for each specify user....
-        expect(getResourceCountTree(result.current)).toBe(8);
-
-    });
+    // The user type resources are added for each specify user....
+    expect(getResourceCountTree(result.current)).toBe(8);
+  });
 });
