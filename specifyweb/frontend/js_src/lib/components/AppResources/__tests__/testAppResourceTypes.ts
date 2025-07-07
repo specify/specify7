ca07684025e87ff9597b8419dea6b9655b2184fd
realVinayak <realvinayak123@gmail.com>
2025-07-07 18:43:15 +0000

    Lint code with ESLint and Prettier
    Triggered by 05a8683bebe9be4b6ffef161b415ef3b6959633c on branch refs/heads/issue-6944

--- specifyweb/frontend/js_src/lib/components/AppResources/__tests__/testAppResourceTypes.ts
+++ specifyweb/frontend/js_src/lib/components/AppResources/__tests__/testAppResourceTypes.ts
@@ -1,122 +1,122 @@
 export const testAppResourcesTypes = [
-    {
+  {
     name: 'preferences',
     mimeType: null,
     expectedType: 'otherPropertiesResource',
-    extenstion: 'properties'
-    },
-    {
+    extenstion: 'properties',
+  },
+  {
     name: 'QueryFreqList',
     mimeType: 'text/xml',
     expectedType: 'otherXmlResource',
-    extenstion: 'xml'
-    },
-    {
+    extenstion: 'xml',
+  },
+  {
     name: 'QueryExtraList',
     mimeType: 'text/xml',
     expectedType: 'otherXmlResource',
-    extenstion: 'xml'
-    },
-    {
+    extenstion: 'xml',
+  },
+  {
     name: 'ExpressSearchConfig',
     mimeType: 'text/xml',
     expectedType: 'expressSearchConfig',
-    extenstion: 'xml'
-    },
-    {
+    extenstion: 'xml',
+  },
+  {
     name: 'WebLinks',
     mimeType: 'text/xml',
     expectedType: 'webLinks',
-    extenstion: 'xml'
-    },
-    {
+    extenstion: 'xml',
+  },
+  {
     name: 'DataObjFormatters',
     mimeType: 'text/xml',
     expectedType: 'dataObjectFormatters',
-    extenstion: 'xml'
-    },
-    {
+    extenstion: 'xml',
+  },
+  {
     name: 'DataEntryTaskInit',
     mimeType: 'text/xml',
     expectedType: 'dataEntryTables',
-    extenstion: 'xml'
-    },
-    {
+    extenstion: 'xml',
+  },
+  {
     name: 'Number Tags',
     mimeType: 'jrxml/label',
     expectedType: 'label',
-    extenstion: 'jrxml'
-    },
-    {
+    extenstion: 'jrxml',
+  },
+  {
     name: 'Fish Loan Report',
     mimeType: 'jrxml/report',
     expectedType: 'report',
-    extenstion: 'jrxml'
-    },
-    {
+    extenstion: 'jrxml',
+  },
+  {
     name: 'Tissue Gift Report',
     mimeType: 'jrxml/report',
     expectedType: 'report',
-    extenstion: 'jrxml'
-    },
-    {
+    extenstion: 'jrxml',
+  },
+  {
     name: 'Fish Gift Report',
     mimeType: 'jrxml/report',
     expectedType: 'report',
-    extenstion: 'jrxml'
-    },
-    {
+    extenstion: 'jrxml',
+  },
+  {
     name: 'Loan Shipping Form',
     mimeType: 'jrxml/report',
     expectedType: 'report',
-    extenstion: 'jrxml'
-    },
-    {
+    extenstion: 'jrxml',
+  },
+  {
     name: 'Gift Shipping Form',
     mimeType: 'jrxml/report',
     expectedType: 'report',
-    extenstion: 'jrxml'
-    },
-    {
+    extenstion: 'jrxml',
+  },
+  {
     name: 'Datamax Jar Labels',
     mimeType: 'jrxml/label',
     expectedType: 'label',
-    extenstion: 'jrxml'
-    },
-    {
+    extenstion: 'jrxml',
+  },
+  {
     name: 'TypeSearches',
     mimeType: 'text/xml',
     expectedType: 'typeSearches',
-    extenstion: 'xml'
-    },
-    {
+    extenstion: 'xml',
+  },
+  {
     name: 'UIFormatters',
     mimeType: 'text/xml',
     expectedType: 'uiFormatters',
-    extenstion: 'xml'
-    },
-    {
+    extenstion: 'xml',
+  },
+  {
     name: 'Teaching labels',
     mimeType: 'jrxml/label',
     expectedType: 'label',
-    extenstion: 'jrxml'
-    },
-    {
+    extenstion: 'jrxml',
+  },
+  {
     name: 'UserPreferences',
     mimeType: 'application/json',
     expectedType: 'userPreferences',
-    extenstion: 'json'
-    },
-    {
+    extenstion: 'json',
+  },
+  {
     name: 'CollectionPreferences',
     mimeType: 'application/json',
     expectedType: 'collectionPreferences',
-    extenstion: 'json'
-    },
-    {
+    extenstion: 'json',
+  },
+  {
     name: 'DefaultUserPreferences',
     mimeType: 'application/json',
     expectedType: 'defaultUserPreferences',
-    extenstion: 'json'
-    }
-]
+    extenstion: 'json',
+  },
+];
