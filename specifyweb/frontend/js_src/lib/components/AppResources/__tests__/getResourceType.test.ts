05a8683bebe9be4b6ffef161b415ef3b6959633c
realVinayak <realvinayak123@gmail.com>
2025-07-07 13:38:41 -0500

    (tests): Add tests for getAppResourceExtension

--- specifyweb/frontend/js_src/lib/components/AppResources/__tests__/getResourceType.test.ts
+++ specifyweb/frontend/js_src/lib/components/AppResources/__tests__/getResourceType.test.ts
@@ -2,107 +2,9 @@ import { requireContext } from '../../../tests/helpers';
 import { addMissingFields } from '../../DataModel/addMissingFields';
 import { getAppResourceType, getResourceType } from '../filtersHelpers';
 import { staticAppResources } from './staticAppResources';
+import { testAppResourcesTypes } from './testAppResourceTypes';
 
 requireContext();
-
-const testAppResources = [
-  {
-    name: 'preferences',
-    mimeType: null,
-    expectedType: 'otherPropertiesResource',
-  },
-  {
-    name: 'QueryFreqList',
-    mimeType: 'text/xml',
-    expectedType: 'otherXmlResource',
-  },
-  {
-    name: 'QueryExtraList',
-    mimeType: 'text/xml',
-    expectedType: 'otherXmlResource',
-  },
-  {
-    name: 'ExpressSearchConfig',
-    mimeType: 'text/xml',
-    expectedType: 'expressSearchConfig',
-  },
-  { name: 'WebLinks', mimeType: 'text/xml', expectedType: 'webLinks' },
-  {
-    name: 'DataObjFormatters',
-    mimeType: 'text/xml',
-    expectedType: 'dataObjectFormatters',
-  },
-  {
-    name: 'DataEntryTaskInit',
-    mimeType: 'text/xml',
-    expectedType: 'dataEntryTables',
-  },
-  {
-    name: 'Number Tags',
-    mimeType: 'jrxml/label',
-    expectedType: 'label',
-  },
-  {
-    name: 'Fish Loan Report',
-    mimeType: 'jrxml/report',
-    expectedType: 'report',
-  },
-  {
-    name: 'Tissue Gift Report',
-    mimeType: 'jrxml/report',
-    expectedType: 'report',
-  },
-  {
-    name: 'Fish Gift Report',
-    mimeType: 'jrxml/report',
-    expectedType: 'report',
-  },
-  {
-    name: 'Loan Shipping Form',
-    mimeType: 'jrxml/report',
-    expectedType: 'report',
-  },
-  {
-    name: 'Gift Shipping Form',
-    mimeType: 'jrxml/report',
-    expectedType: 'report',
-  },
-  {
-    name: 'Datamax Jar Labels',
-    mimeType: 'jrxml/label',
-    expectedType: 'label',
-  },
-  {
-    name: 'TypeSearches',
-    mimeType: 'text/xml',
-    expectedType: 'typeSearches',
-  },
-  {
-    name: 'UIFormatters',
-    mimeType: 'text/xml',
-    expectedType: 'uiFormatters',
-  },
-  {
-    name: 'Teaching labels',
-    mimeType: 'jrxml/label',
-    expectedType: 'label',
-  },
-  {
-    name: 'UserPreferences',
-    mimeType: 'application/json',
-    expectedType: 'userPreferences',
-  },
-  {
-    name: 'CollectionPreferences',
-    mimeType: 'application/json',
-    expectedType: 'collectionPreferences',
-  },
-  {
-    name: 'DefaultUserPreferences',
-    mimeType: 'application/json',
-    expectedType: 'defaultUserPreferences',
-  },
-];
 
 describe('getResourceType', () => {
   test('viewset type', () => {
@@ -110,7 +12,7 @@ describe('getResourceType', () => {
   });
 
   test('appresource type', () => {
-    testAppResources.forEach(({ expectedType, ...partialResource }) => {
+    testAppResourcesTypes.forEach(({ expectedType, ...partialResource }) => {
       expect(
         getResourceType(addMissingFields('SpAppResource', partialResource))
       ).toBe(expectedType);
@@ -120,7 +22,7 @@ describe('getResourceType', () => {
 
 describe('getAppResourceType', () => {
   test('returned type matches expected', () => {
-    testAppResources.forEach(({ expectedType, ...partialResource }) => {
+    testAppResourcesTypes.forEach(({ expectedType, ...partialResource }) => {
       expect(
         getAppResourceType(addMissingFields('SpAppResource', partialResource))
       ).toBe(expectedType);
---
+++ specifyweb/frontend/js_src/lib/components/AppResources/__tests__/testAppResourceTypes.ts
@@ -0,0 +1,122 @@
+export const testAppResourcesTypes = [
+    {
+    name: 'preferences',
+    mimeType: null,
+    expectedType: 'otherPropertiesResource',
+    extenstion: 'properties'
+    },
+    {
+    name: 'QueryFreqList',
+    mimeType: 'text/xml',
+    expectedType: 'otherXmlResource',
+    extenstion: 'xml'
+    },
+    {
+    name: 'QueryExtraList',
+    mimeType: 'text/xml',
+    expectedType: 'otherXmlResource',
+    extenstion: 'xml'
+    },
+    {
+    name: 'ExpressSearchConfig',
+    mimeType: 'text/xml',
+    expectedType: 'expressSearchConfig',
+    extenstion: 'xml'
+    },
+    {
+    name: 'WebLinks',
+    mimeType: 'text/xml',
+    expectedType: 'webLinks',
+    extenstion: 'xml'
+    },
+    {
+    name: 'DataObjFormatters',
+    mimeType: 'text/xml',
+    expectedType: 'dataObjectFormatters',
+    extenstion: 'xml'
+    },
+    {
+    name: 'DataEntryTaskInit',
+    mimeType: 'text/xml',
+    expectedType: 'dataEntryTables',
+    extenstion: 'xml'
+    },
+    {
+    name: 'Number Tags',
+    mimeType: 'jrxml/label',
+    expectedType: 'label',
+    extenstion: 'jrxml'
+    },
+    {
+    name: 'Fish Loan Report',
+    mimeType: 'jrxml/report',
+    expectedType: 'report',
+    extenstion: 'jrxml'
+    },
+    {
+    name: 'Tissue Gift Report',
+    mimeType: 'jrxml/report',
+    expectedType: 'report',
+    extenstion: 'jrxml'
+    },
+    {
+    name: 'Fish Gift Report',
+    mimeType: 'jrxml/report',
+    expectedType: 'report',
+    extenstion: 'jrxml'
+    },
+    {
+    name: 'Loan Shipping Form',
+    mimeType: 'jrxml/report',
+    expectedType: 'report',
+    extenstion: 'jrxml'
+    },
+    {
+    name: 'Gift Shipping Form',
+    mimeType: 'jrxml/report',
+    expectedType: 'report',
+    extenstion: 'jrxml'
+    },
+    {
+    name: 'Datamax Jar Labels',
+    mimeType: 'jrxml/label',
+    expectedType: 'label',
+    extenstion: 'jrxml'
+    },
+    {
+    name: 'TypeSearches',
+    mimeType: 'text/xml',
+    expectedType: 'typeSearches',
+    extenstion: 'xml'
+    },
+    {
+    name: 'UIFormatters',
+    mimeType: 'text/xml',
+    expectedType: 'uiFormatters',
+    extenstion: 'xml'
+    },
+    {
+    name: 'Teaching labels',
+    mimeType: 'jrxml/label',
+    expectedType: 'label',
+    extenstion: 'jrxml'
+    },
+    {
+    name: 'UserPreferences',
+    mimeType: 'application/json',
+    expectedType: 'userPreferences',
+    extenstion: 'json'
+    },
+    {
+    name: 'CollectionPreferences',
+    mimeType: 'application/json',
+    expectedType: 'collectionPreferences',
+    extenstion: 'json'
+    },
+    {
+    name: 'DefaultUserPreferences',
+    mimeType: 'application/json',
+    expectedType: 'defaultUserPreferences',
+    extenstion: 'json'
+    }
+]
