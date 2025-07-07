8313313834cbd94cade86b64f31d9a3395d839a7
realVinayak <realvinayak123@gmail.com>
2025-07-07 12:11:16 -0500

    (tests): Fix tests for useAppResources

--- specifyweb/frontend/js_src/lib/components/AppResources/__tests__/useAppResources.test.ts
+++ specifyweb/frontend/js_src/lib/components/AppResources/__tests__/useAppResources.test.ts
@@ -1,15 +1,34 @@
-import { render } from "@testing-library/react";
-import React from "react";
-import { act } from "react-dom/test-utils";
+import { renderHook, waitFor } from "@testing-library/react";
 
 import { overrideAjax } from "../../../tests/ajax";
 import { requireContext } from "../../../tests/helpers";
-import type { GetOrSet, RA } from "../../../utils/types";
-import type { AppResources} from "../hooks";
+import type { RA } from "../../../utils/types";
 import { useAppResources } from "../hooks";
 import { staticAppResources } from "./staticAppResources";
+import { useAsyncStateMock } from "../../../hooks/useAsyncStateMock";
+
+const mockFn = jest.fn();
+
+function mockState<T>(
+    callback: () => Promise<T | undefined> | undefined,
+) {
+    useAsyncStateMock(callback, mockFn);
+    return [undefined, undefined];
+}
+
+jest.mock("../../../hooks/useAsyncState", () => {
+    const module = jest.requireActual("../../../hooks/useAsyncState");
+    return {
+        ...module,
+        useAsyncState: mockState
+    }
+});
 
 requireContext();
+
+beforeAll(() => {
+    mockFn.mockClear();
+});
 
 describe("useAppResources", () => {
 
@@ -29,21 +48,16 @@ describe("useAppResources", () => {
     overrideAjax("/api/specify/spappresource/?limit=0", makeCollection(staticAppResources.appResources));
     overrideAjax("/api/specify/spviewsetobj/?limit=0", makeCollection(staticAppResources.viewSets));
 
-    function TestComponent({ onSet: handleSet }: { readonly onSet: (value: GetOrSet<AppResources | undefined>) => void }) {
-        const result = useAppResources(false);
-        React.useEffect(() => {
-            handleSet(result);
-        }, [result[0], result[1]]);
-        return <></>
-    }
+    test("no loading screen", async () => {
 
-    test.skip("no loading screen", async () => {
+        jest.spyOn(console, 'warn').mockImplementation();
+        
+        renderHook(()=>useAppResources(false));
 
-        act(() => {
-            render(
-                <TestComponent onSet={jest.fn()} />
-            );
-        })
+        await waitFor(()=>{
+            expect(mockFn).toBeCalledTimes(1);
+            expect(mockFn.mock.lastCall).toEqual([staticAppResources])
+        });
 
     });
 })
