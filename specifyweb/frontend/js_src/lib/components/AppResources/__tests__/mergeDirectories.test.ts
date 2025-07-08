f74a271e8dec44913af0062746b11cba6565b90b
realVinayak <realvinayak123@gmail.com>
2025-07-08 11:20:39 -0500

    (tests): Add tests for mergeDirectories

---
+++ specifyweb/frontend/js_src/lib/components/AppResources/__tests__/mergeDirectories.test.ts
@@ -0,0 +1,66 @@
+import { staticAppResources } from "./staticAppResources";
+import { utilsForTests } from "./utils";
+import { exportsForTests } from "../tree";
+
+const { setAppResourceDir } = utilsForTests;
+const { mergeDirectories } = exportsForTests;
+
+
+describe("mergeDirectories", ()=>{
+
+    test("spappresourcedirs matching", ()=>{
+        // In this case, the spappresources
+        // have the dirs in the directory list
+
+        const ids = [3, 3, 4];
+
+        const appResourceAdjusted = ids.reduce(
+            (resource, id, index)=>setAppResourceDir(resource, 'appResources', index, id),
+            staticAppResources
+        );
+
+        const viewSetAdjusted = setAppResourceDir(appResourceAdjusted, 'viewSets', 0, 3);
+        const merged = mergeDirectories(viewSetAdjusted.directories, viewSetAdjusted);
+        expect(merged.appResources.length).toBe(3);
+        expect(merged.viewSets.length).toBe(1);
+
+    });
+
+    test("spappresource extra dirs", ()=>{
+        // In this case, the spappresources
+        // have the dirs in the directory list
+
+        const ids = [3, 3, 4];
+
+        const appResourceAdjusted = ids.reduce(
+            (resource, id, index)=>setAppResourceDir(resource, 'appResources', index, id),
+            staticAppResources
+        );
+
+        const viewSetAdjusted = setAppResourceDir(appResourceAdjusted, 'viewSets', 0, 4);
+
+        const testSpec = [
+            {
+                dirId: 3,
+                apps: 2,
+                views: 0
+            },
+            {
+                dirId: 4,
+                apps: 1,
+                views: 1
+            }
+        ] as const;
+
+        testSpec.forEach(({apps, views}, index)=>{
+            
+            const merged = mergeDirectories([viewSetAdjusted.directories[index]], viewSetAdjusted);
+            expect(merged.appResources.length).toBe(apps);
+            expect(merged.viewSets.length).toBe(views);
+        });
+
+
+
+    });
+
+});
