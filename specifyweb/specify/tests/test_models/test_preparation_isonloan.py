1ba47966ee6bae7a42811bbd25d0deacc456d75b
realVinayak <realvinayak123@gmail.com>
2025-07-09 17:06:30 -0500

    (tests): Add more tricky cases

--- specifyweb/specify/tests/test_models/test_preparation_isonloan.py
+++ specifyweb/specify/tests/test_models/test_preparation_isonloan.py
@@ -1,4 +1,4 @@
-from specifyweb.specify.models import Loan
+from specifyweb.specify.models import Loan, Loanpreparation
 from specifyweb.specify.tests.test_api import ApiTests
 from specifyweb.interactions.tests.utils import _create_interaction_prep_generic
 
@@ -22,9 +22,30 @@ class TestPreparationIsOnLoan(ApiTests):
         _create_interaction_prep_generic(self, self.loan_1, prep, None, quantity=3, quantityresolved=3, isresolved=True)
         self.assertFalse(prep.isonloan())
 
-    def test_not_loan_isresolved_false(self):
+    def test_loan_isresolved_false(self):
         prep = self._create_prep(self.collectionobjects[0], None, countamt=3)
         _create_interaction_prep_generic(self, self.loan_1, prep, None, quantity=3, quantityresolved=3, isresolved=True)
         _create_interaction_prep_generic(self, self.loan_1, prep, None, quantity=3, quantityresolved=2, isresolved=False)
 
         self.assertTrue(prep.isonloan())
+
+    def test_loan_isresolved_false_coalesce(self):
+        prep = self._create_prep(self.collectionobjects[0], None, countamt=3)
+        _create_interaction_prep_generic(self, self.loan_1, prep, None, quantity=3, quantityresolved=3, isresolved=True)
+        loanprep  = _create_interaction_prep_generic(self, self.loan_1, prep, None, quantity=3, quantityresolved=2, isresolved=False)
+        Loanpreparation.objects.filter(id=loanprep.id).update(
+            quantity=2,
+            quantityresolved=0
+        )
+        self.assertTrue(prep.isonloan())
+
+    def test_not_loan_isresolved_negative(self):
+        prep = self._create_prep(self.collectionobjects[0], None, countamt=3)
+        _create_interaction_prep_generic(self, self.loan_1, prep, None, quantity=3, quantityresolved=3, isresolved=True)
+        loanprep  = _create_interaction_prep_generic(self, self.loan_1, prep, None, quantity=3, quantityresolved=2, isresolved=False)
+        Loanpreparation.objects.filter(id=loanprep.id).update(
+            quantity=2,
+            # This will force the the expression to be negative
+            quantityresolved=5
+        )
+        self.assertFalse(prep.isonloan())
