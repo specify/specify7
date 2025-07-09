611bfb9faf90e28bb7e94e6381f2cef8834b8c16
pashiav <118556884+pashiav@users.noreply.github.com>
2025-07-09 17:12:51 +0000

    Lint code with ESLint and Prettier
    Triggered by 36a05ed92593878c67ef21b8fa06c9bc3cb55599 on branch refs/heads/issue-6624

--- specifyweb/frontend/js_src/lib/components/AppResources/__tests__/disambiguateGlobalPrefs.test.ts
+++ specifyweb/frontend/js_src/lib/components/AppResources/__tests__/disambiguateGlobalPrefs.test.ts
@@ -1,82 +1,87 @@
-import { resourcesText } from "../../../localization/resources";
-import { replaceItem } from "../../../utils/utils";
-import { getResourceApiUrl } from "../../DataModel/resource";
-import type { AppResourcesTree } from "../hooks";
-import { exportsForTests } from "../tree";
-import { staticAppResources } from "./staticAppResources";
+import { resourcesText } from '../../../localization/resources';
+import { replaceItem } from '../../../utils/utils';
+import { getResourceApiUrl } from '../../DataModel/resource';
+import type { AppResourcesTree } from '../hooks';
+import { exportsForTests } from '../tree';
+import { staticAppResources } from './staticAppResources';
 
 const { disambiguateGlobalPrefs } = exportsForTests;
 
-describe("disambiguateGlobalPrefs", ()=>{
+describe('disambiguateGlobalPrefs', () => {
+  test('no preference app resources', () => {
+    const appResources = [0, 2].map(
+      (index) => staticAppResources.appResources[index]
+    );
+    expect(
+      disambiguateGlobalPrefs(appResources, staticAppResources.directories)
+    ).toEqual(appResources);
+  });
 
-    test("no preference app resources", ()=>{
-        const appResources = ([0, 2]).map((index)=>staticAppResources.appResources[index]);
-        expect(disambiguateGlobalPrefs(appResources, staticAppResources.directories)).toEqual(
-            appResources
-        );
-    });
+  test('preference app resources (global prefs)', () => {
+    expect(
+      disambiguateGlobalPrefs(
+        staticAppResources.appResources,
+        staticAppResources.directories
+      )
+    ).toEqual(
+      replaceItem(
+        staticAppResources.appResources as AppResourcesTree[number]['appResources'],
+        1,
+        {
+          ...staticAppResources.appResources[1],
+          label: resourcesText.globalPreferences(),
+        }
+      )
+    );
+  });
 
-    test("preference app resources (global prefs)", ()=>{
+  test('preference app resources (remote prefs)', () => {
+    const directories = [
+      {
+        ...staticAppResources.directories[0],
+        userType: 'Prefs',
+      },
+      staticAppResources.directories[1],
+    ];
 
-        expect(disambiguateGlobalPrefs(staticAppResources.appResources, staticAppResources.directories)).toEqual(
-            replaceItem(
-                staticAppResources.appResources as AppResourcesTree[number]['appResources'],
-                1, 
-                {
-                    ...staticAppResources.appResources[1],
-                    label: resourcesText.globalPreferences()
-                }
-            )
-        );
+    expect(
+      disambiguateGlobalPrefs(staticAppResources.appResources, directories)
+    ).toEqual(
+      replaceItem(
+        staticAppResources.appResources as AppResourcesTree[number]['appResources'],
+        1,
+        {
+          ...staticAppResources.appResources[1],
+          label: resourcesText.remotePreferences(),
+        }
+      )
+    );
+  });
 
-    });
+  test('preference app resources (unmatched usertype)', () => {
+    const directories = [
+      {
+        ...staticAppResources.directories[0],
+        userType: 'Common',
+      },
+      staticAppResources.directories[1],
+    ];
 
-    test("preference app resources (remote prefs)", ()=>{
+    expect(
+      disambiguateGlobalPrefs(staticAppResources.appResources, directories)
+    ).toEqual(
+      staticAppResources.appResources as AppResourcesTree[number]['appResources']
+    );
+  });
 
-        const directories = [
-            {
-                ...staticAppResources.directories[0],
-                userType: "Prefs"
-            },
-            staticAppResources.directories[1]
-        ];
+  test('preference app resources (unmatched spappresourcedir)', () => {
+    const appResources = staticAppResources.appResources.map((resource) => ({
+      ...resource,
+      spAppResourceDir: getResourceApiUrl('SpAppResourceDir', 4),
+    }));
 
-        expect(disambiguateGlobalPrefs(staticAppResources.appResources, directories)).toEqual(
-            replaceItem(
-                staticAppResources.appResources as AppResourcesTree[number]['appResources'],
-                1, 
-                {
-                    ...staticAppResources.appResources[1],
-                    label: resourcesText.remotePreferences()
-                }
-            )
-        );
-    });
-
-    test("preference app resources (unmatched usertype)", ()=>{
-        const directories = [
-            {
-                ...staticAppResources.directories[0],
-                userType: "Common"
-            },
-            staticAppResources.directories[1]
-        ];
-
-        expect(disambiguateGlobalPrefs(staticAppResources.appResources, directories)).toEqual(
-            staticAppResources.appResources as AppResourcesTree[number]['appResources']
-        );
-
-    });
-
-    test("preference app resources (unmatched spappresourcedir)", ()=>{
-        const appResources = staticAppResources.appResources.map(
-            (resource)=>({...resource, spAppResourceDir: getResourceApiUrl("SpAppResourceDir", 4)})
-        );
-
-        expect(disambiguateGlobalPrefs(appResources, staticAppResources.directories)).toEqual(
-            appResources
-        );
-
-    });
-
+    expect(
+      disambiguateGlobalPrefs(appResources, staticAppResources.directories)
+    ).toEqual(appResources);
+  });
 });
