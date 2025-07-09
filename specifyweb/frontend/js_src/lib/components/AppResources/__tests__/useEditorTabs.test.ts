26983af8764ddfd59e87e65d05149644313fe167
realVinayak <realvinayak123@gmail.com>
2025-07-09 09:40:05 -0500

    (tests): Add tests for useEditorTabs

---
+++ specifyweb/frontend/js_src/lib/components/AppResources/__tests__/useEditorTabs.test.ts
@@ -0,0 +1,37 @@
+import { renderHook } from "@testing-library/react";
+import { useEditorTabs } from "../Tabs";
+import { staticAppResources } from "./staticAppResources";
+import { addMissingFields } from "../../DataModel/addMissingFields";
+import { requireContext } from "../../../tests/helpers";
+
+requireContext();
+
+describe("useEditorTabs", ()=>{
+
+    test("xml editor", ()=>{
+        const {result} = renderHook(()=>useEditorTabs(staticAppResources.viewSets[0]));
+        expect(result.current.map(({label})=>label)).toEqual(
+            ["Visual Editor", 'XML Editor']
+        );
+    });
+
+    test("text editor", ()=>{
+        const {result} = renderHook(()=>useEditorTabs(staticAppResources.appResources[1]));
+        expect(result.current.map(({label})=>label)).toEqual(
+            ["Text Editor"]
+        );  
+    });
+
+    test("user preferences editor", ()=>{
+        const {result} = renderHook(()=>useEditorTabs(addMissingFields("SpAppResource", {
+            name: "UserPreferences",
+            mimeType: "application/json"
+        })));
+    
+        expect(result.current.map(({label})=>label)).toEqual(
+            ["Visual Editor", "JSON Editor"]
+        );
+    });
+
+
+});
