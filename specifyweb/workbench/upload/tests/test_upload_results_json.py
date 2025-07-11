cccf8675196e7a8e74e409445efa6f2f886acdb2
melton-jason <meltonjason12@yahoo.com>
2025-07-11 13:52:26 -0500

    Resolve type error in workbench upload test

--- specifyweb/workbench/upload/tests/test_upload_results_json.py
+++ specifyweb/workbench/upload/tests/test_upload_results_json.py
@@ -68,7 +68,7 @@ class UploadResultsTests(unittest.TestCase):
 
     @settings(suppress_health_check=[HealthCheck.too_slow])
     @given(record_result=infer, toOne=infer, toMany=infer)
-    def testUploadResult(self, record_result: RecordResult, toOne: Dict[str, RecordResult], toMany: Dict[str, List[RecordResult]]):
+    def testUploadResult(self, record_result: RecordResult, toOne: dict[str, RecordResult], toMany: dict[str, list[RecordResult]]):
         uploadResult = UploadResult(
             record_result=record_result,
             toOne={k: UploadResult(v, {}, {}) for k, v in toOne.items()},
