37075d66bc2e14731b03153d9ee6fd5749154fb4
Mark Pitblado <mark@pitblado.me>
2025-07-14 14:27:23 -0700

    feat(queries): add endsWith operator to the backend
    The same logic as startsWith but reversed (regex matching). Does not
    include the logic that startsWith implements for leading zeroes (as it
    is not applicable in an endsWith operation).

--- specifyweb/stored_queries/query_ops.py
+++ specifyweb/stored_queries/query_ops.py
@@ -24,31 +24,32 @@ class QueryOps(namedtuple("QueryOps", "uiformatter")):
 
     OPERATIONS = [
         # operation,      # op number
-        'op_like',              # 0
-        'op_equals',            # 1
-        'op_greaterthan',       # 2
-        'op_lessthan',          # 3
-        'op_greaterthanequals', # 4
-        'op_lessthanequals',    # 5
-        'op_true',              # 6
-        'op_false',             # 7
-        'op_dontcare',          # 8
-        'op_between',           # 9
-        'op_in',                # 10
-        'op_contains',          # 11
-        'op_empty',             # 12
-        'op_trueornull',        # 13
-        'op_falseornull',       # 14
-        'op_startswith',        # 15
-        'op_age_range',         # 16
-        'op_age_period',        # 17
+        "op_like",  # 0
+        "op_equals",  # 1
+        "op_greaterthan",  # 2
+        "op_lessthan",  # 3
+        "op_greaterthanequals",  # 4
+        "op_lessthanequals",  # 5
+        "op_true",  # 6
+        "op_false",  # 7
+        "op_dontcare",  # 8
+        "op_between",  # 9
+        "op_in",  # 10
+        "op_contains",  # 11
+        "op_empty",  # 12
+        "op_trueornull",  # 13
+        "op_falseornull",  # 14
+        "op_startswith",  # 15
+        "op_age_range",  # 16
+        "op_age_period",  # 17
+        "op_endswith",  # 18
     ]
 
     PRECALCUALTED_OPERATION_NUMS = {16, 17}
 
     def by_op_num(self, op_num):
         return getattr(self, self.OPERATIONS[op_num])
-    
+
     def is_precalculated(self, op_num):
         return op_num in self.PRECALCUALTED_OPERATION_NUMS
 
@@ -95,12 +96,12 @@ class QueryOps(namedtuple("QueryOps", "uiformatter")):
         return None
 
     def op_between(self, field, value):
-        values = [self.format(v.strip()) for v in value.split(',')[:2]]
+        values = [self.format(v.strip()) for v in value.split(",")[:2]]
         return field.between(*values)
 
     def op_in(self, field, values):
-        if hasattr(values, 'split'):
-            values = [self.format(v.strip()) for v in values.split(',')]
+        if hasattr(values, "split"):
+            values = [self.format(v.strip()) for v in values.split(",")]
         return field.in_(values)
 
     def op_contains(self, field, value):
@@ -108,7 +109,7 @@ class QueryOps(namedtuple("QueryOps", "uiformatter")):
 
     def op_empty(self, field, value):
         if isinstance(field.type, sqlalchemy.types.String):
-            return (field == '') | (field == None)
+            return (field == "") | (field == None)
         else:
             return field == None
 
@@ -119,16 +120,25 @@ class QueryOps(namedtuple("QueryOps", "uiformatter")):
         return (field == False) | (field == None)
 
     def op_startswith(self, field, value):
-        if self.uiformatter is not None and isinstance(self.uiformatter.fields[0], CNNField) and len(self.uiformatter.fields) == 1:
-            value = value.lstrip('0')
-            return field.op('REGEXP')("^0*" + value)
+        if (
+            self.uiformatter is not None
+            and isinstance(self.uiformatter.fields[0], CNNField)
+            and len(self.uiformatter.fields) == 1
+        ):
+            value = value.lstrip("0")
+            return field.op("REGEXP")("^0*" + value)
         else:
             return field.like(value + "%")
 
+    def op_endswith(self, field, value):
+        return field.like("%" + value)
+
     def op_age_range(self, field, value, query, is_strict=False):
-        values = [self.format(v.strip()) for v in value.split(',')[:2]]
+        values = [self.format(v.strip()) for v in value.split(",")[:2]]
         start_time, end_time = float(values[0]), float(values[1])
-        geo_time_co_ids = geo_time_query(start_time, end_time, require_full_overlap=is_strict, query=query.query)
+        geo_time_co_ids = geo_time_query(
+            start_time, end_time, require_full_overlap=is_strict, query=query.query
+        )
         if isinstance(geo_time_co_ids, Query):
             return geo_time_co_ids
         else:
@@ -136,7 +146,9 @@ class QueryOps(namedtuple("QueryOps", "uiformatter")):
 
     def op_age_period(self, field, value, query, is_strict=False):
         time_period_name = value
-        geo_time_co_ids = geo_time_period_query(time_period_name, require_full_overlap=is_strict, query=query.query)
+        geo_time_co_ids = geo_time_period_query(
+            time_period_name, require_full_overlap=is_strict, query=query.query
+        )
         if isinstance(geo_time_co_ids, Query):
             return geo_time_co_ids
         else:
