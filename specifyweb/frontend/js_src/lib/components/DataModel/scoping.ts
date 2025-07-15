98833b37057fbfc50122020ddaa92d486bea9da4
melton-jason <meltonjason12@yahoo.com>
2025-07-14 19:54:36 -0500

    Initialize DependentCollection before setting automatically created resources
    See #6578, #7069

--- specifyweb/frontend/js_src/lib/components/DataModel/scoping.ts
+++ specifyweb/frontend/js_src/lib/components/DataModel/scoping.ts
@@ -9,7 +9,6 @@ import type { AnySchema } from './helperTypes';
 import type { SpecifyResource } from './legacyTypes';
 import { getResourceApiUrl, idFromUrl } from './resource';
 import { schema } from './schema';
-import { serializeResource } from './serializers';
 import type { Relationship } from './specifyField';
 import type { SpecifyTable } from './specifyTable';
 import { strictGetTable, tables } from './tables';
@@ -52,9 +51,10 @@ export function initializeResource(resource: SpecifyResource<AnySchema>): void {
     resource.createdBy !== 'clone'
   ) {
     if (collectionObject.getDependentResource('preparations') === undefined)
-      collectionObject.set('preparations', [
-        serializeResource(new tables.Preparation.Resource()),
-      ]);
+      // This is needed to initialize the DependentCollection on the resource
+      // See pulls #6581 and #7073
+      // REFACTOR: generalize this and move it to resourceApi.ts
+      collectionObject.set('preparations', []);
     const preps = defined(
       collectionObject.getDependentResource('preparations')
     );
@@ -66,11 +66,12 @@ export function initializeResource(resource: SpecifyResource<AnySchema>): void {
     hasTablePermission('Determination', 'create') &&
     resource.createdBy !== 'clone'
   ) {
-    if (collectionObject.getDependentResource('determinations') === undefined) {
-      collectionObject.set('determinations', [
-        serializeResource(new tables.Determination.Resource()),
-      ]);
-    }
+    if (collectionObject.getDependentResource('determinations') === undefined)
+      // This is needed to initialize the DependentCollection on the resource
+      // See pulls #6581 and #7073
+      // REFACTOR: generalize this and move it to resourceApi.ts
+      collectionObject.set('determinations', []);
+
     const determinations = defined(
       collectionObject.getDependentResource('determinations')
     );
