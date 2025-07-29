cc9e2361710071064c765a45331a1a58ff823fef
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2025-07-29 14:27:55 -0400

    Add backend def to retrieve unique catnum coll pref

--- specifyweb/specify/utils.py
+++ specifyweb/specify/utils.py
@@ -158,3 +158,31 @@ def get_parent_cat_num_inheritance_setting(collection, user) -> bool:
         logger.warning(f"An unexpected error occurred: {e}")
     
     return parent_inheritance_enabled
+
+def get_unique_catnum_across_comp_co_coll_pref(collection, user) -> bool:
+    import specifyweb.context.app_resource as app_resource
+
+    unique_catnum_enabled: bool = False  
+
+    try:
+        collection_prefs_json, _, __ = app_resource.get_app_resource(collection, user, 'CollectionPreferences')
+
+        if collection_prefs_json is not None:
+            collection_prefs_dict = json.loads(collection_prefs_json)
+
+            unique_catalog_number_pref = collection_prefs_dict.get('catalogNumberParentInheritance', {})
+            behavior = unique_catalog_number_pref.get('behavior', {}) \
+                if isinstance(unique_catalog_number_pref, dict) else {}
+            unique_catnum_enabled = behavior.get('inheritance', False) if isinstance(behavior, dict) else False
+
+            if not isinstance(unique_catnum_enabled, bool):
+                unique_catnum_enabled = False
+
+    except json.JSONDecodeError:
+        logger.warning(f"Error: Could not decode JSON for collection preferences")
+    except TypeError as e:
+        logger.warning(f"Error: Unexpected data structure in collection preferences: {e}")
+    except Exception as e:
+        logger.warning(f"An unexpected error occurred: {e}")
+    
+    return unique_catnum_enabled
