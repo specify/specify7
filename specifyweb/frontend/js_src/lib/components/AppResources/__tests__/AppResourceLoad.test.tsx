a3a50cc36eac3dd21c752684e8d18ae998aff2e6
realVinayak <realvinayak123@gmail.com>
2025-07-30 02:46:49 +0000

    Lint code with ESLint and Prettier
    Triggered by b72677e5e8783e26d6e033d9afc3ea660f9903ad on branch refs/heads/issue-7257

--- specifyweb/frontend/js_src/lib/components/AppResources/__tests__/AppResourceLoad.test.tsx
+++ specifyweb/frontend/js_src/lib/components/AppResources/__tests__/AppResourceLoad.test.tsx
@@ -1,63 +1,66 @@
-import { fireEvent, render, waitFor } from "@testing-library/react";
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import React from 'react';
 
-import { clearIdStore } from "../../../hooks/useId";
+import { clearIdStore } from '../../../hooks/useId';
 import { mount } from '../../../tests/reactUtils';
-import { f } from "../../../utils/functools";
-import { LoadingContext } from "../../Core/Contexts";
+import { f } from '../../../utils/functools';
+import { LoadingContext } from '../../Core/Contexts';
 import { UnloadProtectsContext } from '../../Router/UnloadProtect';
 import { AppResourceLoad } from '../EditorComponents';
 
 beforeEach(() => {
-    clearIdStore();
+  clearIdStore();
 });
 
+describe('AppResourceLoad', () => {
+  test('simple render', () => {
+    const handleLoaded = jest.fn();
+    const { asFragment } = render(
+      <LoadingContext.Provider value={f.void}>
+        <AppResourceLoad onLoaded={handleLoaded} />
+      </LoadingContext.Provider>
+    );
 
-describe("AppResourceLoad", () => {
+    expect(handleLoaded).not.toHaveBeenCalled();
+    expect(asFragment()).toMatchSnapshot();
+  });
 
-    test("simple render", () => {
-        const handleLoaded = jest.fn();
-        const { asFragment } = render(
-            <LoadingContext.Provider value={f.void}>
-                <AppResourceLoad onLoaded={handleLoaded} />
-            </LoadingContext.Provider>
-        );
+  test('load file', async () => {
+    const handleLoaded = jest.fn();
+    const { getByRole, user } = mount(
+      <UnloadProtectsContext.Provider value={[]}>
+        <LoadingContext.Provider value={f.void}>
+          <AppResourceLoad onLoaded={handleLoaded} />
+        </LoadingContext.Provider>
+      </UnloadProtectsContext.Provider>
+    );
 
-        expect(handleLoaded).not.toHaveBeenCalled();
-        expect(asFragment()).toMatchSnapshot();
+    await user.click(getByRole('button'));
+
+    await waitFor(() => {
+      getByRole('dialog');
     });
 
-    test("load file", async () => {
-        const handleLoaded = jest.fn();
-        const { getByRole, user } = mount(
-            <UnloadProtectsContext.Provider value={[]}>
-                <LoadingContext.Provider value={f.void}>
-                    <AppResourceLoad onLoaded={handleLoaded} />
-                </LoadingContext.Provider>
-            </UnloadProtectsContext.Provider>
-        );
+    expect(getByRole('heading').textContent).toMatchInlineSnapshot(
+      `"Load File"`
+    );
+    const input = Array.from(
+      getByRole('dialog').getElementsByTagName('input')
+    )[0];
 
-        await user.click(getByRole('button'));
+    const testFile = new File(['Some Text Contents'], 'testName', {
+      type: 'text/plain',
+    });
 
-        await waitFor(() => {
-            getByRole('dialog');
-        });
+    await user.upload(input, testFile);
+    fireEvent.change(input, { target: { files: [testFile] } });
 
-        expect(getByRole('heading').textContent).toMatchInlineSnapshot(`"Load File"`);
-        const input = (Array.from(getByRole('dialog').getElementsByTagName('input')))[0];
-
-        const testFile = new File(['Some Text Contents'], "testName", {
-            type: 'text/plain',
-        });
-
-        await user.upload(input, testFile);
-        fireEvent.change(input, { target: { files: [testFile] } });
-
-        await waitFor(() => {
-            expect(handleLoaded).toHaveBeenCalled()
-            expect(handleLoaded.mock.lastCall).toEqual(['Some Text Contents', 'text/plain']);
-        });
-
-    })
-
-})
+    await waitFor(() => {
+      expect(handleLoaded).toHaveBeenCalled();
+      expect(handleLoaded.mock.lastCall).toEqual([
+        'Some Text Contents',
+        'text/plain',
+      ]);
+    });
+  });
+});
--- specifyweb/frontend/js_src/lib/components/AppResources/__tests__/useIndent.test.ts
+++ specifyweb/frontend/js_src/lib/components/AppResources/__tests__/useIndent.test.ts
@@ -1,30 +1,28 @@
-import { renderHook } from "@testing-library/react";
-import { useIndent } from "../EditorComponents";
-import { userPreferences } from "../../Preferences/userPreferences";
-import { act } from "react-dom/test-utils";
+import { renderHook } from '@testing-library/react';
+import { useIndent } from '../EditorComponents';
+import { userPreferences } from '../../Preferences/userPreferences';
+import { act } from 'react-dom/test-utils';
 
-describe("useIndent", ()=>{
+describe('useIndent', () => {
+  test('simple render', () => {
+    const { result } = renderHook(useIndent);
+    expect(result.current).toMatchInlineSnapshot(`"  "`);
+  });
 
-    test('simple render', ()=>{
+  test('changes on updates', async () => {
+    userPreferences.set('appResources', 'behavior', 'indentSize', 4);
+    userPreferences.set('appResources', 'behavior', 'indentWithTab', false);
 
-        const {result} = renderHook(useIndent);
-        expect(result.current).toMatchInlineSnapshot(`"  "`);
+    const { result } = renderHook(useIndent);
+    const initialValue = result.current;
+
+    await act(() => {
+      userPreferences.set('appResources', 'behavior', 'indentSize', 5);
     });
 
-    test('changes on updates', async ()=>{
-        userPreferences.set('appResources', 'behavior', 'indentSize', 4);
-        userPreferences.set('appResources', 'behavior', 'indentWithTab', false);
+    const laterValue = result.current;
+    expect(initialValue).not.toBe(laterValue);
 
-        const {result} = renderHook(useIndent);
-        const initialValue = result.current;
-
-        await act(()=>{
-            userPreferences.set('appResources', 'behavior', 'indentSize', 5);
-        })
-        
-        const laterValue = result.current;
-        expect(initialValue).not.toBe(laterValue);
-
-        expect(laterValue).toMatchInlineSnapshot(`"     "`);
-    });
+    expect(laterValue).toMatchInlineSnapshot(`"     "`);
+  });
 });
