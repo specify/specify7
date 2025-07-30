4a4c66c4c780f4e67c1170d2751a7ddcc7968e8e
realVinayak <realvinayak123@gmail.com>
2025-07-30 23:04:56 +0000

    Lint code with ESLint and Prettier
    Triggered by f056251463fac1140a31e3cebbe934b13d84a2cf on branch refs/heads/issue-7266-instrument

--- specifyweb/frontend/js_src/lib/components/AppResources/__tests__/AppResourcesFilters.test.tsx
+++ specifyweb/frontend/js_src/lib/components/AppResources/__tests__/AppResourcesFilters.test.tsx
@@ -1,112 +1,102 @@
-import React from "react";
+import React from 'react';
 
-import { mount } from "../../../tests/reactUtils";
-import { getCache, setCache } from "../../../utils/cache";
-import { UnloadProtectsContext } from "../../Router/UnloadProtect";
-import { AppResourcesFilters } from "../Filters";
-import { testAppResources } from "./testAppResources";
+import { mount } from '../../../tests/reactUtils';
+import { getCache, setCache } from '../../../utils/cache';
+import { UnloadProtectsContext } from '../../Router/UnloadProtect';
+import { AppResourcesFilters } from '../Filters';
+import { testAppResources } from './testAppResources';
 
 beforeEach(() => {
-    setCache('appResources', 'filters', { appResources: [], viewSets: false }, false);
-})
+  setCache(
+    'appResources',
+    'filters',
+    { appResources: [], viewSets: false },
+    false
+  );
+});
 
-describe("AppResourcesFilters", () => {
+describe('AppResourcesFilters', () => {
+  test('simple render', () => {
+    const { asFragment } = mount(
+      <AppResourcesFilters initialResources={testAppResources} />
+    );
 
-    test("simple render", () => {
+    expect(asFragment()).toMatchSnapshot();
+  });
 
-        const { asFragment } = mount(
-            <AppResourcesFilters initialResources={testAppResources} />
-        );
+  test('selecting viewsets', async () => {
+    const { getAllByRole, user } = mount(
+      <AppResourcesFilters initialResources={testAppResources} />
+    );
 
-        expect(asFragment()).toMatchSnapshot();
-    })
+    const button = getAllByRole('button')[0];
 
-    test("selecting viewsets", async () => {
+    await user.click(button);
 
-        const { getAllByRole, user } = mount(
-            <AppResourcesFilters initialResources={testAppResources} />
-        );
-
-        const button = getAllByRole('button')[0];
-
-        await user.click(button);
-
-        const filters = getCache('appResources', 'filters');
-        expect(filters).toEqual({
-            'appResources': [],
-            'viewSets': true
-        });
-
-
+    const filters = getCache('appResources', 'filters');
+    expect(filters).toEqual({
+      appResources: [],
+      viewSets: true,
     });
+  });
 
-    test("selecting app resources", async () => {
+  test('selecting app resources', async () => {
+    const { getAllByRole, user } = mount(
+      <AppResourcesFilters initialResources={testAppResources} />
+    );
 
-        const { getAllByRole, user } = mount(
-            <AppResourcesFilters initialResources={testAppResources} />
-        );
+    const button = getAllByRole('button')[1];
 
-        const button = getAllByRole('button')[1];
+    await user.click(button);
 
-        await user.click(button);
-
-        const filters = getCache('appResources', 'filters');
-        expect(filters).toEqual(
-            {
-                viewSets: false,
-                appResources: [
-                    'collectionPreferences',
-                    'dataEntryTables',
-                    'dataObjectFormatters',
-                    'defaultUserPreferences',
-                    'expressSearchConfig',
-                    'interactionsTables',
-                    'label',
-                    'leafletLayers',
-                    'otherAppResources',
-                    'otherJsonResource',
-                    'otherPropertiesResource',
-                    'otherXmlResource',
-                    'report',
-                    'rssExportFeed',
-                    'typeSearches',
-                    'uiFormatters',
-                    'userPreferences',
-                    'webLinks'
-                ]
-            }
-        );
-
+    const filters = getCache('appResources', 'filters');
+    expect(filters).toEqual({
+      viewSets: false,
+      appResources: [
+        'collectionPreferences',
+        'dataEntryTables',
+        'dataObjectFormatters',
+        'defaultUserPreferences',
+        'expressSearchConfig',
+        'interactionsTables',
+        'label',
+        'leafletLayers',
+        'otherAppResources',
+        'otherJsonResource',
+        'otherPropertiesResource',
+        'otherXmlResource',
+        'report',
+        'rssExportFeed',
+        'typeSearches',
+        'uiFormatters',
+        'userPreferences',
+        'webLinks',
+      ],
     });
+  });
 
-    test("custom app resource filter", async () => {
+  test('custom app resource filter', async () => {
+    const { user, getAllByRole } = mount(
+      <UnloadProtectsContext.Provider value={[]}>
+        <AppResourcesFilters initialResources={testAppResources} />
+      </UnloadProtectsContext.Provider>
+    );
 
-        const { user, getAllByRole } = mount(
-            <UnloadProtectsContext.Provider value={[]}>
-                <AppResourcesFilters initialResources={testAppResources} />
-            </UnloadProtectsContext.Provider>
-        );
+    const button = getAllByRole('button')[2];
 
-        const button = getAllByRole('button')[2];
+    await user.click(button);
 
-        await user.click(button);
+    const checkBoxes = getAllByRole('checkbox');
 
-        const checkBoxes = getAllByRole('checkbox');
+    await user.click(checkBoxes[0]);
 
-        await user.click(checkBoxes[0]);
+    await user.click(checkBoxes[2]);
+    await user.click(checkBoxes[3]);
 
-        await user.click(checkBoxes[2]);
-        await user.click(checkBoxes[3]);
-
-        const filters = getCache('appResources', 'filters');
-        expect(filters).toEqual(
-            {
-                appResources: [
-                    'label',
-                    'report'
-                ],
-                viewSets: true
-            }
-        );
+    const filters = getCache('appResources', 'filters');
+    expect(filters).toEqual({
+      appResources: ['label', 'report'],
+      viewSets: true,
     });
+  });
 });
--- specifyweb/frontend/js_src/lib/components/AppResources/__tests__/AppResourcesTab.test.tsx
+++ specifyweb/frontend/js_src/lib/components/AppResources/__tests__/AppResourcesTab.test.tsx
@@ -1,69 +1,68 @@
-import React from "react";
-import { mount } from "../../../tests/reactUtils";
-import { f } from "../../../utils/functools";
-import { localized } from "../../../utils/types";
-import { deserializeResource } from "../../DataModel/serializers";
-import { AppResourcesTab } from "../Tabs";
-import { testAppResources } from "./testAppResources";
-import { requireContext } from "../../../tests/helpers";
-import { Button } from "../../Atoms/Button";
-import { AppResourceTabProps } from "../TabDefinitions";
-import { UnloadProtectsContext } from "../../Router/UnloadProtect";
-import { clearIdStore } from "../../../hooks/useId";
+import React from 'react';
 
+import { clearIdStore } from '../../../hooks/useId';
+import { requireContext } from '../../../tests/helpers';
+import { mount } from '../../../tests/reactUtils';
+import { f } from '../../../utils/functools';
+import { localized } from '../../../utils/types';
+import { Button } from '../../Atoms/Button';
+import { deserializeResource } from '../../DataModel/serializers';
+import { UnloadProtectsContext } from '../../Router/UnloadProtect';
+import type { AppResourceTabProps } from '../TabDefinitions';
+import { AppResourcesTab } from '../Tabs';
+import { testAppResources } from './testAppResources';
 
 beforeEach(() => {
-    clearIdStore();
+  clearIdStore();
 });
 
 requireContext();
 
 function Component(props: AppResourceTabProps) {
-    return <h1>Data: {props.data}</h1>
+  return <h1>Data: {props.data}</h1>;
 }
 
-describe("AppResourcesTab", () => {
+describe('AppResourcesTab', () => {
+  test('simple render', () => {
+    const { asFragment } = mount(
+      <AppResourcesTab
+        appResource={deserializeResource(testAppResources.appResources[0])}
+        data="TestData"
+        directory={testAppResources.directories[0]}
+        footer={<h3>TestFooter</h3>}
+        headerButtons={<Button.Info onClick={f.void} />}
+        isFullScreen={[false, f.void]}
+        label={localized('test')}
+        resource={testAppResources.appResources[0]}
+        tab={Component}
+        onChange={f.void}
+        onSetCleanup={f.void}
+      />
+    );
 
-    test("simple render", () => {
+    expect(asFragment()).toMatchSnapshot();
+  });
 
-        const { asFragment } = mount(<AppResourcesTab
-            tab={Component}
-            label={localized("test")}
-            appResource={deserializeResource(testAppResources.appResources[0])}
-            resource={testAppResources.appResources[0]}
-            directory={testAppResources.directories[0]}
-            data={"TestData"}
-            isFullScreen={[false, f.void]}
-            onChange={f.void}
-            onSetCleanup={f.void}
-            footer={<h3>TestFooter</h3>}
-            headerButtons={<Button.Info onClick={f.void} />}
-        />);
+  test('dialog render', () => {
+    const { getByRole } = mount(
+      <UnloadProtectsContext.Provider value={[]}>
+        <AppResourcesTab
+          appResource={deserializeResource(testAppResources.appResources[0])}
+          data="TestData"
+          directory={testAppResources.directories[0]}
+          footer={<h3>TestFooter</h3>}
+          headerButtons={<Button.Info onClick={f.void} />}
+          isFullScreen={[true, f.void]}
+          label={localized('test')}
+          resource={testAppResources.appResources[0]}
+          tab={Component}
+          onChange={f.void}
+          onSetCleanup={f.void}
+        />
+      </UnloadProtectsContext.Provider>
+    );
 
-        expect(asFragment()).toMatchSnapshot();
-    });
-
-    test("dialog render", () => {
-
-        const { getByRole } = mount(
-            <UnloadProtectsContext.Provider value={[]}>
-                <AppResourcesTab
-                    tab={Component}
-                    label={localized("test")}
-                    appResource={deserializeResource(testAppResources.appResources[0])}
-                    resource={testAppResources.appResources[0]}
-                    directory={testAppResources.directories[0]}
-                    data={"TestData"}
-                    isFullScreen={[true, f.void]}
-                    onChange={f.void}
-                    onSetCleanup={f.void}
-                    footer={<h3>TestFooter</h3>}
-                    headerButtons={<Button.Info onClick={f.void} />}
-                />
-            </UnloadProtectsContext.Provider>
-        );
-
-        const dialog = getByRole('dialog');
-        expect(dialog).toMatchSnapshot();
-    });
+    const dialog = getByRole('dialog');
+    expect(dialog).toMatchSnapshot();
+  });
 });
