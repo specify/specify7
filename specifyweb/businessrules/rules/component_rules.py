8f322e69cbef4c250706602ced41e5e58674691e
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2025-07-31 14:45:23 -0400

    Handle null user

--- specifyweb/businessrules/rules/component_rules.py
+++ specifyweb/businessrules/rules/component_rules.py
@@ -6,16 +6,18 @@ from specifyweb.specify.utils import get_unique_catnum_across_comp_co_coll_pref
 
 @orm_signal_handler('pre_save', 'Component')
 def component_pre_save(comp):
-    unique_catnum_across_comp_co_coll_pref = get_unique_catnum_across_comp_co_coll_pref(comp.collectionobject.collection, comp.createdbyagent.specifyuser)
+    agent = comp.createdbyagent
+    if agent is not None and agent.specifyuser is not None:
+        unique_catnum_across_comp_co_coll_pref = get_unique_catnum_across_comp_co_coll_pref(comp.collectionobject.collection, comp.createdbyagent.specifyuser)
 
-    if unique_catnum_across_comp_co_coll_pref: 
-        if comp.catalognumber is not None:
-            contains_co_duplicates = Collectionobject.objects.filter(
-            catalognumber=comp.catalognumber).exclude(pk=comp.pk).exists()
+        if unique_catnum_across_comp_co_coll_pref: 
+            if comp.catalognumber is not None:
+                contains_co_duplicates = Collectionobject.objects.filter(
+                catalognumber=comp.catalognumber).exclude(pk=comp.pk).exists()
 
-            contains_component_duplicates = Component.objects.filter(
-            catalognumber=comp.catalognumber).exclude(pk=comp.pk).exists()
+                contains_component_duplicates = Component.objects.filter(
+                catalognumber=comp.catalognumber).exclude(pk=comp.pk).exists()
 
-            if contains_co_duplicates or contains_component_duplicates: 
-                raise BusinessRuleException(
-                    'Catalog Number is already in use for another Collection Object or Component in this collection.')
+                if contains_co_duplicates or contains_component_duplicates: 
+                    raise BusinessRuleException(
+                        'Catalog Number is already in use for another Collection Object or Component in this collection.')
