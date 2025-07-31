94bcb04a618d07fbd19c4502d100a887cbcd8c87
realVinayak <realvinayak123@gmail.com>
2025-07-31 00:02:42 +0000

    Lint code with ESLint and Prettier
    Triggered by 5e907defea0e48c2422e4b528df332122060d073 on branch refs/heads/issue-7268-instrument

--- specifyweb/frontend/js_src/lib/components/AppResources/__tests__/OtherCollectionWarning.test.tsx
+++ specifyweb/frontend/js_src/lib/components/AppResources/__tests__/OtherCollectionWarning.test.tsx
@@ -1,71 +1,78 @@
-import { render } from "@testing-library/react";
-import { requireContext } from "../../../tests/helpers";
-import { exportsForTests } from "../Tabs";
-import React from "react";
-import { testAppResources } from "./testAppResources";
+import { render } from '@testing-library/react';
+import React from 'react';
+
+import { requireContext } from '../../../tests/helpers';
+import { exportsForTests } from '../Tabs';
+import { testAppResources } from './testAppResources';
 
 const { OtherCollectionWarning } = exportsForTests;
 
 requireContext();
 
+describe('OtherCollectionWarning', () => {
+  test('(no collection, no discipline)', () => {
+    const { asFragment } = render(
+      <OtherCollectionWarning directory={testAppResources.directories[0]} />
+    );
+    expect(asFragment()).toMatchInlineSnapshot(`<DocumentFragment />`);
+  });
 
-describe("OtherCollectionWarning", () => {
+  test('(same collection)', () => {
+    const { asFragment } = render(
+      <OtherCollectionWarning directory={testAppResources.directories[2]} />
+    );
+    expect(asFragment()).toMatchInlineSnapshot(`<DocumentFragment />`);
+  });
 
-    test('(no collection, no discipline)', () => {
+  test('(no collection, same discipline)', () => {
+    const { asFragment } = render(
+      <OtherCollectionWarning
+        directory={{ ...testAppResources.directories[2], collection: null }}
+      />
+    );
+    expect(asFragment()).toMatchInlineSnapshot(`<DocumentFragment />`);
+  });
 
-        const { asFragment } = render(
-            <OtherCollectionWarning directory={testAppResources.directories[0]} />
-        );
-        expect(asFragment()).toMatchInlineSnapshot(`<DocumentFragment />`);
-    });
+  test('(different collection, same discipline)', () => {
+    const { asFragment } = render(
+      <OtherCollectionWarning
+        directory={{
+          ...testAppResources.directories[2],
+          collection: '/api/specify/collection/909/',
+        }}
+      />
+    );
+    expect(asFragment()).toMatchInlineSnapshot(`
+          <DocumentFragment>
+            <div
+              class="flex flex-col gap-2 p-2 text-white bg-orange-500 dark:bg-transparent border border-orange-500 rounded"
+              role="alert"
+            >
+              This resource belongs to a different collection/discipline than the one you are currently in. It's recommended to switch collection before editing this resource
+            </div>
+          </DocumentFragment>
+        `);
+  });
 
-    test('(same collection)', () => {
-
-        const { asFragment } = render(
-            <OtherCollectionWarning directory={testAppResources.directories[2]} />
-        );
-        expect(asFragment()).toMatchInlineSnapshot(`<DocumentFragment />`);
-    });
-
-    test('(no collection, same discipline)', () => {
-
-        const { asFragment } = render(
-            <OtherCollectionWarning directory={{ ...testAppResources.directories[2], collection: null }} />
-        );
-        expect(asFragment()).toMatchInlineSnapshot(`<DocumentFragment />`);
-    });
-
-    test('(different collection, same discipline)', () => {
-
-        const { asFragment } = render(
-            <OtherCollectionWarning directory={{ ...testAppResources.directories[2], collection: '/api/specify/collection/909/' }} />
-        );
-        expect(asFragment()).toMatchInlineSnapshot(`
-<DocumentFragment>
-  <div
-    class="flex flex-col gap-2 p-2 text-white bg-orange-500 dark:bg-transparent border border-orange-500 rounded"
-    role="alert"
-  >
-    This resource belongs to a different collection/discipline than the one you are currently in. It's recommended to switch collection before editing this resource
-  </div>
-</DocumentFragment>
-`);
-    });
-
-    test('(no collection, different discipline)', () => {
-
-        const { asFragment } = render(
-            <OtherCollectionWarning directory={{ ...testAppResources.directories[2], discipline: '/api/specify/discipline/909/', collection: null }} />
-        );
-        expect(asFragment()).toMatchInlineSnapshot(`
-<DocumentFragment>
-  <div
-    class="flex flex-col gap-2 p-2 text-white bg-orange-500 dark:bg-transparent border border-orange-500 rounded"
-    role="alert"
-  >
-    This resource belongs to a different collection/discipline than the one you are currently in. It's recommended to switch collection before editing this resource
-  </div>
-</DocumentFragment>
-`);
-    });
-})
+  test('(no collection, different discipline)', () => {
+    const { asFragment } = render(
+      <OtherCollectionWarning
+        directory={{
+          ...testAppResources.directories[2],
+          discipline: '/api/specify/discipline/909/',
+          collection: null,
+        }}
+      />
+    );
+    expect(asFragment()).toMatchInlineSnapshot(`
+          <DocumentFragment>
+            <div
+              class="flex flex-col gap-2 p-2 text-white bg-orange-500 dark:bg-transparent border border-orange-500 rounded"
+              role="alert"
+            >
+              This resource belongs to a different collection/discipline than the one you are currently in. It's recommended to switch collection before editing this resource
+            </div>
+          </DocumentFragment>
+        `);
+  });
+});
