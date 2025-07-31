c2ea4385275f5278548b7a0cd5b4e92a018f68f1
Caroline D. <108160931+CarolineDenis@users.noreply.github.com>
2025-07-31 12:53:08 +0000

    Lint code with ESLint and Prettier
    Triggered by a70d51f6c7f82d7d13e34809a7302afcc6bba9a3 on branch refs/heads/issue-7266

--- specifyweb/frontend/js_src/lib/components/AppResources/__tests__/OtherCollectionWarning.test.tsx
+++ specifyweb/frontend/js_src/lib/components/AppResources/__tests__/OtherCollectionWarning.test.tsx
@@ -1,47 +1,48 @@
-import { render } from "@testing-library/react";
-import React from "react";
+import { render } from '@testing-library/react';
+import React from 'react';
 
-import { requireContext } from "../../../tests/helpers";
-import { exportsForTests } from "../Tabs";
-import { testAppResources } from "./testAppResources";
+import { requireContext } from '../../../tests/helpers';
+import { exportsForTests } from '../Tabs';
+import { testAppResources } from './testAppResources';
 
 const { OtherCollectionWarning } = exportsForTests;
 
 requireContext();
 
+describe('OtherCollectionWarning', () => {
+  test('(no collection, no discipline)', () => {
+    const { asFragment } = render(
+      <OtherCollectionWarning directory={testAppResources.directories[0]} />
+    );
+    expect(asFragment()).toMatchInlineSnapshot(`<DocumentFragment />`);
+  });
 
-describe("OtherCollectionWarning", () => {
+  test('(same collection)', () => {
+    const { asFragment } = render(
+      <OtherCollectionWarning directory={testAppResources.directories[2]} />
+    );
+    expect(asFragment()).toMatchInlineSnapshot(`<DocumentFragment />`);
+  });
 
-    test('(no collection, no discipline)', () => {
+  test('(no collection, same discipline)', () => {
+    const { asFragment } = render(
+      <OtherCollectionWarning
+        directory={{ ...testAppResources.directories[2], collection: null }}
+      />
+    );
+    expect(asFragment()).toMatchInlineSnapshot(`<DocumentFragment />`);
+  });
 
-        const { asFragment } = render(
-            <OtherCollectionWarning directory={testAppResources.directories[0]} />
-        );
-        expect(asFragment()).toMatchInlineSnapshot(`<DocumentFragment />`);
-    });
-
-    test('(same collection)', () => {
-
-        const { asFragment } = render(
-            <OtherCollectionWarning directory={testAppResources.directories[2]} />
-        );
-        expect(asFragment()).toMatchInlineSnapshot(`<DocumentFragment />`);
-    });
-
-    test('(no collection, same discipline)', () => {
-
-        const { asFragment } = render(
-            <OtherCollectionWarning directory={{ ...testAppResources.directories[2], collection: null }} />
-        );
-        expect(asFragment()).toMatchInlineSnapshot(`<DocumentFragment />`);
-    });
-
-    test('(different collection, same discipline)', () => {
-
-        const { asFragment } = render(
-            <OtherCollectionWarning directory={{ ...testAppResources.directories[2], collection: '/api/specify/collection/909/' }} />
-        );
-        expect(asFragment()).toMatchInlineSnapshot(`
+  test('(different collection, same discipline)', () => {
+    const { asFragment } = render(
+      <OtherCollectionWarning
+        directory={{
+          ...testAppResources.directories[2],
+          collection: '/api/specify/collection/909/',
+        }}
+      />
+    );
+    expect(asFragment()).toMatchInlineSnapshot(`
           <DocumentFragment>
             <div
               class="flex flex-col gap-2 p-2 text-white bg-orange-500 dark:bg-transparent border border-orange-500 rounded"
@@ -51,14 +52,19 @@ describe("OtherCollectionWarning", () => {
             </div>
           </DocumentFragment>
         `);
-    });
+  });
 
-    test('(no collection, different discipline)', () => {
-
-        const { asFragment } = render(
-            <OtherCollectionWarning directory={{ ...testAppResources.directories[2], discipline: '/api/specify/discipline/909/', collection: null }} />
-        );
-        expect(asFragment()).toMatchInlineSnapshot(`
+  test('(no collection, different discipline)', () => {
+    const { asFragment } = render(
+      <OtherCollectionWarning
+        directory={{
+          ...testAppResources.directories[2],
+          discipline: '/api/specify/discipline/909/',
+          collection: null,
+        }}
+      />
+    );
+    expect(asFragment()).toMatchInlineSnapshot(`
           <DocumentFragment>
             <div
               class="flex flex-col gap-2 p-2 text-white bg-orange-500 dark:bg-transparent border border-orange-500 rounded"
@@ -68,5 +74,5 @@ describe("OtherCollectionWarning", () => {
             </div>
           </DocumentFragment>
         `);
-    });
-})
+  });
+});
