9f9994fc3d684eca2a3b4b710c687b3d377e6717
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2025-08-19 13:24:04 -0400

    test

--- specifyweb/frontend/js_src/lib/components/Attachments/__tests__/UploadAttachment.test.tsx
+++ specifyweb/frontend/js_src/lib/components/Attachments/__tests__/UploadAttachment.test.tsx
@@ -1,68 +1,63 @@
-import { fireEvent, waitFor } from "@testing-library/react";
-import React from "react";
-
-import { clearIdStore } from "../../../hooks/useId";
-import { overrideAjax } from "../../../tests/ajax";
+import React from 'react';
+import { mount } from '../../../tests/reactUtils';
+import { UploadAttachment } from '../Plugin';
+import { clearIdStore } from '../../../hooks/useId';
+import { LoadingContext } from '../../Core/Contexts';
+import { f } from '../../../utils/functools';
+import { fireEvent, waitFor } from '@testing-library/react';
+import { overrideAttachmentSettings } from '../attachments';
 import attachmentSettings from '../../../tests/ajax/static/context/attachment_settings.json';
-import { requireContext } from "../../../tests/helpers";
-import { mount } from "../../../tests/reactUtils";
-import { f } from "../../../utils/functools";
-import { LoadingContext } from "../../Core/Contexts";
-import { SpecifyResource } from "../../DataModel/legacyTypes";
-import { deserializeResource } from "../../DataModel/serializers";
-import { Attachment } from "../../DataModel/types";
-import { overrideAttachmentSettings } from "../attachments";
-import * as Attachments from "../attachments";
-import { UploadAttachment } from "../Plugin";
-import { testAttachment } from "./utils";
+import { overrideAjax } from '../../../tests/ajax';
+import * as Attachments from '../attachments';
+import { requireContext } from '../../../tests/helpers';
+import { deserializeResource } from '../../DataModel/serializers';
+import { testAttachment } from './utils';
+import { SpecifyResource } from '../../DataModel/legacyTypes';
+import { Attachment } from '../../DataModel/types';
 
 requireContext();
 
-
 async function uploadFileMock() {
-    return deserializeResource(testAttachment) ;
+  return deserializeResource(testAttachment) as SpecifyResource<Attachment>;
 }
 
 beforeEach(() => {
-    clearIdStore();
+  clearIdStore();
 });
 
-describe("UploadAttachment", () => {
+describe('UploadAttachment', () => {
+  const testToken = 'testToken';
+  const testAttachmentLocation = 'testLocation';
 
-    const testToken = 'testToken';
-    const testAttachmentLocation = 'testLocation';
+  overrideAjax(
+    `/attachment_gw/get_upload_params/`,
+    [{ token: testToken, attachmentLocation: testAttachmentLocation }],
+    { method: 'POST' }
+  );
 
-    overrideAjax(
-        `/attachment_gw/get_upload_params/`,
-        [{ token: testToken, attachmentLocation: testAttachmentLocation }],
-        { method: 'POST' }
+  test('simple render', async () => {
+    jest.spyOn(Attachments, 'uploadFile').mockImplementation(uploadFileMock);
+    jest.spyOn(console, 'warn').mockImplementation();
+    const handleUploaded = jest.fn();
+
+    overrideAttachmentSettings(attachmentSettings);
+    const { asFragment, container, user } = mount(
+      <LoadingContext.Provider value={f.void}>
+        <UploadAttachment onUploaded={handleUploaded} />
+      </LoadingContext.Provider>
     );
+    expect(asFragment()).toMatchSnapshot();
 
-    test("simple render", async () => {
-        jest.spyOn(Attachments, 'uploadFile').mockImplementation(uploadFileMock);
-        jest.spyOn(console, 'warn').mockImplementation();
-        const handleUploaded = jest.fn();
-
-        overrideAttachmentSettings(attachmentSettings);
-        const { asFragment, container, user } = mount(
-            <LoadingContext.Provider value={f.void}>
-                <UploadAttachment onUploaded={handleUploaded} />
-            </LoadingContext.Provider>
-        );
-        expect(asFragment()).toMatchSnapshot();
-
-        const input = Array.from(container.getElementsByTagName('input'))[0];
-        const testFile = new File(['Some Text Contents'], 'testName', {
-            type: 'text/plain',
-        });
-
-        await user.upload(input, testFile);
-        fireEvent.change(input, { target: { files: [testFile] } });
-
-        await waitFor(() => {
-            expect(handleUploaded).toHaveBeenCalled();
-        });
-
+    const input = Array.from(container.getElementsByTagName('input'))[0];
+    const testFile = new File(['Some Text Contents'], 'testName', {
+      type: 'text/plain',
     });
 
-})
+    await user.upload(input, testFile);
+    fireEvent.change(input, { target: { files: [testFile] } });
+
+    await waitFor(() => {
+      expect(handleUploaded).toBeCalled();
+    });
+  });
+});
