1c12d8f1fc9653db2e1fc3d39b5d423188025cf6
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2025-08-20 11:55:28 -0400

    [Fix]: Update beforeChange types in hooks to adhere new version

--- specifyweb/frontend/js_src/lib/components/WorkBench/hooks.ts
+++ specifyweb/frontend/js_src/lib/components/WorkBench/hooks.ts
@@ -1,4 +1,5 @@
 import type Handsontable from 'handsontable';
+import type { CellChange } from 'handsontable/common';
 import type { Events } from 'handsontable/pluginHooks';
 import type { Action } from 'handsontable/plugins/undoRedo';
 import React from 'react';
@@ -158,13 +159,21 @@ export function useHotHooks({
      * arguments that are inconvenient to work with
      *
      */
-    beforeChange: (unfilteredChanges, source) => {
+    beforeChange: (
+      unfilteredChanges: readonly (CellChange | null)[],
+      source
+    ) => {
       if (source !== 'CopyPaste.paste') return true;
       if (!unfilteredChanges) return true;
 
       const filteredChanges = unfilteredChanges.filter(
-        ([, property]) =>
-          (property as number) < workbench.dataset.columns.length
+        (change): change is CellChange => {
+          if (change === null) {
+            return false;
+          }
+          const [, property] = change;
+          return (property as number) < workbench.dataset.columns.length;
+        }
       );
 
       if (
