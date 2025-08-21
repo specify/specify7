9798033d61612863aea56e7b0cb6df09c504874c
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2025-08-21 08:49:46 -0400

    Add tree urls to main url file

--- specifyweb/urls.py
+++ specifyweb/urls.py
@@ -20,7 +20,7 @@ from .specify.views import images, properties
 from specifyweb.backend.stored_queries import urls as query_urls
 from specifyweb.backend.workbench import urls as wb_urls
 from specifyweb.backend.stats import urls as stat_urls
-
+from specifyweb.backend.trees import urls as trees_urls
 # print(get_resolver().reverse_dict.keys()) # Use for debugging urls
 
 urlpatterns = [
@@ -61,5 +61,6 @@ urlpatterns = [
     path('export/', include(export_urls)), # permissions added
     path('permissions/', include(permissions_urls)), # permissions added
     # url(r'^testcontext/', include()),
-    path('stats/', include(stat_urls))
+    path('stats/', include(stat_urls)),
+    path('trees/', include(trees_urls))
 ]
