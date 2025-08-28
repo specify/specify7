5a803b5184d0947768f37729a8c13c0b4d9ca1e8
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2025-08-28 13:56:19 -0400

    Fix import

--- specifyweb/backend/stored_queries/execution.py
+++ specifyweb/backend/stored_queries/execution.py
@@ -14,6 +14,7 @@ from django.db import transaction
 from django.utils import timezone
 from specifyweb.backend.inheritance.api import cog_inheritance_post_query_processing, parent_inheritance_post_query_processing
 from specifyweb.backend.inheritance.utils import get_parent_cat_num_inheritance_setting
+from specifyweb.backend.stored_queries.utils import log_sqlalchemy_query
 from sqlalchemy import sql, orm, func, text
 from sqlalchemy.sql.expression import asc, desc, insert, literal
 
@@ -30,7 +31,6 @@ from .field_spec_maps import apply_specify_user_name
 from specifyweb.backend.notifications.models import Message
 from specifyweb.backend.permissions.permissions import check_table_permissions
 from specifyweb.specify.models import Loan, Loanpreparation, Loanreturnpreparation, Taxontreedef
-from specifyweb.backend.stored_queries import log_sqlalchemy_query
 from specifyweb.backend.workbench.upload.auditlog import auditlog
 from specifyweb.backend.stored_queries.group_concat import group_by_displayed_fields
 from specifyweb.backend.stored_queries.queryfield import fields_from_json
