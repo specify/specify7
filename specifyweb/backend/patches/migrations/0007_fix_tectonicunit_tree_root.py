c6dfed0e5978a4371213084977fe6ca472b49158
alec_dev <acwhite211@gmail.com>
2025-09-02 09:34:58 -0500

    Fix case sensitivity in TectonicUnit migration

--- specifyweb/backend/patches/migrations/0007_fix_tectonicunit_tree_root.py
+++ specifyweb/backend/patches/migrations/0007_fix_tectonicunit_tree_root.py
@@ -1,15 +1,15 @@
 from django.db import migrations
 
 def set_tectonicunit_root_fullname(apps, schema_editor):
-    TectonicUnit = apps.get_model('specify', 'TectonicUnit')
+    TectonicUnit = apps.get_model('specify', 'Tectonicunit')
     if not TectonicUnit:
         return
 
     TectonicUnit.objects.filter(
         parent__isnull=True,
         name="Root",
-        fullName__isnull=True
-    ).update(fullName="Root")
+        fullname__isnull=True
+    ).update(fullname="Root")
 
 class Migration(migrations.Migration):
 
