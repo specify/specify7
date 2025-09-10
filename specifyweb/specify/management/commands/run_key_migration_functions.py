f22d8639105f4f4bddd75051c1849aea5871b87c
melton-jason <meltonjason12@yahoo.com>
2025-09-10 13:10:40 -0500

    feat(commands): improve output of command

--- specifyweb/specify/management/commands/run_key_migration_functions.py
+++ specifyweb/specify/management/commands/run_key_migration_functions.py
@@ -1,11 +1,12 @@
 import logging
+from typing import Any
+from collections.abc import Callable, Iterable
 from django.core.management.base import BaseCommand
 from django.apps import apps
 from django.db import transaction
 from specifyweb.backend.businessrules.migration_utils import catnum_rule_editable
 from specifyweb.backend.businessrules.uniqueness_rules import (
     apply_default_uniqueness_rules,
-    create_uniqueness_rule,
     fix_global_default_rules
 )
 from specifyweb.permissions.migration_utils.edit_permissions import add_permission, add_stats_edit_permission
@@ -26,42 +27,55 @@ from specifyweb.backend.patches.migration_utils import apply_migrations as apply
 
 logger = logging.getLogger(__name__)
 
-def fix_cots():
-    create_default_collection_types(apps)
-    create_default_discipline_for_tree_defs(apps)
-    create_cogtype_type_picklist(apps)
-    set_discipline_for_taxon_treedefs(apps)
-    fix_taxon_treedef_discipline_links(apps)
-    create_cotype_picklist(apps)
+MigrationFunction = Callable[[Any, Any | None], None]
 
-def fix_schema_config():
-    usc.create_geo_table_schema_config_with_defaults(apps) # specify 0002
-    usc.create_cotype_splocalecontaineritem(apps) # specify 0003
-    usc.create_strat_table_schema_config_with_defaults(apps) # specify 0004 - getting skip warnings
-    usc.create_agetype_picklist(apps) # specify 0004
-    usc.update_cog_type_fields(apps) # specify 0007
-    usc.create_cogtype_picklist(apps) # specify 0007
-    usc.update_cogtype_splocalecontaineritem(apps) # specify 0007
-    usc.update_systemcogtypes_picklist(apps) # specify 0007
-    usc.update_cogtype_type_splocalecontaineritem(apps) # specify 0007
-    usc.update_relative_age_fields(apps) # specify 0008
-    usc.add_cojo_to_schema_config(apps) # specify 0012
-    usc.update_cog_schema_config(apps) # specify 0013
-    usc.update_age_schema_config(apps) # specify 0015
-    usc.schemaconfig_fixes(apps) # specify 0017
-    usc.add_cot_catnum_to_schema(apps) # specify 0018
-    usc.add_tectonicunit_to_pc_in_schema_config(apps) # specify 0020
-    usc.fix_hidden_geo_prop(apps) # specify 0021
-    usc.update_schema_config_field_desc(apps) # specify 0023
-    usc.update_hidden_prop(apps) # specify 0023
-    usc.update_storage_unique_id_fields(apps) # specify 0024
-    usc.update_co_children_fields(apps) # specify 0027
-    usc.remove_collectionobject_parentco(apps) # specify 0029
-    usc.add_quantities_gift(apps) # specify 0032
-    usc.update_paleo_desc(apps) # specify 0033
-    usc.update_accession_date_fields(apps) # specify 0034
+def log_and_run(funcs: Iterable[MigrationFunction], stdout = None) -> None:
+    for func in funcs:
+        (stdout.write if stdout is not None else logger.info)(f"Running {func.__name__}...")
+        func(apps)
 
-def fix_business_rules():
+def fix_cots(stdout):
+    funcs = [
+        create_default_collection_types,
+        create_default_discipline_for_tree_defs,
+        create_cogtype_type_picklist,
+        set_discipline_for_taxon_treedefs,
+        fix_taxon_treedef_discipline_links,
+        create_cotype_picklist
+    ]
+    log_and_run(funcs, stdout)
+
+def fix_schema_config(stdout):
+    funcs = [
+        usc.create_geo_table_schema_config_with_defaults, # specify 0002
+        usc.create_cotype_splocalecontaineritem, # specify 0003
+        usc.create_strat_table_schema_config_with_defaults, # specify 0004 - getting skip warnings
+        usc.create_agetype_picklist, # specify 0004
+        usc.update_cog_type_fields, # specify 0007
+        usc.create_cogtype_picklist, # specify 0007
+        usc.update_cogtype_splocalecontaineritem, # specify 0007
+        usc.update_systemcogtypes_picklist, # specify 0007
+        usc.update_cogtype_type_splocalecontaineritem, # specify 0007
+        usc.update_relative_age_fields, # specify 0008
+        usc.add_cojo_to_schema_config, # specify 0012
+        usc.update_cog_schema_config, # specify 0013
+        usc.update_age_schema_config, # specify 0015
+        usc.schemaconfig_fixes, # specify 0017
+        usc.add_cot_catnum_to_schema, # specify 0018
+        usc.add_tectonicunit_to_pc_in_schema_config, # specify 0020
+        usc.fix_hidden_geo_prop, # specify 0021
+        usc.update_schema_config_field_desc, # specify 0023
+        usc.update_hidden_prop, # specify 0023
+        usc.update_storage_unique_id_fields, # specify 0024
+        usc.update_co_children_fields, # specify 0027
+        usc.remove_collectionobject_parentco, # specify 0029
+        usc.add_quantities_gift, # specify 0032
+        usc.update_paleo_desc, # specify 0033
+        usc.update_accession_date_fields # specify 0034
+    ]
+    log_and_run(funcs, stdout)
+
+def apply_default_uniqueness_rules_to_disciplines(apps):
     Discipline = apps.get_model('specify', 'Discipline')
     UniquenessRule = apps.get_model('businessrules', 'UniquenessRule')
 
@@ -69,22 +83,39 @@ def fix_business_rules():
         id__in=set(UniquenessRule.objects.values_list('discipline_id', flat=True).distinct())):
         apply_default_uniqueness_rules(discipline, registry=apps)
 
-    catnum_rule_editable(apps)
-    create_uniqueness_rule("Storage", None, True, ["uniqueIdentifier"], [], apps)
-    fix_global_default_rules(apps)
 
-def fix_permissions():
+def fix_business_rules(stdout):
+    funcs = [
+        apply_default_uniqueness_rules_to_disciplines,
+        catnum_rule_editable,
+        fix_global_default_rules
+    ]
+    log_and_run(funcs, stdout)
+
+def initialize_permissions(apps):
     initialize(False, apps)
-    add_permission(apps)
-    add_stats_edit_permission(apps)
 
-def fix_tectonic_ranks():
-    create_default_tectonic_ranks(apps)
-    create_root_tectonic_node(apps)
-    fix_tectonic_unit_treedef_discipline_links(apps)
+def fix_permissions(stdout):
+    funcs = [
+        initialize_permissions,
+        add_permission,
+        add_stats_edit_permission
+    ]
+    log_and_run(funcs, stdout)
 
-def fix_misc():
-    make_selectseries_false(apps) # specify 0031
+def fix_tectonic_ranks(stdout):
+    funcs = [
+        create_default_tectonic_ranks,
+        create_root_tectonic_node,
+        fix_tectonic_unit_treedef_discipline_links
+    ]
+    log_and_run(funcs, stdout)
+
+def fix_misc(stdout):
+    funcs = [
+        make_selectseries_false # specify 0031
+    ]
+    log_and_run(funcs, stdout)
 
 class Command(BaseCommand):
     help = "Runs this Django command to re-run important data migrations functions"
@@ -103,7 +134,7 @@ class Command(BaseCommand):
         func_name = options.get("function")
 
         funcs = {
-            "apply_patches": lambda: apply_patches(apps),
+            "apply_patches": lambda _stdout: apply_patches(apps),
             "fix_cots": fix_cots,
             "fix_permissions": fix_permissions,
             "fix_business_rules": fix_business_rules,
@@ -123,12 +154,12 @@ class Command(BaseCommand):
                     self.stdout.write(
                         self.style.SUCCESS(f"Running only {func_name}...")
                     )
-                    funcs[func_name]()
+                    funcs[func_name](self.stdout)
                 else:
                     self.stdout.write(self.style.SUCCESS("Running full pipeline..."))
                     for func_name, func in funcs.items():
-                        self.stdout.write(f"Applying {func_name}...")
-                        func()
+                        self.stdout.write(self.style.SUCCESS(f"Applying {func_name}..."))
+                        func(self.stdout)
                         self.stdout.write(self.style.SUCCESS(f"Applied {func_name}"))
         except Exception as e:
             logger.error(f"An error occurred: {e}")
