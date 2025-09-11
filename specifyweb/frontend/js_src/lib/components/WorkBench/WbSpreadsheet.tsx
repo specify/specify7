af4f2ee0b0c966581aec7106c88158ca1cbbb613
Gitesh Sagvekar <giteshsagvekar07@gmail.com>
2025-09-11 10:31:18 -0400

    Fix: replace legacy table icons with SvgIcon

--- specifyweb/frontend/js_src/lib/components/WorkBench/WbSpreadsheet.tsx
+++ specifyweb/frontend/js_src/lib/components/WorkBench/WbSpreadsheet.tsx
@@ -7,13 +7,13 @@ import type Handsontable from 'handsontable';
 import type { DetailedSettings } from 'handsontable/plugins/contextMenu';
 import { registerAllModules } from 'handsontable/registry';
 import React from 'react';
-
+import { renderToStaticMarkup } from 'react-dom/server';
 import { commonText } from '../../localization/common';
 import { LANGUAGE } from '../../localization/utils/config';
 import { wbText } from '../../localization/workbench';
 import type { RA } from '../../utils/types';
 import { writable } from '../../utils/types';
-import { iconClassName, legacyNonJsxIcons } from '../Atoms/Icons';
+import { iconClassName, icons } from '../Atoms/Icons';
 import { ReadOnlyContext } from '../Core/Contexts';
 import { strictGetTable } from '../DataModel/tables';
 import { getIcon, unknownIcon } from '../InitialContext/icons';
@@ -68,53 +68,53 @@ function WbSpreadsheetComponent({
     hot === undefined
       ? undefined
       : {
-          items: isUploaded
-            ? ({
-                // Display uploaded record
-                upload_results: {
-                  disableSelection: true,
-                  isCommand: false,
-                  renderer: (_, wrapper) => {
-                    const { endRow: visualRow, endCol: visualCol } =
-                      getSelectedRegions(hot).at(-1) ?? {};
-                    const physicalRow = hot.toPhysicalRow(visualRow ?? 0);
-                    const physicalCol = hot.toPhysicalColumn(visualCol ?? 0);
+        items: isUploaded
+          ? ({
+            // Display uploaded record
+            upload_results: {
+              disableSelection: true,
+              isCommand: false,
+              renderer: (_, wrapper) => {
+                const { endRow: visualRow, endCol: visualCol } =
+                  getSelectedRegions(hot).at(-1) ?? {};
+                const physicalRow = hot.toPhysicalRow(visualRow ?? 0);
+                const physicalCol = hot.toPhysicalColumn(visualCol ?? 0);
 
-                    const createdRecords =
-                      validation.uploadResults.interestingRecords[
-                        physicalRow
-                      ]?.[physicalCol];
+                const createdRecords =
+                  validation.uploadResults.interestingRecords[
+                  physicalRow
+                  ]?.[physicalCol];
 
-                    if (
-                      visualRow === undefined ||
-                      visualCol === undefined ||
-                      createdRecords === undefined ||
-                      !cells.isResultCell(
-                        cells.getCellMetaArray(physicalRow, physicalCol)
-                      )
-                    ) {
-                      wrapper.textContent = wbText.noUploadResultsAvailable();
-                      wrapper.parentElement?.classList.add('htDisabled');
-                      const span = document.createElement('span');
-                      span.style.display = 'none';
-                      return span;
-                    }
+                if (
+                  visualRow === undefined ||
+                  visualCol === undefined ||
+                  createdRecords === undefined ||
+                  !cells.isResultCell(
+                    cells.getCellMetaArray(physicalRow, physicalCol)
+                  )
+                ) {
+                  wrapper.textContent = wbText.noUploadResultsAvailable();
+                  wrapper.parentElement?.classList.add('htDisabled');
+                  const span = document.createElement('span');
+                  span.style.display = 'none';
+                  return span;
+                }
 
-                    wrapper.setAttribute(
-                      'class',
-                      `${wrapper.getAttribute('class')} flex flex-col !m-0
+                wrapper.setAttribute(
+                  'class',
+                  `${wrapper.getAttribute('class')} flex flex-col !m-0
                     pb-1`
-                    );
-                    wrapper.innerHTML = createdRecords
-                      .map(([tableName, recordId, label]) => {
-                        const tableLabel =
-                          label === ''
-                            ? strictGetTable(tableName).label
-                            : label;
-                        // REFACTOR: use new table icons
-                        const tableIcon = getIcon(tableName) ?? unknownIcon;
+                );
+                wrapper.innerHTML = createdRecords
+                  .map(([tableName, recordId, label]) => {
+                    const tableLabel =
+                      label === ''
+                        ? strictGetTable(tableName).label
+                        : label;
+                    // REFACTOR: use new table icons
+                    const tableIcon = getIcon(tableName) ?? unknownIcon;
 
-                        return `<a
+                    return `<a
                         class="link"
                         href="/specify/view/${tableName}/${recordId}/"
                         target="_blank"
@@ -124,57 +124,57 @@ function WbSpreadsheetComponent({
                         <span
                           title="${commonText.opensInNewTab()}"
                           aria-label="${commonText.opensInNewTab()}"
-                        >${legacyNonJsxIcons.link}</span>
+                          >${renderToStaticMarkup(icons.externalLink)}</span>
                       </a>`;
-                      })
-                      .join('');
+                  })
+                  .join('');
 
-                    const div = document.createElement('div');
-                    div.style.display = 'none';
-                    return div;
-                  },
-                },
-              } as const)
-            : ({
-                row_above: {
-                  disabled: () => isReadOnly || dataset.isupdate,
-                },
-                row_below: {
-                  disabled: () => isReadOnly || dataset.isupdate,
-                },
-                remove_row: {
-                  disabled: () => {
-                    if (isReadOnly) return true;
-                    // Or if called on the last row
-                    const selectedRegions = getSelectedRegions(hot);
-                    // Allow removing last row in Batch Edit since rows cannot be added in Batch Edit
-                    const disableRemoveLastRow = dataset.isupdate
-                      ? false
-                      : selectedRegions[0].startRow === data.length - 1 &&
-                        selectedRegions[0].startRow ===
-                          selectedRegions[0].endRow;
+                const div = document.createElement('div');
+                div.style.display = 'none';
+                return div;
+              },
+            },
+          } as const)
+          : ({
+            row_above: {
+              disabled: () => isReadOnly || dataset.isupdate,
+            },
+            row_below: {
+              disabled: () => isReadOnly || dataset.isupdate,
+            },
+            remove_row: {
+              disabled: () => {
+                if (isReadOnly) return true;
+                // Or if called on the last row
+                const selectedRegions = getSelectedRegions(hot);
+                // Allow removing last row in Batch Edit since rows cannot be added in Batch Edit
+                const disableRemoveLastRow = dataset.isupdate
+                  ? false
+                  : selectedRegions[0].startRow === data.length - 1 &&
+                  selectedRegions[0].startRow ===
+                  selectedRegions[0].endRow;
 
-                    return selectedRegions.length === 1 && disableRemoveLastRow;
-                  },
-                },
-                disambiguate: {
-                  name: wbText.disambiguate(),
-                  disabled: (): boolean =>
-                    !disambiguation.isAmbiguousCell() || isReadOnly,
-                  callback: handleClickDisambiguate,
-                },
-                ['separator_1' as 'undo']: '---------',
-                fill_down: fillCellsContextMenuItem(hot, 'down', isReadOnly),
-                fill_up: fillCellsContextMenuItem(hot, 'up', isReadOnly),
-                ['separator_2' as 'redo']: '---------',
-                undo: {
-                  disabled: () => !hot.isUndoAvailable() || isReadOnly,
-                },
-                redo: {
-                  disabled: () => !hot.isRedoAvailable() || isReadOnly,
-                },
-              } as const),
-        };
+                return selectedRegions.length === 1 && disableRemoveLastRow;
+              },
+            },
+            disambiguate: {
+              name: wbText.disambiguate(),
+              disabled: (): boolean =>
+                !disambiguation.isAmbiguousCell() || isReadOnly,
+              callback: handleClickDisambiguate,
+            },
+            ['separator_1' as 'undo']: '---------',
+            fill_down: fillCellsContextMenuItem(hot, 'down', isReadOnly),
+            fill_up: fillCellsContextMenuItem(hot, 'up', isReadOnly),
+            ['separator_2' as 'redo']: '---------',
+            undo: {
+              disabled: () => !hot.isUndoAvailable() || isReadOnly,
+            },
+            redo: {
+              disabled: () => !hot.isRedoAvailable() || isReadOnly,
+            },
+          } as const),
+      };
 
   React.useEffect(() => {
     if (hot === undefined || hasBatchEditRolledBack) return;
