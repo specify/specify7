83b58f0caca3643b01406daf4ccc0da122028764
melton-jason <meltonjason12@yahoo.com>
2025-09-12 12:27:25 -0500

    fix(urls):restore backwards-compatability to urls

--- specifyweb/specify/urls.py
+++ specifyweb/specify/urls.py
@@ -5,20 +5,10 @@ from django.urls import include, re_path
 from specifyweb.specify import views
 from specifyweb.specify.models_utils import schema
 
-urlpatterns = [
-    # check if the user is new at login
-    re_path(r'^specify/is_new_user/$', views.is_new_user),
-
-    # the main business data API
-    re_path(r'^specify_schema/openapi.json$', schema.openapi),
-    re_path(r'^specify_schema/(?P<model>\w+)/$', schema.view),
-    re_path(r'^specify/(?P<model>\w+)/(?P<id>\d+)/$', views.resource), # permissions added
-    re_path(r'^specify/(?P<model>\w+)/$', views.collection), # permissions added
-
-    # this url always triggers a 500 for testing purposes
-    re_path(r'^test_error/', views.raise_error),
-
-    # === Backwards compatibility ===
+backward_compatible_urls = [
+    # The paths of these urls were changed in v7.11.2. To maintain backwards 
+    # compatibilty with services using the old structure, they are aliased here
+    # See #7352
 
     # Merge endpoints
     re_path(r'^specify/merge/', include('specifyweb.backend.merge.urls')),
@@ -27,7 +17,7 @@ urlpatterns = [
     re_path(r'^specify/', include('specifyweb.backend.inheritance.urls')),
 
     # Series endpoints
-    re_path(r'^specify', include('specifyweb.backend.series.urls')),
+    re_path(r'^specify/', include('specifyweb.backend.series.urls')),
 
     # Table rows
     re_path(r'^', include('specifyweb.backend.table_rows.urls')),
@@ -36,7 +26,7 @@ urlpatterns = [
     re_path(r'^', include('specifyweb.backend.delete_blockers.urls')),
 
     ## Bulk copy
-    re_path(r'^specify', include('specifyweb.backend.bulk_copy.urls')),
+    re_path(r'^specify/', include('specifyweb.backend.bulk_copy.urls')),
 
     # Trees
     re_path(r'^', include('specifyweb.backend.trees.urls')),
@@ -46,4 +36,20 @@ urlpatterns = [
 
     # Master key + User management
     re_path(r'^', include('specifyweb.backend.accounts.urls')),
+]
+
+urlpatterns = [
+    *backward_compatible_urls,
+
+    # check if the user is new at login
+    re_path(r'^specify/is_new_user/$', views.is_new_user),
+
+    # this url always triggers a 500 for testing purposes
+    re_path(r'^test_error/', views.raise_error),
+
+    # the main business data API
+    re_path(r'^specify_schema/openapi.json$', schema.openapi),
+    re_path(r'^specify_schema/(?P<model>\w+)/$', schema.view),
+    re_path(r'^specify/(?P<model>\w+)/(?P<id>\d+)/$', views.resource), # permissions added
+    re_path(r'^specify/(?P<model>\w+)/$', views.collection), # permissions added
 ]
