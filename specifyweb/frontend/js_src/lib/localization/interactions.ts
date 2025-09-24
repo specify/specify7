f9833b10aa371405efafc9bb45a5e02878ae2a15
Miguel Díaz de León <migueldlm1307@gmail.com>
2025-09-24 15:02:53 +0000

    Sync localization strings with Weblate
    Triggered by 839239e9d069f130e58c46eed838a4333f6b9da8 on branch refs/heads/weblate-localization

--- specifyweb/frontend/js_src/lib/localization/interactions.ts
+++ specifyweb/frontend/js_src/lib/localization/interactions.ts
@@ -100,7 +100,7 @@ export const interactionsText = createDictionary({
   withoutPreparations: {
     "en-us": "Without preparations",
     "ru-ru": "Без подготовки",
-    "es-es": "Sin preparativos",
+    "es-es": "Sin preparaciones",
     "fr-fr": "Sans préparations",
     "uk-ua": "Без підготовки",
     "de-ch": "Ohne Präparate",
@@ -109,7 +109,7 @@ export const interactionsText = createDictionary({
   continueWithoutPreparations: {
     "en-us": "Continue without preparations",
     "de-ch": "Ohne Vorbereitungen weitermachen",
-    "es-es": "Continuar sin preparativos",
+    "es-es": "Continuar sin preparaciones",
     "fr-fr": "Continuer sans préparation",
     "ru-ru": "Продолжить без подготовки",
     "uk-ua": "Продовжуйте без підготовки",
@@ -127,7 +127,7 @@ export const interactionsText = createDictionary({
   preparations: {
     "en-us": "Preparations",
     "ru-ru": "Препараты",
-    "es-es": "Preparativos",
+    "es-es": "Preparaciones",
     "fr-fr": "Preparations",
     "uk-ua": "Підготовка",
     "de-ch": "Präparate",
@@ -136,7 +136,7 @@ export const interactionsText = createDictionary({
   preparationsCanNotBeReturned: {
     "en-us": "Preparations cannot be returned in this context.",
     "ru-ru": "Препараты в данном случае возврату не подлежат.",
-    "es-es": "En este contexto no es posible retomar los preparativos.",
+    "es-es": "En este contexto no es posible retomar las preparaciones",
     "fr-fr": "Les preparations ne peuvent être renvoyées dans ce contexte.",
     "uk-ua": "У цьому контексті препарати не можна повернути.",
     "de-ch": "Präparate können in diesem Kontext nicht zurückgegeben werden.",
@@ -146,7 +146,7 @@ export const interactionsText = createDictionary({
     "en-us": "There are no unresolved preparations for this loan.",
     "ru-ru":
       "Никаких нерешенных подготовительных вопросов по данному кредиту нет.",
-    "es-es": "No existen preparativos pendientes para este préstamo.",
+    "es-es": "No existen preparaciones pendientes para este préstamo.",
     "fr-fr": "Il n’y a pas de preparations non retournées pour ce prêt.",
     "uk-ua":
       "Немає жодних невирішених питань щодо підготовки до цього кредиту.",
@@ -156,7 +156,7 @@ export const interactionsText = createDictionary({
   unresolved: {
     "en-us": "Unresolved",
     "ru-ru": "Нерешенный",
-    "es-es": "Irresoluto",
+    "es-es": "Sin resolver",
     "fr-fr": "Non résolu",
     "uk-ua": "Невирішено",
     "de-ch": "Ungelöst",
