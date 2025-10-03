2a7ad23f62181d0ff324ed80bfa8306cad465e47
Grant Fitzsimmons <37256050+grantfitzsimmons@users.noreply.github.com>
2025-10-03 09:45:14 -0500

    feat(env): improve naming for env variables

--- sp7_db_setup_check.sh
+++ sp7_db_setup_check.sh
@@ -11,34 +11,34 @@ DB_HOST="${DATABASE_HOST}"
 DB_PORT="${DATABASE_PORT}"
 MASTER_USER_NAME="${MASTER_NAME:-$MASTER_USER_NAME}"
 MASTER_USER_PASSWORD="${MASTER_PASSWORD:-$MASTER_USER_PASSWORD}"
-SUPER_USER_NAME="${SUPER_USER_NAME:-$SUPER_NAME}"
-SUPER_USER_PASSWORD="${SUPER_USER_PASSWORD:-$SUPER_PASSWORD}"
+MIGRATOR_NAME="${MIGRATOR_NAME}"
+MIGRATOR_PASSWORD="${MIGRATOR_PASSWORD}"
 DB_ROOT_PASSWORD="${MYSQL_ROOT_PASSWORD}"
 DB_NAME="${DATABASE_NAME}"
-TARGET_USER_NAME="${TARGET_NAME:-$TARGET_USER_NAME}"
-TARGET_USER_PASSWORD="${TARGET_PASSWORD:-$TARGET_USER_PASSWORD}"
+APP_USER_NAME="${APP_USER_NAME}"
+APP_USER_PASSWORD="${APP_USER_PASSWORD}"
 
 # Use fallback values if needed
-SUPER_USER_PASSWORD=${SUPER_USER_PASSWORD:-$DB_ROOT_PASSWORD}
+MIGRATOR_PASSWORD=${MIGRATOR_PASSWORD:-$DB_ROOT_PASSWORD}
 
-# If super user name is not set, set it to master
-if [[ -z "$SUPER_USER_NAME" ]]; then
-  SUPER_USER_NAME="$MASTER_USER_NAME"
-  SUPER_USER_PASSWORD="$MASTER_USER_PASSWORD"
+# If migrator user name is not set, set it to master
+if [[ -z "$MIGRATOR_NAME" ]]; then
+  MIGRATOR_NAME="$MASTER_USER_NAME"
+  MIGRATOR_PASSWORD="$MASTER_USER_PASSWORD"
 fi
-# If super user name is still not set, set it to root
-if [[ -z "$SUPER_USER_NAME" ]]; then
-  SUPER_USER_NAME="root"
-  SUPER_USER_PASSWORD="$DB_ROOT_PASSWORD"
+# If migrator name is still not set, set it to root
+if [[ -z "$MIGRATOR_NAME" ]]; then
+  MIGRATOR_NAME="root"
+  MIGRATOR_PASSWORD="$DB_ROOT_PASSWORD"
 fi
-# If target user name is not set, set it to super
-if [[ -z "$TARGET_USER_NAME" ]]; then
-  TARGET_USER_NAME="$SUPER_USER_NAME"
-  TARGET_USER_PASSWORD="$SUPER_USER_PASSWORD"
+# If target user name is not set, set it to migrator
+if [[ -z "$APP_USER_NAME" ]]; then
+  APP_USER_NAME="$MIGRATOR_NAME"
+  APP_USER_PASSWORD="$MIGRATOR_PASSWORD"
 fi
 
 # Validate required variables
-if [[ -z "$DB_HOST" || -z "$DB_PORT" || -z "$SUPER_USER_PASSWORD" || -z "$DB_NAME" || -z "$TARGET_USER_NAME" || -z "$TARGET_USER_PASSWORD" ]]; then
+if [[ -z "$DB_HOST" || -z "$DB_PORT" || -z "$MIGRATOR_PASSWORD" || -z "$DB_NAME" || -z "$APP_USER_NAME" || -z "$APP_USER_PASSWORD" ]]; then
   echo "Error: One or more required environment variables are missing or empty."
   exit 1
 fi
@@ -48,8 +48,8 @@ echo "  DB Host: $DB_HOST"
 echo "  DB Port: $DB_PORT"
 echo "  DB Name: $DB_NAME"
 echo "  Master User: $MASTER_USER_NAME"
-echo "  Super User: $SUPER_USER_NAME"
-echo "  Target User: $TARGET_USER_NAME"
+echo "  Super User: $MIGRATOR_NAME"
+echo "  Target User: $APP_USER_NAME"
 
 NEW_DATABASE_CREATED=0
 NEW_SUPER_USER_CREATED=0
@@ -79,25 +79,25 @@ else
 fi
 
 # Create target user if it doesn't exist
-USER_EXISTS=$(mysql -h "$DB_HOST" -P "$DB_PORT" -u "$MASTER_USER_NAME" --password="$MASTER_USER_PASSWORD" -sse "SELECT COUNT(*) FROM mysql.user WHERE user = '$SUPER_USER_NAME' AND host = '$SUPER_USER_HOST';")
-if [[ "$USER_EXISTS" -eq 0 && "$TARGET_USER_NAME" != "root" ]]; then
-  echo "Creating user '$SUPER_USER_NAME'..."
-  echo "Executing: mysql -h \"$DB_HOST\" -P \"$DB_PORT\" -u \"$MASTER_USER_NAME\" --password=\"<hidden>\" -e \"CREATE USER '${SUPER_USER_NAME}'@'${SUPER_USER_HOST}' IDENTIFIED BY '${SUPER_USER_PASSWORD}';\""
-  if mysql -h "$DB_HOST" -P "$DB_PORT" -u "$MASTER_USER_NAME" --password="$MASTER_USER_PASSWORD" -e "CREATE USER '${SUPER_USER_NAME}'@'${SUPER_USER_HOST}' IDENTIFIED BY '${SUPER_USER_PASSWORD}';"; then
+USER_EXISTS=$(mysql -h "$DB_HOST" -P "$DB_PORT" -u "$MASTER_USER_NAME" --password="$MASTER_USER_PASSWORD" -sse "SELECT COUNT(*) FROM mysql.user WHERE user = '$MIGRATOR_NAME' AND host = '$SUPER_USER_HOST';")
+if [[ "$USER_EXISTS" -eq 0 && "$APP_USER_NAME" != "root" ]]; then
+  echo "Creating user '$MIGRATOR_NAME'..."
+  echo "Executing: mysql -h \"$DB_HOST\" -P \"$DB_PORT\" -u \"$MASTER_USER_NAME\" --password=\"<hidden>\" -e \"CREATE USER '${MIGRATOR_NAME}'@'${SUPER_USER_HOST}' IDENTIFIED BY '${MIGRATOR_PASSWORD}';\""
+  if mysql -h "$DB_HOST" -P "$DB_PORT" -u "$MASTER_USER_NAME" --password="$MASTER_USER_PASSWORD" -e "CREATE USER '${MIGRATOR_NAME}'@'${SUPER_USER_HOST}' IDENTIFIED BY '${MIGRATOR_PASSWORD}';"; then
     NEW_SUPER_USER_CREATED=1
   else
     echo "Error: Failed to create user."
     exit 1
   fi
 else
-  echo "User '$SUPER_USER_NAME' already exists."
+  echo "User '$MIGRATOR_NAME' already exists."
 fi
 
 # Grant privileges only if a super new user was created
 if [[ "$NEW_SUPER_USER_CREATED" -eq 1 ]]; then
   echo "Granting privileges to new user..."
-  echo "Executing: mysql -h \"$DB_HOST\" -P \"$DB_PORT\" -u \"$MASTER_USER_NAME\" --password=\"<hidden>\" -e \"GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${SUPER_USER_NAME}'@'${SUPER_USER_HOST}'; FLUSH PRIVILEGES;\""
-  if ! mysql -h "$DB_HOST" -P "$DB_PORT" -u "$MASTER_USER_NAME" --password="$MASTER_USER_PASSWORD" -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${SUPER_USER_NAME}'@'${SUPER_USER_HOST}'; FLUSH PRIVILEGES;"; then
+  echo "Executing: mysql -h \"$DB_HOST\" -P \"$DB_PORT\" -u \"$MASTER_USER_NAME\" --password=\"<hidden>\" -e \"GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${MIGRATOR_NAME}'@'${SUPER_USER_HOST}'; FLUSH PRIVILEGES;\""
+  if ! mysql -h "$DB_HOST" -P "$DB_PORT" -u "$MASTER_USER_NAME" --password="$MASTER_USER_PASSWORD" -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${MIGRATOR_NAME}'@'${SUPER_USER_HOST}'; FLUSH PRIVILEGES;"; then
     echo "Error: Failed to grant privileges to new user."
     exit 1
   fi
@@ -106,25 +106,25 @@ else
 fi
 
 # Create target user if it doesn't exist
-USER_EXISTS=$(mysql -h "$DB_HOST" -P "$DB_PORT" -u "$MASTER_USER_NAME" --password="$MASTER_USER_PASSWORD" -sse "SELECT COUNT(*) FROM mysql.user WHERE user = '$TARGET_USER_NAME' AND host = '$TARGET_USER_HOST';")
-if [[ "$USER_EXISTS" -eq 0 && "$TARGET_USER_NAME" != "root" ]]; then
-  echo "Creating user '$TARGET_USER_NAME'..."
-  echo "Executing: mysql -h \"$DB_HOST\" -P \"$DB_PORT\" -u \"$MASTER_USER_NAME\" --password=\"<hidden>\" -e \"CREATE USER '${TARGET_USER_NAME}'@'${TARGET_USER_HOST}' IDENTIFIED BY '${TARGET_USER_PASSWORD}';\""
-  if mysql -h "$DB_HOST" -P "$DB_PORT" -u "$MASTER_USER_NAME" --password="$MASTER_USER_PASSWORD" -e "CREATE USER '${TARGET_USER_NAME}'@'${TARGET_USER_HOST}' IDENTIFIED BY '${TARGET_USER_PASSWORD}';"; then
+USER_EXISTS=$(mysql -h "$DB_HOST" -P "$DB_PORT" -u "$MASTER_USER_NAME" --password="$MASTER_USER_PASSWORD" -sse "SELECT COUNT(*) FROM mysql.user WHERE user = '$APP_USER_NAME' AND host = '$TARGET_USER_HOST';")
+if [[ "$USER_EXISTS" -eq 0 && "$APP_USER_NAME" != "root" ]]; then
+  echo "Creating user '$APP_USER_NAME'..."
+  echo "Executing: mysql -h \"$DB_HOST\" -P \"$DB_PORT\" -u \"$MASTER_USER_NAME\" --password=\"<hidden>\" -e \"CREATE USER '${APP_USER_NAME}'@'${TARGET_USER_HOST}' IDENTIFIED BY '${APP_USER_PASSWORD}';\""
+  if mysql -h "$DB_HOST" -P "$DB_PORT" -u "$MASTER_USER_NAME" --password="$MASTER_USER_PASSWORD" -e "CREATE USER '${APP_USER_NAME}'@'${TARGET_USER_HOST}' IDENTIFIED BY '${APP_USER_PASSWORD}';"; then
     NEW_TARGET_USER_CREATED=1
   else
     echo "Error: Failed to create user."
     exit 1
   fi
 else
-  echo "User '$TARGET_USER_NAME' already exists."
+  echo "User '$APP_USER_NAME' already exists."
 fi
 
 # Grant privileges only if a target new user was created
 if [[ "$NEW_TARGET_USER_CREATED" -eq 1 ]]; then
   echo "Granting privileges to new user..."
-  echo "Executing: mysql -h \"$DB_HOST\" -P \"$DB_PORT\" -u \"$MASTER_USER_NAME\" --password=\"<hidden>\" -e \"GRANT SELECT, INSERT, UPDATE, DELETE, CREATE TEMPORARY TABLES, LOCK TABLES, EXECUTE ON \`${DB_NAME}\`.* TO '${TARGET_USER_NAME}'@'${TARGET_USER_HOST}'; FLUSH PRIVILEGES;\""
-  if ! mysql -h "$DB_HOST" -P "$DB_PORT" -u "$MASTER_USER_NAME" --password="$MASTER_USER_PASSWORD" -e "GRANT SELECT, INSERT, UPDATE, ALTER, INDEX, DELETE, CREATE TEMPORARY TABLES, LOCK TABLES, EXECUTE ON \`${DB_NAME}\`.* TO '${TARGET_USER_NAME}'@'${TARGET_USER_HOST}'; FLUSH PRIVILEGES;"; then
+  echo "Executing: mysql -h \"$DB_HOST\" -P \"$DB_PORT\" -u \"$MASTER_USER_NAME\" --password=\"<hidden>\" -e \"GRANT SELECT, INSERT, UPDATE, DELETE, CREATE TEMPORARY TABLES, LOCK TABLES, EXECUTE ON \`${DB_NAME}\`.* TO '${APP_USER_NAME}'@'${TARGET_USER_HOST}'; FLUSH PRIVILEGES;\""
+  if ! mysql -h "$DB_HOST" -P "$DB_PORT" -u "$MASTER_USER_NAME" --password="$MASTER_USER_PASSWORD" -e "GRANT SELECT, INSERT, UPDATE, ALTER, INDEX, DELETE, CREATE TEMPORARY TABLES, LOCK TABLES, EXECUTE ON \`${DB_NAME}\`.* TO '${APP_USER_NAME}'@'${TARGET_USER_HOST}'; FLUSH PRIVILEGES;"; then
     echo "Error: Failed to grant privileges to new user."
     exit 1
   fi
