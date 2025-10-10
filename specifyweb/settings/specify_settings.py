851b4998ba36d93ff397b5585298e7e5c8103060
alec_dev <acwhite211@gmail.com>
2025-10-10 13:59:28 -0500

    try reverting MasterUser

--- specifyweb/settings/specify_settings.py
+++ specifyweb/settings/specify_settings.py
@@ -57,12 +57,18 @@ DATABASE_OPTIONS = {}
 
 # The master user login. This is the MySQL user used to connect to the
 # database. This can be the same as the Specify 6 master user.
-MASTER_NAME = os.environ.get('MASTER_NAME', 'root')
-MASTER_PASSWORD = os.environ.get('MASTER_NAME', 'password')
-MIGRATOR_NAME = os.environ.get('MIGRATOR_NAME', MASTER_NAME)
-MIGRATOR_PASSWORD = os.environ.get('MIGRATOR_PASSWORD', MASTER_PASSWORD)
-APP_USER_NAME = os.environ.get('APP_USER_NAME', MIGRATOR_NAME)
-APP_USER_PASSWORD = os.environ.get('APP_USER_PASSWORD', MIGRATOR_PASSWORD)
+# MASTER_NAME = os.environ.get('MASTER_NAME', 'root')
+# MASTER_PASSWORD = os.environ.get('MASTER_NAME', 'password')
+# MIGRATOR_NAME = os.environ.get('MIGRATOR_NAME', MASTER_NAME)
+# MIGRATOR_PASSWORD = os.environ.get('MIGRATOR_PASSWORD', MASTER_PASSWORD)
+# APP_USER_NAME = os.environ.get('APP_USER_NAME', MIGRATOR_NAME)
+# APP_USER_PASSWORD = os.environ.get('APP_USER_PASSWORD', MIGRATOR_PASSWORD)
+MASTER_NAME = 'MasterUser'
+MASTER_PASSWORD = 'MasterPassword'
+MIGRATOR_NAME = 'MasterUser'
+MIGRATOR_PASSWORD = 'MasterPassword'
+TARGET_NAME = 'MasterUser'
+TARGET_PASSWORD = 'MasterPassword'
 
 # The Specify web attachment server URL.
 WEB_ATTACHMENT_URL = None
