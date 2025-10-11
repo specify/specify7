47407e1e03bfd00e2c0948e731f018eaa9d479d4
melton-jason <meltonjason12@yahoo.com>
2025-10-10 22:10:27 -0500

    feat: make support login more secure

--- specifyweb/specify/management/commands/support_login.py
+++ specifyweb/specify/management/commands/support_login.py
@@ -1,9 +1,11 @@
-from optparse import make_option
+from urllib.parse import quote_plus
 
+from Crypto.Random import get_random_bytes
 from django.core.management.base import BaseCommand, CommandError
+from django.utils.http import urlencode
 from django.conf import settings
 
-from specifyweb.specify.auth.support_login import make_token
+from specifyweb.specify.auth.support_login import make_token, bytes_to_b64_url
 from specifyweb.specify.models import Specifyuser
 
 TTL = settings.SUPPORT_LOGIN_TTL
@@ -30,7 +32,8 @@ class Command(BaseCommand):
             def admin(user): return 'admin' if user.is_legacy_admin() else ''
 
             for user in Specifyuser.objects.all():
-                self.stdout.write('\t'.join((user.name, user.usertype, admin(user))))
+                self.stdout.write(
+                    '\t'.join((user.name, user.usertype, admin(user))))
             return
 
         username = options['username']
@@ -41,10 +44,23 @@ class Command(BaseCommand):
         if username is None:
             raise CommandError('username must be supplied')
 
+        # The symmetric encryption algorithm (AES CGM - see make_token) expects
+        # one of three key sizes:
+        # - 128 bit (16 byte)
+        # - 192 bit (24 byte)
+        # - 256 bit (32 byte)
+        # We use the most cryptographically secure key here: doesn't seem to impact
+        # performance too much
+        # This key is essentially a private key, it is used in conjuction with 
+        # the SECRET_KEY of the server to encrypt/decrtpy
+        key = get_random_bytes(32)
+
         try:
             user = Specifyuser.objects.get(name=username)
         except Specifyuser.DoesNotExist:
             raise CommandError('No user with name "%s"' % username)
 
         self.stdout.write("The following token is good for %d seconds:" % TTL)
-        self.stdout.write("/accounts/support_login/?token=" + make_token(user))
+        self.stdout.write(f"Append the token to your server domain to login as {username}")
+        self.stdout.write(
+            f"/accounts/support_login/?token={quote_plus(make_token(user, key))}&key={bytes_to_b64_url(key)}")
