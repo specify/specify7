d99d8c2939fd42312bcefb961d52871b401508f8
Gitesh Sagvekar <giteshsagvekar07@gmail.com>
2025-10-31 21:02:59 -0400

    Fix Entire Table picklist scoping toggle to respect collection prefs

--- specifyweb/frontend/js_src/lib/components/PickLists/__tests__/fetch.test.ts
+++ specifyweb/frontend/js_src/lib/components/PickLists/__tests__/fetch.test.ts
@@ -111,12 +111,21 @@ describe('fetchPickListItems', () => {
     await expect(fetchPickListItems(pickList)).resolves.toEqual(pickListItems);
   });
 
-  overrideAjax('/api/specify/locality/?domainfilter=true&limit=0', {
-    meta: {
-      total_count: 1,
-    },
-    objects: [{ id: 3, _tableName: 'Locality', localityname: 'abc' }],
-  });
+overrideAjax('/api/specify/locality/?domainfilter=true&limit=0', {
+  meta: {
+    total_count: 1,
+  },
+  objects: [{ id: 3, _tableName: 'Locality', localityname: 'abc' }],
+});
+overrideAjax('/api/specify/locality/?domainfilter=false&limit=0', {
+  meta: {
+    total_count: 2,
+  },
+  objects: [
+    { id: 3, _tableName: 'Locality', localityname: 'abc' },
+    { id: 4, _tableName: 'Locality', localityname: 'def' },
+  ],
+});
   test('pick list from entire table', async () => {
     const pickList = deserializeResource(
       addMissingFields('PickList', {
@@ -134,18 +143,18 @@ describe('fetchPickListItems', () => {
     ]);
   });
 
-  overrideAjax('/api/specify/collection/?domainfilter=true&limit=0', {
-    meta: {
-      total_count: 1,
-    },
-    objects: [{ id: 1, _tableName: 'Collection', collectionname: 'abc' }],
+overrideAjax('/api/specify/collection/?domainfilter=true&limit=0', {
+  meta: {
+    total_count: 1,
+  },
+  objects: [{ id: 1, _tableName: 'Collection', collectionname: 'abc' }],
   });
 
 overrideAjax('/api/specify/collection/?domainfilter=false&limit=0', {
-    meta: {
-      total_count: 2,
-    },
-    objects: [
+  meta: {
+    total_count: 2,
+  },
+  objects: [
       { id: 1, _tableName: 'Collection', collectionname: 'abc' },
       { id: 2, _tableName: 'Collection', collectionname: 'cba' },
     ],
