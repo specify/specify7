e9d05225a925af694684deeddc8681e5f843b9fd
alesan99 <alesanesp99@gmail.com>
2025-12-12 11:24:11 -0600

    Add discipline picklist to run_key_migration_functions

--- specifyweb/specify/migrations/0042_discipline_type_picklist.py
+++ specifyweb/specify/migrations/0042_discipline_type_picklist.py
@@ -1,64 +1,5 @@
 from django.db import migrations
-from specifyweb.backend.context.app_resource import DISCIPLINE_NAMES
-
-DISCIPLINE_TYPE_PICKLIST_NAME = 'DisciplineType'
-
-def create_discipline_type_picklist(apps):
-    Collection = apps.get_model('specify', 'Collection')
-    Picklist = apps.get_model('specify', 'Picklist')
-    Picklistitem = apps.get_model('specify', 'Picklistitem')
-
-    # Create a discipline type picklist for each collection
-    for collection in Collection.objects.all():
-        picklist, created = Picklist.objects.get_or_create(
-            name=DISCIPLINE_TYPE_PICKLIST_NAME,
-            type=0,
-            collection=collection,
-            defaults={
-                "issystem": True,
-                "readonly": True,
-                "sizelimit": -1,
-                "sorttype": 1,
-            }
-        )
-        # If the picklist doesn't exist, create a new one
-        if created:
-            ordinal = 1
-            items = []
-            for value, title in DISCIPLINE_NAMES.items():
-                items.append(
-                    Picklistitem(
-                        picklist=picklist,
-                        ordinal=ordinal,
-                        value=value,
-                        title=title,
-                    )
-                )
-                ordinal += 1
-            Picklistitem.objects.bulk_create(items)
-
-def revert_discipline_type_picklist(apps):
-    Picklist = apps.get_model('specify', 'Picklist')
-
-    Picklist.objects.filter(name=DISCIPLINE_TYPE_PICKLIST_NAME).delete()
-
-def update_discipline_type_splocalecontaineritem(apps):
-    Splocalecontaineritem = apps.get_model("specify", "Splocalecontaineritem")
-
-    Splocalecontaineritem.objects.filter(
-        container__name="discipline",
-        container__schematype=0,
-        name="type",
-    ).update(picklistname=DISCIPLINE_TYPE_PICKLIST_NAME, isrequired=True)
-
-def revert_discipline_type_splocalecontaineritem(apps):
-    Splocalecontaineritem = apps.get_model("specify", "Splocalecontaineritem")
-
-    Splocalecontaineritem.objects.filter(
-        container__name="discipline",
-        container__schematype=0,
-        name="type",
-    ).update(picklistname=None, isrequired=None)
+from specifyweb.specify.migration_utils import update_schema_config as usc
 
 class Migration(migrations.Migration):
     dependencies = [
@@ -66,12 +7,12 @@ class Migration(migrations.Migration):
     ]
 
     def apply_migration(apps, schema_editor):
-        create_discipline_type_picklist(apps)
-        update_discipline_type_splocalecontaineritem(apps)
+        usc.create_discipline_type_picklist(apps)
+        usc.update_discipline_type_splocalecontaineritem(apps)
 
     def revert_migration(apps, schema_editor):
-        revert_discipline_type_picklist(apps)
-        revert_discipline_type_splocalecontaineritem(apps)
+        usc.revert_discipline_type_picklist(apps)
+        usc.revert_discipline_type_splocalecontaineritem(apps)
         
     operations = [
         migrations.RunPython(
