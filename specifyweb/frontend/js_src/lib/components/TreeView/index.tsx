4ac17731b8819bd0eb076831b1e5b37859d94164
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2025-12-17 16:59:03 +0000

    Lint code with ESLint and Prettier
    Triggered by c4a039b56c1bf6e794d2c998f350ba81416fd00e on branch refs/heads/issue-7553

--- specifyweb/frontend/js_src/lib/components/TreeView/index.tsx
+++ specifyweb/frontend/js_src/lib/components/TreeView/index.tsx
@@ -162,13 +162,16 @@ function TreeView<TREE_NAME extends AnyTree['tableName']>({
    * below).
    * Instead, the stale remote preference from the old XML configuration was still being
    * used. As a result,name`—which was the default—was always applied.
-   * */
-  // For now, name is being hard-coded to preserve the behavior described in issue-6043.
-  // const sortField = userPreferences.get(
-  //   'treeEditor',
-  //   'behavior',
-  //   'orderByField'
-  // );
+   * 
+   */
+  /*
+   * For now, name is being hard-coded to preserve the behavior described in issue-6043.
+   * const sortField = userPreferences.get(
+   *   'treeEditor',
+   *   'behavior',
+   *   'orderByField'
+   * );
+   */
 
   const includeAuthor = userPreferences.get(
     'treeEditor',
