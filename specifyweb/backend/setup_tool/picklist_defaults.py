ff1c018bbd91f91f0fbdc73e68b155f486dadb7a
alesan99 <alesanesp99@gmail.com>
2026-01-07 12:32:34 -0600

    Fix: Create global picklists even if discipline type has no defaults Fix: Make root ranks required

--- specifyweb/backend/setup_tool/picklist_defaults.py
+++ specifyweb/backend/setup_tool/picklist_defaults.py
@@ -17,34 +17,31 @@ def create_default_picklists(collection: Collection, discipline_type: str | None
     """
     Creates defaults picklists for -one- collection, including discipline specific picklists.
     """
-    # Get global picklists
-    logger.debug('Reading default global picklists.')
+    # Create global picklists
+    logger.debug('Creating default global picklists.')
     defaults = load_json_from_file(Path(__file__).parent.parent.parent.parent / 'config' / 'common' / 'global_picklists.json')
     
     global_picklists = defaults.get('picklists', None) if defaults is not None else None
     if global_picklists is None:
         logger.exception('No global picklists found in global_picklists.json.')
         return
+    
+    create_picklists(global_picklists, collection)
 
     # Get discipline picklists
-    logger.debug('Reading default discipline picklists.')
+    logger.debug('Creating default discipline picklists.')
     if discipline_type is None:
         return
     discipline_defaults = load_json_from_file(Path(__file__).parent.parent.parent.parent / 'config' / 'common' / 'picklists.json')
     
     discipline_picklists = discipline_defaults.get(discipline_type, None)
     if discipline_picklists is None:
-        logger.exception(f'No picklists found for discipline "{discipline_type}" in picklists.json.')
+        logger.warning(f'No picklists found for discipline "{discipline_type}" in picklists.json.')
         return
 
-    # Create the picklists
-    logger.debug('Creating default picklists.')
-    picklists_to_create = []
-    picklists_to_create.extend(global_picklists)
-    picklists_to_create.extend(discipline_picklists)
-    create_picklists(picklists_to_create, collection)
+    create_picklists(discipline_picklists, collection)
  
-def create_picklists(configuraton: list, collection: Collection):
+def create_picklists(configuration: list, collection: Collection):
     """
     Create a set of picklists from a configuration list.
     """
@@ -53,7 +50,7 @@ def create_picklists(configuraton: list, collection: Collection):
         with transaction.atomic():
             # Create picklists in bulk
             picklists_bulk = []
-            for picklist in configuraton:
+            for picklist in configuration:
                 picklists_bulk.append(
                     Picklist(
                         collection=collection,
@@ -77,7 +74,7 @@ def create_picklists(configuraton: list, collection: Collection):
             name_to_obj = {pl.name: pl for pl in picklist_records}
 
             picklistitems_bulk = []
-            for picklist in configuraton:
+            for picklist in configuration:
                 parent = name_to_obj.get(picklist['name'])
                 for picklistitem in picklist['items']:
                     picklistitems_bulk.append(
@@ -91,3 +88,4 @@ def create_picklists(configuraton: list, collection: Collection):
 
     except Exception:
         logger.exception('An error occured when creating default picklists.')
+        raise
