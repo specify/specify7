8ffe028d07e021e4cc6f18f5750a36e067f3d61f
Google Translate <noreply-mt-google-translate@weblate.org>
2026-01-07 18:02:35 +0000

    Sync localization strings with Weblate
    Triggered by 81459f9603f0573d419e95c123e75d30c1fdd399 on branch refs/heads/weblate-localization

--- specifyweb/frontend/js_src/lib/localization/wbPlan.ts
+++ specifyweb/frontend/js_src/lib/localization/wbPlan.ts
@@ -84,7 +84,7 @@ export const wbPlanText = createDictionary({
     'en-us': 'Match Behavior:',
     'ru-ru': 'Поведение при совпадении:',
     'es-es': 'Coincidir en comportamiento:',
-    'fr-fr': 'Comportement de match :',
+    'fr-fr': "Comportement d'adaptation :",
     'uk-ua': 'Поведінка відповідності:',
     'de-ch': 'Übereinstimmungsverhalten:',
     'pt-br': 'Comportamento de correspondência:',
@@ -394,7 +394,7 @@ export const wbPlanText = createDictionary({
     'es-es': 'Seleccione una tabla base con archivos adjuntos',
     'fr-fr': 'Sélectionnez une table de base avec pièces jointes',
     'pt-br': 'Selecione uma mesa base com anexos.',
-    'ru-ru': 'Выберите базовую таблицу с прикреплениями',
+    'ru-ru': 'Выберите базовую таблицу с вложениями.',
     'uk-ua': 'Виберіть базову таблицю з вкладеннями',
   },
   dataSetUploaded: {
@@ -418,7 +418,7 @@ export const wbPlanText = createDictionary({
     'es-es':
       'Está viendo las asignaciones de campos/mapeo para un conjunto de datos ya cargado.\n\nPara editar los mapeos, d´s marcha-atrás para los datos cargados o cree un nuevo conjunto de datos',
     'fr-fr':
-      "Vous visualisez les correspondances d'un jeu de données importé.\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nPour modifier les correspondances, annulez l'importation des données ou créez un nouveau jeu de données.",
+      "Vous visualisez les correspondances d'un jeu de données importé.\n\n\n\n\n\nPour modifier les correspondances, annulez l'importation des données ou créez un nouveau jeu de données.",
     'uk-ua':
       'Ви переглядаєте зіставлення для завантаженого набору даних.\n\nЩоб редагувати зіставлення, відкотіть завантажені дані або створіть новий набір даних',
     'de-ch':
