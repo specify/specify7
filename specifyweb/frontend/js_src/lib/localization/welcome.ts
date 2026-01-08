b394e13f6596eb11452c05c4d5239682ed7a350c
Grant Fitzsimmons <37256050+grantfitzsimmons@users.noreply.github.com>
2026-01-07 18:29:00 -0600

    chore: update copyright year

--- specifyweb/frontend/js_src/lib/localization/welcome.ts
+++ specifyweb/frontend/js_src/lib/localization/welcome.ts
@@ -95,19 +95,7 @@ export const welcomeText = createDictionary({
   },
   licence: {
     'en-us':
-      'Specify 7, Copyright 2025, University of Kansas Center for Research. Specify comes with ABSOLUTELY NO WARRANTY. This is free, open-source software licensed under GNU General Public License v2.',
-    'ru-ru':
-      'Specify 7, Авторские права 2025, University of Kansas для исследования. Specify поставляется с СОВЕРШЕННО ОТСУТСТВИЕМ ГАРАНТИИ. Это бесплатное программное обеспечение с открытым исходным кодом под лицензией GNU General Public License v2.',
-    'es-es':
-      'Specify 7 Copyright © 2025 University of Kansas Center for Research. Specify viene SIN NINGUNA GARANTÍA EN ABSOLUTO. Este es un programa libre, bajo licencia GNU General Public License 2 (GPL2).',
-    'fr-fr':
-      "Specify 7, Copyright 2025, Centre de recherche de l'Université du Kansas. Specify est fourni sans AUCUNE GARANTIE. Il s'agit d'un logiciel libre et open source sous licence GNU General Public License v2.",
-    'uk-ua':
-      'Укажіть 7, авторське право 2025, Дослідницький центр Канзаського університету. Specify поставляється без АБСОЛЮТНОЇ ГАРАНТІЇ. Це безкоштовне програмне забезпечення з відкритим кодом, ліцензоване згідно з GNU General Public License v2.',
-    'de-ch':
-      'Specify 7, Copyright 2025, University of Kansas Center for Research. Specify kommt mit ABSOLUT KEINER GARANTIE. Dies ist freie, quelloffene Software, lizenziert unter GNU General Public License v2.',
-    'pt-br':
-      'Especifique 7, Copyright 2025, Centro de Pesquisa da Universidade do Kansas. Especifique NÃO OFERECE NENHUMA GARANTIA. Este é um software gratuito e de código aberto, licenciado sob a GNU General Public License v2.',
+      'Specify 7, Copyright 2026, University of Kansas Center for Research. Specify comes with ABSOLUTELY NO WARRANTY. This is free, open-source software licensed under GNU General Public License v3.',
   },
   systemInformation: {
     'en-us': 'System Information',
@@ -164,7 +152,7 @@ export const welcomeText = createDictionary({
     'pt-br': 'Versão do banco de dados:',
   },
   schemaVersion: {
-    'en-us': 'Database Schema Version:',
+    'en-us': 'Database Schema',
     'ru-ru': 'Версия схемы базы данных',
     'es-es': 'Versión del Esquema de base de datos',
     'fr-fr': 'Version du schéma de base de données',
