d450dc53e61649c273ad97c28db2c2d1a85af397
Iwantexpresso <mmartinez231006@gmail.com>
2026-01-08 12:45:16 -0600

    Revert "canged remove button to use Delete button element created"
    This reverts commit 5a796373acc4aa13afe38372373c1f785911a1cb.

--- specifyweb/frontend/js_src/lib/components/FormCells/FormTable.tsx
+++ specifyweb/frontend/js_src/lib/components/FormCells/FormTable.tsx
@@ -41,7 +41,6 @@ import { AttachmentPluginSkeleton } from '../SkeletonLoaders/AttachmentPlugin';
 import { relationshipIsToMany } from '../WbPlanView/mappingHelpers';
 import { COJODialog } from './COJODialog';
 import { FormCell } from './index';
-import { DeleteButton } from '../Forms/DeleteButton';
 
 const cellToLabel = (
   table: SpecifyTable,
@@ -496,17 +495,25 @@ export function FormTable<SCHEMA extends AnySchema>({
                         relationship.relatedTable.name,
                         isDependent ? 'delete' : 'update'
                       )) ? (
-                      <DeleteButton
-                      component ={Button.Small}
-                      deferred={true}
-                      isIcon={true}
-                      resource ={resource}
-                      onDeleted = {(): void => {
-                        if (typeof handleDelete === 'function') {
-                          handleDelete(resource);
+                      <Button.Small
+                        aria-label={commonText.remove()}
+                        className="h-full"
+                        disabled={
+                          (!resource.isNew() &&
+                            !hasTablePermission(
+                              resource.specifyTable.name,
+                              isDependent ? 'delete' : 'update'
+                            )) ||
+                          (renderedResourceId !== undefined &&
+                            resource.id === renderedResourceId) ||
+                          disableRemove
                         }
-                      }}
-                      />
+                        title={commonText.remove()}
+                        // NOTE TO SELF THIS area should retreive parent resources and check if theres any blockers before deleting
+                        onClick={(): void => handleDelete(resource)}
+                      >
+                        {icons.trash}
+                      </Button.Small>
                     ) : undefined}
                     {isExpanded[resource.cid] === true && (
                       <FormMeta
