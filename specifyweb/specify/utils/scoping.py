d48a7a0401bedb5f96b8ccd90eea7468e0e6e63f
melton-jason <meltonjason12@yahoo.com>
2026-01-29 20:37:46 -0600

    fix: resolve typo in static method decorator

--- specifyweb/specify/utils/scoping.py
+++ specifyweb/specify/utils/scoping.py
@@ -218,7 +218,7 @@ class Scoping:
         instance = ModelInstanceScope(obj)
         return instance.scope_type, instance.scope_model
 
-    @staticmethod()
+    @staticmethod
     def model_from_instance(obj: Model) -> Model:
         instance = ModelInstanceScope(obj)
         return instance.scope_model
