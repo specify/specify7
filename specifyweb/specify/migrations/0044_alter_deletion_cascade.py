59c6ab7ee9211ec25f96330077beff4ae1e4ce49
Caroline D <108160931+CarolineDenis@users.noreply.github.com>
2026-02-19 09:22:21 -0500

    Fix: Rearange migrations

--- specifyweb/specify/migrations/0044_alter_deletion_cascade.py
+++ specifyweb/specify/migrations/0044_alter_deletion_cascade.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('specify', '0042_discipline_type_picklist'),
+        ('specify', '0043_normalize_many_to_many'),
     ]
 
     operations = [
