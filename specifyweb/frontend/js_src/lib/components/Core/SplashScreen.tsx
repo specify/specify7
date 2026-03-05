273453eea6f01a55ba57fb4c22350e62c2fb9c29
Grant Fitzsimmons <37256050+grantfitzsimmons@users.noreply.github.com>
2026-03-05 09:07:51 -0600

    crunchy

--- specifyweb/frontend/js_src/lib/components/Core/SplashScreen.tsx
+++ specifyweb/frontend/js_src/lib/components/Core/SplashScreen.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { useHueDifference } from '../../hooks/useHueDifference';
 import { commonText } from '../../localization/common';
+import { useDarkMode } from '../Preferences/Hooks';
 
 export function SplashScreen({
   children,
@@ -9,6 +10,7 @@ export function SplashScreen({
   readonly children: React.ReactNode;
 }): JSX.Element {
   const hueDifference = useHueDifference();
+  const isDarkMode = useDarkMode();
   return (
     <div
       className={`
@@ -27,7 +29,7 @@ export function SplashScreen({
           <img
             alt=""
             className="max-w-xs hover:animate-hue-rotate"
-            src="/static/img/logo.svg"
+            src={isDarkMode ? '/static/img/logo.svg' : '/static/img/logo_dark.svg'}
             style={{ filter: `hue-rotate(${hueDifference}deg)` }}
           />
         </header>
