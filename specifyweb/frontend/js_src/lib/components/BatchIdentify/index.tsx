99d58765f781a7a0c734ba0f01843d0706f63704
Grant Fitzsimmons <37256050+grantfitzsimmons@users.noreply.github.com>
2026-03-05 21:35:28 -0600

    fix tests

--- specifyweb/frontend/js_src/lib/components/BatchIdentify/index.tsx
+++ specifyweb/frontend/js_src/lib/components/BatchIdentify/index.tsx
@@ -170,7 +170,7 @@ const fetchRecordSetCollectionObjectIds = async (
   const limit = 2000;
   let offset = 0;
   let totalCount = 0;
-  const collectionObjectIds: readonly number[] = [];
+  const collectionObjectIds: number[] = [];
 
   do {
     const { records, totalCount: fetchedTotalCount } = await fetchCollection(
