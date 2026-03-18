adb24be27e1764650bf663de2301fbb59070d3c6
alec_dev <acwhite211@gmail.com>
2026-03-18 13:09:50 -0500

    Merge pull request #7812 from specify/issue-7649
    Allow new discipline deletion when it has no users and no collections

--- specifyweb/backend/delete_blockers/views.py
+++ specifyweb/backend/delete_blockers/views.py
@@ -1,9 +1,13 @@
 from django import http
-from django.db import router
+from django.db import router, transaction
 from django.db.models.deletion import Collector
 
 from specifyweb.middleware.general import require_http_methods
-from specifyweb.specify.api.crud import get_object_or_404
+from specifyweb.specify.api.crud import (
+    get_discipline_delete_guard_blockers,
+    get_object_or_404,
+    prepare_discipline_for_delete,
+)
 from specifyweb.specify.api.serializers import toJson
 from specifyweb.specify.views import login_maybe_required
 
@@ -16,10 +20,30 @@ def delete_blockers(request, model, id):
     """
     obj = get_object_or_404(model, id=int(id))
     using = router.db_for_write(obj.__class__, instance=obj)
+
+    if obj._meta.model_name == 'discipline': # Special case for discipline
+        if not request.specify_user.is_admin():
+            return http.HttpResponseForbidden('Specifyuser must be an institution admin')
+        guard_blockers = get_discipline_delete_guard_blockers(obj)
+        if guard_blockers:
+            result = guard_blockers
+        else:
+            # Try pre-delete discipline tree detaching
+            with transaction.atomic(using=using):
+                prepare_discipline_for_delete(obj)
+                result = _collect_delete_blockers(obj, using)
+                transaction.set_rollback(True, using=using)
+    else:
+        # Standard delete blockers behavior
+        result = _collect_delete_blockers(obj, using)
+
+    return http.HttpResponse(toJson(result), content_type='application/json')
+
+def _collect_delete_blockers(obj, using) -> list[dict]:
     collector = Collector(using=using)
     collector.delete_blockers = []
     collector.collect([obj])
-    result = flatten([
+    return flatten([
         [
             {
                 'table': sub_objs[0].__class__.__name__,
@@ -28,7 +52,6 @@ def delete_blockers(request, model, id):
             }
         ] for field, sub_objs in collector.delete_blockers
     ])
-    return http.HttpResponse(toJson(result), content_type='application/json')
 
 def flatten(l):
     return [item for sublist in l for item in sublist]
