0ac84704007e7668aed5efe9c6a5f2a0ae0b8b76
Caroline Denis <caroline@Carolines-MacBook-Pro.local>
2026-03-30 19:10:08 +0200

    Prettier

--- specifyweb/frontend/js_src/lib/components/SchemaConfig/DwCSection.tsx
+++ specifyweb/frontend/js_src/lib/components/SchemaConfig/DwCSection.tsx
@@ -6,74 +6,63 @@ import { Link } from '../Atoms/Link';
 import jsonTerms from '../../data/dwcTerms.json';
 import { darwinCoreText } from '../../localization/dwc';
 
+export function DwCSection({}: {}): JSX.Element {
+  const [isOpen, _, __, handleOpen] = useBooleanState(false);
 
-export function DwCSection({
+  const terms = jsonTerms.dwc.terms;
 
-}: {
+  return (
+    <div className="flex flex-col">
+      <div className="flex">
+        <Label.Block>{darwinCoreText.darwinCore()}</Label.Block>
+        <Button.Icon
+          className={`ml-1`}
+          icon={isOpen ? 'chevronDown' : 'chevronRight'}
+          title="collapse"
+          onClick={handleOpen}
+        />
+      </div>
 
-}): JSX.Element {
-    const [isOpen, _, __, handleOpen] = useBooleanState(false);
+      {isOpen && (
+        <div className="space-y-4">
+          {terms.map((termObj, index) => {
+            const iri = Object.keys(termObj)[0];
+            const term = termObj[iri];
 
-    const terms = jsonTerms.dwc.terms;
-
-    return(
-        <div className='flex flex-col'>
-            <div className='flex'>
-            <Label.Block>
-                {darwinCoreText.darwinCore()}
-            </Label.Block>
-            <Button.Icon
-                className={`ml-1`}
-                icon={isOpen ? 'chevronDown' : 'chevronRight'}
-                title="collapse"
-                onClick={handleOpen}
-            />
-            </div>
-
-            {isOpen && 
-                <div className="space-y-4">
-                    {terms.map((termObj, index) => {
-                    const iri = Object.keys(termObj)[0];
-                    const term = termObj[iri];
-
-                    return (
-                        <div
-                        key={index}
-                        className="p-3"
-                        >
-                        {/* Term name */}
-                        <div className="mb-2">
-                            <span className="text-sm font-semibold mr-2">Term:</span>
-                            <span className="inline-block px-2 py-1 text-sm bg-gray-200 rounded-md text-brand-300">
-                            {term.termName}
-                            </span>
-                        </div>
-
-                        {/* Description */}
-                        <div className="text-sm mb-1">
-                            <span className="font-semibold">Description:</span>{" "}
-                            {term.description}
-                        </div>
-
-                        {/* IRI */}
-                        <div className="text-sm mb-1 mt-2">
-                            <span className="font-semibold">IRI:</span>
-                            <Link.NewTab href={iri} className='ml-1'>
-                            {iri}
-                            </Link.NewTab>
-                        </div>
-
-                        {/* Vocabulary */}
-                        <div className="text-sm mt-2">
-                            <span className="font-semibold">Vocabulary:</span>{" "}
-                            {jsonTerms.dwc.vocabularyURI}
-                        </div>
-                        </div>
-                    );
-                    })}
+            return (
+              <div key={index} className="p-3">
+                {/* Term name */}
+                <div className="mb-2">
+                  <span className="text-sm font-semibold mr-2">Term:</span>
+                  <span className="inline-block px-2 py-1 text-sm bg-gray-200 rounded-md text-brand-300">
+                    {term.termName}
+                  </span>
                 </div>
-            }
+
+                {/* Description */}
+                <div className="text-sm mb-1">
+                  <span className="font-semibold">Description:</span>{' '}
+                  {term.description}
+                </div>
+
+                {/* IRI */}
+                <div className="text-sm mb-1 mt-2">
+                  <span className="font-semibold">IRI:</span>
+                  <Link.NewTab href={iri} className="ml-1">
+                    {iri}
+                  </Link.NewTab>
+                </div>
+
+                {/* Vocabulary */}
+                <div className="text-sm mt-2">
+                  <span className="font-semibold">Vocabulary:</span>{' '}
+                  {jsonTerms.dwc.vocabularyURI}
+                </div>
+              </div>
+            );
+          })}
         </div>
-        
-        )
+      )}
+    </div>
+  );
 }
