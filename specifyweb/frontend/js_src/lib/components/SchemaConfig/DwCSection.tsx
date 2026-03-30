f076c8ffc85b8813b37c5d20190c404b080dc533
Caroline Denis <caroline@Carolines-MacBook-Pro.local>
2026-03-30 18:54:54 +0200

    Feat: Add dwcTerms json file

--- specifyweb/frontend/js_src/lib/components/SchemaConfig/DwCSection.tsx
+++ specifyweb/frontend/js_src/lib/components/SchemaConfig/DwCSection.tsx
@@ -4,7 +4,7 @@ import { darwinCoreText } from '../../localization/DwC';
 import { useBooleanState } from '../../hooks/useBooleanState';
 import { Button } from '../Atoms/Button';
 import { Link } from '../Atoms/Link';
-
+import jsonTerms from '../../data/dwcTerms.json';
 
 
 export function DwCSection({
@@ -14,28 +14,7 @@ export function DwCSection({
 }): JSX.Element {
     const [isOpen, _, __, handleOpen] = useBooleanState(false);
 
-    const jsonTerms = {
-    "dwc": {
-        "desc": "Darwin Core",
-        "abbreviation": "dwc",
-        "vocabularyURI": "http://rs.tdwg.org/dwc/terms/",
-        "lastUpdated": "2023-09-18",
-        "terms": [
-            {"http://rs.tdwg.org/dwc/terms/eventDate": 
-                {
-                    "name": "Event Date", 
-                    "mappingPath": "table:collectionObject → field:startDateOrEndDate", 
-                    "description": "The date-time or interval during which a dwc:Event occurred. For occurrences, this is the date-time when the dwc:Event was recorded. Not suitable for a time in a geological context.", 
-                    "termName": "eventDate"
-                }
-            },
-            {"http://rs.tdwg.org/dwc/terms/basisOfRecord": {"name": "Basis Of Record", "mappingPath": "table:collectionObject → field:basisOfRecord", "description": "The specific nature of the data record.", "termName": "basisOfRecord"}},
-            {"http://rs.tdwg.org/dwc/terms/scientificName": {"name": "Scientific Name", "mappingPath": "table:collectionObject → field:fullName", "description": "The full scientific name, with authorship and date information if known. When forming part of a dwc:Identification, this should be the name in lowest level taxonomic rank that can be determined. This term should not contain identification qualifications, which should instead be supplied in the dwc:identificationQualifier term.", "termName": "scientificName"}},
-            {"http://rs.tdwg.org/dwc/terms/occurrenceID": {"name": "Occurrence ID", "mappingPath": "table:collectionObject → field:guid", "description": "An identifier for the dwc:Occurrence (as opposed to a particular digital record of the dwc:Occurrence). In the absence of a persistent global unique identifier, construct one from a combination of identifiers in the record that will most closely make the dwc:occurrenceID globally unique.", "termName": "occurrenceId"}}
-        ]
-    }
-}
-const terms = jsonTerms.dwc.terms;
+    const terms = jsonTerms.dwc.terms;
 
     return(
         <div className='flex flex-col'>
