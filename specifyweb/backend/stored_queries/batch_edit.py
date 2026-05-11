5db50f58b68ed24d371d31088ce4c4119d678148
alesan99 <alesanesp99@gmail.com>
2026-05-11 10:23:52 -0500

    Group duplicate fields by record

--- specifyweb/backend/stored_queries/batch_edit.py
+++ specifyweb/backend/stored_queries/batch_edit.py
@@ -1096,15 +1096,26 @@ def run_batch_edit_query(props: BatchEditProps):
     # We would have arbitarily sorted the columns, so our columns will not be correct.
     # Rather than sifting the data, we just add a default visual order.
     visual_order_groups: list[list[int]] = [[] for _ in visible_fields]
+    prev_group: int | None = None
+    prev_duplicate_index: int | None = None
     for index, (key, _header) in headers_enumerated:
         # Find the column's original position if it existed in the origin query
         original_place = key[0]
         duplicate_index = key[1]
         if original_place < len(visible_fields):
-            visual_order_groups[original_place].insert(duplicate_index, index)
+            if prev_duplicate_index is not None and duplicate_index > 1 and (duplicate_index == prev_duplicate_index or duplicate_index == prev_duplicate_index-1):
+                # There are duplicate columns, group them by record instead of original query order
+                # Columns are already grouped by record, just preserve the order if the dupe index is greater than the previous.
+                visual_order_groups[prev_group].append(index)
+            else:
+                visual_order_groups[original_place].append(index)
+                prev_group = original_place
         else:
             # New field/column. Add it to the end of the dataset.
             visual_order_groups.append([index])
+            prev_group = len(visual_order_groups)-1
+        
+        prev_duplicate_index = duplicate_index
 
     visual_order: list[int] = []
     for bucket in visual_order_groups:
