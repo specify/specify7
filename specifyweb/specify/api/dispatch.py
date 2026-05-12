1ede349c548ecf73c55cdd5f6d49bb72bcbf82d2
melton-jason <meltonjason12@yahoo.com>
2026-05-12 13:26:46 -0500

    chore: remove duplicated bulk copy functions

--- specifyweb/specify/api/dispatch.py
+++ specifyweb/specify/api/dispatch.py
@@ -116,62 +116,6 @@ def collection_dispatch(request, model) -> HttpResponse:
 
     return resp
 
-def collection_dispatch_bulk(request, model) -> HttpResponse:
-    """
-    Do the same as collection_dispatch, but for bulk POST operations.
-    Call this endpoint with a list of objects of the same type to create.
-    This reduces the amount of API calls needed to create multiple objects, like when creating multiple carry forwards.
-    """
-    checker = table_permissions_checker(request.specify_collection, request.specify_user_agent, "read")
-
-    if request.method != 'POST':
-        return HttpResponseNotAllowed(['POST'])
-
-    data = json.loads(request.body)
-    resp_objs = []
-    with (
-        cache_unique_catnum_preferences(),
-        cache_uniqueness_rules(),
-        cache_permission_queries(),
-    ):
-        for obj_data in data:
-            obj = post_resource(
-                request.specify_collection,
-                request.specify_user_agent,
-                model,
-                obj_data,
-                request.GET.get("recordsetid", None),
-            )
-            resp_objs.append(_obj_to_data(obj, checker))
-
-    return HttpResponseCreated(toJson(resp_objs), content_type='application/json')
-
-def collection_dispatch_bulk_copy(request, model, copies) -> HttpResponse:
-    checker = table_permissions_checker(request.specify_collection, request.specify_user_agent, "read")
-
-    if request.method != 'POST':
-        return HttpResponseNotAllowed(['POST'])
-
-    data = json.loads(request.body)
-    data = dict(filter(lambda item: item[0] != 'id', data.items())) # Remove ID field before making copies
-    resp_objs = []
-    with (
-        cache_unique_catnum_preferences(),
-        cache_uniqueness_rules(),
-        cache_permission_queries(),
-    ):
-        for _ in range(int(copies)):
-            obj = post_resource(
-                request.specify_collection,
-                request.specify_user_agent,
-                model,
-                data,
-                request.GET.get("recordsetid", None),
-            )
-            resp_objs.append(_obj_to_data(obj, checker))
-
-    return HttpResponseCreated(toJson(resp_objs), content_type='application/json')
-
 def rows(request, model_name: str) -> HttpResponse:
     enforce(request.specify_collection, request.specify_user_agent, [f'/table/{model_name.lower()}'], "read")
 
