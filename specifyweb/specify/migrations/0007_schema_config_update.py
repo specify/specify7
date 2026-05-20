7bc1f7926255f3b9625b491f66cc38b038d5d1c9
alec_dev <acwhite211@gmail.com>
2026-05-20 14:21:39 -0500

    Revert "Make migration edits while preserving PR review fixes"
    This reverts commit e8a005f092e4bf937299fdc51ed3567d878eed4f.

--- specifyweb/specify/migrations/0007_schema_config_update.py
+++ specifyweb/specify/migrations/0007_schema_config_update.py
@@ -16,122 +16,7 @@ Creates a picklist for COGType -> type and updates an existing incorrect picklis
 from django.db import migrations, models
 import django.db.models.deletion
 
-from specifyweb.specify.migration_utils.update_schema_config import revert_table_field_schema_config, update_table_field_schema_config_with_defaults
-from specifyweb.specify.migration_utils.sp7_schemaconfig import MIGRATION_0007_FIELDS as SCHEMA_CONFIG_MOD_TABLE_FIELDS
-
-PICKLIST_NAME = 'COGTypes'
-COGTYPE_FIELD_NAME = 'cogType'
-SYSTEM_COGTYPE_PICKLIST_NAME = "SystemCOGTypes"
-
-def update_fields(apps):
-    Discipline = apps.get_model('specify', 'Discipline')
-    Splocalecontaineritem = apps.get_model('specify', 'Splocalecontaineritem')
-    Splocaleitemstr = apps.get_model('specify', 'Splocaleitemstr')
-
-    # Revert COG -> children before adding to avoid duplicates
-    revert_table_field_schema_config('CollectionObjectGroup', 'children', apps)
-    # Add StorageTreeDef -> institution and COG -> children
-    for discipline in Discipline.objects.all():
-        for table, fields in SCHEMA_CONFIG_MOD_TABLE_FIELDS.items():
-            for field in fields: 
-                update_table_field_schema_config_with_defaults(table, discipline.id, field, apps)
-
-
-    # Remove COG -> cojo
-    revert_table_field_schema_config('CollectionObjectGroup', 'cojo', apps)
-
-    # Remove duplicate CollectionObject -> collectionObjectType
-    container_items = Splocalecontaineritem.objects.filter(name='collectionObjectType', picklistname=None, container__name='CollectionObject')
-    for container_item in container_items:
-        Splocaleitemstr.objects.filter(itemname=container_item).delete()
-        Splocaleitemstr.objects.filter(itemdesc=container_item).delete()
-    container_items.delete()
-
-# NOTE: The reverse function will not re-add the duplicate CO -> coType or COG -> cojo as its unnecessary
-def revert_update_fields(apps):
-    # Remove StorageTreeDef -> institution and COG -> children
-    for table, fields in SCHEMA_CONFIG_MOD_TABLE_FIELDS.items():
-            for field in fields: 
-                revert_table_field_schema_config(table, field, apps)
-        
-def create_cogtype_picklist(apps):
-    Collection = apps.get_model('specify', 'Collection')
-    Picklist = apps.get_model('specify', 'Picklist')
-    
-    # Create a cogtype picklist for each collection
-    for collection in Collection.objects.all():
-        Picklist.objects.get_or_create(
-            name=PICKLIST_NAME,
-            issystem=True,
-            readonly=True,
-            sizelimit=-1,
-            sorttype=1,
-            type=1,
-            tablename='collectionobjectgrouptype',
-            collection=collection,
-            formatter='CollectionObjectGroupType'
-        )
-
-def revert_cogtype_picklist(apps):
-    Picklist = apps.get_model('specify', 'Picklist')
-
-    Picklist.objects.filter(name=PICKLIST_NAME).delete()
-
-# Updates COG -> cogtype to use the type 1 picklist created above
-def update_cogtype_splocalecontaineritem(apps):
-    Splocalecontaineritem = apps.get_model('specify', 'Splocalecontaineritem')
-
-    Splocalecontaineritem.objects.filter(container__name='collectionobjectgroup', container__schematype=0, name=COGTYPE_FIELD_NAME).update(
-        picklistname=PICKLIST_NAME,
-        type='ManyToOne',
-        isrequired=True
-    )
-
-def revert_cogtype_splocalecontaineritem(apps):
-    Splocalecontaineritem = apps.get_model('specify', 'Splocalecontaineritem')
-
-    Splocalecontaineritem.objects.filter(container__name='collectionobjectgroup', container__schematype=0, name=COGTYPE_FIELD_NAME).update(
-        picklistname=None,
-        type=None,
-        isrequired=None
-    )
-
-def update_systemcogtypes_picklist(apps):
-    Picklist = apps.get_model('specify', 'Picklist')
-
-    Picklist.objects.filter(name='Default Collection Object Group Types').update(
-        name=SYSTEM_COGTYPE_PICKLIST_NAME,
-        type=0,
-        issystem=True,
-        readonly=True,
-        sizelimit=3,
-        tablename=None
-    )
-
-def revert_systemcogtypes_picklist(apps):
-    Picklist = apps.get_model('specify', 'Picklist')
-
-    # revert only changes the name and not the other attributes as those were incorrect
-    Picklist.objects.filter(name=SYSTEM_COGTYPE_PICKLIST_NAME).update(
-        name='Default Collection Object Group Types',
-    )
-
-# Updates cogtype -> type to use the Default COGType picklist (Drill Core, Discrete, Consolidated)
-def update_cogtype_type_splocalecontaineritem(apps):
-    Splocalecontaineritem = apps.get_model('specify', 'Splocalecontaineritem')
-
-    Splocalecontaineritem.objects.filter(container__name='collectionobjectgrouptype', container__schematype=0, name='type').update(
-        picklistname=SYSTEM_COGTYPE_PICKLIST_NAME,
-        isrequired=True
-    )
-
-def revert_cogtype_type_splocalecontaineritem(apps):
-    Splocalecontaineritem = apps.get_model('specify', 'Splocalecontaineritem')
-
-    Splocalecontaineritem.objects.filter(container__name='collectionobjectgrouptype', container__schematype=0, name='type').update(
-        picklistname=None,
-        isrequired=None
-    )
+from specifyweb.specify.migration_utils import update_schema_config as usc
 
 class Migration(migrations.Migration):
     dependencies = [
@@ -139,24 +24,29 @@ class Migration(migrations.Migration):
     ]
 
     def apply_migration(apps, schema_editor):
-        update_fields(apps)
-        create_cogtype_picklist(apps)
-        update_cogtype_splocalecontaineritem(apps)
-        update_systemcogtypes_picklist(apps)
-        update_cogtype_type_splocalecontaineritem(apps)
+        usc.update_cog_type_fields(apps)
+        usc.create_cogtype_picklist(apps)
+        usc.update_cogtype_splocalecontaineritem(apps)
+        usc.update_systemcogtypes_picklist(apps)
+        usc.update_cogtype_type_splocalecontaineritem(apps)
 
     def revert_migration(apps, schema_editor):
-        revert_update_fields(apps)
-        revert_cogtype_picklist(apps)
-        revert_cogtype_splocalecontaineritem(apps)
-        revert_systemcogtypes_picklist(apps)
-        revert_cogtype_type_splocalecontaineritem(apps)
+        usc.revert_cog_type_fields(apps)
+        usc.revert_cogtype_picklist(apps)
+        usc.revert_cogtype_splocalecontaineritem(apps)
+        usc.revert_systemcogtypes_picklist(apps)
+        usc.revert_cogtype_type_splocalecontaineritem(apps)
 
     operations = [
         migrations.AlterField(
-            model_name='collectionobjectgroupjoin',
-            name='parentcog',
-            field=models.ForeignKey(db_column='ParentCOGID', on_delete=django.db.models.deletion.CASCADE, related_name='children', to='specify.collectionobjectgroup'),
+            model_name="collectionobjectgroupjoin",
+            name="parentcog",
+            field=models.ForeignKey(
+                db_column="ParentCOGID",
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="children",
+                to="specify.collectionobjectgroup",
+            ),
         ),
-        migrations.RunPython(apply_migration, revert_migration, atomic=True)
+        migrations.RunPython(apply_migration, revert_migration, atomic=True),
     ]
