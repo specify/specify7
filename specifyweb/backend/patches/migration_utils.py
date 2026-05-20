e31e8382c120be75fa65ed7cd4db4a6d77824edb
alec_dev <acwhite211@gmail.com>
2026-05-20 16:00:51 -0500

    Use migration database alias in patch helpers

--- specifyweb/backend/patches/migration_utils.py
+++ specifyweb/backend/patches/migration_utils.py
@@ -10,6 +10,7 @@ def apply_migrations(app_registry, schema_editor=None):
     update_coordinates(app_registry, schema_editor)
 
 def update_is_accepted(app_registry, schema_editor=None):
+    db_alias = schema_editor.connection.alias if schema_editor is not None else "default"
     for tree in SPECIFY_TREES:
         tree_filters = {
             "isaccepted": False,
@@ -17,20 +18,21 @@ def update_is_accepted(app_registry, schema_editor=None):
         }
 
         tree_model = app_registry.get_model("specify", tree)
-        tree_model.objects.filter(**tree_filters).update(isaccepted=True)
+        tree_model._base_manager.using(db_alias).filter(**tree_filters).update(isaccepted=True)
 
 
 def update_coordinates(app_registry, schema_editor=None):
+    db_alias = schema_editor.connection.alias if schema_editor is not None else "default"
     Locality = app_registry.get_model("specify", "Locality")
 
-    Locality.objects.filter(lat1text__isnull=True, latitude1__isnull=False) \
+    Locality._base_manager.using(db_alias).filter(lat1text__isnull=True, latitude1__isnull=False) \
         .update(lat1text=F("latitude1"))
 
-    Locality.objects.filter(long1text__isnull=True, longitude1__isnull=False) \
+    Locality._base_manager.using(db_alias).filter(long1text__isnull=True, longitude1__isnull=False) \
         .update(long1text=F("longitude1"))
 
-    Locality.objects.filter(lat2text__isnull=True, latitude2__isnull=False) \
+    Locality._base_manager.using(db_alias).filter(lat2text__isnull=True, latitude2__isnull=False) \
         .update(lat2text=F("latitude2"))
 
-    Locality.objects.filter(long2text__isnull=True, longitude2__isnull=False) \
+    Locality._base_manager.using(db_alias).filter(long2text__isnull=True, longitude2__isnull=False) \
         .update(long2text=F("longitude2"))
