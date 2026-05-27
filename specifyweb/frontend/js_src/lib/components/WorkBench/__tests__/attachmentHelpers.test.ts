1ab1510384491710b8200def3198ce3f7cba9a71
alesan99 <alesanesp99@gmail.com>
2026-05-27 10:32:05 -0500

    Add unit tests to attachment helpers

---
+++ specifyweb/frontend/js_src/lib/components/WorkBench/__tests__/attachmentHelpers.test.ts
@@ -0,0 +1,44 @@
+import { theories } from '../../../tests/utils';
+import {
+  attachmentsToCell,
+  formatAttachmentsFromCell,
+  getAttachmentsFromCell,
+  usesAttachments,
+} from '../attachmentHelpers';
+
+theories(attachmentsToCell, [
+  [[[], 'baseTable'], '{"attachments":[],"formatted":""}'],
+]);
+
+theories(getAttachmentsFromCell, [
+  [[''], undefined],
+  [['{"attachments":[],"formatted":""}'], undefined],
+  [
+    [
+      '{"attachments":[{"id":1,"table":"baseTable"}, {"id": 2, "table":"baseTable"}],"formatted":"image1.jpg; image2.jpg"}',
+    ],
+    {
+      attachments: [
+        { id: 1, table: 'baseTable' },
+        { id: 2, table: 'baseTable' },
+      ],
+      formatted: 'image1.jpg; image2.jpg',
+    },
+  ],
+]);
+
+theories(formatAttachmentsFromCell, [
+  [
+    [
+      '{"attachments":[{"id":1,"table":"baseTable"}, {"id": 2, "table":"baseTable"}],"formatted":"image1.jpg; image2.jpg"}',
+    ],
+    'image1.jpg; image2.jpg',
+  ],
+]);
+
+const datasetWithoutAttachments = { columns: [] };
+const datasetWithAttachments = { columns: ['_UPLOADED_ATTACHMENTS'] };
+theories(usesAttachments, [
+  [[datasetWithoutAttachments as never], false],
+  [[datasetWithAttachments as never], true],
+]);
