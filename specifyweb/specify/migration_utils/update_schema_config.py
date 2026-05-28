5ac77e3dcb9e587c6fb55da297adb0dccb754003
CarolineDenis <caroline.denis@specifysoftware.org>
2026-05-28 11:11:28 +0200

    fix: Call componenent specific migration call in componets_schema_config_migrations

--- specifyweb/specify/migration_utils/update_schema_config.py
+++ specifyweb/specify/migration_utils/update_schema_config.py
@@ -1723,9 +1723,9 @@ def reverse_hide_component_fields(apps, schema_editor=None):
 def components_schema_config_migrations(apps, schema_editor=None):
         remove_0029_schema_config_fields(apps, schema_editor)
         create_table_schema_config_with_defaults(apps, schema_editor)
-        update_schema_config_field_desc(apps, schema_editor)
-        update_hidden_prop(apps, schema_editor)
-        create_cotype_splocalecontaineritem(apps)
+        update_schema_config_field_desc_for_components(apps, schema_editor)
+        update_hidden_prop_for_compoenents(apps, schema_editor)
+        create_cotype_splocalecontaineritem_for_components(apps)
         hide_component_fields(apps, schema_editor)
 
 # ##########################################
