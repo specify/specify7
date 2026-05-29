d97acbdc546986eb90c5e2d9bc51bffd5eec1908
CarolineDenis <caroline.denis@specifysoftware.org>
2026-05-29 12:18:51 +0200

    Fix: Incomplete field initialization in conditional create

--- specifyweb/specify/migration_utils/update_schema_config.py
+++ specifyweb/specify/migration_utils/update_schema_config.py
@@ -557,6 +557,9 @@ def update_table_field_schema_config_params(
             name=table.name.lower(),
             discipline_id=discipline_id,
             schematype=table_config.schema_type,
+            ishidden=False,
+            issystem=table.system,
+            version=0,
         )
 
     try:
