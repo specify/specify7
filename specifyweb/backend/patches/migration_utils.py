48face1bab8e85eb87e3c439e077914913abde84
melton-jason <meltonjason12@yahoo.com>
2026-06-03 09:28:26 -0500

    fix: use manager over base manager for tree patches

--- specifyweb/backend/patches/migration_utils.py
+++ specifyweb/backend/patches/migration_utils.py
@@ -19,7 +19,7 @@ def update_is_accepted(app_registry, schema_editor=None):
         }
 
         tree_model = app_registry.get_model("specify", tree)
-        tree_model._base_manager.using(db_alias).filter(**tree_filters).update(isaccepted=True)
+        tree_model.objects.using(db_alias).filter(**tree_filters).update(isaccepted=True)
 
 
 def update_coordinates(app_registry, schema_editor=None):
@@ -27,14 +27,18 @@ def update_coordinates(app_registry, schema_editor=None):
 
     Locality = app_registry.get_model("specify", "Locality")
 
-    Locality._base_manager.using(db_alias).filter(lat1text__isnull=True, latitude1__isnull=False) \
+    Locality.objects.using(db_alias) \
+        .filter(lat1text__isnull=True, latitude1__isnull=False) \
         .update(lat1text=F("latitude1"))
 
-    Locality._base_manager.using(db_alias).filter(long1text__isnull=True, longitude1__isnull=False) \
+    Locality.objects.using(db_alias) \
+        .filter(long1text__isnull=True, longitude1__isnull=False) \
         .update(long1text=F("longitude1"))
 
-    Locality._base_manager.using(db_alias).filter(lat2text__isnull=True, latitude2__isnull=False) \
+    Locality.objects.using(db_alias) \
+        .filter(lat2text__isnull=True, latitude2__isnull=False) \
         .update(lat2text=F("latitude2"))
 
-    Locality._base_manager.using(db_alias).filter(long2text__isnull=True, longitude2__isnull=False) \
+    Locality.objects.using(db_alias) \
+        .filter(long2text__isnull=True, longitude2__isnull=False) \
         .update(long2text=F("longitude2"))
