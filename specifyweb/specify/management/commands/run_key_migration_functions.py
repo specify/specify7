4f93c13cdfa908590aeb2ac5dd87337a0a8e5985
CarolineDenis <caroline.denis@specifysoftware.org>
2026-06-16 13:03:55 +0200

    fix: Typo

--- specifyweb/specify/management/commands/run_key_migration_functions.py
+++ specifyweb/specify/management/commands/run_key_migration_functions.py
@@ -21,7 +21,7 @@ from specifyweb.specify.migration_utils.default_cots import (
 )
 from specifyweb.backend.permissions.initialize import initialize
 from specifyweb.specify.migration_utils import migration_helpers as usc
-from specifyweb.specify.migration_utils.Deduplication import deduplicate_schema_config_orm
+from specifyweb.specify.migration_utils.deduplication import deduplicate_schema_config_orm
 from specifyweb.specify.migration_utils.router import use_migration_connection
 from specifyweb.specify.migration_utils.misc_migrations import make_selectseries_false
 from specifyweb.specify.migration_utils.tectonic_ranks import create_default_tectonic_ranks, create_root_tectonic_node, fix_tectonic_unit_treedef_discipline_links
