63b26930329d705660742327c1e3a5e8bb48247f
melton-jason <meltonjason12@yahoo.com>
2026-06-23 14:15:36 -0500

    refactor: optimizations

--- specifyweb/specify/migration_utils/migration_helpers/helper_0007_schema_config_update.py
+++ specifyweb/specify/migration_utils/migration_helpers/helper_0007_schema_config_update.py
@@ -1,5 +1,7 @@
-from specifyweb.specify.migration_utils.schema_writer import revert_table_field_schema_config, update_table_field_schema_config_with_defaults
+from django.db.models import Q, Exists, OuterRef
 
+from specifyweb.specify.migration_utils.schema_writer import revert_table_field_schema_config, update_table_field_schema_config_with_defaults
+from specifyweb.specify.migration_utils.utils import batch_query
 from specifyweb.specify.migration_utils.migration_helpers.helper_0002_schema_config_update import HISTORICAL_COGTYPES_PICKLIST
 
 # ##########################################
@@ -35,10 +37,12 @@ def update_cog_type_fields(apps):
         name="collectionObjectType",
         picklistname=None,
         container__name="collectionobject",
+        container__schematype=0
     )
-    for container_item in container_items:
-        Splocaleitemstr.objects.filter(itemname=container_item).delete()
-        Splocaleitemstr.objects.filter(itemdesc=container_item).delete()
+    Splocaleitemstr.objects.filter(
+        Q(itemname__in=container_items) | Q(itemdesc__in=container_items),
+        language="en"
+    ).delete()
     container_items.delete()
 
 # NOTE: The reverse function will not re-add the duplicate CO -> coType or COG -> cojo as its unnecessary
@@ -48,24 +52,38 @@ def revert_cog_type_fields(apps):
             for field in fields: 
                 revert_table_field_schema_config(table, field, apps)
 
+
 def create_cogtype_picklist(apps):
     Collection = apps.get_model('specify', 'Collection')
     Picklist = apps.get_model('specify', 'Picklist')
 
+    collections_without_picklists = Collection.objects.annotate(
+        has_existing_picklist=Exists(
+            Picklist.objects.filter(
+                collection_id=OuterRef("pk"),
+                name=COG_PICKLIST_NAME,
+                type=0
+            )
+        )
+    ).filter(has_existing_picklist=False).values_list("pk", flat=True)
+
     # Create a cogtype picklist for each collection
-    for collection in Collection.objects.all():
-        Picklist.objects.update_or_create(
-            collection=collection,
-            name=COG_PICKLIST_NAME,
-            defaults={
-                "type": 1,
-                "tablename": "collectionobjectgrouptype",
-                "issystem": True,
-                "readonly": True,
-                "sizelimit": -1,
-                "sorttype": 1,
-                "formatter": "CollectionObjectGroupType",
-            },
+    for collection_ids in batch_query(collections_without_picklists):
+        Picklist.objects.bulk_create(
+            [
+                Picklist(
+                    name=COG_PICKLIST_NAME,
+                    collection_id=collection_id,
+                    type=1,
+                    tablename="collectionobjectgrouptype",
+                    formatter="CollectionObjectGroupType",
+                    issystem=True,
+                    readonly=True,
+                    sizelimit=-1,
+                    sorttype=1
+                )
+                for collection_id in collection_ids
+            ]
         )
 
 def revert_cogtype_picklist(apps):
