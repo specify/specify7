95aaefe7b3e104402b866688bb0554dcdc2d3944
Caroline Denis <caroline@carolines-macbook-pro-1.home>
2026-07-01 14:42:53 +0200

    Test: Improve test_run_key order

--- specifyweb/specify/management/commands/tests/test_run_key_migration_functions_order.py
+++ specifyweb/specify/management/commands/tests/test_run_key_migration_functions_order.py
@@ -1,51 +1,11 @@
-from contextlib import ExitStack
-from datetime import timedelta
-from io import StringIO
-from types import SimpleNamespace
-from unittest.mock import Mock, call, patch, sentinel
+from unittest.mock import patch, sentinel
 
-from django.apps import apps as django_apps
-from django.test import SimpleTestCase, TestCase
-from django.utils import timezone
-
-from specifyweb.backend.businessrules.models import (
-    UniquenessRule,
-    UniquenessRuleField,
-)
-from specifyweb.specify import models
 from specifyweb.specify.management.commands import run_key_migration_functions as rkm
-from specifyweb.specify.tests.test_api import ApiTests
+
+from specifyweb.specify.management.commands.tests.test_migration_base import MigrationCommandTestCase
 
 
-class KeyMigrationSectionTests(SimpleTestCase):
-    def _recorder(self, name, calls):
-        def func(apps):
-            calls.append((name, apps))
-
-        func.__name__ = name
-        return func
-
-    def _patch_recorders(self, stack, patch_targets, calls):
-        for target, attr in patch_targets:
-            stack.enter_context(
-                patch.object(target, attr, self._recorder(attr, calls))
-            )
-
-    def _assert_section_calls(self, section, patch_targets, expected_names):
-        calls = []
-        stdout = Mock()
-
-        with ExitStack() as stack:
-            stack.enter_context(patch.object(rkm, "apps", sentinel.apps))
-            self._patch_recorders(stack, patch_targets, calls)
-
-            section(stdout)
-
-        self.assertEqual(calls, [(name, sentinel.apps) for name in expected_names])
-        self.assertEqual(
-            stdout.call_args_list,
-            [call(f"Running {name}...") for name in expected_names],
-        )
+class KeyMigrationSectionTests(MigrationCommandTestCase):
 
     def test_log_and_run_without_stdout_still_calls_each_function(self):
         calls = []
