a46b390e320e52a7b737308cb7b1d062fe6ffd0e
melton-jason <meltonjason12@yahoo.com>
2026-07-02 11:05:42 -0500

    fix: remove DATABASE_NAME from docker-compose.yml fixes #8107
    This resulted in the following workflow:
    1. MariaDB would create the database on startup
    2. The sp7_db_setup_check.sh would see the existing database
       and assume it was a Specify 6 DB, inserting the fake specify/0001
       migration
    3. When running the accounts/0001 migration, the operation would fail
       because there is no Specifyuser table

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,7 +8,6 @@ if [ "$1" = 've/bin/gunicorn' ] || [ "$1" = 've/bin/python' ]; then
   echo "Updating static files in /volumes/static-files/."
   rsync -a --delete specifyweb/frontend/static/ /volumes/static-files/frontend-static
   cd /opt/specify7
-  echo "Applying Django migrations."
   if ./sp7_db_setup_check.sh; then
     # sp7_db_setup_check.sh intentionally runs manage.py base_specify_migration and manage.py migrate;
     # only manage.py run_key_migration_functions is invoked here afterward.
