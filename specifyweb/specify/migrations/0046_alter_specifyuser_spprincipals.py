827c6c9a459223f95c0798f15485028164c39449
Caroline Denis <caroline@carolines-macbook-pro.home>
2026-07-07 20:59:52 +0200

    Fix: Migration order

--- specifyweb/specify/migrations/0046_alter_specifyuser_spprincipals.py
+++ specifyweb/specify/migrations/0046_alter_specifyuser_spprincipals.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('specify', '0044_alter_deletion_cascade'),
+        ('specify', '0045_add_indexes'),
     ]
 
     operations = [
