739abc30913ab548a8fc3f6343660d5c2b98a4de
Rijul Poudel <rijulpoudel72@gmail.com>
2026-07-07 16:00:00 -0500

    created a permit object

--- specifyweb/backend/businessrules/tests/test_permit.py
+++ specifyweb/backend/businessrules/tests/test_permit.py
@@ -2,6 +2,7 @@ from django.db.models import ProtectedError
 from specifyweb.specify import models
 from specifyweb.specify.tests.test_api import ApiTests
 from ..exceptions import BusinessRuleException
+import datetime
 
 
 class PermitTests(ApiTests):
@@ -31,3 +32,25 @@ class PermitTests(ApiTests):
 
         aa.delete()
         permit.delete()
+
+    def test_create_permit_with_fields(self):
+        # Fill in Permit#, Type, and Dates fields
+        # Fill in remaining fields
+        permit = models.Permit.objects.create( #creating a permit object
+            institution=self.institution,
+            permitnumber='P-FIELDS-001',
+            type='Collection',
+            startdate=datetime.datetime(2024, 1, 1),
+            enddate=datetime.datetime(2024, 12, 31),
+            issueddate=datetime.datetime(2024, 1, 15),
+            renewaldate=datetime.datetime(2025, 1, 15),
+            status='Active',
+            remarks='Annual collection permit',
+            permittext='Authorized for scientific collection',
+            yesno1=True,
+            text1='Filed under drawer 3',
+            number1=42.5,
+        )
+        
+
+    
