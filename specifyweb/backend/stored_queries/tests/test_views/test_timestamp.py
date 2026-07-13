6be8aa8ee33792f263d10d3fd18c8258af5e787a
Rijul Poudel <rijulpoudel72@gmail.com>
2026-07-13 16:45:56 -0500

    [test]: Add timestampModified coverage to query results test

--- specifyweb/backend/stored_queries/tests/test_views/test_timestamp.py
+++ specifyweb/backend/stored_queries/tests/test_views/test_timestamp.py
@@ -28,6 +28,20 @@ class TestTimestampQuery(SQLAlchemySetup):
                 "isnot": False,
                 "isstrict": False,
                 "_tableName": "SpQueryField"
+            },
+            {
+                "tablelist": "1",
+                "stringid": "1.collectionobject.timestampModified",
+                "fieldname": "timestampModified",
+                "isrelfld": False,
+                "sorttype": 0,
+                "position": 2,
+                "isdisplay": True,
+                "operstart": 8,
+                "startvalue": "",
+                "isnot": False,
+                "isstrict": False,
+                "_tableName": "SpQueryField"
             }
         ]
 
@@ -39,10 +53,13 @@ class TestTimestampQuery(SQLAlchemySetup):
         # Should have 5 results (5 collection objects from setUp)
         self.assertEqual(len(results), 5)
 
-        # Each result should be [id, catalogNumber, timestampCreated]
+        # Each result should be [id, catalogNumber, timestampCreated, timestampModified]
         for i, row in enumerate(results):
             self.assertEqual(row[0], self.collectionobjects[i].id)
             self.assertEqual(row[1], f"num-{i}")
-            # Timestamp should not be None or empty
+            # Timestamp Created should not be None or empty
             self.assertIsNotNone(row[2])
             self.assertNotEqual(row[2], "")
+            # Timestamp Modified should not be None or empty
+            self.assertIsNotNone(row[3])
+            self.assertNotEqual(row[3], "")
