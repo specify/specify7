25ec887b56f1c8a6962c4c961a0a75bb7d1c6044
Rijul Poudel <rijulpoudel72@gmail.com>
2026-07-14 09:15:15 -0500

    [test]: Added test to sort in ascending order

--- specifyweb/backend/stored_queries/tests/test_views/test_sorting.py
+++ specifyweb/backend/stored_queries/tests/test_views/test_sorting.py
@@ -28,5 +28,26 @@ class TestSorting(SQLAlchemySetup):
             self.assertEqual(row[1], f"num-{i}")
 
 
+    @patch("specifyweb.backend.stored_queries.execution.models.session_context")
+    def test_sort_descending(self, context: Mock):
+        context.return_value = TestSorting.test_session_context()
+
+        c = Client()
+        c.force_login(self.specifyuser)
+
+        query = get_simple_query(self.specifyuser).copy()
+        query["fields"][0]["sorttype"] = 2 # 0 --> no sort, 1 --> ascending, 2 --> descending
+
+        response = c.post('/stored_query/ephemeral/', query, content_type="application/json")
+        self._assertStatusCodeEqual(response, 200)
+
+        results = json.loads(response.content.decode())["results"]
+
+        # Should have 5 results (5 collection objects from setUp)
+        self.assertEqual(len(results), 5)
+
+        for i, row in enumerate(results):
+            self.assertEqual(row[1], f"num-{4 - i}")
+
 
 
