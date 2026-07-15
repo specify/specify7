1c69c7fa03a86de7d34026c695012a5a2c891653
Caroline Denis <caroline@carolines-mbp.home>
2026-07-15 09:20:27 +0200

    Fix: Allow to delete unused tree ranks

--- specifyweb/specify/models.py
+++ specifyweb/specify/models.py
@@ -7216,7 +7216,8 @@ class Taxontreedefitem(model_extras.Taxontreedefitem):
     # Relationships: Many-to-One
     createdbyagent = models.ForeignKey('Agent', db_column='CreatedByAgentID', related_name='+', null=True, on_delete=protect_with_blockers)
     modifiedbyagent = models.ForeignKey('Agent', db_column='ModifiedByAgentID', related_name='+', null=True, on_delete=protect_with_blockers)
-    parent = models.ForeignKey('TaxonTreeDefItem', db_column='ParentItemID', related_name='children', null=True, on_delete=models.CASCADE)
+    #TODO: Chnaging parent on_delete to models.DO_NOTHING will break the no default tree deletion feature. Need to figure out how to handle that.
+    parent = models.ForeignKey('TaxonTreeDefItem', db_column='ParentItemID', related_name='children', null=True, on_delete=models.DO_NOTHING)
     treedef = models.ForeignKey('TaxonTreeDef', db_column='TaxonTreeDefID', related_name='treedefitems', null=False, on_delete=models.CASCADE)
 
     class Meta:
