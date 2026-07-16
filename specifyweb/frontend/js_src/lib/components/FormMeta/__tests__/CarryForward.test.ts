ac75998b9778bc64d8361f2155a4a3892462d92c
Rijul Poudel <rijulpoudel72@gmail.com>
2026-07-16 15:21:44 -0500

    [test]: Add unit test for tableValidForBulkClone returning true for CollectionObject

--- specifyweb/frontend/js_src/lib/components/FormMeta/__tests__/CarryForward.test.ts
+++ specifyweb/frontend/js_src/lib/components/FormMeta/__tests__/CarryForward.test.ts
@@ -1,7 +1,15 @@
 import { requireContext } from '../../../tests/helpers';
-import { dependentFields, strictDependentFields } from '../CarryForward';
+import {
+  dependentFields,
+  strictDependentFields,
+  tableValidForBulkClone,
+} from '../CarryForward';
+import { tables } from '../../DataModel/tables';
 
 requireContext();
+
+test('tableValidForBulkClone returns true for CollectionObject', () =>
+  expect(tableValidForBulkClone(tables.CollectionObject)).toBe(true));
 
 test('strictDependentFields', () =>
   expect(strictDependentFields()).toMatchSnapshot());
