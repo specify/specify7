59c74daf17a87630ced60616c0b2dc59f9073246
Rijul Poudel <rijulpoudel72@gmail.com>
2026-07-16 15:27:24 -0500

    [test]: Add unit test for tableValidForBulkClone with regex formatter part

--- specifyweb/frontend/js_src/lib/components/FormMeta/__tests__/CarryForward.test.ts
+++ specifyweb/frontend/js_src/lib/components/FormMeta/__tests__/CarryForward.test.ts
@@ -14,6 +14,16 @@ test('tableValidForBulkClone returns true for CollectionObject', () =>
 test('tableValidForBulkClone returns false for non-CollectionObject table', () =>
   expect(tableValidForBulkClone(tables.Locality)).toBe(false));
 
+test('tableValidForBulkClone returns false when formatter has regex part', () => {
+  const catalogNumber =
+    tables.CollectionObject.strictGetLiteralField('catalogNumber');
+  jest.spyOn(catalogNumber, 'getUiFormatter').mockReturnValue({
+    parts: [{ type: 'regex', canAutonumber: () => false } as any],
+  } as any);
+  expect(tableValidForBulkClone(tables.CollectionObject)).toBe(false);
+  jest.restoreAllMocks();
+});
+
 test('strictDependentFields', () =>
   expect(strictDependentFields()).toMatchSnapshot());
 
