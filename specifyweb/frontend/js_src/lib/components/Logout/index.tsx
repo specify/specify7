15c82a234fe8587faa321652d24b81c5ebb05ee4
g1rly-c0d3r <girly_c0d3r@aol.com>
2026-07-20 12:36:25 -0500

    fix: fix 421 error code caused by race condition from two `.then()` chains

--- specifyweb/frontend/js_src/lib/components/Logout/index.tsx
+++ specifyweb/frontend/js_src/lib/components/Logout/index.tsx
@@ -2,23 +2,22 @@ import React from 'react';
 import { ajax } from '../../utils/ajax';
 import { LoadingScreen } from '../Molecules/Dialog';
 import { formatUrl } from '../Router/queryString';
-import { Http } from '../../utils/ajax/definitions';
+import { LoadingContext } from '../Core/Contexts';
 import { softError } from '../Errors/assert';
 
 export function Logout() : JSX.Element {
- ajax<any>('/accounts/logout/', {
-   method: 'POST',
-   headers: { },
-  })
-  .then(
-    (response) => {
-      if (response.status === Http.OK) {
-	    globalThis.location.assign(formatUrl('/accounts/login/', {next: '/specify/'}))
-      } else {
-		  softError("POST to '/accounts/logout/' failed with code " + response.status + "\nBody:\n" + String(response.data))
-	  }
-  })
-  .catch( (error) => softError(error) );
+  const loading = React.useContext(LoadingContext);
+  
+  loading(
+    ajax<string>('/accounts/logout/', {
+      method: 'POST',
+      headers: {  },
+    })
+	.then( 
+	  () => globalThis.location.assign(formatUrl("/accounts/login/", {next: "/specify/"})),
+	  (error) => softError(error)
+	)
+  )
 
   return <LoadingScreen />
 }
