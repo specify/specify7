0cd483a65eb92028dba81a697eecf5426e214c26
Rijul Poudel <rijulpoudel72@gmail.com>
2026-07-21 15:45:59 -0500

    [test]: show delete unmapped columns for unsaved columns

---
+++ specifyweb/frontend/js_src/lib/components/WbPlanView/__tests__/MapperComponents.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+
+import { commonText } from '../../../localization/common';
+import { MappingsControlPanel } from '../MapperComponents';
+
+// [WorkBench] Delete unmapped columns from a new dataset
+test('shows delete unmapped columns for unsaved columns', () => {
+  render(
+    <MappingsControlPanel
+      columnsNotSaved
+      showHiddenFields={false}
+      onClear={jest.fn()}
+    />
+  );
+
+  expect(
+    screen.getByRole('button', {
+      name: commonText.deleteUnmapped(),
+    })
+  ).toBeInTheDocument();
+});
