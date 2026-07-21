44dc029421a505d9b2b1c72c6daba03b2b98b26a
Rijul Poudel <rijulpoudel72@gmail.com>
2026-07-21 09:30:16 -0500

    [test]: return PDF from report runner

--- specifyweb/backend/report_runner/tests.py
+++ specifyweb/backend/report_runner/tests.py
@@ -1,16 +1,82 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+import json
+from unittest.mock import Mock, patch
 
-Replace this with more appropriate tests for your application.
-"""
+from django.test import Client, override_settings
 
-from django.test import TestCase
+from specifyweb.specify.tests.test_api import ApiTests
 
+class TestRunReport(ApiTests):
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    @override_settings(
+        REPORT_RUNNER_HOST='report-runner',
+        REPORT_RUNNER_PORT='8080',
+    )
+    @patch('specifyweb.backend.report_runner.views.requests.post')
+    @patch('specifyweb.backend.report_runner.views.run_query')
+    def test_run_returns_pdf(
+        self,
+        run_query: Mock,
+        requests_post: Mock,
+    ):
+        query_json = json.dumps({'name': 'New Query'})
+        parameters_json = json.dumps(
+            {'title': 'Collection Object Report'}
+        )
+        report_jrxml = '<jasperReport />'
+        pdf_content = b'%PDF-1.4 test report'
+
+        report_data = {
+            'fields': [
+                'id',
+                '1.collectionobject.catalogNumber',
+            ],
+            'rows': [
+                [
+                    self.collectionobjects[0].id,
+                    'num-0',
+                ],
+            ],
+        }
+
+        run_query.return_value = report_data
+        requests_post.return_value.status_code = 200
+        requests_post.return_value.content = pdf_content
+
+        client = Client()
+        client.force_login(self.specifyuser)
+
+        response = client.post( # calls the real specify endpoint
+            '/report_runner/run/',
+            {
+                'query': query_json,
+                'parameters': parameters_json,
+                'report': report_jrxml,
+            },
+        )
+
+        self._assertStatusCodeEqual(response, 200)
+        self.assertEqual(response['Content-Type'], 'application/pdf') ## it checks if user receives the pdf
+        self.assertEqual(response.content, pdf_content)
+
+        run_query.assert_called_once_with(
+            self.collection,
+            self.specifyuser,
+            query_json,
+        )
+
+        request_url = requests_post.call_args.args[0]
+        request_data = requests_post.call_args.kwargs['data']
+
+        self.assertEqual(
+            request_url,
+            'http://report-runner:8080/report',
+        )
+        self.assertEqual(request_data['report'], report_jrxml)
+        self.assertEqual(
+            request_data['parameters'],
+            parameters_json,
+        )
+        self.assertEqual(
+            json.loads(request_data['data']),
+            report_data,
+        )
