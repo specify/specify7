5a792a07aede1f3236c29352a8b8178c13e613dd
melton-jason <meltonjason12@yahoo.com>
2026-07-21 11:53:41 -0500

    fix: flatten QueryValues list in specify 0035 migration

--- specifyweb/specify/migration_utils/migration_helpers/helper_0035_version_required.py
+++ specifyweb/specify/migration_utils/migration_helpers/helper_0035_version_required.py
@@ -206,7 +206,7 @@ def update_version_required(apps):
     }
 
     # Update the schema config for each discipline with the version isHidden change
-    for discipline_id in Discipline.objects.all().values_list('pk'):
+    for discipline_id in Discipline.objects.all().values_list('pk', flat=True):
         for table, fields in MIGRATION_0035_FIELDS.items():
             for field in fields:    
                 update_table_field_schema_config_params(table, discipline_id, field, updated_config_params, apps)
@@ -218,7 +218,7 @@ def revert_version_required(apps):
     }
 
     # Revert the schema config for each discipline with the version isHidden change
-    for discipline_id in Discipline.objects.all().values_list('pk'):
+    for discipline_id in Discipline.objects.all().values_list('pk', flat=True):
         for table, fields in MIGRATION_0035_FIELDS.items():
             for field in fields:    
                 update_table_field_schema_config_params(table, discipline_id, field, updated_config_params, apps)
