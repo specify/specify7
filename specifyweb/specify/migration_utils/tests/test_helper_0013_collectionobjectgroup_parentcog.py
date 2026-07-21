4a536c16125bd3dbb891552d1fcf146b9abeba8d
melton-jason <meltonjason12@yahoo.com>
2026-07-21 14:38:27 -0500

    fix(tests): remove check for Schema Config field and leave comment

--- specifyweb/specify/migration_utils/tests/test_helper_0013_collectionobjectgroup_parentcog.py
+++ specifyweb/specify/migration_utils/tests/test_helper_0013_collectionobjectgroup_parentcog.py
@@ -9,11 +9,14 @@ from specifyweb.specify.migration_utils.migration_helpers.helper_0013_collection
 
 class UpdateCogSchemaConfigTests(ApiTests):
     def test_update_cog_schema_config(self):
+        # This won't actually create any new Schema Config records, as those
+        # fields don't exist in the newest datamodel
         update_cog_schema_config(apps)
         schema_reader = SchemaReader(self.discipline.pk)
         # FEATURE: update_cog_schema_config also removes any previous
         # COG -> parentCog entry before creating the new defaults based on the
         # datamodel
-        schema_reader.get_field("CollectionObjectGroup", "parentCog")
+
+        # schema_reader.get_field("CollectionObjectGroup", "parentCog")
         with self.assertRaises(MissingRecordError):
             schema_reader.get_field("CollectionObjectGroup", "parentCojo")
