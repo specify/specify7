8aa9cfa2941a9c3c9f21c49e85607db1f448a2ef
Rijul Poudel <rijulpoudel72@gmail.com>
2026-07-21 15:58:35 -0500

    [test]: run delete unmapped columns

--- specifyweb/frontend/js_src/lib/components/WbPlanView/__tests__/MapperComponents.test.tsx
+++ specifyweb/frontend/js_src/lib/components/WbPlanView/__tests__/MapperComponents.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 import React from 'react';
 
 import { commonText } from '../../../localization/common';
+import { mount } from '../../../tests/reactUtils';
 import { MappingsControlPanel } from '../MapperComponents';
 
 // [WorkBench] Delete unmapped columns from a new dataset
@@ -36,4 +37,25 @@ test('hides delete unmapped columns for saved columns', () => {
       name: commonText.deleteUnmapped(),
     })
   ).not.toBeInTheDocument();
+});
+
+// [WorkBench] Delete unmapped columns from a new dataset
+test('runs delete unmapped columns', async () => {
+  const handleClear = jest.fn();
+
+  const { user } = mount(
+    <MappingsControlPanel
+      columnsNotSaved
+      showHiddenFields={false}
+      onClear={handleClear}
+    />
+  );
+
+  await user.click(
+    screen.getByRole('button', {
+      name: commonText.deleteUnmapped(),
+    })
+  );
+
+  expect(handleClear).toHaveBeenCalledTimes(1);
 });
