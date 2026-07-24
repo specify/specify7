a684c355106f3d953e013de97a0bba903d96587b
g1rly-c0d3r <girly_c0d3r@aol.com>
2026-07-24 11:08:56 -0500

    chore: address coderabbit comments:
    level.xml int => string
    bump dependencies for `mypy` and `PyJWT`

--- specifyweb/backend/context/testurls.py
+++ specifyweb/backend/context/testurls.py
@@ -9,7 +9,7 @@ from . import testsviews as views
 urlpatterns = [
     path('collection/', views.collection),
     path('domain.json', views.domain),
-    path('viewsets/<int:level>.xml', views.viewsets),
+    path('viewsets/<str:level>.xml', views.viewsets),
     path('schema_localization.json', views.schema_localization),
     path('app.resource', views.app_resource),
     path('available_related_searches.json', views.available_related_searches),
