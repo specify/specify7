75026ea2810fb945e85cd0ed1e840854aeb970b9
g1rly-c0d3r <girly_c0d3r@aol.com>
2026-07-28 20:05:42 +0000

    Lint code with ESLint and Prettier
    Triggered by d68dac5c5d90a7ef52580877cfc8d1fb1d961575 on branch refs/heads/issue-8046

--- specifyweb/frontend/js_src/lib/components/Logout/index.tsx
+++ specifyweb/frontend/js_src/lib/components/Logout/index.tsx
@@ -6,23 +6,23 @@ import { formatUrl } from '../Router/queryString';
 import { LoadingContext } from '../Core/Contexts';
 import { softError } from '../Errors/assert';
 
-export function Logout() : JSX.Element {
+export function Logout(): JSX.Element {
   const loading = React.useContext(LoadingContext);
-  const [ nextUrl = '/specify/'] = useSearchParameter('nextUrl');
+  const [nextUrl = '/specify/'] = useSearchParameter('nextUrl');
 
   console.log('next => ' + nextUrl);
 
+  React.useEffect(() => {
+    loading(
+      ajax<string>('/accounts/logout/', { method: 'POST', headers: {} })
+        .catch((error) => softError(error))
+        .finally(() =>
+          globalThis.location.assign(
+            formatUrl('/accounts/login/', { next: nextUrl })
+          )
+        )
+    );
+  }, [loading]);
 
-
-  React.useEffect(
-    () => {
-      loading(
-        ajax<string>('/accounts/logout/', { method: 'POST', headers: {  }, })
-          .catch( (error) => softError(error) )
-          .finally( () => globalThis.location.assign(formatUrl("/accounts/login/", {next: nextUrl})) )
-    )},
-    [loading]
-  )
-
-  return <LoadingScreen />
+  return <LoadingScreen />;
 }
