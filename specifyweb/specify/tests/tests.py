6c62b72eebe8e04dfcbab7be94261bdd18db81a3
Grant Fitzsimmons <37256050+grantfitzsimmons@users.noreply.github.com>
2026-07-29 10:25:58 -0500

    fix: import TestCase properly

--- specifyweb/specify/tests/tests.py
+++ specifyweb/specify/tests/tests.py
@@ -3,8 +3,11 @@ Test suite entry point
 """
 
 from django.conf import settings
+from django.test import TestCase
 from jsonschema import validate  # type: ignore
 from jsonschema.exceptions import ValidationError  # type: ignore
+
+from specifyweb.specify.tests.test_load_datamodel import DatamodelTests
 
 if settings.TEST_RUNNER == 'selenium_testsuite_runner.SeleniumTestSuiteRunner':
     pass
