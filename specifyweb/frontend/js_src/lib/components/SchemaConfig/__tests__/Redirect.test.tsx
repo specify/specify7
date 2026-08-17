c595e20c4ff7264b94a682606cbaacc205cc7cbf
Grant Fitzsimmons <37256050+grantfitzsimmons@users.noreply.github.com>
2026-08-16 20:08:26 -0500

    test(schema-config): assert redirect target table name

--- specifyweb/frontend/js_src/lib/components/SchemaConfig/__tests__/Redirect.test.tsx
+++ specifyweb/frontend/js_src/lib/components/SchemaConfig/__tests__/Redirect.test.tsx
@@ -1,14 +1,29 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import { MemoryRouter, Route, Routes, useParams } from 'react-router-dom';
 
 import { requireContext } from '../../../tests/helpers';
+import { defined } from '../../../utils/types';
+import { sortFunction } from '../../../utils/utils';
+import { genericTables } from '../../DataModel/tables';
 import { SchemaConfigRedirect } from '../Redirect';
+import { tablesFilter } from '../Tables';
 
 requireContext();
 
+function TableName(): JSX.Element {
+  const { tableName = '' } = useParams();
+  return <div>{tableName}</div>;
+}
+
 describe('SchemaConfigRedirect', () => {
   test('redirects to the first accessible table', () => {
+    const firstTable = defined(
+      Object.values(genericTables)
+        .filter((table) => tablesFilter(false, false, true, table))
+        .sort(sortFunction(({ name }) => name))[0]
+    );
+
     render(
       <MemoryRouter
         future={{
@@ -23,13 +38,13 @@ describe('SchemaConfigRedirect', () => {
             path="/specify/schema-config/:language"
           />
           <Route
-            element={<div>table page</div>}
+            element={<TableName />}
             path="/specify/schema-config/:language/:tableName"
           />
         </Routes>
       </MemoryRouter>
     );
 
-    expect(screen.getByText('table page')).toBeInTheDocument();
+    expect(screen.getByText(firstTable.name)).toBeInTheDocument();
   });
 });
