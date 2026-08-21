512528dbb36e44ef8306478d55c686894d2ab1bc
g1rly-c0d3r <girly_c0d3r@aol.com>
2026-08-21 13:09:52 -0500

    fix: filter in .ts files

--- specifyweb/frontend/js_src/jest.config.cjs
+++ specifyweb/frontend/js_src/jest.config.cjs
@@ -41,7 +41,7 @@ const config = {
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: [
-      '**/*.{js,jsx,tx,tsx}',
+      '**/*.{js,jsx,ts,tsx}',
       '!**/__tests__/**',
   ],
 
