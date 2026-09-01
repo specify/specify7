413a4b40cc77c6ab4d5a440be829be12f0586386
g1rly-c0d3r <girly_c0d3r@aol.com>
2026-09-01 15:29:18 -0500

    fix: add TextEncoder

--- specifyweb/frontend/js_src/lib/tests/globalSetup.js
+++ specifyweb/frontend/js_src/lib/tests/globalSetup.js
@@ -2,3 +2,12 @@
 export default () => {
   process.env.TZ = 'UTC';
 };
+
+// taken from  https://github.com/remix-run/react-router/blob/94d4351290267f1a7ed93f8639a588e7a901d24f/packages/react-router/__tests__/setup.ts#L4-L9
+// via https://github.com/remix-run/react-router/issues/12363#issuecomment-2496226528
+// to fix ReferenceError: TextEncoder is not defined
+if (!globalThis.TextEncoder || !globalThis.TextDecoder) {
+  const { TextDecoder, TextEncoder } = require("node:util");
+  globalThis.TextEncoder = TextEncoder;
+  globalThis.TextDecoder = TextDecoder;
+}
