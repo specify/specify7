0df69bcd17ccd199579398456cec35420ab2b43e
g1rly-c0d3r <girly_c0d3r@aol.com>
2026-09-01 16:14:20 -0500

    fix: remove dependency on Max's prettier config

--- specifyweb/frontend/js_src/prettier.config.js
+++ specifyweb/frontend/js_src/prettier.config.js
@@ -1,4 +1,15 @@
-import base from '@maxpatiiuk/prettier-config';
+const base = {
+  // Single quote is more popular among JS libraries
+  singleQuote: true,
+  // GitHub renders Markdown in whitespace-insensitive way, so we should wrap prose
+  proseWrap: 'always',
+  plugins: [
+    '@prettier/plugin-xml',
+    'prettier-plugin-package',
+    'prettier-plugin-sh',
+    'prettier-plugin-tailwindcss',
+  ],
+};
 
 /**
  * @see https://prettier.io/docs/en/configuration.html
