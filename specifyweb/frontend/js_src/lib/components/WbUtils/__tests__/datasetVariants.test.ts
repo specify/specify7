0cc0e4569a6a031bd39d5cb5b020a3a633fc9ac6
kwhuber <kaden.huber@gmail.com>
2026-09-10 17:13:42 -0500

    more WIP of disable relationships for batch edit and enable rollback in user preferences

--- specifyweb/frontend/js_src/lib/components/WbUtils/__tests__/datasetVariants.test.ts
+++ specifyweb/frontend/js_src/lib/components/WbUtils/__tests__/datasetVariants.test.ts
@@ -1,4 +1,3 @@
-// test
 import { hasPermission } from '../../Permissions/helpers';
 import { userPreferences } from '../../Preferences/userPreferences';
 import { datasetVariants } from '../datasetVariants';
@@ -21,3 +20,43 @@ const setBatchEditPreferences = (
   );
   userPreferences.set('batchEdit', 'editor', 'showRollback', showRollback);
 };
+
+beforeAll(() => {
+  jest.useFakeTimers();
+});
+
+afterAll(() => {
+  jest.useRealTimers();
+});
+
+describe('batch edit rollback availability', () => {
+  test.each([
+    [false, true, true, true],
+    [true, true, true, false],
+    [false, false, true, false],
+    [false, true, false, false],
+    [true, false, true, false],
+    [true, true, false, false],
+    [false, false, false, false],
+    [true, false, false, false],
+  ])(
+    'enableRelationships=%s showRollback=%s permission=%s -> canUndo=%s',
+    (enableRelationships, showRollback, permission, expected) => {
+      setBatchEditPreferences(enableRelationships, showRollback);
+      mockedHasPermission.mockReturnValue(permission);
+
+      expect(datasetVariants.batchEdit.canUndo()).toBe(expected);
+    }
+  );
+
+  test('checks the batch edit rollback permission', () => {
+    setBatchEditPreferences(false, true);
+    mockedHasPermission.mockReturnValue(true);
+
+    datasetVariants.batchEdit.canUndo();
+
+    expect(mockedHasPermission).toHaveBeenCalledWith(
+      '/batch_edit/dataset',
+      'rollback'
+    );
+  });
