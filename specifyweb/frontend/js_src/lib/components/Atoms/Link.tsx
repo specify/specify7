49b3f3ec6d3c90eec4a0fbb60b1966a8066b78e6
Grant Fitzsimmons <37256050+grantfitzsimmons@users.noreply.github.com>
2026-09-14 08:50:38 +0100

    fix(keyboard-shortcuts): import link localization

--- specifyweb/frontend/js_src/lib/components/Atoms/Link.tsx
+++ specifyweb/frontend/js_src/lib/components/Atoms/Link.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import type { LocalizedString } from 'typesafe-i18n';
 
+import { commonText } from '../../localization/common';
 import type { IR, RA, RR } from '../../utils/types';
 import { className } from './className';
 import type { IconProps } from './Icons';
