174d522a30bad7068bf375ff0d7dc1af518502ff
Heet Jani <heetjani@BI-C07ZP1XXJYW0.local>
2026-09-15 13:33:32 -0500

    [test]:added test for creating collection event

--- specifyweb/backend/businessrules/tests/test_collectingevents.py
+++ specifyweb/backend/businessrules/tests/test_collectingevents.py
@@ -1,9 +1,22 @@
 from django.db.models import ProtectedError
+from django.utils import timezone
 
 from specifyweb.specify import models
 from specifyweb.specify.tests.test_api import ApiTests
 
 class CollectingEventTests(ApiTests):
+
+    def test_create_date_time(self):
+        startdate = timezone.now()
+
+        ce = models.Collectingevent.objects.create(
+            discipline=self.discipline,
+            startdate=startdate,
+        )
+        fetched_event = models.Collectingevent.objects.get(id=ce.id)
+        self.assertEqual(fetched_event.startdate, startdate)
+        self.assertEqual(fetched_event.discipline, self.discipline)
+
     def test_collectionobjects_block_delete(self):
         ce = models.Collectingevent.objects.create(
             discipline=self.discipline)
