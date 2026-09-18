b92a6be5afd4a813b184c06ce2a3b763763924d4
specify <specify@bi-d6yh9l76ql.home.ku.edu>
2026-09-18 13:33:17 -0500

    [test]: created a file for the unit test for rename existing query

---
+++ specifyweb/backend/businessrules/tests/test_rename_query.py
@@ -0,0 +1,35 @@
+from specifyweb.specify import models
+from specifyweb.specify.tests.test_api import ApiTests
+from specifyweb.specify.api.crud import post_resource, update_obj
+from .raw_query import get_simple_query
+
+
+
+
+class TestRenameQuery(ApiTests):
+
+
+    def setUp(self):
+        super().setUp()
+
+
+    def test_rename_existing_query(self):
+        query = post_resource(
+            self.collection, 
+            self.agent,
+            'spquery',
+            get_simple_query(self.specifyuser)
+        )
+                
+        update_obj(
+            self.collection,
+            self.agent,
+            query.id,
+            query.version,
+            {
+                "name": "Renamed Query",
+            },
+        )
+
+        query.refresh_from_db()
+        self.assertEqual(query.name, "Renamed Query")
